68bc4eec421413857a4a6935aa91fb97ac4842ad
Matt Cavallo <mcavallo@boneal.com>
2018-09-27 18:40:07 -0400

    upd

--- _workstation/Scripts/BingBackground/UpdateBackground.bat
+++ _workstation/Scripts/BingBackground/UpdateBackground.bat
@@ -1,1 +1,3 @@
-wscript.exe .\invisible.vbs "C:\BingBackground\BingBackground.exe"
+%SystemRoot%\System32\wscript.exe "%cd%\invisible.vbs" "%cd%\BingBackground.exe"
+
+REM %SystemRoot%\System32\wscript.exe "C:\ISO\BingBackground\invisible.vbs" "C:\ISO\BingBackground\BingBackground.exe"

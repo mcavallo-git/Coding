be1ffdcf8896713950aaa3735e34db2f1a69495f
Matt Cavallo <39514686+mcavallo-git@users.noreply.github.com>
2018-09-27 23:23:26 -0400

    imagick trimmer

---
+++ batch & cmd/imagick trimmer/Scale_Image_Width_1000px.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM ImageMagick must be installed to computer running this script
+ECHO.
+
+SET "INPUT_DIR=__TO_BE_TRIMMED"
+SET "OUTPUT_DIR=_FINISHED_ITEMS"
+
+FOR %%I IN ("%INPUT_DIR%\*.jpg") DO (
+	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.jpg"
+	REM /***  RESIZE TO 1000px WIDTH  ***/
+	magick "%%I" -resize 1000 "%OUTPUT_DIR%\%%~nI.jpg"
+	REM /***  DELETE INPUT IMAGE AFTER WE CONVERTED TO EXPORT IMAGE  ***/
+	DEL /f "%%I"
+	ECHO.
+)
+
+START explorer.exe "%OUTPUT_DIR%"

604edc6f5779e347e4e57228d08cac7b45e05eef
Matt Cavallo <mcavallo@boneal.com>
2019-01-04 11:46:53 -0500

    Update Service Deleter.bat

--- windows/Scripts/Service Deleter.bat
+++ windows/Scripts/Service Deleter.bat
@@ -1,9 +1,12 @@
+EXIT
 
 REM Deleting a Service
-REM EXAMPLE: Delete service named 'ltsvcmon' under Task Manager -> Services
 
 REM  USING CMD:
-sc.exe delete ltsvcmon
+sc.exe delete SERVICE_NAME
+
+REM EXAMPLE: Delete service named 'ltsvcmon' under Task Manager -> Services
+REM sc.exe delete ltsvcmon
 
 REM USING REGEDIT (PREFERRED METHOD IS CMD)
 REM		HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\services\ltsvcmon

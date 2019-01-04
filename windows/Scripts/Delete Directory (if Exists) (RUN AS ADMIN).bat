4d2843d2317e285c5fa8390f5b817f646d7b8b40
Matt Cavallo <mcavallo@boneal.com>
2019-01-04 11:39:29 -0500

    Update Delete Directory (if Exists) (RUN AS ADMIN).bat

--- windows/Scripts/Delete Directory (if Exists) (RUN AS ADMIN).bat
+++ windows/Scripts/Delete Directory (if Exists) (RUN AS ADMIN).bat
@@ -1,5 +1,5 @@
 
-@ECHO OFF
+@ECHO  OFF
 	SET FolderToDelete=%programdata%\LabTech Client\Cache\
 	CALL:DeleteIfExists "%FolderToDelete%"
 	TIMEOUT /T 10
@@ -7,18 +7,20 @@
 
 	: DeleteIfExists
 	ECHO.
-	ECHO  Attempting to delete [%1]
+	ECHO  In Runtime:
+	ECHO  DeleteIfExists %1
 	ECHO.
 	IF EXIST %1 (
+		ECHO  Calling:
+		ECHO  RMDIR %1 /S /Q
 		RMDIR %1 /S /Q
 		IF EXIST %1 (
-			ECHO Success - Directory [%1] was deleted
+			ECHO  Success - Directory was deleted
 		) ELSE (
-			ECHO Failed - Directory [%1] could not be deleted
+			ECHO  Failed - Directory delete operation failed
 		)
-
 	) ELSE (
-		ECHO Failed - Directory [%1] not found while attempting to delete it)
+		ECHO  Failed - Can't delete a directory which doesn't already exist
 	)
 	ECHO.
 	EXIT /B

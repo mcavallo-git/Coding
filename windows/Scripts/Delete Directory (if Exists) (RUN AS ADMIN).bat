68b1872f7d0d927f5cb43d8ef6b4db617892e547
Matt Cavallo <mcavallo@boneal.com>
2019-01-04 11:37:10 -0500

    Update Delete Directory (if Exists) (RUN AS ADMIN).bat

--- windows/Scripts/Delete Directory (if Exists) (RUN AS ADMIN).bat
+++ windows/Scripts/Delete Directory (if Exists) (RUN AS ADMIN).bat
@@ -1,33 +1,25 @@
 
-@echo off
-
-	set FolderToDelete=%programdata%\LabTech Client\Cache\watchdog.netcomm.com
-
-	Call:DeleteIfExists "%FolderToDelete%"
-
-	timeout 10
-
-	Exit																
+@ECHO OFF
+	SET FolderToDelete=%programdata%\LabTech Client\Cache\
+	CALL:DeleteIfExists "%FolderToDelete%"
+	TIMEOUT /T 10
+	EXIT
 
 	: DeleteIfExists
-
-		ECHO.
-		ECHO  Attempting to delete:
-		ECHO    %1
-		ECHO.
-		
-		if EXIST %1 (
-
+	ECHO.
+	ECHO  Attempting to delete [%1]
+	ECHO.
+	IF EXIST %1 (
 		RMDIR %1 /S /Q
-		ECHO   Success!
-
-		) else (
-
-		ECHO  Cache folder has already been deleted.
-
+		IF EXIST %1 (
+			ECHO Success - Directory [%1] was deleted
+		) ELSE (
+			ECHO Failed - Directory [%1] could not be deleted
 		)
 
-		ECHO.		
+	) ELSE (
+		ECHO Failed - Directory [%1] not found while attempting to delete it)
+	)
+	ECHO.
+	EXIT /B
 
-		Exit /B
-	

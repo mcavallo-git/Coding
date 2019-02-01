e77750455ae033551d1a6f4d9332ee0f6bc0c4aa
Matt Cavallo <mcavallo.it@gmail.com>
2019-02-01 18:48:20 -0500

    Update PowerShell - which (add alias to Get-Command).ps1

--- powershell/PowerShell - which (add alias to Get-Command).ps1
+++ powershell/PowerShell - which (add alias to Get-Command).ps1
@@ -1,17 +1,2 @@
-# PowerShell - which (add alias to Get-Command)
-"`nNew-Alias which Get-Command" | Add-Content ${Profile};
-
-
-# ${Split-Path} = (${Env:Path}).Split(';');
-
-${Env-System} = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path;
-
-
-${Env-User} = Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment';
-
-
-
-
-# Split up the path into an array
-${Split-Path} = (${Env:Path}).Split(';');
-
+# Get the current Environment "Path" Directories (Combined System + User Dirs)
+$EnvPath = (${Env:Path}).Split(';');

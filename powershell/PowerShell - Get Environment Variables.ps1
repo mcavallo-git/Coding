03783fbe90d46c25c1daf45177b07bb9d53313ac
Matt Cavallo <mcavallo.it@gmail.com>
2019-02-01 19:16:55 -0500

    Update PowerShell - Get Environment Variables.ps1

--- powershell/PowerShell - Get Environment Variables.ps1
+++ powershell/PowerShell - Get Environment Variables.ps1
@@ -1,60 +1,43 @@
-# Show all environment variables via Powershell
-Get-ChildItem Env:
-
-# Get Single Environment variable (via powershell)
-Write-Host $Env:Username;
-
-
-# PowerShell - which (add alias to Get-Command)
-"`nNew-Alias which Get-Command" | Add-Content ${Profile};
-
-
 
 $EnvSystem = @();
-$EnvSystem += Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment';
-$EnvUser = @();
-$EnvUser += Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment';
-# $EnvSystem.ComSpec;
-# $EnvSystem.OS;
-# $EnvSystem.Path;
-# 	$EnvSystem.Path.Split(';');
-# $EnvSystem.TEMP;
-# $EnvSystem.TMP;
-# $EnvSystem.windir;
+$EnvSystem += (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment');
+$EnvSystem;
+$EnvSystem.GetType();
+$EnvSystem | Measure-Object;
 
-$EnvUser = @();
-$EnvUser += Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment';
-# $EnvUser.Path;
-# 	$EnvUser.Path.Split(';');
-# $EnvUser.TEMP;
-# $EnvUser.TMP;
+# ($EnvSystem | Measure-Object).Count;
+
+# $EnvUser = @();
+$EnvUser = Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment';
+$EnvUser;
+$EnvUser.GetType();
 
 $EnvAll = @();
 $EnvAll += $EnvSystem;
 $EnvAll += $EnvUser;
-$EnvAll.GetType().FullName;
+$EnvAll;
+$EnvAll.GetType();
 
-foreach($i in $EnvSystem.Keys)
-{
-	$EnvSystem[$i];
-}
-    # $Applications[$i] = @{
-    #     Colour = $i
-    #     Prod = 'SrvProd05'
-    #     QA   = 'SrvQA02'
-    #     Dev  = 'SrvDev12'
-    # }
-}
-foreach(${Key} in $EnvSystem.Keys)
-{
-	if (${Key} in $testInfo.Keys.GetEnumerator())
-    $pet # Print each Key
-    $pets.$pet # Print value of each Key
-}
+$EnvAll.Path;
+$EnvAll.Path.Split(';');
 
-### Split up the path into an array
-#${Var} = (${Env:Path}).Split(';');
-
-
-### Get Var-Type:
+### GetType:
+#
 # $EnvAll.GetType().FullName;
+
+
+### Indices
+#
+# 		$EnvSystem.ComSpec;
+# 		$EnvSystem.OS;
+# 		$EnvSystem.Path;
+# 			$EnvSystem.Path.Split(';');
+# 		$EnvSystem.TEMP;
+# 		$EnvSystem.TMP;
+# 		$EnvSystem.windir;
+#
+# 		$EnvUser.Path;
+# 			$EnvUser.Path.Split(';');
+# 		$EnvUser.TEMP;
+# 		$EnvUser.TMP;
+#

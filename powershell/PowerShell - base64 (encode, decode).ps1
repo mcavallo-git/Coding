5c496dfef48af97ab44fda6563357184c922f865
Matt Cavallo <mcavallo.it@gmail.com>
2019-02-04 12:58:16 -0500

    Create PowerShell - base64 (encode, decode).ps1

---
+++ powershell/PowerShell - base64 (encode, decode).ps1
@@ -0,0 +1,11 @@
+# Base64 - Encode/Encoding a String (via PowerShell):
+$secret_plaintext = "Some secret, i.e. a password, token, etc.";
+$secret_base64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(${secret_plaintext}));
+Write-Host ${secret_base64};
+
+# Base64 - Decode/Decoding a String (via PowerShell):
+$secret_base64 = "UwBvAG0AZQAgAHMAZQBjAHIAZQB0ACwAIABpAC4AZQAuACAAYQAgAHAAYQBzAHMAdwBvAHIAZAAsACAAdABvAGsAZQBuACwAIABlAHQAYwAuAA==";
+$secret_plaintext = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String(${secret_base64}))
+Write-Host ${secret_plaintext};
+
+# Citation: https://adsecurity.org/?p=478

239c904a8aad0ea8121635ca0a6f422beb7f5137
Matt Cavallo <39514686+mcavallo-git@users.noreply.github.com>
2019-02-05 19:50:56 -0500

    Create PowerShell - Set-ExecutionPolicy (allow-disallow local script execution, aka to run locally).ps1

---
+++ powershell/PowerShell - Set-ExecutionPolicy (allow-disallow local script execution, aka to run locally).ps1
@@ -0,0 +1,9 @@
+Exit
+
+# -------------------------------------------------------------------------------------------------------------------------------
+
+Set-ExecutionPolicy RemoteSigned -Force ### Allow Powershell (.ps1) Scripts to run locally
+# Set-ExecutionPolicy Restricted -Force ### Disallow Powershell (.ps1) Scripts from running locally
+# Get-ExecutionPolicy ### Check the status of whether-or-not PowerShell scripts can run locally (or not)
+
+# -------------------------------------------------------------------------------------------------------------------------------

8a5f6d31af523a5a93d142ca3fe2667d3aa95666
Matt Cavallo <mcavallo.it@gmail.com>
2019-02-07 10:31:15 -0500

    Update PowerShell - .NET Core (check-if-installed).ps1

--- powershell/PowerShell - .NET Core (check-if-installed).ps1
+++ powershell/PowerShell - .NET Core (check-if-installed).ps1
@@ -1,7 +1,14 @@
+
 # Using Powershell:
+
 
 # Check .NET Core Runtime Version (if-installed):
 (dir (Get-Command dotnet).Path.Replace('dotnet.exe', 'shared\Microsoft.NETCore.App')).Name
 
+
 # Check .NET Core SDK Version (if-installed):
 (dir (Get-Command dotnet).Path.Replace('dotnet.exe', 'sdk')).Name
+
+
+# Download & Install .NET Core Updated Version(s) from:
+https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/install

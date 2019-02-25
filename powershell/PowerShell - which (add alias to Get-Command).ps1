be4d835f9e994566a45a8db835ba3ca046b8333d
Matt Cavallo <mcavallo.it@gmail.com>
2019-02-25 10:47:06 -0500

    Update PowerShell - which (add alias to Get-Command).ps1

--- powershell/PowerShell - which (add alias to Get-Command).ps1
+++ powershell/PowerShell - which (add alias to Get-Command).ps1
@@ -3,9 +3,3 @@
 
 # PowerShell - Quick-Import
 "`nNew-Alias ImportMods ((`$HOME)+(`"\Documents\WindowsPowerShell\Modules\ImportModules.ps1`"));`nImportMods;" | Add-Content ${Profile};
-
-
-$Path = "C:\temp"
-$Text = "This is the data that I am looking for"
-$PathArray = @()
-$Results = "C:\temp\test.txt"

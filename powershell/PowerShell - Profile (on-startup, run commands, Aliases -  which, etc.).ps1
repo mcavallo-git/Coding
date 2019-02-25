c370c39927c5926157877509f68036c93b85c239
Matt Cavallo <mcavallo.it@gmail.com>
2019-02-25 10:51:47 -0500

    Update PowerShell - Profile (on-startup, run commands, Aliases -  which, etc.).ps1

--- powershell/PowerShell - Profile (on-startup, run commands, Aliases -  which, etc.).ps1
+++ powershell/PowerShell - Profile (on-startup, run commands, Aliases -  which, etc.).ps1
@@ -1,8 +1,2 @@
 
-$Pro = @{};
-
-$Pro.Which = "New-Alias which Get-Command;";
-
-$Pro.ImportMods = "New-Alias ImportMods `"~\Documents\WindowsPowerShell\Modules\ImportModules.ps1`"; ImportMods;";
-
-Foreach ($k In ($Pro.Keys)) { If (!(Get-Content $Profile | Select-String $Pro[$k].Replace("\", "\\"))) { (("`n")+($Pro[$k])) | Add-Content ${Profile}; } }
+## Refer to local file "_WindowsPowerShell/ProfilePrep/ProfilePrep.psm1"

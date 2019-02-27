d3218bbbb01eb3e067169faf7bd2646ec121dc26
Matt Cavallo <mcavallo.it@gmail.com>
2019-02-27 15:12:39 -0500

    Create clouddrive.ps1

---
+++ azure/az cli/clouddrive.ps1
@@ -0,0 +1,5 @@
+
+## "Mount your Azure Cloud Shell to another Storage Account" from "Marco Mansi's Blog"
+##   |-->  https://blog.olandese.nl/2017/09/07/mount-your-azure-cloud-shell-to-another-storage-account/ ]
+
+clouddrive mount -s mySubscription -g myRG -n storageAccountName -f fileShareName

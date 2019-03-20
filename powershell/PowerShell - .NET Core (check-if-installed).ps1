e98b15edad5c80d36d6f4c453e8cc7f6b8736d9b
Matt Cavallo <mcavallo.it@gmail.com>
2019-03-20 16:54:51 -0400

    Update PowerShell - .NET Core (check-if-installed).ps1

--- powershell/PowerShell - .NET Core (check-if-installed).ps1
+++ powershell/PowerShell - .NET Core (check-if-installed).ps1
@@ -3,15 +3,34 @@
 
 
 # Check .NET Core Runtime Version (if-installed):
-(Get-ChildItem -Path (Get-Command dotnet).Path.Replace('dotnet.exe', 'shared\Microsoft.NETCore.App')).Name
+(Get-ChildItem -Path (Get-Command dotnet).Path.Replace('dotnet.exe', 'shared\Microsoft.NETCore.App')).Name;
+(Get-ChildItem -Path (Get-Command dotnet).Path.Replace('dotnet.exe', 'sdk')).Name;
 
 
-# Check .NET Core SDK Version (if-installed):
-(Get-ChildItem -Path (Get-Command dotnet).Path.Replace('dotnet.exe', 'sdk')).Name
 
+# Check [ dotnet ] directly, via --info command
+dotnet --info;
+$DotNet_InfoVersion = ((dotnet --info)[1]).Replace(' Version:   ',''); $DotNet_InfoVersion;
+
+
+
+# Perform an in-depth, extra thorough search for any installs
+$AppName_MatchesAllOf = @();
+$AppName_MatchesAllOf += "Microsoft ";
+$AppName_MatchesAllOf += ".NET Core";
+
+$AppName_Filter = (("Name LIKE '%")+($AppName_MatchesAllOf -Join ("%"))+"%'");
+
+$DotNetCoreApps = (Get-WmiObject -Class Win32_Product -Filter $AppName_Filter);
+
+$DotNetCoreApps | Sort-Object Name | Format-Table Name;
+
+#### App.Uninstall() Non-functional on domain-attached workstation running as local admin (2019-03-20 16:47:22)
+# ForEach ($EachApp In ($DotNetCoreApps)) {
+	# Write-Host (("Uninstalling Application [ ")+($EachApp.Name)+(" ]"));
+	# $EachApp.Uninstall();
+# }
 
 # Download & Install .NET Core Updated Version(s) from:
-https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/install
+# https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/install
 
-
-# sed '/^a test$/{$!{N;s/^a test\nPlease do not$/not a test\nBe/;ty;P;D;:y}}' alpha.txt

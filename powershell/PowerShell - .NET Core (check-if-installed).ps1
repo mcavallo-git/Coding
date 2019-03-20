da6708f0438b9156262a2269eaefc7685f07a987
Matt Cavallo <mcavallo.it@gmail.com>
2019-03-20 16:09:25 -0400

    Update PowerShell - .NET Core (check-if-installed).ps1

--- powershell/PowerShell - .NET Core (check-if-installed).ps1
+++ powershell/PowerShell - .NET Core (check-if-installed).ps1
@@ -3,12 +3,15 @@
 
 
 # Check .NET Core Runtime Version (if-installed):
-(dir (Get-Command dotnet).Path.Replace('dotnet.exe', 'shared\Microsoft.NETCore.App')).Name
+(Get-ChildItem -Path (Get-Command dotnet).Path.Replace('dotnet.exe', 'shared\Microsoft.NETCore.App')).Name
 
 
 # Check .NET Core SDK Version (if-installed):
-(dir (Get-Command dotnet).Path.Replace('dotnet.exe', 'sdk')).Name
+(Get-ChildItem -Path (Get-Command dotnet).Path.Replace('dotnet.exe', 'sdk')).Name
 
 
 # Download & Install .NET Core Updated Version(s) from:
 https://dotnet.microsoft.com/learn/dotnet/hello-world-tutorial/install
+
+
+# sed '/^a test$/{$!{N;s/^a test\nPlease do not$/not a test\nBe/;ty;P;D;:y}}' alpha.txt

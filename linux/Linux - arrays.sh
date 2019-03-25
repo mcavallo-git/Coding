db77b2338d75728e99c5f991829370db1721bf55
mcavallo <programmers@boneal.net>
2019-03-25 15:56:53 -0400

    Create Linux - arrays.sh

---
+++ linux/Linux - arrays.sh
@@ -0,0 +1,107 @@
+
+# Linux - Arrays
+
+#  -------------------------------------------------------------
+#
+# Create an array
+#
+
+DAT_ARRAY=("Item-One" "Item-Two" "Item-A" "Item-B");
+
+# #  ^
+# #  Methods have equivalent output
+# #  v
+
+DAT_ARRAY=();
+DAT_ARRAY+=("Item-One");
+DAT_ARRAY+=("Item-Two");
+DAT_ARRAY+=("Item-A");
+DAT_ARRAY+=("Item-B");
+
+# #  ^
+# #  Methods have equivalent output
+# #  v
+
+DAT_ARRAY=( \
+"Item-One" \
+"Item-Two" \
+"Item-A" \
+"Item-B" \
+);
+
+
+
+#  -------------------------------------------------------------
+#
+#  Walk through an array
+#
+
+DAT_ARRAY=("Item-One" "Item-Two" "Item-A" "Item-B");
+for DAT_ITEM in "${DAT_ARRAY[@]}"; do
+	echo "${DAT_ITEM}";
+done;
+
+
+
+#  -------------------------------------------------------------
+#
+#	Sort an array, remove duplicates from it, then walk through it
+#
+#		NOTE
+#			Each item in the array must NOT have spaces in them for
+#			the [ tr ] (translate) method to sort items as-intended
+#
+
+DAT_ARRAY=("Item-One" "Item-Two" "Item-A" "Item-B");
+DAT_ARRAY_SORTED_NO_DUPES=($(echo "${DAT_ARRAY[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '));
+for DAT_ITEM in "${DAT_ARRAY_SORTED_NO_DUPES[@]}"; do
+	echo "${DAT_ITEM}";
+done;
+
+
+
+#  -------------------------------------------------------------
+#
+#	Reference Arrays as variables whose names are built from other variable-names
+#
+
+DAT_A_ARRAY=("Item-One" "Item-Two");
+DAT_B_ARRAY=("Item-A" "Item-B");
+DAT_C_ARRAY=("Item-B" "Item-One");
+DAT_D_ARRAY=("Item-B" "Item-C" "Item-Two" "Item-Two");
+
+ARRAY_LETTERS=("A" "B" "C" "D");
+
+echo -e "\n\n";
+
+ALL_ITEMS_ARRAY=();
+
+for EACH_ARRAY_LETTER in "${ARRAY_LETTERS[@]}"; do
+
+	EACH_ARRAY_VARNAME=DAT_${EACH_ARRAY_LETTER}_ARRAY[@];
+
+	EACH_DAT_ARRAY=("${!EACH_ARRAY_VARNAME}");
+
+	for EACH_ITEM in "${EACH_DAT_ARRAY[@]}"; do
+
+		ALL_ITEMS_ARRAY+=("${EACH_ITEM}");
+
+		echo "\$ALL_ITEMS_ARRAY+=(\"${EACH_ITEM}\")";
+
+	done;
+	
+done;
+
+echo -e "\n\n";
+
+echo "\$ALL_ITEMS_ARRAY=(${ALL_ITEMS_ARRAY[@]})";
+
+ALL_ITEMS_SORTED_UNIQUE=($(echo "${ALL_ITEMS_ARRAY[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '));
+
+echo -e "\n\n";
+
+echo "\$ALL_ITEMS_SORTED_UNIQUE=(${ALL_ITEMS_SORTED_UNIQUE[@]})";
+
+echo -e "\n\n";
+
+#  -------------------------------------------------------------

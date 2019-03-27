4e976358f7595a283d7c409cc464f5af1da05b43
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-27 15:11:55 -0400

    Update PowerShell - which (add alias to Get-Command).ps1

--- powershell/PowerShell - which (add alias to Get-Command).ps1
+++ powershell/PowerShell - which (add alias to Get-Command).ps1
@@ -1,5 +1,27 @@
-# PowerShell - which (add alias to Get-Command)
+
+#
+# Aliases (PowerShell)
+#
+
+
+
+#
+# Ex) Set an alias to a command & save it to ${Profile} (for next startup)
+#
 "`nNew-Alias which Get-Command;" | Add-Content ${Profile};
 
-# PowerShell - Quick-Import
+
+
+#
+# Ex) Set an alias to a filepath & save it to ${Profile} (for next startup)
+#
 "`nNew-Alias ImportMods ((`$HOME)+(`"\Documents\WindowsPowerShell\Modules\ImportModules.ps1`"));`nImportMods;" | Add-Content ${Profile};
+
+
+
+#
+#	Citation(s)
+#
+#		docs.microsoft.com, "New-Alias"
+#			https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/new-alias?view=powershell-6
+#

a9e7e5b0e622709e202f003bd2b52a18d24fed1d
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-27 12:48:57 -0400

    Update Windows - New-Object -ComObject WScript.Shell (run vbscript commands).ps1

--- windows/Windows - New-Object -ComObject WScript.Shell (run vbscript commands).ps1
+++ windows/Windows - New-Object -ComObject WScript.Shell (run vbscript commands).ps1
@@ -1,56 +1,57 @@
-# Note: SendKeys is incompatible with OS-Specific keystrokes, and as-such, cannot send [ the Windows-Key ] or [ the Macintosh-Key ]
-# Note: SendKeys cannot send the Print-Screen key, {PRTSC} to applications
+
+# ---------------------------------------------------------------------------------------------------------- #
+#
+#	WScript.Shell - SendKeys  https://docs.microsoft.com/en-us/office/vba/language/reference/user-interface-help/sendkeys-statement
+#		-->	Simulates Keystrokes
+#
+
+$TypeIntoStartMenu = "powershell.exe";
+
+# ---------------------------------------------------------------------------------------------------------- #
+# Activators
+#		--> Use activators to simulate holding ALT, CTRL, or SHIFT while simultaneously sending other keystrokes
+#		--> Use activators followed-by a series of parenthesis-wrapped-keystrokes to avoid the "Shift-Up Shift-Down" etc. between each keystroke
+
+$Activators = @{}; # Note: Use activators to hold ALT, CTRL, or SHIFT during keystrokes: i.e. hold SHIFT and type ABC -> +(ABC)
+$Activators.ALT="%";
+$Activators.CTRL="^";
+$Activators.SHIFT="+"; # ex: simulate typing 'ABC' <---[   +A+B+C   ]---OR--[   +(ABC)   ]
+
+# Note: Because SendKeys doesn't support OS-Specific keystrokes, use CTRL+ESC to [ open the Start Menu ]  ( which achieves a similar result to [ pressing the WindowsKey ] )
+$OpenStartMenu = "^{ESC}";
+
+$Shell = (New-Object -ComObject WScript.Shell);
+
+$Shell.SendKeys($OpenStartMenu); Start-Sleep -Milliseconds 100; # Wait for the start-menu to open
+$Shell.SendKeys($TypeIntoStartMenu);
+
+# ---------------------------------------------------------------------------------------------------------- #
+
+# One-Liner - Open the Start Menu
+# 		PowerShell:   (New-Object -ComObject WScript.Shell).SendKeys('^{ESC}');
+# 		CMD:          powershell -command "(New-Object -ComObject WScript.Shell).SendKeys('^{ESC}');"
+# 		VB-Script:    WScript.CreateObject("WScript.Shell").SendKeys "^{ESC}"
 
 
-$oWscript = New-Object -ComObject WScript.Shell;
+# ---------------------------------------------------------------------------------------------------------- #
 
-$oWscript.Popup("Demo Message", 5, "Hello World", 3);
+#
+#	WScript.Shell - Popup
+#		-->	Show a confirmation yes/no/cancel message (which [ times-out & returns -1 ] after 5 seconds)
+(New-Object -ComObject WScript.Shell).Popup("Demo Message", 5, "Hello World", 3);
+#
+#
 
-(New-Object -ComObject WScript.Shell).Popup("Demo Message", 5, "Hello World", 3)
+# ---------------------------------------------------------------------------------------------------------- #
 
-(New-Object -ComObject WScript.Shell).SendKeys('^{ESC}')
 
-# Soln: Simulate a start-menu keypress by opening the start-menu with 'CTRL+ESC'
-(New-Object -ComObject WScript.Shell).SendKeys('^{ESC}');
 
 
 
 
 
 #
-# CMD / Batch Scripts 
-#		Using 'mshta', which is IE's [ Microsoft HTML Application Host ]
-#		Note: Ensure that 'mshta' command refers to 'C:\Windows\System32\mshta.exe' (common malware fakeout)
-#
-#
-# C:\Windows\System32\mshta.exe vbscript:Execute("MsgBox(""Demo Message"",64,""Hello World"")(window.close)")
-#
-# C:\Windows\System32\mshta.exe vbscript:Execute("WshShell.SendKeys ""^{ESC}""")
-#
-
-
-#
-# VB-Script
-#
-#
-# Set wShell=wscript.createobject("wscript.shell")
-# (wscript.createobject("wscript.shell")).SendKeys "^{ESC}"
-# Set WshShell = Nothing
-#
-
-
-
-
-
-
-
-
-
-
-#
-#	Citations
-#
-#		(SendKeys, General) docs.microsoft.com "SendKeys statement": https://docs.microsoft.com/en-us/office/vba/language/reference/user-interface-help/sendkeys-statement
+#	Citations (other)
 #
 #		(CMD / Batch Script) Thanks to Stack Overflow user 'tinyfiledialogs' on the forum: https://stackoverflow.com/questions/13115508
 #

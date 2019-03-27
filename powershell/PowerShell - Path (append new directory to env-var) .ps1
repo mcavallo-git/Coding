4749e4e99b3f046a2ff42a825de382d62c70f94a
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-26 23:29:52 -0400

    Update PowerShell - Path (append new directory to env-var) .ps1

--- powershell/PowerShell - Path (append new directory to env-var) .ps1
+++ powershell/PowerShell - Path (append new directory to env-var) .ps1
@@ -1,2 +1,6 @@
-# Get the current Environment "Path" Directories (Combined System + User Dirs)
+# Get the current Environment "Path" Directories (Combined System + User Dirs) & store them in the variable $EnvPath
 $EnvPath = (${Env:Path}).Split(';');
+
+
+# Show all PATH items (to the console)
+(${Env:Path}).Split(';');

6bd81ce11b149ed72ee4523bd4824df5704aaf02
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-28 13:44:21 -0400

    ImageMagick-Trimmer

--- cmd/cmd - ImageMagick-Trimmer/imagick - compress 1000.bat
+++ cmd/cmd - ImageMagick-Trimmer/imagick - compress 1000.bat
@@ -6,12 +6,19 @@ SET "INPUT_DIR=__TO_BE_TRIMMED"
 SET "OUTPUT_DIR=_FINISHED_ITEMS"
 
 FOR %%I IN ("%INPUT_DIR%\*.jpg") DO (
+
 	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.jpg"
+	
 	REM /***  RESIZE TO 1000px WIDTH  ***/
+	
 	magick "%%I" -resize 1000 "%OUTPUT_DIR%\%%~nI.jpg"
+	
 	REM /***  DELETE INPUT IMAGE AFTER WE CONVERTED TO EXPORT IMAGE  ***/
-	DEL /f "%%I"
+	
+	REM DEL /f "%%I"
+	
 	ECHO.
+	
 )
 
 START explorer.exe "%OUTPUT_DIR%"

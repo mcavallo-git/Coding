73d15d1d87d471d5ca35b0655aada7e3464d4a85
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-28 14:10:55 -0400

    ImageMagickDo not upload Images from ImageMagick input/output working-directories to Git-Repo

--- cmd/cmd - ImageMagick-Trimmer/ImageMagick - Scale Width=1000px.bat
+++ cmd/cmd - ImageMagick-Trimmer/ImageMagick - Scale Width=1000px.bat
@@ -1,24 +1,26 @@
 @ECHO OFF
-REM ImageMagick must be installed to computer running this script
+REM		!!! Prerequisite !!! 
+REM			ImageMagick" must be installed for this script to function as-intended
+REM			https://www.imagemagick.org/script/download.php#windows
 ECHO.
 
 SET "INPUT_DIR=__TO_BE_TRIMMED"
 SET "OUTPUT_DIR=_FINISHED_ITEMS"
 
+SET ResizeToWidthPx=1000
+
 FOR %%I IN ("%INPUT_DIR%\*.jpg") DO (
 
 	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.jpg"
-	
-	REM /***  RESIZE TO 1000px WIDTH  ***/
-	
-	magick "%%I" -resize 1000 "%OUTPUT_DIR%\%%~nI.jpg"
-	
+
+	REM /***  Resize Image to %ResizeToWidthPx% Pixels in Width  ***/
+	magick "%%I" -resize %ResizeToWidthPx% "%OUTPUT_DIR%\%%~nI.jpg"
+
 	REM /***  DELETE INPUT IMAGE AFTER WE CONVERTED TO EXPORT IMAGE  ***/
-	
-	REM DEL /f "%%I"
-	
+	DEL /f "%%I"
+
 	ECHO.
-	
+
 )
 
 START explorer.exe "%OUTPUT_DIR%"
--- cmd/cmd - ImageMagick-Trimmer/ImageMagick - Scale Width=2000px.bat
+++ cmd/cmd - ImageMagick-Trimmer/ImageMagick - Scale Width=2000px.bat
@@ -1,17 +1,26 @@
 @ECHO OFF
-REM ImageMagick must be installed to computer running this script
+REM		!!! Prerequisite !!! 
+REM			ImageMagick" must be installed for this script to function as-intended
+REM			https://www.imagemagick.org/script/download.php#windows
 ECHO.
 
 SET "INPUT_DIR=__TO_BE_TRIMMED"
 SET "OUTPUT_DIR=_FINISHED_ITEMS"
 
+SET ResizeToWidthPx=2000
+
 FOR %%I IN ("%INPUT_DIR%\*.jpg") DO (
+
 	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.jpg"
-	REM /***  RESIZE TO 1000px WIDTH  ***/
-	magick "%%I" -resize 2000 "%OUTPUT_DIR%\%%~nI.jpg"
+
+	REM /***  Resize Image to %ResizeToWidthPx% Pixels in Width  ***/
+	magick "%%I" -resize %ResizeToWidthPx% "%OUTPUT_DIR%\%%~nI.jpg"
+
 	REM /***  DELETE INPUT IMAGE AFTER WE CONVERTED TO EXPORT IMAGE  ***/
 	DEL /f "%%I"
+
 	ECHO.
+
 )
 
 START explorer.exe "%OUTPUT_DIR%"

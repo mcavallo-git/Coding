399840e0012efc342022a0f7c019826aefa4217d
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-28 05:20:38 -0400

    Update PowerShell - Get-WmiObject [Get AD User Info].ps1

--- powershell/PowerShell - Get-WmiObject [Get AD User Info].ps1
+++ powershell/PowerShell - Get-WmiObject [Get AD User Info].ps1
@@ -10,3 +10,15 @@ $ResultsnAD;
 # $ResultsnAD.DS_userPrincipalName;
 # $ResultsnAD.DS_mail;
 # $ResultsnAD.DS_name;
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------- #
+#
+# Citation(s)
+#
+#		docs.microsoft.com, "Get-WmiObject"
+#			https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-wmiobject?view=powershell-5.1
+#
+
+

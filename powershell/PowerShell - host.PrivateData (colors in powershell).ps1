d157be6cec75422fcaa1b85c0bb60a9ded56cb3b
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-30 02:21:33 -0400

    Update PowerShell - host.PrivateData (colors in powershell).ps1

--- powershell/PowerShell - host.PrivateData (colors in powershell).ps1
+++ powershell/PowerShell - host.PrivateData (colors in powershell).ps1
@@ -1,13 +1,20 @@
 
+$host.Pri
+$host.PrivateData.HaHa
+{{vateData}}
+
+Guesswho m? "PrivateData"  "Red";
+
+	$host.PrivateData.ErrorForegroundColor = @{};
+	$host.PrivateData.ErrorBackgroundColor = "Black";
+	$host.PrivateData.WarningForegroundColor = "Yellow";
+	$host.PrivateData.WarningBackgroundColor = "Black";
+	$host.PrivateData.DebugForegroundColor = "Yellow";
+	$host.PrivateData.DebugBackgroundColor = "Black";
+	$host.PrivateData.VerboseForegroundColor = "Yellow";
+	$host.PrivateData.VerboseBackgroundColor = "Black";
+	$host.PrivateData.ProgressForegroundColor = "Yellow";
+	$host.PrivateData.ProgressBackgroundColor = "DarkCyan";
+
 $host.PrivateData
 
-ErrorForegroundColor    : Red
-ErrorBackgroundColor    : Black
-WarningForegroundColor  : Yellow
-WarningBackgroundColor  : Black
-DebugForegroundColor    : Yellow
-DebugBackgroundColor    : Black
-VerboseForegroundColor  : Yellow
-VerboseBackgroundColor  : Black
-ProgressForegroundColor : Yellow
-ProgressBackgroundColor : DarkCyan

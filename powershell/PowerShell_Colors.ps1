0894d14c82875c4ad4c79e181908677b59fdc7a5
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-30 02:48:28 -0400

    Create PowerShell_Colors.ps1

---
+++ powershell/PowerShell_Colors.ps1
@@ -0,0 +1,44 @@
+
+$ColorSets = @{};
+$ColorSets.ErrorForegroundColor = "Red";
+$ColorSets.ErrorBackgroundColor = "Black";
+$ColorSets.WarningForegroundColor = "Yellow";
+$ColorSets.WarningBackgroundColor = "Black";
+$ColorSets.DebugForegroundColor = "Yellow";
+$ColorSets.DebugBackgroundColor = "Black";
+$ColorSets.VerboseForegroundColor = "Yellow";
+$ColorSets.VerboseBackgroundColor = "Black";
+$ColorSets.ProgressForegroundColor = "Yellow";
+$ColorSets.ProgressBackgroundColor = "DarkCyan";
+
+$HOST.PrivateData.ErrorForegroundColor = "Red";
+$HOST.PrivateData.ErrorBackgroundColor = "Black";
+$HOST.PrivateData.WarningForegroundColor = "Yellow";
+$HOST.PrivateData.WarningBackgroundColor = "Black";
+$HOST.PrivateData.DebugForegroundColor = "Yellow";
+$HOST.PrivateData.DebugBackgroundColor = "Black";
+$HOST.PrivateData.VerboseForegroundColor = "Yellow";
+$HOST.PrivateData.VerboseBackgroundColor = "Black";
+$HOST.PrivateData.ProgressForegroundColor = "Yellow";
+$HOST.PrivateData.ProgressBackgroundColor = "DarkCyan";
+
+
+Foreach ($DatSetting In $ColorSets) {
+	
+	$DatSettingName = $DatSetting.Value;
+	$DatSettingCurrent = $HOST.PrivateData[$DatSettingName];
+	$DatSettingUpdated = $DatSetting.Value;
+
+	If ($DatSettingUpdated -ne $DatSettingCurrent) {
+
+		Write-Host -NoNewLine "Updating variable $HOST.PrivateData[$DatSettingName]";
+		Write-Host -NoNewLine "from";
+		Write-Host -NoNewLine $DatSettingCurrent -ForegroundColor $DatSettingCurrent;
+		Write-Host -NoNewLine "to";
+		Write-Host -NoNewLine $DatSettingUpdated; -ForegroundColor $DatSettingUpdated;
+
+	}
+
+}
+
+# $HOST.PrivateData

205e215504d3e8b73e58c3359d58a3ad010cc893
mcavallo@boneal.com <mcavallo@boneal.com>
2019-03-30 02:01:53 -0400

    Create PowerShell - host.PrivateData (colors in powershell).ps1

---
+++ powershell/PowerShell - host.PrivateData (colors in powershell).ps1
@@ -0,0 +1,13 @@
+
+$host.PrivateData
+
+ErrorForegroundColor    : Red
+ErrorBackgroundColor    : Black
+WarningForegroundColor  : Yellow
+WarningBackgroundColor  : Black
+DebugForegroundColor    : Yellow
+DebugBackgroundColor    : Black
+VerboseForegroundColor  : Yellow
+VerboseBackgroundColor  : Black
+ProgressForegroundColor : Yellow
+ProgressBackgroundColor : DarkCyan

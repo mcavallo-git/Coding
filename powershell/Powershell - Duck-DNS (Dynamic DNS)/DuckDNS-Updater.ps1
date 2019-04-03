e25ed844bc8488a99ed877c0763133d64d9d399e
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-03 02:01:40 -0400

    Update DuckDNS-Updater.ps1

--- powershell/Powershell - Duck-DNS (Dynamic DNS)/DuckDNS-Updater.ps1
+++ powershell/Powershell - Duck-DNS (Dynamic DNS)/DuckDNS-Updater.ps1
@@ -1,2 +1,24 @@
-
-#### Refer to DuckDNS-README.md
+# ----------------------------------------------------------------------------------------------------------------------------------------
+#
+#### Refer to "DuckDNS-README.md"
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------
+#
+# PowerShell -Command "
+ForEach ($LocalUser In (Get-ChildItem ('C:\Users'))) {
+	If (Test-Path (($LocalUser.FullName)+('\.duck-dns\secret'))) {
+		[System.Net.WebRequest]::Create(
+			[System.Text.Encoding]::Unicode.GetString(
+				[System.Convert]::FromBase64String(
+					(Get-Content(
+						(($LocalUser.FullName)+('\.duck-dns\secret')))
+					)
+				)
+			)
+		).GetResponse();
+	}
+}
+Exit 0;
+# "
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------

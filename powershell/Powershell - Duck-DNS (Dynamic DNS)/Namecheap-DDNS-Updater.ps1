209f2f33f15e4cd48e49135a23f760d77683fc08
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-03 03:10:06 -0400

    nc

--- powershell/Powershell - Duck-DNS (Dynamic DNS)/Namecheap-DDNS-Updater.ps1
+++ powershell/Powershell - Duck-DNS (Dynamic DNS)/Namecheap-DDNS-Updater.ps1
@@ -1,5 +1,9 @@
 # ----------------------------------------------------------------------------------------------------------------------------------------
 #
+#### Refer to "README.md"
+#
+# ----------------------------------------------------------------------------------------------------------------------------------------
+#
 # Single Line (for use w/ Task Scheduler):
 #
 PowerShell -Command "ForEach ($LocalUser In (Get-ChildItem ('C:/Users'))) { If (Test-Path (($LocalUser.FullName)+('/.namecheap/secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((($LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse();}} Exit 0;"

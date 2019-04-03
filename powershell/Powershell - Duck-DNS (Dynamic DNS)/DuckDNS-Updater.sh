15d39639fb1f408018c03bdf1dfef0f24d21b97a
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-02 21:32:53 -0400

    ddns

--- powershell/Powershell - Duck-DNS (Dynamic DNS)/DuckDNS-Updater.sh
+++ powershell/Powershell - Duck-DNS (Dynamic DNS)/DuckDNS-Updater.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
 
 if [ -f ~/.duck-dns/secret ]; then curl --max-time 10 --connect-timeout 10 "$(cat ~/.duck-dns/secret | base64 --decode)" && echo ""; fi;
-
-
-. "/https://www.namecheap.com/support/knowledgebase/article.aspx/29/11/how-do-i-use-a-browser-to-dynamically-update-the-hosts-ip";
-
-
-
-RequestForm="https://dynamicdns.park-your-domain.com/update?host=[host]&domain=[domain_name]&password=[ddns_password]&ip=[your_ip]"

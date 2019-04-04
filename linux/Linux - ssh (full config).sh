2e3978dff85982b1a3e1541c3fecc365e1201c89
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-04 13:17:38 -0400

    Delete Linux - ssh (full config).sh

--- linux/Linux - ssh (full config).sh
+++
@@ -1,6 +0,0 @@
-
-# To get the current session's full SSH-configuration (at any given point in time), run the command:
-ssh -G host;
-
-# Use the following resource (or any other resource which exhaustively states all SSH-Config options)
-https://www.ssh.com/ssh/config/

30b6c53c647a521769577bf82ea2d2011d22c622
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-04 13:17:36 -0400

    Create Linux - ssh (show full config).sh

---
+++ linux/Linux - ssh (show full config).sh
@@ -0,0 +1,6 @@
+
+# To get the current session's full SSH-configuration (at any given point in time), run the command:
+ssh -G host;
+
+# Use the following resource (or any other resource which exhaustively states all SSH-Config options)
+https://www.ssh.com/ssh/config/

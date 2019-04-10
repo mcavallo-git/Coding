cb4677e13b865016d3ed1718ddc40429e6360a59
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-10 04:25:30 -0400

    Update assoc (open extension-less files as .txt).cmd

---
+++ cmd/assoc (open extension-less files as .txt).cmd
@@ -0,0 +1,12 @@
+ASSOC .=txtfile
+
+@REM ASSOC .bat=batfile
+@REM ASSOC .cmd=cmdfile
+
+@REM ASSOC .ps1=Microsoft.PowerShellScript.1
+@REM ASSOC .psm1=Microsoft.PowerShellModule.1
+
+@REM ASSOC .7z=7-Zip.7z
+@REM ASSOC .gz=7-Zip.gz
+@REM ASSOC .rar=7-Zip.rar
+@REM ASSOC .tar=7-Zip.tar

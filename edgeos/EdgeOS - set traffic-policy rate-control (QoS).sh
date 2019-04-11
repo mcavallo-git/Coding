fc38cfae1207bf393fb64f0a19b19dbebf51906d
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-11 02:20:15 -0400

    Update EdgeOS - set traffic-policy rate-control (QoS).sh

--- edgeos/EdgeOS - set traffic-policy rate-control (QoS).sh
+++ edgeos/EdgeOS - set traffic-policy rate-control (QoS).sh
@@ -1,6 +1,6 @@
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #
-#	EdgeOS - Rate Control (QoS) - https://help.ubnt.com/hc/en-us/articles/216787288-EdgeRouter-Quality-of-Service-QoS-#4
+#	EdgeOS - Rate Control (QoS) - https://help.ubnt.com/hc/en-us/articles/216787288-EdgeRouter-Quality-of-Service-QoS-#rate
 #
 #
 # The rate-control policy aims to ensure that the traffic is transmitted at no more than a pre-defined rate. It can be applied to the out direction, and the main parameter is the maximum rate for the outgoing traffic. For example, the following CLI commands create a rate-control policy that will attempt to ensure that the outgoing traffic is transmitted at no more than 1 Mbps on interface eth0.

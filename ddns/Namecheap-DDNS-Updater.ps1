b002b975ff278a2a688b154221c07af801d060c7
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-11 08:07:59 -0400

    Update Namecheap-DDNS-Updater.ps1

--- ddns/Namecheap-DDNS-Updater.ps1
+++ ddns/Namecheap-DDNS-Updater.ps1
@@ -35,12 +35,12 @@ Exit 0;
 #	Creating the Credentials file(s) - Read-in the user-specific hostname/domain-name/token from the credentials file(s), below
 
 
-$nc_host = Get-Content -Path (($Home)+("/.namecheap/host"));
+$nc_subdomain = Get-Content -Path (($Home)+("/.namecheap/subdomain"));
 $nc_domain = Get-Content -Path (($Home)+("/.namecheap/domain"));
 $nc_token = Get-Content -Path (($Home)+("/.namecheap/token"));
 $nc_ip = "";
 
-$nc_urlPlaintext = (("https://dynamicdns.park-your-domain.com/update?host=")+($nc_host)+("&domain=")+($nc_domain)+("&password=")+($nc_token)+("&ip=")+($nc_ip));
+$nc_urlPlaintext = (("https://dynamicdns.park-your-domain.com/update?host=")+($nc_subdomain)+("&domain=")+($nc_domain)+("&password=")+($nc_token)+("&ip=")+($nc_ip));
 
 $nc_urlBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($nc_urlPlaintext));
 

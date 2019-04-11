30c08a74577a9d6feb8419303a02926551a7ead0
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-11 03:35:08 -0400

    Update EdgeOS - set traffic-policy shaper (QoS).sh

--- edgeos/EdgeOS - set traffic-policy shaper (QoS).sh
+++ edgeos/EdgeOS - set traffic-policy shaper (QoS).sh
@@ -1,6 +1,7 @@
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #
-#	EdgeOS - Shaper (QoS) - https://help.ubnt.com/hc/en-us/articles/216787288-EdgeRouter-Quality-of-Service-QoS-#6
+#	EdgeOS - Shaper (QoS)
+#			https://help.ubnt.com/hc/en-us/articles/216787288-EdgeRouter-Quality-of-Service-QoS-#6
 #
 #
 # A more complicated policy is the shaper policy, which uses the Hierarchical Token Bucket technique to provide different bandwidth guarantees to different classes of traffic on a network link. A simple example is shown below.
@@ -24,3 +25,55 @@ commit
 #
 # Note: The interface mentioned in this example is eth0 (WAN). When a PPPoE interface is used for WAN, the policy should be applied to pppoeX rather than the Ethernet interface. For EdgeRouter modules that include a switch interface, the traffic policy will need to be applied to the switch interface (switch0) rather than the single Ethernet interface (eth1). 
 #
+
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#
+# EdgeRouter - Traffic Policies (Shaper) for Upload, Download and VoIP (QoS)
+#			https://help.ubnt.com/hc/en-us/articles/204911404-EdgeRouter-Traffic-Policies-Shaper-for-Upload-Download-and-VoIP
+#
+# The following example uses two traffic policies (one for upload, one for download) to limit client 10.0.3.2 to an upload rate of 512Kbps (allowing bursts to 640Kbps) and downloads of 1Mbit (allowing bursts to 1.5Mbit if the bandwidth is available). 
+#
+# Note: One class is for each client
+#
+
+
+#
+# DOWNLOAD
+#
+
+configure
+set traffic-policy shaper client-down bandwidth '1000mbit'
+set traffic-policy shaper client-down class 2 bandwidth '1mbit'
+set traffic-policy shaper client-down class 2 burst '1k'
+set traffic-policy shaper client-down class 2 ceiling '1.5mbit'
+set traffic-policy shaper client-down class 2 match ADDR ip destination address '10.0.3.2/32'
+set traffic-policy shaper client-down class 2 queue-type 'fair-queue'
+set traffic-policy shaper client-down class 3 bandwidth '1mbit'
+set traffic-policy shaper client-down class 3 burst '1k'
+set traffic-policy shaper client-down class 3 ceiling '1.5mbit'
+set traffic-policy shaper client-down class 3 match ADDR ip destination address '10.0.3.3/32'
+set traffic-policy shaper client-down class 3 queue-type 'fair-queue'
+set traffic-policy shaper client-down default bandwidth '100%'
+set traffic-policy shaper client-down default burst '1k'
+set traffic-policy shaper client-down default ceiling '100%'
+set traffic-policy shaper client-down default queue-type 'fair-queue'
+set interfaces ethernet eth1 traffic-policy out client-down
+commit
+save
+
+
+#
+# UPLOAD
+#
+
+configure
+set traffic-policy limiter client-up class 1 bandwidth '512kbit'
+set traffic-policy limiter client-up class 1 burst '1mb'
+set traffic-policy limiter client-up class 1 match ADDR ip source address '10.0.3.2/32'
+set traffic-policy limiter client-up default bandwidth '1000mbit'
+set interfaces ethernet eth1 traffic-policy in 'client-up'
+commit
+save
+

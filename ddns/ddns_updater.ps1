f50ffb306cd9954f0ea8b4ffa9f48ed4873c8ea9
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-12 12:14:23 -0400

    Update ddns_updater.ps1

--- ddns/ddns_updater.ps1
+++ ddns/ddns_updater.ps1
@@ -1,16 +1,7 @@
-# ----------------------------------------------------------------------------------------------------------------------------------------
 #
-# Single Line (for use w/ Task Scheduler)
+# ddns_updater
+#		--> Applying basic DDNS - Allows for resolution of a static hostname to proxy towards a non-static (dynamic) endpoint (IPv4)
 #
-
-
-PowerShell -Command "ForEach (`$LocalUser In (Get-ChildItem ('C:/Users'))) { If (Test-Path ((`$LocalUser.FullName)+('/.namecheap/secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content(((`$LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse(); }; If (Test-Path ((`$LocalUser.FullName)+('/.duck-dns/secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content(((`$LocalUser.FullName)+('/.duck-dns/secret'))))))).GetResponse(); }; }; Exit 0;"
-
-
-#
-# ----------------------------------------------------------------------------------------------------------------------------------------
-#
-# PowerShell -Command "
 ForEach (`$LocalUser In (Get-ChildItem ('C:/Users'))) {
 	If (Test-Path ((`$LocalUser.FullName)+('/.namecheap/secret'))) {
 		[System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content(((`$LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse();
@@ -19,6 +10,3 @@ ForEach (`$LocalUser In (Get-ChildItem ('C:/Users'))) {
 		[System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content(((`$LocalUser.FullName)+('/.duck-dns/secret'))))))).GetResponse();
 	};
 };
-# Exit 0;"
-#
-# ----------------------------------------------------------------------------------------------------------------------------------------

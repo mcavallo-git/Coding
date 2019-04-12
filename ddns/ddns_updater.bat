2fb7133dec8bf1be95d4f333268a11ab59017bf8
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-12 13:07:47 -0400

    Update ddns_updater.bat

--- ddns/ddns_updater.bat
+++ ddns/ddns_updater.bat
@@ -5,7 +5,6 @@ REM ddns_updater
 REM		--> Applying basic DDNS - Allows for resolution of a static hostname to proxy towards a non-static (dynamic) endpoint (IPv4)
 REM
 
-
 REM	Task Scheduler DDNS (trigger to run the following action, daily, starting at 12:01 AM, repeating every 5 minutes, ending after 1 day, max 10-second timeout)
 
 PowerShell -Command "ForEach ($LocalUser In (Get-ChildItem ('C:/Users'))) { If (Test-Path (($LocalUser.FullName)+('/.namecheap/secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((($LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse(); }; If (Test-Path (($LocalUser.FullName)+('/.duck-dns/secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((($LocalUser.FullName)+('/.duck-dns/secret'))))))).GetResponse(); }; }; Exit 0;"

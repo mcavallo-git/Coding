67fb1421b572f9928606bbecfc6323af6d625680
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-16 17:15:31 -0400

    Update PowerShell - Regex (matching capture groups).ps1

--- powershell/PowerShell - Regex (matching capture groups).ps1
+++ powershell/PowerShell - Regex (matching capture groups).ps1
@@ -1,34 +1,37 @@
 
+# ------------------------------------------------------------- #
+
 # Regex-Matching using separate ^(Capture)(Groups)$ in the output
 
-
+# ------------------------------------------------------------- #
+#
 # General Syntax
-
+#
 $Needle = [Regex]::Match($Haystack, $RegexPattern);
 
 
-
-
-# Ex 1)  General Syntax
-
+# ------------------------------------------------------------- #
+#
+# Ex 1)  Hello World
+#
 
 $Haystack = 'hello world'; # "Haystack", aka the string to parse (may have newlines aplenty)
 
 $RegexPattern = '^(hello)\s(world)$'; # Regex pattern which defines the "Needle" to match while parsing the through the "Haystack"
 
-
 $Needle = [Regex]::Match($Haystack, $RegexPattern); # Parse through the "Haystack", looking for the "Needle"
 
+$Needle.Groups[0].Value; # Should output:  'hello world'   (original entire input, denoting successful regex string-match)
 
-$Needle.Groups[0].Value; # Should output:  'hello world'
-
-$Needle.Groups[1].Value; # Should output:  'hello'
+$Needle.Groups[1].Value; # Should output:  'hello'   (first capture group, aka first parenthesis-bound )
 
 $Needle.Groups[2].Value; # Should output:  'world'
 
 
-
-# Example 
+# ------------------------------------------------------------- #
+#
+# Ex 2)  Obtain the version of local Azure-CLI Module
+#
 
 $Haystack = az --version;
 
@@ -37,3 +40,5 @@ $RegexPattern = '^(azure\-cli\ \()([\d\.]+)(\))';
 $Needle = [Regex]::Match($Haystack, $RegexPattern);
 
 $Needle.Groups[2].Value;
+
+# ------------------------------------------------------------- #

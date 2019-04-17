1d24aa291caf3d8ae5401e88809098118744ba92
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-17 09:06:17 -0400

    Create PowerShell - Start-ScheduledTask (send manual trigger to Task Scheduler).ps1

---
+++ powershell/PowerShell - Start-ScheduledTask (send manual trigger to Task Scheduler).ps1
@@ -0,0 +1,13 @@
+#
+# Kick-off a scheduled task via PowerShell
+#
+
+Start-ScheduledTask -TaskName "GitSyncAll"
+
+
+#
+#	Citation(s)
+#	
+#	docs.microsoft.com
+#		https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/start-scheduledtask
+#

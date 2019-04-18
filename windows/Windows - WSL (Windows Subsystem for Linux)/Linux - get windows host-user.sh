7a86bdbf13249595bcda3223fd514ddbcde4d353
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-18 15:24:36 -0400

    Update Linux - get windows host-user.sh

--- windows/Windows - WSL (Windows Subsystem for Linux)/Linux - get windows host-user.sh
+++ windows/Windows - WSL (Windows Subsystem for Linux)/Linux - get windows host-user.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-WINDOWS_USER_DIRS="/mnt/c/Users";
+USERS_MATCHED=();
 
-find "${WINDOWS_USER_DIRS}" -mindepth 1 -maxdepth 1 -type 'd' -name '*' -print0 | while IFS= read -r -d $'\0' EACH_USER_DIR; do
-	echo "------------------------------------------------------------";
+find "/mnt/c/Users" \
+-mindepth 1 \
+-maxdepth 1 \
+-name '*' \
+-type 'd' \
+-not -path "/mnt/c/Users/Default" \
+-not -path "/mnt/c/Users/Public" \
+-print0 \
+| while IFS= read -r -d $'\0' EACH_USER_DIR; do
 
 	LAST_EXIT_CODE=$([[ -r "${EACH_USER_DIR}/Documents" ]]; echo $?);
 
-	if [ "${LAST_EXIT_CODE}" == "0" ]; then	# Valid read-privileges
-
-		echo "Pass - Can read \"${EACH_USER_DIR}\" - Current session has sufficient privilege(s)";
-
-	else # Insufficient read-privileges
-
-		echo "Fail - Cannot read \"${EACH_USER_DIR}\" - Current session lacks sufficient privilege(s)";
-		
+	if [ "${LAST_EXIT_CODE}" == "0" ]; then
+		USERS_MATCHED+=("$(basename ${EACH_USER_DIR})");
 	fi;
 
+	#
+	##### if more 'verbose' output is desired (and for the sake of transparency)
+	#
+	# if [ "${LAST_EXIT_CODE}" == "0" ]; then
+	# 	echo "PASS - Can read \"${EACH_USER_DIR}\" - Current session has sufficient privilege(s)";
+	# else
+	# 	echo "FAIL - Cannot read \"${EACH_USER_DIR}\" - Current session lacks sufficient privilege(s)";
+	# fi;
+
 done;
+
+echo "Current Linux-Instance hosted by Windows-User \"${WINDOWS_USERNAME}\"";
+
 
 
 

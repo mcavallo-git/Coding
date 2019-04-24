7f4365de6b61106cdf1de8480692f684e1e681c9
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-24 06:27:21 -0400

    Create Ubnt - show configuration commands.sh

---
+++ ubnt/Ubnt - show configuration commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Ubnt (ERLite-3)
+
+# 'show configuration commands' reverse-engineers the current config and outputs what it would take to rebuild it (as a set of shell-commands)
+
+show configuration commands
+

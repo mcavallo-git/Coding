f9bbc42b926b9ef13e618827b460357090a877fe
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-24 12:24:18 -0400

    Update PowerShell - Regex (matching capture groups).ps1

--- powershell/PowerShell - Regex (matching capture groups).ps1
+++ powershell/PowerShell - Regex (matching capture groups).ps1
@@ -20,25 +20,39 @@ $Haystack = 'hello world'; # "Haystack", aka the string to parse (may have newli
 $RegexPattern = '^(hello)\s(world)$'; # Regex pattern which defines the "Needle" to match while parsing the through the "Haystack"
 
 $Needle = [Regex]::Match($Haystack, $RegexPattern); # Parse through the "Haystack", looking for the "Needle"
+	
+Write-Host (("`$Needle.Success = ")+($Needle.Success));
 
-$Needle.Groups[0].Value; # Should output:  'hello world'   (original entire input, denoting successful regex string-match)
+If ($Needle.Success -ne $False) {
 
-$Needle.Groups[1].Value; # Should output:  'hello'   (first capture group, aka first parenthesis-bound )
+	$Needle.Groups[0].Value; # Should output:  'hello world'   (original entire input, denoting successful regex string-match)
 
-$Needle.Groups[2].Value; # Should output:  'world'
+	$Needle.Groups[1].Value; # Should output:  'hello'   (first capture group, aka first parenthesis-bound )
 
+	$Needle.Groups[2].Value; # Should output:  'world'
+
+}
 
 # ------------------------------------------------------------- #
 #
 # Ex 2)  Obtain the version of local Azure-CLI Module
 #
 
-$Haystack = az --version;
+$Haystack = (az --version);
 
-$RegexPattern = '^(azure\-cli\ \()([\d\.]+)(\))';
+$RegexPattern = '^(azure\-cli)\s*([\d\.]+)\s*\*?$';
 
-$Needle = [Regex]::Match($Haystack, $RegexPattern);
+ForEach ($EachLine in $Haystack){
+	$Needle = [Regex]::Match($EachLine, $RegexPattern);
+	If ($Needle.Success -eq $True) {
+		Break;
+	}
+}
 
-$Needle.Groups[2].Value;
+If ($Needle.Success -eq $True) {
+	Write-Host (("Found local install: `"")+($Needle.Groups[1].Value) + ("`" v") + ($Needle.Groups[2].Value));
+} Else {
+	Write-Host ("No matches found");
+}
 
 # ------------------------------------------------------------- #

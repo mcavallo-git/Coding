4998f72aed7b1347d5004c8be1dd6098cdfb7226
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-25 19:22:21 -0400

    Update PowerShell - ReadKey (await user keypress).ps1

--- powershell/PowerShell - ReadKey (await user keypress).ps1
+++ powershell/PowerShell - ReadKey (await user keypress).ps1
@@ -1,8 +1,13 @@
 
 # Wait for user to press any key
 
-Write-Host -NoNewLine "Press any key to continue...";
+Write-Host -NoNewLine "`n`n  Awaiting KeyPress...";
 $KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+Write-Host (("`n`n  You pressed the `"")+($KeyPress.Character)+("`" Key`n`n"));
+
+
+Write-Host -NoNewLine "`n`n  Press any key to exit...";
+$KeyPressExit = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
 #
 # Thanks to stackoverflow user 'Knuckle-Dragger' on forum: https://stackoverflow.com/questions/20886243

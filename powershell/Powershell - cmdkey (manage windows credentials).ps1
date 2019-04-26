a2aacd951f22085845f3258e80c391bd0b9bd175
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-25 21:50:55 -0400

    Update Powershell - cmdkey (manage windows credentials).ps1

--- powershell/Powershell - cmdkey (manage windows credentials).ps1
+++ powershell/Powershell - cmdkey (manage windows credentials).ps1
@@ -5,22 +5,46 @@
 
 $CredentialMatches = @();
 
-$Haystack = (cmdkey /list);
+$TargetStartsWith="TERMSRV";
+$TargetContains="remote";
 
-$NameQuery="git:https://";
-$NameQuery="TERMSRV";
-
-$CredentialType="Generic";
-# $CredentialType="Generic Certificate";
-# $CredentialType="Domain Password";
-
-$RegexName = (('^\s*Target:\s*([^:]+):target=(')+($NameQuery)+('[a-zA-Z0-9\/\-\@\.]+)\s*$'));
-$RegexType = (('^\s*Type:\s*')+($CredentialType)+('$'));
+$Haystack = (cmdkey /list:"$TargetStartsWith*");
+$FullHaystack = (cmdkey /list);
+$SelectedArray = $Haystack;
 
 $NeedlesFound = 0;
-
-ForEach ($EachLine in $Haystack) {
+$LineNumber = 0;
+ForEach ($EachLine in $SelectedArray) {
+	# Write-Host ('-'*25);
 	$EachLine;
+	If ($EachLine.StartsWith("    Target: ") -eq $True) {
+		$NextLine = $SelectedArray[$LineNumber+1];
+		$EachTarget = $EachLine.Replace("    Target: ","");
+		$EachType = $NextLine.Replace("    Type: ","");
+		If ($EachTarget.Contains($TargetContains)) {
+			$CredentialMatches += @{
+				Type = $EachType;
+				Target = $EachTarget;
+			}
+			$NeedlesFound++;
+		}
+
+	}
+	$LineNumber++;
+}
+
+<#
+# $CredentialType="Generic";
+# $CredentialType="Generic Certificate";
+# $CredentialType="Domain Password";
+$RegexName = (('^\s*Target:\s*([^:]+):target=(')+($TargetContains)+('[a-zA-Z0-9\/\-\@\.]+)\s*$'));
+$RegexType = (('^\s*Type:\s*')+($CredentialType)+('$'));
+$NeedlesFoundRegex = 0;
+$LineNumber = 0;
+ForEach ($EachLine in $FullHaystack) {
+	Write-Host ('-'*25);
+	$EachLine;
+	$FullHaystack[$LineNumber+1];
 	$NeedleResults = [Regex]::Match($EachLine, $RegexName);
 	If ($NeedleResults.Success -eq $True) {
 		If ($CredentialMatches -eq $Null) {
@@ -33,33 +57,41 @@ ForEach ($EachLine in $Haystack) {
 			Type = $NeedleResults.Groups[1].Value;
 			Target = $NeedleResults.Groups[2].Value;
 		}
-		$NeedlesFound++;
-		# Break;
+		$NeedlesFoundRegex++;
 	}
+	$LineNumber++;
 }
+#>
 
 Write-Host "`n";
 
 If ($NeedlesFound -eq 0) {
 
-	Write-Host (("Found [ 0 ] Windows-Credentials matching `"")+($NameQuery)+("`" "));
+	Write-Host (("Found [ 0 ] Windows-Credentials whose target starts with `"")+($TargetStartsWith)+("`" "));
 
 } Else {
 
-	Write-Host (("`nMatched [ ")+($NeedlesFound)+(" ] Windows-Credentials matching `"")+($NameQuery)+("`"`n")) -ForegroundColor Yellow;
+	Write-Host "`n";
+	$DeletePrepMsg = (("This will delete the following [ ")+($NeedlesFound)+(" ] credential(s):"));
+	Write-Host ((" "*($DeletePrepMsg.Length))+("`n")+($DeletePrepMsg)+("`n")+((" "*($DeletePrepMsg.Length)+("`n")))) -BackgroundColor Magenta -ForegroundColor White;
 	# $CredentialMatches | Format-List;
 	ForEach ($EachCredential in $CredentialMatches) {
-		Write-Host (("    [ ")+($EachCredential.Type)+(" ]   ")+($EachCredential.Target));
+		Write-Host (("  Type:`"")+($EachCredential.Type)+("`", Target:`"")+($EachCredential.Target)+("`"  "));
 	}
-
-	Write-Host -NoNewLine (("`nDelete [ ")+($NeedlesFound)+(" ] matched item(s)? (Y/N)`n")) -ForegroundColor Yellow;
+	
+	Write-Host -NoNewLine "`n`nIf you're sure you want to delete these credentials, press 'y': " -ForegroundColor Yellow;
 	$UserKeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+	Write-Host (($UserKeyPress.Character)+("`n"));
 
 	If ($UserKeyPress.Character -eq 'y') {
-		Write-Host (("Deleting [ ")+($NeedlesFound)+(" ] credentials")) -ForegroundColor Magenta;
-		cmdkey /del:($_ -replace " ","" -replace "Target:","");
+		ForEach ($EachCredential in $CredentialMatches) {
+			$EachTarget = $EachCredential.Target;
+			$EachType = $EachCredential.Type;
+			Write-Host "  Deleting $EachType w/ Target `"$EachTarget`"..." -ForegroundColor Magenta;
+			# cmdkey /delete:($EachTarget);
+		}
 	} Else {
-		Write-Host "No Action Taken" -ForegroundColor Green;
+		Write-Host "`nNo Action Taken" -ForegroundColor Green;
 	}
 
 }

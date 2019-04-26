932b85df61c8eff9810c6b533d844fb091cac0b6
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-25 23:11:16 -0400

    Update PowerShell - Random (generate random alpha string).ps1

--- powershell/PowerShell - Random (generate random alpha string).ps1
+++ powershell/PowerShell - Random (generate random alpha string).ps1
@@ -1,34 +1,55 @@
 
 # ------------------------------------------------------------
 #
-#	Generate a string built from random characters [a-zA-Z]
+#	Generate a random string built from various characters:
 #
 $CharacterCount = 29;
 $ASCII = @{};
-$ASCII.Numeric = (48..57);
-$ASCII.UpperAlpha = (65..90);
-$ASCII.LowerAlpha = (97..122);
-$ASCII.UpperAlphaNumeric = (48..57 + 65..90);
-$ASCII.LowerAlphaNumeric = (48..57 + 97..122);
-$ASCII.ComboAlpha = (65..90 + 97..122);
-$ASCII.ComboAlphaNumeric = (48..57 + 65..90 + 97..122);
+
+$ASCII.Alpha_Upper = [char[]](65..90);  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+$ASCII.Alpha_Lower = [char[]](97..122); # abcdefghijklmnopqrstuvwxyz
+$ASCII.Numeric = [char[]](48..57); # 0123456789
+$ASCII.Specials = [char[]](33,35,36,37,38,42,63,64,94,126); # !#$%&*?@^~
+
+$ASCII.AlphaNumeric_Upper = [char[]]((48..57) + (65..90));  # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+$ASCII.AlphaNumeric_Lower = [char[]]((48..57) + (97..122)); # 0123456789abcdefghijklmnopqrstuvwxyz
+$ASCII.AlphaNumeric_Mixed = [char[]]((48..57) + (65..90) + (97..122)); # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+
+$ASCII.AlphaNumSpecials_Upper = [char[]]((48..57) + (65..90) + (33,35,36,37,38,42,63,64,94,126));  # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ
+$ASCII.AlphaNumSpecials_Lower = [char[]]((48..57) + (97..122) + (33,35,36,37,38,42,63,64,94,126)); # 0123456789abcdefghijklmnopqrstuvwxyz
+$ASCII.AlphaNumSpecials_Mixed = [char[]]((48..57) + (65..90) + (97..122) + (33,35,36,37,38,42,63,64,94,126)); # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
+
+$ASCII.Alpha_Mixed = [char[]](65..90) + (97..122); # ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
 
 $RandomStrings = @{};
 ForEach ($EachKey In $ASCII.Keys) {
 	If ($RandomStrings[$EachKey] -eq $null) { $RandomStrings[$EachKey] = "" };
-	For ($i = 0; $i -lt $CharacterCount; $i++) { $RandomStrings[$EachKey] += [char](Get-Random -InputObject ($ASCII[$EachKey])) };
+	For ($i = 0; $i -lt $CharacterCount; $i++) { $RandomStrings[$EachKey] += (Get-Random -InputObject ($ASCII[$EachKey])) };
 }
-# $RandomStrings | Sort-Object -Property Name | Format-Table Name, Value;
-$RandomStrings | sort-object -property @{Expression="Name";Descending=$true}, @{Expression="Name";Descending=$false} | format-table
+# $RandomStrings | Sort-Object -Property Name | Format-Table;
+
+# $RandomStrings.PSObject.Properties
+
+# $RandomStrings.Keys | Sort-Object
+
+Write-Host "`n";
+$RandomStrings.Keys | Sort-Object | ForEach { Write-Host (("`n  ")+($_).PadRight(25," ")+(":::  ")+($RandomStrings[$_])); };
+Write-Host "`n";
+
+# $RandomStrings | Get-Member
+
+
+
 <#
-# ------------------------------------------------------------
-#
-# Generate a pseudo-random string by base-64 encoding random numbers
-#
-$RandomCharacterCount = 20;
-$RandString = (([Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes((Get-Random -SetSeed (Get-Random))))).Substring(0,$RandomCharacterCount));
-$RandString;
+	# ------------------------------------------------------------
+	#
+	# Generate a pseudo-random string by base-64 encoding random numbers
+	#
+	$RandomCharacterCount = 20;
+	$RandString = (([Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes((Get-Random -SetSeed (Get-Random))))).Substring(0,$RandomCharacterCount));
+	$RandString;
 #>
+
 
 
 # ------------------------------------------------------------

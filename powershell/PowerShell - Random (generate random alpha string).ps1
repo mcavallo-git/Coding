21e34cd7453255239a027e0b1e5391721efba8d0
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-25 22:10:57 -0400

    Update PowerShell - Random (generate random alpha string).ps1

--- powershell/PowerShell - Random (generate random alpha string).ps1
+++ powershell/PowerShell - Random (generate random alpha string).ps1
@@ -4,19 +4,22 @@
 #	Generate a string built from random characters [a-zA-Z]
 #
 $CharacterCount = 29;
-$AlphaCodes = @{};
-$AlphaCodes.Upper = (65..90);
-$AlphaCodes.Lower = (97..122);
-$AlphaCodes.Ints = (48..57);
-$AlphaCodes.Combo = ($AlphaCodes.Upper) + ($AlphaCodes.Lower);
+$ASCII = @{};
+$ASCII.Numeric = (48..57);
+$ASCII.UpperAlpha = (65..90);
+$ASCII.LowerAlpha = (97..122);
+$ASCII.UpperAlphaNumeric = (48..57 + 65..90);
+$ASCII.LowerAlphaNumeric = (48..57 + 97..122);
+$ASCII.ComboAlpha = (65..90 + 97..122);
+$ASCII.ComboAlphaNumeric = (48..57 + 65..90 + 97..122);
 
 $RandomStrings = @{};
-ForEach ($EachKey In $AlphaCodes.Keys) {
+ForEach ($EachKey In $ASCII.Keys) {
 	If ($RandomStrings[$EachKey] -eq $null) { $RandomStrings[$EachKey] = "" };
-	For ($i = 0; $i -lt $CharacterCount; $i++) { $RandomStrings[$EachKey] += [char](Get-Random -InputObject ($AlphaCodes[$EachKey])) };
+	For ($i = 0; $i -lt $CharacterCount; $i++) { $RandomStrings[$EachKey] += [char](Get-Random -InputObject ($ASCII[$EachKey])) };
 }
-$RandomStrings | Format-Table;
-
+# $RandomStrings | Sort-Object -Property Name | Format-Table Name, Value;
+$RandomStrings | sort-object -property @{Expression="Name";Descending=$true}, @{Expression="Name";Descending=$false} | format-table
 <#
 # ------------------------------------------------------------
 #

f51f9b5120ec11419c628584d07e900640f4c18b
mcavallo@boneal.com <mcavallo@boneal.com>
2019-04-29 07:37:45 -0400

    Create Linux - hostname (set hostname, domain-name).sh

---
+++ linux/Linux - hostname (hostname, hostnamectl)/Linux - hostname (set hostname, domain-name).sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+if [ "$(whoami)" != "root" ]; then
+
+	echo "Must run \"${0}\" as user 'root'.";
+	exit 1;
+
+elif [ ! -n "${NEW_HOSTNAME}" ]; then
+	# Required variable empty: NEW_HOSTNAME
+	echo "Variable \${NEW_HOSTNAME} is empty";
+	echo "Please set the variable to your desired hostname's value, then re-run this script.";
+	echo "  ( e.g. NEW_HOSTNAME=[your-desired-hostname]; )";
+	exit 1;
+
+elif [ ! -n "${NEW_DOMAIN}" ]; then
+	# Required variable empty: NEW_HOSTNAME
+	echo "Variable \${NEW_DOMAIN} is empty";
+	echo "Please set the variable to your desired hostname's value, then re-run this script.";
+	echo "  ( e.g. NEW_DOMAIN=[your-desired-domain]; )";
+	exit 1;
+else 
+
+	DRY_RUN=1;
+	if [[ -n "$1" ]]; then
+		if [[ "$1" == "0" ]]; then
+			DRY_RUN=0;
+		fi;
+	fi;
+
+	if [[ "${DRY_RUN}" == "0" ]]; then
+		echo "NOT RUNNING IN DRY-RUN MODE";
+		echo "HOSTNAME / DOMAIN-NAME CHANGES WILL BE MADE";
+		read -p "CONFIRM CHANGES TO HOST-NAME / DOMAIN-NAME ( Y/N )" -n 1 -r
+		echo "";
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo "CONFIRMED - PROCEEDING WITH CHANGES TO HOST-NAME / DOMAIN-NAME";
+		else
+			echo "CANCELLED - EXITING";
+			exit 1;
+		fi;
+	else
+		echo "DRY-RUN MODE ACTIVE";
+	fi;
+
+	# get linux distro
+	IS_CENTOS=$(if [[ $(cat /etc/*release | grep -i centos | wc -l) -gt 0 ]]; then echo 1; else echo 0; fi; );
+	IS_UBUNTU=$(if [[ $(cat /etc/*release | grep -i ubuntu | wc -l) -gt 0 ]]; then echo 1; else echo 0; fi; );
+	IS_ALPINE=$(if [[ $(cat /etc/*release | grep -i alpine | wc -l) -gt 0 ]]; then echo 1; else echo 0; fi; );
+	IS_DEBIAN=$(if [[ $(cat /etc/*release | grep -i debian | wc -l) -gt 0 ]]; then echo 1; else echo 0; fi; );
+	THIS_LINUX_DISTRO="$(if [[ ${IS_CENTOS} -gt 0 ]]; then echo CENTOS; elif [[ ${IS_UBUNTU} -gt 0 ]]; then echo UBUNTU; elif [[ ${IS_ALPINE} -gt 0 ]]; then echo ALPINE; elif [[ ${IS_DEBIAN} -gt 0 ]]; then echo DEBIAN; else echo UNKNOWN; fi; )";
+
+	# External server(s) to resolve WAN-IP through
+	RESOLVER_1="https://icanhazip.com";
+	RESOLVER_2="https://ipecho.net/plain";
+	RESOLVER_3="https://ident.me";
+	RESOLVER_4="https://bot.whatismyipaddress.com";
+
+	# Determine WAN IPv4
+	WAN_IP=$(curl -L -s "${RESOLVER_1}" | curl -L -s "${RESOLVER_2}" | curl -L -s "${RESOLVER_3}" | curl -L -s "${RESOLVER_4}");
+
+	# Determine LAN IPv4
+	MY_LAN_IPV4=$(ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//' | grep '\.');
+
+	# Verify values
+	if [ ! -n "${MY_LAN_IPV4}" ]; then exit 1; fi; # error-out if ${MY_LAN_IPV4} is empty
+	if [ ! -n "${WAN_IP}" ]; then exit 1; fi; # error-out if ${WAN_IP} is empty
+	if [ ! -n "${NEW_HOSTNAME}" ]; then exit 1; fi; # error-out if ${NEW_HOSTNAME} is empty
+	if [ ! -n "${NEW_DOMAIN}" ]; then exit 1; fi; # error-out if ${NEW_DOMAIN} is empty
+
+	echo "";
+	echo "SET HOSTNAME TO \"${NEW_HOSTNAME}\"";
+	echo "SET DOMAIN TO \"${NEW_DOMAIN}\"";
+	echo "CURRENT WAN IP: [${WAN_IP}]";
+	echo "CURRENT LAN IP: [${MY_LAN_IPV4}]";
+
+	# /etc/hostname
+	HOSTNAME_FILE="/etc/hostname";
+	if [[ -f "${HOSTNAME_FILE}" ]]; then
+		if [[ "${DRY_RUN}" == "0" ]]; then # NOT a dry-run
+			echo "${NEW_HOSTNAME}" > "${HOSTNAME_FILE}";
+		else # dry-run
+			echo "";
+			echo "APPLY HOSTNAME VIA [ ${HOSTNAME_FILE} ]";
+		fi;
+	fi;
+
+	# /etc/hosts
+	HOSTS_FILE="/etc/hosts";
+	if [[ -f "${HOSTS_FILE}" ]]; then
+		if [[ "${DRY_RUN}" == "0" ]]; then # NOT a dry-run
+			echo "";
+			echo "HOSTS FILE (BEFORE-EDITS)" && cat "${HOSTS_FILE}";
+			TEMP_HOSTS="${HOSTS_FILE}_TEMP";
+			cp -f "${HOSTS_FILE}" "${TEMP_HOSTS}";
+			sed_1="/^${WAN_IP}/c\ ";
+			sed_2="/^${MY_LAN_IPV4}/c\ ";
+			sed --in-place --expression="${sed_1}" --expression="${sed_2}" "${TEMP_HOSTS}";
+			sed_3="/^127.0.0.1/c\127.0.0.1 localhost localhost.localdomain\n${WAN_IP} ${NEW_HOSTNAME}.${NEW_DOMAIN} ${NEW_HOSTNAME}";
+			sed --in-place --expression="${sed_3}" "${TEMP_HOSTS}";
+			sed_whitespace_only='/^\s*$/d';
+			sed --in-place --expression="${sed_whitespace_only}" "${TEMP_HOSTS}"; # Remove whitespace-only lines
+			cp -f "${TEMP_HOSTS}" "${HOSTS_FILE}" && rm -f "${TEMP_HOSTS}";
+			echo "";
+			echo "HOSTS FILE (AFTER-EDITS)" && cat "${HOSTS_FILE}";
+		else # dry-run
+			echo "";
+			echo "APPLY HOSTNAME VIA [ ${HOSTS_FILE}]";
+		fi;
+	fi;
+
+	# centos 6
+	if [[ -f "/etc/sysconfig/network" ]]; then
+		if [[ "${DRY_RUN}" == "0" ]]; then # NOT a dry-run
+			sed --in-place --expression="/^HOSTNAME=/c\HOSTNAME=${NEW_HOSTNAME}.${NEW_DOMAIN}" "/etc/sysconfig/network";
+		else # dry-run
+			echo "";
+			echo "APPLY HOSTNAME VIA [ /etc/sysconfig/network ]";
+		fi;
+	fi;
+
+	# debian 7 / slackware / ubuntu 14.04
+	if [[ -f "/etc/init.d/hostname.sh" ]]; then
+		if [[ "${DRY_RUN}" == "0" ]]; then # NOT a dry-run
+			/etc/init.d/hostname.sh;
+		else # dry-run
+			echo "";
+			echo "APPLY HOSTNAME VIA [ /etc/init.d/hostname.sh ]";
+		fi;
+	fi;
+
+	# arch / centos 7 / debian 8 / fedora / ubuntu 16.04 and above
+	if [[ $(hostnamectl | wc -l) -gt 0 ]]; then
+		if [[ "${DRY_RUN}" == "0" ]]; then # NOT a dry-run
+			hostnamectl set-hostname "${NEW_HOSTNAME}";
+		else # dry-run
+			echo "";
+			echo "APPLY HOSTNAME VIA [ hostnamectl ]";
+		fi;
+	fi;
+
+
+	# Apply Changes
+	if [[ -f "/etc/init.d/network" ]]; then # centos distros
+		if [[ "${DRY_RUN}" == "0" ]]; then # NOT a dry-run
+			echo "RESTART REQUIRED FOR [ /etc/init.d/network ]";
+			read -p "RESTART NOW? ( Y/N )" -n 1 -r
+			echo "";
+			if [[ $REPLY =~ ^[Yy]$ ]]; then
+				echo "";
+				echo "RESTARTING [ /etc/init.d/network ]";
+				/etc/init.d/network restart;
+			else
+				echo "";
+				echo "POST-PONING RESTART OF [ /etc/init.d/network ]";
+				exit 1;
+			fi;
+		else # dry-run
+			echo "";
+			echo "APPLY CHANGES VIA [ /etc/init.d/network restart ]";
+		fi;
+	else # [other] distros
+		if [[ "${DRY_RUN}" == "0" ]]; then # NOT a dry-run
+			read -p "SHUTDOWN REQUIRED - PERFORM NOW? ( Y/N )" -n 1 -r
+			echo "";
+			if [[ $REPLY =~ ^[Yy]$ ]]; then
+				echo "";
+				echo "SHUTTING DOWN";
+				shutdown -r now;
+			else
+				echo "";
+				echo "POST-PONING SHUTDOWN";
+				exit 1;
+			fi;
+		else # dry-run
+			echo "";
+			echo "APPLY CHANGES VIA [ shutdown -r now ]";
+		fi;
+	fi;
+fi;
+
+# References
+#   https://jblevins.org/log/hostname
+#   https://support.rackspace.com/how-to/centos-hostname-change/
+#   https://www.linode.com/docs/getting-started/#setting-the-hostname

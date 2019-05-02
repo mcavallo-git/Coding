1c17303cf2615cf916cf15cbfb22eb05c6131f5f
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-02 17:17:33 -0400

    Create Azure-DDNS-Updater.ps1

---
+++ ddns/Azure-DDNS-Updater.ps1
@@ -0,0 +1,98 @@
+
+# az network nsg
+#		"Manage Azure Network Security Groups (NSGs)"
+#		 https://docs.microsoft.com/en-us/cli/azure/network/nsg
+
+# az network nsg rule
+#		"Manage network security group rules"
+#		 https://docs.microsoft.com/en-us/cli/azure/network/nsg/rule
+
+
+$Az  = (JsonDecoder -InputObject (Get-Content "${HOME}\.ddns\azure_nsg.json"));
+
+$DDNS_UIDS  = (JsonDecoder -InputObject (Get-Content "${HOME}\.ddns\cidr_ddns.json"));
+
+$ResolvedIPv4s = @();
+
+$DDNS_UIDS | ForEach {
+	# Perform a DNS query to reverse-lookup the IPv4 for each hostname
+	$DnsRecord_A = (
+		Resolve-DnsName `
+			-Name ($_.Host) `
+			-Type A `
+			-Server "8.8.8.8" `
+			-QuickTimeout `
+			-NoHostsFile `
+	);
+	If ($DnsRecord_A.IPAddress -eq $null) {
+		Write-Host (("Error - Unable to resolve hostname: ")+($_.Host)) -BackgroundColor Black -ForegroundColor Yellow;
+	} ElseIf (!$ResolvedIPv4s.Contains((($DnsRecord_A.IPAddress)+("/32")))) {
+		$ResolvedIPv4s += @((($DnsRecord_A.IPAddress)+("/32")));
+	}
+
+};
+
+if ($ResolvedIPv4s.Length -eq 0)  {
+	Write-Host "No results returned valid IPv4 data (DDNS_UIDS.IPv4.Length===0)";
+} else {
+	# $Az.NsgSourceAddressPrefixes = ($ResolvedIPv4s -join " ");
+	$Az.NsgSourceAddressPrefixes = $ResolvedIPv4s;
+
+
+	$Az.NsgShowRuleDDNS = `
+		JsonDecoder -InputObject (
+			az network nsg rule show `
+			--subscription ($Az.Subscription) `
+			--resource-group ($Az.ResourceGroup) `
+			--nsg-name ($Az.NsgParentName) `
+			--name ($Az.NsgRuleName) `
+		);
+		
+	Write-Host ($Az.NsgShowRuleDDNS);
+	$Az.NsgShowRuleDDNS;
+
+	if (($Az.NsgShowRuleDDNS) -eq $null) {
+		
+		$Az.NsgRuleCreate = `
+			JsonDecoder -InputObject ( `
+				az network nsg rule create `
+					--subscription ($Az.Subscription) `
+					--resource-group ($Az.ResourceGroup) `
+					--nsg-name ($Az.NsgParentName) `
+					--name ($Az.NsgRuleName) `
+					--priority ($Az.NsgRulePriority) `
+					--access ("Allow") `
+					--description ($Az.NsgRuleName) `
+					--destination-address-prefixes ("VirtualNetwork") `
+					--destination-port-ranges ("*") `
+					--direction ("Inbound") `
+					--protocol ("*") `
+					--source-address-prefixes ($Az.NsgSourceAddressPrefixes) `
+					--source-port-ranges ("*") `
+		);
+
+	} else {
+
+		$Az.NsgRuleUpdate = `
+			JsonDecoder -InputObject ( `
+				az network nsg rule update `
+					--subscription ($Az.Subscription) `
+					--resource-group ($Az.ResourceGroup) `
+					--nsg-name ($Az.NsgParentName) `
+					--name ($Az.NsgRuleName) `
+					--priority ($Az.NsgRulePriority) `
+					--access ("Allow") `
+					--description ($Az.NsgRuleName) `
+					--destination-address-prefixes ("VirtualNetwork") `
+					--destination-port-ranges ("*") `
+					--direction ("Inbound") `
+					--protocol ("*") `
+					--source-address-prefixes ($Az.NsgSourceAddressPrefixes) `
+					--source-port-ranges ("*") `
+			);
+
+	}
+
+}
+
+$Az;

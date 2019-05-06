a4a22bfcb7c1068f745848beab0a55e8f80af97f
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-06 10:15:48 -0400

    Update ddns_updater.ps1

--- ddns/ddns_updater.ps1
+++ ddns/ddns_updater.ps1
@@ -2,11 +2,11 @@
 # ddns_updater
 #		--> Applying basic DDNS - Allows for resolution of a static hostname to proxy towards a non-static (dynamic) endpoint (IPv4)
 #
-ForEach (`$LocalUser In (Get-ChildItem ('C:/Users'))) {
-	If (Test-Path ((`$LocalUser.FullName)+('/.namecheap/secret'))) {
-		[System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content(((`$LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse();
+ForEach ($LocalUser In (Get-ChildItem ('C:/Users'))) {
+	If (Test-Path (($LocalUser.FullName)+('/.namecheap/secret'))) {
+		[System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((($LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse();
 	};
-	If (Test-Path ((`$LocalUser.FullName)+('/.duck-dns/secret'))) {
-		[System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content(((`$LocalUser.FullName)+('/.duck-dns/secret'))))))).GetResponse();
+	If (Test-Path (($LocalUser.FullName)+('/.duck-dns/secret'))) {
+		[System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((($LocalUser.FullName)+('/.duck-dns/secret'))))))).GetResponse();
 	};
 };

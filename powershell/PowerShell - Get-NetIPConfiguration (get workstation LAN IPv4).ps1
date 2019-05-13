7ad3a9c730ccd0a6016fd155edc632c108255dd5
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-13 12:31:15 -0400

    PowerShell LAN IPv4

--- powershell/PowerShell - Get-NetIPConfiguration (get workstation LAN IPv4).ps1
+++ powershell/PowerShell - Get-NetIPConfiguration (get workstation LAN IPv4).ps1
@@ -1,16 +1,24 @@
 
-$ThisIPv4_LAN = (
-	Get-NetIPConfiguration |
-	Where-Object {
-		$_.IPv4DefaultGateway -ne $null -and
-		$_.NetAdapter.Status -ne "Disconnected"
-	}
-).IPv4Address.IPAddress;
+### DEPRECATED - USE POWERSHELL METHOD "Test-Connection", INSTEAD
 
-$ThisIPv4_LAN;
+<#
+	$ThisIPv4_LAN = (
+		Get-NetIPConfiguration |
+		Where-Object {
+			$_.IPv4DefaultGateway -ne $null -and
+			$_.NetAdapter.Status -ne "Disconnected"
+		}
+	).IPv4Address.IPAddress;
+
+	$ThisIPv4_LAN;
+#>
 
 #
 #	Citation(s)
 #
-#		Thanks to StackOverflow user [ Lucas ] on forum [ https://stackoverflow.com/questions/27277701 ]
+#		Documentation
+#			docs.microsoft.com, "Get-NetIPConfiguration", https://docs.microsoft.com/en-us/powershell/module/nettcpip/get-netipconfiguration
+#
+#		Solution
+#			Thanks to StackOverflow user [ Lucas ] on forum [ https://stackoverflow.com/questions/27277701 ]
 #
---
+++ powershell/PowerShell - Test-Connection (get workstation LAN IPv4).ps1
@@ -0,0 +1,17 @@
+
+
+
+$ThisIPv4_LAN = (Test-Connection -ComputerName($env:COMPUTERNAME) -Count (1)).IPV4Address.IPAddressToString;
+
+$ThisIPv4_LAN;
+
+
+#
+#	Citation(s)
+#
+#		Documentation
+#			docs.microsoft.com, "Test-Connection", https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-connection
+#
+#		Solution
+#			Thanks to StackOverflow user [ Jana Sattainathan ] on forum [ https://stackoverflow.com/questions/27277701 ]
+#

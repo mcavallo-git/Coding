3e8c1eebfe9f2a1ad51ed25d534b86220956a34e
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-13 12:24:41 -0400

    Create PowerShell - Get-NetIPConfiguration (get workstation LAN IPv4).ps1

---
+++ powershell/PowerShell - Get-NetIPConfiguration (get workstation LAN IPv4).ps1
@@ -0,0 +1,16 @@
+
+$ThisIPv4_LAN = (
+    Get-NetIPConfiguration |
+    Where-Object {
+        $_.IPv4DefaultGateway -ne $null -and
+        $_.NetAdapter.Status -ne "Disconnected"
+    }
+).IPv4Address.IPAddress;
+
+$ThisIPv4_LAN;
+
+#
+#	Citation(s)
+#
+#		Thanks to StackOverflow user [ Lucas ] on forum [ https://stackoverflow.com/questions/27277701 ]
+#

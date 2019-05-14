05e6d8c24ef31ba863aa0504bebc7316f575ce33
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-14 19:23:02 -0400

    Defender exclusions

--- powershell/PowerShell - Add-MpPreference (Windows Defender Exclusions).ps1
+++ powershell/PowerShell - Add-MpPreference (Windows Defender Exclusions).ps1
@@ -1,6 +1,6 @@
 # Windows Security - Items to add to Windows Defender's Exclusion List
 
-# 
+# User / Windows Directories
 $LocalAppData = (${Env:LocalAppData}); # LocalAppData
 $ProgFilesX64 = ((${Env:SystemDrive})+("\Program Files")); # ProgFilesX64
 $ProgFilesX86 = ((${Env:SystemDrive})+("\Program Files (x86)")); # ProgFilesX86
@@ -8,29 +8,56 @@ $SystemDrive = (${Env:SystemDrive}); # SystemDrive
 $System32 = ((${Env:SystemRoot})+("\System32")); # System32
 $UserProfile = (${Env:USERPROFILE}); # UserProfile
 
-# Windows Defender Exclusions-list --> Processes
+# **** PROCESS EXCLUSIONS
+
 $ExclusionProcesses = @();
+# -- PROCESSES -- LocalAppData
+$ExclusionProcesses += ((${LocalAppData})+("\Dropbox\Client\Dropbox.exe"));
+$ExclusionProcesses += ((${LocalAppData})+("\GitHubDesktop\GitHubDesktop.exe"));
+# -- PROCESSES -- ProgFiles X64
 $ExclusionProcesses += ((${ProgFilesX64})+("\AutoHotkey\AutoHotkey.exe"));
-$ExclusionProcesses += ((${ProgFilesX64})+("\Microsoft VS Code\Code.exe"));
+$ExclusionProcesses += ((${ProgFilesX64})+("\Classic Shell\ClassicStartMenu.exe"));
 $ExclusionProcesses += ((${ProgFilesX64})+("\Cryptomator\Cryptomator.exe"));
-$ExclusionProcesses += ((${ProgFilesX86})+("\Dropbox\Client\Dropbox.exe"));
-$ExclusionProcesses += ((${ProgFilesX86})+("\Mobatek\MobaXterm\MobaXterm.exe"));
-$ExclusionProcesses += ((${ProgFilesX86})+("\Microsoft Office\root\Office16\lync.exe"));
 $ExclusionProcesses += ((${ProgFilesX64})+("\FileZilla FTP Client\filezilla.exe"));
 $ExclusionProcesses += ((${ProgFilesX64})+("\Git\cmd\git.exe"));
 $ExclusionProcesses += ((${ProgFilesX64})+("\Git\git-bash.exe"));
 $ExclusionProcesses += ((${ProgFilesX64})+("\Greenshot\Greenshot.exe"));
-$ExclusionProcesses += ((${LocalAppData})+("\Dropbox\Client\Dropbox.exe"));
-$ExclusionProcesses += ((${LocalAppData})+("\GitHubDesktop\GitHubDesktop.exe"));
+$ExclusionProcesses += ((${ProgFilesX64})+("\Microsoft VS Code\Code.exe"));
+$ExclusionProcesses += ((${ProgFilesX64})+("\NVIDIA Corporation\NvContainer\nvcontainer.exe"));
+$ExclusionProcesses += ((${ProgFilesX64})+("\NVIDIA Corporation\ShadowPlay\nvsphelper64.exe"));
+$ExclusionProcesses += ((${ProgFilesX64})+("\NVIDIA Corporation\NVIDIA GeForce Experience\NVIDIA GeForce Experience.exe"));
+$ExclusionProcesses += ((${ProgFilesX64})+("\NVIDIA Corporation\NVIDIA GeForce Experience\NVIDIA Notification.exe"));
+$ExclusionProcesses += ((${ProgFilesX64})+("\NVIDIA Corporation\NVIDIA GeForce Experience\NVIDIA Share.exe"));
+# -- PROCESSES -- ProgFiles X86
+$ExclusionProcesses += ((${ProgFilesX86})+("\Dropbox\Client\Dropbox.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Intel\Thunderbolt Software\tbtsvc.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Intel\Thunderbolt Software\Thunderbolt.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Unigine\Heaven Benchmark 4.0\bin\Heaven.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Microsoft Office\root\Office16\lync.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Mobatek\MobaXterm\MobaXterm.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Razer\Razer Services\Razer Central\Razer Central.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Razer\Razer Services\Razer Central\Razer Updater.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Razer\Razer Services\Razer Central\RazerCentralService.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Razer\Synapse3\Service\Razer Synapse Service.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Razer\Synapse3\UserProcess\Razer Synapse Service Process.exe"));
+$ExclusionProcesses += ((${ProgFilesX86})+("\Razer\Synapse3\WPFUI\Framework\Razer Synapse 3 Host\Razer Synapse 3.exe"));
+# -- PROCESSES -- System32
+$ExclusionProcesses += ((${System32})+("\DbxSvc.exe"));  # (DROPBOX)
+$ExclusionProcesses += ((${System32})+("\DriverStore\FileRepository\igdlh64.inf_amd64_8a9535cd18c90bc3\igfxEM.exe"));  # (INTEL)
+$ExclusionProcesses += ((${System32})+("\DriverStore\FileRepository\igdlh64.inf_amd64_8a9535cd18c90bc3\IntelCpHDCPSvc.exe"));  # (INTEL)
+# -- PROCESSES -- UserProfile
 $ExclusionProcesses += ((${UserProfile})+("\Documents\MobaXterm\slash\bin\Motty.exe"));
 
-# Windows Defender Exclusions-list --> Filepaths
+
+
+# **** FILEPATH EXCLUSIONS
+
 $ExclusionPaths = @();
-# -- LocalAppData
+# -- FILEPATHS -- LocalAppData
 $ExclusionPaths += ((${LocalAppData})+("\Google\Google Apps Sync"));
 $ExclusionPaths += ((${LocalAppData})+("\GitHubDesktop"));
 $ExclusionPaths += ((${LocalAppData})+("\Microsoft\OneDrive"));
-# -- ProgFilesX64
+# -- FILEPATHS -- ProgFiles X64
 $ExclusionPaths += ((${ProgFilesX64})+("\7-Zip"));
 $ExclusionPaths += ((${ProgFilesX64})+("\AirParrot 2"));
 $ExclusionPaths += ((${ProgFilesX64})+("\AutoHotkey"));
@@ -48,7 +75,7 @@ $ExclusionPaths += ((${ProgFilesX64})+("\Microsoft Office 15"));
 $ExclusionPaths += ((${ProgFilesX64})+("\Microsoft VS Code"));
 $ExclusionPaths += ((${ProgFilesX64})+("\NVIDIA Corporation"));
 $ExclusionPaths += ((${ProgFilesX64})+("\paint.net"));
-# -- ProgFilesX86
+# -- FILEPATHS -- ProgFiles X86
 $ExclusionPaths += ((${ProgFilesX86})+("\Dropbox"));
 $ExclusionPaths += ((${ProgFilesX86})+("\efs"));
 $ExclusionPaths += ((${ProgFilesX86})+("\GIGABYTE"));
@@ -65,15 +92,15 @@ $ExclusionPaths += ((${ProgFilesX86})+("\Razer Chroma SDK"));
 $ExclusionPaths += ((${ProgFilesX86})+("\Reflector 3"));
 $ExclusionPaths += ((${ProgFilesX86})+("\Splashtop"));
 $ExclusionPaths += ((${ProgFilesX86})+("\WinDirStat"));
-# -- SystemDrive
+# -- FILEPATHS -- SystemDrive
 $ExclusionPaths += ((${SystemDrive})+("\BingBackground"));
 $ExclusionPaths += ((${SystemDrive})+("\ISO\BingBackground"));
 $ExclusionPaths += ((${SystemDrive})+("\ISO\QuickNoteSniper"));
-# -- System32
+# -- FILEPATHS -- System32
 $ExclusionPaths += ((${System32})+("\wbem\WmiPrvSE.exe")); # Windows Management Instrumentation Provider
 $ExclusionPaths += ((${System32})+("\DbxSvc.exe")); # Dropbox
-
-$UserProfile=(${Env:UserProfile}); # UserProfile Path(s)
+# -- FILEPATHS -- UserProfile
+$UserProfile=(${Env:UserProfile});
 $ExclusionPaths += ((${UserProfile})+("\Dropbox"));
 
 # Add OneDrive paths to exclusions list

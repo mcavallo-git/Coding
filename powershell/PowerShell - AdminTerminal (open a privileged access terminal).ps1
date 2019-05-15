ae643362c093b1b576c5706e2c6402445071e2c2
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-15 11:42:44 -0400

    Update PowerShell - AdminTerminal (open a privileged access terminal).ps1

--- powershell/PowerShell - AdminTerminal (open a privileged access terminal).ps1
+++ powershell/PowerShell - AdminTerminal (open a privileged access terminal).ps1
@@ -1,22 +1,25 @@
 
 
 # Determine if runtime user is a privilged user (or not)
-Function RuntimeUserIsPrivileged() {
-	$ReturnedVal;
+Function RuntimeUserIsPrivileged {
+	$ReturnedVal = $Null;
 	If (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-		$ReturnedVal = $true;
+		$ReturnedVal = $True;
 	} Else {
-		$ReturnedVal = $false;
+		$ReturnedVal = $False;
 	}
 	Return $ReturnedVal;
 }
 
-
+Write-Host (("RuntimeUserIsPrivileged ? [")+(RuntimeUserIsPrivileged)+("]"));
+Start-Sleep 3;
 
 # Relaunch the script with administrator privileges
-Function AdminTerminal() {
-	If (RuntimeUserIsPrivileged -eq $true) {
+Function AdminTerminal {
+	If ((RuntimeUserIsPrivileged) -eq $True) {
+		Write-Host "Opening Privileged-Access Terminal...";
 		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -Verb RunAs;
+		Start-Sleep 10;
 		Exit;
 	} Else {
 		Write-Host "Admin required by non-existent";
@@ -27,6 +30,7 @@ Function AdminTerminal() {
 # Run the function
 AdminTerminal;
 
+Start-Sleep 10;
 
 #
 #	Citation(s)

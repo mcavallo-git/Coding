ece54c2b0bb1128384d00b74d738485c358d04b3
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-15 14:00:57 -0400

    Update PowerShell - AdminTerminal (open a privileged access terminal).ps1

--- powershell/PowerShell - AdminTerminal (open a privileged access terminal).ps1
+++ powershell/PowerShell - AdminTerminal (open a privileged access terminal).ps1
@@ -1,9 +1,11 @@
 
 
-# Determine if runtime user is a privilged user (or not)
-Function RuntimeUserIsPrivileged {
+# Determine if Session is running as an Administrator
+Function  {
 	$ReturnedVal = $Null;
-	If (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	$RuntimeIdentity = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent());
+	$AdminRole = ([Security.Principal.WindowsBuiltInRole]"Administrator");
+	If ($RuntimeIdentity.IsInRole($AdminRole)) {
 		$ReturnedVal = $True;
 	} Else {
 		$ReturnedVal = $False;
@@ -11,12 +13,36 @@ Function RuntimeUserIsPrivileged {
 	Return $ReturnedVal;
 }
 
-Write-Host (("RuntimeUserIsPrivileged ? [")+(RuntimeUserIsPrivileged)+("]"));
+Write-Host (("RuntimeIsPrivileged ? [")+(RuntimeIsPrivileged)+("]"));
 Start-Sleep 3;
+
+# Determine if the runtime user is part of the "Administrators" security identifier, "S-1-5-32-544"
+#		--> Note: This is what happens a user right-clicks & selects "Run as Administrator" on a given executable in Windows
+$Owners = @{};
+# [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+([System.Security.Principal.WindowsIdentity]::GetCurrent()).Groups.Value.Contains("S-1-5-32-544")
+(([System.Security.Principal.WindowsIdentity]::GetCurrent()).Owner -eq "S-1-5-32-544")
+
+([System.Security.Principal.WindowsIdentity]::GetCurrent()).Groups
+
+([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(([Security.Principal.WindowsBuiltInRole]"Administrator"))
+[Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
+
+Function RunningAsAdministratorUserGroup {
+	$ReturnedVal = $Null;
+	$RuntimeIdentity = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent());
+	$AdminRole = ([Security.Principal.WindowsBuiltInRole]"Administrator");
+	If ($RuntimeIdentity.IsInRole($AdminRole)) {
+		$ReturnedVal = $True;
+	} Else {
+		$ReturnedVal = $False;
+	}
+	Return $ReturnedVal;
+}
 
 # Relaunch the script with administrator privileges
 Function AdminTerminal {
-	If ((RuntimeUserIsPrivileged) -eq $False) {
+	If ((RuntimeIsPrivileged) -eq $False) {
 		Write-Host "Opening Privileged-Access Terminal...";
 		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -Verb RunAs;
 		Start-Sleep 10;
@@ -35,6 +61,11 @@ Start-Sleep 10;
 #
 #	Citation(s)
 #
-#		github.com, "Windows 10 Initial Setup Script"
-#			https://github.com/Disassembler0/Win10-Initial-Setup-Script
+#		docs.microsoft.com
+#			"WindowsPrincipal.IsInRole Method"
+#			 https://docs.microsoft.com/en-us/dotnet/api/system.security.principal.windowsprincipal.isinrole?view=netframework-4.8
+#
+#		github.com
+#			"Windows 10 Initial Setup Script"
+#			 https://github.com/Disassembler0/Win10-Initial-Setup-Script
 #

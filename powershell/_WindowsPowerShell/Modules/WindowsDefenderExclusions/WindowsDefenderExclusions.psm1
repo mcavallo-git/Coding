8c0cc7bd0002e3b84e8f5554912128aa8dc6f218
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-19 20:33:31 -0400

    Update WindowsDefenderExclusions.psm1

--- powershell/_WindowsPowerShell/Modules/WindowsDefenderExclusions/WindowsDefenderExclusions.psm1
+++ powershell/_WindowsPowerShell/Modules/WindowsDefenderExclusions/WindowsDefenderExclusions.psm1
@@ -146,7 +146,6 @@ function WindowsDefenderExclusions {
 		$ExcludedProcesses += ((${ProgFilesX64})+("\Malwarebytes\Anti-Malware\mbamtray.exe"));
 		$ExcludedProcesses += ((${ProgFilesX64})+("\Malwarebytes\Anti-Malware\mbamservice.exe"));
 		$ExcludedProcesses += ((${ProgFilesX64})+("\Microsoft VS Code\Code.exe"));
-		$ExcludedProcesses += ((${ProgFilesX64})+("\Microsoft VS Code\*\rg.exe"));
 		$ExcludedProcesses += ((${ProgFilesX64})+("\Microsoft VS Code\resources\app\node_modules.asar.unpacked\vscode-ripgrep\bin\rg.exe"));
 		$ExcludedProcesses += ((${ProgFilesX64})+("\NVIDIA Corporation\Display.NvContainer\NVDisplay.Container.exe"));
 		$ExcludedProcesses += ((${ProgFilesX64})+("\NVIDIA Corporation\NvContainer\nvcontainer.exe"));

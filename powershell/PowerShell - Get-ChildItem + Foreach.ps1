2f609f2f68bdbb840cb5c6ff3b31a587f502dd27
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-21 02:25:00 -0400

    Update PowerShell - Get-ChildItem + Foreach.ps1

--- powershell/PowerShell - Get-ChildItem + Foreach.ps1
+++ powershell/PowerShell - Get-ChildItem + Foreach.ps1
@@ -1,4 +1,36 @@
-
-ForEach ($EachDir In ((Get-ChildItem -Directory -Name -Path ($Home) -Include ("Do*")).GetEnumerator())) {
-	Write-Host $EachDir;
+ForEach ($EachItem In ((Get-ChildItem -File -Path ($Home) -Include ("*GitHub*") -Recurse).GetEnumerator())) {
+Write-Host "-------------------------";
+# Write-Host "EachItem | Format-List"; $EachItem | Format-List;
+# Write-Host "EachItem.GetType()"; $EachItem.GetType();
+Write-Host "EachItem.FullName"; $EachItem.FullName;
+Write-Host "=========================";
 }
+
+#
+# Get-ChildItem switch-parameter "-Directory" returns items with type "System.IO.FileSystemInfo.DirectoryInfo"
+#
+# Get-ChildItem switch-parameter "-File" returns items with type "System.IO.FileSystemInfo.FileInfo"
+#
+
+
+#
+#
+#
+#
+#		https://docs.microsoft.com/en-us/dotnet/api/system.io.directoryinfo
+#
+#
+#	Citation(s)
+#		
+#		Icon file "GitSyncAll.ico" thanks-to:  https://docs.microsoft.com/en-us/dotnet/api/system.io.directoryinfo
+
+#
+
+#		docs.microsoft.com
+#			"DirectoryInfo Class"
+#		 	 https://docs.microsoft.com/en-us/dotnet/api/system.io.directoryinfo
+#
+#		docs.microsoft.com
+#			"FileInfo Class"
+#		 	 https://docs.microsoft.com/en-us/dotnet/api/system.io.fileinfo
+#

7a8d4742b578a49f8c6901748fa7e9b9c7a987f5
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-21 03:59:14 -0400

    Create PowerShell - _CommonParameters.ps1

---
+++ powershell/PowerShell - _CommonParameters.ps1
@@ -0,0 +1,27 @@
+
+# The following list displays the common parameters. Their aliases are listed in parentheses.
+Debug (db)
+ErrorAction (ea)
+ErrorVariable (ev)
+InformationAction (infa)
+InformationVariable (iv)
+OutVariable (ov)
+OutBuffer (ob)
+PipelineVariable (pv)
+Verbose (vb)
+WarningAction (wa)
+WarningVariable (wv)
+
+# The risk mitigation parameters are:
+WhatIf (wi)
+Confirm (cf)
+
+
+#
+#	Citation(s)
+#
+#		docs.microsoft.com
+#		"About CommonParameters"
+#		 https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_commonparameters?view=powershell-6
+#		 2019-05-21_03-13-42
+#

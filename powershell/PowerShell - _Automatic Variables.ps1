36100e456f571ac02c590c03bd420bc651cb8020
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-21 12:25:34 -0400

    Create PowerShell - _Automatic Variables.ps1

---
+++ powershell/PowerShell - _Automatic Variables.ps1
@@ -0,0 +1,12 @@
+
+See citations, below - especially docs.microsoft.com
+
+#
+#	Citation(s)
+#
+#		docs.microsoft.com | About Automatic Variables
+#			https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-5.1 | 2019-05-21_10-37-41
+#
+#		ss64.com | Automatic Variables
+#			https://ss64.com/ps/syntax-automatic-variables.html | 2019-05-21_10-36-13
+#

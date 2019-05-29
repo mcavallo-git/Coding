ff885bb906d39d2c17126408040eb286383470ac
mcavallo@boneal.com <mcavallo@boneal.com>
2019-05-29 04:46:22 -0400

    Update PowerShell - PSScriptRoot (get script directory).ps1

--- powershell/PowerShell - PSScriptRoot (get script directory).ps1
+++ powershell/PowerShell - PSScriptRoot (get script directory).ps1
@@ -1,26 +1,9 @@
 
 $ReversePathLookups = @();
 
-# Running Script - Fullpath
+# Running Script - Parent Directory
 $ReversePathLookups += @{
-	Description = "Fullpath of the PowerShell script currently being executed";
-	Commands = @(
-		@{
-			Command = "`$PSCommandPath";
-			CurrentValue = ($PSCommandPath);
-			MinimumPowerShellVersion = 3.0;
-		},
-		@{
-			Command = "`$MyInvocation.MyCommand.Path";
-			CurrentValue = ($MyInvocation.MyCommand.Path);
-			MinimumPowerShellVersion = 2.0;
-		}
-	)
-};
-
-# Running Script - Parent directort
-$ReversePathLookups += @{
-	Description = "Dirname (parent directory) of the PowerShell script currently being executed";
+	Description = "Dirname / parent directory of the script currently being executed";
 	Commands = @(
 		@{
 			Command = "`$PSScriptRoot";
@@ -37,24 +20,45 @@ $ReversePathLookups += @{
 
 # Working-Path / Working-Directory
 $ReversePathLookups += @{
-	Description = "Working-Path / Working-Directory (directory of the current PowerShell session)";
+	Description = "Working-Path / Working-Directory of the current session";
 	Commands = @(
 		@{
 			Command = "(Get-Item -Path `".\`").FullName;";
 			CurrentValue = (Get-Item -Path ".\").FullName;
-			MinimumPowerShellVersion = '??';
+			MinimumPowerShellVersion = 3.0;
 		}
 	)
 };
 
+# Running Script - Fullpath
+$ReversePathLookups += @{
+	Description = "Fullpath of the script currently being executed";
+	Commands = @(
+		@{
+			Command = "`$PSCommandPath";
+			CurrentValue = ($PSCommandPath);
+			MinimumPowerShellVersion = 3.0;
+		},
+		@{
+			Command = "`$MyInvocation.MyCommand.Path";
+			CurrentValue = ($MyInvocation.MyCommand.Path);
+			MinimumPowerShellVersion = 2.0;
+		}
+	)
+};
+
+Clear-Host;
 ForEach ($EachLookup In $ReversePathLookups) {
 	Write-Host (("`n")+("-"*60)+("`n"));
-	Write-Host (("  Description: ")+($EachLookup.Description));
+	Write-Host ((" *** ")+($EachLookup.Description)+("`n"));
 	ForEach ($EachCommand In $EachLookup.Commands) {
-		Write-Host "";
-		ForEach ($EachProperty In $EachCommand.GetEnumerator()) {
-			Write-Host (("    ")+($EachProperty.Name)+(": ")+($EachProperty.Value));
-		}
+		Write-Host "    " -NoNewLine;
+		Write-Host $EachCommand.Command -NoNewLine -ForegroundColor "Green";
+		Write-Host (("   -->   ")+($EachCommand.CurrentValue)+("   (requires PowerShell ")+($EachCommand.MinimumPowerShellVersion)+("+)`n"));
+		# Write-Host "";
+		# ForEach ($EachProperty In $EachCommand.GetEnumerator()) {
+		# 	Write-Host (("    ")+($EachProperty.Name)+(": ")+($EachProperty.Value));
+		# }
 	}
 }
 Write-Host (("`n")+("-"*60)+("`n"));

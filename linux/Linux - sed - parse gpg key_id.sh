d676578b16fdb645f7a8633a0f490dff34266c28
Matt Cavallo <mcavallo.work@gmail.com>
2019-05-31 16:13:19 -0400

    Update Linux - sed - parse gpg key_id.sh

--- linux/Linux - sed - parse gpg key_id.sh
+++ linux/Linux - sed - parse gpg key_id.sh
@@ -2,5 +2,5 @@
 
 # Linux - sed - parse GnuPG key_id's string out of gpg's returned LONG format-values
 
-GNUPG_KEY_ID=$(gpg --list-secret-keys --keyid-format 'LONG' | sed --regexp-extended --quiet --expression='s/^sec\ +([A-Za-z0-9]+)\/([A-F0-9]{16})\ +([0-9\-]{1,10})\ +(.+)$/\2/p');
-
+GnuPG_KeyID=$(gpg --list-secret-keys --keyid-format 'LONG' | sed --regexp-extended --quiet --expression='s/^sec\ +([A-Za-z0-9]+)\/([A-F0-9]{16})\ +([0-9\-]{1,10})\ +(.+)$/\2/p');
+echo "GnuPG_KeyID=\"${GnuPG_KeyID}\"";

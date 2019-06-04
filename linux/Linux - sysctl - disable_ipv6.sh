36a9d6d1799230ed7b53ba4e50072311428df414
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-04 14:39:29 -0400

    SysctlDisableIPV6

--- linux/Linux - sysctl - disable_ipv6.sh
+++ linux/Linux - sysctl - disable_ipv6.sh
@@ -20,19 +20,19 @@ GREP_IPV6_LO=$(cat "${CONF_DISABLE_IPV6}" | grep "${DISABLE_IPV6_LO}");
 
 if [ -z "${GREP_IPV6_ALL}" ]; then
 	echo "$(date +'%Y-%m-%d %H:%M:%S') Adding \"${DISABLE_IPV6_ALL}\" to sysctl file \"${CONF_DISABLE_IPV6}\"...";
-	echo "${DISABLE_IPV6_ALL}" > "${CONF_DISABLE_IPV6}";
+	echo "${DISABLE_IPV6_ALL}" >> "${CONF_DISABLE_IPV6}";
 	RELOAD_SYSCTL="1";
 fi;
 
 if [ -z "${GREP_IPV6_DEF}" ]; then
 	echo "$(date +'%Y-%m-%d %H:%M:%S') Adding \"${DISABLE_IPV6_DEF}\" to sysctl file \"${CONF_DISABLE_IPV6}\"...";
-	echo "${DISABLE_IPV6_DEF}" > "${CONF_DISABLE_IPV6}";
+	echo "${DISABLE_IPV6_DEF}" >> "${CONF_DISABLE_IPV6}";
 	RELOAD_SYSCTL="1";
 fi;
 
 if [ -z "${GREP_IPV6_LO}" ]; then
 	echo "$(date +'%Y-%m-%d %H:%M:%S') Adding \"${DISABLE_IPV6_LO}\" to sysctl file \"${CONF_DISABLE_IPV6}\"...";
-	echo "${DISABLE_IPV6_LO}" > "${CONF_DISABLE_IPV6}";
+	echo "${DISABLE_IPV6_LO}" >> "${CONF_DISABLE_IPV6}";
 	RELOAD_SYSCTL="1";
 fi;
 

a3d6ab5aed178d816966dfa2512bd801dd7674f5
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-06 11:41:54 -0400

    Update Linux - cron (logging all jobs).sh

--- linux/Linux - cron (logging all jobs).sh
+++ linux/Linux - cron (logging all jobs).sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 if [ "$(whoami)" != "root" ]; then
-	FAIL_MSG="${FAIL_MSG} Must run ${0} as user 'root'";
+
+	echo "Error -  Must run ${0} as user 'root'";
+	exit 1;
+
+else 
+
+	### Use sed to parse-out and remove the comment block in-front of the cron-job logger
+
+	RSYSLOG_DEFAULTS_CONF="/etc/rsyslog.d/50-default.conf";
+	SED_ENABLE_CRON_LOGS='s|^#cron|cron|g';
+	sed --in-place --expression="${SED_ENABLE_CRON_LOGS}" "${RSYSLOG_DEFAULTS_CONF}";
+
+	# "#cron.*                         /var/log/cron.log"
+
 fi;
-
-### Use sed to parse-out and remove the comment block in-front of the cron-job logger
-
-RSYSLOG_DEFAULTS_CONF="/etc/rsyslog.d/50-default.conf";
-SED_ENABLE_CRON_LOGS='s|^#cron|cron|g';
-sed --in-place --expression="${SED_ENABLE_CRON_LOGS}" "${RSYSLOG_DEFAULTS_CONF}";
-
-# "#cron.*                         /var/log/cron.log"
-

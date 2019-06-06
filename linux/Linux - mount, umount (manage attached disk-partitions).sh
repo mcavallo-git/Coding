e74cbaec751b5c5e8810842acb9cbe9fd026d6cd
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-06 15:41:07 -0400

    Update Linux - mount, umount (manage attached disk-partitions).sh

--- linux/Linux - mount, umount (manage attached disk-partitions).sh
+++ linux/Linux - mount, umount (manage attached disk-partitions).sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 # check that file-system doesn't just say "data", otherwise it must be formatted
-sudo file -s /dev/xvdf
 sudo file -s /dev/sda1
+sudo file -s /dev/xvda1
 
 # make a location to mount the drive
 sudo mkdir /data
 
 # mount a drive at a location
-sudo mount /dev/xvdf1 /data
+mkdir -p "/mnt/xvda1";
+sudo mount "/dev/xvda1" "/mnt/xvda1";
 
 # unmount a drive
-umount -d /dev/xvdf1
-#  (^^ did this via AWS console
-# 			shutdown EC2 Instance - volumes -> detach volume
-# 			volume->attach volume - /dev/sd... <--- THIS NAME WAS KEY, MUST BE EXACTLY THIS - 2017-09-25, MCavallo)
+umount -d "/dev/xvda1"
+#
+# ^^ did this via AWS console
+#    shutdown EC2 Instance - volumes -> detach volume
+#    volume->attach volume - /dev/sda1
+
+
 
 
 #
-# AWS | EBS (EC2) Encryption at Rest (EBS-Volume Encryption)
-# 	It's possible to copy an unencrypted EBS snapshot to an encrypted EBS snapshot. So the following process can be used:
+# AWS | EBS (EC2) Encryption at Rest
+#  |
+#  |--> Overview: Create a snapshot of unencrypted volume - find the snapshot -> create volume -> select "enrcypted". While EC2 is off, find the old partition/volume's mount-path, then indiana jones the new volume onto the EC2 instance by first unmounting the old volume, then mounting the new volume @ the same mount-path. Done.
+#
 # 	1. Stop your EC2 instance.
 # 	2. Create an EBS snapshot of the volume you want to encrypt.
 # 	3. Copy the EBS snapshot, encrypting the copy in the process (checkbox somewhere along the way, "Encrypt...", can't miss it)
@@ -28,12 +34,17 @@ umount -d /dev/xvdf1
 # 	6. Detach the original (unencrypted) EBS volume
 # 	7. Attach your new (encrypted) EBS volume, making sure to match the device name (/dev/sda1, etc.)
 #	
-# AWS | RDS Encryption at Rest (Must make a new DB from a copied, encrypted Snapshot)
+
+#
+# AWS | RDS Encryption at Rest
+#  |
+#  |--> Overview: Create a snapshot of unencrypted volume - find the snapshot -> restore snapshot -> select "enrcypted". Apply security groups. Point services to new hostname. Done.
+#
 # 	1. Take a snapshot of RDS Instance (must leave it on to do this)
 # 	2. Copy the snapshot, encrypting the copy in the process (checkbox somewhere along the way, "Encrypt...", can't miss it)
 # 	3. Restore Database from the Encrypted Snapshot
 # 	4. Redirect Traffic to new DB
-
+#
 
 
 #

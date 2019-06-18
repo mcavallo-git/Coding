9ad727ef794f730f5bd3a9e5e34b6b6f1812a417
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-18 14:59:03 -0400

    Update PowerShell - Get-WmiObject [Get AD User Info].ps1

--- powershell/PowerShell - Get-WmiObject [Get AD User Info].ps1
+++ powershell/PowerShell - Get-WmiObject [Get AD User Info].ps1
@@ -1,24 +1,23 @@
-
+#
 # PowerShell - Get Info regarding current user which is held in Active Directory
+# 	|--> Potentially shows all (accessible) Office365/Azure Active-Directory related attributes/properties, as well 
+#
 
-$MatchUsername = ($Env:USERNAME);
-$QueryAD = "SELECT * FROM ds_user WHERE ds_sAMAccountName='$MatchUsername'";
-$ResultsnAD = Get-WmiObject -Query $QueryAD -Namespace "root\Directory\LDAP";
 
-$ResultsnAD;
+Get-WmiObject -Query "SELECT * FROM ds_user WHERE ds_sAMAccountName='${Env:USERNAME}'" -Namespace "root\Directory\LDAP";
 
+
+#
+# nltest /dclist:${Env:USERDOMAIN};
+# nltest /dsgetdc:${Env:USERDOMAIN};
+#
 # $ResultsnAD.DS_userPrincipalName;
 # $ResultsnAD.DS_mail;
 # $ResultsnAD.DS_name;
+#
 
-
-
-# ---------------------------------------------------------------------------------------------------------------------- #
 #
 # Citation(s)
 #
-#		docs.microsoft.com, "Get-WmiObject"
-#			https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-wmiobject?view=powershell-5.1
+#	docs.microsoft.com  |  Get-WmiObject  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-wmiobject?view=powershell-5.1
 #
-
-

1414a3666c84396483151d71a1952c5bfeee7050
Matt Cavallo <mcavallo.git@gmail.com>
2019-06-18 06:29:09 -0400

    Create PowerShell - Get-WindowsDriver -Online -All (lists all drivers installed locally).ps1

---
+++ powershell/PowerShell - Get-WindowsDriver -Online -All (lists all drivers installed locally).ps1
@@ -0,0 +1,8 @@
+
+#
+# List all drivers installed on local workstation
+#
+# ***  REQUIRES ADMIN POWERSHELL PROMPT  ***
+#
+
+Get-WindowsDriver -Online -All > (("${HOME}\Desktop\Drivers_")+(${Env:COMPUTERNAME})+("_")+(Get-Date -UFormat "%Y%m%d%H%M%S")+(".log"));

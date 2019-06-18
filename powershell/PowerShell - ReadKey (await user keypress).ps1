c2f0609e1bba349522bf00399b9d0ee9085adcb1
Matt Cavallo <mcavallo.git@gmail.com>
2019-06-18 05:50:12 -0400

    Update PowerShell - ReadKey (await user keypress).ps1

--- powershell/PowerShell - ReadKey (await user keypress).ps1
+++ powershell/PowerShell - ReadKey (await user keypress).ps1
@@ -1,14 +1,47 @@
-
-# Wait for user to press any key
-
-Write-Host -NoNewLine "`n`n  Awaiting KeyPress...";
-$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-Write-Host (("`n`n  You pressed the `"")+($KeyPress.Character)+("`" Key`n`n"));
-
+#	------------------------------------------------------------
+#
+#		PRESS ANY KEY TO EXIT 
+#
 
 Write-Host -NoNewLine "`n`n  Press any key to exit...";
 $KeyPressExit = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
+
+
+#	------------------------------------------------------------
+#
+#		PRESS 'ESCAPE' TO EXIT
+#
+
+Write-Host -NoNewLine "`n`nPress the 'Escape' key to exit... " -BackgroundColor "Black" -ForegroundColor "Yellow";
+$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+While ($KeyPress.VirtualKeyCode -ne 27) {
+	$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
+
+
+
+#	------------------------------------------------------------
+#
+# Debug - View input codes based on key(s) pressed
+#
+Write-Host -NoNewLine "`n`n  Awaiting KeyPress... ";
+$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+Write-Host "`n`nKey-Press:`"";
+$KeyPress | Format-List;
+
+If ( $KeyPress.VirtualKeyCode -eq 27 ) {
+	Write-Host "'Escape' key was pressed";
+} ElseIf ( $KeyPress.VirtualKeyCode -eq 13 ) {
+	Write-Host "'Enter' key was pressed";
+} ElseIf ( $KeyPress.VirtualKeyCode -eq 8 ) {
+	Write-Host "'Backspace' key was pressed";
+}
+
+
+
+#	------------------------------------------------------------
 #
 # Thanks to stackoverflow user 'Knuckle-Dragger' on forum: https://stackoverflow.com/questions/20886243
 #
+#	------------------------------------------------------------

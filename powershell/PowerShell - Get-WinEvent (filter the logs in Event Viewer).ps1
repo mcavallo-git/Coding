d52f70fb6cb4c0119eae95fc78a8f69e9364796b
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-18 15:21:26 -0400

    Update PowerShell - Get-WinEvent (filter the logs in Event Viewer).ps1

--- powershell/PowerShell - Get-WinEvent (filter the logs in Event Viewer).ps1
+++ powershell/PowerShell - Get-WinEvent (filter the logs in Event Viewer).ps1
@@ -7,6 +7,7 @@ $AllLogTimestamps = @();
 $EID_Logon = 4624; #(Security event)
 $EID_Logoff = 4647; #(Security event)
 $EID_Startup = 6005; #(System event)
+$EID_Winlogon_AuthStopped = 2; #(Winlogon authentication started event)
 $EID_Winlogon_AuthStarted = 1; #(Winlogon authentication started event)
 $EID_Winlogoff = 6000; #(Winlogon logoff event)
 $EID_RDP_Reconnect = 4778; #(Security event)
@@ -28,39 +29,72 @@ $Regex_UnlockPC = "\s*Logon Type:\s+7";
 $Regex_LogoffInteractive = "\s*Logon Type:\s+2";
 
 # ------------------------------------------------------------
+# LOGON CATCH 1
 
-# Get-WinEvent -FilterHashtable @{
-# 	LogName='Security'; ProviderName='Microsoft-Windows-Security-Auditing'; ID=${EID_Logon}; StartTime=${StartTime}; EndTime=${EndTime};
-# } | Foreach-Object {
-# 	If (($_.Message -match $Regex_User) -And ($_.Message -match $Regex_Domain) -And ($_.Message -match $Regex_UnlockPC)) {
-# 		$AllLogTimestamps += ((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      !!!!!!! Login !!!!!!!"));
-# 	}
-# }
-
-# ------------------------------------------------------------
-
-$Previous_EventID = $Null;
 Get-WinEvent -FilterHashtable @{
-	LogName='Microsoft-Windows-Winlogon/Operational'; ProviderName='Microsoft-Windows-Winlogon'; ID=${EID_Winlogon_AuthStarted}; StartTime=${StartTime}; EndTime=${EndTime};
+	LogName='Security';
+	ProviderName='Microsoft-Windows-Security-Auditing';
+	ID=${EID_Logon};
+	StartTime=${StartTime};
+	EndTime=${EndTime};
 } | Foreach-Object {
-	If (($_.ID -eq ${EID_Winlogon_AuthStarted}) -And ($_.ID -eq ${EID_Winlogon_AuthStarted}))
 	If (($_.Message -match $Regex_User) -And ($_.Message -match $Regex_Domain) -And ($_.Message -match $Regex_UnlockPC)) {
 		$AllLogTimestamps += ((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      !!!!!!! Login !!!!!!!"));
-		((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      !!!!!!! Login !!!!!!!"));
 	}
-	$Previous_EventID = $_.ID;
 }
 
 # ------------------------------------------------------------
+# LOGON CATCH 2
 
-# Get-WinEvent -FilterHashtable @{
-# 	LogName='Security'; ProviderName='Microsoft-Windows-Security-Auditing'; ID=${EID_Logoff}; StartTime=${StartTime}; EndTime=${EndTime};
-# } | Foreach-Object {
-# 	$_.Message
-# 	If (($_.Message -match $Regex_User) -And ($_.Message -match $Regex_Domain)) {
-# 		$AllLogTimestamps += ((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      xxxxxxx Logout xxxxxxxx"));
-# 	}
-# }
+$PreviousEvent_ID = $Null;
+$PreviousEvent_DateTime = $Null;
+Get-WinEvent -FilterHashtable @{
+	LogName='Microsoft-Windows-Winlogon/Operational';
+	ProviderName='Microsoft-Windows-Winlogon';
+	StartTime=${StartTime};
+	EndTime=${EndTime};
+} | Foreach-Object {
+	If ( `
+		(($_.ID -eq ${EID_Winlogon_AuthStarted}) -Or ($_.ID -eq ${EID_Winlogon_AuthStopped})) `
+			-And `
+		(($PreviousEvent_ID -ne ${EID_Winlogon_AuthStarted}) -And ($PreviousEvent_ID -ne ${EID_Winlogon_AuthStopped})) `
+			-And `
+		($_.TimeCreated.DateTime -eq ${PreviousEvent_DateTime}) `
+	) {
+	# If (($_.Message -match $Regex_User) -And ($_.Message -match $Regex_Domain) -And ($_.Message -match $Regex_UnlockPC)) {
+		$AllLogTimestamps += ((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      !!!!!!! Login !!!!!!!"));
+		# ((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      !!!!!!! Login !!!!!!!"));
+		$_;
+	}
+	$PreviousEvent_ID = $_.ID;
+	$PreviousEvent_DateTime = $_.TimeCreated.DateTime;
+
+}
+
+# ------------------------------------------------------------
+# LOGOFF CATCH 1
+Get-WinEvent -FilterHashtable @{
+	LogName='Security';
+	ProviderName='Microsoft-Windows-Security-Auditing';
+	ID=${EID_Logoff};
+	StartTime=${StartTime};
+	EndTime=${EndTime};
+} | Foreach-Object {
+	$_.Message
+	If (($_.Message -match $Regex_User) -And ($_.Message -match $Regex_Domain)) {
+		$AllLogTimestamps += ((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      xxxxxxx Logout xxxxxxxx"));
+	}
+}
+
+# ------------------------------------------------------------
+# LOGOFF CATCH 2
+Get-WinEvent -FilterHashtable @{
+	LogName='Application';
+	ID="6000";
+} | Foreach-Object {
+	$AllLogTimestamps += ((Get-Date -Date ($_.TimeCreated.DateTime) -UFormat "%Y-%m-%d (%a)  %H:%M:%S")+("      xxxxxxx Logout xxxxxxxx"));
+}
+
 
 # ------------------------------------------------------------
 

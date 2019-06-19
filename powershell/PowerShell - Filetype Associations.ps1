6c2cc8ddb68906a046f32617e28afdd1b94796c1
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-19 17:59:26 -0400

    Update PowerShell - Filetype Associations.ps1

--- powershell/PowerShell - Filetype Associations.ps1
+++ powershell/PowerShell - Filetype Associations.ps1
@@ -3,7 +3,7 @@
 # Lookup associations for a given file-extenson
 #
 
-$FileExtension = ".txt";
+$FileExtension = ".ahk";
 $ExtensionProperties = (Get-ItemProperty (("Registry::HKEY_Classes_root\")+(${FileExtension})));
 $ExtensionAssociations = @{
 	Extension = $ExtensionProperties.PSChildName;
@@ -13,15 +13,29 @@ $ExtensionAssociations = @{
 };
 $ExtensionAssociations;
 
-
-
 Write-Host -NoNewLine "`n`nPress any key to exit...";
 $KeyPressExit = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+Exit 0;
+
+
+
+
+# IN-PROGRESS:
+
+# Resolve all file-extensions to their relative file-types
+cmd /c assoc | more > %USERPROFILE%\Desktop\assoc.more.log;
+
+Get-ChildItem -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts" | Select-Object {
+	$_ | Format-List;
+}
+
 
 
 
 #
 #	Citation(s)
 #
-#		Thanks to StackOverflow user [ Frode F. ] on forum [ https://stackoverflow.com/questions/27645850 ]
+#		Thanks to StackExchange user [ Frode F. ] on forum [ https://stackoverflow.com/questions/27645850 ]
+#
+#		Thanks to StackExchange user [ Keltari ] on forum [ https://superuser.com/questions/362063 ]
 #

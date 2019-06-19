4459a83922b294ff1aee4c01fabe1fd3cf5bd56a
Matt Cavallo <mcavallo.git@gmail.com>
2019-06-19 08:10:15 -0400

    Update Linux - cronjob, one-time run.sh

--- linux/Linux - cronjob, one-time run.sh
+++ linux/Linux - cronjob, one-time run.sh
@@ -5,12 +5,12 @@
 #		Note: to ensure this program runs at-least (and no more-than) once, avoid using
 #		stars/asterisks in the first TWO cronjob numbers (minute/hour), so that we can be exact
 #
-# 18 07 * * * /root/finish_upgrade.sh
+# 37 07 * * * sudo -E "/root/finish_upgrade.sh";
 #
 
 
 # Run this file only on a given day
-if [ "$(date +'%Y-%m-%d %H:%M')" == "2019-06-19 07:18" ]; then # match the cronjob runtime-minute, here
+if [ "$(date +'%Y-%m-%d %H:%M')" == "2019-06-19 07:37" ]; then # match the cronjob runtime-minute, here
 
 # Enable Logging
 LOGFILE="/var/log/finish_upgrade$(date +'%Y-%m-%d_%H-%M-%S')";

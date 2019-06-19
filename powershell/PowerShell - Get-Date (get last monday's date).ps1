8b33246b9d16dab9b326cfe5eb5e985e840fad1b
Matt Cavallo <mcavallo.git@gmail.com>
2019-06-19 07:33:01 -0400

    Update PowerShell - Get-Date (get last monday's date).ps1

--- powershell/PowerShell - Get-Date (get last monday's date).ps1
+++ powershell/PowerShell - Get-Date (get last monday's date).ps1
@@ -5,7 +5,7 @@
 #
 # For example's sake, let's say today is Saturday - From the Saturday perspective, saying "this Monday" could equally refer to the next Monday OR the previous Monday, which, respectively, is 2 days in the future vs. 5 days in the past.
 #
-# The weekend prevents simple arithmetic such as "2 days is less than 5 so 'this Monday' must refer to the next Monday" from being correct, as Saturday appears so much ((visually) closer to the previous week on a paper calendar (lending to "this" referring to "previous", keeping it in the picture).
+# The weekend prevents simple arithmetic such as "2 days is less than 5 so 'this Monday' must refer to the next Monday" from being correct, as Saturday appears much closer to the previous week's row-of-days on a paper calendar (lending to "this" referring to "previous" - keeping both Mondays plausibly in the picture).
 #
 # The outcome: The date of "this Monday" ends up referring to a date in the future while also simultaneously referring to a date in the past - Monday has now become a paradox. Monday must be resolved.
 #

a774667251185e297e72bb03ea949cc835580108
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-20 12:28:14 -0400

    Update Windows - startup-scripts.bat

--- windows/Windows - startup-scripts.bat
+++ windows/Windows - startup-scripts.bat
@@ -1,16 +1,19 @@
 @ECHO OFF
 
-SET "INVIS_SKIPWAIT_VBS=%TEMP%\invis_nowait.vbs"
-ECHO CreateObject("Wscript.Shell").Run """" ^& WScript.Arguments(0) ^& """", 0, FALSE> "%INVIS_SKIPWAIT_VBS%"
-
 SET STARTUP_SCRIPTS_BAT=%USERPROFILE%\.startup-scripts\startup-%COMPUTERNAME%.bat
+
+SET "INVIS_SKIPWAIT_VBS=%TEMP%\invis_nowait.vbs"
+IF NOT EXIST "%STARTUP_SCRIPTS_BAT%" (
+ECHO CreateObject("Wscript.Shell").Run """" ^& WScript.Arguments(0) ^& """", 0, FALSE> "%INVIS_SKIPWAIT_VBS%"
+)
+
 IF EXIST "%STARTUP_SCRIPTS_BAT%" (
-	wscript.exe "%INVIS_SKIPWAIT_VBS%" "%STARTUP_SCRIPTS_BAT%"
+wscript.exe "%INVIS_SKIPWAIT_VBS%" "%STARTUP_SCRIPTS_BAT%"
 )
 
 SET STARTUP_SCRIPTS_LNK=%USERPROFILE%\.startup-scripts\startup-%COMPUTERNAME%.lnk
 IF EXIST "%STARTUP_SCRIPTS_LNK%" (
-	wscript.exe "%INVIS_SKIPWAIT_VBS%" "%STARTUP_SCRIPTS_LNK%"
+wscript.exe "%INVIS_SKIPWAIT_VBS%" "%STARTUP_SCRIPTS_LNK%"
 )
 
 EXIT

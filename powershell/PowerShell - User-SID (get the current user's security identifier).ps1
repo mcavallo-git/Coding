f2a1836a789a253a324e4a358d599c5bcd4c84bf
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-20 14:01:23 -0400

    Update PowerShell - User-SID (get the current user's security identifier).ps1

--- powershell/PowerShell - User-SID (get the current user's security identifier).ps1
+++ powershell/PowerShell - User-SID (get the current user's security identifier).ps1
@@ -1,10 +1,16 @@
 # ------------------------------------------------------------
 #
 # PowerShell
-#   Get User-SID (Security Identifier) for current user
+#   User SIDs (Security Identifiers)
 #
 #                                MCavallo, 2019-06-20_13-58-45
 # ------------------------------------------------------------
 
+
+# Get User-SID (Security Identifier) for current user
 $UserSid = (cmd /c "FOR /f `"tokens=2`" %A IN ('WHOAMI /USER /FO TABLE /NH') DO @echo %A");
 $UserSid;
+
+
+# Get all local (non-domain) user SIDs
+#		wmic useraccount get * /format:list

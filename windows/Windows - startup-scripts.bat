5c70697a03bdd0c2c0396dd09e49674e454ddada
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-20 12:31:08 -0400

    Update Windows - startup-scripts.bat

--- windows/Windows - startup-scripts.bat
+++ windows/Windows - startup-scripts.bat
@@ -1,9 +1,11 @@
 @ECHO OFF
 
-SET STARTUP_SCRIPTS_BAT=%USERPROFILE%\.startup-scripts\startup-%COMPUTERNAME%.bat
+SET STARTUP_SCRIPTS=%USERPROFILE%\.startup-scripts\startup-%COMPUTERNAME%
+SET STARTUP_SCRIPTS_BAT=%STARTUP_SCRIPTS%.bat
+SET STARTUP_SCRIPTS_LNK=%STARTUP_SCRIPTS%.lnk
 
 SET "INVIS_SKIPWAIT_VBS=%TEMP%\invis_nowait.vbs"
-IF NOT EXIST "%STARTUP_SCRIPTS_BAT%" (
+IF NOT EXIST "%INVIS_SKIPWAIT_VBS%" (
 ECHO CreateObject("Wscript.Shell").Run """" ^& WScript.Arguments(0) ^& """", 0, FALSE> "%INVIS_SKIPWAIT_VBS%"
 )
 
@@ -11,7 +13,6 @@ IF EXIST "%STARTUP_SCRIPTS_BAT%" (
 wscript.exe "%INVIS_SKIPWAIT_VBS%" "%STARTUP_SCRIPTS_BAT%"
 )
 
-SET STARTUP_SCRIPTS_LNK=%USERPROFILE%\.startup-scripts\startup-%COMPUTERNAME%.lnk
 IF EXIST "%STARTUP_SCRIPTS_LNK%" (
 wscript.exe "%INVIS_SKIPWAIT_VBS%" "%STARTUP_SCRIPTS_LNK%"
 )
@@ -21,7 +22,7 @@ EXIT
 REM	------------------------------------------------------------
 REM	Revisioning | 2018-11-28 | Created
 REM	Revisioning | 2018-12-12 | Generalized parameters
-REM	Revisioning | 2018-12-12 | New "~\.startup-scripts" format
+REM	Revisioning | 2019-06-19 | Format "~\.startup-scripts\..."
 REM	------------------------------------------------------------
 REM	Author | Matt Cavallo | https://github.com/mcavallo-git
 REM	------------------------------------------------------------

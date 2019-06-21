e8485611ac8c1aab4964fcef6d06a079d8b16d10
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-21 13:55:59 -0400

    Update TaskSnipe.psm1

--- powershell/_WindowsPowerShell/Modules/TaskSnipe/TaskSnipe.psm1
+++ powershell/_WindowsPowerShell/Modules/TaskSnipe/TaskSnipe.psm1
@@ -10,6 +10,9 @@ function TaskSnipe {
 		[String]$SnipeTarget,
 
 		[Switch]$CurrentUserMustOwn,
+
+		[Switch]$ExactImageName,
+
 		[Switch]$Quiet
 
 	)
@@ -21,17 +24,29 @@ function TaskSnipe {
 	If ($PSBoundParameters.ContainsKey('CurrentUserMustOwn')) {
 		$TASKLIST_FILTERS += " /FI `"USERNAME eq ${Env:USERDOMAIN}\${Env:USERNAME}`"";
 	}
+	If ($PSBoundParameters.ContainsKey('ExactImageName')) {
+		$TASKLIST_FILTERS += " /FI `"IMAGENAME eq ${SnipeTarget}`"";
+	}
 
 	# TaskList | Select-Object -Unique | Sort-Object
 	(CMD /C "TASKLIST${TASKLIST_FILTERS}") | Select-Object -Unique | ForEach-Object {
-		$_.Split(" ")[0];
-		
-		# TaskKill /FI "USERNAME eq ${Env:USERDOMAIN}\${Env:USERNAME}" /FI "IMAGENAME eq %IMAGENAME_TO_KILL%"
-
+		# $_.Split(" ")[0];
+		$Haystack = $_; # "Haystack", aka the string to parse (may have newlines aplenty)
+		$RegexPattern = '^(\S+)\s+(\S+)\s+(\S+)\s+(\S+\ [a-zA-Z])\s*$'; # Regex pattern which defines the "Needle" to match while parsing the through the "Haystack"
+		$Needle = [Regex]::Match($Haystack, $RegexPattern); # Parse through the "Haystack", looking for the "Needle"
+		Write-Host (("`$Needle.Success = ")+($Needle.Success));
+		If ($Needle.Success -ne $False) {
+			$Needle.Groups[0].Value; 
+			$Needle.Groups[1].Value; 
+			$Needle.Groups[2].Value; 
+			$Needle.Groups[3].Value; 
+		}
+		Write-Host "------------------------------------------------------------`n`n";
 	}
 
 
 
+	# CMD /C "TASKKILL /FI `"USERNAME eq ${Env:USERDOMAIN}\${Env:USERNAME}`" /FI `"IMAGENAME eq ${IMAGENAME_TO_KILL}`""
 
 
 

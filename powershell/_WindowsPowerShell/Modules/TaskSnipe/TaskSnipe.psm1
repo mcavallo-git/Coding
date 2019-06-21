199074425a1db92f5609eda87cfea3b21ade5718
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-21 14:14:20 -0400

    Update TaskSnipe.psm1

--- powershell/_WindowsPowerShell/Modules/TaskSnipe/TaskSnipe.psm1
+++ powershell/_WindowsPowerShell/Modules/TaskSnipe/TaskSnipe.psm1
@@ -34,19 +34,19 @@ function TaskSnipe {
 		$Haystack = $_; # "Haystack", aka the string to parse (may have newlines aplenty)
 		$RegexPattern = '^((?:[a-zA-Z\.]\ ?)+)(?<!\ )[\ \t]+([0-9]+)[\ \t]+([a-zA-Z]+)[\ \t]+([0-9]+)[\ \t]+([0-9\,]+\ [a-zA-Z])[\ \t]*$'; # Regex pattern which defines the "Needle" to match while parsing the through the "Haystack"
 		$Needle = [Regex]::Match($Haystack, $RegexPattern); # Parse through the "Haystack", looking for the "Needle"
-		Write-Host "`$Needle = [Regex]::Match(`"${Haystack}`", `"${RegexPattern}`");";
-		Write-Host (("`$Needle.Success = ")+($Needle.Success));
+		# Write-Host "`$Needle = [Regex]::Match(`"${Haystack}`", `"${RegexPattern}`");";
+		# Write-Host (("`$Needle.Success = ")+($Needle.Success));
 		If ($Needle.Success -ne $False) {
-			$Each_ImageName = $Needle.Groups[1].Value;
+			$Each_ImageName = $Needle.Groups[1].Value; $Each_ImageName;
 			If ($Each_ImageName.Contains($Name) -Eq $True) {
 				$Each_ImageName;
 				$Each_PID = $Needle.Groups[2].Value; $Each_PID;
 				$Each_SessionName = $Needle.Groups[3].Value; $Each_SessionName;
 				$Each_SessionNumber = $Needle.Groups[4].Value; $Each_SessionNumber;
 				$Each_MemoryUsage = $Needle.Groups[5].Value; $Each_MemoryUsage;
+				Write-Host "------------------------------------------------------------`n`n";
 			}
 		}
-		Write-Host "------------------------------------------------------------`n`n";
 	}
 
 

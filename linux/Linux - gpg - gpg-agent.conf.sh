102897083cb8ff0163067d9228fbebd36a339748
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-26 12:02:50 -0400

    Update Linux - gpg - gpg-agent.conf.sh

--- linux/Linux - gpg - gpg-agent.conf.sh
+++ linux/Linux - gpg - gpg-agent.conf.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-
-GPG_DIR="${HOME}/.gnupg/gpg-agent.conf";
-
-GPG_AGENT_CONF="${HOME}/.gnupg/gpg-agent.conf";
-
-# How long GPG remembers a cached password
+# GPG remembers a cached password
 mkdir -p "${HOME}/.gnupg"; echo "default-cache-ttl 3600" >> "${HOME}/.gnupg/gpg-agent.conf";

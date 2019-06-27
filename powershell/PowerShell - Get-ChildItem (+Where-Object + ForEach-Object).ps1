9d35997f21f965963dfb5f98b98ec5371e18f138
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-27 12:31:28 -0400

    Update PowerShell - Get-ChildItem (+Where-Object + ForEach-Object).ps1

--- powershell/PowerShell - Get-ChildItem (+Where-Object + ForEach-Object).ps1
+++ powershell/PowerShell - Get-ChildItem (+Where-Object + ForEach-Object).ps1
@@ -2,6 +2,8 @@
 # ------------------------------------------------------------
 #
 #	Get-ChildItem
+#		PowerShell's [ Get-ChildItem ] command is essentially a equivalent to Linux's [ find ] command
+#		(barring case-sensitivty by default & much more - just relating at a high-level)
 #		
 #		Parameters
 #				-Directory		returns items with type "System.IO.FileSystemInfo.DirectoryInfo"   (see "DirectoryInfo Class", below)
@@ -11,12 +13,20 @@
 #				-Include			used to perform general searches, commonly with wildcards
 #
 # ------------------------------------------------------------
-
 #
-# Find all Git-Repo directories within the current user's "${HOME}" directory
-#		|--> Does this by locating all files named "config" whose the parent directory is named ".git", all of which is located within the current user's "${HOME}" directory
+# Example 1 (Git oriented)
+#		Search for any git-repositories located within the ${HOME} directory (same as %USERPROFILE% on cmd)
+#		 |--> Syntax performs a lookup by beginning at the users ${HOME} directory and searching for files named "config" who have an immediate-parent directory named ".git"
 #
 Get-ChildItem -Path "${HOME}" -Filter "config" -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Directory.Name -Eq ".git"} | Foreach-Object { $_.Directory.Parent.FullName; }
+#
+# ------------------------------------------------------------
+#
+#	Example 2 (GnuPG oriented)
+# 	Search the main drive for files named 'gpg.exe'
+#		 |--> Note: On windows devices, "/" resolves to the systemdrive (commonly "C:") with a backslash ("\") appended, by default
+#
+Get-ChildItem -Path "/" -Filter "gpg.exe" -File -Recurse -Force -ErrorAction "SilentlyContinue" | Foreach-Object { $_.FullName; }
 
 
 

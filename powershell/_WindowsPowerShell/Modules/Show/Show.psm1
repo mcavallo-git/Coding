e0abc7a5a5999c1eabdf28868f92a515024a682d
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-28 14:10:38 -0400

    Update Show.psm1

--- powershell/_WindowsPowerShell/Modules/Show/Show.psm1
+++ powershell/_WindowsPowerShell/Modules/Show/Show.psm1
@@ -25,11 +25,11 @@ Function Show() {
 		Write-Output "${Dashes}`n	`$$(${VarName})	: ";
 		Write-Output "";
 		If ($PSBoundParameters.ContainsKey('Enumerate') -Eq $False) {
-			# Write-Output -NoEnumerate $VarValue | Get-Member;
-			Get-Variable -Name ($VarName) | Write-Output -NoEnumerate | Get-Member;
+			Write-Output -NoEnumerate $VarValue | Get-Member;
+			# Get-Variable -Name ($VarName) | Write-Output -NoEnumerate | Get-Member;
 		} Else {
-			# Write-Output $VarValue | Get-Member;
-			Get-Variable -Name ($VarName) | Write-Output | Get-Member;
+			Write-Output $VarValue | Get-Member;
+			# Get-Variable -Name ($VarName) | Write-Output | Get-Member;
 		}
 		Write-Output "${Dashes}";
 	}

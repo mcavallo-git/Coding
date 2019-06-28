9591db02ad75da453a3ed39486ac46be4d37fec3
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-28 16:28:18 -0400

    Update Show.psm1

--- powershell/_WindowsPowerShell/Modules/Show/Show.psm1
+++ powershell/_WindowsPowerShell/Modules/Show/Show.psm1
@@ -28,9 +28,9 @@ Function Show() {
 		$EachArg | Format-List;
 		If ($ShowStructure -Eq $True) {
 			Write-Output "`n`n--> Properties:`n";
-			Get-Member -View ("All") -InputObject ($EachArg) | Where-Object { $_.MemberType -eq "Property"};
+			Get-Member -View ("All") -InputObject ($EachArg) | Where-Object { $_.MemberType -eq "Property"} | Format-List;
 			Write-Output "`n`n--> Methods:`n";
-			Get-Member -View ("All") -InputObject ($EachArg) | Where-Object { $_.MemberType -ne "Property"};
+			Get-Member -View ("All") -InputObject ($EachArg) | Where-Object { $_.MemberType -ne "Property"} | Format-List;
 		}
 		Write-Output "`n------------------------------------------------------------";
 	}

30563ce7840b13909ed5b58adbe59e8b4a81d5f3
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-28 13:12:38 -0400

    Update PrivilegeEscalation.psm1

--- powershell/_WindowsPowerShell/Modules/PrivilegeEscalation/PrivilegeEscalation.psm1
+++ powershell/_WindowsPowerShell/Modules/PrivilegeEscalation/PrivilegeEscalation.psm1
@@ -1,38 +1,9 @@
-
-# Determine if Runtime-User is an Administrator
-#		--> Note: The SID (Security Identifier) value "S-1-5-32-544" refers to the "Administrator" user, and is static across Windows installs
-#		--> Note: This is what happens a user right-clicks & selects "Run as Administrator" on a given executable in Windows
-Function RunningAsAdministrator {
-	Param(
-	)
-	$ReturnedVal = $Null;
-	$AdminSID = "S-1-5-32-544";
-	$AdminRole = ([Security.Principal.WindowsBuiltInRole]"Administrator");
-	$RuntimeUser = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent());
-	$RuntimeSessionOwner = (([System.Security.Principal.WindowsIdentity]::GetCurrent()).Owner);
-	If (($RuntimeUser.IsInRole($AdminRole)) -Or ($RuntimeSessionOwner -eq $AdminSID)) {
-		$ReturnedVal = $True;
-	} Else {
-		$ReturnedVal = $False;
-	}
-	Return $ReturnedVal;
-}
-
-# Determine if the Runtime-User is part of the "Administrators" Local UserGroup
-Function UserCanEscalatePrivileges {
-	Param(
-	)
-	$ReturnedVal = $Null;
-	$RuntimeUserName = (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).Identities.Name);
-	If (((Get-LocalGroupMember -Group "Administrators").Name).Contains($RuntimeUserName)) {
-		$ReturnedVal = $True;
-	} Else {
-		$ReturnedVal = $False;
-	}
-	Return $ReturnedVal;
-}
-
-# If needed, Relaunch the script with escalated (administrator) privileges
+#
+#	PrivilegeEscalation
+# 	|--> Checks if running w/ administrator privileges - If not, relaunch the script with escalated (administrator) privileges
+#		|--> Uses sub-module "RunningAsAdministrator"
+#		|--> Uses sub-module "UserCanEscalatePrivileges"
+#
 Function PrivilegeEscalation {
 	Param (
 
@@ -65,14 +36,45 @@ Function PrivilegeEscalation {
 		}
 	}
 }
-
 Export-ModuleMember -Function "PrivilegeEscalation";
+# Install-Module -Name "PrivilegeEscalation"
+
+# Determine if Runtime-User is an Administrator
+#		--> Note: The SID (Security Identifier) value "S-1-5-32-544" refers to the "Administrator" user, and is static across Windows installs
+#		--> Note: This is what happens a user right-clicks & selects "Run as Administrator" on a given executable in Windows
+Function RunningAsAdministrator {
+	Param(
+	)
+	$ReturnedVal = $Null;
+	$AdminSID = "S-1-5-32-544";
+	$AdminRole = ([Security.Principal.WindowsBuiltInRole]"Administrator");
+	$RuntimeUser = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent());
+	$RuntimeSessionOwner = (([System.Security.Principal.WindowsIdentity]::GetCurrent()).Owner);
+	If (($RuntimeUser.IsInRole($AdminRole)) -Or ($RuntimeSessionOwner -eq $AdminSID)) {
+		$ReturnedVal = $True;
+	} Else {
+		$ReturnedVal = $False;
+	}
+	Return $ReturnedVal;
+}
 Export-ModuleMember -Function "RunningAsAdministrator";
+# Install-Module -Name "RunningAsAdministrator"
+
+# Determine if the Runtime-User is part of the "Administrators" Local UserGroup
+Function UserCanEscalatePrivileges {
+	Param(
+	)
+	$ReturnedVal = $Null;
+	$RuntimeUserName = (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).Identities.Name);
+	If (((Get-LocalGroupMember -Group "Administrators").Name).Contains($RuntimeUserName)) {
+		$ReturnedVal = $True;
+	} Else {
+		$ReturnedVal = $False;
+	}
+	Return $ReturnedVal;
+}
 Export-ModuleMember -Function "UserCanEscalatePrivileges";
-
-
-
-# Install-Module -Name "CredentialManager"
+# Install-Module -Name "UserCanEscalatePrivileges"
 
 #
 #	Citation(s)

d00dd864a3448afde4fe55fe8d6fdd7853906684
Matt Cavallo <mcavallo.work@gmail.com>
2019-06-28 15:45:50 -0400

    Update Show.psm1

--- powershell/_WindowsPowerShell/Modules/Show/Show.psm1
+++ powershell/_WindowsPowerShell/Modules/Show/Show.psm1
@@ -20,7 +20,8 @@ Function Show() {
 
 		[Hashtable]$BoundParam_Hashtable=@{},
 
-		[Switch]$Enumerate
+		[Switch]$Enumerate,
+		[Switch]$Splat
 		
 	)
 
@@ -32,27 +33,40 @@ Function Show() {
 	# $VarsToShow["args[0]"] = ($args[0]);
 	# $VarsToShow["args[1]"] = ($args[1]);
 
-	Write-Host @args;
-	Return;
+	# $VarsToShow = @{};
+	# $VarsToShow["MyInvocation.MyCommand"] = ($MyInvocation.MyCommand);
+	# $VarsToShow["PSScriptRoot"] = ($PSScriptRoot);
+	# $VarsToShow["PsBoundParameters.Values"] = ($PsBoundParameters.Values);
+	# $VarsToShow["args"] = ($args);
+	# $VarsToShow["args[0]"] = ($args[0]);
+	# $VarsToShow["args[1]"] = ($args[1]);
+	If ($PSBoundParameters.ContainsKey('Splat')) {
 
-	ForEach ($EachTopLevelVar in (@($args,$MyInvocation))) {
-		ForEach ($EachKey in ($EachTopLevelVar)) {
-			# $EachVarValue = $VarsToShow[$EachKey];
-			$EachVarValue = $EachKey;
-			Write-Output "============================================================";
-			Write-Output "`n`n--> Variable Name:`n";
-			Write-Output "`$$($EachVarValue.Name)";
-			# Write-Output "`$$(${EachKey})";
-			Write-Output "`n`n--> Value (List):`n";
-			$EachVarValue | Format-List;
-			Write-Output "`n`n--> Methods:`n";
-			# If ($PSBoundParameters.ContainsKey('Enumerate') -Eq $False) {
-				# Write-Output -NoEnumerate $EachVarValue | Get-Member -View ("All");
-			# } Else {
-				Get-Member -View ("All") -InputObject ($EachVarValue) ;
-				# Write-Output $EachVarValue | Get-Member -View ("All");
-			# }
-			Write-Output "`n------------------------------------------------------------";
+	} Else {
+
+		Show @args;
+
+		Return;
+
+		ForEach ($EachTopLevelVar in (@($args,$MyInvocation))) {
+			ForEach ($EachKey in ($EachTopLevelVar)) {
+				# $EachVarValue = $VarsToShow[$EachKey];
+				$EachVarValue = $EachKey;
+				Write-Output "============================================================";
+				Write-Output "`n`n--> Variable Name:`n";
+				Write-Output "`$$($EachVarValue.Name)";
+				# Write-Output "`$$(${EachKey})";
+				Write-Output "`n`n--> Value (List):`n";
+				$EachVarValue | Format-List;
+				Write-Output "`n`n--> Methods:`n";
+				# If ($PSBoundParameters.ContainsKey('Enumerate') -Eq $False) {
+					# Write-Output -NoEnumerate $EachVarValue | Get-Member -View ("All");
+				# } Else {
+					Get-Member -View ("All") -InputObject ($EachVarValue) ;
+					# Write-Output $EachVarValue | Get-Member -View ("All");
+				# }
+				Write-Output "`n------------------------------------------------------------";
+			}
 		}
 	}
 

2189808016aec3152b1d6717f1def342fe74f38b
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-01 11:46:24 -0400

    Create Linux - shopt (set bash environment variables).sh

---
+++ linux/Linux - shopt (set bash environment variables).sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Linux - shopt
+#			nocasematch     Bash matches patterns in a case-insensitive fashion when performing matching while executing case or [[ conditional commands.
+#
+
+shopt -s nocasematch;
+
+
+
+#
+# Citation(s)
+# 	
+# 	gnu.org, "4.3.2 The Shopt Builtin", https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
+#
+# 	ss64.com, "shopt Man Page - Linux - SS64.com", https://ss64.com/bash/shopt.html
+#

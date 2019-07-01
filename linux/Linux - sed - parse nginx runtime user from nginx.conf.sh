eb141e89c89a7a2f74cb56e147cd433b0a275ad1
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-01 10:30:46 -0400

    Update Linux - sed - parse nginx runtime user from nginx.conf.sh

--- linux/Linux - sed - parse nginx runtime user from nginx.conf.sh
+++ linux/Linux - sed - parse nginx runtime user from nginx.conf.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-
-# Linux - sed - parse nginx runtime user's name from nginx.conf
+#
+# ------------------------------------------------------------
+# 
+# Linux - sed
+# 	--> parse nginx runtime user's name from nginx.conf
+#
 if [ -f "/etc/nginx/nginx.conf" ]; then
 	NGINX_UNAME=$(sed --regexp-extended --quiet --expression='s/^user ([a-z_][a-z0-9_\-]{0,30}[a-z0-9_\-\$]?)\s*;\s*$/\1/p' "/etc/nginx/nginx.conf");
 	NGINX_GNAME=$(id -gn "${NGINX_UNAME}");
 	NGINX_UID=$(id -u "${NGINX_UNAME}");
 	NGINX_GID=$(id -g "${NGINX_UNAME}");
 fi;
+#
+#
+# ------------------------------------------------------------
+# 
+# Linux - sed
+# 	--> parse GnuPG key_id's out of gpg's 'LONG' formated-values
+#
+GnuPG_KeyIDs=$(gpg --list-secret-keys --keyid-format 'LONG' | sed --regexp-extended --quiet --expression='s/^sec\ +([A-Za-z0-9]+)\/([A-F0-9]{16})\ +([0-9\-]{1,10})\ +(.+)$/\2/p');
+echo "GnuPG_KeyIDs=\"${GnuPG_KeyIDs}\"";
+#
+# ------------------------------------------------------------
+
+

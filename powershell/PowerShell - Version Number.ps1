4b6c9f760dde57ae23a6f3cc671e2d3133458fab
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-01 15:50:37 -0400

    Update PowerShell - Version Number.ps1

--- powershell/PowerShell - Version Number.ps1
+++ powershell/PowerShell - Version Number.ps1
@@ -4,8 +4,8 @@
 #
 
 # As a Variable
-$PowerShellVersion=$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Major)/10));
+$PowerShellVersion=$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Minor)/10));
 Write-Output "PowerShell v$PowerShellVersion";
 
 # Show in one-line:
-Write-Output "PowerShell v$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Major)/10))";
+Write-Output "PowerShell v$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Minor)/10))";

8bfa866c7290da715e510ea3a27e54698cc444fe
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-01 10:30:52 -0400

    Delete Linux - sed - parse gpg key_id.sh

--- linux/Linux - sed - parse gpg key_id.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Linux - sed - parse GnuPG key_id's string out of gpg's returned LONG format-values
-
-GnuPG_KeyID=$(gpg --list-secret-keys --keyid-format 'LONG' | sed --regexp-extended --quiet --expression='s/^sec\ +([A-Za-z0-9]+)\/([A-F0-9]{16})\ +([0-9\-]{1,10})\ +(.+)$/\2/p');
-echo "GnuPG_KeyID=\"${GnuPG_KeyID}\"";

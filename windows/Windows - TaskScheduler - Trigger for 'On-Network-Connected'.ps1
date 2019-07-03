45b0c61296ca8d0190ac9d61a85576db91fe8a8c
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-03 13:34:09 -0400

    Update Windows - TaskScheduler - Trigger for 'On-Network-Connected'.ps1

--- windows/Windows - TaskScheduler - Trigger for 'On-Network-Connected'.ps1
+++ windows/Windows - TaskScheduler - Trigger for 'On-Network-Connected'.ps1
@@ -3,12 +3,19 @@
 # Windows Task Scheduler
 #  |--> Trigger any time a local network connection reports the "Connected" (10000) status
 
+
+
 # ------------------------------------------------------------
+# TRIGGER LOGIC:
 #
-#		Scheduled Task -> New Trigger
+#			Begin the task:  On an Event
 #
-#			     Log:  Microsoft-Windows-NetworkProfile/Operational
-#			  Source:  NetworkProfile
-#			Event ID:  10000
+#			           Log:  Microsoft-Windows-NetworkProfile/Operational
+#			        Source:  NetworkProfile
+#			      Event ID:  10000
+#
+#			Delay task for:  20 Seconds
 #
 # ------------------------------------------------------------
+
+

6a4b78dfcef25fe70ddff5d6f89e07ca5d59bda5
Matt Cavallo <mcavallo.git@gmail.com>
2019-07-04 10:05:11 -0400

    Update UniFi - config.gateway.json.sh

--- unifi/config.gateway.json/UniFi - config.gateway.json.sh
+++ unifi/config.gateway.json/UniFi - config.gateway.json.sh
@@ -5,9 +5,15 @@
 # Copy/Download current config (in JSON format) from your USG
 #
 
-# - SSH into USG-3P device & run the following command:
-mca-ctrl -t dump-cfg > "${HOME}/$(hostname).$(date +'%Y%m%d_%H%M%S').config.gateway.json";
+# Step 1 - SSH into your Unifi device (USG, for example)
 
+# Step 2 - Export the config to JSON format via the following commands
+EXPORT_FILEPATH="$(getent passwd ${SUDO_USER:-${USER}} | cut -d : -f 6)/$(hostname).$(date +'%Y%m%d_%H%M%S').config.gateway.json";
+sudo mca-ctrl -t dump-cfg > "${EXPORT_FILEPATH}";
+echo "Export Filepath:   ${EXPORT_FILEPATH}";
+
+
+# Step 3 - Download the exported JSON file via your SFTP tool of choice (file placed in user's home-directory)
 
 
 # ------------------------------------------------------------
@@ -18,7 +24,7 @@ mca-ctrl -t dump-cfg > "${HOME}/$(hostname).$(date +'%Y%m%d_%H%M%S').config.gate
 # - SSH into linux machine hosting the unifi controller
 
 # - Create and/or make-edits-to your unifi site's "config.gateway.json" file, located at:
-UNIFI_HOMEDIR=$(getent passwd unifi | cut --delimiter=: --fields=6);
+UNIFI_HOMEDIR=$(getent passwd unifi | cut -d : -f 6);
 vi "${UNIFI_HOMEDIR:-/usr/lib/unifi}/data/sites/default/config.gateway.json";
 
 # - Force a Re-provision on the USG via your method-of-choice
@@ -26,6 +32,7 @@ vi "${UNIFI_HOMEDIR:-/usr/lib/unifi}/data/sites/default/config.gateway.json";
 #    |--> you may prefer the cli method directly on the usg:  (NEED METHOD, HERE)
 
 
+mca-ctrl -t dump-cfg > "${HOME}/$(hostname).$(date +'%Y%m%d_%H%M%S').config.gateway.json";
 
 # ------------------------------------------------------------
 #

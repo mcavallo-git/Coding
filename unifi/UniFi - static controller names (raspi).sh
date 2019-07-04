63ef33a810680a916914495fdd4729b3e6bdc62b
Matt Cavallo <mcavallo.git@gmail.com>
2019-07-04 07:44:47 -0400

    Update UniFi - static controller names (raspi).sh

--- unifi/UniFi - static controller names (raspi).sh
+++ unifi/UniFi - static controller names (raspi).sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-configure set system static-host-mapping host-name HOSTNAME inet IPADDRESS commit save
+configure
 
-configure edit system static-host-mapping set host-name HOSTNAME inet IPADDRESS commit save
+set system static-host-mapping host-name HOSTNAME inet IPADDRESS
+
+edit system static-host-mapping set host-name HOSTNAME inet IPADDRESS
+
+del system static-host-mapping host-name HOSTNAME
+
+commit
+save

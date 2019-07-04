f986ee3f07710af5a6b3e37247bbfb10d97f739a
Matt Cavallo <mcavallo.git@gmail.com>
2019-07-04 09:17:21 -0400

    un

--- unifi/config.gateway.json/UniFi - config.gateway.json.sh
+++ unifi/config.gateway.json/UniFi - config.gateway.json.sh
@@ -1,25 +1,36 @@
+#!/bin/bash
 
 # ------------------------------------------------------------
 #
-# Show current USG-3P config in JSON format:
+# Copy/Download current config (in JSON format) from your USG
+#
 
 # - SSH into USG-3P device & run the following command:
-mca-ctrl -t dump-cfg
+mca-ctrl -t dump-cfg > "${HOME}/$(hostname).$(date +'%Y%m%d_%H%M%S').config.gateway.json";
 
 
 
 # ------------------------------------------------------------
 #
-# Apply aftermarket config(s) not availble on Unifi dashboard:
-
+# Apply aftermarket config(s) not availble on Unifi dashboard
+#
 
 # - SSH into linux machine hosting the unifi controller
 
 # - Create and/or make-edits-to your unifi site's "config.gateway.json" file, located at:
+UNIFI_HOMEDIR=$(getent passwd unifi | cut --delimiter=: --fields=6);
+
 vi "/usr/lib/unifi/data/sites/default/config.gateway.json";
+
 
 # - Re-provision the USG-3p on the unifi dashboard under "Devices" -> USG-3P -> "Manage" (cog) -> "Provision"
 
 
 
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+# 	help.ubnt.com  |  "UniFi - USG Advanced Configuration"  |  https://help.ubnt.com/hc/en-us/articles/215458888
+#
 # ------------------------------------------------------------

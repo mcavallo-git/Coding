80a1dc8c367fc76530964b2a867602b6aca5e84c
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-10 11:39:43 -0400

    Update Linux - arrays.sh

--- linux/Linux - arrays.sh
+++ linux/Linux - arrays.sh
@@ -1,12 +1,16 @@
-
+#!/bin/bash
+#
 # Linux - Arrays
-
-#  -------------------------------------------------------------
 #
-# Create an array
+# -------------------------------------------------------------
+#
+# Create & walk-through an array
 #
 
-DAT_ARRAY=("Item-One" "Item-Two" "Item-A" "Item-B");
+DAT_ARRAY=("Item-One" "Item-One" "Item-Two" "Item-A" "Item-A" "Item-B");
+for DAT_ITEM in "${DAT_ARRAY[@]}"; do
+echo "${DAT_ITEM}";
+done;
 
 # #  ^
 # #  Methods have equivalent output
@@ -14,9 +18,15 @@ DAT_ARRAY=("Item-One" "Item-Two" "Item-A" "Item-B");
 
 DAT_ARRAY=();
 DAT_ARRAY+=("Item-One");
+DAT_ARRAY+=("Item-One");
 DAT_ARRAY+=("Item-Two");
 DAT_ARRAY+=("Item-A");
+DAT_ARRAY+=("Item-A");
 DAT_ARRAY+=("Item-B");
+for DAT_ITEM in "${DAT_ARRAY[@]}"; do
+echo "${DAT_ITEM}";
+done;
+
 
 # #  ^
 # #  Methods have equivalent output
@@ -24,26 +34,19 @@ DAT_ARRAY+=("Item-B");
 
 DAT_ARRAY=( \
 "Item-One" \
+"Item-One" \
 "Item-Two" \
+"Item-A" \
 "Item-A" \
 "Item-B" \
 );
-
-
-
-#  -------------------------------------------------------------
-#
-#  Walk through an array
-#
-
-DAT_ARRAY=("Item-One" "Item-Two" "Item-A" "Item-B");
 for DAT_ITEM in "${DAT_ARRAY[@]}"; do
-	echo "${DAT_ITEM}";
+echo "${DAT_ITEM}";
 done;
 
 
 
-#  -------------------------------------------------------------
+# -------------------------------------------------------------
 #
 #	Sort an array, remove duplicates from it, then walk through it
 #
@@ -52,15 +55,15 @@ done;
 #			the [ tr ] (translate) method to sort items as-intended
 #
 
-DAT_ARRAY=("Item-One" "Item-Two" "Item-A" "Item-B");
+DAT_ARRAY=("Item-One" "Item-One" "Item-Two" "Item-A" "Item-A" "Item-B");
 DAT_ARRAY_SORTED_NO_DUPES=($(echo "${DAT_ARRAY[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '));
 for DAT_ITEM in "${DAT_ARRAY_SORTED_NO_DUPES[@]}"; do
-	echo "${DAT_ITEM}";
+echo "${DAT_ITEM}";
 done;
 
 
 
-#  -------------------------------------------------------------
+# -------------------------------------------------------------
 #
 #	Reference Arrays as variables whose variable-names are built from other arrays' string-values.
 #	Then, combine all arrays and find unique items amongst their net result;
@@ -105,4 +108,10 @@ echo "\$ALL_ITEMS_SORTED_UNIQUE=(${ALL_ITEMS_SORTED_UNIQUE[@]})";
 
 echo -e "\n\n";
 
-#  -------------------------------------------------------------
+# -------------------------------------------------------------
+#
+# Citation(s)
+#
+# 	stackoverflow.com  |  "Loop through an array of strings in Bash?"  |  https://stackoverflow.com/a/8880633
+#
+# ------------------------------------------------------------

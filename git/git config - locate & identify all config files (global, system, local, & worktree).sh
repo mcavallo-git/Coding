ed0a845123b088b244244e1299579c349c9220e5
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-10 11:52:58 -0400

    Update git config - locate & identify all config files (global, system, local, & worktree).sh

--- git/git config - locate & identify all config files (global, system, local, & worktree).sh
+++ git/git config - locate & identify all config files (global, system, local, & worktree).sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+
+# [ GLOBAL ] git config filepath
+GIT_CONFIGFILE_GLOBAL="";
+GET_PATH=$(git config --global --list --show-origin | head -n 1 | sed --regexp-extended --quiet --expression='s/^file\:(.+)\s+(.+)=(.+)$/\1/p');
+GET_PATH="${GET_PATH/C:/\/c}"; GET_PATH="${GET_PATH//\\/\/}"; GET_PATH="$(realpath ${GET_PATH})"; GIT_CONFIGFILE_GLOBAL="${GET_PATH}";
+
+# [ SYSTEM ] git config filepath
+GIT_CONFIGFILE_SYSTEM="";
+GET_PATH=$(git config --local --list --show-origin | head -n 1 | sed --regexp-extended --quiet --expression='s/^file\:(.+)\s+(.+)=(.+)$/\1/p');
+GET_PATH="${GET_PATH/C:/\/c}"; GET_PATH="${GET_PATH//\\/\/}"; GET_PATH="$(realpath ${GET_PATH})"; GIT_CONFIGFILE_SYSTEM="${GET_PATH}";
+
+# [ LOCAL ] git config filepath
+GIT_CONFIGFILE_LOCAL="";
+GET_PATH=$(git config --local --list --show-origin | head -n 1 | sed --regexp-extended --quiet --expression='s/^file\:(.+)\s+(.+)=(.+)$/\1/p');
+GET_PATH="${GET_PATH/C:/\/c}"; GET_PATH="${GET_PATH//\\/\/}"; GET_PATH="$(realpath ${GET_PATH})"; GIT_CONFIGFILE_LOCAL="${GET_PATH}";
+
+# [ WORKTREE ] git config filepath
+GIT_CONFIGFILE_WORKTREE="";
+GET_PATH=$(git config --worktree --list --show-origin | head -n 1 | sed --regexp-extended --quiet --expression='s/^file\:(.+)\s+(.+)=(.+)$/\1/p');
+GET_PATH="${GET_PATH/C:/\/c}"; GET_PATH="${GET_PATH//\\/\/}"; GET_PATH="$(realpath ${GET_PATH})"; GIT_CONFIGFILE_WORKTREE="${GET_PATH}";
+
+
+
+# ------------------------------------------------------------
+#
+# IF MULTIPLE CONFIG FILES PER-SCOPE
+#
+
 # global
 GIT_GLOBAL_CONFIG=$(git config --global --list --show-origin | sed --regexp-extended --quiet --expression='s/^file\:(.+)\s+(.+)=(.+)$/\1/p'); \
 GIT_GLOBAL_CONFIG_NO_DUPES=($(echo "${GIT_GLOBAL_CONFIG[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')); \

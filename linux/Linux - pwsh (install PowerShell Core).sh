0ff13dac44c38b354a37c46a6a2faf3feda1f164
Matt Cavallo <mcavallo.git@gmail.com>
2019-07-10 21:04:56 -0400

    Update Linux - pwsh (install PowerShell Core).sh

--- linux/Linux - pwsh (install PowerShell Core).sh
+++ linux/Linux - pwsh (install PowerShell Core).sh
@@ -1,51 +1,6 @@
 #!/bin/bash
-
-OS_VERSION_MAJOR=$(cat /etc/*release | grep -i VERSION_ID | sed --regexp-extended --quiet --expression='s/^VERSION_ID="([0-9]+)\.([0-9]+)"$/\1/p');
-OS_VERSION_MINOR=$(cat /etc/*release | grep -i VERSION_ID | sed --regexp-extended --quiet --expression='s/^VERSION_ID="([0-9]+)\.([0-9]+)"$/\2/p');
-
-if [[ ${OS_VERSION_MAJOR} -ge 18 ]]; then
-# UBUNTU 18+
-wget -q "https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb"; # Download the Microsoft repository GPG keys
-sudo dpkg -i packages-microsoft-prod.deb; # Register the Microsoft repository GPG keys
-sudo apt-get update -y; # Update the list of products
-sudo add-apt-repository "universe"; # Enable the "universe" repositories
-sudo apt-get install -y powershell; # Install PowerShell
-
-elif [[ ${OS_VERSION_MAJOR} -lt 18 ]] && [[ ${OS_VERSION_MAJOR} -ge 16 ]]; then
-# UBUNTU 16+
-wget -q "https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb"; # Download the Microsoft repository GPG keys
-sudo dpkg -i packages-microsoft-prod.deb; # Register the Microsoft repository GPG keys
-sudo apt-get update -y; # Update the list of products
-sudo apt-get install -y "powershell"; # Install PowerShell
-sudo ln -sf $(readlink -f $(which pwsh)) "/usr/bin/powershell"; # Redirect "powershell" to use "pwsh" command
-
-elif [[ ${OS_VERSION_MAJOR} -lt 16 ]] && [[ ${OS_VERSION_MAJOR} -ge 14 ]]; then
-# UBUNTU 14+
-wget -q "https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb"; # Download the Microsoft repository GPG keys
-sudo dpkg -i packages-microsoft-prod.deb # Register the Microsoft repository GPG keys
-sudo apt-get update -y; # Update the list of products
-sudo apt-get install -y "powershell"; # Install PowerShell
-
-else
-# UNKNOWN/UNHANDLED
-echo "";
-echo "Unhandled version of Linux";
-echo "";
-echo "Please see Microsoft's tutorial page for installing PowerShell Core @:";
-echo " |    ";
-echo " |--> https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux";
-
-fi;
-
-# Start PowerShell
-# pwsh;
 #
-# -------------------------------------------------------------
+# Install PowerShell Core - Relocated to:
 #
-# Citation(s)
+#		https://github.com/search?l=&q=pwsh+user%3Amcavallo-git+repo%3Acloud-infrastructure+filename%3Apwsh&type=Code
 #
-#		docs.microsoft.com
-#			"Installing PowerShell Core on Linux"
-#			 https://docs.microsoft.com/en-us/powershell/scripting/install/installing-powershell-core-on-linux
-#
-# ------------------------------------------------------------

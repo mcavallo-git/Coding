72a446677a0531852e787fd3715d3128497ea5e4
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-12 18:15:11 -0400

    Update MySQL - STR_TO_DATE (Convert strings to dates, times, datetimes, timestamps, etc.).sql

--- mysql/MySQL - STR_TO_DATE (Convert strings to dates, times, datetimes, timestamps, etc.).sql
+++ mysql/MySQL - STR_TO_DATE (Convert strings to dates, times, datetimes, timestamps, etc.).sql
@@ -1,16 +1,21 @@
 
+/* MySQL - Converting Strings to DateTimes */
+
+-- If using military-time you can use any of:
+SELECT
+	STR_TO_DATE('1/2/3 14:25:36', '%c/%e/%Y %H:%i:%S'),
+	STR_TO_DATE('01/02/2003 14:25:36', '%c/%e/%Y %T')
+;
+
+
+
 -- NOTE: If you want to convert strings containing 'AM'/'PM' to DateTimes/etc. in MySQL, you must do one of the following:
 --       NOT use [ %H ] ... [ %p ], but rather use [ %I ]/[ %h ]/[ %l	] ... [ %p ]
 --       NOT use [ %T ] ... [ %p ], but rather use [ %r ] ...[ %p ]
 --       ^-- STR_TO_DATE seems to break when given both [ military 24-hour ] and [ AM/PM ] specifiers, simultaneously
 SELECT
-	STR_TO_DATE('11/22/33 01:02:03 PM', '%c/%e/%Y %h:%i:%S %p'),
-	STR_TO_DATE('11/22/33 01:02:03 PM', '%c/%e/%Y %r')
-;
-
-SELECT
-	STR_TO_DATE('11/22/33 13:02:03 PM', '%c/%e/%Y %h:%i:%S %p'),
-	STR_TO_DATE('11/22/33 13:02:03 PM', '%c/%e/%Y %T')
+	STR_TO_DATE('1/2/3 1:2:3 PM', '%c/%e/%Y %h:%i:%S %p'),
+	STR_TO_DATE('01/02/2003 01:02:03 PM', '%c/%e/%Y %r')
 ;
 
 

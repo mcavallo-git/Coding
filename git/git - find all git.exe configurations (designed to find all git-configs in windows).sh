9980c770f3a91fdf9317d894b4e611fcd0659db5
Matt Cavallo <mcavallo.git@gmail.com>
2019-07-19 01:09:30 -0400

    Update git - find all git.exe configurations (designed to find all git-configs in windows).sh

--- git/git - find all git.exe configurations (designed to find all git-configs in windows).sh
+++ git/git - find all git.exe configurations (designed to find all git-configs in windows).sh
@@ -2,22 +2,26 @@
 
 # ------------------------------------------------------------
 
-DAT_ARRAY=(); \
-DAT_ARRAY+=("${LOCALAPPDATA}/"); \
-DAT_ARRAY+=("${PROGRAMFILES}/"); \
-DAT_ARRAY+=("${PROGRAMFILES} (x86)/"); \
-for DAT_ITEM in "${DAT_ARRAY[@]}"; do \
-SEARCH_IN_DIRECTORY=$(realpath "${DAT_ITEM}"); \
-FIND_GIT_EXE=$(find "${SEARCH_IN_DIRECTORY}" -name 'git.exe' 2>/dev/null); \
-FIND_GIT_EXE_NO_DUPES=($(echo "${FIND_GIT_EXE[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' ')); \
-for EACH_GIT_EXE in "${FIND_GIT_EXE[@]}"; do \
-EACH_REALPATH=$(realpath "${EACH_GIT_EXE}"); \
-echo ""; \
-echo $(ls -al "${EACH_REALPATH}"); \
-echo "------------------------------------------------------------"; \
-"${EACH_REALPATH}" config --list --show-origin; \
-echo "------------------------------------------------------------"; \
-done; \
+DAT_ARRAY=();
+DAT_ARRAY+=("${LOCALAPPDATA}/");
+DAT_ARRAY+=("${PROGRAMFILES}/");
+DAT_ARRAY+=("${PROGRAMFILES} (x86)/");
+for DAT_ITEM in "${DAT_ARRAY[@]}"; do
+SEARCH_IN_DIRECTORY=$(realpath "${DAT_ITEM}");
+FIND_GIT_EXE=$(find "${SEARCH_IN_DIRECTORY}" -name 'git.exe' 2>/dev/null);
+FIND_GIT_EXE_NO_DUPES=($(echo "${FIND_GIT_EXE[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '));
+find "${SEARCH_IN_DIRECTORY}" \
+-name 'git.exe' \
+2>"/dev/null" \
+-print0 \
+| while IFS= read -r -d $'\0' EACH_ITEM; do
+EACH_REALPATH=$(realpath "${EACH_ITEM}");
+echo "";
+echo $(ls -al "${EACH_REALPATH}");
+echo "------------------------------------------------------------";
+"${EACH_REALPATH}" config --list --show-origin;
+echo "------------------------------------------------------------";
+done;
 done;
 
 # ------------------------------------------------------------

3e29847c82467b4591c6a07b493339afe2cf50c9
Matt Cavallo <mcavallo.work@gmail.com>
2019-07-25 12:39:32 -0400

    Update ExclusionsListUpdate.psm1

--- powershell/_WindowsPowerShell/Modules/ExclusionsListUpdate/ExclusionsListUpdate.psm1
+++ powershell/_WindowsPowerShell/Modules/ExclusionsListUpdate/ExclusionsListUpdate.psm1
@@ -637,6 +637,7 @@ function ESET_ExportModifier {
 			If ($ValidInjectionPoint -eq $False) {
 
 				Write-Host "";
+				Write-Host "`n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Red");
 				Write-Host "  Error (ESET Exclusions): Unable to locate a valid injection point for:  `n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Red");
 				Write-Host "   |--> Regex: `"$($EachCfg.RegexStart)`"  `n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Red");
 				Write-Host "   |-->  Path: $($Fullpath_NewImport)  `n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Red");
@@ -646,6 +647,7 @@ function ESET_ExportModifier {
 			} Else {
 
 				Write-Host "";
+				Write-Host "`n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Green");
 				Write-Host "  Success (ESET Exclusions): Located a valid injection point for:  `n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Green");
 				Write-Host "   |--> Regex: `"$($EachCfg.RegexStart)`"  `n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Green");
 				Write-Host "   |-->  Path: $($Fullpath_NewImport)  `n" -NoNewLine -BackgroundColor ("Black") -ForegroundColor ("Green");

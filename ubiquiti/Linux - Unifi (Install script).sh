97a4e881161aad27aee5320c5e9213f4bb3f3016
Matt Cavallo <mcavallo.git@gmail.com>
2019-08-01 08:29:55 -0400

    Update Linux - Unifi (Install script).sh

--- ubiquiti/Linux - Unifi (Install script).sh
+++ ubiquiti/Linux - Unifi (Install script).sh
@@ -24,19 +24,19 @@ apt-get update;
 #  |        W: GPG error: http://ftp.debian.org jessie-backports InRelease: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 8B48AD6246925553 NO_PUBKEY 7638D0442B90D010
 #  |        ...
 #  |     }
-#  |--> HOTFIX 1:
+#  |--> HOTFIX 1 - Unifi Fingerprint Issue:
 HOTFIX_FINGERPRINT="8B48AD6246925553"; gpg --keyserver pgpkeys.mit.edu --recv-key ${HOTFIX_FINGERPRINT}; gpg -a --export ${HOTFIX_FINGERPRINT} | sudo apt-key add -;
-#  |--> HOTFIX 2:
+#  |--> HOTFIX 2 - Unifi Fingerprint Issue:
 HOTFIX_FINGERPRINT="7638D0442B90D010"; gpg --keyserver pgpkeys.mit.edu --recv-key ${HOTFIX_FINGERPRINT}; gpg -a --export ${HOTFIX_FINGERPRINT} | sudo apt-key add -;
-#  |--> HOTFIX 3 (2019-08-01_07-53-58):
+#  |--> HOTFIX 3 (2019-08-01_07-53-58) - Unifi Fingerprint Issue:
 apt-get install -y debian-keyring;
 apt-get install -y debian-archive-keyring;
 HOTFIX_FINGERPRINT="06E85760C0A52C50"; gpg --keyserver "keyserver.ubuntu.com" --recv-key ${HOTFIX_FINGERPRINT}; gpg -a --export ${HOTFIX_FINGERPRINT} | sudo apt-key add -;
 sudo apt-key adv --keyserver "keyserver.ubuntu.com" --recv "0C49F3730359A14518585931BC711F9BA15703C6";
 echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee "/etc/apt/sources.list.d/mongodb-org-3.4.list";
 apt-get update -y;
-
-# gpg --delete-key 06E85760C0A52C50
+#  |--> HOTFIX 4 (2019-08-01_08-27-52) - Note: Not a Unifi Fingeprint, but rather the "Raspberry Pi Archive Signing Key" - errors were still shown when trying to update Unifi
+HOTFIX_FINGERPRINT="82B129927FA3303E"; gpg --keyserver "hkps://hkps.pool.sks-keyservers.net:443" --recv-key ${HOTFIX_FINGERPRINT}; gpg -a --export ${HOTFIX_FINGERPRINT} | sudo apt-key add -;
 
 # REPEAT
 apt-get -y update;

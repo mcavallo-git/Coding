39d7b5956000ec3b63182b556d54e25e99d14455
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-02 12:50:00 -0400

    Update Windows - LogOff-Workstation.bat

--- windows/Windows - LogOff-Workstation.bat
+++ windows/Windows - LogOff-Workstation.bat
@@ -1,5 +1,16 @@
-REM @ECHO OFF
+@ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM
+REM		Created by Matt Cavallo <mcavallo@boneal.com>
+REM		Creation Date: [ 2016-09-29 ]
+REM		Updated:	[ 2018-11-28 ]
+REM		Updated:	[ 2018-12-12 ] Generalized IMAGENAME_TO_KILL as parameter #1
+REM		Updated:	[ 2019-08-02 ] Adding logging to disk
+REM		Updated:	[ 2019-08-02 ] Replaced 'SHUTDOWN /L /F' with 'logoff.exe' implementation
+REM
+
+SET LOGFILE=%TEMP%\logoff.log
 
 SET IMAGENAME_TO_KILL=
 IF NOT "%1"=="" (
@@ -12,7 +23,9 @@ SET RUNTIME_USERDOMAIN=%USERDOMAIN%
 SET TARGET_USERNAME=%RUNTIME_USERNAME%
 SET TARGET_USERDOMAIN=%RUNTIME_USERDOMAIN%
 
-REM	 Find Session-ID tied to [target-user]
+REM
+REM	Find Session-ID tied to [target-user]
+REM
 SET USER_SESSION_ID=NOTFOUND
 FOR /F "tokens=3-4" %%a IN ('QUERY SESSION %TARGET_USERNAME%') DO (
 	@IF "%%b"=="Active" (
@@ -20,49 +33,54 @@ FOR /F "tokens=3-4" %%a IN ('QUERY SESSION %TARGET_USERNAME%') DO (
 	)
 )
 
-REM	 Determine if [target-user] is logged-in or not
+REM
+REM	Determine if [target-user] is logged-in or not
+REM
 IF NOT %USER_SESSION_ID%==NOTFOUND (
 
-	REM	 Safely end processes for sessions started by runtime-user
+	REM
+	REM	Safely end processes for sessions started by runtime-user
+	REM
 	IF NOT "%IMAGENAME_TO_KILL%"=="" (
-		TASKKILL /FI "USERNAME eq %TARGET_USERDOMAIN%\%TARGET_USERNAME%" /FI "IMAGENAME eq %IMAGENAME_TO_KILL%"
-		REM Show the screen (to see the action just performed) for just a second before proceeding (visual confirmation)
-		TIMEOUT /T 1 > NUL
+		TASKKILL /F /FI "USERNAME eq %TARGET_USERDOMAIN%\%TARGET_USERNAME%" /FI "IMAGENAME eq %IMAGENAME_TO_KILL%"
+		REM Wait 1 second (to give the user visual confirmation that their session is locked before closing their tunnel)
+			TIMEOUT /T 1 >> %LOGFILE% 2>&1
 	)
 
-	REM	Lock the remote user-session, Wait 1 second, then Kill the RDP Session
-	REM *** Note that this 'wait exactly 1 second' is so that we show (during that second) the remote PC being locked before cutting the video feed to the user's remote desktop & closing it for-them (Next Step)
-	REM *** This gives the power to the user's visual inspection because it does exactly the same action as what they're used to when locking/logging-off of a PC
-	
-	REM	 Lock the Session for [target-user] (acts exactly the same as them selecting 'lock' from the start menu)
-	RUNDLL32 USER32.DLL,LockWorkStation
+	REM	Lock the Session for [target-user]
+	REM	  |--> Visually depicted the same as if they selected 'lock' (from the start menu) on a local Windows workstation
+		RUNDLL32 USER32.DLL,LockWorkStation >> %LOGFILE% 2>&1
 
-	REM Show the screen (to see the action just performed) for just a second before proceeding (visual confirmation)
-	TIMEOUT /T 1 > NUL
+	REM Wait 1 second
+		TIMEOUT /T 1 >> %LOGFILE% 2>&1
 
 	REM	 Kill the RDP Session (Closes the Remote-Deskop window on the Client's End)
-	TSDISCON %USER_SESSION_ID%
+		TSDISCON %USER_SESSION_ID% >> %LOGFILE% 2>&1
 
-	ECHO.
-	ECHO LOGGING OFF
-	ECHO	 To cancel, close this window.
-	ECHO.
-	
+	ECHO. >> %LOGFILE% 2>&1
+	ECHO LOGGING OFF >> %LOGFILE% 2>&1
+	ECHO	 To cancel, close this CMD Window (Hit the top-right "X") >> %LOGFILE% 2>&1
+	ECHO. >> %LOGFILE% 2>&1
+
+
+	REM
+	REM	Allow any boot-up processes (for this user's session) to complete before forcing logoff
+	REM	  |--> Use-Case: Log-on followed immediately by a log-off
+	REM
+	TIMEOUT /T 30 >> %LOGFILE% 2>&1
+
+	REM
+	REM Logoff of the machine (cleanly)
+	REM
+	%SystemRoot%\System32\logoff.exe %USER_SESSION_ID% /V >> %LOGFILE% 2>&1
+
+) ELSE (
+
+	REM
+	REM Logoff of the machine (cleanly)
+	REM
+	%SystemRoot%\System32\logoff.exe /V >> %LOGFILE% 2>&1
+
 )
 
-REM	 Allow any boot-up processes (for this user's session) to complete before forcing logoff
-REM	 	(Handler for login followed by immediate logoff scenario)
-TIMEOUT /T 30
-	
-REM Log off (Ends the current session)
-SHUTDOWN -L
-
 EXIT
-
-REM	------------------------------------------------------------
-REM	Revisioning | 2016-09-29 | Created
-REM	Revisioning | 2018-12-12 | Generalized IMAGENAME_TO_KILL as parameter #1
-REM	Revisioning | 2019-06-14 | Revision formatting moved to bottom
-REM	------------------------------------------------------------
-REM	Author | Matt Cavallo | https://github.com/mcavallo-git
-REM	------------------------------------------------------------

e25c3ec82c9c566ea75968ce49e1734e7809b3ec
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-05 18:06:58 -0400

    Update Linux - file, eol detection (find files with non-LF EOL, e.g. find CRLF files).sh

--- linux/Linux - file, eol detection (find files with non-LF EOL, e.g. find CRLF files).sh
+++ linux/Linux - file, eol detection (find files with non-LF EOL, e.g. find CRLF files).sh
@@ -5,15 +5,17 @@
 INPUT_DIRECTORY="${HOME}/Documents/GitHub/"; # Make sure input-directory ends with a "/" (forward slash)
 OUTPUT_RESULTS_TO="${HOME}/Desktop/non-lf-files.log";
 
-echo -e "\n   INITIATED FILE-SEARCH";
-echo -e "\n    |--> PATTERN:  EOL != LF";
-echo -e "\n    |--> PATTERN:  DIR == \"${INPUT_DIRECTORY}\"";
+echo "";
+echo "  INITIATED FILE-SEARCH";
+echo "    |--> PATTERN:  EOL != LF";
+echo "    |--> PATTERN:  DIR == \"${INPUT_DIRECTORY}\"";
 
 find "${INPUT_DIRECTORY}" -not -type d -exec file "{}" ";" | grep 'line terminators' > "${OUTPUT_RESULTS_TO}";
 
-echo -e "\n   COMPLETED FILE-SEARCH";
-echo -e "\n    |--> MATCHED:  $(cat \"${OUTPUT_RESULTS_TO}\" | wc -l) File(s)";
-echo -e "\n    |--> LOGFILE:  \"${OUTPUT_RESULTS_TO}\"";
+echo "";
+echo "  COMPLETED FILE-SEARCH";
+echo "    |--> MATCHED:  $(cat \"${OUTPUT_RESULTS_TO}\" | wc -l) File(s)";
+echo "    |--> LOGFILE:  \"${OUTPUT_RESULTS_TO}\"";
 
 
 # ------------------------------------------------------------

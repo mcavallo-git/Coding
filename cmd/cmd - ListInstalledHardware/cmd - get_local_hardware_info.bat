742aa3f576ef834d6fedb6b6beb432fa4280d075
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-08 15:02:13 -0400

    Update cmd - get_local_hardware_info.bat

--- cmd/cmd - ListInstalledHardware/cmd - get_local_hardware_info.bat
+++ cmd/cmd - ListInstalledHardware/cmd - get_local_hardware_info.bat
@@ -46,27 +46,27 @@ REM **********************
 
 : SETUP_FILESTREAM
 	REM  \*/*\   Get the domain name
-	FOR /f "tokens=2 delims==" %%I in ('wmic computersystem get domain /format:list') do set "domain=%%I"
+	FOR /f "tokens=2 delims==" %%I in ('wmic computersystem get domain /format:list') do SET "domain=%%I"
 	REM  \*/*\   Get the username
-	FOR /f "tokens=2 delims==" %%I in ('wmic computersystem get username /format:list') do set "username=%%I"
+	FOR /f "tokens=2 delims==" %%I in ('wmic computersystem get username /format:list') do SET "username=%%I"
 	REM  \*/*\   Get the hostname (computer name)
-	FOR /f "tokens=2 delims==" %%I in ('wmic computersystem get name /format:list') do set "hostname=%%I"
-	REM set date=date /T
-	SET output_file=.\SystemSpecsWMIC___%hostname%___%domain%.txt
+	FOR /f "tokens=2 delims==" %%I in ('wmic computersystem get name /format:list') do SET "hostname=%%I"
+	REM SET date=date /T
+	SET output_file=.\get_local_hardware_info.results.%hostname%.%domain%.txt
 	CALL :SETUP_DATETIME
 	ECHO SYSTEM INFO FOR PC [%hostname%] RAN [%dt_spaces%] BY [%username%] > %output_file%
 	EXIT /b
 
 : SETUP_DATETIME
-	set hour=%time:~0,2%
+	SET hour=%time:~0,2%
 	REM  \*/*\   Datetime with underscores (no spaces, for filenames etc.)
-	set dt_underscores9=%date:~-4%-%date:~4,2%-%date:~7,2%_0%time:~1,1%-%time:~3,2%-%time:~6,2% 
-	set dt_underscores24=%date:~-4%-%date:~4,2%-%date:~7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
-	if "%hour:~0,1%" == " " (set dt_underscores=%dt_underscores9%) else (set dt_underscores=%dt_underscores24%)
+	SET dt_underscores9=%date:~-4%-%date:~4,2%-%date:~7,2%_0%time:~1,1%-%time:~3,2%-%time:~6,2% 
+	SET dt_underscores24=%date:~-4%-%date:~4,2%-%date:~7,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%
+	if "%hour:~0,1%" == " " (SET dt_underscores=%dt_underscores9%) else (SET dt_underscores=%dt_underscores24%)
 	REM  \*/*\   Datetime with spaces, for cleaner viewing
-	set dt_spaces9=%date:~-4%-%date:~4,2%-%date:~7,2% 0%time:~1,1%:%time:~3,2%:%time:~6,2% 
-	set dt_spaces24=%date:~-4%-%date:~4,2%-%date:~7,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
-	if "%hour:~0,1%" == " " (set dt_spaces=%dt_spaces9%) else (set dt_spaces=%dt_spaces24%)
+	SET dt_spaces9=%date:~-4%-%date:~4,2%-%date:~7,2% 0%time:~1,1%:%time:~3,2%:%time:~6,2% 
+	SET dt_spaces24=%date:~-4%-%date:~4,2%-%date:~7,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
+	if "%hour:~0,1%" == " " (SET dt_spaces=%dt_spaces9%) else (SET dt_spaces=%dt_spaces24%)
 	EXIT /b
 	
 REM *****************
@@ -74,70 +74,70 @@ REM   WMIC ROUTINES
 REM *****************
 	
 : WMIC_CPU
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- PROCESSOR CORE INFO -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- PROCESSOR CORE INFO -- >> %output_file%
 	wmic cpu get name,numberofcores,numberoflogicalprocessors | findstr /r /v "^$" >> %output_file%
-	echo. >> %output_file%
-	echo              -- PROCESSOR CACHE, CLOCK INFO -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- PROCESSOR CACHE, CLOCK INFO -- >> %output_file%
 	wmic cpu get name,l2cachesize,l3cachesize,currentclockspeed,maxclockspeed | findstr /r /v "^$" >> %output_file%
 	EXIT /b	
 
 : WMIC_DISKS
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- STORAGE DEVICES (SIZE IN BYTES) -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- STORAGE DEVICES (SIZE IN BYTES) -- >> %output_file%
 	wmic diskdrive get index,caption,size | findstr /r /v "^$" >> %output_file%
 	EXIT /b	
 	
 : WMIC_GRAPHICS
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- GRAPHICS INFO -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- GRAPHICS INFO -- >> %output_file%
 	wmic path Win32_VideoController get name,description,adapterram,driverversion | findstr /r /v "^$" >> %output_file%
 	EXIT /b
 
 : WMIC_LOGIN
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- USERNAME / DOMAIN -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- USERNAME / DOMAIN -- >> %output_file%
 	wmic computersystem get domain,username,partofdomain,primaryownername | findstr /r /v "^$" >> %output_file%
 	EXIT /b	
 	
 : WMIC_MOBO
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- MOTHERBOARD INFO -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- MOTHERBOARD INFO -- >> %output_file%
 	wmic baseboard get manufacturer,product,serialnumber | findstr /r /v "^$" >> %output_file%
 	EXIT /b	
 	
 : WMIC_MODEL
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- PC NAME / MODEL -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- PC NAME / MODEL -- >> %output_file%
 	wmic computersystem get model,manufacturer,name,systemtype | findstr /r /v "^$" >> %output_file%
 	EXIT /b	
 
 : WMIC_NIC 
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- NIC INFO -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- NIC INFO -- >> %output_file%
 	wmic nicconfig where "macaddress is not null" get macaddress,ipaddress,index,defaultipgateway,dhcpserver,description | findstr /r /v "^$" >> %output_file%
 	EXIT /b	
 	
 : WMIC_OS
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- OPERATING SYSTEM -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- OPERATING SYSTEM -- >> %output_file%
 	wmic os get caption,osarchitecture,version | findstr /r /v "^$" >> %output_file%
 	EXIT /b
 		
 : WMIC_RAM
-	echo. >> %output_file%
-	echo. >> %output_file%
-	echo              -- MOBO RAM LIMITS / SLOTS -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- MOBO RAM LIMITS / SLOTS -- >> %output_file%
 	wmic memphysical get memorydevices,maxcapacity | findstr /r /v "^$" >> %output_file%
-	echo. >> %output_file%
-	echo              -- RAM MODULES (CAPACITY IN BYTES) -- >> %output_file%
+	ECHO. >> %output_file%
+	ECHO              -- RAM MODULES (CAPACITY IN BYTES) -- >> %output_file%
 	wmic memorychip get tag,banklabel,capacity,speed,devicelocator | findstr /r /v "^$" >> %output_file%
 	EXIT /b

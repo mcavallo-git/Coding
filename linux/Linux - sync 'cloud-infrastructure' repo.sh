e68a1f901fbb8eedd997f2ce507ede142fe5816f
Matt Cavallo <mcavallo.git@gmail.com>
2019-08-09 06:30:36 -0400

    Update Linux - sync 'cloud-infrastructure' repo.sh

--- linux/Linux - sync 'cloud-infrastructure' repo.sh
+++ linux/Linux - sync 'cloud-infrastructure' repo.sh
@@ -2,17 +2,24 @@
 
 GIT_URL="https://github.com/mcavallo-git/cloud-infrastructure.git";
 
-GIT_WORKINGTREE="${HOME}/Documents/GitHub/cloud-infrastructure/";
-
-if [ ! -d "${GIT_WORKINGTREE}" ]; then
-	cd $(dirname "${GIT_WORKINGTREE}");
-	git clone "https://github.com/mcavallo-git/cloud-infrastructure.git";
+# Ensure the (GitHub) Repos directory exists
+DOCS_GITHUB="${HOME}/Documents/GitHub/";
+if [ ! -d "${DOCS_GITHUB}" ]; then
+	mkdir "${DOCS_GITHUB}";
 fi;
 
-cd "${GIT_WORKINGTREE}" && \
+# Clone the repo if it hasn't been synced, yet
+GIT_WORKTREE="${DOCS_GITHUB}/cloud-infrastructure/";
+if [ ! -d "${GIT_WORKTREE}" ]; then
+	cd "${DOCS_GITHUB}" && \
+	git clone "${GIT_URL}";
+fi;
+
+# Sync local workstation's commands with the repo's commands
+cd "${GIT_WORKTREE}" && \
 git fetch && \
 git reset --hard "origin/master" && \
 git pull && \
-DO_UPD="${GIT_WORKINGTREE}/usr/local/sbin/update_bins_from_sftp" && \
+DO_UPD="${GIT_WORKTREE}/usr/local/sbin/update_bins_from_sftp" && \
 sudo chmod 755 "${DO_UPD}" && \
 sudo "${DO_UPD}";

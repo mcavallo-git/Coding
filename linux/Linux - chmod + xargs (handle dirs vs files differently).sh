b42091d8e995b791831e03f10272c7ac54cef258
Matt Cavallo <mcavallo.git@gmail.com>
2019-08-09 06:53:44 -0400

    Update Linux - chmod + xargs (handle dirs vs files differently).sh

--- linux/Linux - chmod + xargs (handle dirs vs files differently).sh
+++ linux/Linux - chmod + xargs (handle dirs vs files differently).sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+
+
 # UPDATED_METHOD
 
 find "/var/www/html" -type d -exec chmod 0755 '{}' \; # directories
@@ -8,13 +10,10 @@ find "/var/www/html" -type f -exec chmod 0644 '{}' \; # files
 
 
 
-
-
 # DEPRECATED METHOD:
 
 # find "/var/www/html" -type d -print0 | xargs -0 chmod 0775; # directories
 
 # find "/var/www/html" -type f -print0 | xargs -0 chmod 0664; # files
-
 
 

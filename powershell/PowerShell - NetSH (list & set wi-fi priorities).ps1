ed27fab2bcab8b2c6e23c849a09815d2d89b968d
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-12 16:03:50 -0400

    Update PowerShell - NetSH (list & set wi-fi priorities).ps1

--- powershell/PowerShell - NetSH (list & set wi-fi priorities).ps1
+++ powershell/PowerShell - NetSH (list & set wi-fi priorities).ps1
@@ -2,48 +2,74 @@
 # IN-PROGRESS AS-OF 2019-08-08_17-22-25
 # IN-PROGRESS AS-OF 2019-08-12_15-46-39
 
-$NIC_Connections = @();
-$WLAN_Connections = @();
+$Connections_NIC_NETSH = @();
+$Connections_NIC_WMIC = @();
+$Connections_WLAN_NETSH = @();
+$Connections_WLAN_WMIC = @();
+
+# ------------------------------------------------------------
 
 # Show all network interfaces
-$Interfaces_NIC = (netsh.exe int ipv4 show interfaces);
+$Interfaces_NIC_NETSH = (netsh.exe int ipv4 show interfaces);
 
-$RegexPattern_NIC = '';
-$RegexPattern_NIC = $RegexPattern_NIC + '^'; # Start Regex Pattern
-$RegexPattern_NIC = $RegexPattern_NIC + '\s+([0-9]+)'; # Capture the col-1 (Idx)
-$RegexPattern_NIC = $RegexPattern_NIC + '\s+([0-9]+)'; # Capture the col-2 (Met)
-$RegexPattern_NIC = $RegexPattern_NIC + '\s+([0-9]+)'; # Capture the col-3 (MTU)
-$RegexPattern_NIC = $RegexPattern_NIC + '\s+(\S+)'; # Capture the col-4 (State)
-$RegexPattern_NIC = $RegexPattern_NIC + '\s+(.+)'; # Capture col-5 (Name - the remainder at the end of the table, including spaces)
-$RegexPattern_NIC = $RegexPattern_NIC + '$'; # End Regex Pattern
+$Interfaces_NIC_WMIC = (wmic nicconfig get * /format:list);
 
+$RegexPattern_NIC_NETSH = '';
+$RegexPattern_NIC_NETSH = $RegexPattern_NIC_NETSH + '^'; # Start Regex Pattern
+$RegexPattern_NIC_NETSH = $RegexPattern_NIC_NETSH + '\s+([0-9]+)'; # Capture the col-1 (Idx)
+$RegexPattern_NIC_NETSH = $RegexPattern_NIC_NETSH + '\s+([0-9]+)'; # Capture the col-2 (Met)
+$RegexPattern_NIC_NETSH = $RegexPattern_NIC_NETSH + '\s+([0-9]+)'; # Capture the col-3 (MTU)
+$RegexPattern_NIC_NETSH = $RegexPattern_NIC_NETSH + '\s+(\S+)'; # Capture the col-4 (State)
+$RegexPattern_NIC_NETSH = $RegexPattern_NIC_NETSH + '\s+(.+)'; # Capture col-5 (Name - the remainder at the end of the table, including spaces)
+$RegexPattern_NIC_NETSH = $RegexPattern_NIC_NETSH + '$'; # End Regex Pattern
+
+# ------------------------------------------------------------
 
 # Show WLAN (Wi-Fi) network interfaces
-$Interfaces_WLAN = (netsh.exe wlan show interfaces);
+$Interfaces_WLAN_NETSH = (netsh.exe wlan show interfaces);
+
+$Interfaces_WLAN_WMIC = (wmic nicconfig where "IPEnabled=True AND TcpipNetbiosOptions!=null AND TcpipNetbiosOptions!=2 AND DHCPServer!=`"255.255.255.255`"" get Description,InterfaceIndex,IPAddress,DefaultIPGateway,IPSubnet,DNSServerSearchOrder,DHCPServer /format:list);
+
 $Profiles_WLAN = (netsh.exe wlan show profiles);
 
-$RegexPattern_WLAN_Description = '^\s*Description\s+:\s+(.+)$';
-$RegexPattern_WLAN_Name = '^\s*Name\s+:\s+(.+)$';
-$RegexPattern_WLAN_MAC = '^\s*Physical address\s+:\s+(.+)$';
-$RegexPattern_WLAN_State = '^\s*State\s+:\s+(.+)$';
+$RegexPattern_WLAN_NETSH_Description = '^\s*Description\s+:\s+(.+)$';
+$RegexPattern_WLAN_NETSH_Name = '^\s*Name\s+:\s+(.+)$';
+$RegexPattern_WLAN_NETSH_MAC = '^\s*Physical address\s+:\s+(.+)$';
+$RegexPattern_WLAN_NETSH_State = '^\s*State\s+:\s+(.+)$';
 
-$Interfaces_NIC | ForEach {
-	$Matches_NIC = [Regex]::Match($_, $RegexPattern_NIC);
-	If ($Matches_NIC.Success -Ne $False) {
-		$Idx_NIC = $Matches_NIC.Groups[1].Value;
-		$State_NIC = $Matches_NIC.Groups[4].Value;
-		$Name_NIC = $Matches_NIC.Groups[5].Value;
+# ------------------------------------------------------------
+
+$RegexPattern_NIC_WMIC = '^([a-zA-Z]+)=(.+)$';
+
+$RegexPattern_Delimiter_WMIC = '^\s*$';
+
+$Each_NIC_WMIC = @();
+$Interfaces_NIC_WMIC | ForEach {
+	$Matches_NIC_WMIC = [Regex]::Match($_, $RegexPattern_NIC_WMIC);
+
+	If ($Matches_NIC_WMIC.Success -Ne $False) {
+
+	}
+
+}
+
+$Interfaces_NIC_NETSH | ForEach {
+	$Matches_NIC_NETSH = [Regex]::Match($_, $RegexPattern_NIC_NETSH);
+	If ($Matches_NIC_NETSH.Success -Ne $False) {
+		$Idx_NIC = $Matches_NIC_NETSH.Groups[1].Value;
+		$State_NIC = $Matches_NIC_NETSH.Groups[4].Value;
+		$Name_NIC = $Matches_NIC_NETSH.Groups[5].Value;
 		If ($State_NIC -Eq "connected") {
 			$Each_NIC = @{};
 			$Each_NIC.Idx = $Idx_NIC;
 			$Each_NIC.State = $State_NIC;
 			$Each_NIC.Name = $Name_NIC;
 
-			$Interfaces_WLAN | ForEach {
-				$Matches_WLAN_Description = [Regex]::Match($_, $RegexPattern_WLAN_Description);
-				$Matches_WLAN_Name = [Regex]::Match($_, $RegexPattern_WLAN_Name);
-				$Matches_WLAN_MAC = [Regex]::Match($_, $RegexPattern_WLAN_MAC);
-				$Matches_WLAN_State = [Regex]::Match($_, $RegexPattern_WLAN_State);
+			$Interfaces_WLAN_NETSH | ForEach {
+				$Matches_WLAN_Description = [Regex]::Match($_, $RegexPattern_WLAN_NETSH_Description);
+				$Matches_WLAN_Name = [Regex]::Match($_, $RegexPattern_WLAN_NETSH_Name);
+				$Matches_WLAN_MAC = [Regex]::Match($_, $RegexPattern_WLAN_NETSH_MAC);
+				$Matches_WLAN_State = [Regex]::Match($_, $RegexPattern_WLAN_NETSH_State);
 				If ($Matches_WLAN_Description.Success -Ne $False) {
 					$Description_WLAN = $Matches_WLAN_Description.Groups[1].Value;
 				} ElseIf ($Matches_WLAN_Name.Success -Ne $False) {
@@ -59,8 +85,7 @@ $Interfaces_NIC | ForEach {
 				If ($Each_NIC.State -Eq $State_WLAN) {
 					$Each_NIC.MAC = $MAC_WLAN;
 					$Each_NIC.Description = $Description_WLAN;
-
-					$WLAN_Connections += $Each_NIC;
+					$Connections_WLAN_NETSH += $Each_NIC;
 				}
 			}
 		}
@@ -71,8 +96,13 @@ $Interfaces_NIC | ForEach {
 
 # Show WLAN (Wi-Fi) adapters
 Write-Output "------------------------------------------------------------";
-Write-Output "WLAN_Connections:";
-$WLAN_Connections | Format-List;
+Write-Output "Connections_WLAN_NETSH:";
+$Connections_WLAN_NETSH | Format-List;
+Write-Output "============================================================";
+
+Write-Output "------------------------------------------------------------";
+Write-Output "Profiles_WLAN:";
+$Profiles_WLAN;
 Write-Output "============================================================";
 
 

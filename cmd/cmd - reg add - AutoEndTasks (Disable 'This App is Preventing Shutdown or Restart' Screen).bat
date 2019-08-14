617588ce77d72270d0e30d3664ef67d0323b7be4
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-14 16:43:15 -0400

    Update cmd - reg add - AutoEndTasks (Disable 'This App is Preventing Shutdown or Restart' Screen).bat

--- cmd/cmd - reg add - AutoEndTasks (Disable 'This App is Preventing Shutdown or Restart' Screen).bat
+++ cmd/cmd - reg add - AutoEndTasks (Disable 'This App is Preventing Shutdown or Restart' Screen).bat
@@ -5,17 +5,25 @@ SET ValueName=AutoEndTasks
 SET DataType=REG_SZ
 SET DataValue=1
 
-ECHO.
-ECHO --- Calling [ REG QUERY "%KeyName%" /v "%ValueName%" ] (Before Update)...
-REG QUERY  "%KeyName%" /v "AutoEndTasks"
+FOR /F "tokens=* USEBACKQ" %%a IN (
+`REG QUERY "%KeyName%" /v "AutoEndTasks" /t "%DataType%" /f "%DataValue%"`
+) DO ( 
+	SET QUERY_KEY_VAL=%%a
+)
+
+SET ARE_REGEDITS_NEEDED=%QUERY_KEY_VAL:End of search: 1 match(es) found.=NO_REGEDITS_ARE_NEEDED%
 
 ECHO.
-ECHO --- Calling [ REG ADD "%KeyName%" /v "%ValueName%" /t "%DataType%" /d "%DataValue%" /f ]...
-REG ADD "HKCU\Control Panel\Desktop" /v "AutoEndTasks" /t "REG_SZ" /d "1" /f
+IF "%ARE_REGEDITS_NEEDED%"=="NO_REGEDITS_ARE_NEEDED" (
+	ECHO Registry-key "%KeyName%" already has a value with name="%ValueName%", type="%DataType%", and data="%DataValue%"
+) ELSE (
+	ECHO Adding a value with name="%ValueName%", type="%DataType%", and data="%DataValue%" to registry-key "%KeyName%"
+	REG ADD "%KeyName%" /v "%ValueName%" /t "%DataType%" /d "%DataValue%" /f
+)
 
 ECHO.
-ECHO --- Calling [ REG QUERY "%KeyName%" /v "%ValueName%" ]  (After Update)...
-REG QUERY  "%KeyName%" /v "AutoEndTasks"
+ECHO Final state of registry key "%KeyName%", value "%ValueName%":
+REG QUERY "%KeyName%" /v "%ValueName%"
 
 TIMEOUT /T 30
 

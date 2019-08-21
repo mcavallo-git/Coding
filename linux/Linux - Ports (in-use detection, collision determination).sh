76af1df78014adf8cba0c89b85c1a9b46c396566
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-21 18:49:07 -0400

    Update Linux - Ports (in-use detection, collision determination).sh

--- linux/Linux - Ports (in-use detection, collision determination).sh
+++ linux/Linux - Ports (in-use detection, collision determination).sh
@@ -1,9 +1,12 @@
 
 # List ports in use
-lsof -i -P -n | grep LISTEN
+lsof -i -P -n | grep 'LISTEN';
 
 # Show all items using port 443
-fuser -k 443/tcp
+fuser -k 443/tcp;
 
 # Kill all items using port 443
-fuser -k 443/tcp
+fuser -k 443/tcp;
+
+# Compare running-processes to actively-listening ports for any process containing the name [ java ]
+echo ""; ps aux | grep '%MEM'; ps aux | grep 'java'; echo ""; lsof -i -P -n | grep 'SIZE/OFF'; lsof -i -P -n | grep 'LISTEN' | grep 'java'; echo "";

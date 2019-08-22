1454f9a45261a1db2cb9a8e404c15495112f85fa
Matt Cavallo <mcavallo.git@gmail.com>
2019-08-22 02:26:26 -0400

    Update EdgeOS (UniFi) - Force DHCP Lease-Renewal.sh

--- ubiquiti/EdgeOS (UniFi) - Force DHCP Lease-Renewal.sh
+++ ubiquiti/EdgeOS (UniFi) - Force DHCP Lease-Renewal.sh
@@ -2,10 +2,10 @@
 
 # Ubnt (ERLite-3) - Force DHCP Lease-Renewal
 
-/opt/vyatta/bin/sudo-users/vyatta-clear-dhcp-lease.pl --lip=all
+sudo /opt/vyatta/bin/sudo-users/vyatta-clear-dhcp-lease.pl --lip=all
 
 
 # UniFi (USG-3P) - Force DHCP Lease-Renewal
 
-/opt/vyatta/bin/sudo-users/vyatta-clear-dhcp-lease.pl --lip=all --ilfile=/config/dhcpd.leases --olfile=/config/dhcpd.leases --pidf=/run/dhcpd.pid
+sudo /opt/vyatta/bin/sudo-users/vyatta-clear-dhcp-lease.pl --lip=all --ilfile=/config/dhcpd.leases --olfile=/config/dhcpd.leases --pidf=/run/dhcpd.pid
 

e74912727c3903b13818c47581d63d9399540238
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-23 19:15:22 -0400

    Update jenkins - get Java system_properties.sh

--- jenkins/jenkins - get Java system_properties.sh
+++ jenkins/jenkins - get Java system_properties.sh
@@ -4,7 +4,7 @@
 # Get Jenkins' system_property key-vals from its Java runtime environment
 jcmd "$(jcmd | grep jenkins | awk '{print $1}')" VM.system_properties;
 jcmd "$(sudo jcmd | grep jenkins | awk '{print $1}')" Compiler.directives_print;
-
+jmap -clstats "$(sudo jcmd | grep jenkins | awk '{print $1}')";
 
 # Show other callable arguments/methods in Jenkins' Java runtime environment
 jcmd "$(sudo jcmd | grep jenkins | awk '{print $1}')" help;

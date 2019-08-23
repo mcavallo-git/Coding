4f6824c61677fdcc38c5e9898a0de1cd72c43131
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-23 18:28:01 -0400

    Update Linux - Ports (in-use detection, collision determination).sh

--- linux/Linux - Ports (in-use detection, collision determination).sh
+++ linux/Linux - Ports (in-use detection, collision determination).sh
@@ -24,8 +24,8 @@ PROC_RESULTS_LSOF=$(lsof -i -P -n | grep "${PROC_ROW_CONTAINS}" | grep "${PROC_C
 if [ -z "${PROC_RESULTS_PS}" ] && [ -z "${PROC_RESULTS_LSOF}" ]; then \
 	echo ""; \
 	echo "Error - No PIDs found for jenkins"; \
-	echo "Calling [ service jenkins restart; sleep 5; ]..."; \
-	service jenkins restart; sleep 5; \
+	echo "Calling [ service jenkins restart; sleep 2; ]..."; \
+	service jenkins restart; sleep 2; \
 	echo ""; \
 fi; \
 PROC_RESULTS_PS=$(ps -A --format "${PROC_FORMAT}" | grep -v 'color=auto' | grep "${PROC_ROW_CONTAINS}" | sed --regexp-extended --quiet --expression='s/^('${PROC_CMD:-\S+}')\s+('${PROC_USER:-\S+}')\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+).*$/\'${PROC_GET_COLNO}'/p' | head -n 1;); \

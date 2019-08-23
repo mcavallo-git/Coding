0b5d16e11fe705a7ae9ff57f6a754d38868dee07
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-23 18:27:48 -0400

    Update Linux - Ports (in-use detection, collision determination).sh

--- linux/Linux - Ports (in-use detection, collision determination).sh
+++ linux/Linux - Ports (in-use detection, collision determination).sh
@@ -12,37 +12,27 @@ fuser -k 443/tcp;
 # List all --format column-options within  [ ps ]  ( example: ps aux --format pid,format,state,tname,time,command )
 ps L;
 
-# Compare running-processes to actively-listening ports for any process containing the name [ java ]
-# | awk '{print $2}'
+# Locate processes which match the following variable declarations:
 
-# ps aux | grep '%MEM' | awk '{print $2}'; \
-# ps u -A -U 'jenkins' --format user,pid,%cpu,%mem,command,bsdstart,lstart,start_time,time --sort=%cpu;
-# ps -u 'jenkins' --format 'fname,pid,user,%cpu,%mem,maj_flt,cmd' --sort='fname' | grep -Ei '^COMMAND' | head -n 1
-
-echo ""; \
-ps -u 'jenkins' --format 'fname,user,pid,%cpu,%mem,maj_flt,cmd' --sort='fname' | grep -Ei '^COMMAND' | head -n 1; \
-
-PROC_CMD="java";
-PROC_USER="jenkins";
-PROC_FORMAT="fname,user,pid,%cpu,%mem,maj_flt,cmd";
-PROC_GET_COLNO="3"; # pid
-PROC_PIDS=$( \
-ps -A --format "${PROC_FORMAT}" \
-| sed --regexp-extended --quiet --expression='s/^('${PROC_CMD:-\S+}')\s+('${PROC_USER:-\S+}')\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+).*$/\'${PROC_GET_COLNO}'/p' \
-);
-echo "PROC_PIDS:";
-echo "${PROC_PIDS}";
-
-echo ""; \
-lsof -i -P -n | grep 'SIZE/OFF' | awk '{print $2}'; \
-lsof -i -P -n | grep '*:8080 (LISTEN)' | grep 'java' | awk '{print $2}'; \
-echo "";
-
-# echo ""; \
-# ps aux | grep '%MEM'; \
-# ps aux | grep 'java' | grep -v 'color'; \
-# echo ""; \
-# lsof -i -P -n | grep 'SIZE/OFF'; \
-# lsof -i -P -n | grep 'LISTEN' | grep 'java' | grep '*:8080 (LISTEN)'; \
-# echo "";
-
+PROC_CMD="java"; \
+PROC_USER="jenkins"; \
+PROC_FORMAT="fname,user,pid,%cpu,%mem,maj_flt,cmd"; \
+PROC_GET_COLNO="3"; \
+PROC_ROW_CONTAINS="8080"; \
+PROC_RESULTS_PS=$(ps -A --format "${PROC_FORMAT}" | grep -v 'color=auto' | grep "${PROC_ROW_CONTAINS}" | sed --regexp-extended --quiet --expression='s/^('${PROC_CMD:-\S+}')\s+('${PROC_USER:-\S+}')\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+).*$/\'${PROC_GET_COLNO}'/p' | head -n 1;); \
+PROC_RESULTS_LSOF=$(lsof -i -P -n | grep "${PROC_ROW_CONTAINS}" | grep "${PROC_CMD}" | awk '{print $2}';); \
+if [ -z "${PROC_RESULTS_PS}" ] && [ -z "${PROC_RESULTS_LSOF}" ]; then \
+	echo ""; \
+	echo "Error - No PIDs found for jenkins"; \
+	echo "Calling [ service jenkins restart; sleep 5; ]..."; \
+	service jenkins restart; sleep 5; \
+	echo ""; \
+fi; \
+PROC_RESULTS_PS=$(ps -A --format "${PROC_FORMAT}" | grep -v 'color=auto' | grep "${PROC_ROW_CONTAINS}" | sed --regexp-extended --quiet --expression='s/^('${PROC_CMD:-\S+}')\s+('${PROC_USER:-\S+}')\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+).*$/\'${PROC_GET_COLNO}'/p' | head -n 1;); \
+PROC_RESULTS_LSOF=$(lsof -i -P -n | grep "${PROC_ROW_CONTAINS}" | grep "${PROC_CMD}" | awk '{print $2}';); \
+echo -e "\n""PROC_RESULTS_PS:\n${PROC_RESULTS_PS}\n"; ps --format "${PROC_FORMAT}" -p "${PROC_RESULTS_PS}"; echo -e "\n\n"; \
+echo -e "\n""PROC_RESULTS_LSOF:\n${PROC_RESULTS_LSOF}\n"; ps --format "${PROC_FORMAT}" -p "${PROC_RESULTS_LSOF}"; echo -e "\n\n"; \
+echo "Calling [ kill \"${PROC_RESULTS_LSOF}\" \"${PROC_RESULTS_PS}\"; ]..."; \
+kill "${PROC_RESULTS_LSOF}" "${PROC_RESULTS_PS}"; \
+echo "Calling [ service jenkins restart; ]..."; \
+service jenkins restart;

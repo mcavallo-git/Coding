e83018175e2a31403d35e916e1ec9db184abfc9d
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-28 12:35:51 -0400

    Update cmd - nslookup (get LAN device hostnames).bat

--- cmd/cmd - nslookup (get LAN device hostnames).bat
+++ cmd/cmd - nslookup (get LAN device hostnames).bat
@@ -1,7 +1,8 @@
-@REM NOTE: Replace "192.168.0." with subnet to be scanned for devices
+@ECHO OFF
 
+REM SWAP  "192.168.0." with the first-three numbers of your subnet's IPv4, or just grab your current IPv4 and use it's first 3 numbers
 SET SUBNET=192.168.0.
 
-FOR /L %%N IN (1,1,254) DO @nslookup %SUBNET%%%N %SUBNET%1 >> DeviceHostnamesLAN.txt
+FOR /L %%N IN (1,1,254) DO @nslookup %SUBNET%%%N %SUBNET%1 >> %USERPROFILE%\Desktop\LAN_Hostnames.txt
 
 TIMEOUT -T 60

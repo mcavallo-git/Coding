46e00ae7e170af3adb8528ea657131cf21f8aec2
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-28 05:38:33 -0400

    Create cmd - bcdedit (remove hyper-v remnants).bat

---
+++ cmd/cmd - bcdedit (remove hyper-v remnants).bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+bcdedit /set hypervisorlaunchtype Off
+
+
+REM
+REM Citation(s)
+REM
+REM		answers.microsoft.com
+REM		  |  "Hyper V remnants left over after disabling, preventing other Virtualization software from running in 64-bit mode"
+REM		  |  https://answers.microsoft.com/en-us/insider/forum/all/hyper-v-remnants-left-over-after-disabling/81e4fef1-d08e-4ef4-8e1e-b24c47bd7677?auth=1
+REM

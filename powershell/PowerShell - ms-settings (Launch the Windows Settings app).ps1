20ad89cbd82b2127f88e1269aae3b31430c3d85c
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-29 07:54:08 -0400

    Create PowerShell - ms-settings (Launch the Windows Settings app).ps1

---
+++ powershell/PowerShell - ms-settings (Launch the Windows Settings app).ps1
@@ -0,0 +1,112 @@
+# Open Win10's Settings
+start ms-settings:;
+<#	Landing page for Settings	#>	start	ms-settings:;
+<#	About	#>	start	ms-settings:about;
+<#	Activation	#>	start	ms-settings:activation;
+<#	Apps & features	#>	start	ms-settings:appsfeatures;
+<#	Apps for websites	#>	start	ms-settings:appsforwebsites;
+<#	App volume and device preferences (1903)	#>	start	ms-settings:apps-volume;
+<#	Software Center (SCCM)	#>	start	softwarecenter:;
+<#	AutoPlay	#>	start	ms-settings:autoplay;
+<#	Backup	#>	start	ms-settings:backup;
+<#	Recovery - Reset/Advanced startup	#>	start	ms-settings:recovery;
+<#	Battery Saver	#>	start	ms-settings:batterysaver;
+<#	Battery Saver - settings	#>	start	ms-settings:batterysaver-settings;
+<#	Battery Saver - usage	#>	start	ms-settings:batterysaver-usagedetails;
+<#	Bluetooth Devices	#>	start	ms-settings:bluetooth;
+<#	Shared experiences	#>	start	ms-settings:crossdevice;
+<#	Default apps	#>	start	ms-settings:defaultapps;
+<#	For developers	#>	start	ms-settings:developers;
+<#	Display	#>	start	ms-settings:display;
+<#	Display Screen rotation	#>	start	ms-settings:screenrotation;
+<#	Storage	#>	start	ms-settings:storagesense;
+<#	Offline Maps	#>	start	ms-settings:maps;
+<#	Connected Devices	#>	start	ms-settings:connecteddevices;
+<#	Touchpad	#>	start	ms-settings:devices-touchpad;
+<#	Mouse & Touchpad Devices	#>	start	ms-settings:mousetouchpad;
+<#	Typing	#>	start	ms-settings:typing;
+<#	Find My Device	#>	start	ms-settings:findmydevice;
+<#	USB	#>	start	ms-settings:usb;
+<#	Airplane mode	#>	start	ms-settings:network-airplanemode;
+<#	Cellular Network	#>	start	ms-settings:network-cellular;
+<#	DirectAccess	#>	start	ms-settings:network-directaccess;
+<#	Dial-up Network	#>	start	ms-settings:network-dialup;
+<#	Ethernet Network	#>	start	ms-settings:network-ethernet;
+<#	Proxy (Network)	#>	start	ms-settings:network-proxy;
+<#	Network Status	#>	start	ms-settings:network-status;
+<#	VPN	#>	start	ms-settings:network-vpn;
+<#	Wi-Fi	#>	start	ms-settings:network-wifi;
+<#	Manage known Wi-Fi	#>	start	ms-settings:network-wifisettings;
+<#	Sync your settings	#>	start	ms-settings:sync;
+<#	Data Sense	#>	start	ms-settings:datausage;
+<#	Mobile Hotspot	#>	start	ms-settings:network-mobilehotspot;
+<#	Lock screen	#>	start	ms-settings:lockscreen;
+<#	Power & sleep	#>	start	ms-settings:powersleep;
+<#	Multitasking	#>	start	ms-settings:multitasking;
+<#	Optional features	#>	start	ms-settings:optionalfeatures;
+<#	Personalization	#>	start	ms-settings:personalization;
+<#	Backgrounds	#>	start	ms-settings:personalization-background;
+<#	Colors	#>	start	ms-settings:personalization-colors;
+<#	Colors	#>	start	ms-settings:colors;
+<#	Start Menu	#>	start	ms-settings:personalization-start;
+<#	Taskbar	#>	start	ms-settings:taskbar;
+<#	Themes	#>	start	ms-settings:themes;
+<#	Printers	#>	start	ms-settings:printers;
+<#	Proximity	#>	start	ms-settings:proximity;
+<#	Email & app accounts	#>	start	ms-settings:emailandaccounts;
+<#	Family & other users	#>	start	ms-settings:otherusers;
+<#	Your workplace	#>	start	ms-settings:workplace;
+<#	Date & Time	#>	start	ms-settings:dateandtime;
+<#	Region & language	#>	start	ms-settings:regionlanguage;
+<#	Sign-in options	#>	start	ms-settings:signinoptions;
+<#	Speech	#>	start	ms-settings:speech;
+<#	Privacy	#>	start	ms-settings:privacy;
+<#	Account info (privacy)	#>	start	ms-settings:privacy-accountinfo;
+<#	Background apps (privacy)	#>	start	ms-settings:privacy-backgroundapps;
+<#	App diagnostics (privacy)	#>	start	ms-settings:privacy-appdiagnostics;
+<#	Call history (privacy)	#>	start	ms-settings:privacy-callhistory;
+<#	Calendar (privacy)	#>	start	ms-settings:privacy-calendar;
+<#	Contacts (privacy)	#>	start	ms-settings:privacy-contacts;
+<#	Email (privacy)	#>	start	ms-settings:privacy-email;
+<#	Feedback & diagnostics (privacy)	#>	start	ms-settings:privacy-feedback;
+<#	Location (privacy)	#>	start	ms-settings:privacy-location;
+<#	Messaging (privacy)	#>	start	ms-settings:privacy-messaging;
+<#	Microphone (privacy)	#>	start	ms-settings:privacy-microphone;
+<#	Motion (privacy)	#>	start	ms-settings:privacy-motion;
+<#	Notifications and Actions	#>	start	ms-settings:notifications;
+<#	Notifications (privacy)	#>	start	ms-settings:privacy-notifications;
+<#	Other (privacy)	#>	start	ms-settings:privacy-customdevices;
+<#	Radio (privacy)	#>	start	ms-settings:privacy-radios;
+<#	Speech (privacy)	#>	start	ms-settings:privacy-speechtyping;
+<#	Tasks (privacy)	#>	start	ms-settings:privacy-tasks;
+<#	Webcam (privacy)	#>	start	ms-settings:privacy-webcam;
+<#	Your info (Microsoft account)	#>	start	ms-settings:yourinfo;
+<#	Closed Captioning	#>	start	ms-settings:easeofaccess-closedcaptioning;
+<#	High Contrast	#>	start	ms-settings:easeofaccess-highcontrast;
+<#	Keyboard	#>	start	ms-settings:easeofaccess-keyboard;
+<#	Magnifier	#>	start	ms-settings:easeofaccess-magnifier;
+<#	Mouse	#>	start	ms-settings:easeofaccess-mouse;
+<#	Narrator	#>	start	ms-settings:easeofaccess-narrator;
+<#	Other options	#>	start	ms-settings:easeofaccess-otheroptions;
+<#	Projecting to this PC	#>	start	ms-settings:project;
+<#	Tablet mode	#>	start	ms-settings:tabletmode;
+<#	Windows Defender	#>	start	ms-settings:windowsdefender;
+<#	Windows Insider Program (beta's)	#>	start	ms-settings:windowsinsider;
+<#	Troubleshoot - Fix Windows Update	#>	start	ms-settings:troubleshoot;
+<#	Windows Update	#>	start	ms-settings:windowsupdate;
+<#	WinUpdate - Check for updates	#>	start	ms-settings:windowsupdate-action;
+<#	WinUpdate - Update history	#>	start	ms-settings:windowsupdate-history;
+<#	WinUpdate - Advanced options	#>	start	ms-settings:windowsupdate-options;
+<#	WinUpdate - Restart options	#>	start	ms-settings:windowsupdate-restartoptions;
+
+# ------------------------------------------------------------
+#
+#	Citation(s)
+#
+#		youtube.com  |  "How to fix Lockscreen Wallpaper not changing issue in Windows 10"  |  https://www.youtube.com/watch?v=rZo4Ste8NfU
+#
+#		ss64.com  |  "ms-settings"  |  https://ss64.com/nt/syntax-settings.html
+#
+#		docs.microsoft.com  |  "Launch the Windows Settings app"  |  https://docs.microsoft.com/en-us/windows/uwp/launch-resume/launch-settings-app
+#
+# ------------------------------------------------------------

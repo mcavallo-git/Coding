7cf2c57e1490677c5d0b0b57254ef8ebec0b3d7b
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-29 07:39:34 -0400

    Update PowerShell - Get-ChildItem (+Where-Object + ForEach-Object).ps1

--- powershell/PowerShell - Get-ChildItem (+Where-Object + ForEach-Object).ps1
+++ powershell/PowerShell - Get-ChildItem (+Where-Object + ForEach-Object).ps1
@@ -14,17 +14,46 @@
 #
 # ------------------------------------------------------------
 #
-# Example 1 (Git oriented)
-#		Search for any git-repositories located within the ${HOME} directory (same as %USERPROFILE% on cmd)
-#		 |--> Syntax performs a lookup by beginning at the users ${HOME} directory and searching for files named "config" who have an immediate-parent directory named ".git"
+#		Example 1
+#			|--> Overview: Find files with multiple different levels of depth, parent-filenames, basenames, etc. matching multiple different criteria, all in one query
 #
-Get-ChildItem -Path "${HOME}" -Filter "config" -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Directory.Name -Eq ".git"} | Foreach-Object { $_.Directory.Parent.FullName; }
-#
+$Basename="*";
+$Parent_1="Settings"; # one step back (first directory name)
+$Parent_2="Microsoft.Windows.ContentDeliveryManager_*"; # another step back
+$Parent_X="${Env:USERPROFILE}\AppData\Local\Packages\"; # remaining steps-back to the root directory ("/" in linux, or the drive letter, such as "C:\", in Windows)
+(`
+Get-ChildItem `
+-Path ("$Parent_X") `
+-Depth (3) `
+-File `
+-Recurse `
+-Force `
+-ErrorAction "SilentlyContinue" `
+| Where-Object { $_.Directory.Parent.Name -Like "$Parent_2" } `
+| Where-Object { $_.Directory.Name -Like "$Parent_1" } `
+| Where-Object { $_.Name -Like "$Basename" } `
+| ForEach-Object { $_.FullName; } `
+);
+
+
+
 # ------------------------------------------------------------
 #
-#	Example 2 (GnuPG oriented)
-# 	Search the main drive for files named 'gpg.exe'
-#		 |--> Note: On windows devices, "/" resolves to the systemdrive (commonly "C:") with a backslash ("\") appended, by default
+#		Example 2
+#			|--> Overview: Search for any git-repositories located within the ${HOME} directory (same as %USERPROFILE% on cmd)
+#			|--> Note: Syntax performs a lookup by beginning at the users ${HOME} directory and searching for files named "config" who have an immediate-parent directory named ".git"
+#			|--> Use: Used for finding git-config files
+#
+Get-ChildItem -Path "${HOME}" -Filter "config" -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Directory.Name -Eq ".git"} | Foreach-Object { $_.Directory.Parent.FullName; }
+
+
+
+# ------------------------------------------------------------
+#
+#		Example 3
+# 		|--> Overview: Search the main drive for files named 'gpg.exe'
+#			|--> Note: On windows devices, "/" resolves to the systemdrive (commonly "C:") with a backslash ("\") appended, by default
+#			|--> Use: Used for syncing all GnuPG (gpg.exe) configs found on a given workstation, so that they all contain the same config-vals (... e.g. 'synced')
 #
 Get-ChildItem -Path "/" -Filter "gpg.exe" -File -Recurse -Force -ErrorAction "SilentlyContinue" | Foreach-Object { $_.FullName; }
 

d20f43fcd074c6a2615f758d0ca1965cb868dd4b
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-29 07:54:07 -0400

    Create PowerShell  - Lock-Screen Hotfix (background not changing to spotlight as-intended).ps1

---
+++ powershell/PowerShell  - Lock-Screen Hotfix (background not changing to spotlight as-intended).ps1
@@ -0,0 +1,34 @@
+
+# Clear the cache containing this user's lock-screen background config
+$Basename="*";
+$Parent_1="Settings"; # one step back (first directory name)
+$Parent_2="Microsoft.Windows.ContentDeliveryManager_*"; # another step back
+$Parent_X="${Env:USERPROFILE}\AppData\Local\Packages\"; # remaining steps-back to the root directory ("/" in linux, or the drive letter, such as "C:\", in Windows)
+(`
+Get-ChildItem `
+-Path ("$Parent_X") `
+-Depth (3) `
+-File `
+-Recurse `
+-Force `
+-ErrorAction "SilentlyContinue" `
+| Where-Object { $_.Directory.Parent.Name -Like "$Parent_2" } `
+| Where-Object { $_.Directory.Name -Like "$Parent_1" } `
+| Where-Object { $_.Name -Like "$Basename" } `
+| ForEach-Object { Remove-Item -Path ($_.FullName) -Force -Recurse; } `
+);
+
+# Open Win10's Settings
+start ms-settings:;
+
+# ------------------------------------------------------------
+#
+#	Citation(s)
+#
+#		youtube.com  |  "How to fix Lockscreen Wallpaper not changing issue in Windows 10"  |  https://www.youtube.com/watch?v=rZo4Ste8NfU
+#
+#		ss64.com  |  "ms-settings"  |  https://ss64.com/nt/syntax-settings.html
+#
+#		docs.microsoft.com  |  "Launch the Windows Settings app"  |  https://docs.microsoft.com/en-us/windows/uwp/launch-resume/launch-settings-app
+#
+# ------------------------------------------------------------

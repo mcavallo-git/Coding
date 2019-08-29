97e88b11446fe16cd688612b86781e2c87e4e115
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-29 09:01:30 -0400

    Create cmd - Reset_and_re-register_Windows_Spotlight.batb.bat

---
+++ cmd/cmd - Reset_and_re-register_Windows_Spotlight.batb.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+
+:: Reset Windows Spotlight
+DEL /F /S /Q /A "%USERPROFILE%/AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+DEL /F /S /Q /A "%USERPROFILE%/AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\Settings"
+
+:: Re-register Windows Spotlight
+PowerShell -ExecutionPolicy Unrestricted -Command "& {$manifest = (Get-AppxPackage *ContentDeliveryManager*).InstallLocation + '\AppxManifest.xml' ; Add-AppxPackage -DisableDevelopmentMode -Register $manifest}"
+
+
+
+REM	------------------------------------------------------------
+REM
+REM	Citation(s)
+REM
+REM		tenforums.com  |  "How to Reset and Re-register Windows Spotlight in Windows 10"  |  https://www.tenforums.com/tutorials/82156-reset-re-register-windows-spotlight-windows-10-a.html
+REM
+REM	------------------------------------------------------------

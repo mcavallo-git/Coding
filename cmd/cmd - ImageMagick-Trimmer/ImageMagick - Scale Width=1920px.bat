1eef0ab6355dc6f8fe6bf4798c92c2bfc58abf57
Matt Cavallo <mcavallo.work@gmail.com>
2019-08-30 18:12:14 -0400

    Create ImageMagick - Scale Width=1920px.bat

---
+++ cmd/cmd - ImageMagick-Trimmer/ImageMagick - Scale Width=1920px.bat
@@ -0,0 +1,26 @@
+@ECHO OFF
+REM		!!! Prerequisite !!! 
+REM			ImageMagick" must be installed for this script to function as-intended
+REM			https://www.imagemagick.org/script/download.php#windows
+ECHO.
+
+SET "INPUT_DIR=__TO_BE_TRIMMED"
+SET "OUTPUT_DIR=_FINISHED_ITEMS"
+
+SET ResizeToWidthPx=1920
+
+FOR %%I IN ("%INPUT_DIR%\*.jpg") DO (
+
+	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.jpg"
+
+	REM /***  Resize Image to %ResizeToWidthPx% Pixels in Width  ***/
+	magick "%%I" -resize %ResizeToWidthPx% "%OUTPUT_DIR%\%%~nI.jpg"
+
+	REM /***  DELETE INPUT IMAGE AFTER WE CONVERTED TO EXPORT IMAGE  ***/
+	DEL /f "%%I"
+
+	ECHO.
+
+)
+
+START explorer.exe "%OUTPUT_DIR%"

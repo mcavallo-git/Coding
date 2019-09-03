87580fc54f65d031eb63bf88f7e1ca1b62a4c604
Matt Cavallo <mcavallo.work@gmail.com>
2019-09-03 13:21:05 -0400

    Create Linux - tr (split string on given character).sh

---
+++ linux/Linux - tr (split string on given character).sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# tr - "translate or delete characters" (e.g. splits strings on a given character)
+# ------------------------------------------------------------
+
+
+
+for EACH_PATH in $(manpath | tr ":" "\n"); do echo -e "\n${EACH_PATH}"; ls -al "${EACH_PATH}"; done;
+
+
+
+# ------------------------------------------------------------
+#
+#	Citation(s)
+#
+#		stackoverflow.com  |  "How do I split a string on a delimiter in Bash?"  |  https://stackoverflow.com/a/918931
+#
+# ------------------------------------------------------------

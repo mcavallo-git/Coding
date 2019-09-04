fee0db8a813bc64e4018a879d5230d66abb7676d
Matt Cavallo <mcavallo.git@gmail.com>
2019-09-04 07:25:50 -0400

    Update PowerShell - Get-ItemProperty (registry keys, regedit).ps1

--- powershell/PowerShell - Get-ItemProperty (registry keys, regedit).ps1
+++ powershell/PowerShell - Get-ItemProperty (registry keys, regedit).ps1
@@ -10,11 +10,9 @@ $RegEdit = @{
 	Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Search";
 	Name="BingSearchEnabled";
 	Type="DWord";
-	Description="Enables (1) or Disables (0) Cortana's ability to send search-resutls to Bing.com";
-	Value="1";
+	Description="Enables (1) or Disables (0) Cortana's ability to send search-resutls to Bing.com. Set to '1' to resolve high-cpu bug from Windows-Update KB4512941 (2019-Sept).";
+	Value=1;
 };
-
-Get-ItemProperty -Path ($RegEdit.Path) -Name ($RegEdit.Name)
 
 If ((Test-Path -Path ($RegEdit.Path)) -Eq $True) {
 	Set-ItemProperty -Path ($RegEdit.Path) -Name ($RegEdit.Name) -Value ($RegEdit.Value);
@@ -24,3 +22,10 @@ If ((Test-Path -Path ($RegEdit.Path)) -Eq $True) {
 
 Get-ItemProperty -Path ($RegEdit.Path) -Name ($RegEdit.Name)
 
+# ------------------------------------------------------------
+#
+#	Citation(s)
+#
+#		ss64.com  |  "New-ItemProperty - Set a new property, for an item at a given location"  |  https://ss64.com/ps/new-itemproperty.html
+#
+# ------------------------------------------------------------

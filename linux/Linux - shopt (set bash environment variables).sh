9c98b7954c1e51dd49e5f4ceaaa77fa67b37b3c9
Matt Cavallo <mcavallo.work@gmail.com>
2019-09-10 14:35:02 -0400

    Update Linux - shopt (set bash environment variables).sh

--- linux/Linux - shopt (set bash environment variables).sh
+++ linux/Linux - shopt (set bash environment variables).sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
 # Linux - shopt
-#			nocasematch     Bash matches patterns in a case-insensitive fashion when performing matching while executing case or [[ conditional commands.
 #
 
-shopt -s nocasematch;
+
+shopt -s nocasematch; # If set, Bash matches patterns in a case-insensitive fashion when performing matching while executing case or [[ conditional commands, when performing pattern substitution word expansions, or when filtering possible completions as part of programmable completion.
+
+
+shopt -s globstar; # If set, the pattern ‘**’ used in a filename expansion context will match all files and zero or more directories and subdirectories. If the pattern is followed by a ‘/’, only directories and subdirectories match.
 
 
 
+# ------------------------------------------------------------
 #
 # Citation(s)
 # 	
@@ -15,3 +19,4 @@ shopt -s nocasematch;
 #
 # 	ss64.com, "shopt Man Page - Linux - SS64.com", https://ss64.com/bash/shopt.html
 #
+# ------------------------------------------------------------

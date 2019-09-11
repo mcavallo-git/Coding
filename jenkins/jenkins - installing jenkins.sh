605f6520d61e40e499d412836224dc6079b64a04
Matt Cavallo <mcavallo.work@gmail.com>
2019-09-11 15:24:18 -0400

    Create jenkins - installing jenkins.sh

---
+++ jenkins/jenkins - installing jenkins.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 
+docker run \
+-u root \
+--rm \
+-d \
+-p 8080:8080 \
+-p 50000:50000 \
+-v "jenkins-data:/var/jenkins_home" \
+-v "/var/run/docker.sock:/var/run/docker.sock" \
+"jenkinsci/blueocean" \
+;
+
+# ------------------------------------------------------------
+
+# Bash into docker-instance
+JENKINS_DOCKER_NAME=$(docker container ls --format '{{.Names}}') && \
+E1="LINES=$(tput lines)" && \
+E2="COLUMNS=$(tput cols)" && \
+docker exec \
+-e "${E1}" \
+-e "${E2}" \
+-it \
+"${JENKINS_DOCKER_NAME}" \
+"/bin/bash" \
+;
+
+# ------------------------------------------------------------
+#	
+#	Citation(s)
+#
+#		jenkins.io  |  "Installing Jenkins"  |  https://jenkins.io/doc/book/installing/
+#
+#		hub.docker.com  |  "jenkinsci/blueocean"  |  https://hub.docker.com/r/jenkinsci/blueocean/
+#
+# ------------------------------------------------------------

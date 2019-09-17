41be9063ae8e07e496c5a1ce012528e21efd5a85
Matt Cavallo <mcavallo.git@gmail.com>
2019-09-17 11:56:10 -0400

    Update Linux - background tasks, delayed parallel runtimes (via parenthesis () blocking, sleep delaying, and ampersand & parellel background tasks).sh

--- linux/Linux - background tasks, delayed parallel runtimes (via parenthesis () blocking, sleep delaying, and ampersand & parellel background tasks).sh
+++ linux/Linux - background tasks, delayed parallel runtimes (via parenthesis () blocking, sleep delaying, and ampersand & parellel background tasks).sh
@@ -11,3 +11,19 @@ TEST_OUTPUT="${HOME}/tester_$(date +'%s')";
 sleep 2 && cat "${TEST_OUTPUT}"; \
 sleep 2 && cat "${TEST_OUTPUT}"; \
 sleep 2 && cat "${TEST_OUTPUT}";
+
+
+# ------------------------------------------------------------
+#
+# DELAYED REBOOT
+#
+
+## Reboot @ 11:59 PM
+sudo shutdown -r 23:59 &
+
+
+## Reboot in 1 minute
+sudo shutdown --reboot "+1" &
+
+
+# ------------------------------------------------------------

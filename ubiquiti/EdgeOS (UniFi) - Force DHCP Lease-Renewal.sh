8ffc56beba39b417ee1ba791d868127d27872068
Matt Cavallo <mcavallo.git@gmail.com>
2019-09-17 07:42:40 -0400

    Update EdgeOS (UniFi) - Force DHCP Lease-Renewal.sh

--- ubiquiti/EdgeOS (UniFi) - Force DHCP Lease-Renewal.sh
+++ ubiquiti/EdgeOS (UniFi) - Force DHCP Lease-Renewal.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
-
-# Ubnt (ERLite-3) - Force DHCP Lease-Renewal
-
+# ------------------------------------------------------------
+# 
+# ERLite-3  :::  Force DHCP Lease-Renewal
+# 
 sudo /opt/vyatta/bin/sudo-users/vyatta-clear-dhcp-lease.pl --lip=all
 
 
-# UniFi (USG-3P) - Force DHCP Lease-Renewal
+# ------------------------------------------------------------
+#
+# USG-3P (Unifi)  :::  Show DHCP Leases
+#
+show dhcp leases;
 
+# ------------------------------------------------------------
+#
+# USG-3P (Unifi)  :::  Clear DHCP Lease(s)
+#
+clear dhcp leases; # Clear all leases from DHCP
+
+IP_RELEASE_DHCP="192.168.1.100" && clear dhcp lease ip ${IP_RELEASE_DHCP}; # Clear one, specific device/ip's lease from DHCP
+
+
+
+# ------------------------------------------------------------
+#
+# USG-3P (Unifi)  :::  Force DHCP Lease-Renewal
+#
 sudo /opt/vyatta/bin/sudo-users/vyatta-clear-dhcp-lease.pl --lip=all --ilfile=/config/dhcpd.leases --olfile=/config/dhcpd.leases --pidf=/run/dhcpd.pid
 
+
+# ------------------------------------------------------------
+#
+#	Citation(s)
+#
+# 	community.ui.com  |  "Clear DHCP Lease not working??"  |  https://community.ui.com/questions/Clear-DHCP-Lease-not-working/65494178-3e41-445f-9da5-8ed89b0a993a#answer/dbf70efc-88ed-4980-be46-d173fcc729e4
+#
+# ------------------------------------------------------------

98718da332a58e912ab33b762fcc749487117a3c
Matt Cavallo <mcavallo.work@gmail.com>
2019-09-20 13:40:57 -0400

    Update cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat

--- cmd/cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat
+++ cmd/cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat
@@ -2,13 +2,13 @@
 REM ------------------------------------------------------------
 REM  IPv4 LAN Connection(s) - Disconnect-Flush-Connect-Register
 
-ECHO "Calling [ ipconfig /release ] ..."
+ECHO Calling [ ipconfig /release ] ...
 ipconfig /release
-ECHO "Calling [ ipconfig /registerdns ] ..."
+ECHO Calling [ ipconfig /registerdns ] ...
 ipconfig /registerdns
-ECHO "Calling [ ipconfig /flushdns ] ..."
+ECHO Calling [ ipconfig /flushdns ] ...
 ipconfig /flushdns
-ECHO "Calling [ ipconfig /renew ] ..."
+ECHO Calling [ ipconfig /renew ] ...
 ipconfig /renew
 
 TIMEOUT -T 30
@@ -18,13 +18,13 @@ EXIT
 REM ------------------------------------------------------------
 REM  IPv6 LAN Connection(s) - Disconnect-Flush-Connect-Register
 
-ECHO "Calling [ ipconfig /release6 ] ..."
+ECHO Calling [ ipconfig /release6 ] ...
 ipconfig /release6
-ECHO "Calling [ ipconfig /registerdns ] ..."
+ECHO Calling [ ipconfig /registerdns ] ...
 ipconfig /registerdns
-ECHO "Calling [ ipconfig /flushdns ] ..."
+ECHO Calling [ ipconfig /flushdns ] ...
 ipconfig /flushdns
-ECHO "Calling [ ipconfig /renew6 ] ..."
+ECHO Calling [ ipconfig /renew6 ] ...
 ipconfig /renew6
 
 TIMEOUT -T 30

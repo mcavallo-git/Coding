310aa0f31cd4c2c8a55f5a3284441d539943288d
Matt Cavallo <mcavallo.work@gmail.com>
2019-09-20 16:13:12 -0400

    Update Linux - ufw.sh

--- linux/Linux - ufw.sh
+++ linux/Linux - ufw.sh
@@ -32,13 +32,32 @@ ufw delete RULE_ID;  # Delete UFW rules by-ID
 
 # ------------------------------------------------------------
 #
+# UFW | Allow Pings (ICMP Echo Requests) over IPv6
+#  |
+#  |--> UFW only allows specifying icmp rules via rule-based files (which are styled simlar-to iptables-restore files)
+#        |--> UFW's rule-based files may be found-in:   /etc/ufw 
+#
+
+cat /etc/ufw/before.rules | grep -v '^$' | grep -v '^#';
+
+#
+# To allow Pings (ICMP Echo Requests) over IPv6, make sure the following line exists in "/etc/ufw/before.rules"
+#
+-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT
+
+
+
+# ------------------------------------------------------------
+#
 # Citation(s)
 #
-#		Documentation
-#			--> https://manpages.ubuntu.com/manpages/trusty/man8/ufw.8.html
 #
-#		Forum
-#			--> https://www.digitalocean.com/community/tutorials/ufw-essentials-common-firewall-rules-and-commands
-#			--> https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-18-04
+#		digitalocean.com  |  "UFW Essentials: Common Firewall Rules and Commands"  |  https://www.digitalocean.com/community/tutorials/ufw-essentials-common-firewall-rules-and-commands
+#
+#		digitalocean.com  |  "How To Set Up a Firewall with UFW on Ubuntu 18.04"  |  https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-18-04
+#
+#		askubuntu.com  |  "How to enable ufw firewall to allow icmp response?"  |  https://askubuntu.com/a/10314
+#
+#		manpages.ubuntu.com  |  "Ubuntu manuals - NAME"  |  https://manpages.ubuntu.com/manpages/trusty/man8/ufw.8.html
 #
 # ------------------------------------------------------------

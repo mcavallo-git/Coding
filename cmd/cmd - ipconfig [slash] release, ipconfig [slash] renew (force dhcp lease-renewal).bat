f1328b26321abc03de70aec75adc0189049b5d41
Matt Cavallo <mcavallo.work@gmail.com>
2019-09-20 13:38:54 -0400

    Update cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat

--- cmd/cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat
+++ cmd/cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat
@@ -1,3 +1,20 @@
 @ECHO OFF
+REM ------------------------------------------------------------
+REM  IPv4 LAN Connection(s) - Disconnect-Flush-Connect-Register
+
 ipconfig /release
+ipconfig /registerdns
+ipconfig /flushdns
 ipconfig /renew
+
+EXIT
+
+REM ------------------------------------------------------------
+REM  IPv6 LAN Connection(s) - Disconnect-Flush-Connect-Register
+
+ipconfig /release6
+ipconfig /flushdns
+ipconfig /renew6
+ipconfig /registerdns
+
+REM ------------------------------------------------------------

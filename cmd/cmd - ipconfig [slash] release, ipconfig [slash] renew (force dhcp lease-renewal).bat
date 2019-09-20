5324edd3a59683a22117feb8bbc16879cf6969ee
Matt Cavallo <mcavallo.work@gmail.com>
2019-09-20 13:40:25 -0400

    Update cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat

--- cmd/cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat
+++ cmd/cmd - ipconfig [slash] release, ipconfig [slash] renew (force dhcp lease-renewal).bat
@@ -2,19 +2,31 @@
 REM ------------------------------------------------------------
 REM  IPv4 LAN Connection(s) - Disconnect-Flush-Connect-Register
 
+ECHO "Calling [ ipconfig /release ] ..."
 ipconfig /release
+ECHO "Calling [ ipconfig /registerdns ] ..."
 ipconfig /registerdns
+ECHO "Calling [ ipconfig /flushdns ] ..."
 ipconfig /flushdns
+ECHO "Calling [ ipconfig /renew ] ..."
 ipconfig /renew
+
+TIMEOUT -T 30
 
 EXIT
 
 REM ------------------------------------------------------------
 REM  IPv6 LAN Connection(s) - Disconnect-Flush-Connect-Register
 
+ECHO "Calling [ ipconfig /release6 ] ..."
 ipconfig /release6
-ipconfig /flushdns
-ipconfig /renew6
+ECHO "Calling [ ipconfig /registerdns ] ..."
 ipconfig /registerdns
+ECHO "Calling [ ipconfig /flushdns ] ..."
+ipconfig /flushdns
+ECHO "Calling [ ipconfig /renew6 ] ..."
+ipconfig /renew6
+
+TIMEOUT -T 30
 
 REM ------------------------------------------------------------

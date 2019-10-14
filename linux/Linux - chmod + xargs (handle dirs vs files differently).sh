8a3b9d4dba9a05d2dbb5fb9ce267a2007e31f097
Matt Cavallo <mcavallo.work@gmail.com>
2019-10-14 14:05:58 -0500

    Update Linux - chmod + xargs (handle dirs vs files differently).sh

--- linux/Linux - chmod + xargs (handle dirs vs files differently).sh
+++ linux/Linux - chmod + xargs (handle dirs vs files differently).sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 
+# ------------------------------------------------------------
 # chown → FASTER Method (piping find's results into xargs)
 
 find "/var/www/html" -type d -print0 | xargs -0 chmod 0775; # directories
@@ -8,9 +9,12 @@ find "/var/www/html" -type d -print0 | xargs -0 chmod 0775; # directories
 find "/var/www/html" -type f -print0 | xargs -0 chmod 0664; # files
 
 
-
+# ------------------------------------------------------------
 # chown → SLOWER Method (built-in "-exec" find method)
 
 # find "/var/www/html" -type d -exec chmod 0755 '{}' \; # directories
 
 # find "/var/www/html" -type f -exec chmod 0644 '{}' \; # files
+
+
+# ------------------------------------------------------------

c262a816fb2d4763bc4b0cabee32b31a4ca18f8f
Matt Cavallo <mcavallo.work@gmail.com>
2019-10-15 09:15:16 -0500

    Update PowerShell - Get-WindowsOptionalFeature, Enable-WindowsOptionalFeature, Disable-WindowsOptionalFeature.ps1

--- powershell/PowerShell - Get-WindowsOptionalFeature, Enable-WindowsOptionalFeature, Disable-WindowsOptionalFeature.ps1
+++ powershell/PowerShell - Get-WindowsOptionalFeature, Enable-WindowsOptionalFeature, Disable-WindowsOptionalFeature.ps1
@@ -5,5 +5,23 @@
 #
 
 Get-WindowsOptionalFeature
+
 Enable-WindowsOptionalFeature
+
 Disable-WindowsOptionalFeature
+
+
+
+
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#		docs.microsoft.com  |  "Get-WindowsOptionalFeature"  |  https://docs.microsoft.com/en-us/powershell/module/dism/get-windowsoptionalfeature?view=win10-ps
+#
+#		docs.microsoft.com  |  "Enable-WindowsOptionalFeature"  |  https://docs.microsoft.com/en-us/powershell/module/dism/enable-windowsoptionalfeature?view=win10-ps
+#
+#		docs.microsoft.com  |  "Disable-WindowsOptionalFeature"  |  https://docs.microsoft.com/en-us/powershell/module/dism/disable-windowsoptionalfeature?view=win10-ps
+#
+# ------------------------------------------------------------

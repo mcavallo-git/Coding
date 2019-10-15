f1a5596b63d7ad907d23b198b7a903b88deb664d
Matt Cavallo <mcavallo.work@gmail.com>
2019-10-15 09:35:12 -0500

    Update PowerShell - Get-WindowsOptionalFeature, Enable-WindowsOptionalFeature, Disable-WindowsOptionalFeature.ps1

--- powershell/PowerShell - Get-WindowsOptionalFeature, Enable-WindowsOptionalFeature, Disable-WindowsOptionalFeature.ps1
+++ powershell/PowerShell - Get-WindowsOptionalFeature, Enable-WindowsOptionalFeature, Disable-WindowsOptionalFeature.ps1
@@ -8,7 +8,11 @@
 
 Get-WindowsOptionalFeature -Online
 
-PrivilegeEscalation -Command ("Get-WindowsOptionalFeature -Online | Sort | Format-Table > '${ENV:USERPROFILE}\Desktop\Get-WindowsOptionalFeature.txt'");
+# PrivilegeEscalation -Command ("Get-WindowsOptionalFeature -Online | Sort | Format-Table > '${ENV:USERPROFILE}\Desktop\Get-WindowsOptionalFeature.txt'");
+
+Get-WindowsOptionalFeature -Online | Sort | Format-Table > "${ENV:USERPROFILE}\Desktop\Get-WindowsOptionalFeature.txt";
+
+DISM /Online /Get-Features /Format:Table | More > "${ENV:USERPROFILE}\Desktop\DISM Online Get-Features.txt";
 
 
 # ------------------------------------------------------------
@@ -29,5 +33,7 @@ Disable-WindowsOptionalFeature
 #		docs.microsoft.com  |  "Enable-WindowsOptionalFeature"  |  https://docs.microsoft.com/en-us/powershell/module/dism/enable-windowsoptionalfeature?view=win10-ps
 #
 #		docs.microsoft.com  |  "Disable-WindowsOptionalFeature"  |  https://docs.microsoft.com/en-us/powershell/module/dism/disable-windowsoptionalfeature?view=win10-ps
+#
+#		docs.microsoft.com  |  "DISM Global Options for Command-Line Syntax"  |  https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/dism-global-options-for-command-line-syntax
 #
 # ------------------------------------------------------------

b374513d2242f56007e7c3b80e6178e056185782
Matt Cavallo <mcavallo.git@gmail.com>
2019-10-20 09:59:37 -0500

    Update SyncRegistry.psm1

--- powershell/_WindowsPowerShell/Modules/SyncRegistry/SyncRegistry.psm1
+++ powershell/_WindowsPowerShell/Modules/SyncRegistry/SyncRegistry.psm1
@@ -166,7 +166,8 @@ function SyncRegistry {
 					}
 					If ($Each_RootKey_Name -ne $Null) {
 						# New-PSDrive -Name "${Each_RootKey_Acronym}" -PSProvider "Registry" -Root "${Each_RootKey_Name}";
-						New-PSDrive -Verbose -Name "${Each_RootKey_Acronym}" -PSProvider "Registry" -Root "${Each_RootKey_Name}";
+						# New-PSDrive -Verbose -Name "${Each_RootKey_Acronym}" -PSProvider "Registry" -Root "${Each_RootKey_Name}";
+						New-PSDrive -Quiet -Name "${Each_RootKey_Acronym}" -PSProvider "Registry" -Root "${Each_RootKey_Name}";
 					}
 				}
 				

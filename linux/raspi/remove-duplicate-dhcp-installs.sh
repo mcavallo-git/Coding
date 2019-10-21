9541c46fbc00daa76eb3eb2eba0373854d50ac39
Matt Cavallo <mcavallo.git@gmail.com>
2019-10-21 02:44:42 -0500

    Update remove-duplicate-dhcp-installs.sh

--- linux/raspi/remove-duplicate-dhcp-installs.sh
+++ linux/raspi/remove-duplicate-dhcp-installs.sh
@@ -1,16 +1,19 @@
 #!/bin/bash 
-
+#
+# ------------------------------------------------------------
 # Error Message received:
 #		"Failed to start dhcpd on all interfaces"
 
 # Reason: Multiple DHCP services installed
 
-sudo dpkg -l | grep dhcp; # Show all DHCP Services
+sudo dpkg -l | grep -i 'dhcp' | grep -i 'client'; # Show all client-based DHCP services
 
 sudo apt-get remove -y dhcpcd5; # Remove unwanted DHCP service (see citation for reasoning)
 
+# ------------------------------------------------------------
 # 
 #	Citation(s)
 #
 #	Thanks to StackExchange user [ Margin Gamza ] on forum [ https://superuser.com/a/943896 ]
 #
+# ------------------------------------------------------------

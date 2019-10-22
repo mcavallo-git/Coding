7f612b840f2eb263bf4138a64ad4576b71dd7761
Matt Cavallo <mcavallo.git@gmail.com>
2019-10-21 21:01:30 -0500

    Update Windows - Unifi Install as a Service.bat

--- ubiquiti/Windows - Unifi Install as a Service.bat
+++ ubiquiti/Windows - Unifi Install as a Service.bat
@@ -1,15 +1,19 @@
 @ECHO OFF
 
-REM 
-REM Install Java
-REM   |
-REM   |--> Updated Installer - Java SE (Standard Edition, Win10) --> https://www.java.com/en/download/win10.jsp
-
-REM Install Unifi Controller
-REM   |
-REM   |--> Updated Installer - UniFi Controller for Windows --> https://www.ui.com/download/unifi
-
-REM Open a Command Prompt (CMD.exe) as admin & run the following commands:
+REM ------------------------------------------------------------
+REM Install "Java SE"
+REM   |--> https://www.java.com/en/download/win10.jsp
+REM
+REM ------------------------------------------------------------
+REM Install "UniFi Controller for Windows"
+REM   |--> https://www.ui.com/download/unifi
+REM
+REM ------------------------------------------------------------
+REM Run Java Jar-File as a Service (RUN AS ADMIN IN CMD)
+REM   |--> Hit Start on your Keyboard
+REM    |--> Type "cmd"
+REM     |--> (Combo keyboard+mouse press) Ctrl + Shift + Left-Click the returned "cmd.exe" in the start menu to "Run as Admin"
+REM      |--> Copy/Paste the following commands, one-by-one
 
 cd "%USERPROFILE%\Ubiquiti UniFi\"
 
@@ -17,12 +21,14 @@ java -jar lib\ace.jar installsvc
 
 java -jar lib\ace.jar startsvc
 
+REM ------------------------------------------------------------
+REM Verify Working State
+REM   |--> Restart machine (to get on a fresh slate, or if you want yo call it simulating the havoc of day-to-day use, that's fine, too)
+REM    |--> After reboot, log back in and make sure not to open the Unifi controller software
+REM     |--> Also, check taskbar/notification area to make sure you cant see it running (it should be running as a background service any time the PC is on)
+REM      |--> Browse to the webpage https://localhost:8443/
+REM       |--> If the page loads the unifi controller, then you have configured the service as-intended
 REM
-REM Verify completion by restarting machine, making sure you don't see the software running after you login (also check taskbar/notification area), then browse to https://localhost:8443/
-REM   |
-REM   |--> If the page loads the unifi controller, then you have configured the service as-intended
-REM
-
 REM ------------------------------------------------------------
 REM Citation(s)
 REM

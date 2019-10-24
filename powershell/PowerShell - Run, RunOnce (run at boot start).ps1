3b64053392ec5d174683257f7c35f09493368124
Matt Cavallo <mcavallo.work@gmail.com>
2019-10-24 16:10:26 -0500

    Create PowerShell - Run, RunOnce (run at boot start).ps1

---
+++ powershell/PowerShell - Run, RunOnce (run at boot start).ps1
@@ -0,0 +1,7 @@
+
+HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run
+HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
+HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce
+HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce
+
+https://docs.microsoft.com/en-us/windows/win32/setupapi/run-and-runonce-registry-keys

acfd1aa0af9aed9afc39bc2761c1bc82a7d9191a
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-03 23:19:43 -0600

    Update sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -1,17 +1,29 @@
+<# Start-Process PowerShell.exe $(New-Object Net.WebClient).DownloadString('https://sync.mcavallo.com/ps') -Verb RunAs; #>
+
 Set-ExecutionPolicy "RemoteSigned" -Force;
 
 Write-Host "Info : Syncing local git repository to origin `"https://github.com/mcavallo-git/Coding.git`"..." -ForegroundColor Green;
 
-If (Test-Path "~/Coding") {
-	Set-Location "~/Coding";
+If ( ${HOME} -Eq ${Null} ) {
+	$HOME = ((Resolve-Path "~").Path);
+}
+
+If (Test-Path "${HOME}/Coding") {
+	Set-Location "${HOME}/Coding";
 	git reset --hard "origin/master";
 	git pull;
 } Else {
-	Set-Location "~"; 
+	Set-Location "${HOME}"; 
 	git clone "https://github.com/mcavallo-git/Coding.git";
-
+	$SSH_KEY_REMOTE="https://raw.githubusercontent.com/mcavallo-git/Coding/master/.shared-deploy-key.pem";
+	$SSH_KEY_LOCAL="${HOME}/Coding/.git/.shared-deploy-key.pem";
+	New-Item -ItemType "File" -Path ("${SSH_KEY_LOCAL}") -Value ($(New-Object Net.WebClient).DownloadString("${SSH_KEY_REMOTE}")) | Out-Null;
+	Set-Location "${HOME}/Coding";
+	git config --local --replace-all "core.sshcommand" "ssh -i `"${SSH_KEY_LOCAL}`"";
+	# git config --local --replace-all "user.name" "${Env:USERNAME}@${Env:COMPUTERNAME}";
+	# git config --local --replace-all "user.email" "email@email.email";
 }
 
-. "~/Coding/powershell/_WindowsPowerShell/Modules/ImportModules.ps1";
+. "${HOME}/Coding/powershell/_WindowsPowerShell/Modules/ImportModules.ps1";
 
 Write-Host "`nInfo: PowerShell Modules Synchronized`n" -ForegroundColor Cyan;

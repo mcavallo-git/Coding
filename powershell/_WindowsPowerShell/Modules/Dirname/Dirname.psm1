b05caf17bc151fae2aba50459dd2f0ed5f6684a4
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-03 19:34:58 -0600

    Create Dirname.psm1

---
+++ powershell/_WindowsPowerShell/Modules/Dirname/Dirname.psm1
@@ -0,0 +1,22 @@
+function Dirname {
+	Param(
+		[Parameter(Position=0, ValueFromRemainingArguments)]
+		$InputPath
+	)
+
+	$Dirname = ([System.IO.Path]::GetDirectoryName(${InputPath}));
+
+	Write-Output (${Dirname});
+
+	Return;
+
+}
+Export-ModuleMember -Function "Dirname";
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Path Class - Performs operations on String instances that contain file or directory path information"  |  https://docs.microsoft.com/en-us/dotnet/api/system.io.path?view=netframework-4.8
+#
+# ------------------------------------------------------------

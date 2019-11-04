c64d49eec369b51dcf000694379c82ca9f1252c9
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-03 21:57:19 -0600

    Update sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -1,5 +1,7 @@
 Set-ExecutionPolicy "RemoteSigned" -Force;
+
 Write-Host "Task - Sync local git repository to origin `"https://github.com/mcavallo-git/Coding.git`"..." -ForegroundColor Green;
+
 If (Test-Path "~/Coding") {
 	Set-Location "~/Coding";
 	git reset --hard "origin/master";
@@ -8,5 +10,7 @@ If (Test-Path "~/Coding") {
 	Set-Location "~"; 
 	git clone "https://github.com/mcavallo-git/Coding.git";
 }
+
 . "~/Coding/powershell/_WindowsPowerShell/Modules/ImportModules.ps1";
+
 Write-Host "`nPass - PowerShell Modules Synchronized`n" -ForegroundColor Cyan;

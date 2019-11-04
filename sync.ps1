2678dfbd5c03270ae93bff6b15f7a027f40b2cc3
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-04 00:08:28 -0600

    Update sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -20,19 +20,25 @@ If ( ${HOME} -Eq ${Null} ) {
 }
 
 If (Test-Path "${HOME}/Coding") {
+
 	Set-Location "${HOME}/Coding";
 	git reset --hard "origin/master";
 	git pull;
+
 } Else {
-	Set-Location "${HOME}"; 
+
+	Set-Location "${HOME}";
 	git clone "https://github.com/mcavallo-git/Coding.git";
+
 	$SSH_KEY_REMOTE="https://raw.githubusercontent.com/mcavallo-git/Coding/master/.shared-deploy-key.pem";
 	$SSH_KEY_LOCAL="${HOME}/Coding/.git/.shared-deploy-key.pem";
 	New-Item -ItemType "File" -Path ("${SSH_KEY_LOCAL}") -Value ($(New-Object Net.WebClient).DownloadString("${SSH_KEY_REMOTE}")) | Out-Null;
+
 	Set-Location "${HOME}/Coding";
 	git config --local --replace-all "core.sshcommand" "ssh -i `"${SSH_KEY_LOCAL}`"";
 	# git config --local --replace-all "user.name" "${Env:USERNAME}@${Env:COMPUTERNAME}";
 	# git config --local --replace-all "user.email" "email@email.email";
+
 }
 
 . "${HOME}/Coding/powershell/_WindowsPowerShell/Modules/ImportModules.ps1";

1996d0dfa03802beaf82617c2c8c8cbf94a3fa8a
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-04 02:43:18 -0600

    Update PowerShell - Get-AppxPackage (run win10 windows-store appications using built-in shell).ps1

--- powershell/PowerShell - Get-AppxPackage (run win10 windows-store appications using built-in shell).ps1
+++ powershell/PowerShell - Get-AppxPackage (run win10 windows-store appications using built-in shell).ps1
@@ -1,1 +1,10 @@
 $APP_NAME="Lockscreenaswallpaper"; (Get-AppxPackage).Name | Select-String "${APP_NAME}";
+
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#  stackoverflow.com  |  "How to Start a Universal Windows App (UWP) from PowerShell in Windows 10?"  |  https://stackoverflow.com/a/48856168
+#
+# ------------------------------------------------------------

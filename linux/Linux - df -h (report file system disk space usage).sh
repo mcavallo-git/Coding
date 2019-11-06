1835adb34d2d714af63417fc33a646080ddb7fdd
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-06 04:01:15 -0600

    Update Linux - df -h (report file system disk space usage).sh

--- linux/Linux - df -h (report file system disk space usage).sh
+++ linux/Linux - df -h (report file system disk space usage).sh
@@ -11,15 +11,15 @@ $ df -h --output="pcent" | sed '1!G;h;$!d' | head -n -1 | sed '1!G;h;$!d' | sort
 
 # df --output="KEY";  # VAL  (associated column-header)
 
-df -h --output="source";  # "Filesystem
-df -h --output="fstype";  # "Type
-df -h --output="itotal";  # "Inodes
-df -h --output="iused";  # "IUsed
-df -h --output="iavail";  # "IFree
-df -h --output="ipcent";  # "IUse%
-df -h --output="size";  # "Size
-df -h --output="used";  # "Used
-df -h --output="avail";  # "Avail
-df -h --output="pcent";  # "Use%
-df -h --output="file";  # "File
-df -h --output="target";  # "Mounted on
+df -h --output="source";  # "Filesystem"
+df -h --output="fstype";  # "Type"
+df -h --output="itotal";  # "Inodes"
+df -h --output="iused";  # "IUsed"
+df -h --output="iavail";  # "IFree"
+df -h --output="ipcent";  # "IUse%"
+df -h --output="size";  # "Size"
+df -h --output="used";  # "Used"
+df -h --output="avail";  # "Avail"
+df -h --output="pcent";  # "Use%"
+df -h --output="file";  # "File"
+df -h --output="target";  # "Mounted on"

f698b3f65ce0d37544858a7c9f33f1e24cb8e78f
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-06 03:49:28 -0600

    Update Linux - df -h (report file system disk space usage).sh

--- linux/Linux - df -h (report file system disk space usage).sh
+++ linux/Linux - df -h (report file system disk space usage).sh
@@ -1,2 +1,22 @@
+#!/bin/bash
 
-df -h --output="source" | grep -v '^Filesystem'
+# df -h --output="source" | grep -v '^Filesystem'
+
+# Slice the top line off-of the output
+df -h --output="source" | sed '1!G;h;$!d' | head -n -1 | sed '1!G;h;$!d'; 
+
+
+# df --output="KEY";  # VAL  (associated column-header)
+
+df -h --output="source";  # "Filesystem
+df -h --output="fstype";  # "Type
+df -h --output="itotal";  # "Inodes
+df -h --output="iused";  # "IUsed
+df -h --output="iavail";  # "IFree
+df -h --output="ipcent";  # "IUse%
+df -h --output="size";  # "Size
+df -h --output="used";  # "Used
+df -h --output="avail";  # "Avail
+df -h --output="pcent";  # "Use%
+df -h --output="file";  # "File
+df -h --output="target";  # "Mounted on

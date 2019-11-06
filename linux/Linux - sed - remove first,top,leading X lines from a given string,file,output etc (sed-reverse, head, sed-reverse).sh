aa32d926a6e542f14038e8f4bf03fd6de8b55a1a
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-06 03:34:06 -0600

    Create Linux - sed - remove first,top,leading X lines from a given string,file,output etc (sed-reverse, head, sed-reverse).sh

---
+++ linux/Linux - sed - remove first,top,leading X lines from a given string,file,output etc (sed-reverse, head, sed-reverse).sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# Remove first-top-leading X line from string, output, etc (sed-reverse, head, sed-reverse)
+#
+
+SED_REVERSE_METHOD1='1!G;h;$!d'; # use with [ sed ${SED_REVERSE_METHOD1} ]
+SED_REVERSE_METHOD2='1!G;h;$p'; # use with  [ sed -n ${SED_REVERSE_METHOD2} ]
+TOP_LINES_TO_SLICE=5;
+
+TEST_STR="1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15"; \
+echo "\${TEST_STR}, as-is (not-reversed):"; echo -e "${TEST_STR}"; \
+echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD1):"; echo -e "${TEST_STR}" | sed "${SED_REVERSE_METHOD1}" | head -n -${TOP_LINES_TO_SLICE} | sed "${SED_REVERSE_METHOD1}"; echo -e "\n";
+echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD2):"; echo -e "${TEST_STR}" | sed -n "${SED_REVERSE_METHOD2}" | head -n -${TOP_LINES_TO_SLICE} | sed -n "${SED_REVERSE_METHOD2}"; echo -e "\n";
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+# 	stackoverflow.com  |  "How can I reverse the order of lines in a file?"  |  https://stackoverflow.com/a/744093
+#
+# ------------------------------------------------------------

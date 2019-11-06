8e81b7776f81d7b1129d9db741a57e1d077408ab
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-06 03:42:37 -0600

    Update Linux - sed - remove first,top,leading X lines from a given string,file,output etc (sed-reverse, head, sed-reverse).sh

--- linux/Linux - sed - remove first,top,leading X lines from a given string,file,output etc (sed-reverse, head, sed-reverse).sh
+++ linux/Linux - sed - remove first,top,leading X lines from a given string,file,output etc (sed-reverse, head, sed-reverse).sh
@@ -1,16 +1,30 @@
 #!/bin/sh
 #
-# Remove first-top-leading X line from string, output, etc (sed-reverse, head, sed-reverse)
+# Remove top 1 line from piped output
 #
 
+df -h | sed '1!G;h;$!d' | head -n -1 | sed '1!G;h;$!d'; 
+
+
+
+#
+# Remove top X lines from piped output
+#
 SED_REVERSE_METHOD1='1!G;h;$!d';  # use with  [ sed '1!G;h;$!d' ]
 SED_REVERSE_METHOD2='1!G;h;$p';   # use with  [ sed -n '1!G;h;$p' ]
 TOP_LINES_TO_SLICE=5;
 
 TEST_STR="1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15"; \
 echo "\${TEST_STR}, as-is (not-reversed):"; echo -e "${TEST_STR}"; \
-echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD1):"; echo -e "${TEST_STR}" | sed "${SED_REVERSE_METHOD1}" | head -n -${TOP_LINES_TO_SLICE} | sed "${SED_REVERSE_METHOD1}"; echo -e "\n"; \
-echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD2):"; echo -e "${TEST_STR}" | sed -n "${SED_REVERSE_METHOD2}" | head -n -${TOP_LINES_TO_SLICE} | sed -n "${SED_REVERSE_METHOD2}"; echo -e "\n";
+ \
+echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD1):";\
+echo -e "${TEST_STR}" | sed "${SED_REVERSE_METHOD1}" | head -n -${TOP_LINES_TO_SLICE} | sed "${SED_REVERSE_METHOD1}"; \
+echo -e "\n"; \
+ \
+echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD2):"; \
+echo -e "${TEST_STR}" | sed -n "${SED_REVERSE_METHOD2}" | head -n -${TOP_LINES_TO_SLICE} | sed -n "${SED_REVERSE_METHOD2}"; echo -e "\n";
+
+
 
 # ------------------------------------------------------------
 #

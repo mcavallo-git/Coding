2fcb8408c62749f20205a74bfe5843936eadf7f7
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-07 16:37:04 -0600

    Update Linux - sed - parse string (e.g. file) contents via regex.sh

--- linux/Linux - sed - parse string (e.g. file) contents via regex.sh
+++ linux/Linux - sed - parse string (e.g. file) contents via regex.sh
@@ -75,28 +75,29 @@ fi;
 
 # ------------------------------------------------------------
 #
-# sed
-#  |--> reverse the order of lines in a given string, output, etc.
+# sed + head + sed
+#   |--> Remove top 1 line from piped output
 #
+df -h | sed '1!G;h;$!d' | head -n -1 | sed '1!G;h;$!d'; 
 
-# sed reverse, method #1
-sed '1!G;h;$!d';
 
-# sed reverse, method #2
-sed -n '1!G;h;$p';
-
-# ex) sed reverse
-TEST_STR="1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15";
-echo ""
-echo "\${TEST_STR}, as-is (not-reversed):"; echo -e "${TEST_STR}";
-echo "\${TEST_STR}, reversed:"; echo -e "${TEST_STR}" | sed -n '1!G;h;$p';
-
-# ex) Remove first-top-leading X line from string, output, etc (sed-reverse, head, sed-reverse)
-SED_REVERSE="1!G;h;\$p";
+# ------------------------------------------------------------
+# sed + head + sed
+#   |--> Remove top X lines from piped output
+#
+SED_REVERSE_METHOD1='1!G;h;$!d';  # use with  [ sed '1!G;h;$!d' ]
+SED_REVERSE_METHOD2='1!G;h;$p';   # use with  [ sed -n '1!G;h;$p' ]
 TOP_LINES_TO_SLICE=5;
-TEST_STR="1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15";
-echo "\${TEST_STR}, as-is (not-reversed):"; echo -e "${TEST_STR}";
-echo "\${TEST_STR}, reversed:"; echo -e "${TEST_STR}" | sed -n "${SED_REVERSE}" | head -n -${TOP_LINES_TO_SLICE} | sed -n ${SED_REVERSE};
+
+TEST_STR="1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15"; \
+echo "\${TEST_STR}, as-is (not-reversed):"; echo -e "${TEST_STR}"; \
+ \
+echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD1):";\
+echo -e "${TEST_STR}" | sed "${SED_REVERSE_METHOD1}" | head -n -${TOP_LINES_TO_SLICE} | sed "${SED_REVERSE_METHOD1}"; \
+echo -e "\n"; \
+ \
+echo -e "\n\${TEST_STR}, reversed (using SED_REVERSE_METHOD2):"; \
+echo -e "${TEST_STR}" | sed -n "${SED_REVERSE_METHOD2}" | head -n -${TOP_LINES_TO_SLICE} | sed -n "${SED_REVERSE_METHOD2}"; echo -e "\n";
 
 
 
@@ -168,5 +169,7 @@ fi;
 # 	stackoverflow.com  |  "How can I reverse the order of lines in a file?"  |  https://stackoverflow.com/a/744093
 #
 # 	stackoverflow.com  |  "Sed - An Introduction and Tutorial by Bruce Barnett"  |  https://www.grymoire.com/Unix/Sed.html#uh-42
+#
+# 	stackoverflow.com  |  "How can I reverse the order of lines in a file?"  |  https://stackoverflow.com/a/744093
 #
 # ------------------------------------------------------------

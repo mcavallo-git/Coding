aa919740f669009b4a6567879fb3a638577f9cda
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-13 12:05:36 -0600

    rn

---
+++ vmware/Linux - ESXi, esxcli storage commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+esxcli storage filesystem rescan;  # Issue a rescan operation to the VMkernel to have is scan storage devices for new mountable filesystems.	
+
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   pubs.vmware.com  |  "vSphere Command-Line Interface Reference"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fvcli-right.html
+#
+#   pubs.vmware.com  |  "esxcli system Commands"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fesxcli_system.html
+#
+# ------------------------------------------------------------

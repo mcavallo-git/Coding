4d08675ddf0149f3547722ee500383b3c9f167e2
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-13 12:06:12 -0600

    Update vSphere Command-Line Interface Reference (esxcli, vicfg, vihostupdate, vmkfstools, vmware-cmd, etc.).sh

--- vmware/vSphere Command-Line Interface Reference (esxcli, vicfg, vihostupdate, vmkfstools, vmware-cmd, etc.).sh
+++ vmware/vSphere Command-Line Interface Reference (esxcli, vicfg, vihostupdate, vmkfstools, vmware-cmd, etc.).sh
@@ -3,6 +3,8 @@
 #
 #   pubs.vmware.com  |  "vSphere Command-Line Interface Reference"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fvcli-right.html
 #
+#   pubs.vmware.com  |  "esxcli storage Commands"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fesxcli_storage.html
+#
 #   pubs.vmware.com  |  "esxcli system Commands"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fesxcli_system.html
 #
 # ------------------------------------------------------------

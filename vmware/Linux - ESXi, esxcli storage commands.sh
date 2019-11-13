1ffb5446cba3e4f75b1bef67941339304bdfb78e
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-13 12:06:14 -0600

    Update Linux - ESXi, esxcli storage commands.sh

--- vmware/Linux - ESXi, esxcli storage commands.sh
+++ vmware/Linux - ESXi, esxcli storage commands.sh
@@ -9,6 +9,8 @@ esxcli storage filesystem rescan;  # Issue a rescan operation to the VMkernel to
 #
 #   pubs.vmware.com  |  "vSphere Command-Line Interface Reference"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fvcli-right.html
 #
+#   pubs.vmware.com  |  "esxcli storage Commands"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fesxcli_storage.html
+#
 #   pubs.vmware.com  |  "esxcli system Commands"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fesxcli_system.html
 #
 # ------------------------------------------------------------

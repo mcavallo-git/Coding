62c64dbcee8b12bd2e798792d4b0a72330cc03d6
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-13 12:31:31 -0600

    Update Linux - ESXi, list-esxcli-configs.sh

--- vmware/Linux - ESXi, list-esxcli-configs.sh
+++ vmware/Linux - ESXi, list-esxcli-configs.sh
@@ -10,33 +10,53 @@ DIR_PATH="/root"; mkdir -p "${DIR_PATH}"; chmod 0700 "${DIR_PATH}"; chown "${DIR
 
 DIR_PATH="/root/esxcli"; mkdir -p "${DIR_PATH}"; chmod 0700 "${DIR_PATH}"; chown "${DIR_CHOWN}" "${DIR_PATH}";
 
+
 # ------------------------------------------------------------
 
-esxcli storage core adapter list > /root/esxcli/esxcli-storage-core-adapter-list.log;
+esxcli storage core adapter list > /root/esxcli/esxcli-storage-core-adapter-list.log;  # List all the SCSI Host Bus Adapters on the system.
 
-esxcli storage core claimrule list > /root/esxcli/esxcli-storage-core-claimrule-list.log;
 
-esxcli storage core device list > /root/esxcli/esxcli-storage-core-device-list.log;
+esxcli storage core claimrule list > /root/esxcli/esxcli-storage-core-claimrule-list.log;  # List all the claimrules on the system.
 
-esxcli storage core device stats get > /root/esxcli/esxcli-storage-core-device-stats-get.log;
 
-esxcli storage core device world list > /root/esxcli/esxcli-storage-core-device-world-list.log;
+esxcli storage core device list > /root/esxcli/esxcli-storage-core-device-list.log;  # For devices currently registered with the PSA, list the filters attached to them.
 
-esxcli storage core path list > /root/esxcli/esxcli-storage-core-path-list.log;
 
-esxcli storage filesystem list > /root/esxcli/esxcli-storage-filesystem-list.log
+esxcli storage core device stats get > /root/esxcli/esxcli-storage-core-device-stats-get.log;  # List the SCSI stats for SCSI Devices in the system.
 
-esxcli storage nmp device list > /root/esxcli/esxcli-storage-nmp-device-list.log;
 
-esxcli storage nmp path list > /root/esxcli/esxcli-storage-nmp-path-list.log;
+esxcli storage core device world list > /root/esxcli/esxcli-storage-core-device-world-list.log;  # Get a list of the worlds that are currently using devices on the ESX host.
 
-esxcli storage vmfs extent list > /root/esxcli/esxcli-storage-vmfs-extent-list.log;
 
-esxcli storage vmfs snapshot list > /root/esxcli/esxcli-storage-vmfs-snapshot-list.log;
+esxcli storage core path list > /root/esxcli/esxcli-storage-core-path-list.log;  # List all the SCSI paths on the system.
+
+
+esxcli storage filesystem list > /root/esxcli/esxcli-storage-filesystem-list.log  # List the volumes available to the host. This includes VMFS, NAS and VFAT partitions.
+
+
+esxcli storage nmp device list > /root/esxcli/esxcli-storage-nmp-device-list.log;  # List the devices currently controlled by the VMware NMP Multipath Plugin and show the SATP and PSP information associated with that device.
+
+
+esxcli storage nmp path list > /root/esxcli/esxcli-storage-nmp-path-list.log;  # List the paths currently claimed by the VMware NMP Multipath Plugin and show the SATP and PSP information associated with that path.
+
+
+esxcli storage vmfs extent list > /root/esxcli/esxcli-storage-vmfs-extent-list.log;  # List the VMFS extents available on the host.
+
+
+esxcli storage vmfs snapshot list > /root/esxcli/esxcli-storage-vmfs-snapshot-list.log;# List unresolved snapshots/replicas of VMFS volume.
+
 
 # ------------------------------------------------------------
 
 find "/root/esxcli" -type f -print0 | xargs -0 chmod 0600;
+
+exit 0;
+
+
+# ------------------------------------------------------------
+
+esxcli storage filesystem rescan;  # Issue a rescan operation to the VMkernel to have is scan storage devices for new mountable filesystems.
+
 
 # ------------------------------------------------------------
 # Citation(s)

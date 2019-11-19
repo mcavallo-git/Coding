4bb60ad7276f06b0e4d25c30b3734a4d2a5ee2e3
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-19 14:01:08 -0600

    Update PowerShell - Enable-Remote-Desktop.ps1

--- powershell/PowerShell - Enable-Remote-Desktop.ps1
+++ powershell/PowerShell - Enable-Remote-Desktop.ps1
@@ -16,13 +16,22 @@ Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\' -Name
 
 Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\' -Name "UserAuthentication" -Value 1; # Enable Network Level Authentication
 
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"; # Enable Windows firewall rules to allow incoming RDP
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"; # Enable the [ Inbound rule for the Remote Desktop service to allow RDP traffic ]
 
-Add-LocalGroupMember -Group "Remote Desktop Users" -Member "foo"; # Grant RDP access a non-admin user
+
+# Get-NetFirewallRule -DisplayGroup "Remote Desktop"; # Get the status of the [ Inbound rule for the Remote Desktop service to allow RDP traffic ]
+
+# Add-LocalGroupMember -Group "Remote Desktop Users" -Member "foo"; # Grant RDP access a non-admin user
 
 
 # ------------------------------------------------------------
 # Citation(s)
+#
+#   docs.microsoft.com  |  "Disable-NetFirewallRule - Disables a firewall rule"  |  https://docs.microsoft.com/en-us/powershell/module/netsecurity/disable-netfirewallrule
+#
+#   docs.microsoft.com  |  "Enable-NetFirewallRule - Enables a previously disabled firewall rule"  |  https://docs.microsoft.com/en-us/powershell/module/netsecurity/enable-netfirewallrule
+#
+#   docs.microsoft.com  |  "Get-NetFirewallRule - Retrieves firewall rules from the target computer"  |  https://docs.microsoft.com/en-us/powershell/module/netsecurity/get-netfirewallrule
 #
 #   exchangepedia.com  |  "Enable remote desktop (RDP) connections for admins on Windows Server 2016"  |  https://exchangepedia.com/2016/10/enable-remote-desktop-rdp-connections-for-admins-on-windows-server-2016.html
 #

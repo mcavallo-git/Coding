9fe0a0165472f2f0793634ef8e2c53230ced55ad
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-19 11:45:21 -0600

    Update Linux - ESXi, scp push (sending a file from local instance to remote host).sh

--- vmware/Linux - ESXi, scp push (sending a file from local instance to remote host).sh
+++ vmware/Linux - ESXi, scp push (sending a file from local instance to remote host).sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-scp -o StrictHostKeyChecking=no -P 22 -i "/PATH/TO/SSH_KEY" "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE"
+scp -o StrictHostKeyChecking=no -P 22 -i "/PATH/TO/SSH_KEY" "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
 

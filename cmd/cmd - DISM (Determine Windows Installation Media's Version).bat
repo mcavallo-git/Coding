360e792a1f3d268baa288ab32e0de46664c54008
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-19 18:49:59 -0600

    Create cmd - DISM (Determine Windows Installation Media's Version).bat

---
+++ cmd/cmd - DISM (Determine Windows Installation Media's Version).bat
@@ -0,0 +1,48 @@
+@ECHO Off
+REM ------------------------------------------------------------
+REM 
+REM Windows - Determine Installation-Media / ISO-File's Version
+REM 
+REM ------------------------------------------------------------
+REM 
+REM Using DISM /Get-WimInfo
+
+
+DISM /Get-WimInfo /WimFile:"D:\sources\install.wim"
+
+DISM /Get-WimInfo /WimFile:"D:\sources\install.esd"
+
+
+REM ------------------------------------------------------------
+REM 
+REM Using DISM /Get-WimInfo (on file's with multiple indices)	
+
+
+DISM /Get-WimInfo /WimFile:"D:\sources\install.wim" /Index:6
+
+
+DISM /Get-WimInfo /WimFile:"D:\sources\install.esd" /Index:6
+
+
+REM ------------------------------------------------------------
+REM 
+REM Using DISM /Get-WimInfo (on file's with multiple indices)	
+
+
+DISM /Get-ImageInfo /ImageFile:"D:\sources\install.wim"
+
+
+DISM /Get-ImageInfo /ImageFile:"D:\sources\install.esd"
+
+
+REM ------------------------------------------------------------
+REM
+REM  Citation(s)
+REM
+REM  	docs.microsoft.com  |  "DISM Image Management Command-Line Options"  |  https://docs.microsoft.com/en-us/windows-hardware/manufacture/desktop/dism-image-management-command-line-options-s14
+REM
+REM  	howtogeek.com  |  "How to See Which Windows Version and Build is on a DVD, ISO, or USB Drive"  |  https://www.howtogeek.com/362502/how-to-see-which-windows-version-and-build-is-on-a-dvd-iso-or-usb-drive/
+REM
+REM  	social.technet.microsoft.com  |  "Get-ImageInfo VS Get-Wiminfo"  |  https://social.technet.microsoft.com/Forums/en-US/e1f020cc-2ab8-4528-8d8e-e0260ad82cab/getimageinfo-vs-getwiminfo
+REM
+REM ------------------------------------------------------------

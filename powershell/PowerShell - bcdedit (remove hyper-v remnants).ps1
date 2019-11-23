ac2e41e509d620f599c336dbf230f6708b94ac18
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-23 17:40:28 -0600

    Update PowerShell - bcdedit (remove hyper-v remnants).ps1

--- powershell/PowerShell - bcdedit (remove hyper-v remnants).ps1
+++ powershell/PowerShell - bcdedit (remove hyper-v remnants).ps1
@@ -1,13 +1,17 @@
 @ECHO OFF
 # 
-# Windows - Fix glitch where "Hyper-V" role/feature is disabled, yet VMWare Workstation still states it can't start VMs because "VMware Workstation and Hyper-V are not compatible. #ove the Hyper-V Role from the system before runnin VMware Workstation."
+# Hyper-V's Enabled/Disabled setting exists in two places:
+#  1) The set of Windows Features currently enabled on target image (windows OS) - Edit using "OptionalFeatures.exe" (native windows command-line tool)
+#  2) The bootup setting pulled from Boot Configuration Data (BCD) - Edit using "bcdedit.exe" (native windows command-line tool)
+
+# glitch where "Hyper-V" role/feature is disabled, yet VMWare Workstation still states it can't start VMs because "VMware Workstation and Hyper V are not compatible. #ove the Hyper V Role from the system before runnin VMware Workstation."
 # 
 # ------------------------------------------------------------
 
+# Using PowerShell:
 
-bcdedit /set hypervisorlaunchtype Off
+bcdedit /set hypervisorlaunchtype Off; <# <-- Disable Hyper V [ Boot Configuration Data ] #> Get-WindowsOptionalFeature -Online | Where-Object { $_.State -Eq "Disabled" } | Where-Object { @("HypervisorPlatform", "Microsoft-Hyper-V-All", "Microsoft-Hyper-V", "Microsoft-Hyper-V-Tools-All", "Microsoft-Hyper-V-Management-PowerShell", "Microsoft-Hyper-V-Hypervisor", "Microsoft-Hyper-V-Services", "Microsoft-Hyper-V-Management-Clients").Contains($_.FeatureName) -Eq $True } | Disable-WindowsOptionalFeature -Online -NoRestart; <# <-- Disable Hyper V [ Windows Features ] #> 
 
-Get-WindowsOptionalFeature -Online | Where-Object { $_.State -Eq "Disabled" } | Where-Object { @("HypervisorPlatform", "Microsoft-Hyper-V-All", "Microsoft-Hyper-V", "Microsoft-Hyper-V-Tools-All", "Microsoft-Hyper-V-Management-PowerShell", "Microsoft-Hyper-V-Hypervisor", "Microsoft-Hyper-V-Services", "Microsoft-Hyper-V-Management-Clients").Contains($_.FeatureName) -Eq $True } | Disable-WindowsOptionalFeature -Online -NoRestart;
 
 # ------------------------------------------------------------
 # Citation(s)

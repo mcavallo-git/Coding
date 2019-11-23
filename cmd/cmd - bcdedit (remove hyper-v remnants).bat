580a2f057e213d77d3edc8d4b21724a9deed12c8
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-23 13:10:50 -0600

    Update cmd - bcdedit (remove hyper-v remnants).bat

--- cmd/cmd - bcdedit (remove hyper-v remnants).bat
+++ cmd/cmd - bcdedit (remove hyper-v remnants).bat
@@ -1,12 +1,16 @@
 @ECHO OFF
+REM 
+REM Windows - Fix glitch where "Hyper-V" role/feature is disabled, yet VMWare Workstation still states it can't start VMs because "VMware Workstation and Hyper-V are not compatible. Remove the Hyper-V Role from the system before runnin VMware Workstation."
+REM 
+REM ------------------------------------------------------------
+
 
 bcdedit /set hypervisorlaunchtype Off
 
 
-REM
+REM ------------------------------------------------------------
 REM Citation(s)
 REM
-REM		answers.microsoft.com
-REM		  |  "Hyper V remnants left over after disabling, preventing other Virtualization software from running in 64-bit mode"
-REM		  |  https://answers.microsoft.com/en-us/insider/forum/all/hyper-v-remnants-left-over-after-disabling/81e4fef1-d08e-4ef4-8e1e-b24c47bd7677?auth=1
+REM   answers.microsoft.com  |  "Hyper V remnants left over after disabling, preventing other Virtualization software from running in 64-bit mode"  |  https://answers.microsoft.com/en-us/insider/forum/all/hyper-v-remnants-left-over-after-disabling/81e4fef1-d08e-4ef4-8e1e-b24c47bd7677?auth=1
 REM
+REM ------------------------------------------------------------

2e4a56db70aaaf3d731e0774fab71c106be034ae
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-23 12:00:33 -0600

    Create Windows - .msc essential runtimes (to remember).bat

---
+++ windows/Windows - .msc essential runtimes (to remember).bat
@@ -0,0 +1,70 @@
+REM ------------------------------------------------------------
+REM
+REM Windows - Default run commands to-remember (.msc, .cpl, .exe)
+REM 
+REM ------------------------------------------------------------
+REM 
+REM How to run Control Panel tools by typing a command [ cited in "Citation(s)", below ]
+REM 
+
+control access.cpl	Accessibility Options 
+control sysdm.cpl add new hardware	Add New Hardware 
+control appwiz.cpl	Add/Remove Programs 
+control timedate.cpl	Date/Time Properties 
+control desk.cpl	Display Properties 
+control findfast.cpl	FindFast 
+control fonts	Fonts Folder 
+control inetcpl.cpl	Internet Properties 
+control joy.cpl	Joystick Properties 
+control main.cpl keyboard	Keyboard Properties 
+control mlcfg32.cpl	Microsoft Exchange 
+control wgpocpl.cpl	Microsoft Mail Post Office 
+control modem.cpl	Modem Properties 
+control main.cpl	Mouse Properties 
+control mmsys.cpl	Multimedia Properties 
+control netcpl.cpl  # NOTE: In Windows NT 4.0, Network properties is Ncpa.cpl, not Netcpl.cpl	Network Properties 
+control password.cpl	Password Properties 
+control main.cpl pc card (PCMCIA)	PC Card 
+control printers	Printers Folder 
+control intl.cpl	Regional Settings 
+control sticpl.cpl	Scanners and Cameras 
+control mmsys.cpl sounds	Sound Properties 
+control sysdm.cpl	System Properties 
+
+
+REM ------------------------------------------------------------
+REM 
+REM Description of Control Panel (.cpl) Files [ cited in "Citation(s)", below ]
+REM 
+
+access.cpl	Accessibility properties
+appwiz.cpl	Add/Remove Programs properties
+desk.cpl	Display properties
+findFast.cpl	FindFast (included with Microsoft Office for Windows 95)
+inetcpl.cpl	Internet properties
+intl.cpl	Regional Settings properties
+joy.cpl	Joystick properties
+main.cpl	Mouse, Fonts, Keyboard, and Printers properties
+mlcfg32.cpl	Microsoft Exchange or Windows Messaging properties
+mmsys.cpl	Multimedia properties
+modem.cpl	Modem properties
+netcpl.cpl	Network properties
+odbccp32.cpl	Data Sources (32-bit ODBC, included w/ Microsoft Office)
+password.cpl	Password properties
+sticpl.cpl	Scanners and Cameras properties
+sysdm.cpl	System properties and Add New Hardware wizard
+themes.cpl	Desktop Themes 
+timeDate.cpl	Date/Time properties
+wgpocpl.cpl	Microsoft Mail Post Office
+
+
+REM ------------------------------------------------------------
+REM Citation(s)
+REM
+REM   support.microsoft.com  |  "Description of Control Panel (.cpl) Files"  |  https://support.microsoft.com/en-us/help/149648/description-of-control-panel-cpl-files
+REM
+REM   support.microsoft.com  |  "How to run Control Panel tools by typing a command"  |  https://support.microsoft.com/en-us/help/192806/how-to-run-control-panel-tools-by-typing-a-command
+REM
+REM   recouncedthoughts.wordpress.com  |  "Run Commands, cpl, msc files for everyday use"  |  https://renouncedthoughts.wordpress.com/2014/05/08/run-commands-cpl-msc-files-for-everyday-use/
+REM
+REM ------------------------------------------------------------

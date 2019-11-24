e7742ea4a9fa312e463b1e5d7084c29d6812e9b4
Matt Cavallo <mcavallo.work@gmail.com>
2019-11-24 01:56:41 -0600

    Update cmd - TASKLIST (display a list running processes).bat

--- cmd/cmd - TASKLIST (display a list running processes).bat
+++ cmd/cmd - TASKLIST (display a list running processes).bat
@@ -44,22 +44,19 @@ REM ECHO TEMP_FILENAME = %TEMP_FILENAME%
 
 REM Get Last Sync-Time from WMMIC
 FOR /F "tokens=5 delims= " %a IN ('w32tm /query /status /verbose ^| find "Last Successful Sync Time:" ') DO SET LAST_SYNC_DATE=%a
+REM SET LAST_SYNC_DATE=%a
 FOR /f "tokens=3 delims=//" %a IN ('ECHO %LAST_SYNC_DATE%') DO SET /A LAST_SYNC_YEAR=%a
 FOR /f "tokens=2 delims=//" %a IN ('ECHO %LAST_SYNC_DATE%') DO SET /A LAST_SYNC_MONTH=%a
 FOR /f "tokens=1 delims=//" %a IN ('ECHO %LAST_SYNC_DATE%') DO SET /A LAST_SYNC_DAY=%a
 FOR /F "tokens=6 delims= " %a IN ('w32tm /query /status /verbose ^| find "Last Successful Sync Time:" ') DO SET LAST_SYNC_TIME=%a
+REM SET LAST_SYNC_DATE=%a
 FOR /f "tokens=1 delims=/:" %a IN ('ECHO %LAST_SYNC_TIME%') DO SET /A LAST_SYNC_HOUR=%a
 FOR /f "tokens=2 delims=/:" %a IN ('ECHO %LAST_SYNC_TIME%') DO SET /A LAST_SYNC_MIN=%a
 FOR /f "tokens=3 delims=/:" %a IN ('ECHO %LAST_SYNC_TIME%') DO SET /A LAST_SYNC_SEC=%a
 FOR /F "tokens=7 delims= " %a IN ('w32tm /query /status /verbose ^| find "Last Successful Sync Time:" ') DO SET LAST_SYNC_AM_PM=%a
-IF %LAST_SYNC_AM_PM%==PM SET /A LAST_SYNC_HOUR=%LAST_SYNC_HOUR%+12
-ECHO LAST_SYNC_YEAR = %LAST_SYNC_YEAR%
-ECHO LAST_SYNC_MONTH = %LAST_SYNC_MONTH%
-ECHO LAST_SYNC_DAY = %LAST_SYNC_DAY%
-ECHO LAST_SYNC_HOUR = %LAST_SYNC_HOUR%
-ECHO LAST_SYNC_MIN = %LAST_SYNC_MIN%
-ECHO LAST_SYNC_SEC = %LAST_SYNC_SEC%
-
+SET LAST_SYNC_HRS_TO_ADD=%LAST_SYNC_AM_PM:AM=00%
+SET LAST_SYNC_HRS_TO_ADD=%LAST_SYNC_AM_PM:PM=12%
+SET /A LAST_SYNC_HOUR=(LAST_SYNC_HOUR+LAST_SYNC_HRS_TO_ADD)
 
 FOR /F "tokens=7 delims= " %a IN ('w32tm /query /status /verbose ^| find "Time since Last Good Sync Time:" ') DO SET ELAPSED_TIME_SINCE_LAST_SYNC=%a
 FOR /f "tokens=1 delims=/." %a IN ('ECHO %ELAPSED_TIME_SINCE_LAST_SYNC:s=%') DO SET /A SECONDS_SINCE_LAST_SYNC=%a
@@ -72,14 +69,30 @@ SET /A NOW_SEC=(LAST_SYNC_SEC+SEC_TO_ADD)%60
 SET /A MIN_TO_ADD=(MIN_TO_ADD+((((LAST_SYNC_SEC+SEC_TO_ADD)/60)%60+60)%60))
 SET /A NOW_MIN=(LAST_SYNC_MIN+MIN_TO_ADD)%60
 SET /A HRS_TO_ADD=(HRS_TO_ADD+((((LAST_SYNC_MIN+MIN_TO_ADD)/60)%60+60)%60))
-SET /A NOW_HR=(LAST_SYNC_HOUR+HRS_TO_ADD)
+SET /A NOW_HR=(LAST_SYNC_HOUR+HRS_TO_ADD)%24
+SET /A NOW_DAY=(LAST_SYNC_DAY+((((LAST_SYNC_HOUR+HRS_TO_ADD)/24)%24+24)%24))
+SET /A NOW_MONTH=LAST_SYNC_MONTH
+SET /A NOW_YEAR=LAST_SYNC_YEAR
 
+SET ECHO_YEAR=%NOW_YEAR%
+SET ECHO_MONTH=0%NOW_MONTH%
+SET ECHO_MONTH=%ECHO_MONTH:~-2%
+SET ECHO_DAY=0%NOW_DAY%
+SET ECHO_DAY=%ECHO_DAY:~-2%
+SET ECHO_HOUR=0%NOW_HR%
+SET ECHO_HOUR=%ECHO_HOUR:~-2%
+SET ECHO_MIN=0%NOW_MIN%
+SET ECHO_MIN=%ECHO_MIN:~-2%
+SET ECHO_SEC=0%NOW_SEC%
+SET ECHO_SEC=%ECHO_SEC:~-2%
 
-SET NOW_TIMESTAMP=%NOW_HR%_%NOW_HR%_%NOW_HR%
-
+SET NOW_TIMESTAMP=%ECHO_YEAR%-%ECHO_MONTH%-%ECHO_DAY%_%ECHO_HOUR%-%ECHO_MIN%-%ECHO_SEC%
 
 ECHO ------------------------------------------------------------
+ECHO LAST_SYNC_DATE = %LAST_SYNC_DATE%
 ECHO LAST_SYNC_TIME = %LAST_SYNC_TIME%
+ECHO LAST_SYNC_AM_PM = %LAST_SYNC_AM_PM%
+ECHO LAST_SYNC_HRS_TO_ADD = %LAST_SYNC_HRS_TO_ADD%
 ECHO LAST_SYNC_HOUR = %LAST_SYNC_HOUR%
 ECHO LAST_SYNC_MIN = %LAST_SYNC_MIN%
 ECHO LAST_SYNC_SEC = %LAST_SYNC_SEC%
@@ -91,16 +104,15 @@ ECHO HRS_TO_ADD = %HRS_TO_ADD%
 ECHO MIN_TO_ADD = %NOW_MIN%
 ECHO SEC_TO_ADD = %SEC_TO_ADD%
 ECHO ------------------------------------------------------------
+ECHO NOW_YEAR = %NOW_YEAR%
+ECHO NOW_MONTH = %NOW_MONTH%
+ECHO NOW_DAY = %NOW_DAY%
 ECHO NOW_HR = %NOW_HR%
 ECHO NOW_MIN = %NOW_MIN%
 ECHO NOW_SEC = %NOW_SEC%
 ECHO ------------------------------------------------------------
-
-ECHO NOW = %LAST_SYNC_HOUR%:%LAST_SYNC_MIN%:%LAST_SYNC_SEC%
-
-SET /A TESTER=(70/60)
-ECHO TESTER = %TESTER%
-
+ECHO NOW_TIMESTAMP = %NOW_TIMESTAMP%
+ECHO ------------------------------------------------------------
 
 
 REM ------------------------------------------------------------
@@ -163,5 +175,7 @@ REM
 REM   stackoverflow.com  |  "CMD set /a, modulus, and negative numbers"  |  https://stackoverflow.com/a/27894447
 REM 
 REM   stackoverflow.com  |  "How to create a unique temporary file path in command prompt without external tools? [duplicate]"  |  https://stackoverflow.com/a/32109191
+REM 
+REM   stackoverflow.com  |  "String processing in windows batch files: How to pad value with leading zeros?"  |  https://stackoverflow.com/a/13398719
 REM 
 REM ------------------------------------------------------------

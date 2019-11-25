9ff284692992a74de2cf29f2c2c572d4fedac1b6
Matt Cavallo <mcavallo.git@gmail.com>
2019-11-25 13:45:07 -0600

    Update Show.psm1

--- powershell/_WindowsPowerShell/Modules/Show/Show.psm1
+++ powershell/_WindowsPowerShell/Modules/Show/Show.psm1
@@ -89,6 +89,16 @@ Function Show() {
 						$RetCode_CLSID = If($?){0}Else{1};
 
 						$ErrorActionPreference = $Revertable_ErrorActionPreference;
+
+						Write-Output "`n";
+						Write-Output "  ${Check_APPID} = [${Check_APPID}]";
+						Write-Output "  ${RetCode_APPID} = [${RetCode_APPID}]";
+						Write-Output "  ${RegistryKey_APPID} = [${RegistryKey_APPID}]";
+						Write-Output "`n";
+						Write-Output "  ${Check_CLSID} = [${Check_CLSID}]";
+						Write-Output "  ${RetCode_CLSID} = [${RetCode_CLSID}]";
+						Write-Output "  ${RegistryKey_CLSID} = [${RegistryKey_CLSID}]";
+
 						If ($RetCode_APPID -Eq 0)	{
 							Write-Output "  Found APPID Key w/ path `"${Check_APPID}`":";
 							$RegistryKey_APPID | Format-List;

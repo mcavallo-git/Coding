df551888e3b9158b661612a0e60f5afaf320f234
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-02 14:09:32 -0600

    Update Linux - firewall-cmd.sh

--- linux/Linux - firewall-cmd.sh
+++ linux/Linux - firewall-cmd.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-firewall-cmd --list-all
+firewall-cmd --list-all;
 
 
 
+# Ex) List all firewall-cmd rules, then allow traffic for SSH (22) & MongoDB (27017) Ports, restart the firewall service, then list all rules again
+firewall-cmd --list-all; firewall-cmd --permanent --add-port=22/tcp --add-port=27017/tcp; firewall-cmd --reload; firewall-cmd --list-all;
 

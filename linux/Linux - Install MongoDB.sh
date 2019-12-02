bd0b90d302400b14e2d4b7b040f93efe3ff25af9
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-02 15:06:51 -0600

    Update Linux - Install MongoDB.sh

--- linux/Linux - Install MongoDB.sh
+++ linux/Linux - Install MongoDB.sh
@@ -45,6 +45,42 @@ fi; \
 fi;
 
 
+# ------------------------------------------------------------
+# Setup replication
+CURRENT_REPL_SET_NAME="$(cat /etc/mongod.conf | grep replSetName | awk '{print $2}')"; if [ -z "${CURRENT_REPL_SET_NAME}" ]; then
+echo ""; read -p "Enable replication for MongoDB? (y/n)  " -n 1 -t 60 -r; echo ""; if [[ $REPLY =~ ^[Yy]$ ]]; then \
+echo ""; read -p "Enter Replication Name:  " -t 60 -r; echo ""; \
+
+
+
+
+fi;
+fi;
+
+
+
+
+NEW_KEYFILE="${REPLY}";
+if [ ! -f "${NEW_KEYFILE}" ]; then echo "Warning - file not found: \"${NEW_KEYFILE}\""; read -p "Create a randomly-generated Keyfile, now? (y/n)  " -n 1 -t 60 -r; echo ""; if [[ $REPLY =~ ^[Yy]$ ]]; then openssl rand -base64 741 > "${NEW_KEYFILE}"; fi; fi; \
+CURRENT_KEYFILE="$(cat /etc/mongod.conf | grep keyFile | awk '{print $2}')"; \
+if [ -z "${CURRENT_KEYFILE}" ]; then \
+sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e '
+/^#security:/ {
+a\
+  keyFile: '${NEW_KEYFILE}'
+a\
+  authorization: enabled
+c\
+security:
+}' "/etc/mongod.conf"; \
+cat "/etc/mongod.conf"; service mongod restart; \
+elif [ "${CURRENT_KEYFILE}" != "${NEW_KEYFILE}" ]; then \
+echo "Warning:  keyFile currently set to path \"${CURRENT_KEYFILE}\""; read -p "Update keyFile to reference \"${NEW_KEYFILE}\", instead? (y/n)  " -n 1 -t 60 -r; echo ""; if [[ $REPLY =~ ^[Yy]$ ]]; then sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e "/^  keyFile:/c\  keyFile: ${NEW_KEYFILE}" "/etc/mongod.conf"; cat "/etc/mongod.conf"; service mongod restart; fi; \
+else \
+echo "Info:  keyFile already set to path \"${CURRENT_KEYFILE}\" in \"/etc/mongod.conf\" - no action required";
+fi; \
+fi;
+
 
 # ------------------------------------------------------------
 # Citation(s)
@@ -54,5 +90,7 @@ fi;
 #   docs.mongodb.com  |  "Install MongoDB Community Edition on Red Hat or CentOS"  |  https://docs.mongodb.com/manual/tutorial/install-mongodb-on-red-hat/
 #
 #   docs.mongodb.com  |  "Install MongoDB Community Edition on Ubuntu"  |  https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
+#
+#   docs.mongodb.com  |  "Deploy a Replica Set"  |  https://docs.mongodb.com/manual/tutorial/deploy-replica-set/
 #
 # ------------------------------------------------------------

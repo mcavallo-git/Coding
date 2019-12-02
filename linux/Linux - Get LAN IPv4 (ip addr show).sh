ccd3504aea1a36eabf17fca63620f47afef4c57e
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-02 14:20:01 -0600

    Update Linux - Get LAN IPv4 (ip addr show).sh

--- linux/Linux - Get LAN IPv4 (ip addr show).sh
+++ linux/Linux - Get LAN IPv4 (ip addr show).sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-$THIS_IPv4=$(ip addr show | grep 'inet' | grep 'scope global' | awk '{ print $2; }' | sed 's/\/.*$//' | grep '\.');
+THIS_IPv4=$(ip addr show | grep 'inet' | grep 'scope global' | awk '{ print $2; }' | sed 's/\/.*$//' | grep '\.');
 echo "THIS_IPv4 = ${THIS_IPv4}";

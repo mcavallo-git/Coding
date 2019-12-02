bb27c02f1bc4a3212a83938b256cd63ab61aa631
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-02 14:15:36 -0600

    Update Linux - Install MongoDB.sh

--- linux/Linux - Install MongoDB.sh
+++ linux/Linux - Install MongoDB.sh
@@ -4,8 +4,10 @@
 # CentOS / RHEL
 echo -e "[mongodb-org-4.2]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/\$releasever/mongodb-org/4.2/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc\n" > "/etc/yum.repos.d/mongodb-org-4.2.repo"; yum check-update; yum install -y mongodb-org;
 
-mkdir -p "/var/lib/mongo"; chown -R "mongod:mongod" "/var/lib/mongo";
-mkdir -p "/var/lib/mongodb"; chown -R "mongod:mongod" "/var/lib/mongodb";
+mkdir -p "/var/lib/mongo"; chown -R "mongod:mongod" "/var/lib/mongo"; chmod 0755 "/var/lib/mongo";
+mkdir -p "/var/lib/mongodb"; chown -R "mongod:mongod" "/var/lib/mongodb"; chmod 0755 "/var/lib/mongodb";
+
+service mongod start;
 
 # ------------------------------------------------------------
 # Citation(s)

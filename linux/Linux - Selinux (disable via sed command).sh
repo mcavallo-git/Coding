6eb7feb173d9381ce3c8e4b4a74bf1dbe9f7bdfb
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-02 12:48:46 -0600

    Update Linux - Selinux (disable via sed command).sh

--- linux/Linux - Selinux (disable via sed command).sh
+++ linux/Linux - Selinux (disable via sed command).sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sudo sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e "/^SELINUX=/c\SELINUX=disabled" "/etc/sysconfig/selinux";
+
+sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e "/^SELINUX=/c\SELINUX=disabled" "/etc/sysconfig/selinux";
 
 echo "$(date +'%Y-%m-%d_%H-%M-%S')  |  REBOOT REQUIRED TO FINISH DISABLING SELINUX";
+read -p " --> REBOOT NOW? (y/n)  " -n 1 -t 60 -r; # Await single keypress
+if [[ $REPLY =~ ^[Yy]$ ]]; then reboot now; fi;

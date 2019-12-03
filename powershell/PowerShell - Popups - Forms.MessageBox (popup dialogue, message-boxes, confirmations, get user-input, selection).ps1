c9d90f4a80132b3c922c165f439fe8356cd0db6b
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-03 03:23:37 -0600

    Update PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1

--- powershell/PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1
+++ powershell/PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1
@@ -27,7 +27,7 @@ $oReturn=[System.Windows.Forms.Messagebox]::Show("Test alert!"); $oReturn;
 # 
 # Ex) Show popup w/ text "Empty the Recycle Bin?" -> If "OK" button is selected, them Empty the Recycle Bin
 #
-If (([System.Windows.Forms.MessageBox]::Show("Empty the Recycle Bin?","Empty Recycle Bin",[System.Windows.Forms.MessageBoxButtons]::OKCancel)) -Eq "OK") { cmd.exe /C 'ECHO Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin'; };
+PowerShell.exe -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); If (([System.Windows.Forms.MessageBox]::Show('Empty the Recycle Bin?','Empty Recycle Bin',[System.Windows.Forms.MessageBoxButtons]::OKCancel)) -Eq 'OK') { cmd.exe /C 'ECHO Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin'; };";
 
 
 
@@ -35,6 +35,8 @@ If (([System.Windows.Forms.MessageBox]::Show("Empty the Recycle Bin?","Empty Rec
 # Citation(s)
 #
 #   docs.microsoft.com  |  "MessageBox Class - Displays a message window, also known as a dialog box, which presents a message to the user"  |  https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.messagebox
+#
+#   stackoverflow.com  |  "Powershell unable to find type [System.Windows.Forms.KeyEventHandler]"  |  https://stackoverflow.com/a/27792262
 #
 #   michlstechblog.info  |  "Powershell: How to show a message box"  |  https://michlstechblog.info/blog/powershell-show-a-messagebox/
 #

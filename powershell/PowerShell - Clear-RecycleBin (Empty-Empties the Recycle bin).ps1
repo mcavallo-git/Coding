ac270aa762b625496ebd7e841d33e5df6fa29158
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-03 02:43:33 -0600

    Update PowerShell - Clear-RecycleBin (Empty-Empties the Recycle bin).ps1

--- powershell/PowerShell - Clear-RecycleBin (Empty-Empties the Recycle bin).ps1
+++ powershell/PowerShell - Clear-RecycleBin (Empty-Empties the Recycle bin).ps1
@@ -1,1 +1,8 @@
 C:\Windows\System32\cmd.exe /C "ECHO Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin";
+
+# ------------------------------------------------------------
+# Citation(s)
+# 
+#   winaero.com  |  "Empty Recycle Bin automatically in Windows 10"  |  https://winaero.com/blog/empty-recycle-bin-automatically-in-windows-10/
+# 
+# ------------------------------------------------------------

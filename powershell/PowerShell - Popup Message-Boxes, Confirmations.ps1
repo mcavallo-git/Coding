6e0e42fd3816ed8a1c9017bc6e4013636f87b0eb
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-03 02:55:37 -0600

    Create PowerShell - Popup Message-Boxes, Confirmations.ps1

---
+++ powershell/PowerShell - Popup Message-Boxes, Confirmations.ps1
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------
+# 
+# PowerShell - Popups (Alert, Confirm)
+# 
+# ------------------------------------------------------------
+
+# Message-Box
+$oReturn=[System.Windows.Forms.Messagebox]::Show("This is the Message text"); $oReturn;
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   michlstechblog.info  |  "Powershell: How to show a message box"  |  https://michlstechblog.info/blog/powershell-show-a-messagebox/
+#
+# ------------------------------------------------------------

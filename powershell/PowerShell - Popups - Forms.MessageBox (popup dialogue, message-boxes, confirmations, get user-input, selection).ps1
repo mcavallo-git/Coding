9e44dd0a0e9693c9a407d4b45898acbba0ae946d
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-03 03:34:06 -0600

    Update PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1

--- powershell/PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1
+++ powershell/PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1
@@ -27,7 +27,7 @@ $oReturn=[System.Windows.Forms.Messagebox]::Show("Test alert!"); $oReturn;
 # 
 # Ex) Show popup w/ text "Empty the Recycle Bin?" -> If "Yes" button is selected, them Empty the Recycle Bin
 #
-PowerShell.exe -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); If (([System.Windows.Forms.MessageBox]::Show('Empty the Recycle Bin?','Confirmation Required',[System.Windows.Forms.MessageBoxButtons]::YesNo)) -Eq 'Yes') { cmd.exe /C 'ECHO Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin'; };" -WindowStyle "Hidden";
+powershell.exe -NoProfile -WindowStyle Hidden -Command ("[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); If (([System.Windows.Forms.MessageBox]::Show('Empty the Recycle Bin?','Confirmation Required',[System.Windows.Forms.MessageBoxButtons]::YesNo)) -Eq 'Yes') { cmd.exe /C 'ECHO Y|powershell.exe -NoProfile -Command Clear-RecycleBin'; };");
 
 
 # ------------------------------------------------------------
@@ -36,6 +36,8 @@ PowerShell.exe -Command "[void] [System.Reflection.Assembly]::LoadWithPartialNam
 #   docs.microsoft.com  |  "MessageBox Class - Displays a message window, also known as a dialog box, which presents a message to the user"  |  https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.messagebox
 #
 #   stackoverflow.com  |  "Powershell unable to find type [System.Windows.Forms.KeyEventHandler]"  |  https://stackoverflow.com/a/27792262
+#
+#   stackoverflow.com  |  "How to run a PowerShell script without displaying a window?"  |  https://stackoverflow.com/a/45473968
 #
 #   michlstechblog.info  |  "Powershell: How to show a message box"  |  https://michlstechblog.info/blog/powershell-show-a-messagebox/
 #

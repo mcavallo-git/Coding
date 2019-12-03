de51394fbc617ee6ef084c00dd58fae300ad1472
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-03 03:28:47 -0600

    Update PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1

--- powershell/PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1
+++ powershell/PowerShell - Popups - Forms.MessageBox (popup dialogue, message-boxes, confirmations, get user-input, selection).ps1
@@ -27,8 +27,7 @@ $oReturn=[System.Windows.Forms.Messagebox]::Show("Test alert!"); $oReturn;
 # 
 # Ex) Show popup w/ text "Empty the Recycle Bin?" -> If "Yes" button is selected, them Empty the Recycle Bin
 #
-PowerShell.exe -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); If (([System.Windows.Forms.MessageBox]::Show('Empty the Recycle Bin?','Confirmation Required',[System.Windows.Forms.MessageBoxButtons]::YesNo)) -Eq 'Yes') { cmd.exe /C 'ECHO Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin'; };";
-
+PowerShell.exe -Command "[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); If (([System.Windows.Forms.MessageBox]::Show('Empty the Recycle Bin?','Confirmation Required',[System.Windows.Forms.MessageBoxButtons]::YesNo)) -Eq 'Yes') { cmd.exe /C 'ECHO Y|PowerShell.exe -NoProfile -Command Clear-RecycleBin'; };" -WindowStyle "Hidden";
 
 
 # ------------------------------------------------------------

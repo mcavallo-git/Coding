5b81e31fa723723cfe85c221eb01dbf0dd9dfe20
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-03 10:09:28 -0600

    Update Linux - Selinux (disable via sed command).sh

--- linux/Linux - Selinux (disable via sed command).sh
+++ linux/Linux - Selinux (disable via sed command).sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e "/^SELINUX=/c\SELINUX=disabled" "/etc/selinux/config"; \
-sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e "/^SELINUX=/c\SELINUX=disabled" "/etc/sysconfig/selinux"; \
+getenforce;
+CONF_FILE="/etc/selinux/config"; if [ -f "${CONF_FILE}" ]; then sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e "/^SELINUX=/c\SELINUX=disabled" "/etc/selinux/config"; fi; \
+CONF_FILE="/etc/sysconfig/selinux"; if [ -f "${CONF_FILE}" ]; then sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -e "/^SELINUX=/c\SELINUX=disabled" "/etc/selinux/config"; fi; \
 setenforce 0; \
-echo "$(date +'%Y-%m-%d_%H-%M-%S')  |  REBOOT REQUIRED TO FINISH DISABLING SELINUX"; \
-read -p " --> REBOOT NOW? (y/n)  " -n 1 -t 60 -r; if [[ $REPLY =~ ^[Yy]$ ]]; then reboot now; fi;
+echo "$(date +'%Y-%m-%d_%H-%M-%S')  |  Reboot required to finish disabling Selinux"; read -p " --> Reboot, now? (y/n)  " -n 1 -t 60 -r; if [[ $REPLY =~ ^[Yy]$ ]]; then reboot now; fi;

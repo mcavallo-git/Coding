d0aef00aa1620d93e80c5ba2ee9bb7a814fd363d
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-06 11:19:34 -0600

    Update Linux - systemctl (show enabled, generated services).sh

--- linux/Linux - systemctl (show enabled, generated services).sh
+++ linux/Linux - systemctl (show enabled, generated services).sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 systemctl list-unit-files --type service --state enabled,generated
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   askubuntu.com  |  "How to list all enabled services from systemctl?"  |  https://askubuntu.com/a/1060852
+#
+# ------------------------------------------------------------

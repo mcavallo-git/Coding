90d58faf5e11c3b9aded1c81733ca95864667ebd
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-06 15:47:41 -0600

    Update Linux - awk.sh

--- linux/Linux - awk.sh
+++ linux/Linux - awk.sh
@@ -2,5 +2,5 @@
 
 # Use the "awk '{print $X}'" command to get a specific 'column' (per-say) from a given output
 
-THIS_DISK_FREE_BYTES="$(df -B1 . | grep -v 'Use% Mounted on' | awk '{print $4}') KB";
+THIS_DISK_FREE_BYTES=$(df -B1 . | grep -v 'Use% Mounted on' | awk '{print $4}');
 echo -e "\n\n""Free disk space (in bytes) on current disk:  [ ${THIS_DISK_FREE_BYTES} ]""\n\n";

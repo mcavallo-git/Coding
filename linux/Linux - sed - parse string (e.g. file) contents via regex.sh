0318be8374220ed52b023e19f8215d15e311b1c5
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-10 11:42:11 -0600

    Update Linux - sed - parse string (e.g. file) contents via regex.sh

--- linux/Linux - sed - parse string (e.g. file) contents via regex.sh
+++ linux/Linux - sed - parse string (e.g. file) contents via regex.sh
@@ -168,11 +168,11 @@ echo "GnuPG_KeyIDs=\"${GnuPG_KeyIDs}\"";
 # Example)  Parse nginx runtime user's name from nginx.conf
 #
 if [ -f "/etc/nginx/nginx.conf" ]; then
-	NGINX_UNAME=$(sed -rne 's/^user ([a-z_][a-z0-9_\-]{0,30}[a-z0-9_\-\$]?)\s*;\s*$/\1/p' "/etc/nginx/nginx.conf");
-	# NGINX_UNAME=$(sed -rne 's/^user ([a-z_][a-z0-9_\-]{0,30}[a-z0-9_\-\$]?)\s*;\s*$/\1/p' "/etc/nginx/nginx.conf");
-	NGINX_GNAME=$(id -gn "${NGINX_UNAME}");
-	NGINX_UID=$(id -u "${NGINX_UNAME}");
-	NGINX_GID=$(id -g "${NGINX_UNAME}");
+	NGINX_UNAME=$(sed -rne 's/^user ([a-z_][a-z0-9_\-]{0,30}[a-z0-9_\-\$]?)\s*;\s*$/\1/p' "/etc/nginx/nginx.conf";);
+	# NGINX_UNAME=$(sed -rne 's/^user ([a-z_][a-z0-9_\-]{0,30}[a-z0-9_\-\$]?)\s*;\s*$/\1/p' "/etc/nginx/nginx.conf";);
+	NGINX_GNAME=$(id -gn "${NGINX_UNAME}";);
+	NGINX_UID=$(id -u "${NGINX_UNAME}";);
+	NGINX_GID=$(id -g "${NGINX_UNAME}";);
 fi;
 
 

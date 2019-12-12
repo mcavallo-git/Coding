cdbaf43b79044f24422979d82347d7e0f71f6c76
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-12 17:09:08 -0600

    Update Linux - firewall-cmd.sh

--- linux/Linux - firewall-cmd.sh
+++ linux/Linux - firewall-cmd.sh
@@ -9,18 +9,30 @@ systemctl start firewalld;
 
 
 # ------------------------------------------------------------
-
 # List everything added for or enabled in zone. If zone is omitted, default zone will be used
 firewall-cmd --list-all;
 
+
+# ------------------------------------------------------------
 # Print currently active zones altogether with interfaces and sources used in these zones. Active zones are zones, that have a binding to an interface or source
 firewall-cmd --get-active-zones;
 
-# List known services
-firewall-cmd --get-services | tr " " "\n" > ~/firewall-cmd.get-services.$(hostname).log;
 
-# List known icmptypes
-firewall-cmd --get-icmptypes | tr " " "\n" > ~/firewall-cmd.get-icmptypes.$(hostname).log;
+# ------------------------------------------------------------
+# Services
+
+LOGFILE_SERVICES="${HOME}/firewall-cmd --info-services.$(hostname).log"; \
+echo "" > "${LOGFILE_SERVICES}"; \
+for EACH_FIREWALL_SERVICE in $(firewall-cmd --get-services | tr ' ' '\n';); do \
+echo "------------------------------------------------------------" >> "${LOGFILE_SERVICES}"; \
+firewall-cmd --info-service="${EACH_FIREWALL_SERVICE}" >> "${LOGFILE_SERVICES}"; \
+done;
+
+
+# ------------------------------------------------------------
+# ICMP Types
+LOGFILE_ICMP_TYPES="${HOME}/firewall-cmd.get-icmptypes.$(hostname).log";
+firewall-cmd --get-icmptypes | tr " " "\n" > "${LOGFILE_ICMP_TYPES}";
 
 
 # ------------------------------------------------------------

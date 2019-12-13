05c2cbbc5d25c0dd5e3fa48712a8b794ebf00b63
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-13 08:04:18 -0600

    Update Namecheap-DDNS-Updater.ps1

--- ddns/Namecheap-DDNS-Updater.ps1
+++ ddns/Namecheap-DDNS-Updater.ps1
@@ -1,26 +1,26 @@
-# ----------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------
 #
 #### Refer to "README.md"
 #
-# ----------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------
 #	Single Line (for use w/ Task Scheduler):
 
 
 PowerShell -Command "ForEach ($LocalUser In (Get-ChildItem ('C:/Users'))) { If (Test-Path (($LocalUser.FullName)+('/.namecheap/secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((($LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse();}} Exit 0;"
 
 
-# ----------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------
 # Expanded (same as single line, above)
 
 
 # PowerShell -Command "
 ForEach ($LocalUser In (Get-ChildItem ('C:/Users'))) {
-	If (Test-Path (($LocalUser.FullName)+('/.namecheap/secret'))) {
+	If (Test-Path ((${LocalUser}.FullName)+('/.namecheap/secret'))) {
 		[System.Net.WebRequest]::Create(
 			[System.Text.Encoding]::Unicode.GetString(
 				[System.Convert]::FromBase64String(
 					(Get-Content(
-						(($LocalUser.FullName)+('/.namecheap/secret')))
+						((${LocalUser}.FullName)+('/.namecheap/secret')))
 					)
 				)
 			)
@@ -31,36 +31,27 @@ Exit 0;
 # "
 
 
-# ----------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------
 #	Creating the Credentials file(s) - Read-in the user-specific hostname/domain-name/token from the credentials file(s), below
 
 
-$nc_subdomain = Get-Content -Path (($Home)+("/.namecheap/subdomain"));
-$nc_domain = Get-Content -Path (($Home)+("/.namecheap/domain"));
-$nc_token = Get-Content -Path (($Home)+("/.namecheap/token"));
-$nc_ip = "";
-
-$nc_urlPlaintext = (("https://dynamicdns.park-your-domain.com/update?host=")+($nc_subdomain)+("&domain=")+($nc_domain)+("&password=")+($nc_token)+("&ip=")+($nc_ip));
-
-$nc_urlBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($nc_urlPlaintext));
-
-New-Item -ItemType "Directory" -Path ("$HOME/.namecheap") -ErrorAction SilentlyContinue; # Credentials - Ensure that parent-directory exists
-
-[IO.File]::WriteAllText(("$HOME/.namecheap/secret"),($nc_urlBase64)); # Credentials - Output the base-64 encoded string into the final url-file
+$nc_subdomain = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/subdomain");
+$nc_domain = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/domain");
+$nc_token = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/token");
+$nc_urlPlaintext = "https://dynamicdns.park-your-domain.com/update?host=$(${nc_subdomain})&domain=$(${nc_domain})&password=$(${nc_token})&ip=";
+$nc_urlBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(${nc_urlPlaintext}));
+New-Item -ItemType "Directory" -Path ("${Env:USERPROFILE}/.namecheap") -ErrorAction SilentlyContinue; # Credentials - Ensure that parent-directory exists
+[IO.File]::WriteAllText(("${Env:USERPROFILE}/.namecheap/secret"),(${nc_urlBase64})); # Credentials - Output the base-64 encoded string into the final url-file
 
 
-# ----------------------------------------------------------------------------------------------------------------------------------------
 
-#
-#	Example_URL
-# https://dynamicdns.park-your-domain.com/update?host=[host]&domain=[domain_name]&password=[ddns_password]&ip=[your_ip]
-#
+#	Example_URL:  https://dynamicdns.park-your-domain.com/update?host=[host]&domain=[domain_name]&password=[ddns_password]&ip=[your_ip]
 
-# ----------------------------------------------------------------------------------------------------------------------------------------
+
+# ------------------------------------------------------------
 # 
 #	Citation(s)
 #
-#		www.namecheap.com,
-#			"How do I use a browser to dynamically update the host's IP?"
-#			 https://www.namecheap.com/support/knowledgebase/article.aspx/29/11/how-do-i-use-a-browser-to-dynamically-update-the-hosts-ip
+#		namecheap.com  |  "How do I use a browser to dynamically update the host's IP?"  |  https://www.namecheap.com/support/knowledgebase/article.aspx/29/11/how-do-i-use-a-browser-to-dynamically-update-the-hosts-ip
 #
+# ------------------------------------------------------------

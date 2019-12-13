7ff1031a7df0857ff9e09a24d6238c32693cf424
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-13 08:13:44 -0600

    Update Namecheap-DDNS-Updater.ps1

--- ddns/Namecheap-DDNS-Updater.ps1
+++ ddns/Namecheap-DDNS-Updater.ps1
@@ -35,13 +35,13 @@ Exit 0;
 #	Creating the Credentials file(s) - Read-in the user-specific hostname/domain-name/token from the credentials file(s), below
 
 
-$nc_subdomain = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/subdomain");
-$nc_domain = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/domain");
-$nc_token = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/token");
-$nc_urlPlaintext = "https://dynamicdns.park-your-domain.com/update?host=$(${nc_subdomain})&domain=$(${nc_domain})&password=$(${nc_token})&ip=";
-$nc_urlBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(${nc_urlPlaintext}));
-New-Item -ItemType "Directory" -Path ("${Env:USERPROFILE}/.namecheap") -ErrorAction SilentlyContinue; # Credentials - Ensure that parent-directory exists
-[IO.File]::WriteAllText(("${Env:USERPROFILE}/.namecheap/secret"),(${nc_urlBase64})); # Credentials - Output the base-64 encoded string into the final url-file
+$nc_hostname = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/hostname"); `
+$nc_domain = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/domain"); `
+$nc_token = Get-Content -Path ("$(${Env:USERPROFILE})/.namecheap/token"); `
+$nc_urlPlaintext = "https://dynamicdns.park-your-domain.com/update?host=$(${nc_hostname})&domain=$(${nc_domain})&password=$(${nc_token})&ip="; `
+$nc_urlBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(${nc_urlPlaintext})); `
+New-Item -ItemType "Directory" -Path ("${Env:USERPROFILE}/.namecheap") -ErrorAction SilentlyContinue; <# Credentials - Ensure that parent-directory exists #> `
+[IO.File]::WriteAllText(("${Env:USERPROFILE}/.namecheap/secret"),(${nc_urlBase64})); <# Credentials - Output the base-64 encoded string into the final url-file #>
 
 
 

a30d31dd27c4e43d11a830957adfdc1e3fb52216
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-20 17:52:55 -0600

    Update Linux - sed - parse string (e.g. file) contents via regex.sh

--- linux/Linux - sed - parse string (e.g. file) contents via regex.sh
+++ linux/Linux - sed - parse string (e.g. file) contents via regex.sh
@@ -217,7 +217,6 @@ fi;
 #
 if [ -f "/etc/nginx/nginx.conf" ]; then
 NGINX_UNAME=$(sed -rne 's/^user ([a-z_][a-z0-9_\-]{0,30}[a-z0-9_\-\$]?)\s*;\s*$/\1/p' "/etc/nginx/nginx.conf";);
-# NGINX_UNAME=$(sed -rne 's/^user ([a-z_][a-z0-9_\-]{0,30}[a-z0-9_\-\$]?)\s*;\s*$/\1/p' "/etc/nginx/nginx.conf";);
 NGINX_GNAME=$(id -gn "${NGINX_UNAME}";);
 NGINX_UID=$(id -u "${NGINX_UNAME}";);
 NGINX_GID=$(id -g "${NGINX_UNAME}";);

f82a79ff5b013cbac5b0e86b0fc672a5a3df227e
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-20 17:56:15 -0600

    Update Linux - sed - parse string (e.g. file) contents via regex.sh

--- linux/Linux - sed - parse string (e.g. file) contents via regex.sh
+++ linux/Linux - sed - parse string (e.g. file) contents via regex.sh
@@ -204,7 +204,7 @@ echo "GnuPG_KeyIDs=\"${GnuPG_KeyIDs}\"";
 #
 if [ $(which java 1>'/dev/null' 2>&1; echo $?;) -eq 0 ]; then
 JAVA_ALL_SETTINGS="$(java -XshowSettings:properties -version 2>&1)"; echo "JAVA_ALL_SETTINGS = [ ${JAVA_ALL_SETTINGS} ]";  # Note: Java outputs to STDERR (2) by default - Redirect its output to STDOUT (1) by using 2>&1
-LOCAL_JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 | sed -rne 's/^\s*java.home = (.+)\s*$/\1/p';); echo "LOCAL_JAVA_HOME = [ ${LOCAL_JAVA_HOME} ]"; 
+LOCAL_JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 | sed -rne 's/^\s*java.home = (.+)\s*$/\1/p';); echo "LOCAL_JAVA_HOME = [ ${LOCAL_JAVA_HOME} ]";  # Note: Java outputs to STDERR (2) by default - Redirect its output to STDOUT (1) by using 2>&1
 fi;
 
 

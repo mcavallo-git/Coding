b71bf3f5fece7284b635cf1de89411298651ef18
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-20 17:55:59 -0600

    Update Linux - sed - parse string (e.g. file) contents via regex.sh

--- linux/Linux - sed - parse string (e.g. file) contents via regex.sh
+++ linux/Linux - sed - parse string (e.g. file) contents via regex.sh
@@ -200,11 +200,10 @@ echo "GnuPG_KeyIDs=\"${GnuPG_KeyIDs}\"";
 
 # ------------------------------------------------------------
 # 
-# Example)  Determine local Java runtime's home-directory ( $JAVA_HOME )
+# Example)  Locate $JAVA_HOME (local Java runtime's home-directory)
 #
 if [ $(which java 1>'/dev/null' 2>&1; echo $?;) -eq 0 ]; then
-# Note: Java outputs to STDERR (2) by default - Redirect its output to STDOUT (1) by using 2>&1
-JAVA_ALL_SETTINGS="$(java -XshowSettings:properties -version 2>&1)"; echo "JAVA_ALL_SETTINGS = [ ${JAVA_ALL_SETTINGS} ]"; 
+JAVA_ALL_SETTINGS="$(java -XshowSettings:properties -version 2>&1)"; echo "JAVA_ALL_SETTINGS = [ ${JAVA_ALL_SETTINGS} ]";  # Note: Java outputs to STDERR (2) by default - Redirect its output to STDOUT (1) by using 2>&1
 LOCAL_JAVA_HOME=$(java -XshowSettings:properties -version 2>&1 | sed -rne 's/^\s*java.home = (.+)\s*$/\1/p';); echo "LOCAL_JAVA_HOME = [ ${LOCAL_JAVA_HOME} ]"; 
 fi;
 

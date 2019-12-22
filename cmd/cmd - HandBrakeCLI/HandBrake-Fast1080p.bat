a60040b6df83fd4386567f9d2f651b32f9e96d8d
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-22 07:01:17 -0600

    Create HandBrake-Fast1080p.bat

---
+++ cmd/cmd - HandBrakeCLI/HandBrake-Fast1080p.bat
@@ -0,0 +1,70 @@
+@ECHO OFF
+REM		!!! Prerequisite !!! 
+REM			HandBrakeCLI must be installed for this script to function as-intended
+REM			https://handbrake.fr/downloads2.php
+
+
+
+REM "C:\Program Files\HandBrake\HandBrakeCLI.exe" --preset "Fast 1080p30"
+
+
+
+REM ------------------------------------------------------------
+
+for /R H:\Video\ %%F in (*.mkv) do (
+
+"C:\Program Files\HandBrake\HandBrakeCLI.exe" --preset-import-file "C:\Users\user1\Desktop\bluraypreset.json" -Z "bluraypreset" -i "%%~fF" -o "%%~dpF%%~nF_conv.mkv"
+
+if exist "%%~dpF%%~nF_conv.mkv" (
+	del "%%~fF"
+	ren "%%~dpF%%~nF_conv.mkv" "%%~nxF"
+)
+
+)
+
+
+REM		Input & Output Directories
+SET "INPUT_DIR=Input"
+SET "OUTPUT_DIR=Output"
+
+REM		--= ImageMagick  :::  Trim the Image(s) ==-
+SET EXT="bmp"
+FOR %%I IN ("%INPUT_DIR%\*.%EXT%") DO (
+	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.%EXT%"
+	magick "%%I" -gravity south -chop 0x%TrimBottom% -gravity north -chop 0x%TrimTop% -gravity east -chop %TrimRight%x0 -gravity west -chop %TrimLeft%x0 -resize %ResizeToWidth% "%OUTPUT_DIR%\%%~nI.%EXT%"
+	DEL /f "%%I"
+	ECHO.
+)
+SET EXT="jpg"
+FOR %%I IN ("%INPUT_DIR%\*.%EXT%") DO (
+	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.%EXT%"
+	magick "%%I" -gravity south -chop 0x%TrimBottom% -gravity north -chop 0x%TrimTop% -gravity east -chop %TrimRight%x0 -gravity west -chop %TrimLeft%x0 -resize %ResizeToWidth% "%OUTPUT_DIR%\%%~nI.%EXT%"
+	DEL /f "%%I"
+	ECHO.
+)
+SET EXT="png"
+FOR %%I IN ("%INPUT_DIR%\*.%EXT%") DO (
+	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.%EXT%"
+	magick "%%I" -gravity south -chop 0x%TrimBottom% -gravity north -chop 0x%TrimTop% -gravity east -chop %TrimRight%x0 -gravity west -chop %TrimLeft%x0 -resize %ResizeToWidth% "%OUTPUT_DIR%\%%~nI.%EXT%"
+	DEL /f "%%I"
+	ECHO.
+)
+
+REM	Open the output directory once finished
+START explorer.exe "%OUTPUT_DIR%"
+
+TIMEOUT /T 30
+
+EXIT
+
+REM [ToDo] Determine if images are portrait or landscape via imagick
+REM
+REM ECHO test width via [magick]:
+REM
+REM magick identify -format '%w' "%OUTPUT_DIR%\%%---TILDE---nI.!IMG_EXT!"
+REM
+REM magick identify -format '%w' '%image%'
+REM
+REM magick "%%I" -gravity south -chop 0x%TrimBottom% -gravity north -chop 0x%TrimTop% -gravity east -chop %TrimRight%x0 -gravity west -chop %TrimLeft%x0 -resize %ResizeToWidth% "%OUTPUT_DIR%\%%~nI.!IMG_EXT!"
+REM
+REM DEL /f "%%I"

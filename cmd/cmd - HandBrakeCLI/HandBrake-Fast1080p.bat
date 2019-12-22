8c2e20495e9845e88c5ff0dea78dacc735e0d6f4
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-22 07:15:24 -0600

    Update HandBrake-Fast1080p.bat

--- cmd/cmd - HandBrakeCLI/HandBrake-Fast1080p.bat
+++ cmd/cmd - HandBrakeCLI/HandBrake-Fast1080p.bat
@@ -1,63 +1,58 @@
 @ECHO OFF
-REM
-REM		!!! Prerequisite !!!
-REM
-REM			HandBrakeCLI must be installed for this script to function as-intended
-REM 
-REM			Download [ HandBrakeCLI ] Application from URL [ https://handbrake.fr/downloads2.php ]
-REM
-REM			Extract [ HandBrakeCLI.exe ] from aforementioned URL (downloads as a .zip archive as-of 20191222-070342 CST)
-REM	
-REM			Place the extracted file at filepath [ C:\Program Files\HandBrake\HandBrakeCLI.exe ]
-REM 
+REM ------------------------------------------------------------
+REM	!!! Prerequisite !!!
+REM		HandBrakeCLI must be installed for this script to function as-intended
+REM		Download [ HandBrakeCLI ] Application from URL [ https://handbrake.fr/downloads2.php ]
+REM		Extract [ HandBrakeCLI.exe ] from aforementioned URL (downloads as a .zip archive as-of 20191222-070342 CST)
+REM		Place the extracted file at filepath [ C:\Program Files\HandBrake\HandBrakeCLI.exe ]
+REM ------------------------------------------------------------
 
 
 REM "C:\Program Files\HandBrake\HandBrakeCLI.exe" --preset "Fast 1080p30"
 
+REM Preset
+SET "PRESET_NAME=Fast 1080p30"
+SET "OUTPUT_EXT=mp4"
 
-
-REM ------------------------------------------------------------
-
-for /R H:\Video\ %%F in (*.mkv) do (
-
-"C:\Program Files\HandBrake\HandBrakeCLI.exe" --preset-import-file "C:\Users\user1\Desktop\bluraypreset.json" -Z "bluraypreset" -i "%%~fF" -o "%%~dpF%%~nF_conv.mkv"
-
-if exist "%%~dpF%%~nF_conv.mkv" (
-	del "%%~fF"
-	ren "%%~dpF%%~nF_conv.mkv" "%%~nxF"
-)
-
-)
-
-
-REM		Input & Output Directories
+REM Input & Output Dirs
 SET "INPUT_DIR=Input"
 SET "OUTPUT_DIR=Output"
 
 REM		--= ImageMagick  :::  Trim the Image(s) ==-
-SET EXT="bmp"
+SET EXT="VOB"
 FOR %%I IN ("%INPUT_DIR%\*.%EXT%") DO (
 	ECHO|set /p="   %%I  --->  %OUTPUT_DIR%\%%~nI.%EXT%"
-	magick "%%I" -gravity south -chop 0x%TrimBottom% -gravity north -chop 0x%TrimTop% -gravity east -chop %TrimRight%x0 -gravity west -chop %TrimLeft%x0 -resize %ResizeToWidth% "%OUTPUT_DIR%\%%~nI.%EXT%"
-	DEL /f "%%I"
+	"C:\Program Files\HandBrake\HandBrakeCLI.exe" --preset "%PRESET_NAME%" -i "%%I" -o "%OUTPUT_DIR%\%%~nI.%OUTPUT_EXT%"
+	IF EXIST "%OUTPUT_DIR%\%%~nI.%OUTPUT_EXT%" (
+		DEL /f "%%I"
+	)
 	ECHO.
 )
 
-REM	Open the output directory once finished
+REM ------------------------------------------------------------
+REM 
+REM ### Original Script (see Citation(s), below)
+REM 
+REM FOR /R H:\Video\ %%F IN (*.mkv) DO (
+REM 	"C:\Program Files\HandBrake\HandBrakeCLI.exe" --preset "Fast 1080p30" -i "%%~fF" -o "%%~dpF%%~nF_conv.mkv"
+REM 	IF EXIST "%%~dpF%%~nF_conv.mkv" (
+REM 		DEL "%%~fF"
+REM 		REN "%%~dpF%%~nF_conv.mkv" "%%~nxF"
+REM 	)
+REM )
+
+
+REM ------------------------------------------------------------
+REM	Once finished, open the output directory
 START explorer.exe "%OUTPUT_DIR%"
-
 TIMEOUT /T 30
-
 EXIT
 
-REM [ToDo] Determine if images are portrait or landscape via imagick
+
+REM ------------------------------------------------------------
 REM
-REM ECHO test width via [magick]:
+REM Citation(s)
 REM
-REM magick identify -format '%w' "%OUTPUT_DIR%\%%---TILDE---nI.!IMG_EXT!"
+REM   reddit.com  |  "A HandBrake script to run through subfolders and convert to a custom preset"  |  https://www.reddit.com/r/PleX/comments/9anvle/a_handbrake_script_to_run_through_subfolders_and/
 REM
-REM magick identify -format '%w' '%image%'
-REM
-REM magick "%%I" -gravity south -chop 0x%TrimBottom% -gravity north -chop 0x%TrimTop% -gravity east -chop %TrimRight%x0 -gravity west -chop %TrimLeft%x0 -resize %ResizeToWidth% "%OUTPUT_DIR%\%%~nI.!IMG_EXT!"
-REM
-REM DEL /f "%%I"
+REM ------------------------------------------------------------

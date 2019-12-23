8c91499cfbbef3f6f705932e468f2bdb5f20e5f7
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-23 12:08:50 -0600

    Update Linux - Don't use the [ which ] command.sh

--- linux/Linux - Don't use the [ which ] command.sh
+++ linux/Linux - Don't use the [ which ] command.sh
@@ -5,13 +5,14 @@
 #
 
 COMMAND_NAME="foo"; \
-echo ""; \
+echo -e "\n\n"; \
 command -v foo >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "[ command -v ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "[ command -v ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
-echo ""; \
+echo -e "\n\n"; \
 type foo >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "[ type ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "[ type ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
-echo ""; \
-hash foo 2>/dev/null && { RET_CODE=$?; echo >&2 "[ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "[ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; 
-echo "";
+echo -e "\n\n"; \
+hash foo 2>/dev/null && { RET_CODE=$?; echo >&2 "[ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "[ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
+echo -e "\n\n";
+
 
 
 # ------------------------------------------------------------

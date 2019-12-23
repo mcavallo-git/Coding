217dc4e377dd744823de1c43c2e0da33472a28fb
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-23 12:15:08 -0600

    Update Linux - Don't use the [ which ] command.sh

--- linux/Linux - Don't use the [ which ] command.sh
+++ linux/Linux - Don't use the [ which ] command.sh
@@ -4,7 +4,7 @@
 # Alternatives for the  [ which ]  command, to adequately test the existence of local commands, methods, programs, etc.
 #
 
-
+### Force a falsey-condition by testing a command which is known not to exist
 COMMAND_NAME=foo && \
 echo -e "\n\n" && \
 command -v ${COMMAND_NAME} >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
@@ -15,6 +15,7 @@ hash ${COMMAND_NAME} 2>/dev/null && { RET_CODE=$?; echo >&2 "Calling [ hash ${CO
 echo -e "\n\n";
 
 
+### Force a truthy-condition by testing a command which is known to exist
 COMMAND_NAME=find && \
 echo -e "\n\n" && \
 command -v ${COMMAND_NAME} >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \

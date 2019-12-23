db361c04059cfc62bc4d18d64a63c8da0d3ba575
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-22 20:36:00 -0600

    Create Wake-on-LAN.psm1

---
+++ powershell/_WindowsPowerShell/Modules/Wake-on-LAN/Wake-on-LAN.psm1
@@ -0,0 +1,48 @@
+#
+#	PowerShell - Wake-on-LAN
+#		|
+#		|--> Description:  Sends a Wake-on-LAN Magic Packet to a MAC Address which is specified by the user at runtime
+#		|
+#		|--> Example:     PowerShell -Command ("Show `$MyInvocation -Methods")
+#
+Function Wake-on-LAN() {
+	Param(
+		[Parameter(Position=0, ValueFromRemainingArguments)]$mac # Address of the network card (MAC address)
+	)
+
+	# Validate the Syntax of the user-defined MAC Address
+	If (!(($mac -Like "*:*:*:*:*:*") -Or ($mac -Like "*-*-*-*-*-*"))){
+		Write-Error "MAC address not in correct format";
+
+	} Else {
+		# Build Magic Packet http://en.wikipedia.org/wiki/Wake-on-LAN#Magic_packet
+		$string=@($mac.Split(":""-") | ForEach {$_.Insert(0,"0x")});
+		$target = [Byte[]]($string[0], $string[1], $string[2], $string[3], $string[4], $string[5]);
+
+		# The Magic Packet is a broadcast frame containing anywhere within its payload 6 bytes of all 255 (FF FF FF FF FF FF in hexadecimal) ...
+		$packet = [Byte[]](,0xFF * 102);
+
+		# ... Followed by sixteen repetitions of the target computer's 48-bit MAC address, for a total of 102 bytes.
+		6..101 |% { $packet[$_] = $target[($_%6)]};
+
+		# .NET framework lib para sockets
+		$UDPclient = New-Object System.Net.Sockets.UdpClient;
+		$UDPclient.Connect(([System.Net.IPAddress]::Broadcast),4000);
+		$UDPclient.Send($packet, $packet.Length) | Out-Null;
+
+	}
+
+	Return;
+
+}
+Export-ModuleMember -Function "Wake-on-LAN";
+# Install-Module -Name "Wake-on-LAN"
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.spiceworks.com  |  "WOL from command line not working"  |  https://community.spiceworks.com/topic/538390-wol-from-command-line-not-working?page=1#entry-3540406
+#
+# ------------------------------------------------------------

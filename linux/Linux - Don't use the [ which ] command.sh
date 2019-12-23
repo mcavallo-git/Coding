dfcc406ba3713fb64576f0d7e338bd9170195c0b
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-23 12:09:17 -0600

    Update Linux - Don't use the [ which ] command.sh

--- linux/Linux - Don't use the [ which ] command.sh
+++ linux/Linux - Don't use the [ which ] command.sh
@@ -6,11 +6,11 @@
 
 COMMAND_NAME="foo"; \
 echo -e "\n\n"; \
-command -v foo >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "[ command -v ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "[ command -v ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
+command -v foo >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
 echo -e "\n\n"; \
-type foo >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "[ type ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "[ type ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
+type foo >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ type ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ type ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
 echo -e "\n\n"; \
-hash foo 2>/dev/null && { RET_CODE=$?; echo >&2 "[ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "[ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
+hash foo 2>/dev/null && { RET_CODE=$?; echo >&2 "Calling [ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ hash ${COMMAND_NAME} ] had a return code of [ ${RET_CODE} ] & evaluated as [ falsey ]"; }; \
 echo -e "\n\n";
 
 

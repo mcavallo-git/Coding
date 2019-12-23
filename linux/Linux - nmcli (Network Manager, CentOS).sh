a30d40afa9079978891d607e4876efe135ab8637
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-23 11:23:31 -0600

    Update Linux - nmcli (Network Manager, CentOS).sh

--- linux/Linux - nmcli (Network Manager, CentOS).sh
+++ linux/Linux - nmcli (Network Manager, CentOS).sh
@@ -3,3 +3,5 @@
 nmcli device show;
 
 nmcli connection show $(nmcli connection show | grep -v "NAME" | head -n 1 | awk '{print $1}';);
+
+nmtui; # Graphical interface to enable-disable NICs on-the-fly

bd73abe69a996dc2c7c73cbc667a23c7db1b8cdd
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-23 14:52:39 -0600

    Update Linux - stat (determine target filepath's read-write permissions, user & group ownership, last modified date, etc.).sh

--- linux/Linux - stat (determine target filepath's read-write permissions, user & group ownership, last modified date, etc.).sh
+++ linux/Linux - stat (determine target filepath's read-write permissions, user & group ownership, last modified date, etc.).sh
@@ -1,1 +1,13 @@
-# Refer to Runtime "get_permissions" @ https://raw.githubusercontent.com/mcavallo-git/cloud-infrastructure/master/usr/local/bin/get_permissions
+#!/bin/bash
+# 
+# ------------------------------------------------------------
+
+
+# For user/group file ownership, refer to "get_permissions" @ https://raw.githubusercontent.com/mcavallo-git/cloud-infrastructure/master/usr/local/bin/get_permissions
+
+
+# %w     time of file birth, human-readable; - if unknown
+# %x     time of last access, human-readable
+# %y     time of last data modification, human-readable
+# %z     time of last status change, human-readable
+stat --dereference --format="%w   %x   %y   %z" "${HOME}";

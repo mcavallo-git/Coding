f91979e05c4137853ea43e7e4db938d6f2d3efba
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-27 12:23:16 -0600

    Update PowerShell - AutoStart VMware VMs on-boot (e.g. on-startup, or on-power-on).ps1

--- powershell/PowerShell - AutoStart VMware VMs on-boot (e.g. on-startup, or on-power-on).ps1
+++ powershell/PowerShell - AutoStart VMware VMs on-boot (e.g. on-startup, or on-power-on).ps1
@@ -1,10 +1,26 @@
 # ------------------------------------------------------------
 #  AutoStart one (or more) VMs on Windows Startup
 # ------------------------------------------------------------
+#
+# Verbose Syntax, using variables and spread-out commands for easier reading
+#
 
-$VMwareStartupList_Vmx="${HOME}\.vmware\startup-vmx.txt"; # 
+$VMwareStartupList_Vmx="${HOME}\.vmware\startup-vmx.txt";
+
 Get-Content -Path "${VMwareStartupList_Vmx}" | ForEach-Object {
-$VMwareHostTypes = @{ Workstation="wt"; Fusion="fusion"; Player="player"; };
-"C:\Windows\System32\CMD" /C "'C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe' -T $(${VMwareHostTypes}.Workstation) start '$_'";
+
+	$VMwareHostTypes = @{ Workstation="wt"; Fusion="fusion"; Player="player"; };
+
+	"C:\Windows\System32\CMD" /C "'C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe' -T $(${VMwareHostTypes}.Workstation) start '$_'";
+
 };
 
+# ------------------------------------------------------------
+#
+# As a one-liner
+#
+
+Get-Content -Path "${HOME}\.vmware\startup-vmx.txt" | ForEach-Object { "C:\Windows\System32\CMD" /C "'C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe' -T wt start '$_'"; };
+
+
+# ------------------------------------------------------------

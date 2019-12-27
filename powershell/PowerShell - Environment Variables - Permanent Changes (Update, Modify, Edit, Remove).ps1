fca549167fdf994d1f4b8800dd35c61a2a2f4ee2
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-27 16:22:18 -0600

    rn

--- powershell/PowerShell - Environment Variables - Permanent Changes (Update, Modify, Edit, Remove).ps1
+++ powershell/PowerShell - Environment Variables - Permanent Changes (Update, Modify, Edit, Remove).ps1
@@ -3,12 +3,14 @@
 # PERMANENTLY modify user/system environment variables in Windows
 # 
 
-### Modify a system environment variable
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
+
+$Env:Path += ";C:\Program Files (x86)\VMware\VMware Workstation";  # Temporarily APPENDS TO existing path
+[Environment]::SetEnvironmentVariable("Path", $Env:Path, [System.EnvironmentVariableTarget]::Machine);  # Permanently updates a system environment variable
 
 
-### Modify a user environment variable
-[Environment]::SetEnvironmentVariable("INCLUDE", $env:INCLUDE, [System.EnvironmentVariableTarget]::User)
+$Env:Path += ";C:\Program Files (x86)\VMware\VMware Workstation";
+C:\Users\matthew.cavallo\Desktop\2019-12-27 16_20_01-Environment Variables.png
+[Environment]::SetEnvironmentVariable("INCLUDE", $Env:INCLUDE, [System.EnvironmentVariableTarget]::User);  # Permanently updates a user environment variable
 
 
 # ------------------------------------------------------------
@@ -16,8 +18,8 @@
 # TEMPORARILY modify session environment variables in Windows
 # 
 
-$env:Path = "SomeRandomPath";  # (temporarily REPLACES existing path)
-$env:Path += ";SomeRandomPath";  # (temporarily APPENDS TO existing path)
+$Env:Path = "C:\Trash";  # Temporarily REPLACES existing path
+$Env:Path += ";C:\Program Files (x86)\VMware\VMware Workstation";  # Temporarily APPENDS TO existing path
 
 
 # ------------------------------------------------------------

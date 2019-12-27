3470806be3b6209cb43f61a797b885cc56fac66c
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-27 12:36:38 -0600

    Update PowerShell - AutoStart VMware VMs on-boot (e.g. on-startup, or on-power-on).ps1

--- powershell/PowerShell - AutoStart VMware VMs on-boot (e.g. on-startup, or on-power-on).ps1
+++ powershell/PowerShell - AutoStart VMware VMs on-boot (e.g. on-startup, or on-power-on).ps1
@@ -2,6 +2,14 @@
 #  AutoStart one (or more) VMs on Windows Startup
 # ------------------------------------------------------------
 #
+# In your user's home-directory, create a folder named ".vmware"
+# Within the ".vmware" folder, create a file named "startup-vmx.txt"
+# Within "startup-vmx.txt", place the fullpath locations of any VMs' [ .vmx ] files which you wish to start on-boot (such as "E:\Images\Windows10.vmx")
+# Create a new basic scheduled Task via Windows' "Task Scheduler" to run the bottom one-liner to grab your newly created "startup-vmx.txt" and start them at-boot (select on pc startup as task trigger)
+# 
+# 
+# ------------------------------------------------------------
+#
 # Verbose Syntax, using variables and spread-out commands for easier reading
 #
 
@@ -20,7 +28,7 @@ Get-Content -Path "${VMwareStartupList_Vmx}" | ForEach-Object {
 # As a one-liner
 #
 
-Get-Content -Path "${HOME}\.vmware\startup-vmx.txt" | ForEach-Object { "C:\Windows\System32\CMD" /C "'C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe' -T wt start '$_'"; };
+PowerShell.exe -Command 'Get-Content -Path "${HOME}\.vmware\startup-vmx.txt" | ForEach-Object { "C:\Windows\System32\cmd.exe" /C "`"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe`" -T wt start `"$_`""; })';
 
 
 # ------------------------------------------------------------

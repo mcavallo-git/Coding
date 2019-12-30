6d4b1fa60a46df86b6644f71c82fa0b53c6aba12
Matt Cavallo <mcavallo.git@gmail.com>
2019-12-30 08:22:11 -0600

    Update cmd - VSCode-Workspace.bat

--- cmd/cmd - VSCode-Workspace.bat
+++ cmd/cmd - VSCode-Workspace.bat
@@ -6,6 +6,14 @@ REM  "%USERPROFILE%\Documents\GitHub\Coding\cmd\cmd - VSCode-Workspace.bat"
 REM
 REM
 REM ------------------------------------------------------------
+REM
+REM TO VIEW LOGS, REPLACE:
+REM   1>NUL 2>&1
+REM
+REM WITH:
+REM   1>>%USERPROFILE%\Desktop\LOGFILE.log 2>>&1
+REM
+REM ------------------------------------------------------------
 
 SET /A VSCODE_ALREADY_RUNNING=0
 FOR /F "tokens=2-2 delims= " %%A IN ('TASKLIST /NH /FI "IMAGENAME eq Code.exe" ^| MORE +3') DO (
@@ -14,7 +22,8 @@ SET /A VSCODE_ALREADY_RUNNING=1
 
 IF %VSCODE_ALREADY_RUNNING% EQU 0 (
 ECHO VSCode not found to be on its feet 1>NUL 2>&1
-START "C:\Program Files\Microsoft VS Code\Code.exe" "%USERPROFILE%\Documents\GitHub\cloud-infrastructure\.vscode\github.code-workspace" | ECHO 1>NUL 2>&1
+ECHO Calling  [ "C:\Program Files\Microsoft VS Code\Code.exe" "%USERPROFILE%\Documents\GitHub\cloud-infrastructure\.vscode\github.code-workspace" ]  1>NUL 2>&1
+"C:\Program Files\Microsoft VS Code\Code.exe" "%USERPROFILE%\Documents\GitHub\cloud-infrastructure\.vscode\github.code-workspace" | ECHO 1>NUL 2>&1
 FOR /L %%N IN (1,1,10) DO (
 FOR /F "tokens=2-2 delims= " %%A IN ('TASKLIST /NH /FI "IMAGENAME eq Code.exe" ^| MORE +3') DO (
 ECHO VSCode now on its feet 1>NUL 2>&1
@@ -28,12 +37,15 @@ TIMEOUT /T 1 | ECHO 1>NUL 2>&1
 :OPEN_TARGET_FILE
 IF ""%1"" == """" (
 ECHO No Inline Arguments - Starting Code-Workspace 1>NUL 2>&1
-START "C:\Program Files\Microsoft VS Code\Code.exe" "%USERPROFILE%\Documents\GitHub\cloud-infrastructure\.vscode\github.code-workspace" | ECHO 1>NUL 2>&1
+ECHO Calling  [ "C:\Program Files\Microsoft VS Code\Code.exe" "%USERPROFILE%\Documents\GitHub\cloud-infrastructure\.vscode\github.code-workspace" ]  1>NUL 2>&1
+"C:\Program Files\Microsoft VS Code\Code.exe" "%USERPROFILE%\Documents\GitHub\cloud-infrastructure\.vscode\github.code-workspace" | ECHO 1>NUL 2>&1
 ) ELSE (
-ECHO At least 1 Inline Argument - Starting Code.exe with them 1>NUL 2>&1
-START "C:\Program Files\Microsoft VS Code\Code.exe" "%1" | ECHO 1>NUL 2>&1
+ECHO At least 1 Inline Argument:  [ %* ]  1>NUL 2>&1
+ECHO Calling  [ "C:\Program Files\Microsoft VS Code\Code.exe" %* ]  1>NUL 2>&1
+"C:\Program Files\Microsoft VS Code\Code.exe" %* | ECHO 1>NUL 2>&1
 )
 
+EXIT 0
 
 REM ------------------------------------------------------------
 REM

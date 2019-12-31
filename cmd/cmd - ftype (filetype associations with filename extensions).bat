d537bf53ab81059496d9b4da251b47e632b99d57
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-30 19:38:24 -0600

    Update cmd - ftype (filetype associations with filename extensions).bat

--- cmd/cmd - ftype (filetype associations with filename extensions).bat
+++ cmd/cmd - ftype (filetype associations with filename extensions).bat
@@ -4,22 +4,28 @@ REM   cmd - ftype (filetype associations with filename extensions)
 REM
 REM ------------------------------------------------------------
 
+
 REM List file name extension associations active for current session
-assoc
+ftype
 
 
-REM Treat extensionless files as though they were text files
-assoc .=txtfile
+REM Store list of current session's [ filetype associations with filename extensions ] on a file on the desktop
+ftype 1>"%USERPROFILE%\Desktop\cmd.ftype.log"
+notepad "%USERPROFILE%\Desktop\cmd.ftype.log"
 
 
 REM General Syntax
-assoc [.EXTENSION]=[NEW_ASSOCIATION]
+REM      <FileType>            Specifies the file type to display or change.
+REM      <OpenCommandString>   Specifies the open command string to use when opening files of the specified file type.
+REM      /?                    Displays help at the command prompt.
+
+ftype [<FileType>[=[<OpenCommandString>]]]
 
 
 REM ------------------------------------------------------------
 REM
 REM Citation(s)
 REM
-REM   docs.microsoft.com  |  "Displays or modifies file types that are used in file name extension associations"  |  https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/ftype
+REM   docs.microsoft.com  |  "ftype - Displays or modifies file types that are used in file name extension associations"  |  https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/ftype
 REM
 REM ------------------------------------------------------------

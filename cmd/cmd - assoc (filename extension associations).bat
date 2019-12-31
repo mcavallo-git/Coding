51645279b9aef56bce0dcc9f0323de8c7bea8949
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-30 19:38:22 -0600

    Update cmd - assoc (filename extension associations).bat

--- cmd/cmd - assoc (filename extension associations).bat
+++ cmd/cmd - assoc (filename extension associations).bat
@@ -4,22 +4,28 @@ REM   cmd - assoc (filename extension associations)
 REM
 REM ------------------------------------------------------------
 
+
+REM General Syntax
+assoc [.EXTENSION]=[NEW_ASSOCIATION]
+
+
 REM List file name extension associations active for current session
 assoc
 
 
-REM Treat extensionless files as though they were text files
+REM Store list of current session's [ filename extension associations ] on a file on the desktop
+assoc 1>"%USERPROFILE%\Desktop\cmd.assoc.log"
+notepad "%USERPROFILE%\Desktop\cmd.assoc.log"
+
+
+REM Example) Treat extensionless files as though they were text files
 assoc .=txtfile
-
-
-REM General Syntax
-assoc [.EXTENSION]=[NEW_ASSOCIATION]
 
 
 REM ------------------------------------------------------------
 REM
 REM Citation(s)
 REM
-REM   docs.microsoft.com  |  "Displays or modifies file name extension associations"  |  https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/assoc
+REM   docs.microsoft.com  |  "assoc - Displays or modifies file name extension associations"  |  https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/assoc
 REM
 REM ------------------------------------------------------------

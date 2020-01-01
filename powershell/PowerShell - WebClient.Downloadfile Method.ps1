fcf66c1e4b0afb3a05d95bd0d36c1019cc1f85c5
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-01 04:18:21 -0600

    Create PowerShell - WebClient.Downloadfile Method.ps1

---
+++ powershell/PowerShell - WebClient.Downloadfile Method.ps1
@@ -0,0 +1,16 @@
+# ------------------------------------------------------------
+
+# PowerShell - Download File from URL (to the Desktop)
+
+$Download_RemoteUrl = "https://github.com/kohsuke/winsw/releases/download/winsw-v2.2.0/WinSW.NET4.exe";
+$Download_LocalPath = "${Home}\Desktop\NGINX-Service.exe";
+$(New-Object Net.WebClient).DownloadFile("${Download_RemoteUrl}", "${Download_LocalPath}");
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "WebClient.DownloadFile Method - Downloads the resource with the specified URI to a local file"  |  https://docs.microsoft.com/en-us/dotnet/api/system.net.webclient.downloadfile
+#
+# ------------------------------------------------------------

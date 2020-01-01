d45d5558a41529da9bab2647fb2e9899e0619fc1
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-31 18:36:43 -0600

    Update NET_Framework_Check.psm1

--- powershell/_WindowsPowerShell/Modules/NET_Framework_Check/NET_Framework_Check.psm1
+++ powershell/_WindowsPowerShell/Modules/NET_Framework_Check/NET_Framework_Check.psm1
@@ -98,16 +98,16 @@ function NET_Framework_Check {
 	Write-Host -ForegroundColor ("DarkGray") -NoNewLine (" "); <# Start of Line #>
 	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 	$Str=([String]("Version"));
-	Write-Host -ForegroundColor ($Color) -NoNewLine ($Str);
-	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ((" ")+("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"))+(" "));
+	Write-Host -ForegroundColor ($Color) -NoNewLine ((" ")+($Str)+(" "));
+	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"));
 	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 	$Str=([String]("Compatibile?"));
-	Write-Host -ForegroundColor ($Color) -NoNewLine ($Str);
-	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ((" ")+("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"))+(" "));
+	Write-Host -ForegroundColor ($Color) -NoNewLine ((" ")+($Str)+(" "));
+	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"));
 	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 	$Str=([String]("Installed Version"));
-	Write-Host -ForegroundColor ($Color) -NoNewLine ($Str);
-	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ((" ")+("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"))+(" "));
+	Write-Host -ForegroundColor ($Color) -NoNewLine ((" ")+($Str)+(" "));
+	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"));
 	Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 	Write-Host -ForegroundColor ("DarkGray") -NoNewLine (" `n"); <# End of Line #>
 	Write-Host -ForegroundColor ("DarkGray") " |$Dashes|$Dashes|$Dashes| ";
@@ -116,16 +116,16 @@ function NET_Framework_Check {
 		Write-Host -ForegroundColor ("DarkGray") -NoNewLine (" "); <# Start of Line #>
 		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 		$Str=([String]($EachVer));
-		Write-Host -ForegroundColor ($Color) -NoNewLine ($Str);
-		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ((" ")+("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"))+(" "));
+		Write-Host -ForegroundColor ($Color) -NoNewLine ((" ")+($Str)+(" "));
+		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"));
 		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 		$Str=([String]($NetFrameworks[$EachVer].Installed));
-		Write-Host -ForegroundColor ($Color) -NoNewLine ($Str);
-		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ((" ")+("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"))+(" "));
+		Write-Host -ForegroundColor ($Color) -NoNewLine ((" ")+($Str)+(" "));
+		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"));
 		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 		$Str=([String]($NetFrameworks[$EachVer].Version));
-		Write-Host -ForegroundColor ($Color) -NoNewLine ($Str);
-		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ((" ")+("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"))+(" "));
+		Write-Host -ForegroundColor ($Color) -NoNewLine ((" ")+($Str)+(" "));
+		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("".PadRight(($Dashes.Length-(2+($Str).Length)),"-"));
 		Write-Host -ForegroundColor ("DarkGray") -NoNewLine ("|");
 		Write-Host -ForegroundColor ("DarkGray") -NoNewLine (" `n"); <# End of Line #>
 	}

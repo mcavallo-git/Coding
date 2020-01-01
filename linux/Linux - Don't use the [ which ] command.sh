dcbd48c38258801962cc36a67c2db934abb98d29
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-01 02:05:34 -0600

    Update Linux - Don't use the [ which ] command.sh

--- linux/Linux - Don't use the [ which ] command.sh
+++ linux/Linux - Don't use the [ which ] command.sh
@@ -6,26 +6,26 @@
 
 ### Force a falsey-condition by testing a command which is known not to exist
 COMMAND_NAME=foo && \
-echo -e "\n\n" && \
+echo -e "\n\n Using [ command -v ... ]\n" && \
 command -v ${COMMAND_NAME} >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
-echo -e "\n\n" && \
+echo -e "\n\n Using [ type ... ]\n" && \
 type ${COMMAND_NAME} >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ type ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ type ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
-echo -e "\n\n" && \
+echo -e "\n\n Using [ hash ... ]\n" && \
 hash ${COMMAND_NAME} 2>/dev/null && { RET_CODE=$?; echo >&2 "Calling [ hash ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ hash ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
-echo -e "\n\n" && \
+echo -e "\n\n Compare to [ which ... ]\n" && \
 which ${COMMAND_NAME} 2>/dev/null && { RET_CODE=$?; echo >&2 "Calling [ which ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ which ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
 echo -e "\n\n";
 
 
 ### Force a truthy-condition by testing a command which is known to exist
 COMMAND_NAME=cd && \
-echo -e "\n\n" && \
+echo -e "\n\n Using [ command -v ... ]\n" && \
 command -v ${COMMAND_NAME} >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ command -v ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
-echo -e "\n\n" && \
+echo -e "\n\n Using [ type ... ]\n" && \
 type ${COMMAND_NAME} >/dev/null 2>&1 && { RET_CODE=$?; echo >&2 "Calling [ type ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ type ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
-echo -e "\n\n" && \
+echo -e "\n\n Using [ hash ... ]\n" && \
 hash ${COMMAND_NAME} 2>/dev/null && { RET_CODE=$?; echo >&2 "Calling [ hash ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ hash ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
-echo -e "\n\n" && \
+echo -e "\n\n Compare to [ which ... ]\n" && \
 which ${COMMAND_NAME} 2>/dev/null && { RET_CODE=$?; echo >&2 "Calling [ which ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ truthy ]"; } || { RET_CODE=$?; echo >&2 "Calling [ which ${COMMAND_NAME} ] yielded a return code of [ ${RET_CODE} ] and was evaluated as [ falsey ]"; } && \
 echo -e "\n\n";
 

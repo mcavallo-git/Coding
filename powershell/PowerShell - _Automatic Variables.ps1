ad4951438385b8edc441edb16516eb0060a15641
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-01 03:03:09 -0600

    Update PowerShell - _Automatic Variables.ps1

--- powershell/PowerShell - _Automatic Variables.ps1
+++ powershell/PowerShell - _Automatic Variables.ps1
@@ -3,18 +3,15 @@ $MyInvocation.MyCommand.Name; # Current Function / File (w/ extension)
 
 [IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name); # Removes Extension
 
+# See Citation(s) (Below) - especially docs.microsoft.com
 
-
-# See citations, below - especially docs.microsoft.com
 
 # ------------------------------------------------------------
 #
 #	Citation(s)
 #
-#		docs.microsoft.com | About Automatic Variables
-#			https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables?view=powershell-5.1 | 2019-05-21_10-37-41
+#		docs.microsoft.com  |  "About Automatic Variables"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables
 #
-#		ss64.com | Automatic Variables
-#			https://ss64.com/ps/syntax-automatic-variables.html | 2019-05-21_10-36-13
+#		ss64.com  |  "Automatic Variables"  |  https://ss64.com/ps/syntax-automatic-variables.html
 #
-#
+# ------------------------------------------------------------

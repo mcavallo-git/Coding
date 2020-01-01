50d30c92b75dc5c37718009071ae373765af598d
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-01 04:44:16 -0600

    Update HandBrake-Fast1080p.ps1

--- windows/HandBrake/HandBrakeCLI Input-Output/HandBrake-Fast1080p.ps1
+++ windows/HandBrake/HandBrakeCLI Input-Output/HandBrake-Fast1080p.ps1
@@ -13,33 +13,6 @@
 #
 #
 # ------------------------------------------------------------
-#
-# Download HandBrakeCLI
-#
-
-# $Workspace = "${Env:TEMP}";
-$Workspace = "${Home}\Desktop";
-
-$ExeArchive_Url="https://download.handbrake.fr/releases/1.3.0/HandBrakeCLI-1.3.0-win-x86_64.zip";
-
-$ExeArchive_Local=("${Workspace}\$(Split-Path ${ExeArchive_Url} -Leaf)");
-# $ExeArchive_Local=("${Workspace}\HandBrakeCLI.zip");
-
-$ExeArchive_Unpacked=("${Workspace}\$([IO.Path]::GetFileNameWithoutExtension(${ExeArchive_Local}))");
-
-
-# Download HandBrakeCLI
-$(New-Object Net.WebClient).DownloadFile("${ExeArchive_Url}", "${ExeArchive_Local}");
-
-# Unpack the downloaded archive
-Expand-Archive -LiteralPath ("${ExeArchive_Local}") -DestinationPath ("${ExeArchive_Unpacked}");
-
-# Cleanup the archive once it has been unpacked
-# Remove-Item -Path ("${Workspace}\esxi-create-bootmedia.ps1");
-
-Exit
-
-# ------------------------------------------------------------
 # 
 # Instantiate Runtime Variable(s)
 #
@@ -47,46 +20,78 @@ Exit
 
 $ThisScript = (Split-Path $MyInvocation.MyCommand.Name -Leaf);
 $ThisDir = (Split-Path $MyInvocation.MyCommand.Path -Parent);
-
 $InputDir = ("${ThisDir}\Input");
 $OutputDir = ("${ThisDir}\Output");
-$HandBrakeCLI = ("${ThisDir}\HandBrakeCLI.exe");
 
-Set-Location -Path ("${ThisDir}\");
+$HandBrakeCLI = ("${ThisDir}\HandBrakeCLI.exe");
 
 $HandBrake_Preset = "Fast 1080p30";
 
 $OutputExtension = "mp4";
 
 # ------------------------------------------------------------
-# Compress input video(s) using HandBrakeCLI
 
-Get-ChildItem -Path ("${InputDir}\") -Exclude (".gitignore") | ForEach-Object {
-	$EachInputFile = $_.FullName;
-	$EachOutputFile = "${OutputDir}\$($_.BaseName).${OutputExtension}";
-	Write-Host "";
-	Write-Host "`$EachInputFile = [ ${EachInputFile} ]";
-	Write-Host "`$EachOutputFile = [ ${EachOutputFile} ]";
-	$EachConversion = (Start-Process -Wait -FilePath "${HandBrakeCLI}" -ArgumentList "--preset `"${HandBrake_Preset}`" -i `"${EachInputFile}`" -o `"${EachOutputFile}`""); $EachExitCode=$?;
-	If ((Test-Path -Path ("${EachOutputFile}")) -Eq $True) {
-		Remove-Item -Path ("${EachInputFile}") -Force;
+$ExeArchive_Url="https://download.handbrake.fr/releases/1.3.0/HandBrakeCLI-1.3.0-win-x86_64.zip";
+
+$ExeArchive_Local=("${Env:TEMP}\$(Split-Path ${ExeArchive_Url} -Leaf)");
+
+$ExeArchive_Unpacked=("${Env:TEMP}\$([IO.Path]::GetFileNameWithoutExtension(${ExeArchive_Local}))");
+
+# Download HandBrakeCLI
+Write-Host "Downloading  [ ${ExeArchive_Url} ]  to  [ ${ExeArchive_Local} ]  ...";
+$(New-Object Net.WebClient).DownloadFile("${ExeArchive_Url}", "${ExeArchive_Local}");
+
+# Unpack the downloaded archive
+Write-Host "Unpacking  [ ${ExeArchive_Local} ]  into  [ ${ExeArchive_Unpacked} ]  ...";
+Expand-Archive -LiteralPath ("${ExeArchive_Local}") -DestinationPath ("${ExeArchive_Unpacked}");
+
+# Cleanup the archive once it has been unpacked
+# Remove-Item -Path ("${Env:TEMP}\esxi-create-bootmedia.ps1");
+
+$ExeArchive_HandBrakeCLI = (Get-ChildItem -Path ("${ExeArchive_Unpacked}") -Depth (0) -File | Where-Object { $_.Name -Like "*HandBrakeCLI*.exe" } | Select-Object -First (1) -ExpandProperty ("FullName"));
+
+
+If ((Test-Path -Path ("${ExeArchive_HandBrakeCLI}")) -Ne $True) {
+	Write-Host "HandBrakeCLI executable path NOT FOUND";
+	Exit 1;
+
+} Else {
+	Write-Host "HandBrakeCLI executable from  [ ${ExeArchive_HandBrakeCLI} ]  to  [ ${HandBrakeCLI} ]"
+	
+	Move-Item -Path ("${ExeArchive_HandBrakeCLI}") -Destination ("${HandBrakeCLI}");
+
+	# Compress videos from the input directory into the output directory
+	Set-Location -Path ("${ThisDir}\");
+	Get-ChildItem -Path ("${InputDir}\") -Exclude (".gitignore") | ForEach-Object {
+		$EachInputFile = $_.FullName;
+		$EachOutputFile = "${OutputDir}\$($_.BaseName).${OutputExtension}";
+		Write-Host "";
+		Write-Host "`$EachInputFile = [ ${EachInputFile} ]";
+		Write-Host "`$EachOutputFile = [ ${EachOutputFile} ]";
+		$EachConversion = (Start-Process -Wait -FilePath "${HandBrakeCLI}" -ArgumentList "--preset `"${HandBrake_Preset}`" -i `"${EachInputFile}`" -o `"${EachOutputFile}`""); $EachExitCode=$?;
+		If ((Test-Path -Path ("${EachOutputFile}")) -Eq $True) {
+			Remove-Item -Path ("${EachInputFile}") -Force;
+		}
+		Write-Host "";
 	}
-	Write-Host "";
+
+	# Open the exported-files directory
+	Explorer.exe "${OutputDir}";
+
+	# Wait a few seconds (for user to read the terminal, etc.) before exiting
+	Start-Sleep -Seconds 60;
+
+	Exit 0;
+
 }
 
-# ------------------------------------------------------------
-# Once finished, open the output directory
-
-Explorer.exe "${OutputDir}";
-
-Start-Sleep -Seconds 30;
-
-EXIT;
 
 
 # ------------------------------------------------------------
 #
 # Citation(s)
+#
+#   docs.microsoft.com  |  "Get-ChildItem - Gets the items and child items in one or more specified locations"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem
 #
 #   docs.microsoft.com  |  "Split-Path - Returns the specified part of a path"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/split-path
 #

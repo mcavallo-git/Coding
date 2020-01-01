7d7a456e1807ed984ee9d9915f4caabbd70bbcf0
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-01 03:14:32 -0600

    Create PowerShell - Split-Path (basename, dirname, parent-dir, leaf-node).ps1

---
+++ powershell/PowerShell - Split-Path (basename, dirname, parent-dir, leaf-node).ps1
@@ -0,0 +1,14 @@
+
+
+$ThisDir = (Split-Path $MyInvocation.MyCommand.Path -Parent);
+
+$ThisScript = (Split-Path $MyInvocation.MyCommand.Name -Leaf);
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Split-Path - Returns the specified part of a path"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/split-path
+#
+# ------------------------------------------------------------

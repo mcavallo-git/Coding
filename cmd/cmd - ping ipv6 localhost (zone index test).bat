417a499f1005bb9353c6d805feac03b9e9b6270a
Matt Cavallo <mcavallo.work@gmail.com>
2019-12-31 19:23:44 -0600

    Create cmd - ping ipv6 localhost (zone index test).bat

---
+++ cmd/cmd - ping ipv6 localhost (zone index test).bat
@@ -0,0 +1,331 @@
+@ECHO OFF 
+
+
+REM %USERPROFILE%\Documents\GitHub\Coding\cmd\cmd - ping ipv6 localhost (zone index test).bat
+
+
+ECHO. > %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%1" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%1 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%2" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%2 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%3" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%3 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%4" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%4 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%5" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%5 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%6" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%6 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%7" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%7 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%8" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%8 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%9" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%9 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%10" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%10 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%11" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%11 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%12" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%12 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%13" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%13 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%14" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%14 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%15" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%15 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%16" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%16 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%17" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%17 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%18" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%18 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%19" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%19 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%20" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%20 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%21" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%21 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%22" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%22 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%23" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%23 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%24" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%24 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%25" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%25 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%26" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%26 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%27" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%27 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%28" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%28 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%29" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%29 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%30" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%30 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%31" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%31 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%32" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%32 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%33" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%33 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%34" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%34 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%35" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%35 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%36" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%36 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%37" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%37 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%38" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%38 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%39" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%39 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%40" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%40 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%41" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%41 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%42" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%42 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%43" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%43 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%44" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%44 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%45" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%45 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%46" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%46 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%47" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%47 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%48" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%48 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%49" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%49 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%50" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%50 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%51" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%51 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%52" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%52 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%53" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%53 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%54" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%54 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%55" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%55 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%56" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%56 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%57" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%57 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%58" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%58 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%59" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%59 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%60" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%60 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%61" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%61 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%62" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%62 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%63" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%63 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+ECHO "------------------------------------------------------------" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ECHO "ping ::1%%64" >> %USERPROFILE%\Desktop\ping-ipv6.log
+ping -n 1 -w 1 ::1%64 >> %USERPROFILE%\Desktop\ping-ipv6.log
+
+
+Notepad %USERPROFILE%\Desktop\ping-ipv6.log
+

2703c108b16ca8dfa5e0ca07ab8c9735d8401900
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-03 00:35:11 -0600

    rn

---
+++ linux/Linux - Bashisms (string replacement, lowercase, uppercase, parameter substitution).sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# ------------------------------------------------------------
+#
+#	Linux - bash completionisms (e.g. bashisms)
+#
+# ------------------------------------------------------------
+#
+### String variable character-replacement, or even substring-replacement
+#
+# SYNTAX:   ${VARNAME//${NEEDLE}/${REPLACEMENT}}
+#
+
+HAYSTACK="this example, that example";
+NEEDLE="th";
+REPLACEMENT="d";
+echo "${HAYSTACK//${NEEDLE}/${REPLACEMENT}}";
+
+EXAMPLE="Dat-Example-Doe" && echo "${EXAMPLE//-/_}";
+
+
+# ------------------------------------------------------------
+#
+### String variable to-Lowercase
+#
+# SYNTAX:   ${VARNAME,,}
+#
+
+EXAMPLE="Dat-Example-Doe" && echo "${EXAMPLE,,}";
+
+
+# ------------------------------------------------------------
+#
+### String variable to-Uppercase
+#
+# SYNTAX:   ${VARNAME^^}
+#
+
+EXAMPLE="Dat-Example-Doe" && echo "${EXAMPLE^^}";
+
+
+# ------------------------------------------------------------
+#
+### Type-agnostic - Parameter substitution (uses a fallback/default value (or parameter) if primary is unset)
+
+echo "${VAR_NOT_SET:-55}";
+
+# Can fallback to another parameter
+DEFAULT_VAL="55" && echo "${VAR_NOT_SET:-${DEFAULT_VAL}}";
+
+# Can be nested
+echo "${VAR_NOT_SET:-${VAR_ALSO_NOT_SET:-72}}";
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   unix.stackexchange.com  |  "How to input / start a new line in bash terminal?"  |  https://unix.stackexchange.com/a/80820
+#
+# ------------------------------------------------------------
--- linux/Linux - variable fallback-default value (bashism).sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ------------------------------------------------------------
-#
-#	Linux - bash parameter substitution - fallback value
-#
-# ------------------------------------------------------------
-
-DEFAULT_VAL="55";
-echo "${VARIABLE_NAME_NOT_SETHAYSTACK:-${DEFAULT_VAL}}";

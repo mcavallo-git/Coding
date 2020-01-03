4133770763e7cf815734dc5909808ed034598333
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-02 19:08:48 -0600

    Update VMWare ESXi - Create boot media using ESXi-Customizer-PS (.vib drviers).ps1

--- vmware/VMWare ESXi - Create boot media using ESXi-Customizer-PS (.vib drviers).ps1
+++ vmware/VMWare ESXi - Create boot media using ESXi-Customizer-PS (.vib drviers).ps1
@@ -6,7 +6,7 @@
 If ($False) { # RUN THIS SCRIPT:
 
 
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) { Write-Host "`nError - Admin PowerShell terminal required`n" -ForegroundColor "Red"; } Else { New-Item -Path ("${Env:TEMP}\esxi-create-bootmedia.ps1") -Value (($(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/mcavallo-git/Coding/master/vmware/VMWare%20ESXi%20-%20Create%20boot%20media%20using%20ESXi-Customizer-PS%20(.vib%20drviers).ps1"))) -Force | Out-Null; PowerShell -NoProfile -ExecutionPolicy Bypass ("${Env:TEMP}\esxi-create-bootmedia.ps1"); Remove-Item -Path ("${Env:TEMP}\esxi-create-bootmedia.ps1"); }
+New-Item -Path ("${Env:TEMP}\esxi-create-bootmedia.ps1") -Value (($(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/mcavallo-git/Coding/master/vmware/VMWare%20ESXi%20-%20Create%20boot%20media%20using%20ESXi-Customizer-PS%20(.vib%20drviers).ps1"))) -Force | Out-Null; PowerShell -NoProfile -ExecutionPolicy Bypass ("${Env:TEMP}\esxi-create-bootmedia.ps1"); Remove-Item -Path ("${Env:TEMP}\esxi-create-bootmedia.ps1");
 
 
 }

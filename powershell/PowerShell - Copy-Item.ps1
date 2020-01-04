b36a39810e243fb179d54befdf7684f80f090404
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-03 21:26:37 -0600

    Create PowerShell - Copy-Item.ps1

---
+++ powershell/PowerShell - Copy-Item.ps1
@@ -0,0 +1,10 @@
+
+Copy-Item -Path ($From) -Destination ($To) -Force;
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Copy-Item - Copies an item from one location to another"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/copy-item
+#
+# ------------------------------------------------------------

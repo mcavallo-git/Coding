681573716de0496021dcac0a59a02f82c06dbab6
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-03 19:55:11 -0600

    Update Linux - firewall-cmd.sh

--- linux/Linux - firewall-cmd.sh
+++ linux/Linux - firewall-cmd.sh
@@ -22,6 +22,16 @@ firewall-cmd --get-active-zones;
 # Services
 for EACH_FIREWALL_SERVICE in $(firewall-cmd --get-services | tr ' ' '\n' | sort;); do echo "------------------------------------------------------------"; firewall-cmd --info-service="${EACH_FIREWALL_SERVICE}"; done;
 
+### Create a custom service
+###   firewall-cmd --permanent --new-service=myservice
+###   firewall-cmd --permanent --service=myservice --set-description=description
+###   firewall-cmd --permanent --service=myservice --set-short=description
+###   firewall-cmd --permanent --service=myservice --add-port=portid[-portid]/protocol
+###   firewall-cmd --permanent --service=myservice --add-protocol=protocol
+###   firewall-cmd --permanent --service=myservice --add-source-port=portid[-portid]/protocol
+###   firewall-cmd --permanent --service=myservice --add-module=module
+###   firewall-cmd --permanent --service=myservice --set-destination=ipv:address[/mask]
+
 
 # ------------------------------------------------------------
 # ICMP Types
@@ -42,6 +52,8 @@ firewall-cmd --list-all;
 # Citation(s)
 # 
 #   digitalocean.com  |  "How To Set Up a Firewall Using FirewallD on CentOS 7"  |  https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-using-firewalld-on-centos-7
+#
+#   firewalld.org  |  "Documentation - HowTo - Add a Service | firewalld"  |  https://firewalld.org/documentation/howto/add-a-service.html
 # 
 #   manpages.debian.org  |  "firewall-cmd - firewalld command line client"  |  https://manpages.debian.org/testing/firewalld/firewall-cmd.1.en.html
 # 

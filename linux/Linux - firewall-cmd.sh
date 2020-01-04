332caa96b66394cc78c89a47839b35669b24d368
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-03 20:24:43 -0600

    Update Linux - firewall-cmd.sh

--- linux/Linux - firewall-cmd.sh
+++ linux/Linux - firewall-cmd.sh
@@ -19,18 +19,68 @@ firewall-cmd --get-active-zones;
 
 
 # ------------------------------------------------------------
-# Services
+# Service - Show firewall details for a given service
+firewall-cmd --info-service http;
+firewall-cmd --info-service docker-swarm;
+
+
+### Services - Show details for ALL services
 for EACH_FIREWALL_SERVICE in $(firewall-cmd --get-services | tr ' ' '\n' | sort;); do echo "------------------------------------------------------------"; firewall-cmd --info-service="${EACH_FIREWALL_SERVICE}"; done;
 
-### Create a custom service
-###   firewall-cmd --permanent --new-service=myservice
-###   firewall-cmd --permanent --service=myservice --set-description=description
-###   firewall-cmd --permanent --service=myservice --set-short=description
-###   firewall-cmd --permanent --service=myservice --add-port=portid[-portid]/protocol
-###   firewall-cmd --permanent --service=myservice --add-protocol=protocol
-###   firewall-cmd --permanent --service=myservice --add-source-port=portid[-portid]/protocol
-###   firewall-cmd --permanent --service=myservice --add-module=module
-###   firewall-cmd --permanent --service=myservice --set-destination=ipv:address[/mask]
+###   Services - enable a service:
+firewall-cmd --add-service=http;
+###   Services - enable multiple services, simultaneously:
+firewall-cmd --add-service={http,https};  # NOTE: this command seems unpredictable when using variables, such as --add-service="{${SERVICES}}"
+
+###   Services - disable a service:
+firewall-cmd --remove-service=http;
+
+###
+###   Custom Services - create a new service:
+###     firewall-cmd --permanent --new-service=dat_service
+###
+###   Custom Services - delete an existing service:
+###     firewall-cmd --permanent --delete-service=dat_service
+###
+###   Custom Services - configure a service:
+###     firewall-cmd --permanent --service=dat_service --set-description=description
+###     firewall-cmd --permanent --service=dat_service --set-short=description
+###     firewall-cmd --permanent --service=dat_service --add-port=portid[-portid]/protocol
+###     firewall-cmd --permanent --service=dat_service --add-protocol=protocol
+###     firewall-cmd --permanent --service=dat_service --add-source-port=portid[-portid]/protocol
+###     firewall-cmd --permanent --service=dat_service --add-module=module
+###     firewall-cmd --permanent --service=dat_service --set-destination=ipv:address[/mask]
+###
+
+
+###   Ex)  Create a custom service for MinIO
+firewall-cmd --permanent --new-service=minio;
+firewall-cmd --permanent --service=minio --set-short="minio" --add-port=9000/tcp;
+firewall-cmd --permanent --zone=public --add-service=minio;
+firewall-cmd --set-default-zone=public;
+firewall-cmd --reload;
+firewall-cmd --info-service minio;
+firewall-cmd --list-all;
+
+
+###   Ex)  Delete a given service entirely ( !! WARNING: service will need to be recreated manually to use it again !! )
+firewall-cmd --permanent --delete-service=minio;
+firewall-cmd --reload;
+firewall-cmd --info-service minio;
+firewall-cmd --list-all;
+
+
+# ------------------------------------------------------------
+### Ports - Open individual ports
+firewall-cmd --permanent --add-port={80,443}/tcp;
+firewall-cmd --reload;
+firewall-cmd --list-all;
+
+
+### Ports - Close individual ports
+firewall-cmd --permanent --remove-port={80,443}/tcp;
+firewall-cmd --reload;
+firewall-cmd --list-all;
 
 
 # ------------------------------------------------------------

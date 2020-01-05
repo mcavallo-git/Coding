4fe29fba7b614cbd5a5bdf228255d79fc4eac7af
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-04 22:52:24 -0600

    Update FiletypeAssociations.psm1

--- powershell/_WindowsPowerShell/Modules/FiletypeAssociations/FiletypeAssociations.psm1
+++ powershell/_WindowsPowerShell/Modules/FiletypeAssociations/FiletypeAssociations.psm1
@@ -12,15 +12,9 @@ Function FiletypeAssociations() {
 	)
 
 	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-		# Script is >> NOT << running as admin
-		#  > Attempt to open an admin terminal with the same command-line arguments as the current
+		# Script is >> NOT << running as admin  --> Attempt to open an admin terminal with the same command-line arguments as the current
 		$CommandString = $MyInvocation.MyCommand.Name;
-		$PSBoundParameters.Keys | ForEach-Object {
-			$CommandString += " -$_";
-			If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) {
-				$CommandString += " `"$($PSBoundParameters[$_])`"";
-			}
-		}
+		$PSBoundParameters.Keys | ForEach-Object { $CommandString += " -$_"; If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) { $CommandString += " `"$($PSBoundParameters[$_])`""; } };
 		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"$($CommandString)`"" -Verb RunAs;
 
 	} ElseIf ($PSBoundParameters.ContainsKey('Pull') -Eq $False) {

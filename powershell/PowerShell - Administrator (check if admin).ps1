f231e5b789abbb23c55832d5457d02287c22621e
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-04 22:52:36 -0600

    Update PowerShell - Administrator (check if admin).ps1

--- powershell/PowerShell - Administrator (check if admin).ps1
+++ powershell/PowerShell - Administrator (check if admin).ps1
@@ -1,17 +1,10 @@
 
 # Check whether-or-not the current PowerShell session is running with elevated privileges (admin rights)
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	# Script is >> NOT << running as admin
-	#  > Attempt to open an admin terminal with the same command-line arguments as the current
+	# Script is >> NOT << running as admin  --> Attempt to open an admin terminal with the same command-line arguments as the current
 	$CommandString = $MyInvocation.MyCommand.Name;
-	$PSBoundParameters.Keys | ForEach-Object {
-		$CommandString += " -$_";
-		If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) {
-			$CommandString += " `"$($PSBoundParameters[$_])`"";
-		}
-	}
+	$PSBoundParameters.Keys | ForEach-Object { $CommandString += " -$_"; If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) { $CommandString += " `"$($PSBoundParameters[$_])`""; } };
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"$($CommandString)`"" -Verb RunAs;
-
 
 } Else {
 	# Script >> IS << running as Admin - Continue

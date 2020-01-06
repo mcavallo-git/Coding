12c61590fa69ee7617b4a4a8edfe6e8dbc6d44bd
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-06 08:08:14 -0600

    Update PowerShell - Sort-Object (multiple columns, different sorts).ps1

--- powershell/PowerShell - Sort-Object (multiple columns, different sorts).ps1
+++ powershell/PowerShell - Sort-Object (multiple columns, different sorts).ps1
@@ -1,12 +1,17 @@
 
-Write-Host "Vibs_Valid:";
 
-$ValidExtraVibs | Sort-Object -Property Name,@{Expression={$_.Version}; Ascending=$False} | Select-Object -Property Name,Version;
+Write-Host "Vibs_Valid:";
+$ValidExtraVibs `
+| Sort-Object -Property Name -Unique `
+| Sort-Object -Property Name,@{Expression={$_.Version}; Ascending=$False} `
+| Select-Object -Property Name,Version
 
 
 # ------------------------------------------------------------
 #
 #	Citation(s)
+#
+#   stackoverflow.com  |  "select distinct items from a column in powershell"  |  https://stackoverflow.com/a/8439487
 #
 #   social.technet.microsoft.com  |  "sort-object multiple properties - use descending first RRS feed"  |  https://social.technet.microsoft.com/Forums/windowsserver/en-US/e2067689-d28b-4455-9a05-d933e31ab311/sortobject-multiple-properties-use-descending-first?forum=winserverpowershell
 #

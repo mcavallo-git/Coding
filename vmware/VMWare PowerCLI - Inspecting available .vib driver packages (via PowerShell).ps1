3d49a34a4cd1b79f20269f271589f20ff58aca0c
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-06 04:59:44 -0600

    Update VMWare PowerCLI - Inspecting available .vib driver packages (via PowerShell).ps1

--- vmware/VMWare PowerCLI - Inspecting available .vib driver packages (via PowerShell).ps1
+++ vmware/VMWare PowerCLI - Inspecting available .vib driver packages (via PowerShell).ps1
@@ -65,7 +65,7 @@ $Vibs
 # $Vibs | Sort-Object -Property "Name" | Where-Object { $_.Depends | ForEach-Object { $_ | Where-Object { (($_.Relation -Eq ">=") -And ($_.Version -Like "6.5*")) } } }
 
 # ForEach ($EachVib in $Vibs) {
-# 	$EachDepends = ($EachVib.Depends | Where-Object { (($_.Relation -Eq ">=") -And ($_.Version -Like "6.5*")) });
+# 	$Depends = ($EachVib.Depends | Where-Object { (($_.Relation -Eq ">=") -And ($_.Version -Like "6.5*")) });
 # 	Write-Host "EachDepends = [ ${EachDepends} ]";
 # };
 
@@ -80,7 +80,7 @@ $Vibs
 
 # $MatchingVibs = @(); `
 # ForEach ($EachVib in $Vibs) {
-# 	$EachDepends = ($EachVib.Depends | Where-Object {
+# 	$Depends = ($EachVib.Depends | Where-Object {
 # 		(
 # 			($_.PackageName -Eq "esx-base") -And (
 # 				(($_.Relation -Eq ">=") -And ($_.Version -Like "6.5*")) -Or
@@ -88,7 +88,7 @@ $Vibs
 # 				(($_.Relation -Eq "<=") -And ($_.Version -Like "6.5*"))
 # 		))
 # 	});
-# 	If ($EachDepends -Ne $Null) {
+# 	If ($Depends -Ne $Null) {
 # 		$MatchingVibs += $EachVib;
 # 	}
 # }; `
@@ -101,8 +101,8 @@ $Vibs
 # $MatchingVibs = @(); `
 # ForEach ($EachVib in $Vibs) {
 # 	$AddVib = $True;
-# 	# ForEach ($EachDepends in $EachVib.Depends) {
-# 		$EachDepends = ($EachVib.Depends | Where-Object {
+# 	# ForEach ($Depends in $EachVib.Depends) {
+# 		$Depends = ($EachVib.Depends | Where-Object {
 # 			(
 # 				($_.PackageName -Eq "esx-base") -And (
 # 					(($_.Relation -Eq ">=") -And ($_.Version -Match "(^6(\.[12345])?)|(^[12345]\.?)")) -Or
@@ -111,7 +111,7 @@ $Vibs
 # 			))
 # 		});
 # 	# }
-# 	If ($EachDepends -Ne $Null) {
+# 	If ($Depends -Ne $Null) {
 # 		$MatchingVibs += $EachVib;
 # 	}
 # }; `
@@ -119,65 +119,108 @@ $Vibs
 
 
 # ForEach ($EachVib in $Vibs) {
-# 	$EachDepends = ($EachVib.Depends | Where-Object {
+# 	$Depends = ($EachVib.Depends | Where-Object {
 # 		(($_.PackageName -Like "esx-*"))
 # 	});
-# 	If ($EachDepends -Ne $Null) {
-# 		$EachDepends
+# 	If ($Depends -Ne $Null) {
+# 		$Depends
 # 	}
 # };
 
 
 # ForEach ($EachVib in $Vibs) {
 # 	$InvalidDependency = $False;
-# 	ForEach ($EachDepends in $EachVib.Depends) {
-# 		If (@("esx-base","esx-update","esx-version") -Contains ($EachDepends.PackageName)) {
-# 			If ($EachDepends.Relation -Eq ">=") {
-# 				# Greater-Than / Equal-To Version
-# 				If ($EachDepends.Version -Match "^6\.[6789]") {
-# 					$EachDepends;
-# 					$InvalidDependency = $True;
-# 				}
+# 	ForEach ($Depends in $EachVib.Depends) {
+# 		If (@("esx-base","esx-update","esx-version") -Contains ($Depends.PackageName)) {
+# 			If ($Relation -Eq ">=") {
+# 			} ElseIf ($Relation -Eq "=") {
+# 			} ElseIf ($Relation -Eq "<=") {
+# 			} Else {
+# 				$Depends;
 # 			}
 # 		}
 # 	}
 # };
 
 
-$MatchingVibs = @(); `
+# $Depends=@{Version="6.7"}; (([Int][String](($Version)[0]) -GE 6) -And (((($Version)[2]) -Eq $Null) -Or ([Int][String](($Version)[2]) -GE 5)))
+
+# $Version="6.0.0-3.96"; ([Decimal](($Version.Split('.')[1])));
+
+# $Version="6.0.0-3.96"; [Decimal](($Version.Split('.') | Select-Object -First 2) -Join ".");
+# $Version="6.7"; [Decimal](($Version.Split('.') | Select-Object -First 2) -Join ".");
+# $Version="6"; [Decimal](($Version.Split('.') | Select-Object -First 2) -Join ".");
+# $Version=""; [Decimal](($Version.Split('.') | Select-Object -First 2) -Join ".");
+
+
+$Vibs_Valid = @(); `
+$Vibs_Invalid = @(); `
+$ESXiVersion = "6.5";
+$ESXiVersionDecimal = [Decimal](($ESXiVersion.Split('.') | Select-Object -First 2) -Join ".");
 ForEach ($EachVib in $Vibs) {
-	$InvalidDependency = $False;
-	ForEach ($EachDepends in $EachVib.Depends) {
-		If (@("esx-base","esx-update","esx-version") -Contains ($EachDepends.PackageName)) {
-			If ($EachDepends.Relation -Eq ">=") {
-				# Greater-Than / Equal-To Version
-				If ($EachDepends.Version -Match "^6\.[6789]") {
-					$EachDepends;
-					$InvalidDependency = $True;
+	$ValidVib = $True;
+	ForEach ($Depends in $EachVib.Depends) {
+		$ValidDependency = $True;
+		$PackageName = $Depends.PackageName;
+		$Relation = $Depends.Relation;
+		$Version = $Depends.Version;
+		If (@("esx-base","esx-update","esx-version").Contains ($PackageName)) {
+			$ValidDependency = $False; <# Assume guilty until proven innocent #>
+			$EachVersionDecimal = [Decimal](($Version.Split('.') | Select-Object -First 2) -Join ".");
+			If (($Relation -Eq ">") -Or ($Relation -Eq ">>")) {
+				<# Greater-Than Version #>
+				If (($MinorVersionSpecified -Eq $True) -And ($ESXiVersionDecimal -GT $EachVersionDecimal)) {
+					$ValidDependency = $True;
+				} ElseIf ($MinorVersionSpecified -Eq $False) -And (([Int]$ESXiVersionDecimal) -GT ([Int]$EachVersionDecimal)) {
+					$ValidDependency = $True;
 				}
-			} Else If ($EachDepends.Relation -Eq "=") {
-				# Equals Version
-				If ($EachDepends.Version -Match "(^6(\.5)|$)") {
-					$EachDepends;
-					$InvalidDependency = $True;
+			} ElseIf ($Relation -Eq ">=") {
+				<# Greater-Than / Equal-To Version #>
+				If (($MinorVersionSpecified -Eq $True) -And ($ESXiVersionDecimal -GE $EachVersionDecimal)) {
+					$ValidDependency = $True;
+				} ElseIf ($MinorVersionSpecified -Eq $False) -And (([Int]$ESXiVersionDecimal) -GE ([Int]$EachVersionDecimal)) {
+					$ValidDependency = $True;
 				}
-			} Else If ($EachDepends.Relation -Eq "<=") {
-				# Less-Than / Equal-To Version
-				If ($EachDepends.Version -Match "(^6(\.[1234])|$)|(^[12345](\.)|$)") {
-					$EachDepends;
-					$InvalidDependency = $True;
+			} ElseIf ($Relation -Eq "=") {
+				<# Equals Version #>
+				If (($MinorVersionSpecified -Eq $True) -And ($ESXiVersionDecimal -Eq $EachVersionDecimal)) {
+					$ValidDependency = $True;
+				} ElseIf ($MinorVersionSpecified -Eq $False) -And (([Int]$ESXiVersionDecimal) -Eq ([Int]$EachVersionDecimal)) {
+					$ValidDependency = $True;
+				}
+			} ElseIf ($Relation -Eq "<=") {
+				<# Less-Than / Equal-To Version #>
+				If (($MinorVersionSpecified -Eq $True) -And ($ESXiVersionDecimal -LE $EachVersionDecimal)) {
+					$ValidDependency = $True;
+				} ElseIf ($MinorVersionSpecified -Eq $False) -And (([Int]$ESXiVersionDecimal) -LE ([Int]$EachVersionDecimal)) {
+					$ValidDependency = $True;
+				}
+			} ElseIf (($Relation -Eq "<") -Or ($Relation -Eq "<<")) {
+				<# Less-Than Version #>
+				If (($MinorVersionSpecified -Eq $True) -And ($ESXiVersionDecimal -LT $EachVersionDecimal)) {
+					$ValidDependency = $True;
+				} ElseIf ($MinorVersionSpecified -Eq $False) -And (([Int]$ESXiVersionDecimal) -LT ([Int]$EachVersionDecimal)) {
+					$ValidDependency = $True;
 				}
 			} Else {
-				$EachDepends.Relation;
+				Write-Host "Unhandled .vib dependency-relation `"$Relation`""; <# Output Un-handled Relations #>
 			}
 		}
+		If ($ValidDependency -Eq $False) {
+			$ValidVib = $False;
+		}
 	}
-	If ($InvalidDependency -Eq $True) {
-		$MatchingVibs += $EachVib;
+	If ($ValidVib -Eq $True) {
+		$Vibs_Valid += $EachVib;
+	} Else {
+		$Vibs_Invalid += $EachVib;
 	}
 };
-
-
+Write-Host "------------------------------------------------------------";
+Write-Host "Vibs_Invalid:"; $Vibs_Invalid;
+Write-Host "------------------------------------------------------------";
+Write-Host "Vibs_Valid:"; $Vibs_Valid;
+Write-Host "------------------------------------------------------------";
 
 
 # ------------------------------------------------------------
@@ -186,11 +229,15 @@ ForEach ($EachVib in $Vibs) {
 #
 #   docs.microsoft.com  |  "about_Automatic_Variables - PowerShell"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_automatic_variables#using-enumerators
 #
+#   docs.microsoft.com  |  "about_Break - PowerShell"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_break
+#
 #   docs.microsoft.com  |  "about_Comparison_Operators - PowerShell"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators
 #
 #   docs.microsoft.com  |  "about_Foreach - PowerShell"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_foreach
 #
 #   docs.microsoft.com  |  "about_Logical_Operators - PowerShell"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_logical_operators
+#
+#   docs.microsoft.com  |  "About numeric literals - PowerShell - PowerShell"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_numeric_literals
 #
 #   docs.microsoft.com  |  "ForEach-Object"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/foreach-object
 #
@@ -199,5 +246,7 @@ ForEach ($EachVib in $Vibs) {
 #   stackoverflow.com  |  "Variables in nested Foreach-Object and Where-Object"  |  https://stackoverflow.com/a/26715697
 #
 #   stackoverflow.com  |  "Where-object $_ matches multiple criterias"  |  https://stackoverflow.com/a/23896434
+#
+#   vdc-download.vmware.com  |  "Best Practices for Creating and Publishing VIBs"  |  https://vdc-download.vmware.com/vmwb-repository/dcr-public/9ec4b759-1f2a-49a9-93e3-7c31230cdbc1/2c177be4-c508-43e4-81fb-5fe3c448dc3a/Best%20Practices%20for%20Creating%20and%20Publishing%20VIBs%3D1%3DProduct%20Doc%20PDF%3Den.pdf
 #
 # ------------------------------------------------------------

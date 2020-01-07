58448ceb424ebc8e04d3c0785379f57a4501e40e
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-07 16:58:35 -0600

    Create PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1

---
+++ powershell/PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1
@@ -0,0 +1,11 @@
+
+[System.Net.HttpWebRequest]::Create("${Win0SDK_Url}").GetResponse().ResponseUri.AbsoluteUri
+
+
+# ------------------------------------------------------------
+# 
+# Citation(s)
+# 
+# https://stackoverflow.com/a/45593554
+# 
+# ------------------------------------------------------------

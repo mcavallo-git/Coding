2ac42274c75af28b98f50adc91dd28f180a022ce
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-07 17:41:41 -0600

    Update PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1

--- powershell/PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1
+++ powershell/PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1
@@ -1,5 +1,7 @@
 
-[System.Net.HttpWebRequest]::Create("${Win0SDK_Url}").GetResponse().ResponseUri.AbsoluteUri
+$Win0SDK_Url = "https://go.microsoft.com/fwlink/p/?linkid=2083338&clcid=0x409";
+
+[System.Net.HttpWebRequest]::Create("${Win0SDK_Url}").GetResponse().ResponseUri.AbsoluteUri;
 
 
 # ------------------------------------------------------------

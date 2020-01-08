d3b4d8951ff5a5ac644cb971e3e11a89943cc852
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-07 19:21:04 -0600

    Update NET_Framework_Install.psm1

--- powershell/_WindowsPowerShell/Modules/NET_Framework_Install/NET_Framework_Install.psm1
+++ powershell/_WindowsPowerShell/Modules/NET_Framework_Install/NET_Framework_Install.psm1
@@ -9,33 +9,33 @@ function NET_Framework_Install {
 	)
 
 	# 2019
-	$URL="https://aka.ms/vs/16/release/VC_redist.x86.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://aka.ms/vs/16/release/VC_redist.x64.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://aka.ms/vs/16/release/VC_redist.x86.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://aka.ms/vs/16/release/VC_redist.x64.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
 
 	# 2017
-	$URL="https://go.microsoft.com/fwlink/?LinkId=746571"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://go.microsoft.com/fwlink/?LinkId=746571"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://go.microsoft.com/fwlink/?LinkId=746571"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://go.microsoft.com/fwlink/?LinkId=746571"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
 
 	# 2015
-	$URL="https://www.microsoft.com/en-pk/download/details.aspx?id=48145"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=53587"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://www.microsoft.com/en-pk/download/details.aspx?id=48145"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=53587"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
 
 	# 2013
-	$URL="https://support.microsoft.com/en-us/help/4032938/update-for-visual-c-2013-redistributable-package"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://support.microsoft.com/en-us/help/4032938/update-for-visual-c-2013-redistributable-package"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
 
 	# 2010
-	$URL="https://www.microsoft.com/en-pk/download/details.aspx?id=8328"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26999"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://www.microsoft.com/en-pk/download/details.aspx?id=8328"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26999"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
 
 	# 2008
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=29"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=15336"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26368"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile($URL,$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=29"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=15336"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26368"; $FN="${Env:Temp}\Install_.NET-Framework-2019.001.exe"; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
 	Return;
 

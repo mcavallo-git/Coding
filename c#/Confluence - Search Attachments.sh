35cad6e6bea57ed44a5f8b66ad82ccf9e7d00214
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-09 11:45:07 -0600

    Create Confluence - Search Attachments.sh

---
+++ c#/Confluence - Search Attachments.sh
@@ -0,0 +1,46 @@
+
+
+## @param Name:title=Name|type=string|required=true|desc=Your name
+
+## @param MYNAME:title=MY TITLE|type=MY TYPE|desc=MY DESCRIPTION|required=true|multiple=true|default=MY DEFAULT VALUE
+
+{attachments:*}
+
+# {attachments:old=false|patterns=.*|sortby=name|page=*|sortorder=descending|labels=*|upload=false|preview=false}
+
+
+## This is an example macro
+## @param Name:title=Name|type=string|required=true|desc=Your name
+## @param Colour:title=Favourite Colour|type=enum|enumValues=red,green,blue|default=red|desc=Choose your favourite colour
+# Hello, <font color="$paramColour">$paramName</font>!
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   display a list of all attachments in confluence
+#   https://community.atlassian.com/t5/Answers-Developer-Questions/display-a-list-of-all-attachments-in-confluence/qaq-p/513384
+#
+#   Confluence Server documentation
+#   https://confluence.atlassian.com/conf66/confluence-server-documentation-943961459.html
+#
+#   Confluence Search Fields
+#   https://confluence.atlassian.com/doc/confluence-search-fields-161188.html
+#
+#   Confluence Search Syntax
+#   https://confluence.atlassian.com/doc/confluence-search-syntax-158720.html
+#
+#   Search
+#   https://confluence.atlassian.com/doc/search-139528.html
+#
+#   Space Attachments Macro (Confluence 7.2)
+#   https://confluence.atlassian.com/doc/space-attachments-macro-278693110.html
+#
+#   Space Attachments Macro (Confluence 6.6)
+#   https://confluence.atlassian.com/conf66/attachments-macro-943962038.html
+#
+#   User Macro Template Syntax
+#   https://confluence.atlassian.com/conf66/user-macro-template-syntax-943962461.html
+#
+# ------------------------------------------------------------

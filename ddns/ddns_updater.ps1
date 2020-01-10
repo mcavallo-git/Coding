97d110da144e031d194ac3da5f56a101dcb0bcae
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-10 06:11:13 -0600

    Update ddns_updater.ps1

--- ddns/ddns_updater.ps1
+++ ddns/ddns_updater.ps1
@@ -3,7 +3,9 @@
 #		|--> Applying basic DDNS - Allows syncing of a given Hostname's A-Record to a given device's dynamic IPv4 address
 #
 ForEach ($LocalUser In (Get-ChildItem ('C:/Users'))) {
-	If (Test-Path (($LocalUser.FullName)+('/.namecheap/secret'))) {
+	If ((Test-Path (($LocalUser.FullName)+('/.namecheap/hostname'))) -And (Test-Path (($LocalUser.FullName)+('/.namecheap/domain'))) -And (Test-Path (($LocalUser.FullName)+('/.namecheap/token')))) {
+		[System.Net.WebRequest]::Create((('https://dynamicdns.park-your-domain.com/update?host=')+(Get-Content (($LocalUser.FullName)+('/.namecheap/hostname')))+('&domain=')+(Get-Content (($LocalUser.FullName)+('/.namecheap/domain')))+('&password=')+(Get-Content (($LocalUser.FullName)+('/.namecheap/token')))+('&ip='))).GetResponse();
+	} ElseIf (Test-Path (($LocalUser.FullName)+('/.namecheap/secret'))) {
 		[System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((($LocalUser.FullName)+('/.namecheap/secret'))))))).GetResponse();
 	};
 	If (Test-Path (($LocalUser.FullName)+('/.duck-dns/secret'))) {

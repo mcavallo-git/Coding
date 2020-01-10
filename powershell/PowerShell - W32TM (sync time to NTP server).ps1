23226a54af11e5792d908d9199a13cc1a4e80b1f
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-10 08:40:57 -0600

    Update PowerShell - W32TM (sync time to NTP server).ps1

--- powershell/PowerShell - W32TM (sync time to NTP server).ps1
+++ powershell/PowerShell - W32TM (sync time to NTP server).ps1
@@ -6,15 +6,13 @@
 #
 # ------------------------------------------------------------
 #
-# --> Test each of the NTP Servers (by grabbing the [ current time ] from each of them, separately)
+### Test NTP Servers
 #
-
 $NtpPeers = @();
 $NtpPeers += "time.nist.gov";
 $NtpPeers += "time.google.com";
 $NtpPeers += "north-america.pool.ntp.org";
 $NtpPeers += "time.windows.com";
-
 Write-Host "`n`n  Before Update to NTP-Config...`n   |";
 ForEach ($EachPeer In $NtpPeers) {
 	$DeltaTimeToPeer = (w32tm.exe /stripchart /computer:$EachPeer /dataonly /samples:1)[3].Split(' ')[1];

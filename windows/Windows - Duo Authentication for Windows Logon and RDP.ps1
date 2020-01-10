3b43fd42f56f82f1d583dd18990d16c1bb0a51f6
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-09 20:13:58 -0600

    Create Windows - Duo Authentication for Windows Logon and RDP.ps1

---
+++ windows/Windows - Duo Authentication for Windows Logon and RDP.ps1
@@ -0,0 +1,14 @@
+
+
+
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   duo.com  |  "Duo Authentication for Windows Logon and RDP | Duo Security"  |  https://duo.com/docs/rdp
+#
+#   duo.com  |  "Remote Desktop Protocol (RDP) | Duo Security"  |  https://duo.com/product/every-application/supported-applications/rdp-remote-desktop-protocol
+#
+# ------------------------------------------------------------

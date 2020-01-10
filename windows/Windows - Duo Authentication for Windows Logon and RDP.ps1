81196cf28bc57b146112dd9ee4c52e47677eb5e1
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-09 21:38:10 -0600

    Update Windows - Duo Authentication for Windows Logon and RDP.ps1

--- windows/Windows - Duo Authentication for Windows Logon and RDP.ps1
+++ windows/Windows - Duo Authentication for Windows Logon and RDP.ps1
@@ -3,11 +3,19 @@
 
 
 
+
+
+
+
+
+
 # ------------------------------------------------------------
 #
-# Citation(s)
+#  Citation(s)
 #
 #   duo.com  |  "Duo Authentication for Windows Logon and RDP | Duo Security"  |  https://duo.com/docs/rdp
+#
+#   duo.com  |  "2FA for Windows Remote Desktop Protocol and Local Logons | Duo Security"  |  https://duo.com/docs/winlogon-gpo
 #
 #   duo.com  |  "Remote Desktop Protocol (RDP) | Duo Security"  |  https://duo.com/product/every-application/supported-applications/rdp-remote-desktop-protocol
 #

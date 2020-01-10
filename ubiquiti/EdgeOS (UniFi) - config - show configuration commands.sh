ba1c564146b9ac608a24c894d19c87ba40baaa3e
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-10 05:23:05 -0600

    Update EdgeOS (UniFi) - config - show configuration commands.sh

--- ubiquiti/EdgeOS (UniFi) - config - show configuration commands.sh
+++ ubiquiti/EdgeOS (UniFi) - config - show configuration commands.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-show configuration commands
+show configuration commands > "${HOME}/show configuration commands.$(date +'%Y%m%d_%H%M%S').$(hostname).sh";
 #  |
 #  |--> Reverse-engineers the current config and outputs what it would take to rebuild it (as a set of shell-commands)

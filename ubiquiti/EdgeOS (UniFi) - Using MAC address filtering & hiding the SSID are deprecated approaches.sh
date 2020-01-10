fe849ae243f93212bc9aa05adcca0aed9d5187bf
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-10 05:08:12 -0600

    Create EdgeOS (UniFi) - Using MAC address filtering & hiding the SSID are deprecated approaches.sh

---
+++ ubiquiti/EdgeOS (UniFi) - Using MAC address filtering & hiding the SSID are deprecated approaches.sh
@@ -0,0 +1,10 @@
+
+### See Citation(s), below
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.ui.com  |  "MAC filtering on UniFi | Ubiquiti Community"  |  https://community.ui.com/questions/MAC-filtering-on-UniFi/e181bbab-6946-466f-b315-13d96557f25e#answer/e7fd1426-8fb7-43fe-ba38-bedb3b67b848
+#
+# ------------------------------------------------------------

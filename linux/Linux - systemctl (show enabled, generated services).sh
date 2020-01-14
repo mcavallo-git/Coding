ad5dc9ca81b39a2696b1a01e311ace83e4800e50
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-14 12:36:11 -0600

    Update Linux - systemctl (show enabled, generated services).sh

--- linux/Linux - systemctl (show enabled, generated services).sh
+++ linux/Linux - systemctl (show enabled, generated services).sh
@@ -5,6 +5,27 @@
 
 systemctl list-unit-files --type service --state enabled,generated;
 
+# ------------------------------------------------------------
+# Check one service's status
+
+SVC="nginx"; systemctl is-enabled "${SVC}.service"; systemctl is-active "${SVC}.service"; systemctl status "${SVC}.service";
+
+
+# ------------------------------------------------------------
+# Autostart on bootup (as well as right now)
+
+SVC="nginx";
+if \
+	[ "$(systemctl is-enabled ${SVC}.service 2>/dev/null;)" != "enabled" ] || \
+	[ "$(systemctl is-active ${SVC}.service 2>/dev/null;)" != "active" ] || \
+	[ $(systemctl is-enabled ${SVC}.service 1>/dev/null 2>&1; echo $?;) -ne 0 ] || \
+	[ $(systemctl is-active ${SVC}.service 1>/dev/null 2>&1; echo $?;) -ne 0 ]; \
+then
+	echo "Info:  Service \"${SVC}\" is not both \"enabled\" and \"active\", currently";
+	echo "Info:  Calling  [ systemctl enable \"${SVC}.service\" --now; ]  ...";
+	systemctl enable "${SVC}.service" --now; # "enable --now" autostarts service at bootup && starts service immediately
+fi;
+
 
 # ------------------------------------------------------------
 # Get the name of the local network service
@@ -23,6 +44,8 @@ SERVICE_NAME="mongod"; cat "/lib/systemd/system/${SERVICE_NAME}.service";
 # ------------------------------------------------------------
 # Citation(s)
 #
-#   askubuntu.com  |  "How to list all enabled services from systemctl?"  |  https://askubuntu.com/a/1060852
+#   askubuntu.com  |  "systemd - How to list all enabled services from systemctl? - Ask Ubuntu"  |  https://askubuntu.com/a/1060852
+#
+#   askubuntu.com  |  "systemd - What is the difference between "systemctl start" and "systemctl enable"? - Ask Ubuntu"  |  https://askubuntu.com/a/733510
 #
 # ------------------------------------------------------------

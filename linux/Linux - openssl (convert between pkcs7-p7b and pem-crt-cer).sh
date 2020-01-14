f30e0e117608b8efc59f6e6a65c2f0ffc7d0f5b4
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-14 13:00:54 -0600

    Update Linux - openssl (convert between pkcs7-p7b and pem-crt-cer).sh

--- linux/Linux - openssl (convert between pkcs7-p7b and pem-crt-cer).sh
+++ linux/Linux - openssl (convert between pkcs7-p7b and pem-crt-cer).sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-openssl pkcs7 -print_certs -in certificate.p7b -out certificate.cer;
+openssl pkcs7 -in certificate.p7b -inform DER -print_certs -out certificate.pem;
 
 
 # ------------------------------------------------------------
@@ -8,5 +8,7 @@ openssl pkcs7 -print_certs -in certificate.p7b -out certificate.cer;
 # Citation(s)
 #
 #   knowledge.digicert.com  |  "How to convert PKCS #7 (.p7b) to PEM certificate format using OpenSSL"  |  https://knowledge.digicert.com/solution/SO21448.html
+#
+#   serverfault.com  |  "Convert from P7B to PEM via OpenSSL - Server Fault"  |  https://serverfault.com/a/417286
 #
 # ------------------------------------------------------------

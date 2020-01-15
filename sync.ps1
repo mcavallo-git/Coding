b8d064e84ec23bedf9abaedc58ba5687d183e0c8
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 06:26:10 -0600

    Update sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -63,12 +63,10 @@ If (Test-Path "${REPO_DIR_WIN32}") {
 
 	$SSH_KEY_REMOTE="https://raw.githubusercontent.com/mcavallo-git/Coding/master/.shared-deploy-key.pem";
 
-	$SSH_KEY_LOCAL_WIN32="${REPO_DIR_WIN32}/.git/.shared-deploy-key.pem";
-
+	$SSH_KEY_LOCAL_WIN32="${HOME}\.ssh\git\.shared-deploy-key.pem";
 	$SSH_KEY_LOCAL_LINUX=(("/")+(((${SSH_KEY_LOCAL_WIN32} -Replace "\\","/") -Replace ":","")));
 
-	New-Item -ItemType "File" -Path ("${SSH_KEY_LOCAL_WIN32}") -Value ($(New-Object Net.WebClient).DownloadString("${SSH_KEY_REMOTE}")) | Out-Null;
-	# New-Item -ItemType "File" -Path ("${SSH_KEY_LOCAL_WIN32}") -Value ($(New-Object Net.WebClient).DownloadString("${SSH_KEY_REMOTE}")) -Force | Out-Null;
+	New-Item -ItemType "File" -Path ("${SSH_KEY_LOCAL_WIN32}") -Value ($(New-Object Net.WebClient).DownloadString("${SSH_KEY_REMOTE}")) -Force | Out-Null;
 
 	Set-Location "${REPO_DIR_WIN32}";
 

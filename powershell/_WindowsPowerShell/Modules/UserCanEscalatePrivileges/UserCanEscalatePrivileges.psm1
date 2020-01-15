1131f97ba3078e3983474258f6c39efa694a748f
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 01:40:32 -0600

    Update UserCanEscalatePrivileges.psm1

--- powershell/_WindowsPowerShell/Modules/UserCanEscalatePrivileges/UserCanEscalatePrivileges.psm1
+++ powershell/_WindowsPowerShell/Modules/UserCanEscalatePrivileges/UserCanEscalatePrivileges.psm1
@@ -6,27 +6,41 @@ Function UserCanEscalatePrivileges {
 	Param(
 	)
 	$ReturnedVal = $Null;
-	### Get Local Admins - Built-in PowerShell Method
-	### v-- Note: The 'Get-LocalGroupMember' method only worked for AD (Domain) connected devices WHILST connected to said domain
-	### V-- Note: Needed to upgrade to using the ADSI methods (below) to correctly query local admins whilst NOT connected to said domain
-	# $LocalAdmins = ((Get-LocalGroupMember -Name "Administrators").Name);
-	#
-	### Get Local Admins - ADSI, Method 1
-	# $LocalAdmins = (@(([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % { $_.GetType().InvokeMember('AdsPath','GetProperty',$null,$($_),$null) }) -match '^WinNT');
-	#
-	# Get Local Admins - ADSI, Method 2
+
 	$LocalAdmins = (([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % {([ADSI]$_).InvokeGet('AdsPath')});
-	#
-	# Get Current User
-	$CurrentUser = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name);
-	$CurrentUserWinNT = "WinNT://$($CurrentUser.Replace("\","/"))";
-	#
-	# Make final (returned) determination of whether the user is able to "Run as Admin" or not
-	If (($LocalAdmins.Contains($CurrentUser)) -Or ($LocalAdmins.Contains($CurrentUserWinNT))) {
-		$ReturnedVal = $True;
-	} Else {
-		$ReturnedVal = $False;
+	$UserHasAdminRights = If (($LocalAdmins.Contains(([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name)) -Or ($LocalAdmins.Contains("WinNT://$($CurrentUser.Replace("\","/"))"))) { $True } Else { $False };
+
+	$ReturnedVal = $UserHasAdminRights;
+
+	If ($False) { # Expanded/Broken-Up - Same functionality as above but calls have been expanded to show more obvious/intuitive functionality of the runtime command(s)
+		
+		### Get Local Admins - Built-in PowerShell Method
+		# $LocalAdmins = ((Get-LocalGroupMember -Name "Administrators").Name);
+		###  |--> NOTE:  Updated script to use the ADSI methods (below), as the Get-LocalGroupMember method acts finnicky for AD-connected devies whilst no available AD controller exists to query off-of
+		#
+		
+		### Get Local Admins - ADSI, Method 1
+		# $LocalAdmins = (@(([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % { $_.GetType().InvokeMember('AdsPath','GetProperty',$null,$($_),$null) }) -match '^WinNT');
+		#
+		
+		# Get Local Admins - ADSI, Method 2
+		$LocalAdmins = (([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % {([ADSI]$_).InvokeGet('AdsPath')});
+		#
+		
+		# Get Current User
+		$CurrentUser = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name);
+		$CurrentUserWinNT = ("WinNT://$($CurrentUser.Replace("\","/"))");
+		#
+		
+		# Make final (returned) determination of whether the user is able to "Run as Admin" or not
+		If (($LocalAdmins.Contains($CurrentUser)) -Or ($LocalAdmins.Contains($CurrentUserWinNT))) {
+			$ReturnedVal = $True;
+		} Else {
+			$ReturnedVal = $False;
+		}
+
 	}
+
 	Return $ReturnedVal;
 
 }

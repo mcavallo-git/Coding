f49d6cd8b07e3982f791daad8efd09dad43f9cb3
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 08:21:27 -0600

    Update NET_Framework_Install.psm1

--- powershell/_WindowsPowerShell/Modules/NET_Framework_Install/NET_Framework_Install.psm1
+++ powershell/_WindowsPowerShell/Modules/NET_Framework_Install/NET_Framework_Install.psm1
@@ -23,38 +23,36 @@ function NET_Framework_Install {
 	
 	# ------------------------------------------------------------
 	# Broken-up into separate .NET Runtime installs:
-	
-	<# Force TLS 1.1 / TLS 1.2 (as windows uses to SSL 3.0 / TLS 1.0 by-default on a fresh format ) #> 
-	[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]'Tls11,Tls12';
 
-	# Install (from Microsoft) the following .NET Frameworks: [ 2019x64, 2019x86, 2017x64, 2017x86, 2015x64, 2015x86, 2010x64, 2010x86, 2008x64, 2008x86 ]
-	# $URL="http://go.microsoft.com/fwlink/?LinkId=2085155"; $FN="${Env:Temp}\Install_.NET-Framework-4.8.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	# $URL="https://download.microsoft.com/download/1/B/E/1BE39E79-7E39-46A3-96FF-047F95396215/dotNetFx40_Full_setup.exe"; $FN="${Env:Temp}\Install_.NET-Framework-4.0.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	# $URL="http://download.microsoft.com/download/2/0/E/20E90413-712F-438C-988E-FDAA79A8AC3D/dotnetfx35.exe"; $FN="${Env:Temp}\Install_.NET-Framework-3.5.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	
+	If ($True) {
+		
+		<# Force TLS 1.1 / TLS 1.2 (as windows uses to SSL 3.0 / TLS 1.0 by-default on a fresh format ) #> 
+		[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]'Tls11,Tls12';
 
-	# Install (from Ninite) the following .NET Frameworks: [ 2019x64, 2019x86, 2017x64, 2017x86, 2015x64, 2015x86, 2010x64, 2010x86, 2008x64, 2008x86 ]
-	$URL="https://ninite.com/.net4.8/ninite.exe"; $FN="${Env:Temp}\Install_.NET-Framework-4.8.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://ninite.com/.net4.0/ninite.exe"; $FN="${Env:Temp}\Install_.NET-Framework-4.0.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://ninite.com/.net3.5/ninite.exe"; $FN="${Env:Temp}\Install_.NET-Framework-3.5.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		# Install (from Microsoft) the following .NET Frameworks: [ 2019x64, 2019x86, 2017x64, 2017x86, 2015x64, 2015x86, 2010x64, 2010x86, 2008x64, 2008x86 ]
+		$URL="http://go.microsoft.com/fwlink/?LinkId=2085155"; $FN="${Env:Temp}\Install_.NET-Framework-4.8.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		$URL="https://download.microsoft.com/download/1/B/E/1BE39E79-7E39-46A3-96FF-047F95396215/dotNetFx40_Full_setup.exe"; $FN="${Env:Temp}\Install_.NET-Framework-4.0.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		$URL="http://download.microsoft.com/download/2/0/E/20E90413-712F-438C-988E-FDAA79A8AC3D/dotnetfx35.exe"; $FN="${Env:Temp}\Install_.NET-Framework-3.5.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
+		# Install (from Ninite) the following .NET Frameworks: [ 2019x64, 2019x86, 2017x64, 2017x86, 2015x64, 2015x86, 2010x64, 2010x86, 2008x64, 2008x86 ]
+		# $URL="https://ninite.com/.net4.8/ninite.exe"; $FN="${Env:Temp}\Install_.NET-Framework-4.8.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		# $URL="https://ninite.com/.net4.0/ninite.exe"; $FN="${Env:Temp}\Install_.NET-Framework-4.0.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		# $URL="https://ninite.com/.net3.5/ninite.exe"; $FN="${Env:Temp}\Install_.NET-Framework-3.5.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
-	<# Microsoft Visual C++ Redistributable for Visual Studio 2015, 2017 and 2019 #>
-	$URL="https://aka.ms/vs/16/release/vc_redist.x86.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.002.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://aka.ms/vs/16/release/VC_redist.x64.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.002.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		<# Microsoft Visual C++ Redistributable for Visual Studio 2015, 2017 and 2019 #>
+		$URL="https://aka.ms/vs/16/release/vc_redist.x86.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		$URL="https://aka.ms/vs/16/release/VC_redist.x64.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
-	<# Microsoft Visual C++ Redistributable for Visual Studio 2015, 2017 and 2019 #>
-	$URL="https://my.visualstudio.com/Downloads?pid=1452"; $FN="${Env:Temp}\Install_.NET-Framework-2019.002.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://aka.ms/vs/16/release/VC_redist.x64.exe"; $FN="${Env:Temp}\Install_.NET-Framework-2019.002.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		<# 2010 #>
+		$URL="https://www.microsoft.com/en-pk/download/details.aspx?id=8328"; $FN="${Env:Temp}\Install_.NET-Framework-2010.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26999"; $FN="${Env:Temp}\Install_.NET-Framework-2010.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
-	<# 2010 #>
-	$URL="https://www.microsoft.com/en-pk/download/details.aspx?id=8328"; $FN="${Env:Temp}\Install_.NET-Framework-2010.001.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26999"; $FN="${Env:Temp}\Install_.NET-Framework-2010.002.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		<# 2008 #>
+		$URL="https://www.microsoft.com/en-us/download/details.aspx?id=29"; $FN="${Env:Temp}\Install_.NET-Framework-2008.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		$URL="https://www.microsoft.com/en-us/download/details.aspx?id=15336"; $FN="${Env:Temp}\Install_.NET-Framework-2008.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+		$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26368"; $FN="${Env:Temp}\Install_.NET-Framework-2008.$(Get-Date -UFormat '%Y%m%d-%H%M%S').exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
 
-	<# 2008 #>
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=29"; $FN="${Env:Temp}\Install_.NET-Framework-2008.001.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=15336"; $FN="${Env:Temp}\Install_.NET-Framework-2008.002.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
-	$URL="https://www.microsoft.com/en-us/download/details.aspx?id=26368"; $FN="${Env:Temp}\Install_.NET-Framework-2008.003.exe"; Write-Host "Downloading/Installing `"${URL}`"..."; $(New-Object Net.WebClient).DownloadFile(([System.Net.HttpWebRequest]::Create("${URL}").GetResponse().ResponseUri.AbsoluteUri),$FN); Start-Process -Filepath ($FN) -ArgumentList (@("/q","/norestart"));
+	}
 
 	Return;
 

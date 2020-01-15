3b2938820c52bdfc65c687a8a1c8327343bf9e59
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 01:53:44 -0600

    Update PowerShell - Administrator (check if admin).ps1

--- powershell/PowerShell - Administrator (check if admin).ps1
+++ powershell/PowerShell - Administrator (check if admin).ps1
@@ -1,12 +1,10 @@
-
-# Check whether-or-not the current PowerShell session is running with elevated privileges (as Administrator)
+<# Check whether-or-not the current PowerShell session is running with elevated privileges (as Administrator) #>
 If (!([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	# Check whether-or-not the current user is able to escalate their own PowerShell terminal to run with elevated privileges (as Administrator)
+	<# Script is >> NOT << running as admin  -->  Check whether-or-not the current user is able to escalate their own PowerShell terminal to run with elevated privileges (as Administrator) #>
 	$LocalAdmins = (([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % {([ADSI]$_).InvokeGet('AdsPath')});
 	$CurrentUser = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name);
 	$CurrentUserWinNT = ("WinNT://$($CurrentUser.Replace("\","/"))");
 	If (($LocalAdmins.Contains($CurrentUser)) -Or ($LocalAdmins.Contains($CurrentUserWinNT))) {
-		# Script is >> NOT << running as admin  --> Attempt to open an admin terminal with the same command-line arguments as the current
 		$CommandString = $MyInvocation.MyCommand.Name;
 		$PSBoundParameters.Keys | ForEach-Object { $CommandString += " -$_"; If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) { $CommandString += " `"$($PSBoundParameters[$_])`""; } };
 		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"$($CommandString)`"" -Verb RunAs;
@@ -14,6 +12,6 @@ If (!([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]
 		Write-Host "Error:  User lacks sufficient privilege to perform privilege escalation (e.g. cannot run as admin)" -BackgroundColor Black -ForegroundColor Red;
 	}
 } Else {
-	# Script >> IS << running as Admin - Continue
+	<# Script >> IS << running as Admin - Continue #>
 	Write-Host "Info:  Script running with Admin rights - Continuing...";
 }

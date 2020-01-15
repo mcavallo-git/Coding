20d0b789e5ef4ba65b6e5926bf964f88b7947d7c
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-14 19:57:28 -0600

    Create cmd - move.bat

---
+++ cmd/cmd - move.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+REM 
+REM ### MOVE A FILE
+REM   MOVE /Y "%Source%" "%Destination%"
+REM 
+REM ------------------------------------------------------------
+
+MOVE /Y "%Source%" "%Destination%"
+
+
+REM ------------------------------------------------------------
+REM
+REM Citation(s)
+REM
+REM   docs.microsoft.com  |  "move - Moves one or more files from one directory to another directory"  |  https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/move
+REM
+REM ------------------------------------------------------------

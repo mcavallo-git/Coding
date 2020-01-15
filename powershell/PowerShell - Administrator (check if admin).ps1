621d8f312878e628c0fd47471cc385a39c77ca2b
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 01:48:52 -0600

    Update PowerShell - Administrator (check if admin).ps1

--- powershell/PowerShell - Administrator (check if admin).ps1
+++ powershell/PowerShell - Administrator (check if admin).ps1
@@ -3,8 +3,9 @@
 If (!([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	# Check whether-or-not the current user is able to escalate their own PowerShell terminal to run with elevated privileges (as Administrator)
 	$LocalAdmins = (([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % {([ADSI]$_).InvokeGet('AdsPath')});
-	$UserHasAdminRights = If (($LocalAdmins.Contains(([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name)) -Or ($LocalAdmins.Contains("WinNT://$($CurrentUser.Replace("\","/"))"))) { $True } Else { $False };
-	If (${UserHasAdminRights} -Eq $True) {
+	$CurrentUser = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name);
+	$CurrentUserWinNT = ("WinNT://$($CurrentUser.Replace("\","/"))");
+	If (($LocalAdmins.Contains($CurrentUser)) -Or ($LocalAdmins.Contains($CurrentUserWinNT))) {
 		# Script is >> NOT << running as admin  --> Attempt to open an admin terminal with the same command-line arguments as the current
 		$CommandString = $MyInvocation.MyCommand.Name;
 		$PSBoundParameters.Keys | ForEach-Object { $CommandString += " -$_"; If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) { $CommandString += " `"$($PSBoundParameters[$_])`""; } };

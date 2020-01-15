8915813418c9f3f754a683d80d44facfa974a255
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-14 18:43:35 -0600

    Create Get-ComputerInfo (List Hardware, Firmware, and OS Properties).ps1

---
+++ powershell/Get-ComputerInfo (List Hardware, Firmware, and OS Properties).ps1
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------
+#
+# PowerShell - List Hardware, Firmware, and OS Properties
+#
+# ------------------------------------------------------------
+
+$ComputerInfo = (Get-ComputerInfo);
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Get-ComputerInfo - Gets a consolidated object of system and operating system properties"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-computerinfo
+
+#
+# ------------------------------------------------------------

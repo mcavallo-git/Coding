ef5871f324c1d1788417b120c1e7512498d08afa
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 01:59:46 -0600

    Update PowerShell - Administrator (check if admin).ps1

--- powershell/PowerShell - Administrator (check if admin).ps1
+++ powershell/PowerShell - Administrator (check if admin).ps1
@@ -1,6 +1,9 @@
 <# Check whether-or-not the current PowerShell session is running with elevated privileges (as Administrator) #>
 $RunningAsAdmin = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"));
-If ($RunningAsAdmin -Eq $False) {
+If ($RunningAsAdmin -Eq $True) {
+	<# Script >> IS << running as Admin - Continue #>
+	Write-Host "Info:  Script running with Admin rights - Continuing...";
+} Else {
 	<# Script is >> NOT << running as admin  -->  Check whether-or-not the current user is able to escalate their own PowerShell terminal to run with elevated privileges (as Administrator) #>
 	$LocalAdmins = (([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % {([ADSI]$_).InvokeGet('AdsPath')});
 	$CurrentUser = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name);
@@ -12,7 +15,4 @@ If ($RunningAsAdmin -Eq $False) {
 	} Else {
 		Write-Host "`n`nError:  Insufficient privileges, unable to escalate (e.g. unable to run as admin)`n`n" -BackgroundColor Black -ForegroundColor Yellow;
 	}
-} Else {
-	<# Script >> IS << running as Admin - Continue #>
-	Write-Host "Info:  Script running with Admin rights - Continuing...";
 }

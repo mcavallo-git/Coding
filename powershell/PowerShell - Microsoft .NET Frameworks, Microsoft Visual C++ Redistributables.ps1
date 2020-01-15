9311ed2f962a358f8cc370f70ec4fe3b284e5f62
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 08:56:02 -0600

    Create PowerShell - Microsoft .NET Frameworks, Microsoft Visual C++ Redistributables.ps1

---
+++ powershell/PowerShell - Microsoft .NET Frameworks, Microsoft Visual C++ Redistributables.ps1
@@ -0,0 +1,11 @@
+# ------------------------------------------------------------
+#
+# Microsoft .NET Frameworks
+#
+# Microsoft Visual C++ Redistributables
+#
+# ------------------------------------------------------------
+
+
+### Refer to "NET_Framework_Install" method  -->  https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/NET_Framework_Install/NET_Framework_Install.psm1  
+

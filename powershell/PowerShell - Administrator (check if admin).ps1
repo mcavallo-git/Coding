29c9ee4a430f73432fe59a2bf3aa21241a90a85e
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-15 01:44:17 -0600

    Update PowerShell - Administrator (check if admin).ps1

--- powershell/PowerShell - Administrator (check if admin).ps1
+++ powershell/PowerShell - Administrator (check if admin).ps1
@@ -1,11 +1,19 @@
 
-# Check whether-or-not the current PowerShell session is running with elevated privileges (admin rights)
+# Check whether-or-not the current PowerShell session is running with elevated privileges (as Administrator)
 If (!([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-	# Script is >> NOT << running as admin  --> Attempt to open an admin terminal with the same command-line arguments as the current
-	$CommandString = $MyInvocation.MyCommand.Name;
-	$PSBoundParameters.Keys | ForEach-Object { $CommandString += " -$_"; If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) { $CommandString += " `"$($PSBoundParameters[$_])`""; } };
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"$($CommandString)`"" -Verb RunAs;
 
+	# Check whether-or-not the current user is able to escalate their own PowerShell terminal to run with elevated privileges (as Administrator)
+	$LocalAdmins = (([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % {([ADSI]$_).InvokeGet('AdsPath')});
+	$UserHasAdminRights = If (($LocalAdmins.Contains(([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name)) -Or ($LocalAdmins.Contains("WinNT://$($CurrentUser.Replace("\","/"))"))) { $True } Else { $False };
+	If (${UserHasAdminRights} -Eq $True) {
+		# Script is >> NOT << running as admin  --> Attempt to open an admin terminal with the same command-line arguments as the current
+		$CommandString = $MyInvocation.MyCommand.Name;
+		$PSBoundParameters.Keys | ForEach-Object { $CommandString += " -$_"; If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) { $CommandString += " `"$($PSBoundParameters[$_])`""; } };
+		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"$($CommandString)`"" -Verb RunAs;
+	} Else {
+		Write-Host "Error:  User lacks sufficient privilege to perform privilege escalation (cannot run as admin)" -BackgroundColor Black -ForegroundColor Red;
+
+	}
 
 } Else {
 	# Script >> IS << running as Admin - Continue

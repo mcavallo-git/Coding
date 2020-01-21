4aecf5fae2fe40c1f00fd7cee48cbde5ba748ffd
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-21 13:19:45 -0600

    Update PowerShell - Get-NetFirewallRule, New-NetFirewallRule (Set Inbound-Outbound rules to open tcp-udp ports, etc. via PowerShell).ps1

--- powershell/PowerShell - Get-NetFirewallRule, New-NetFirewallRule (Set Inbound-Outbound rules to open tcp-udp ports, etc. via PowerShell).ps1
+++ powershell/PowerShell - Get-NetFirewallRule, New-NetFirewallRule (Set Inbound-Outbound rules to open tcp-udp ports, etc. via PowerShell).ps1
@@ -11,14 +11,21 @@ Get-NetFirewallRule -All;
 <# Create a firewall rule to allow 21 (FTP) through #> 
 New-NetFirewallRule -DisplayName "FTP (TCP 21)" -Description "FTP - Allow Inbound TCP Traffic on Port(s) 21" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 21;
 
+
 <# Create a firewall rule to allow 80 & 443 (HTTP, HTTPS) through #> 
 New-NetFirewallRule -DisplayName "HTTP,HTTPS (TCP 80,443)" -Description "HTTP,HTTPS - Allow Inbound TCP Traffic on Port(s) 80,443" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 80,443;
+
 
 <# Create a new firewall rule to allow 9000 (Minio) service through #>
 New-NetFirewallRule -DisplayName "Minio (TCP 9000)" -Description "MinIO - Allow Inbound TCP Traffic on Port(s) 9000" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 9000;
 
+
 <# Create a new firewall rule to allow 27017 (MongoDB) service through #>
 New-NetFirewallRule -DisplayName "MongoDB (TCP 27017)" -Description "MongoDB - Allow Inbound TCP Traffic on Port(s) 27017" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 27017;
+
+
+<# Create a new firewall rule to allow 3389 (Microsoft SQL) service through #>
+New-NetFirewallRule -DisplayName "GVS Microsoft SQL (TCP 3389)" -Description "Microsoft SQL - Allow Inbound TCP Traffic on Port(s) 3389" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 3389;
 
 
 # ------------------------------------------------------------

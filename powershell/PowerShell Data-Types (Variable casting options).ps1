216df4a3d3005294e6f12da38d63890a4e9dfca8
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-21 06:35:41 -0600

    Create PowerShell Data-Types (Variable casting options).ps1

---
+++ powershell/PowerShell Data-Types (Variable casting options).ps1
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------
+# 
+# PowerShell Data-Types (Variable casting options)
+#  |--> For insight into numeric precision for applications without being wasteful regarding system resources)
+# 
+# ------------------------------------------------------------
+#
+#   [string]    Fixed-length string of Unicode characters
+#   [char]      A Unicode 16-bit character
+#   [byte]      An 8-bit unsigned character
+#
+#   [int]       32-bit signed integer
+#   [long]      64-bit signed integer
+#   [bool]      Boolean True/False value
+#
+#   [decimal]   A 128-bit decimal value
+#   [single]    Single-precision 32-bit floating point number
+#   [double]    Double-precision 64-bit floating point number
+#   [DateTime]  Date and Time
+#
+#
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#		docs.microsoft.com  |  "Get-Date - Gets the current date and time."  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-date
+#
+#		docs.microsoft.com  |  "Math.Floor Method (System) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/dotnet/api/system.math.floor
+#
+#		stackoverflow.com  |  "Powershell Data Types - PowerShell - SS64.com"  |  https://ss64.com/ps/syntax-datatypes.html
+#
+#		stackoverflow.com  |  "Midnight last Monday in Powershell"  |  https://stackoverflow.com/a/42578179
+#
+#		stackoverflow.com  |  "Powershell - Round down to nearest whole number - Stack Overflow"  |  https://stackoverflow.com/a/5864061
+#
+# ------------------------------------------------------------

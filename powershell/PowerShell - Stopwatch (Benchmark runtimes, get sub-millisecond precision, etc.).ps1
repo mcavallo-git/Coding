9527a9bdbac40ac197b560d54878fa353a02f47a
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-21 06:35:36 -0600

    Create PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1

---
+++ powershell/PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1
@@ -0,0 +1,22 @@
+
+
+$Benchmark = New-Object System.Diagnostics.Stopwatch;
+
+$Benchmark.Start();
+
+$Benchmark.Stop();
+
+$Benchmark;
+
+
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Stopwatch Class (System.Diagnostics) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch
+#
+#   stackoverflow.com  |  "timespan - Find time in milliseconds using PowerShell? - Stack Overflow"  |  https://stackoverflow.com/a/9066226
+#
+# ------------------------------------------------------------

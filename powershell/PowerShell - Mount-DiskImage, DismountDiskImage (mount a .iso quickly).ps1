5a9b64211178b5b4ebdf6af9d747f93be91ff851
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-22 05:32:31 -0600

    Update PowerShell - Mount-DiskImage, DismountDiskImage (mount a .iso quickly).ps1

--- powershell/PowerShell - Mount-DiskImage, DismountDiskImage (mount a .iso quickly).ps1
+++ powershell/PowerShell - Mount-DiskImage, DismountDiskImage (mount a .iso quickly).ps1
@@ -15,7 +15,7 @@ Dismount-DiskImage -ImagePath ("${Home}\Desktop\Windows.iso");
 #
 # Citation(s)
 #
-#   docs.microsoft.com  |  "Dismount-DiskImage - Dismounts a disk image (virtual hard disk or ISO) so that it can no longer be accessed as a disk"  |  https://docs.microsoft.com/en-us/powershell/module/storage/dismount-diskimage?view=win10-ps
+#   docs.microsoft.com  |  "Dismount-DiskImage - Dismounts a disk image (virtual hard disk or ISO) so that it can no longer be accessed as a disk"  |  https://docs.microsoft.com/en-us/powershell/module/storage/dismount-diskimage
 #
 #   docs.microsoft.com  |  "Mount-DiskImage - Mounts a previously created disk image (virtual hard disk or ISO), making it appear as a normal disk"  |  https://docs.microsoft.com/en-us/powershell/module/storage/mount-diskimage
 #

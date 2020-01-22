a4f3ee355353c0628795e5cc9c9c7b30d7d5ddda
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-22 04:56:43 -0600

    Update Windows - Intel VROC Drivers.ps1

--- windows/Windows - Intel VROC Drivers.ps1
+++ windows/Windows - Intel VROC Drivers.ps1
@@ -1,12 +1,20 @@
 # ------------------------------------------------------------
-
+#
 # !!! NOTE: The following is stated in the "Intel VROC Quick Configuration Guide" @  [ https://www.intel.com/content/dam/support/us/en/documents/server-products/server-boards/Intel-VROC-Quick-Configuration-Guide.pdf ]:
-$Intel_QuickStart_DirectStatement="Note: Intel VROC is not compatible with Secure Boot. If you want to use Intel VROC (VMD NVME RAID), do not enable the system's Secure Boot feature. If Secure boot is required for the solution Intel VROC cannot be used";
 
+$Intel_QuickStart_DirectStatement= `
+"Note: Intel VROC is not compatible with Secure Boot. 
+ If you want to use Intel VROC (VMD NVME RAID), do not enable the
+ system's Secure Boot feature. If Secure boot is required for the
+ solution Intel VROC cannot be used.        --Intel";
 
+#
+# ------------------------------------------------------------
+#
 ### The soution for a Lenovo P520c workstation was two-part:
-
-
+#
+# ------------------------------------------------------------
+#
 ### Part 1/2: Set the following values within the BIOS/UEFI settings:
 #
 # SECURE BOOT = OFF
@@ -15,19 +23,19 @@ $Intel_QuickStart_DirectStatement="Note: Intel VROC is not compatible with Secur
 #
 # BOOT FROM PCI-E/PCI DEVICES --> SET TO BOOT FROM UEFI, FIRST
 #
+#
+# ------------------------------------------------------------
+#
+### Part 2/2
+# > Injecting the necessary drivers into the Windows Install media's image (much easier than it sounds thanks to guide, below)
+#  > Pull the RAID Controller drivers from your motherboard-manufacturer's website (if software RAID) or from your RAID-controller manufacturer's website (if hardware RAID)
+#   > Install (unpack) all drivers into the directory "C:\Drivers" (create it if it doesn't already exist)
+#
+#
+# Once you feel you have your necessary drivers, follow the guide @ http://woshub.com/integrate-drivers-to-windows-install-media/
+# > Run the necessary DISM/PowerShell commands to create Windows boot media w/ RAID (VROC) drivers pre-installed and ready to go
+#
 
-
-### Part 2/2: Inject the necessary drivers into the Windows Install media's image (much easier than it sounds thanks to guide, below)
-#
-# Pull the RAID Controller drivers from your motherboard-manufacturer's website (if software RAID) or from your RAID-controller manufacturer's website (if hardware RAID)
-#
-# Install (unpack) all drivers into the directory "C:\Drivers" (create it if it doesn't already exist)
-#
-# Once you feel you have your necessary drivers, follow the guide @ 
-# http://woshub.com/integrate-drivers-to-windows-install-media/
-# to run the necessary DISM/etc. commands to create Windows boot media w/ RAID (VROC) drivers pre-installed and ready to go
-#
-#
 
 
 # ------------------------------------------------------------

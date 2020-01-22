fa4cf5ff94be27a791924d4c205e8e1c914fb7f7
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-21 19:54:02 -0600

    Update Linux - ps -p PID (get info about specific PID which is currently running).sh

--- linux/Linux - ps -p PID (get info about specific PID which is currently running).sh
+++ linux/Linux - ps -p PID (get info about specific PID which is currently running).sh
@@ -3,10 +3,15 @@
 # Linux - ps -p PID (get info about specific PID which is currently running)
 PID=60010; ps -p $PID -o pid,vsz=MEMORY -o user,group=GROUP -o comm,args=ARGS;
 
+# Linux - ps -p PID (get info about specific PID which is currently running)
+PID=60010; ps -Flww -p $PID;
+
 
 # ------------------------------------------------------------
 #
 # Citation(s)
+#
+#   askubuntu.com  |  "command line - How to see detailed information about a given PID? - Ask Ubuntu"  |  https://askubuntu.com/a/831521
 #
 #   stackoverflow.com  |  "go - How to get process details from its pid - Stack Overflow"  |  https://stackoverflow.com/a/13780824
 #

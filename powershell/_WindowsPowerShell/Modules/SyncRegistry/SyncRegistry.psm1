9ebc0424af34963d6b2665c9d5ffe5d7b2a0ab06
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-22 01:16:42 -0600

    Update SyncRegistry.psm1

--- powershell/_WindowsPowerShell/Modules/SyncRegistry/SyncRegistry.psm1
+++ powershell/_WindowsPowerShell/Modules/SyncRegistry/SyncRegistry.psm1
@@ -9,11 +9,27 @@ function SyncRegistry {
 	Param(
 	)
 
-	If ((RunningAsAdministrator) -Ne ($True)) {
+	# If ((RunningAsAdministrator) -Ne ($True)) {
+	# 	PrivilegeEscalation -Command ("SyncRegistry") {
+	# } Else {
 
-		PrivilegeEscalation -Command ("SyncRegistry");
-	
+	<# Check whether-or-not the current PowerShell session is running with elevated privileges (as Administrator) #>
+	$RunningAsAdmin = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator"));
+	If ($RunningAsAdmin -Eq $False) {
+		<# Script is >> NOT << running as admin  -->  Check whether-or-not the current user is able to escalate their own PowerShell terminal to run with elevated privileges (as Administrator) #>
+		$LocalAdmins = (([ADSI]"WinNT://./Administrators").psbase.Invoke('Members') | % {([ADSI]$_).InvokeGet('AdsPath')});
+		$CurrentUser = (([Security.Principal.WindowsPrincipal]([Security.Principal.WindowsIdentity]::GetCurrent())).Identities.Name);
+		$CurrentUserWinNT = ("WinNT://$($CurrentUser.Replace("\","/"))");
+		If (($LocalAdmins.Contains($CurrentUser)) -Or ($LocalAdmins.Contains($CurrentUserWinNT))) {
+			$CommandString = $MyInvocation.MyCommand.Name;
+			$PSBoundParameters.Keys | ForEach-Object { $CommandString += " -$_"; If (@('String','Integer','Double').Contains($($PSBoundParameters[$_]).GetType().Name)) { $CommandString += " `"$($PSBoundParameters[$_])`""; } };
+			Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -Command `"$($CommandString)`"" -Verb RunAs;
+		} Else {
+			Write-Host "`n`nError:  Insufficient privileges, unable to escalate (e.g. unable to run as admin)`n`n" -BackgroundColor Black -ForegroundColor Yellow;
+		}
 	} Else {
+	<# Script >> IS << running as Admin - Continue #>
+
 
 		# ------------------------------------------------------------
 		# TO-DO

7e4621de01aa5b07ca7d447cebe2ca229354cb4b
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-28 08:04:57 -0600

    Update PowerShell - Get-EventLog (search Event Viewer via PowerShell).ps1

--- powershell/PowerShell - Get-EventLog (search Event Viewer via PowerShell).ps1
+++ powershell/PowerShell - Get-EventLog (search Event Viewer via PowerShell).ps1
@@ -2,17 +2,24 @@
 
 $Logfile = "${Home}\Desktop\InstalledProgramHistory_$($(${Env:USERNAME}).Trim())@$($(${Env:COMPUTERNAME}).Trim())" + $(If(${Env:USERDNSDOMAIN}){Write-Output ((".") + ($(${Env:USERDNSDOMAIN}).Trim()))}) +"_$(Get-Date -UFormat '%Y%m%d-%H%M%S').log;"; `
 Get-EventLog -LogName "Application" -Source "MsiInstaller" `
-| Where-Object {$_.Message -NotLike "*EXCLUDE_THIS*"} `
-| Where-Object {$_.Message -like "*INCLUDE_THIS*"} `
+| Select-Object UserName, TimeGenerated, Message `
+| Where-Object {$_.Message -NotLike "*IRSA*"} `
+| Where-Object {$_.Message -NotLike "*GETAC*"} `
+| Where-Object {$_.Message -NotLike "*Product: Config Manager -- Installation completed successfully.*"} `
 | Format-Table -AutoSize `
 | Out-File -Width 16384 "${Logfile}"; `
 Notepad "${Logfile}";
 
 
 
+# NOTE: Event Log will only go back as far as the space alloted for which its log-files.
+
+
 # ------------------------------------------------------------
 #
 # Citation(s)
+#
+#   answers.microsoft.com  |  "Why is my event log so small? - Microsoft Community"  |  https://answers.microsoft.com/en-us/windows/forum/all/why-is-my-event-log-so-small/d6654890-0450-4dfc-ba49-95a7f9b63009
 #
 #   docs.microsoft.com  |  "Get-EventLog - Gets the events in an event log, or a list of the event logs, on the local computer or remote computers"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-eventlog
 #

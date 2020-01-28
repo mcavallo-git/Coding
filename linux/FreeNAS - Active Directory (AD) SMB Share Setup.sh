d87d4f6c12d1eeb5c4298153740f689488919f14
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-27 18:56:56 -0600

    Create FreeNAS - Active Directory (AD) SMB Share Setup.sh

---
+++ linux/FreeNAS - Active Directory (AD) SMB Share Setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ------------------------------------------------------------
+#
+# Setting up FreeNAS with AD Authentication for SMB Share(s)
+#
+# See Citations (below), for tutorials
+#
+#
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.ixsystems.com  |  "FreeNAS unable to find domain controllers | iXsystems Community"  |  https://www.ixsystems.com/community/threads/freenas-unable-to-find-domain-controllers.30543/#post-209908
+#
+#   www.ixsystems.com  |  "[How-To] Properly setup FreeNAS 9.2.1.5 to use Active Directory folder/file/user permissions | iXsystems Community"  |  https://www.ixsystems.com/community/threads/how-to-properly-setup-freenas-9-2-1-5-to-use-active-directory-folder-file-user-permissions.20610/
+#
+# ------------------------------------------------------------

ebacc24eb3ce8b7abca43ca6c57c07b136fdc65b
Matt Cavallo <mcavallo.git@gmail.com>
2020-01-28 06:48:56 -0600

    Create PowerShell - Format-Table -Autosize + Out-File -Width X (Show full table data without truncation of values).ps1

---
+++ powershell/PowerShell - Format-Table -Autosize + Out-File -Width X (Show full table data without truncation of values).ps1
@@ -0,0 +1,26 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Format-Table -Autosize + Out-File -Width X (Show full table data without truncation of values)
+#
+# ------------------------------------------------------------
+
+
+$Logfile = "${Home}\Desktop\ProgramsAndFeatures_$(Hostname)_$(Get-Date -UFormat '%Y%m%d-%H%M%S').log;"; `
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* `
+| Select-Object DisplayName, DisplayVersion, Publisher, InstallDate `
+| Where-Object { ([String]($_.DisplayName)).Trim() -NE "" } `
+| Sort-Object -Property DisplayName `
+| Format-Table -AutoSize `
+| Out-File -Width 512 "${Logfile}"; `
+Notepad "${Logfile}";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   stackoverflow.com  |  "powershell - How do I use Format-Table without truncation of values? - Stack Overflow"  |  https://stackoverflow.com/a/49123225
+#
+#   www.howtogeek.com  |  "How to Create a List of Your Installed Programs on Windows"  |  https://www.howtogeek.com/165293/how-to-get-a-list-of-software-installed-on-your-pc-with-a-single-command/
+#
+# ------------------------------------------------------------

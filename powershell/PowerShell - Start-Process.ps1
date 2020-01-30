13bb6d7090b3443add7ac36a335607e067ff02dd
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-29 19:06:33 -0600

    Create PowerShell - Start-Process.ps1

---
+++ powershell/PowerShell - Start-Process.ps1
@@ -0,0 +1,12 @@
+
+
+Start-Process -Filepath ("C:\Windows\system32\sc.exe") -ArgumentList (@("restart","W32Time")) -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Start-Process - Starts one or more processes on the local computer"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process
+#
+# ------------------------------------------------------------

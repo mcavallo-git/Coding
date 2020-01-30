2ae76b818b8c637da71eec7dd69e283248fa52f9
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-30 12:43:19 -0600

    Update Linux - selinux contexts (determine blocking issues and create security policies quickly via audit2allow, chcon).sh

--- linux/Linux - selinux contexts (determine blocking issues and create security policies quickly via audit2allow, chcon).sh
+++ linux/Linux - selinux contexts (determine blocking issues and create security policies quickly via audit2allow, chcon).sh
@@ -36,7 +36,7 @@ ls -Z "/var/cache/jenkins/war/images/";
 semanage fcontext -l | grep httpd_sys_content_t;
 
 #
-# View all available policies
+# View all available selinux contexts/policies
 #
 man httpd_selinux;
 

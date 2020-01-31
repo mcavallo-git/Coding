84fd1cfed80e0568589c3d47624392574e369339
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-30 18:17:25 -0600

    Update TeamCity - Migrating TeamCity's database between MS SQL instances.bat

--- teamcity/TeamCity - Migrating TeamCity's database between MS SQL instances.bat
+++ teamcity/TeamCity - Migrating TeamCity's database between MS SQL instances.bat
@@ -16,9 +16,13 @@ maxConnections=50
 poolPreparedStatements=true
 
 
+REM Start-Process "C:\Program Files (x86)\Microsoft Visual Studio\Installer\vs_installer.exe" -ArgumentList 'modify --installPath "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" --quiet --add Microsoft.VisualStudio.Component.NuGet.BuildTools --add Microsoft.Net.Component.4.5.TargetingPack --norestart --force' -Wait -PassThru
+
 REM ------------------------------------------------------------
 REM 
 REM Citation(s)
+REM 
+REM   developercommunity.visualstudio.com  |  ""The "GetReferenceNearestTargetFrameworkTask" task was not found." when building app project with reference to library project if .NET Core cross-platform development workload not installed - Developer Community"  |  https://developercommunity.visualstudio.com/content/problem/137779/the-getreferencenearesttargetframeworktask-task-wa.html
 REM 
 REM   www.jetbrains.com  |  "Migrating to an External Database - Help | TeamCity"  |  https://www.jetbrains.com/help/teamcity/migrating-to-an-external-database.html
 REM 

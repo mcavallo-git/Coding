05552a1ec7df1e0a6f4831c1f74576d896b66fc1
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-31 14:27:36 -0600

    Create VMware ESXi - Datastore Space-Reclamation.sh

---
+++ vmware/VMware ESXi - Datastore Space-Reclamation.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# ------------------------------------------------------------
+
+esxcli storage vmfs unmap;
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   docs.vmware.com  |  "Configure Space Reclamation for a VMFS6 Datastore"  |  https://docs.vmware.com/en/VMware-vSphere/6.5/com.vmware.vsphere.storage.doc/GUID-98901BEE-8675-4D39-8AA2-E790EE341986.html
+#
+#   docs.vmware.com  |  "Verify Configuration for Automatic Space Reclamation"  |  https://docs.vmware.com/en/VMware-vSphere/6.5/com.vmware.vsphere.storage.doc/GUID-57DAC8D9-C64F-4747-9CE8-14068A073712.html
+#
+# ------------------------------------------------------------

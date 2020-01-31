4df5ac76b5e04ab8df2c6ec01e775a9b9b1baf88
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-31 17:32:30 -0600

    Update VMware ESXi - Datastore Space-Reclamation.sh

--- vmware/VMware ESXi - Datastore Space-Reclamation.sh
+++ vmware/VMware ESXi - Datastore Space-Reclamation.sh
@@ -2,11 +2,19 @@
 #
 # ------------------------------------------------------------
 
-esxcli storage vmfs unmap;
+### Check datastore reclamation settings
+esxcli storage vmfs reclaim config get -l=VMFS_LABEL;
+esxcli storage vmfs reclaim config get -u=VMFS_UUID;
+
+### Manually reclaim all space on datastore
+esxcli storage vmfs unmap -l=VMFS_LABEL -n=RECLAIM_RATE
+esxcli storage vmfs unmap -u=VMFS_UUID;
 
 
 # ------------------------------------------------------------
 # Citation(s)
+#
+#   docs.vmware.com  |  "vSphere Documentation Center"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vcli.ref.doc_50%2Fesxcli_storage.html
 #
 #   docs.vmware.com  |  "Configure Space Reclamation for a VMFS6 Datastore"  |  https://docs.vmware.com/en/VMware-vSphere/6.5/com.vmware.vsphere.storage.doc/GUID-98901BEE-8675-4D39-8AA2-E790EE341986.html
 #

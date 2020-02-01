805010655f56ab612a03f192fb63b1a6783dfa59
Matt Cavallo <mcavallo.work@gmail.com>
2020-01-31 21:42:32 -0600

    Update VMware ESXi - Disk (.vmdk) conversion from thick to thin (shrink, compress, deflate to save space, etc.) - vmkfstools -d thin.sh

--- vmware/VMware ESXi - Disk (.vmdk) conversion from thick to thin (shrink, compress, deflate to save space, etc.) - vmkfstools -d thin.sh
+++ vmware/VMware ESXi - Disk (.vmdk) conversion from thick to thin (shrink, compress, deflate to save space, etc.) - vmkfstools -d thin.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+### NOTE: You must (or at the very least, definitely SHOULD) shut down any machine(s) attached to disks which you intend to copy/resize to OR from
+
 vmkfstools -i /vmfs/volumes/datastore1/SERVER_NAME/SERVER_NAME.vmdk -d thin /vmfs/volumes/datastore1/SERVER_NAME/SERVER_NAME-thin.vmdk

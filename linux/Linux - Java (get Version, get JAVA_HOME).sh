c24316f83236dc274b5ef8f00569b0328ea26a78
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-03 15:48:14 -0600

    Update Linux - Java (get Version, get JAVA_HOME).sh

--- linux/Linux - Java (get Version, get JAVA_HOME).sh
+++ linux/Linux - Java (get Version, get JAVA_HOME).sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get java version 
-clear; java -XshowSettings:properties -version 2>&1 | grep -i version;
+clear; java -XshowSettings:properties -version 2>&1 | grep -i java.version;
 
 # get JAVA_HOME
-clear; java -XshowSettings:properties -version 2>&1 | grep -i home;
+clear; java -XshowSettings:properties -version 2>&1 | grep -i java.home;

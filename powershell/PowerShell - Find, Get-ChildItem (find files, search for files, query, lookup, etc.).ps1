8fbeb44f2d9b083a5797cedbba24fcc01d7c663e
Matt Cavallo <mcavallo.git@gmail.com>
2020-02-03 23:53:32 -0600

    Update PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1

--- powershell/PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1
+++ powershell/PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1
@@ -10,6 +10,9 @@
 Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Name -Eq "MSBuild.exe" } | ForEach-Object { $_.FullName; }
 
 
+Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Name -Like "devenv.*" } | ForEach-Object { $_.FullName; }
+
+
 # ------------------------------------------------------------
 #
 # Advanced search

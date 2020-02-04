84ee7ea7a84773642a604bf51eb2c023478d108e
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-03 20:52:46 -0600

    Create VSCode - Identifiers for setting [ files.associations ].ps1

---
+++ powershell/VSCode - Identifiers for setting [ files.associations ].ps1
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------
+#
+# VSCode - Identifiers for setting [ files.associations ]
+#  |
+#  |--> https://code.visualstudio.com/docs/languages/identifiers
+#
+#
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   code.visualstudio.com  |  "Filetype Associations (Language Identifiers) - Visual Studio Code / VSCode"  |  https://code.visualstudio.com/docs/languages/identifiers
+#
+# ------------------------------------------------------------

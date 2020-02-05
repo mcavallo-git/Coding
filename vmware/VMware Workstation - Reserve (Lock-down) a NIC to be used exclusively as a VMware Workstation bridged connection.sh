7f1ed983fc79d331d57425f4c5a2dd4f57ca79f4
Matt Cavallo <mcavallo.git@gmail.com>
2020-02-05 02:21:15 -0600

    Create VMware Workstation - Reserve (Lock-down) a NIC to be used exclusively as a VMware Workstation bridged connection.sh

---
+++ vmware/VMware Workstation - Reserve (Lock-down) a NIC to be used exclusively as a VMware Workstation bridged connection.sh
@@ -0,0 +1,10 @@
+
+https://kb.vmware.com/s/article/1020359
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   kb.vmware.com  |  "Using a network adapter only with the VMware Workstation guest virtual machine (1020359)"  |  https://kb.vmware.com/s/article/1020359
+#
+# ------------------------------------------------------------

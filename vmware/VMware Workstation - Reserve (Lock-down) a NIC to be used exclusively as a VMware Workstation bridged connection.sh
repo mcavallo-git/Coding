f94e0762b995a206cf0f3a6dd504f3e3f3e706cd
Matt Cavallo <mcavallo.git@gmail.com>
2020-02-05 02:22:55 -0600

    Update VMware Workstation - Reserve (Lock-down) a NIC to be used exclusively as a VMware Workstation bridged connection.sh

--- vmware/VMware Workstation - Reserve (Lock-down) a NIC to be used exclusively as a VMware Workstation bridged connection.sh
+++ vmware/VMware Workstation - Reserve (Lock-down) a NIC to be used exclusively as a VMware Workstation bridged connection.sh
@@ -1,5 +1,8 @@
 
-https://kb.vmware.com/s/article/1020359
+Similar to setting a static IP in windows, open "View Network Connections" (or run "ncpa.cpl")
+ > Right click the connection you wish to defined as a VMware Workstation Bridged -> Select 'Properties'
+  > Uncheck all items EXCEPT "VMware Bridge Protocol"
+   > Hit OK and you should be all set
 
 
 # ------------------------------------------------------------

998268163b616787cb8d64648818090a4570e52a
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-11 21:13:44 -0600

    Create Decode-SecureString.psm1

---
+++ powershell/_WindowsPowerShell/Modules/Decode-SecureString/Decode-SecureString.psm1
@@ -0,0 +1,45 @@
+# ------------------------------------------------------------
+#
+# .Synopsis
+#    Tries to decode a SecureString and returns its clear text value
+#
+# .DESCRIPTION
+#    Tries to decode a SecureString from a PSCredential Object and returns its clear text value
+#
+# .EXAMPLE
+#    SecureString_Decoder -SecureString (Read-Host -AsSecureString)
+#
+# .AUTHOR
+#    paolofrigo@gmail.com , 2018 https://www.scriptinglibrary.com
+#
+# ------------------------------------------------------------
+Function SecureString_Decoder
+{
+	[CmdletBinding()]
+	[Alias('dssp')]
+	[OutputType([string])]
+	Param
+	(
+		[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)] $SecureString
+	)
+	Begin
+	{
+	}
+	Process
+	{
+		Return [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($SecureString));
+	}
+	End
+	{
+	}
+}
+Export-ModuleMember -Function "SecureString_Decoder";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.scriptinglibrary.com  |  "Passwords and SecureString, How To Decode It with Powershell | Scripting Library"  |  https://www.scriptinglibrary.com/languages/powershell/securestring-how-to-decode-it-with-powershell/
+#
+# ------------------------------------------------------------

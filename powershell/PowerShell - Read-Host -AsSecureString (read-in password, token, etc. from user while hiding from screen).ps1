19d8acb70f70ab2199b13367de66c1d197ce5ab2
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-11 21:17:45 -0600

    Create PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1

---
+++ powershell/PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1
@@ -0,0 +1,12 @@
+
+
+$PasswordPlaintext = [System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode(Read-Host -AsSecureString));
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.scriptinglibrary.com  |  "Passwords and SecureString, How To Decode It with Powershell | Scripting Library"  |  https://www.scriptinglibrary.com/languages/powershell/securestring-how-to-decode-it-with-powershell/
+#
+# ------------------------------------------------------------

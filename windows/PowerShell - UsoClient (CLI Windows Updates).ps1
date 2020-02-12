e90b93c18bd4a48fb7485492aad3ce8ceaba9ffb
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-11 19:40:42 -0600

    Update PowerShell - UsoClient (CLI Windows Updates).ps1

--- windows/PowerShell - UsoClient (CLI Windows Updates).ps1
+++ windows/PowerShell - UsoClient (CLI Windows Updates).ps1
@@ -1,34 +1,41 @@
-
-# Check for Windows Updates
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartScan") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
-
-
-# Download any available Windows Updates
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartDownload") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
-
-
-# Install downloaded Windows Updates
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartInstall") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
-
-
 # Refresh settings if any changes were made
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("RefreshSettings") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
-
-
-# May ask for user input and/or open dialogues to show progress or report errors
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartInteractiveScan") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("RefreshSettings") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe RefreshSettings
 
 
 # Restart device to finish installation of Windows Updates
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("RestartDevice") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
-
-
-# Combined scan, download, and install Windows Updates
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("ScanInstallWait") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("RestartDevice") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe RestartDevice
 
 
 # Resume update installation on boot
-Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("ResumeUpdate") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("ResumeUpdate") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe ResumeUpdate
+
+
+# Download any available Windows Updates
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartDownload") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe StartDownload
+
+
+# Install downloaded Windows Updates
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartInstall") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe StartInstall
+
+
+# May ask for user input and/or open dialogues to show progress or report errors
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartInteractiveScan") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe StartInteractiveScan
+
+
+# Combined scan, download, and install Windows Updates
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("ScanInstallWait") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe ScanInstallWait
+
+
+# Check for Windows Updates
+# Start-Process -Filepath ("C:\Windows\system32\UsoClient.exe") -ArgumentList ("StartScan") -Verb ("RunAs") -ErrorAction ("SilentlyContinue");
+UsoClient.exe StartScan
 
 
 # ------------------------------------------------------------

39ead51d046141c39329a4c5b394ea880902baa2
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-12 18:53:57 -0600

    Update PowerShell - Microsoft.Update.AutoUpdate (CLI Windows Updates).ps1

--- windows/PowerShell - Microsoft.Update.AutoUpdate (CLI Windows Updates).ps1
+++ windows/PowerShell - Microsoft.Update.AutoUpdate (CLI Windows Updates).ps1
@@ -4,7 +4,8 @@
 <# Check-for & Download available Windows Updates #> ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());
 
 
-<# Clear out cached Windows Updates #> Get-Service | Where-Object { $_.DisplayName -Eq 'Windows Update' } | ForEach-Object { Write-Host "Stopping `"$($_.DisplayName)`"..."; $_ | Stop-Service; }; If (Test-Path ("C:\Windows\SoftwareDistribution\Download")) { Rename-Item -Path ("C:\Windows\SoftwareDistribution\Download") -NewName ("Download.$(Get-Date -UFormat '%Y%m%d-%H%M%S').bak") -Force; }; Get-Service | Where-Object { $_.DisplayName -Eq 'Windows Update' } | ForEach-Object { Write-Host "Starting `"$($_.DisplayName)`"..."; $_ | Start-Service; }; <# Check-for & Download available Windows Updates #> ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());
+<# Clear out cached Windows Updates #> Get-Service | Where-Object { $_.DisplayName -Eq 'Windows Update' } | ForEach-Object { Write-Host "Stopping `"$($_.DisplayName)`"..."; $_ | Stop-Service; }; If (Test-Path ("C:\Windows\SoftwareDistribution\Download")) { Move-Item -Path ("C:\Windows\SoftwareDistribution\Download") -Destination ("${Env:APPDATA}\SoftwareDistribution-Download.$(Get-Date -UFormat '%Y%m%d-%H%M%S').bak") -Force; }; Get-Service | Where-Object { $_.DisplayName -Eq 'Windows Update' } | ForEach-Object { Write-Host "Starting `"$($_.DisplayName)`"..."; $_ | Start-Service; }; <# Check-for & Download available Windows Updates #> ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());
+
 
 
 # ------------------------------------------------------------

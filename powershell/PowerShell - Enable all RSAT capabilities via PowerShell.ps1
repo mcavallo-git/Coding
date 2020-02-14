a1cda4f0eb1c1b480b9df0ca5bbb80aea48228d2
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-13 19:21:59 -0600

    Update PowerShell - Enable all RSAT capabilities via PowerShell.ps1

--- powershell/PowerShell - Enable all RSAT capabilities via PowerShell.ps1
+++ powershell/PowerShell - Enable all RSAT capabilities via PowerShell.ps1
@@ -8,6 +8,8 @@ Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online;
 #
 # Citation(s)
 #
+#   docs.microsoft.com  |  "ktpass | Microsoft Docs"  |  https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/ktpass
+#
 #   mikefrobbins.com  |  "Use PowerShell to Install the Remote Server Administration Tools (RSAT) on Windows 10 version 1809 – Mike F Robbins"  |  https://mikefrobbins.com/2018/10/03/use-powershell-to-install-the-remote-server-administration-tools-rsat-on-windows-10-version-1809/
 #
 # ------------------------------------------------------------

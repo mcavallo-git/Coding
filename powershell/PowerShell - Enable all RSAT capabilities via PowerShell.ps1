c4426097abc78276946bd8aca896d99b7e27afc2
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-13 19:19:34 -0600

    Create PowerShell - Enable all RSAT capabilities via PowerShell.ps1

---
+++ powershell/PowerShell - Enable all RSAT capabilities via PowerShell.ps1
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+Get-WindowsCapability -Name "RSAT*" -Online;
+
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   mikefrobbins.com  |  "Use PowerShell to Install the Remote Server Administration Tools (RSAT) on Windows 10 version 1809 – Mike F Robbins"  |  https://mikefrobbins.com/2018/10/03/use-powershell-to-install-the-remote-server-administration-tools-rsat-on-windows-10-version-1809/
+#
+# ------------------------------------------------------------

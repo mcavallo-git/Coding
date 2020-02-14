561fe55402f114a68aa0fec01d108acf47a3ff20
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-14 13:59:25 -0600

    Create Linux - wbinfo (AD authentication, troubleshooting).sh

---
+++ linux/Linux - wbinfo (AD authentication, troubleshooting).sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Check whether domain has been joined (or not)
+wbinfo -u
+wbinfo -g
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   help.ubuntu.com  |  "ActiveDirectoryWinbindHowto - Community Help Wiki"  |  https://help.ubuntu.com/community/ActiveDirectoryWinbindHowto
+#
+# ------------------------------------------------------------

2ed98d67b3197528d6be1b7a241798ab4b9d5cb7
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-14 19:41:26 -0600

    Create Microsoft SQL - Add Index (for single Column on target Table).sql

---
+++ sql/Microsoft SQL - Add Index (for single Column on target Table).sql
@@ -0,0 +1,13 @@
+
+CREATE INDEX TABLENAME_COLUMNNAME ON TABLENAME (COLUMNNAME);
+
+
+/*
+----------------------------------------------------------
+
+Citation(s)
+
+docs.microsoft.com  |  "Find Indexes On A Table In SQL Server | My Tec Bits"  |  https://docs.microsoft.com/en-us/sql/t-sql/statements/create-index-transact-sql?view=sql-server-ver15
+
+----------------------------------------------------------
+*/

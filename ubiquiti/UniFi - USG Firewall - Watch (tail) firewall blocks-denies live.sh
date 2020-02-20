89b0435678be7a4d59dd6079d03f1dcbcabac2d4
Matt Cavallo <mcavallo.git@gmail.com>
2020-02-20 04:36:45 -0600

    Watch the USG-3P firewall logs (e.g. see active denials, blocks, etc. as they come in, live)

---
+++ ubiquiti/UniFi - USG Firewall - Watch (tail) firewall blocks-denies live.sh
@@ -0,0 +1,17 @@
+
+
+### SSH into the Unifi USG-3P in question
+
+
+### Watch the USG-3P firewall logs (e.g. see active denials, blocks, etc. as they come in, live)
+
+tail -f /var/log/messages
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.ui.com  |  "USG Firewall Logs? | Ubiquiti Community"  |  https://community.ui.com/questions/USG-Firewall-Logs/24591d62-d209-405f-8abd-fc0c9b9d589c#answer/f5ab6add-6ee1-408c-9e38-4c249ffb1466
+#
+# ------------------------------------------------------------

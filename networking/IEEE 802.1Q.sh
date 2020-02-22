faef2068c8d01724c5f0bc3f9146104d9c986826
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-22 00:54:28 -0600

    VLans

---
+++ networking/IEEE 802.1Q.sh
@@ -0,0 +1,16 @@
+
+
+Refer to citation(s), below
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.spiceworks.com  |  "[SOLVED] Understanding PVID - Networking - Spiceworks"  |  https://community.spiceworks.com/topic/994968-understanding-pvid
+#
+#   en.wikipedia.org  |  "IEEE 802.1Q - Wikipedia"  |  https://en.wikipedia.org/wiki/IEEE_802.1Q
+#
+#   tools.ietf.org  |  "RFC 4675 - RADIUS Attributes for Virtual LAN and Priority Support"  |  https://tools.ietf.org/html/rfc4675
+#
+# ------------------------------------------------------------

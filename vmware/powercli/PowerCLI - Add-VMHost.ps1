7454767b81b75d50fb4c77fc6104662732f5fcd2
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-21 19:13:44 -0600

    Update PowerCLI - Add-VMHost.ps1

--- vmware/powercli/PowerCLI - Add-VMHost.ps1
+++ vmware/powercli/PowerCLI - Add-VMHost.ps1
@@ -2,8 +2,8 @@
 
 If ($True) {
 $vSphere_Server=(Read-Host "Enter FQDN/IP of vSphere Server");  # DNS name (Fully Qualified Domain Name) or IP address of the vCenter Server system which will have the new VM host added to it
-$vSphere_Datastore=(Read-Host "Enter Name of vSphere Datastore");  # Specifies a datacenter or folder where you want to place the host
 $VM_Name=(Read-Host "Enter Name for the new VM");  # Sets the VM Title/Name and Datastore directory name
+$vSphere_Datastore=(Read-Host "Enter Datastore name which should contain this VM (enter only the top-level datastore name/nickname)");  # Specifies a datacenter or folder where you want to place the host
 $vSphere_User = (Read-Host "Enter vSphere Login-Username");
 $vSphere_Pass = ([System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($(Read-Host -AsSecureString "Enter vSphere Login-Password"))));
 $vSphere_ConnectionStream = Connect-VIServer -Server ${vSphere_Server};

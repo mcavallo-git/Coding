0a33f613166f504765712253166a120ccc34c518
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-21 20:44:37 -0600

    Update VMware ESXi - Get Hardware Info.sh

--- vmware/VMware ESXi - Get Hardware Info.sh
+++ vmware/VMware ESXi - Get Hardware Info.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# Save output to a logfile in user-dir
-echo "" > "/tmp/hostsvc-hostsummary.$(hostname).log";
+echo "" > "/tmp/hostsvc-hostsummary.$(hostname).log"; # Save output to a logfile in /tmp
+
 chmod 0600 "/tmp/hostsvc-hostsummary.$(hostname).log";
+
 vim-cmd hostsvc/hostsummary >> "/tmp/hostsvc-hostsummary.$(hostname).log";
+
 lspci -vvv >> "/tmp/hostsvc-hostsummary.$(hostname).log";
 
+esxcli storage nmp device list >> "/tmp/hostsvc-hostsummary.$(hostname).log"; # Get storage array (RAID) info
 
-# SFTP into the box and download file @ "/tmp/hostsvc-hostsummary.$(hostname).log"
+# SFTP into the box and download the logfile
+echo "";
+echo "  Hardware info saved to \"/tmp/hostsvc-hostsummary.$(hostname).log\"";
+echo "";
+echo "  Connect to this host via SFTP to download the file, or move it to a secured/controlled datastore and download via ESXi's vCenter-Agent or Web GUI --> Datastore Browser";
+echo "";
 
 
 # ------------------------------------------------------------

477fe8eeaa34c9722726ceff607f81098d5bc36b
Matt Cavallo <mcavallo.git@gmail.com>
2020-02-24 07:17:48 -0600

    Create Linux - kill process (kill -9, stop process).sh

---
+++ linux/Linux - kill process (kill -9, stop process).sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+exit 1;
+
+# Kill a linux process
+PID="PID_TO_KILL_AS_INTEGER"; kill ${PID}l
+
+
+# Example: Kill all java processes
+for EACH_PID in $(ps -ef | grep 'java' | grep -v 'grep' | awk '{print $2}'); do kill ${EACH_PID}; done;
+

a4e0ff5812f1f08522e93d76cd039f30ba5132c3
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-27 12:44:17 -0600

    Update Linux - kdump (kernel crash dump logging).sh

--- linux/Linux - kdump (kernel crash dump logging).sh
+++ linux/Linux - kdump (kernel crash dump logging).sh
@@ -7,22 +7,12 @@
 # Install the kernel crash dump logging module
 #
 
-if [ $(which yum 2>'/dev/null' | wc -l;) -gt 0 ]; then # Distros: Fedora, Oracle Linux, Red Hat Enterprise Linux, CentOS, etc.
-	### Check if kdump is installed (or not)
-	# rpm -q kexec-tools
-	#
-	### 
-	### Install kdump
+if [ $(which apt 2>'/dev/null' | wc -l;) -gt 0 ]; then # Distros: Debian, Ubuntu, etc.
+	apt-get -y install linux-crashdump;
+elif [ $(which yum 2>'/dev/null' | wc -l;) -gt 0 ]; then # Distros: Fedora, Oracle Linux, Red Hat Enterprise Linux, CentOS, etc.
 	if [ $(which kdump 2>'/dev/null' | wc -l;) -gt 0 ]; then
-		yum install kexec-tools;
+		yum -y install kexec-tools;
 	fi;
-	### Check if kdump's GUI-based configuration module is installed (or not)
-	# rpm -q system-config-kdump
-	#
-	### Install kdump's GUI-based configuration module
-	# if [ $(which kdump 2>'/dev/null' | wc -l;) -gt 0 ]; then
-	# 	yum install system-config-kdump;
-	# fi;
 fi;
 
 
@@ -31,5 +21,7 @@ fi;
 # Citation(s)
 #
 #   access.redhat.com  |  "Chapter 7. Kernel crash dump guide Red Hat Enterprise Linux 7 | Red Hat Customer Portal"  |  https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/kernel_administration_guide/kernel_crash_dump_guide
+#
+#   help.ubuntu.com  |  "Kernel Crash Dump"  |  https://help.ubuntu.com/lts/serverguide/kernel-crash-dump.html
 #
 # ------------------------------------------------------------

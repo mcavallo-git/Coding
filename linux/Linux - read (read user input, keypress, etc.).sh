98e307b10f22c6f38e70eac07cd8ced3fde939c2
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-28 01:41:33 -0600

    Update Linux - read (read user input, keypress, etc.).sh

--- linux/Linux - read (read user input, keypress, etc.).sh
+++ linux/Linux - read (read user input, keypress, etc.).sh
@@ -7,29 +7,36 @@ read -p "Press any key to continue...  " -n 1 -t 60 -r; # Await single keypress
 # ------------------------------------------------------------
 # Await multiple keypresses (general keyboard string-entry)
 
-READ_TIMEOUT=60;
-read -p "Enter a string:  " -t ${READ_TIMEOUT} -r; RETURN_CODE_READ=$?;
-echo "";
-if [ ${RETURN_CODE_READ} -le 128 ] && [ -n "${REPLY}" ]; then
-	echo "Info:  Response received: \"${REPLY}\"";
-elif [ ${RETURN_CODE_READ} -gt 128 ]; then
-	echo "Error:  Response timed out after ${READ_TIMEOUT}s";
-else
-	echo "Info:  Response's string-length is zero (empty/unset)";
+if [ 1 ]; then
+	READ_TIMEOUT=60;
+	read -p "Enter a string:  " -t ${READ_TIMEOUT} -r; RETURN_CODE_READ=$?;
+	echo "";
+	if [ ${RETURN_CODE_READ} -le 128 ] && [ -n "${REPLY}" ]; then
+		echo "Info:  Response received: \"${REPLY}\"";
+	elif [ ${RETURN_CODE_READ} -gt 128 ]; then
+		echo "Error:  Response timed out after ${READ_TIMEOUT}s";
+	else
+		echo "Info:  Response's string-length is zero (empty/unset)";
+	fi;
 fi;
+
 
 # ------------------------------------------------------------
 # Await single keypress & Listen for "y" (or "Y")
 
-READ_TIMEOUT=60;
-read -p "Perform action [ xyz ], now? (y/n)  " -n 1 -t 60 -r; RETURN_CODE_READ=$?;
-echo "";
-if [ ${RETURN_CODE_READ} -gt 128 ]; then
-	echo "Error:  Response timed out after ${READ_TIMEOUT}s";
-elif [ -n "${REPLY}" ] && [[ $REPLY =~ ^[Yy]$ ]]; then
-	echo "Info:  Confirmed - Performing Action [ xyz ] ...";
-else
-	echo "Info:  Denied - Skipping action [ xyz ]";
+
+if [ 1 ]; then
+	READ_TIMEOUT=60;
+	read -p "Perform action [ xyz ], now? (y/n)  " -n 1 -t 60 -r; RETURN_CODE_READ=$?;
+	echo "";
+	if [ ${RETURN_CODE_READ} -gt 128 ]; then
+		echo "Error:  Response timed out after ${READ_TIMEOUT}s";
+	elif [ -n "${REPLY}" ] && [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "Info:  Confirmed - Performing Action [ xyz ] ...";
+	else
+		echo "Info:  Denied - Skipping action [ xyz ]";
+	fi;
 fi;
+
 
 # ------------------------------------------------------------

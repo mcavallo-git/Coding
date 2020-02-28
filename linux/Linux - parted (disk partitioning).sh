b0134bffc4bf91003b625a1dd753d75e0489d4b7
Matt Cavallo <mcavallo.work@gmail.com>
2020-02-28 00:39:54 -0600

    Update Linux - parted (disk partitioning).sh

--- linux/Linux - parted (disk partitioning).sh
+++ linux/Linux - parted (disk partitioning).sh
@@ -8,7 +8,7 @@ EACH_PROTOCOL=$(echo "${EACH_LINE}" | cut -d':' -f3;);
 EACH_SECTORSIZE_LOGICAL=$(echo "${EACH_LINE}" | cut -d':' -f4;);
 EACH_SECTORSIZE_PHYSICAL=$(echo "${EACH_LINE}" | cut -d':' -f5;);
 EACH_PARTITION_TABLE=$(echo "${EACH_LINE}" | cut -d':' -f6;);
-EACH_DISK_NAME=$(echo "${EACH_LINE}" | cut -d':' -f7;);
+EACH_DISK_DESCRIPTION=$(echo "${EACH_LINE}" | cut -d':' -f7;);
 EACH_UNKOWWN=$(echo "${EACH_LINE}" | cut -d':' -f8;);
 echo "------------------------------------------------------------";
 echo "EACH_DISK_NAME = [ ${EACH_DISK_NAME} ]";
@@ -17,7 +17,7 @@ echo "EACH_PROTOCOL = [ ${EACH_FIELD_3} ]";
 echo "EACH_SECTORSIZE_LOGICAL = [ ${EACH_SECTORSIZE_LOGICAL} ]";
 echo "EACH_SECTORSIZE_PHYSICAL = [ ${EACH_SECTORSIZE_PHYSICAL} ]";
 echo "EACH_PARTITION_TABLE = [ ${EACH_PARTITION_TABLE} ]";
-echo "EACH_DISK_NAME = [ ${EACH_FIELD_7} ]";
+echo "EACH_DISK_DESCRIPTION = [ ${EACH_FIELD_7} ]";
 echo "EACH_UNKOWWN = [ ${EACH_FIELD_8} ]";
 done; IFS="${ROLLBACK_IFS}";
 

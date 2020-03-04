ee912ebe6de2a96eaa55daab090a171cb82d939a
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-04 05:13:29 -0600

    Create FreeNAS - S3 Storage setup.sh

---
+++ linux/FreeNAS - S3 Storage setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.ixsystems.com  |  "FreeNAS unable to find domain controllers | iXsystems Community"  |  https://www.ixsystems.com/community/threads/freenas-unable-to-find-domain-controllers.30543/#post-209908
+#
+#   www.ixsystems.com  |  "Run S3 Object Storage on FreeNAS and TrueNAS - iXsystems, Inc. - Enterprise Storage & Servers"  |  https://www.ixsystems.com/blog/s3-on-truenas-freenas/
+#
+# ------------------------------------------------------------

58bcef2d98f282c3bd7faf6f3598c52bf7a4ce5d
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-03 21:48:18 -0600

    Create Microsoft SSMS - Backup (and Restore) target Database.ps1

---
+++ sql/Microsoft SSMS - Backup (and Restore) target Database.ps1
@@ -0,0 +1,14 @@
+
+
+#
+# Refer to Microsoft's documentation for T-SQL Database Backup & Recovers  @  https://docs.microsoft.com/en-us/sql/relational-databases/backup-restore/quickstart-backup-restore-database?view=sql-server-ver15&viewFallbackFrom=sql-server-2014
+ #  
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Quickstart: Back up & restore database - SQL Server | Microsoft Docs"  |  https://docs.microsoft.com/en-us/sql/relational-databases/backup-restore/quickstart-backup-restore-database?view=sql-server-ver15&viewFallbackFrom=sql-server-2014
+#
+# ------------------------------------------------------------

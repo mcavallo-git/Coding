9be378ec2f4f1c79eb5ef7bf3d076ec4cebc48ce
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-05 23:15:28 -0600

    Update Linux - parted (disk partitioning).sh

--- linux/Linux - parted (disk partitioning).sh
+++ linux/Linux - parted (disk partitioning).sh
@@ -1,37 +1,39 @@
 #!/bin/bash
 # ------------------------------------------------------------
-# Show disk info
-
-
-lsblk -p -n -o KNAME,TYPE,MOUNTPOINT;
-
-
-fdisk -l | grep sectors | grep '^Disk';
-
-
-# Get filesystem-type(s) for the current system
-df -h --output="source,fstype";
-lsblk -fp;
-
-
-# List partition beginning/end points
-for hdd in /dev/sd? ; do parted -m $hdd unit B print; done;
-
-
-# ------------------------------------------------------------
-# Create a new partition
+# Parted - Create a new partition
 #
-###  mkpart part-type [fs-type] start end
+
+# Determine partition's disk-size (based on start- & end-bytes)
+for EACH_DEVICE in /dev/sd? ; do parted -m "${EACH_DEVICE}" unit B print; done;
+#  |--> Set start-byte to 1 byte past the LAST partition's end-byte (in B)
+#  |--> Set end-byte to the total disk size (in B)
 #
-###  mklabel label-type
+### EXAMPLE --> for EACH_DEVICE in /dev/sd? ; do parted -m "${EACH_DEVICE}" unit B print; done;
+#
+# BYT;
+# /dev/sda:429496729600B:scsi:512:512:msdos:VMware Virtual disk:;
+# 1:1048576B:1074790399B:1073741824B:xfs::boot;
+# 2:1074790400B:107374182399B:106299392000B:::lvm;
+#
+# ^-- Device name is '/dev/sda' (based on the header line's FIRST value of "/dev/sda)"
+#      |--> DEVICE="/dev/sda";
+#
+# ^-- Total disk size is 400GB disk (based on the '/dev/sda ...' line's SECOND value, "429496729600B")
+#      |--> END_BYTE="429496729600B";
+#
+# ^-- The second (and last) currently-existent partition ends at byte 107374182399, so start new partition just after on 107374182399+1 (based on the '1:...' line's THIRD value, "1074790399B")
+#      |--> START_BYTE="107374182400B";
+#
+# ^-- The filesystem type for the currently boot partition on this device is "xfs" (based on the '1:...' line's FIFTH value, "xfs")
+#      |--> FS_TYPE="xfs";
 #
 
 DEVICE="/dev/sda";
-PART_TYPE="logical";
+START_BYTE="107374182400B";
+END_BYTE="429496729600B";
 FS_TYPE="xfs";
-START_BYTE="";
-END_BYTE=""
-parted "${DEVICE}" mkpart "${PART_TYPE}" "${FS_TYPE}" "1GiB" "8GiB";
+PART_TYPE="logical";
+parted "${DEVICE}" mkpart "${PART_TYPE}" "${FS_TYPE}" "${START_BYTE}" "${END_BYTE}";
 
 # parted "/dev/sda" mklabel "gpt" "mkpart" "${PART_TYPE}" "${FS_TYPE}" "1GiB" "8GiB";
 
@@ -46,6 +48,21 @@ parted "${DEVICE}" mkpart "${PART_TYPE}" "${FS_TYPE}" "1GiB" "8GiB";
 # Partition un-partitioned disk(s)
 
 ### UNDER CONSTRUCTION AS-OF 20200305-225420  -->  wget "https://raw.githubusercontent.com/mcavallo-git/cloud-infrastructure/master/usr/local/sbin/partition_unpartitioned_disks" -O "/usr/local/sbin/partition_unpartitioned_disks" -q && chmod 0755 "/usr/local/sbin/partition_unpartitioned_disks" && /usr/local/sbin/partition_unpartitioned_disks;
+
+
+# ------------------------------------------------------------
+# Additional disk-info packages/commands/tools
+
+# Determine FS (filesystem) type based on existing filesystem(s)
+df -h --output="source,fstype" | grep -v '^tmp' | grep -v '^dev';
+
+lsblk -p -n -o KNAME,TYPE,MOUNTPOINT;
+
+fdisk -l | grep sectors | grep '^Disk';
+
+# Get filesystem-type(s) for the current system
+df -h --output="source,fstype";
+lsblk -fp;
 
 
 # ------------------------------------------------------------

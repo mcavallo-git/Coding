f278dc33464b3de85a25f0e8fe2dac34cdc6e52c
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-05 20:30:16 -0600

    Update Microsoft SQL - Get all Column names in a given Database Table (or Tables).sql

--- sql/Microsoft SQL - Get all Column names in a given Database Table (or Tables).sql
+++ sql/Microsoft SQL - Get all Column names in a given Database Table (or Tables).sql
@@ -8,11 +8,16 @@ select schema_name(tab.schema_id) as schema_name,
     t.name as data_type,    
     col.max_length,
     col.precision
+
 from sys.tables as tab
     inner join sys.columns as col
         on tab.object_id = col.object_id
     left join sys.types as t
     on col.user_type_id = t.user_type_id
+
+WHERE t.NAME = 'TAGBLE_NAME_EXACT_MATCH' 
+	OR t.NAME LIKE '%TABLE_NAME_PARTIAL_MATCH%'
+
 order by schema_name,
     table_name, 
     column_id;

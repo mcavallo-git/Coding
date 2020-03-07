d425a3e96128a4dab37a3ac0e6c4e52d0a346d4c
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-06 18:52:05 -0600

    Create VMware ESXi - Dell OMSA (OpenManage Server Administrator) vSphere VIB Installation.sh

---
+++ vmware/VMware ESXi - Dell OMSA (OpenManage Server Administrator) vSphere VIB Installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# Step 1 --> Install the Dell OMSA backend package onto target ESXi host, then RESTART the ESXi host (required)
+#
+#   Name:  Dell EMC OpenManage Server Administrator vSphere Installation Bundle (VIB) for ESXi 6.5 U3, v9.4.0
+#   URL:   https://dl.dell.com/FOLDER05993176M/1/OM-SrvAdmin-Dell-Web-9.4.0-3776.VIB-ESX65i_A00.zip
+#
+
+
+#
+# Step 2 --> Install & run the Dell OMSA software as a service in Windows, then connect to it via its browser-based GUI
+#
+#   Name:  Dell EMC OpenManage Server Administrator Managed Node for Windows, v9.4.0
+#   URL:   https://dl.dell.com/FOLDER06019899M/1/OM-SrvAdmin-Dell-Web-WINX64-9.4.0-3787_A00.exe
+#
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.dell.com  |  "Support for PowerEdge R720 | Drivers & Downloads | Dell US"  |  https://www.dell.com/support/home/us/en/19/product-support/product/poweredge-r720/drivers
+#
+# ------------------------------------------------------------

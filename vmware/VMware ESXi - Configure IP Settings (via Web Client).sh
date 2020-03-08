a7d3845ca4b4482603ca187946b2ea35a81b5cef
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-08 06:08:23 -0500

    Create VMware ESXi - Configure IP Settings (via Web Client).sh

---
+++ vmware/VMware ESXi - Configure IP Settings (via Web Client).sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# See Citation(s), below
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.vmware.com  |  "Configure IP Settings from the vSphere Web Client"  |  https://docs.vmware.com/en/VMware-vSphere/6.7/com.vmware.esxi.install.doc/GUID-8C70FB9B-D2AB-424E-BBBD-B77AC33C6EF8.html
+#
+#   docs.vmware.com  |  "Configure IP Settings from the Direct Console"  |  https://docs.vmware.com/en/VMware-vSphere/6.7/com.vmware.esxi.install.doc/GUID-EED14995-3515-40AD-B7EE-09A2046D841B.html
+#
+# ------------------------------------------------------------

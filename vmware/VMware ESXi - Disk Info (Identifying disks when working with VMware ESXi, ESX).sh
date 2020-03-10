39989a716aee1e4d4be532020ec541ad90c432ad
Matt Cavallo <mcavallo.git@gmail.com>
2020-03-10 01:42:36 -0500

    Create VMware ESXi - Disk Info (Identifying disks when working with VMware ESXi, ESX).sh

---
+++ vmware/VMware ESXi - Disk Info (Identifying disks when working with VMware ESXi, ESX).sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### Refer to article(s) in Citation(s), below
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   kb.vmware.com  |  "Identifying disks when working with VMware ESXi/ESX"  |  https://kb.vmware.com/s/article/1014953
+#
+# ------------------------------------------------------------

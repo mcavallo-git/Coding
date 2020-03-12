39634e649135e856ff93e2d76a552d3916cdae50
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-11 23:51:14 -0500

    Create VMware ESXi - Kill VMs which are unresponsive.sh

---
+++ vmware/VMware ESXi - Kill VMs which are unresponsive.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+exit 1;
+
+
+### List running processes (mainly VMs) using "esxtop"
+esxtop
+
+
+### Press "m" to sort by memory
+m
+
+
+### Press "f" to add a filter
+f
+
+
+### Press "c" then "enter" to add the "LWID = Leader World Id (World Group Id)" column to esxtop (think of this as the PID in traditional Linux distros)
+c  (enter)
+
+
+### Press "k" topen the dialogue at the top of esxtop asking which LWID/WID (essentially which PID) to kill 
+World to kill (WID):  12345
+# ^-- example: kill process with LWID of 12345
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   kb.vmware.com  |  "Unable to Power off a Virtual Machine in an ESXi host (1014165)"  |  https://kb.vmware.com/s/article/1014165
+#
+# ------------------------------------------------------------

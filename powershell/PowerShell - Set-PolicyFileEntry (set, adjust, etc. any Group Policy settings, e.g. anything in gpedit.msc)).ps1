734b61d13847a5763f96ab8196783da7e3d149ac
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-19 21:09:34 -0500

    Update PowerShell - Set-PolicyFileEntry (set, adjust, etc. any Group Policy settings, e.g. anything in gpedit.msc)).ps1

--- powershell/PowerShell - Set-PolicyFileEntry (set, adjust, etc. any Group Policy settings, e.g. anything in gpedit.msc)).ps1
+++ powershell/PowerShell - Set-PolicyFileEntry (set, adjust, etc. any Group Policy settings, e.g. anything in gpedit.msc)).ps1
@@ -41,13 +41,12 @@ Set-PolicyFileEntry `
 
 $HKLM_Path="SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services"; `
 $Name="MaxCompressionLevel"; `
-$Value="MaxCompressionLevel"; `
+[UInt32]$Value = 0x00000002; `
 $Type="DWord"; `
 Write-Host ""; `
 Write-Host "The following property sets the value to for Group Policy (gpedit.msc) titled 'Configure compression for RemoteFX data' to:  [ 0 - 'Do not use an RDP compression algorithm' ],  [ 1 - 'Optimized to use less memory' ],  [ 2 - 'Balances memory and network bandwidth' ],  or  [ 3 - 'Optimized to use less network bandwidth' ]"; `
 Write-Host -NoNewLine "`n"; `
-Set-PolicyFileEntry `
--Path ("${Env:SystemRoot}\System32\GroupPolicy\Machine\Registry.pol") -Key ("${HKLM_Path}") -ValueName ("${Name}") -Data (${Value}) -Type ("${Type}");
+Set-PolicyFileEntry -Path ("${Env:SystemRoot}\System32\GroupPolicy\Machine\Registry.pol") -Key ("${HKLM_Path}") -ValueName ("${Name}") -Data (${Value}) -Type ("${Type}");
 
 
 # ------------------------------------------------------------

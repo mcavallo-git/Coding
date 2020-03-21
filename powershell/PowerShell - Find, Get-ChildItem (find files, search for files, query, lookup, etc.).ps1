d418b1191eb307b04d90adeca50e1bd846bf483f
Matt Cavallo <mcavallo.git@gmail.com>
2020-03-20 21:11:28 -0500

    Update PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1

--- powershell/PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1
+++ powershell/PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1
@@ -19,6 +19,8 @@ Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue
 
 Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { ($_.Name -Like "vs_*.exe") } | ForEach-Object { $_.FullName; }
 
+Get-ChildItem -Path ("C:\Program Files (x86)") -Depth 1 -Directory -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Name -Like "WiX Toolset v*" }
+
 <# Search at-most [ 9 ] subdirectory-levels deep within parent directory [ C:\ ] (e.g. C:\ is at depth=0) for a file whose basename is equal to [ MSBuild.exe ] #>
 Get-ChildItem -Path ("C:\") -File -Recurse -Depth (9) -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Name -Eq "MSBuild.exe" };
 

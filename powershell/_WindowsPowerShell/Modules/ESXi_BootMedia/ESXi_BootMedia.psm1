7f9abd3c67a3c3742470c97067242c4eb0b9e8b4
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-23 01:16:55 -0500

    Update ESXi_BootMedia.psm1

--- powershell/_WindowsPowerShell/Modules/ESXi_BootMedia/ESXi_BootMedia.psm1
+++ powershell/_WindowsPowerShell/Modules/ESXi_BootMedia/ESXi_BootMedia.psm1
@@ -261,11 +261,11 @@ Function ESXi_BootMedia() {
 
 					$VibNames_Valid = ($ValidExtraVibs | Sort-Object -Property Name -Unique).Name;
 					$VibNames_Valid > "${Home}\Desktop\VibNames_Valid.log";
-					$ValidExtraVibs | Sort-Object -Property Name,@{Expression={$_.Version}; Ascending=$False} | Format-List > "${LogFilesDir}\Verbose-ValidExtraVibs.log";
+					$ValidExtraVibs | Sort-Object -Property Name,@{Expression={$_.Version}; Ascending=$False} | Format-List > "${Home}\Desktop\Verbose-ValidExtraVibs.log";
 
 					$VibNames_Ignored = ($IgnoredExtraVibs | Sort-Object -Property Name -Unique).Name;
 					$VibNames_Ignored > "${Home}\Desktop\VibNames_Ignored.log";
-					$IgnoredExtraVibs | Sort-Object -Property Name,@{Expression={$_.Version}; Ascending=$False} | Format-List > "${LogFilesDir}\Verbose-IgnoredExtraVibs.log";
+					$IgnoredExtraVibs | Sort-Object -Property Name,@{Expression={$_.Version}; Ascending=$False} | Format-List > "${Home}\Desktop\Verbose-IgnoredExtraVibs.log";
 
 				}
 

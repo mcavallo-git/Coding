9f6b2df54fb367cd99ea37e629ceb41bd34211fd
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-29 20:46:46 -0500

    Update PowerShell - Run program as Admin (or in compatibility mode) by default.ps1

--- powershell/PowerShell - Run program as Admin (or in compatibility mode) by default.ps1
+++ powershell/PowerShell - Run program as Admin (or in compatibility mode) by default.ps1
@@ -8,7 +8,7 @@ $ExeList_AlwaysRunAsAdmin += "devenv.exe";
 $ExeList_AlwaysRunAsAdmin += "msbuild.exe";
 
 Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $ExeList_AlwaysRunAsAdmin.Contains("$($_.Name)") } | ForEach-Object {
-Set-ItemProperty -Path ("${RegistryKey_RunAsAdmin}") -Name ("$($_.FullName)") -PropertyType ("String") -Value ("RUNASADMIN") -Force -ErrorAction "SilentlyContinue" | Out-Null;
+Set-ItemProperty -Path ("${RegistryKey_RunAsAdmin}") -Name ("$($_.FullName)") -Type ("String") -Value ("RUNASADMIN") -Force -ErrorAction "SilentlyContinue" | Out-Null;
 }
 
 

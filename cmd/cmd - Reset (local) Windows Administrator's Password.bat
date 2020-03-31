5d6d4629be9c2e3798773ee00ea2c2bdc6ab43af
Matt Cavallo <mcavallo.work@gmail.com>
2020-03-30 19:49:18 -0500

    Create cmd - Reset (local) Windows Administrator's Password.bat

---
+++ cmd/cmd - Reset (local) Windows Administrator's Password.bat
@@ -0,0 +1,56 @@
+REM ------------------------------------------------------------
+REM
+REM cmd - Reset (local) Windows Administrator's Password
+REM
+REM ------------------------------------------------------------
+
+
+REM Boot to bootable Windows PE media (create Windows Installation Media w/ a thumb drive if you don't have any)
+
+
+REM Backup Utilman.exe
+COPY /Y C:\Windows\System32\Utilman.exe C:\Windows\System32\Utilman.bak.exe
+
+
+REM Overwrite Utilman.exe with cmd.exe
+COPY /Y C:\Windows\System32\cmd.exe C:\Windows\System32\Utilman.exe
+
+
+REM Restart the computer
+SHUTDOWN /R /T 0
+
+
+REM At the login, click the Ease of Access button to open a command prompt - THIS IS AN ADMIN COMAND PROMPT
+
+
+REM Reset desired user's password
+NET USER Administrator /active:yes
+NET USER Administrator "example_password"
+
+
+REM If username is not known, inspect all users via...
+NET USER
+
+
+REM If needed, convert Microsoft Account to Local Account (Win8/10 only)
+REM (Manually done in windows)
+
+
+REM Log in using the freshly reset password
+
+
+REM Restore Utilman.exe from Utilman.bak.exe
+COPY /Y C:\Windows\System32\Utilman.bak.exe C:\Windows\System32\Utilman.exe
+
+
+REM  Restart your computer; create a password reset disk.
+SHUTDOWN /R /T 0
+
+
+REM ------------------------------------------------------------
+REM 
+REM  Citation(s)
+REM 
+REM   www.infopackets.com  |  "How to Reset Admin Password for Windows Vista, 7, 8 and 10"  |  https://www.infopackets.com/news/9483/how-reset-any-password-windows-vista-7-8-10
+REM 
+REM ------------------------------------------------------------

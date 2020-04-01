b0152656061bf841b8102859815028a075bfa7f7
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-01 15:55:23 -0500

    Update VMware ESXi - Dell OMSA (OpenManage Server Administrator) vSphere VIB Installation.sh

--- vmware/VMware ESXi - Dell OMSA (OpenManage Server Administrator) vSphere VIB Installation.sh
+++ vmware/VMware ESXi - Dell OMSA (OpenManage Server Administrator) vSphere VIB Installation.sh
@@ -41,6 +41,13 @@
 Get-Service | Where-Object { ($_.Name -Eq "Server Administrator") -Or ( $_.DisplayName -Eq "DSM SA Connection Service" ) } | Format-List;
 
 
+#
+# Add a firewall rule for the Dell EMC OMSA Service
+#  |--> Run the following command in via PowerShell to show the status of the Dell EMC OMSA service
+#
+New-NetFirewallRule -DisplayName "Dell OMSA (TCP 1311)" -Description "Dell EMC OpenManage Server Administrator (OMSA) - Allow Inbound TCP Traffic on Port(s) 1311" -Direction Inbound -Action Allow -EdgeTraversalPolicy Allow -Protocol TCP -LocalPort 1311; <# Create a firewall rule to allow 1311 (Dell EMC OMSA) through #>
+netsh advfirewall firewall add rule name="My Application" dir=in action=allow program="C:\MyApp\MyApp.exe" enable=yes remoteip=157.60.0.1,172.16.0.0/16,LocalSubnet profile=domain
+
 # ------------------------------------------------------------
 #
 # Citation(s)
@@ -48,5 +55,7 @@ Get-Service | Where-Object { ($_.Name -Eq "Server Administrator") -Or ( $_.Displ
 #   www.dell.com  |  "Re: replacing the SSL certificate in Dell OMSA 7.2 - Dell Community"  |  https://www.dell.com/community/Systems-Management-General/replacing-the-SSL-certificate-in-Dell-OMSA-7-2/m-p/4031998/highlight/true#M16590
 #
 #   www.dell.com  |  "Support for PowerEdge R720 | Drivers & Downloads | Dell US"  |  https://www.dell.com/support/home/us/en/19/product-support/product/poweredge-r720/drivers
+#
+#   docs.microsoft.com  |  "New-NetFirewallRule - Creates a new inbound or outbound firewall rule and adds the rule to the target computer"  |  https://docs.microsoft.com/en-us/powershell/module/netsecurity/new-netfirewallrule
 #
 # ------------------------------------------------------------

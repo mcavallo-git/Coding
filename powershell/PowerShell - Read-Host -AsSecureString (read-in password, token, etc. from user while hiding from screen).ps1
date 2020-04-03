b71f76a903191dd2cb9a52d41eead78ba2d4d53b
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-03 13:46:06 -0500

    Update PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1

--- powershell/PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1
+++ powershell/PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1
@@ -7,6 +7,8 @@ $PasswordPlaintext = ([System.Runtime.InteropServices.Marshal]::PtrToStringUni([
 #
 # Citation(s)
 #
+#   stackoverflow.com  |  "c# - Convert String to SecureString - Stack Overflow"  |  https://stackoverflow.com/a/43084626
+#
 #   www.scriptinglibrary.com  |  "Passwords and SecureString, How To Decode It with Powershell | Scripting Library"  |  https://www.scriptinglibrary.com/languages/powershell/securestring-how-to-decode-it-with-powershell/
 #
 # ------------------------------------------------------------

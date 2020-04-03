eec6be5f48aa62e25999a79e78f906e58a746603
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-03 17:40:42 -0500

    Create PowerShell - Set-AuthenicodeSignature (perform code-signing with local machine attached certificates).ps1

---
+++ powershell/PowerShell - Set-AuthenicodeSignature (perform code-signing with local machine attached certificates).ps1
@@ -0,0 +1,54 @@
+
+$ExitCode = 1;
+
+<# Determine Working Directory #>
+$WorkingDir = $Null;
+If (("%system.teamcity.build.workingDir%") -NE (("%")+(@("system","teamcity","build","workingDir") -join ".")+("%"))) {
+	<# TeamCity build-environment #>
+	$WorkingDir = "%system.teamcity.build.workingDir%";
+} ElseIf (Test-Path -Path ("Env:WORKSPACE") -PathType ("Leaf")) {
+	<# Jenkins build-environment #>
+	$WorkingDir = "${Env:WORKSPACE}";
+}
+If ($WorkingDir -Eq $Null) {
+	Write-Output "`nError - Unable to detetermine working directory. Exiting...`n";
+
+} Else {
+
+	<# Get the first non-expired code signing certificate found in the windows certificate store which has been imported onto the current Local Machine #>
+	$Cert_CodeSigning = ((Get-ChildItem "Cert:\LocalMachine\My" -CodeSigningCert | Where-Object { ($_.NotAfter) -GT (Get-Date) })[0]);
+	
+	If ($Cert_CodeSigning -Eq $Null) {
+		Write-Output "`nError - No code signing certificate(s) found in the Local Machine certificate store. Please retry after installing a code-signing (.pfx) certificate onto the Local Machine certificate store`n";
+
+	} Else {
+
+		Write-Output "`nInfo - Using code signing certificate (pulled from Local Machine certificate store) with specs:`n";
+		$Cert_CodeSigning | Format-List;
+
+		<# Use the code signing certificate to sign all unsigned { .dll, .exe, .msi, & .pdb } files found under the directory specified by "${Env:WORKSPACE}" #>
+		Get-Item "${WorkingDir}\**\*" `
+		| Where-Object { ($_.FullName -Like "*.dll") -Or ($_.FullName -Like "*.exe") -Or ($_.FullName -Like "*.msi") -Or ($_.FullName -Like "*.pdb") } `
+		| Where-Object { ((Get-AuthenticodeSignature -FilePath ("$($_.FullName)")).Status -NE "Valid") } `
+		| ForEach-Object { `
+			Write-Output "Into - Signing file `"$_.FullName`"";
+			Set-AuthenticodeSignature -FilePath ("$($_.FullName)") -Certificate (${Cert_CodeSigning}) -IncludeChain All -TimestampServer ("http://tsa.starfieldtech.com") | Out-Null;
+		};
+
+		$ExitCode = 0;
+
+	}
+}
+
+Exit $ExitCode;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Set-AuthenticodeSignature"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/set-authenticodesignature?view=powershell-5.1
+#
+#   docs.microsoft.com  |  "Test-Path - Determines whether all elements of a path exist"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-path?view=powershell-5.1
+#
+# ------------------------------------------------------------

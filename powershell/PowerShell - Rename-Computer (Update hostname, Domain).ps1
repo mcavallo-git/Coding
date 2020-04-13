800c95f5b1187bc1278851f8c28fa613e7fb1c97
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-13 15:57:38 -0500

    Update PowerShell - Rename-Computer (Update hostname, Domain).ps1

--- powershell/PowerShell - Rename-Computer (Update hostname, Domain).ps1
+++ powershell/PowerShell - Rename-Computer (Update hostname, Domain).ps1
@@ -4,39 +4,41 @@
 #
 # ------------------------------------------------------------
 
-$ValidHostname = $False;
-While ($ValidHostname -Eq $False) {
-	$New_Hostname = Read-Host -Prompt "Enter Hostname for this device:  ";
-	$New_Domain = Read-Host -Prompt "Enter Domain (Active Directory to join):  ";
-	$AD_Credential_Username = Read-Host -Prompt "Enter username (without domain) for user on the `"${New_Domain}`" domain:  ";
+If ($True) {
+	$ValidHostname = $False;
+	While ($ValidHostname -Eq $False) {
+		$New_Hostname = Read-Host -Prompt "Enter Hostname for this device:  ";
+		$New_Domain = Read-Host -Prompt "Enter Domain (Active Directory to join):  ";
+		$AD_Credential_Username = Read-Host -Prompt "Enter username (without domain) for user on the `"${New_Domain}`" domain:  ";
 
-	$Regex_NetBiosHostname = "^(([A-Za-z0-9]|([A-Za-z0-9][A-Za-z0-9\-]))*[A-Za-z0-9])$";
-	$Needle = [Regex]::Match($New_Hostname, $Regex_NetBiosHostname);
-	If ($Needle.Success -Eq $False) {
-		Write-Host " Hostname `"${New_Hostname}`" contains 1 or more invalid characters - please use only alphanumeric (A-Z, a-z, 0-9) and, if-desired, dashes `"-`" which must be preceeded and followed immediately by an alphanumeric characeter";
-	} ElseIf ($Needle.Success -Eq $False) {
-		Write-Host " Hostname `"${New_Hostname}`" contains 1 or more invalid characters - please use only alphanumeric (A-Z, a-z, 0-9) and, if-desired, dashes `"-`" which must be preceeded and followed immediately by an alphanumeric characeter";
-	} Else {
-		Try {
-			Rename-Computer -NewName "${New_Hostname}" -DomainCredential "${New_Domain}\${AD_Credential_Username}" -Force;
-			Write-Host "";
-			Write-Host "Device renamed to hostname `"${New_Hostname}`" on domain `"${New_Domain}`"";
-			$ValidHostname = $True;
-		} Catch {
-			Write-Host "";
-			Write-Host -NoNewLine "An error occurred: ";
-			Write-Host $_ -BackgroundColor "Black" -ForegroundColor "Yellow";
+		$Regex_NetBiosHostname = "^(([A-Za-z0-9]|([A-Za-z0-9][A-Za-z0-9\-]))*[A-Za-z0-9])$";
+		$Needle = [Regex]::Match($New_Hostname, $Regex_NetBiosHostname);
+		If ($Needle.Success -Eq $False) {
+			Write-Host " Hostname `"${New_Hostname}`" contains 1 or more invalid characters - please use only alphanumeric (A-Z, a-z, 0-9) and, if-desired, dashes `"-`" which must be preceeded and followed immediately by an alphanumeric characeter";
+		} ElseIf ($Needle.Success -Eq $False) {
+			Write-Host " Hostname `"${New_Hostname}`" contains 1 or more invalid characters - please use only alphanumeric (A-Z, a-z, 0-9) and, if-desired, dashes `"-`" which must be preceeded and followed immediately by an alphanumeric characeter";
+		} Else {
+			Try {
+				Rename-Computer -NewName "${New_Hostname}" -DomainCredential "${New_Domain}\${AD_Credential_Username}" -Force;
+				Write-Host "";
+				Write-Host "Device renamed to hostname `"${New_Hostname}`" on domain `"${New_Domain}`"";
+				$ValidHostname = $True;
+			} Catch {
+				Write-Host "";
+				Write-Host -NoNewLine "An error occurred: ";
+				Write-Host $_ -BackgroundColor "Black" -ForegroundColor "Yellow";
+			}
 		}
 	}
-}
-If ($ValidHostname -Eq $True) {
-	# Reboot the machine (only after user presses 'y')
-	Write-Host -NoNewLine "`n`n  Restart required - Press 'y' to confirm and reboot this machine, now...`n`n" -BackgroundColor "Black" -ForegroundColor "Yellow";
-	$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-	While ($KeyPress.Character -NE "y") {
+	If ($ValidHostname -Eq $True) {
+		# Reboot the machine (only after user presses 'y')
+		Write-Host -NoNewLine "`n`n  Restart required - Press 'y' to confirm and reboot this machine, now...`n`n" -BackgroundColor "Black" -ForegroundColor "Yellow";
 		$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+		While ($KeyPress.Character -NE "y") {
+			$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+		}
+		Start-Process -Filepath ("shutdown") -ArgumentList (@("/t 0","/r")) -NoNewWindow -Wait -PassThru;
 	}
-	Start-Process -Filepath ("shutdown") -ArgumentList (@("/t 0","/r")) -NoNewWindow -Wait -PassThru;
 }
 
 

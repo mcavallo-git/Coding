13f72268c4b294bc8979b9f09e6e7cdd3e7c6b04
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-13 16:14:26 -0500

    Update PowerShell - Rename-Computer (Update hostname, Domain).ps1

--- powershell/PowerShell - Rename-Computer (Update hostname, Domain).ps1
+++ powershell/PowerShell - Rename-Computer (Update hostname, Domain).ps1
@@ -7,9 +7,9 @@
 If ($True) {
 	$ValidHostname = $False;
 	While ($ValidHostname -Eq $False) {
-		$New_Hostname = Read-Host -Prompt "Enter Hostname for this device (characters allowed: alphanumeric & dashes, max length: 15):  ";
-		$New_Domain = Read-Host -Prompt "Enter Domain (Active Directory to join):  ";
-		$AD_Credential_Username = Read-Host -Prompt "Enter username (without domain) for user on the `"${New_Domain}`" domain:  ";
+		$New_Hostname = Read-Host -Prompt "Enter Hostname for this device (characters allowed: alphanumeric & dashes, max length: 15)";
+		$New_Domain = Read-Host -Prompt "Enter Domain (Active Directory to join)";
+		$AD_Credential_Username = Read-Host -Prompt "Enter username (without domain) for user on the `"${New_Domain}`" domain";
 
 		$Regex_NetBiosHostname = "^(([A-Za-z0-9]|([A-Za-z0-9][A-Za-z0-9\-]))*[A-Za-z0-9])$";
 		$Needle = [Regex]::Match($New_Hostname, $Regex_NetBiosHostname);
@@ -20,8 +20,10 @@ If ($True) {
 		} Else {
 			Try {
 				Rename-Computer -NewName "${New_Hostname}" -DomainCredential "${New_Domain}\${AD_Credential_Username}" -Force;
+				Add-Computer -DomainMame "${New_Domain}" -DomainCredential "${New_Domain}\${AD_Credential_Username}" -Restart –Force
 				Write-Host "";
-				Write-Host "Device renamed to hostname `"${New_Hostname}`" on domain `"${New_Domain}`"";
+				Write-Host "Device renamed to hostname `"${New_Hostname}`"";
+				Write-Host "Device joined on domain `"${New_Domain}`"";
 				$ValidHostname = $True;
 			} Catch {
 				Write-Host "";
@@ -42,33 +44,18 @@ If ($True) {
 }
 
 
-# ### Check Hostname (via regex) before attempting to rename device
-# $Regex_NetBiosHostname = "^(([A-Za-z0-9]|([A-Za-z0-9][A-Za-z0-9\-]))*[A-Za-z0-9])$";
-# $Needle = [Regex]::Match($New_Hostname, $Regex_NetBiosHostname);
-# If ($Needle.Success -Eq $False) {
-# 	Write-Host " Hostname `"${New_Hostname}`" contains 1 or more invalid characters - please use only alphanumeric (A-Z, a-z, 0-9) and, if-desired, dashes `"-`" which must be preceeded and followed immediately by an alphanumeric characeter";
-# } ElseIf ($Needle.Success -Eq $False) {
-# 	Write-Host " Hostname `"${New_Hostname}`" contains 1 or more invalid characters - please use only alphanumeric (A-Z, a-z, 0-9) and, if-desired, dashes `"-`" which must be preceeded and followed immediately by an alphanumeric characeter";
-# } Else {
-# 	Try {
-# 		Rename-Computer -ComputerName "${Env:COMPUTERNAME}" -NewName "${New_Hostname}" -DomainCredential "${New_Domain}\${AD_Credential_Username}" -Force;
-# 		Write-Host "Device renamed to hostname `"${New_Hostname}`" on domain `"${New_Domain}`"";
-# 	} Catch {
-# 		Write-Host -NoNewLine "An error occurred: ";
-# 		Write-Host $_ -BackgroundColor "Black" -ForegroundColor "Yellow";
-# 	}
-# }
-
-
-
 # ------------------------------------------------------------
 #
 # Citation(s)
 #
+#   docs.microsoft.com  |  "Add-Computer - Add the local computer to a domain or workgroup."  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/add-computer?view=powershell-5.1
+#
 #   docs.microsoft.com  |  "Rename-Computer - Renames a computer"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-computer?view=powershell-5.1
 #
-#		regextester.com | "Match a valid hostname" | https://www.regextester.com/23
-#
 #		support.microsoft.com | "Naming conventions in Active Directory for computers, domains, sites, and OUs" | https://support.microsoft.com/en-us/help/909264/naming-conventions-in-active-directory-for-computers-domains-sites-and
+#
+#		www.petri.com | "Add Computers to a Domain with PowerShell" | https://www.petri.com/add-computer-to-domain-powershell
+#
+#		www.regextester.com | "Match a valid hostname" | https://www.regextester.com/23
 #
 # ------------------------------------------------------------

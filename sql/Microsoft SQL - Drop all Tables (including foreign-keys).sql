42bbd570924f146aa708090597dcc36cd1add988
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-14 18:59:46 -0500

    Create Microsoft SQL - Drop all Tables (including foreign-keys).sql

---
+++ sql/Microsoft SQL - Drop all Tables (including foreign-keys).sql
@@ -0,0 +1,32 @@
+USE "DATABASE_TO_DROP_TABLES_FROM"
+GO
+
+DECLARE @Sql NVARCHAR(500) DECLARE @Cursor CURSOR
+
+SET @Cursor = CURSOR FAST_FORWARD FOR
+SELECT DISTINCT sql = 'ALTER TABLE [' + tc2.TABLE_SCHEMA + '].[' +  tc2.TABLE_NAME + '] DROP [' + rc1.CONSTRAINT_NAME + '];'
+FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc1
+LEFT JOIN INFORMATION_SCHEMA.TABLE_CONSTRAINTS tc2 ON tc2.CONSTRAINT_NAME =rc1.CONSTRAINT_NAME
+
+OPEN @Cursor FETCH NEXT FROM @Cursor INTO @Sql
+
+WHILE (@@FETCH_STATUS = 0)
+BEGIN
+Exec sp_executesql @Sql
+FETCH NEXT FROM @Cursor INTO @Sql
+END
+
+CLOSE @Cursor DEALLOCATE @Cursor
+GO
+
+EXEC sp_MSforeachtable 'DROP TABLE ?'
+GO
+
+
+----------------------------------------------------------
+--
+-- Citation(s)
+--
+--   stackoverflow.com  |  "How to drop all tables in a SQL Server database? - Stack Overflow"  |  https://stackoverflow.com/a/8439798
+--
+----------------------------------------------------------

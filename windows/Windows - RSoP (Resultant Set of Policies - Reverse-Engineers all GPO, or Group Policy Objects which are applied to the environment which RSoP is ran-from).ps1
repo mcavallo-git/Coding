e32ab62e7aba207144f334f5904c5c0a0e6571e6
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-14 22:36:19 -0500

    Create Windows - RSoP (Resultant Set of Policies - Reverse-Engineers all GPO, or Group Policy Objects which are applied to the environment which RSoP is ran-from).ps1

---
+++ windows/Windows - RSoP (Resultant Set of Policies - Reverse-Engineers all GPO, or Group Policy Objects which are applied to the environment which RSoP is ran-from).ps1
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------
+#
+#   RSoP.msc
+#
+#    Shows the "Resultant Set of Policies" - Reverse-Engineers all GPO, or Group Policy Objects which are applied to the environment which RSoP is ran-from).ps1
+#
+#    Native Win10 and Server2016-later command)
+#
+# ------------------------------------------------------------
+
+
+rsop.msc
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   activedirectorypro.com  |  "How to use RSoP to check and troubleshoot group policy settings"  |  https://activedirectorypro.com/how-to-use-rsop-to-check-and-troubleshoot-group-policy-settings/
+#
+# ------------------------------------------------------------

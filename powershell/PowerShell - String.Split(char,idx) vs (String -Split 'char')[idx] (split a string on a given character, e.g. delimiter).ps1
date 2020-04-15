8924abe85337d541ee25378f93752f0149bc429d
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-15 16:46:46 -0500

    rn

---
+++ powershell/PowerShell - String.Split(char,idx) vs (String -Split 'char')[idx] (split a string on a given character, e.g. delimiter).ps1
@@ -0,0 +1,35 @@
+
+
+# PowerShell - Split a string on a given character (delimter)
+
+("1|2|3|4|5".Split("|"))[2]; # Get the 3rd element
+
+
+("1|2|3|4|5".Split("|"))[-1];
+
+
+# ------------------------------------------------------------
+
+If ($True) {
+
+# These should all return the same value
+
+# ("repo/origin/master".Split("/",-1)); # !!! ERROR - Split() can't handle negative indicies
+("repo/origin/master".Split("/"))[-1];
+("repo/origin/master" -Split "/")[-1];
+# ("origin/master".Split("/",-1)); # !!! ERROR - Split() can't handle negative indicies
+("origin/master".Split("/"))[-1];
+("origin/master" -Split "/")[-1];
+
+}
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "About Split - Explains how to use the Split operator to split one or more strings into substrings"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_split?view=powershell-5.1
+#
+#   docs.microsoft.com  |  "String.Split Method (System) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/dotnet/api/system.string.split?redirectedfrom=MSDN&view=netframework-4.8#System_String_Split_System_Char___System_Int32_
+#
+# ------------------------------------------------------------

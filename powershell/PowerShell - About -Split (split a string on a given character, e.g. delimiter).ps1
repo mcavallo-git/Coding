de0cb6554394b90e987005f1570ee26a35317a7d
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-15 16:29:15 -0500

    Create PowerShell - About -Split (split a string on a given character, e.g. delimiter).ps1

---
+++ powershell/PowerShell - About -Split (split a string on a given character, e.g. delimiter).ps1
@@ -0,0 +1,14 @@
+
+
+# PowerShell - Split a string on a given character (delimter)
+
+("1|2|3|4|5" -Split '|')[2]
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "About Split - Explains how to use the Split operator to split one or more strings into substrings.  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_split?view=powershell-5.1
+#
+# ------------------------------------------------------------

1cd550cd71fc99561964624ebfa8f03bcd3f4fa7
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-16 20:31:14 -0500

    Update ESXi PowerCLI - Search connected repos' available vibs for a driver.ps1

--- vmware/powercli (ESXi PowerShell CLI)/ESXi PowerCLI - Search connected repos' available vibs for a driver.ps1
+++ vmware/powercli (ESXi PowerShell CLI)/ESXi PowerCLI - Search connected repos' available vibs for a driver.ps1
@@ -5,5 +5,9 @@ Install-Module -Name "VMware.PowerCLI";
 Add-EsxSoftwareDepot "https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml";
 Add-EsxSoftwareDepot "https://vibsdepot.v-front.de/index.xml";
 $Vibs = (Get-EsxSoftwarePackage);
+$Vibs_Sorted = ($Vibs | Sort-Object -Property Name,@{Expression={$_.Version}; Ascending=$False});
+$Vibs_Sorted | Format-List > "${Home}\Desktop\Vibs_FormatList.txt";
+$Vibs_Sorted | Format-Table -Wrap > "${Home}\Desktop\Vibs_FormatTable.txt";
+
 }
 

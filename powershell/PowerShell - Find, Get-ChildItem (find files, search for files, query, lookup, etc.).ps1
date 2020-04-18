907be351bb31eff7592f8361c06468d812f7b250
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-18 00:01:26 -0500

    Update PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1

--- powershell/PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1
+++ powershell/PowerShell - Find, Get-ChildItem (find files, search for files, query, lookup, etc.).ps1
@@ -15,6 +15,8 @@ Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue
 
 Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { ($_.Name -Eq "devenv.com") -Or ($_.Name -Eq "devenv.exe") } | ForEach-Object { $_.FullName; }
 
+Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Name -Like "*.msixbundle" } | ForEach-Object { $_.FullName; }
+
 Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Name -Like "devenv.*" } | ForEach-Object { $_.FullName; }
 
 Get-ChildItem -Path ("C:\") -File -Recurse -Force -ErrorAction "SilentlyContinue" | Where-Object { $_.Name -Like "*devenv*" } | ForEach-Object { $_.FullName; }

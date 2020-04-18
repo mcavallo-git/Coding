755aed01e5ebba39d096c1517f785ff81c390a5d
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-18 02:43:38 -0500

    Create Linux - ssh, openssh -G (view git or ssh config files applied to connection, for system etc.).sh

---
+++ linux/Linux - ssh, openssh -G (view git or ssh config files applied to connection, for system etc.).sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TargetHost="localhost";
+ssh -vvv -G "${TargetHost}";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   serverfault.com  |  "How to show the host/configured/default ssh "configuration"? - Server Fault"  |  https://serverfault.com/a/717177
+#
+# ------------------------------------------------------------

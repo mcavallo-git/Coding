a5ab3535e42500f31a95d5eb2cb3cf6e38b0c03f
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-18 02:52:20 -0500

    rn

--- linux/Linux - ssh -g (view ssh, openssh, git config files applied to connection, for system etc.).sh
+++ linux/Linux - ssh -g (view ssh, openssh, git config files applied to connection, for system etc.).sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 TargetHost="localhost";
-ssh -vvv -G "${TargetHost}";
+ssh -vG "${TargetHost}"; # Show both included config files as well as their contained values
+ssh -vG "${TargetHost}" | grep 'Reading configuration data'; # Show only filepaths for included config files
 
 
+# ------------------------------------------------------------
+# Info regarding command(s) used above (info from 'man ssh'):
+#
+#     ssh -G      Causes ssh to print its configuration after evaluating Host and Match blocks and exit.
+#     ssh -v      Verbose mode.  Causes ssh to print debugging messages about its progress.
+#
+#
 # ------------------------------------------------------------
 #
 # Citation(s)

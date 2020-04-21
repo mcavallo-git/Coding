7229a2f526655b695b8058fc456b6a809ab0b0cb
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-21 02:36:14 -0500

    Update EdgeOS (UniFi) - repairing mongodb.sh

--- ubiquiti/EdgeOS (UniFi) - repairing mongodb.sh
+++ ubiquiti/EdgeOS (UniFi) - repairing mongodb.sh
@@ -1,18 +1,29 @@
-
-#
-#	Referring to guide:
-#
-# https://help.ubnt.com/hc/en-us/articles/360006634094-UniFi-Network-Controller-Repairing-Database-Issues-on-the-UniFi-Controller
-#
-
-
+#!/bin/bash
+# ------------------------------------------------------------
 # How to Repair a Database on Debian-based Linux
 
 service unifi stop;
 
 mongod --dbpath /usr/lib/unifi/data/db --smallfiles --logpath /usr/lib/unifi/logs/server.log --repair;
 
-chown -R "unifi:unifi /usr/lib/unifi/data/db/";
-chown -R "unifi:unifi /usr/lib/unifi/logs/server.log";
+chown -R "unifi:unifi" "/usr/lib/unifi/data/db/";
+chown -R "unifi:unifi" "/usr/lib/unifi/logs/server.log";
 
 service unifi start;
+
+
+# ------------------------------------------------------------
+
+mongoexport --collection="startup_log" --db="local" --out="mongoexport.$(date +'%Y%m%d_%H%M%S').json";
+
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.mongodb.com  |  "mongoexport — MongoDB Manual"  |  https://docs.mongodb.com/manual/reference/program/mongoexport/#cmdoption-mongoexport-collection
+#
+#   help.ubnt.com  |  "UniFi - Network Controller: Repairing Database Issues on the UniFi Controller – Ubiquiti Networks Support and Help Center"  |  https://help.ubnt.com/hc/en-us/articles/360006634094-UniFi-Network-Controller-Repairing-Database-Issues-on-the-UniFi-Controller
+#
+# ------------------------------------------------------------

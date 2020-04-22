fc5cbef40a61a32a00bbccc2a8c75a88e3068768
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-21 22:59:32 -0500

    Update Linux - substrings (count specific characters).sh

--- linux/Linux - substrings (count specific characters).sh
+++ linux/Linux - substrings (count specific characters).sh
@@ -1,18 +1,42 @@
 #!/bin/bash
+# ------------------------------------------------------------
+#
+# Linux - Count the number of occurrences of a specific characters in a given string
+#
+# ------------------------------------------------------------
+#
+# Method 1 - Using bashisms
+#
 
-DAT_STRING="lostlhost";
-SUBSTRING_ONLY_PERIODS="${DAT_STRING//[^.]}";
+HAYSTACK_TO_SEARCH="localhost";
+NEEDLE_TO_FIND=".";
+ALL_NEEDLES_FOUND="${HAYSTACK_TO_SEARCH//[^${NEEDLE_TO_FIND}]}";
+NEEDLE_TOTAL_COUNT="${#ALL_NEEDLES_FOUND}";
+echo "HAYSTACK_TO_SEARCH = \"${HAYSTACK_TO_SEARCH}\"";
+echo "NEEDLE_TO_FIND = \"${NEEDLE_TO_FIND}\"";
+echo "NEEDLE_TOTAL_COUNT = \"${NEEDLE_TOTAL_COUNT}\"";
+
+
+FIND_PERIODS_IN="localhost";
+SUBSTRING_ONLY_PERIODS="${FIND_PERIODS_IN//[^.]}";
 COUNT_PERIODS="${#SUBSTRING_ONLY_PERIODS}";
-echo "DAT_STRING = \"${DAT_STRING}\"";
+echo "FIND_PERIODS_IN = \"${FIND_PERIODS_IN}\"";
 echo "COUNT_PERIODS = \"${COUNT_PERIODS}\"";
 
 
-
-DAT_STRING="www.example.com";
-SUBSTRING_ONLY_PERIODS="${DAT_STRING//[^.]}";
+FIND_PERIODS_IN="www.example.com";
+SUBSTRING_ONLY_PERIODS="${FIND_PERIODS_IN//[^.]}";
 COUNT_PERIODS="${#SUBSTRING_ONLY_PERIODS}";
-echo "DAT_STRING = \"${DAT_STRING}\"";
+echo "FIND_PERIODS_IN = \"${FIND_PERIODS_IN}\"";
 echo "COUNT_PERIODS = \"${COUNT_PERIODS}\"";
+
+
+# ------------------------------------------------------------
+#
+# Method 2 - Using 'tr' and 'awk'
+#
+
+tr -d -c '"\n' < dat | awk '{ print length; }';
 
 
 
@@ -21,5 +45,7 @@ echo "COUNT_PERIODS = \"${COUNT_PERIODS}\"";
 #	Citation(s)
 #
 #		stackoverflow.com  |  "Count occurrences of a char in a string using Bash"  |  https://stackoverflow.com/a/16679640
+#
+#   unix.stackexchange.com  |  "text processing - How to count the number of a specific character in each line? - Unix & Linux Stack Exchange"  |  https://unix.stackexchange.com/a/18742
 #
 # ------------------------------------------------------------

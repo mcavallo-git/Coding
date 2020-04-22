16ad620be4ae1e6d0779dfd4ffcb60364e6019e4
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-22 03:00:22 -0500

    Update Linux - Trim whitespace from string.sh

--- linux/Linux - Trim whitespace from string.sh
+++ linux/Linux - Trim whitespace from string.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-STRING_WITH_LEADING_TRAILING_WHITESPACE="$(echo -e "${STRING_WITH_LEADING_TRAILING_WHITESPACE}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
+STRING_WITH_LEADING_TRAILING_WHITESPACE="$(echo -e "${STRING_WITH_LEADING_TRAILING_WHITESPACE}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')";
 
-# Thanks to https://stackoverflow.com/a/3232433
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   stackoverflow.com  |  "string - How to trim whitespace from a Bash variable? - Stack Overflow"  |  https://stackoverflow.com/a/3232433
+#
+# ------------------------------------------------------------

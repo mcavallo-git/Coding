0cec157372a377d3f8097206aa3386e49726604e
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-21 22:59:31 -0500

    Update ipv4_cidr_regex.sh

--- linux/bash_scripts/ipv4_cidr_regex.sh
+++ linux/bash_scripts/ipv4_cidr_regex.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+# ------------------------------------------------------------
 #
-#	Check to see if string is a valid IPv4 (either with or without CIDR notation)
+#  Check to see if string is a valid IPv4 (either with or without CIDR notation)
 #
+# ------------------------------------------------------------
+
+
+if [ 1 -eq 1 ]; then
 
 ARRAY_ITEMS_TO_TEST=( \
 "0.0.0.0" \
@@ -21,27 +27,31 @@ echo -e "${LINEBREAK}";
 
 for EACH_ITEM in "${ARRAY_ITEMS_TO_TEST[@]}"; do
 
-	if [[ ${EACH_ITEM} =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\/([0-9]|[1-2][0-9]|3[0-2]))?$ ]]; then
+  if [[ ${EACH_ITEM} =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\/([0-9]|[1-2][0-9]|3[0-2]))?$ ]]; then
 
-		# EACH_CIDR="${EACH_ITEM}";
+    # EACH_CIDR="${EACH_ITEM}";
 
-		if [[ ${EACH_ITEM} =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(())$ ]]; then
-			# Add "/32" to the end of string, if no CIDR notation is found - ex: 192.168.1.1
-			EACH_CIDR="${EACH_ITEM}/32";
-		else
-			# Valid IPv4 in CIDR Notation - ex: 192.168.1.0/24
-			EACH_CIDR="${EACH_ITEM}";
-		fi;
+    if [[ ${EACH_ITEM} =~ ^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(())$ ]]; then
+      # Add "/32" to the end of string, if no CIDR notation is found - ex: 192.168.1.1
+      EACH_CIDR="${EACH_ITEM}/32";
+    else
+      # Valid IPv4 in CIDR Notation - ex: 192.168.1.0/24
+      EACH_CIDR="${EACH_ITEM}";
+    fi;
 
-		echo "  Valid IPv4: \"${EACH_CIDR}\"";
+    echo "  Valid IPv4: \"${EACH_CIDR}\"";
 
-	else
+  else
 
-		echo "  Invalid IPv4: \"${EACH_ITEM}\"";
+    echo "  Invalid IPv4: \"${EACH_ITEM}\"";
 
-	fi;
+  fi;
 
 done;
 
 echo -e "${LINEBREAK}";
 
+fi;
+
+
+# ------------------------------------------------------------

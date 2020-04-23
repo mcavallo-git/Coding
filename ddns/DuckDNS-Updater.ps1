36a95a9d8e40601c4aadaced255e2f196e98f0e5
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-23 15:03:39 -0500

    Update DuckDNS-Updater.ps1

--- ddns/DuckDNS-Updater.ps1
+++ ddns/DuckDNS-Updater.ps1
@@ -16,12 +16,12 @@ powershell.exe -Command "If (Test-Path ((${Home})+('\.ddns\secret'))) { [System.
 
 # PowerShell -Command "
 ForEach ($LocalUser In (Get-ChildItem ('C:/Users'))) {
-	If (Test-Path (($LocalUser.FullName)+('/.duck-dns/secret'))) {
+	If (Test-Path (($LocalUser.FullName)+('/.ddns/secret'))) {
 		[System.Net.WebRequest]::Create(
 			[System.Text.Encoding]::Unicode.GetString(
 				[System.Convert]::FromBase64String(
 					(Get-Content(
-						(($LocalUser.FullName)+('/.duck-dns/secret')))
+						(($LocalUser.FullName)+('/.ddns/secret')))
 					)
 				)
 			)
@@ -36,18 +36,18 @@ Exit 0;
 # Creating the Credentials file(s) - Read-in the user-specific domain/password from the credentials file(s), below
 
 
-$nc_host = Get-Content -Path (($Home)+("/.duck-dns/host"));
-$nc_domain = Get-Content -Path (($Home)+("/.duck-dns/domain"));
-$nc_token = Get-Content -Path (($Home)+("/.duck-dns/token"));
+$nc_host = Get-Content -Path (($Home)+("/.ddns/host"));
+$nc_domain = Get-Content -Path (($Home)+("/.ddns/domain"));
+$nc_token = Get-Content -Path (($Home)+("/.ddns/token"));
 $nc_ip = "";
 
 $nc_urlPlaintext = (("https://dynamicdns.park-your-domain.com/update?host=")+($nc_host)+("&domain=")+($nc_domain)+("&password=")+($nc_token)+("&ip=")+($nc_ip));
 
 $nc_urlBase64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($nc_urlPlaintext));
 
-New-Item -ItemType "Directory" -Path ("$HOME/.duck-dns") -ErrorAction SilentlyContinue; # Credentials - Ensure that parent-directory exists
+New-Item -ItemType "Directory" -Path ("$HOME/.ddns") -ErrorAction SilentlyContinue; # Credentials - Ensure that parent-directory exists
 
-[IO.File]::WriteAllText(("$HOME/.duck-dns/secret"),($nc_urlBase64)); # Credentials - Output the base-64 encoded string into the final url-file
+[IO.File]::WriteAllText(("$HOME/.ddns/secret"),($nc_urlBase64)); # Credentials - Output the base-64 encoded string into the final url-file
 
 
 

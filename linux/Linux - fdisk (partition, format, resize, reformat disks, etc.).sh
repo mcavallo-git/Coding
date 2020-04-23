961d3411a91a3bb4b37699b40b62f5c4dac5803a
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-23 00:04:17 -0500

    Create Linux - fdisk (partition, format, resize, reformat disks, etc.).sh

---
+++ linux/Linux - fdisk (partition, format, resize, reformat disks, etc.).sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+fdisk /dev/sdb
+
+# Type 'n' > Enter
+
+# fdisk's built-in wizard will walk you through the rest
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   devconnected.com  |  "How To Create Disk Partitions on Linux – devconnected"  |  https://devconnected.com/how-to-create-disk-partitions-on-linux/
+#
+# ------------------------------------------------------------

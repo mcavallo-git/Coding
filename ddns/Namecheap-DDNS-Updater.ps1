fde729e9f08c6734fa8f4f3084501148b15b901b
Matt Cavallo <mcavallo.work@gmail.com>
2020-04-23 15:06:22 -0500

    Update Namecheap-DDNS-Updater.ps1

--- ddns/Namecheap-DDNS-Updater.ps1
+++ ddns/Namecheap-DDNS-Updater.ps1
@@ -6,7 +6,7 @@
 If ($False) { # One-Liner command (for use w/ Task Scheduler):
 
 
-powershell.exe -Command "If (Test-Path ((${Home})+('\.ddns\secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((${Home})+('\.ddns\secret'))))))).GetResponse(); }; Exit 0;"
+powershell.exe -Command "If (Test-Path ((${Home})+('\.ddns\secret'))) { [System.Net.WebRequest]::Create([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String((Get-Content((${Home})+('\.ddns\secret')))))).GetResponse(); }; Exit 0;"
 
 
 }

4f703c4124ea10403c27c1225b9740ccaa6ce71f
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-26 21:09:59 -0500

    Update Windows - xbox_fix_teredo_address_not_obtained.bat

--- windows/Windows - xbox_fix_teredo_address_not_obtained.bat
+++ windows/Windows - xbox_fix_teredo_address_not_obtained.bat
@@ -8,13 +8,19 @@ REM
 REM METHOD 1 (more reliable)
 REM
 
+REM
 REM   1. Open Win + X menu and choose Device Manager from the list.
+REM 
 REM   2. When Device Manager opens, go to View and select Show hidden devices from the menu.
+REM 
 REM   3. Locate Teredo in the Network Adapters section, right-click it and choose Uninstall device. Repeat this for all Teredo devices.
+REM 
 REM   4. Uninstall anything you see with “Teredo” in it from the right-click menu
+REM 
 REM   5. Restart your PC and re-enable Teredo with this line in Command Prompt (Admin): netsh interface Teredo set state type=default
+REM 
 REM   6. Make sure to NOT manually install the Teredo Tunneling Pseudo-Interface adapter as it might cause trouble since it reinstalls automatically
-
+REM
 
 
 REM ------------------------------------------------------------
@@ -25,7 +31,6 @@ REM
 NETSH interface Teredo set state disable
 TIMEOUT /T 3
 NETSH interface Teredo set state type=default
-
 
 
 REM ------------------------------------------------------------

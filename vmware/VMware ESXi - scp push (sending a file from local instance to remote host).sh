54884143bfcb32370784ca9aff3ba1c364d949e0
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-29 06:30:29 -0500

    Update VMware ESXi - scp push (sending a file from local instance to remote host).sh

--- vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
+++ vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 
-# Setup public key on local device for incoming SSH authentication
-SSH_AS_USERNAME="root";
-# > Upload public key to the root directory as "/USERNAME.pub"
-mkdir "/etc/ssh/keys-${SSH_AS_USERNAME}"; chmod 0755 "/etc/ssh/keys-${SSH_AS_USERNAME}"; mv "/${SSH_AS_USERNAME}.pub" "/etc/ssh/keys-${SSH_AS_USERNAME}/authorized_keys"; chmod 0600 "/etc/ssh/keys-${SSH_AS_USERNAME}/authorized_keys";
-
-
 # Connect to external SSH host (using SSH key) & send file(s)
 scp -o StrictHostKeyChecking=no -P 22 -i "/PATH/TO/SSH_KEY" "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
 

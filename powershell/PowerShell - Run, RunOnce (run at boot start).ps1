402f60bca63971e8e5e57f644d987c4bc45d4b13
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-29 12:04:46 -0500

    Update PowerShell - Run, RunOnce (run at boot start).ps1

--- powershell/PowerShell - Run, RunOnce (run at boot start).ps1
+++ powershell/PowerShell - Run, RunOnce (run at boot start).ps1
@@ -4,4 +4,11 @@ HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
 HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce
 HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce
 
-https://docs.microsoft.com/en-us/windows/win32/setupapi/run-and-runonce-registry-keys
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Run and RunOnce Registry Keys - Windows applications | Microsoft Docs"  |  https://docs.microsoft.com/en-us/windows/win32/setupapi/run-and-runonce-registry-keys
+#
+# ------------------------------------------------------------

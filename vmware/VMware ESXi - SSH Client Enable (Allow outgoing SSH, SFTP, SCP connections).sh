caeba0acafb3b096512cdec431beacc80ff0cfba
Matt Cavallo <mcavallo.git@gmail.com>
2020-04-29 06:27:18 -0500

    Update VMware ESXi - SSH Client Enable (Allow outgoing SSH, SFTP, SCP connections).sh

--- vmware/VMware ESXi - SSH Client Enable (Allow outgoing SSH, SFTP, SCP connections).sh
+++ vmware/VMware ESXi - SSH Client Enable (Allow outgoing SSH, SFTP, SCP connections).sh
@@ -10,6 +10,12 @@ esxcli network firewall ruleset list --ruleset-id sshClient;  # Check if SSH cli
 esxcli network firewall ruleset set --ruleset-id sshClient --enabled=true;  # Enable SSH client
 
 
+# Setup public key on local device for incoming SSH authentication
+SSH_AS_USERNAME="root";
+# > Upload public key to the root directory as "/USERNAME.pub"
+mkdir "/etc/ssh/keys-${SSH_AS_USERNAME}"; chmod 0755 "/etc/ssh/keys-${SSH_AS_USERNAME}"; mv "/${SSH_AS_USERNAME}.pub" "/etc/ssh/keys-${SSH_AS_USERNAME}/authorized_keys"; chmod 0600 "/etc/ssh/keys-${SSH_AS_USERNAME}/authorized_keys";
+
+
 # ------------------------------------------------------------
 # Citation(s)
 #

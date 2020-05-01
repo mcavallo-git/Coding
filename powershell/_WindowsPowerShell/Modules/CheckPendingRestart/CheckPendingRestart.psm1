3be7e029451e361b2fe0a2db4c1b0bea68b82bc7
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-01 05:26:16 -0500

    update to TLS 1.2

--- powershell/_WindowsPowerShell/Modules/CheckPendingRestart/CheckPendingRestart.psm1
+++ powershell/_WindowsPowerShell/Modules/CheckPendingRestart/CheckPendingRestart.psm1
@@ -14,7 +14,7 @@ Function CheckPendingRestart() {
 	If ($False) { # RUN THIS SCRIPT:
 
 
-		$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol;	[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]"Tls11,Tls12"; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/CheckPendingRestart/CheckPendingRestart.psm1')); CheckPendingRestart; [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak; CheckPendingRestart;
+		$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol;	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/CheckPendingRestart/CheckPendingRestart.psm1')); CheckPendingRestart; [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak; CheckPendingRestart;
 
 
 	}

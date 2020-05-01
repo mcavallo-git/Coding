4345fbbdd0b3dfc03c008329d0d211a415f58564
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-01 18:01:35 -0500

    Update PowerShell - Test-Connection (get workstation LAN IPv4).ps1

--- powershell/PowerShell - Test-Connection (get workstation LAN IPv4).ps1
+++ powershell/PowerShell - Test-Connection (get workstation LAN IPv4).ps1
@@ -1,15 +1,24 @@
+# ------------------------------------------------------------
 
 $ThisIPv4_LAN = (Test-Connection -ComputerName($env:COMPUTERNAME) -Count (1)).IPV4Address.IPAddressToString;
 
 $ThisIPv4_LAN;
 
 
+# ------------------------------------------------------------
+
+$AllIPAddresses = (Get-NetIPAddress).IPAddress;
+
+$AllIPAddresses;
+
+
+# ------------------------------------------------------------
 #
-#	Citation(s)
+# Citation(s)
 #
-#		Documentation
-#			docs.microsoft.com, "Test-Connection", https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-connection
+#   docs.microsoft.com  |  "Test-Connection"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/test-connection
 #
-#		Solution
-#			Thanks to StackOverflow user [ Jana Sattainathan ] on forum [ https://stackoverflow.com/questions/27277701 ]
 #
+#   stackoverflow.com  |  "windows - Powershell get ipv4 address into a variable - Stack Overflow"  |  https://stackoverflow.com/a/32204521
+#
+# ------------------------------------------------------------

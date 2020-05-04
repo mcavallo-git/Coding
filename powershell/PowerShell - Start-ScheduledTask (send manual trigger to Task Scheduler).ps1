7853eacc812c41bee7578ca25b944c229c5b789a
Matt Cavallo <mcavallo.work@gmail.com>
2020-05-04 01:03:39 -0500

    Update PowerShell - Start-ScheduledTask (send manual trigger to Task Scheduler).ps1

--- powershell/PowerShell - Start-ScheduledTask (send manual trigger to Task Scheduler).ps1
+++ powershell/PowerShell - Start-ScheduledTask (send manual trigger to Task Scheduler).ps1
@@ -8,6 +8,16 @@ Start-ScheduledTask -TaskName "GitSyncAll";
 
 # ------------------------------------------------------------
 #
+# Rmotely Kick-Off one (or more) separate device's scheduled task(s)
+#
+
+$cimSession = New-CimSession -ComputerName SERVER1,SERVER2,SERVER3,SERVER4
+Start-ScheduledTask TASKNAME -CimSession $cimSession;
+Remove-CimSession $cimSession;
+
+
+# ------------------------------------------------------------
+#
 # Determine task names (to apply to Start-ScheduldTask once found)
 #
 
@@ -19,5 +29,7 @@ Start-ScheduledTask -TaskName "GitSyncAll";
 #	Citation(s)
 #
 #  	docs.microsoft.com  |  "Start-ScheduledTask - Starts one or more instances of a scheduled task"  |  https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/start-scheduledtask
+#
+#  	www.granikos.eu  |  "Just can't get enough of IT | Start Scheduled Tasks remotely"  |  https://www.granikos.eu/en/justcantgetenough/PostId/184/start-scheduled-tasks-remotely
 #
 # ------------------------------------------------------------

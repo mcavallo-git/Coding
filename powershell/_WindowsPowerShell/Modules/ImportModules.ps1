a08cda6939b65c98b56fcacae65ed945e3810113
Matt Cavallo <mcavallo.work@gmail.com>
2020-05-04 22:07:43 -0500

    Update ImportModules.ps1

--- powershell/_WindowsPowerShell/Modules/ImportModules.ps1
+++ powershell/_WindowsPowerShell/Modules/ImportModules.ps1
@@ -6,7 +6,7 @@
 If ($False) { ### RUN THIS SCRIPT:
 
 
-$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/ImportModules.ps1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Start-Process PowerShell.exe $(New-Object Net.WebClient).DownloadString("https://ps.mcavallo.com/$((Date).Ticks).ps1") -Verb RunAs; [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
 
 
 }

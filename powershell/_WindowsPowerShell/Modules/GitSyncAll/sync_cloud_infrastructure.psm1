59d7acf72c439f70042bc4b7d899993694c75b73
Matt Cavallo <mcavallo.work@gmail.com>
2020-05-04 22:29:17 -0500

    Create sync_cloud_infrastructure.psm1

---
+++ powershell/_WindowsPowerShell/Modules/GitSyncAll/sync_cloud_infrastructure.psm1
@@ -0,0 +1,21 @@
+# ------------------------------------------------------------
+#
+#	sync_cloud_infrastructure
+#		Quickly pull & update the local device's git repository from the cloud
+#
+# ------------------------------------------------------------
+function sync_cloud_infrastructure {
+	#
+	# Module to Fetch & Pull all Repositories in a given directory [ %USERPROFILE%\Documents\GitHub ] Directory 
+	#
+	Param(
+		[Switch]$Quiet
+	)
+	Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; $ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/mcavallo-git/Coding/master/sync.ps1?t=$((Date).Ticks)")); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+	Return;
+}
+
+<# Only export the module if the caller is attempting to import it #>
+If (($MyInvocation.GetType()) -Eq ("System.Management.Automation.InvocationInfo")) {
+	Export-ModuleMember -Function "sync_cloud_infrastructure";
+}

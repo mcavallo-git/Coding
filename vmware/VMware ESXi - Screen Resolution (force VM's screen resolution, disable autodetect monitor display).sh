59b259ab2e37b9f6ae90a4d4d41cd9c0f809be14
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-08 15:04:30 -0500

    Update VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display).sh

--- vmware/VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display).sh
+++ vmware/VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display).sh
@@ -1,11 +1,27 @@
 #!/bin/sh
+# ------------------------------------------------------------
+#
+# Force a resolution onto a VM which is powered-on and has VMware Tools currently running
+#
+ESXI_VM_NAME="YOUR_VM_NAME_HERE";
+SCREEN_WIDTH="1366";
+SCREEN_HEIGHT="768";
+ID_VM=$(vim-cmd vmsvc/getallvms | sed -e '1d' -e 's/ \[.*$//' | awk '$1 ~ /^[0-9]+$/ {print $1":"substr($0,8,80)}' | sed -rne "s/^([0-9]+):(${ESXI_VM_NAME}\s*)$/\1/p";); echo "ID_VM: ${ID_VM}";
+vim-cmd vmsvc/setscreenres ${ID_VM} ${SCREEN_WIDTH} ${SCREEN_HEIGHT};
 
-
-# Refer to citation (below)
-
-
-# Basically, force the parameters by editing the target VM's ".vmx" file
-
+# ------------------------------------------------------------
+#
+# Shutdown the VM, then manually SSH into the ESXi server and edit said VM's ".vmx" config file
+#  > Add the following lines
+#
+### Note: svga.vramSize is calculated by multiplying the max width by the max height you wish to grant to target vm - e.g. If a 1920x1080 resolution should be the cap for a VM's resolution, then set svga.vramSize=2073600, as 1920 * 1080 = 2073600
+svga.vramSize = "2073600"
+svga.present = "TRUE"
+svga.guestBackedPrimaryAware = "TRUE"
+guestInfo.svga.wddm.modeset = "FALSE"
+guestInfo.svga.wddm.modesetCCD = "FALSE"
+guestInfo.svga.wddm.modesetLegacySingle = "FALSE"
+guestInfo.svga.wddm.modesetLegacyMulti = "FALSE"
 
 # ------------------------------------------------------------
 #

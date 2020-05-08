55f59cf7a17c824e6e731053edbf605e0d7e46ad
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-08 14:04:35 -0500

    Create VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display).sh

---
+++ vmware/VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display).sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+# Refer to citation (below)
+
+
+# Basically, force the parameters by editing the target VM's ".vmx" file
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   kb.vmware.com  |  "Adding video resolution modes to Windows guest operating systems (1003)"  |  https://kb.vmware.com/s/article/1003
+#
+# ------------------------------------------------------------

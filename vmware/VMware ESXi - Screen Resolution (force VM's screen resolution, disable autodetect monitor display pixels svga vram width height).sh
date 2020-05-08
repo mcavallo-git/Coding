d545bc73406bf842859684e1ea4085849c249f93
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-08 17:14:10 -0500

    rn

--- vmware/VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display pixels svga vram width height).sh
+++ vmware/VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display pixels svga vram width height).sh
@@ -3,11 +3,13 @@
 #
 # Force a resolution onto a VM which is powered-on and has VMware Tools currently running
 #
+
 ESXI_VM_NAME="YOUR_VM_NAME_HERE";
 SCREEN_WIDTH="1366";
 SCREEN_HEIGHT="768";
 ID_VM=$(vim-cmd vmsvc/getallvms | sed -e '1d' -e 's/ \[.*$//' | awk '$1 ~ /^[0-9]+$/ {print $1":"substr($0,8,80)}' | sed -rne "s/^([0-9]+):(${ESXI_VM_NAME}\s*)$/\1/p";); echo "ID_VM: ${ID_VM}";
 vim-cmd vmsvc/setscreenres ${ID_VM} ${SCREEN_WIDTH} ${SCREEN_HEIGHT};
+
 
 # ------------------------------------------------------------
 #
@@ -15,6 +17,8 @@ vim-cmd vmsvc/setscreenres ${ID_VM} ${SCREEN_WIDTH} ${SCREEN_HEIGHT};
 #  > Add the following lines
 #
 ### Note: svga.vramSize is calculated by multiplying the max width by the max height you wish to grant to target vm - e.g. If a 1920x1080 resolution should be the cap for a VM's resolution, then set svga.vramSize=2073600, as 1920 * 1080 = 2073600
+vi "/vmfs/volumes'datastore1/VMDIR/VMNAME.vmx";
+# ... (Add the following lines to the bottom of the ".vmx" file)
 svga.vramSize = "2073600"
 svga.present = "TRUE"
 svga.guestBackedPrimaryAware = "TRUE"
@@ -23,10 +27,13 @@ guestInfo.svga.wddm.modesetCCD = "FALSE"
 guestInfo.svga.wddm.modesetLegacySingle = "FALSE"
 guestInfo.svga.wddm.modesetLegacyMulti = "FALSE"
 
+
 # ------------------------------------------------------------
 #
 # Citation(s)
 #
 #   kb.vmware.com  |  "Adding video resolution modes to Windows guest operating systems (1003)"  |  https://kb.vmware.com/s/article/1003
+#
+#   www.vgemba.net  |  "VM Console Display Resolution Change - vGemba.net"  |  https://www.vgemba.net/vmware/powercli/VMConsole-Resolution-Change/
 #
 # ------------------------------------------------------------

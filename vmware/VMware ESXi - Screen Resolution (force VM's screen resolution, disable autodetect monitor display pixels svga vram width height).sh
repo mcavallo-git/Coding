58681f0f0bf66e588d380749277c911680762441
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-08 20:48:13 -0500

    Update VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display pixels svga vram width height).sh

--- vmware/VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display pixels svga vram width height).sh
+++ vmware/VMware ESXi - Screen Resolution (force VM's screen resolution, disable autodetect monitor display pixels svga vram width height).sh
@@ -6,21 +6,28 @@
 vi "/vmfs/volumes'datastore1/VMDIR/VMNAME.vmx";
 svga.autodetect = "FALSE"
 svga.minVRAMSize = "8388608"
-# vmotion.checkpointSVGAPrimarySize = "8388608"
+
+# Reload the VM's config (applies any changes made to the VM's ".vmx" config-file)
+ESXI_VM_NAME="VM_NAME"; ID_VM=$(vim-cmd vmsvc/getallvms | sed -e '1d' -e 's/ \[.*$//' | awk '$1 ~ /^[0-9]+$/ {print $1":"substr($0,8,80)}' | sed -rne "s/^([0-9]+):(${ESXI_VM_NAME}\s*)$/\1/p";); echo "ID_VM: ${ID_VM}"; vim-cmd vmsvc/reload ${ID_VM};
 
 
 
 # ------------------------------------------------------------
 #
-# Shutdown the VM, then manually SSH into the ESXi server and edit said VM's ".vmx" config file
-#  > Add the following lines
+# Increase total video memory allocated to a target VM (to allow for larger resolution outputs)
+#  > Step 1: Shutdown the VM, then manually SSH into the ESXi server and edit said VM's ".vmx" config file
+#   > Step 2: Add the following lines to said ".vmx" config-file
+#    > Note: The [ svga.vramSize ] config-value can be quickly calculated by multiplying desired-max-width by desired-max-height you wish to grant to target VM (regarding display resolution)
+#             - e.g. If a 1920x1080 resolution is desired to be the maximum resolution allocated for a given VM, then one would set [ svga.vramSize=2073600 ] in the target VM's ".vmx" config-file, since 1920 * 1080 = 2073600
 #
-### Note: svga.vramSize is calculated by multiplying the max width by the max height you wish to grant to target vm - e.g. If a 1920x1080 resolution should be the cap for a VM's resolution, then set svga.vramSize=2073600, as 1920 * 1080 = 2073600
 vi "/vmfs/volumes'datastore1/VMDIR/VMNAME.vmx";
 # ... (Add the following lines to the bottom of the ".vmx" file)
 svga.vramSize = "2073600"
 svga.present = "TRUE"
 svga.guestBackedPrimaryAware = "TRUE"
+
+# Reload the VM's config (applies any changes made to the VM's ".vmx" config-file)
+ESXI_VM_NAME="VM_NAME"; ID_VM=$(vim-cmd vmsvc/getallvms | sed -e '1d' -e 's/ \[.*$//' | awk '$1 ~ /^[0-9]+$/ {print $1":"substr($0,8,80)}' | sed -rne "s/^([0-9]+):(${ESXI_VM_NAME}\s*)$/\1/p";); echo "ID_VM: ${ID_VM}"; vim-cmd vmsvc/reload ${ID_VM};
 
 
 
@@ -37,6 +44,10 @@ guestInfo.svga.wddm.modeset = "FALSE"
 guestInfo.svga.wddm.modesetCCD = "FALSE"
 guestInfo.svga.wddm.modesetLegacySingle = "FALSE"
 guestInfo.svga.wddm.modesetLegacyMulti = "FALSE"
+svga.ignoreSavedState = "TRUE"
+
+# Reload the VM's config (applies any changes made to the VM's ".vmx" config-file)
+ESXI_VM_NAME="VM_NAME"; ID_VM=$(vim-cmd vmsvc/getallvms | sed -e '1d' -e 's/ \[.*$//' | awk '$1 ~ /^[0-9]+$/ {print $1":"substr($0,8,80)}' | sed -rne "s/^([0-9]+):(${ESXI_VM_NAME}\s*)$/\1/p";); echo "ID_VM: ${ID_VM}"; vim-cmd vmsvc/reload ${ID_VM};
 
 
 
@@ -57,6 +68,8 @@ vim-cmd vmsvc/setscreenres ${ID_VM} ${SCREEN_WIDTH} ${SCREEN_HEIGHT};
 # Citation(s)
 #
 #   kb.vmware.com  |  "Adding video resolution modes to Windows guest operating systems (1003)"  |  https://kb.vmware.com/s/article/1003
+#
+#   kb.vmware.com  |  "Reloading a vmx file without removing the virtual machine from inventory (1026043)"  |  https://kb.vmware.com/s/article/1026043
 #
 #   www.vgemba.net  |  "VM Console Display Resolution Change - vGemba.net"  |  https://www.vgemba.net/vmware/powercli/VMConsole-Resolution-Change/
 #

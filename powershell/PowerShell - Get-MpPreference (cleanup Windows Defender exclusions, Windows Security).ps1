bd249af7b353db8dd536e30d605b6af31a18c2f8
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-11 03:18:42 -0500

    Update PowerShell - Get-MpPreference (cleanup Windows Defender exclusions, Windows Security).ps1

--- powershell/PowerShell - Get-MpPreference (cleanup Windows Defender exclusions, Windows Security).ps1
+++ powershell/PowerShell - Get-MpPreference (cleanup Windows Defender exclusions, Windows Security).ps1
@@ -1,9 +1,9 @@
 If ($True) {
 $ProcessExclusions_All = ((Get-MpPreference).ExclusionProcess);
-$ProcessExclusions_ToRemove = @();
-$ProcessExclusions_All | ForEach-Object { If ((Test-Path -LiteralPath ("$_")) -NE $True) { Write-Output "INVALID:  $_"; $ProcessExclusions_ToRemove += $_; }; };
+$ProcessExclusions_Removed = @();
+$ProcessExclusions_All | ForEach-Object { If ((Test-Path -LiteralPath ("$_")) -NE $True) { Write-Output "INVALID:  $_"; $ProcessExclusions_Removed += $_; }; };
 Write-Output "`n`n";
 Write-Output "`$ProcessExclusions_All.Count: $($ProcessExclusions_All.Count) `n";
-Write-Output "`$ProcessExclusions_Invalid.Count: $($ProcessExclusions_Invalid.Count) `n";
+Write-Output "`$ProcessExclusions_Removed.Count: $($ProcessExclusions_Removed.Count) `n";
 Write-Output "`n`n";
 }

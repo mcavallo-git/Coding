2cc43d95f78c6cd7de73340671164cc47867fd1e
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-14 23:00:01 -0500

    Create PowerShell - Check for pending reboot, restart.ps1

---
+++ powershell/PowerShell - Check for pending reboot, restart.ps1
@@ -0,0 +1,13 @@
+
+<# Check for pending reboot #>
+$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "ByPass" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/CheckPendingRestart/CheckPendingRestart.psm1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+CheckPendingRestart;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   adamtheautomator.com  |  "How to check for a pending reboot (automated with PowerShell)"  |  https://adamtheautomator.com/pending-reboot-registry-windows/
+#
+# ------------------------------------------------------------

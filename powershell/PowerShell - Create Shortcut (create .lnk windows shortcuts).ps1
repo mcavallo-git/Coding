2cf380e500ab5dfb3e760e7014c0601b01566dc5
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 03:21:38 -0500

    Update PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1

--- powershell/PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1
+++ powershell/PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1
@@ -1,10 +1,9 @@
 
 <# Example Shortcut #>
 
-<# Create a shortcut on current user's Desktop which triggers Windows Updates' "Check for updates" command then opens the "Windows Update" page within Windows' "Settings" App #>
+<# Windows Updates (Creates a shortcut on the desktop) - Open, Check-for, and Download "Windows Updates" (let user select the "Install Now" button) #>
 $NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Home}\Desktop\Check for Updates.lnk");
 $NewShortcut.TargetPath=("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe");
-$NewShortcut.Arguments=("-Command `"((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()); Start-Process -Filepath ('C:\Windows\explorer.exe') -ArgumentList (@('ms-settings:windowsupdate'));`"");
+$NewShortcut.Arguments=("-Command `"Start-Process -Filepath ('C:\Windows\explorer.exe') -ArgumentList (@('ms-settings:windowsupdate')); ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());`"");
 $NewShortcut.WorkingDirectory=("");
 $NewShortcut.Save();
-

e5ba678563b036c461dcbb7909c791db466415fc
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 04:37:42 -0500

    Update PowerShell - Microsoft.Update.AutoUpdate (Check for Updates via CLI, Windows Updates).ps1

--- windows/PowerShell - Microsoft.Update.AutoUpdate (Check for Updates via CLI, Windows Updates).ps1
+++ windows/PowerShell - Microsoft.Update.AutoUpdate (Check for Updates via CLI, Windows Updates).ps1
@@ -8,11 +8,7 @@ Start-Process -Filepath ("C:\Windows\explorer.exe") -ArgumentList (@("ms-setting
 
 <# (ALL EXCEPT INSTALL NOW) #>
 <# Windows Updates (Creates a shortcut on the desktop) - Open, Check-for, and Download "Windows Updates" (let user select the "Install Now" button) #>
-$NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Home}\Desktop\Check for Updates.lnk");
-$NewShortcut.TargetPath=("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe");
-$NewShortcut.Arguments=("-Command `"Start-Process -Filepath ('C:\Windows\explorer.exe') -ArgumentList (@('ms-settings:windowsupdate')); ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());`"");
-$NewShortcut.WorkingDirectory=("");
-$NewShortcut.Save();
+$NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Home}\Desktop\Check for Updates.lnk"); $NewShortcut.TargetPath=("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"); $NewShortcut.Arguments=("-Command `"Start-Process -Filepath ('C:\Windows\explorer.exe') -ArgumentList (@('ms-settings:windowsupdate')); ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());`""); $NewShortcut.WorkingDirectory=(""); $NewShortcut.Save();
 
 
 # ------------------------------------------------------------

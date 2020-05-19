e837c7beca0e7de8808b64cad17c098621eb82fe
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 08:26:19 -0500

    Update PowerShell - Disable IPv6 (via the Registry).ps1

--- powershell/PowerShell - Disable IPv6 (via the Registry).ps1
+++ powershell/PowerShell - Disable IPv6 (via the Registry).ps1
@@ -12,28 +12,22 @@ $ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointM
 }
 # ------------------------------------------------------------
 #
-# Verify that the user does, indeed wish to disable IPv6 (require them to press 'y' (for yes)
+# Ask user if they wish to disable IPv6 Networking for this machine
 #
 
 Write-Output -NoNewLine "Info:  Disable IPv6 Networking to/from this Machine? (y/n)";
 $KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 If ($KeyPress.Character -Eq "y") {
 	Write-Output "Info:  Confirmed - Disabling IPv6....";
-	# Check if IPv4 IP address is preferred
-	Ping $Env:COMPUTERNAME;
-
-	# If the previous ping command responds with an IPv6 address (such as ::1), run following registry setting to disable IPv6 via the Registry --> reboot for the change to take effect
+	<# If the previous ping command responds with an IPv6 address (such as ::1), run following registry setting to disable IPv6 via the Registry --> reboot for the change to take effect #>
 	Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents"  -Type "DWord" -Value 0x20;
-
-	# Write-Host "You need to reboot the computer in order for the changes to take effect";
-	# Restart-Computer
+	<# Restore the previously-defined networking security protocol #>
+	[System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+	<# Check for pending reboot #>
 	$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "ByPass" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/CheckPendingRestart/CheckPendingRestart.psm1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
 	CheckPendingRestart;
-
 } Else {
-	Write-Output "Info:  Cancelled, exiting...";
-	Start-Sleep 10;
-
+	Write-Output "Info:  Skipped IPv6 Networking - NO CHANGES MADE";
 }
 
 

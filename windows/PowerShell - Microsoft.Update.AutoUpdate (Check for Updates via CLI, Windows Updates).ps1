30b8452582457b13e7977df6cb3d031666360a11
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 01:11:08 -0500

    Update PowerShell - Microsoft.Update.AutoUpdate (Check for Updates via CLI, Windows Updates).ps1

--- windows/PowerShell - Microsoft.Update.AutoUpdate (Check for Updates via CLI, Windows Updates).ps1
+++ windows/PowerShell - Microsoft.Update.AutoUpdate (Check for Updates via CLI, Windows Updates).ps1
@@ -1,12 +1,15 @@
 # ------------------------------------------------------------
 
 <# Check-for & Download available Windows Updates #>
-
 ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());
 
 
-# Create a shortcut in Windows named "Check for Updates" and paste the following into the "Target" field:
-#   C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe PowerShell -Command "((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());"
+<# Create a shortcut on current user's Desktop which triggers Windows Updates' "Check for updates" command #>
+$NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Home}\Desktop\Check for Updates.lnk");
+$NewShortcut.TargetPath=("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe");
+$NewShortcut.Arguments=("-Command `"((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());`"");
+$NewShortcut.WorkingDirectory=("");
+$NewShortcut.Save();
 
 
 # ------------------------------------------------------------

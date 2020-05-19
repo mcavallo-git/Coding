80eafa7403bf4612523a75cc7aa5c893001c263e
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 08:15:07 -0500

    Update PowerShell - Disable IPv6 (via the Registry).ps1

--- powershell/PowerShell - Disable IPv6 (via the Registry).ps1
+++ powershell/PowerShell - Disable IPv6 (via the Registry).ps1
@@ -22,11 +22,8 @@ If ($KeyPress.Character -Eq "y") {
 	# Check if IPv4 IP address is preferred
 	Ping $Env:COMPUTERNAME;
 
-	# If the previous ping command responds with an IPv6 address (such as ::1), run following registry setting to disable IPv6 via the Registry --> reboot to apply the change
-	New-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents" -Value 0x20 -PropertyType "DWord";
-
-	# If DisabledComponents exists, use the set cmdlet
-	Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents" -Value 0x20;
+	# If the previous ping command responds with an IPv6 address (such as ::1), run following registry setting to disable IPv6 via the Registry --> reboot for the change to take effect
+	Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters\" -Name "DisabledComponents"  -Type "DWord" -Value 0x20;
 
 	# Write-Host "You need to reboot the computer in order for the changes to take effect";
 	# Restart-Computer

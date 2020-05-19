7c2273eb70d5e2eda96568cfdb1ac24ca74d8ec2
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 01:06:40 -0500

    Update PowerShell - WSL, Enable feature.ps1

--- powershell/PowerShell - WSL, Enable feature.ps1
+++ powershell/PowerShell - WSL, Enable feature.ps1
@@ -1,5 +1,16 @@
+# ------------------------------------------------------------
+If ($False) {  # RUN THIS SCRIPT REMOTELY
+
+
+Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; $ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/PowerShell%20-%20WSL,%20Enable%20feature.ps1")); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+
+
+}
+# ------------------------------------------------------------
+
 
 # If the WSL Feature is currently set to Disabled, Enable it
 If ( ((Get-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux").State) -Eq "Disabled" ) { 
 	Enable-WindowsOptionalFeature -Online -FeatureName "Microsoft-Windows-Subsystem-Linux";
 }
+

4bba4252ebb3859e553f401238e5594c2b299b05
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 09:10:22 -0500

    Update PowerShell - Install-Debugging-Tools.ps1

--- powershell/PowerShell - Install-Debugging-Tools.ps1
+++ powershell/PowerShell - Install-Debugging-Tools.ps1
@@ -37,3 +37,4 @@ If ($True) {
 	$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "ByPass" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/CheckPendingRestart/CheckPendingRestart.psm1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
 	CheckPendingRestart;
 }
+

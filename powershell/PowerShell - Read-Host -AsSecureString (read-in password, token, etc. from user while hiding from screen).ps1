2655facb752d9baf3cb7b31ecf7f6489f4dd3f6c
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 08:03:34 -0500

    Update PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1

--- powershell/PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1
+++ powershell/PowerShell - Read-Host -AsSecureString (read-in password, token, etc. from user while hiding from screen).ps1
@@ -1,10 +1,45 @@
 # ------------------------------------------------------------
+#
+# Show a confirmation prompt to the user
+#  |--> Note: This only really cares about "y" confirmation responses, and lumps all the others as cancel actions
+#
+
+If ($True) {
+	Write-Host -NoNewLine "Info:  Yes or no? (y/n)" -ForegroundColor "Yellow" -BackgroundColor "Black";
+	$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+	If ($KeyPress.Character -Eq "y") {
+		Write-Output "Info:  You selected `"y`"";
+	} Else {
+		Write-Output "Info:  You did not select `"y`"";
+	}
+}
+
+
+# ------------------------------------------------------------
+#
+# Wait endlessly for a single keypress
+#
+
+Write-Host -NoNewLine "Info:  System restart required - Press 'y' to confirm and reboot this machine, now..." -ForegroundColor "Yellow" -BackgroundColor "Black";
+$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+While ($KeyPress.Character -NE "y") {
+	$KeyPress = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+}
+
+
+# ------------------------------------------------------------
+#
+# Read a generic string in from the user
+#
 
 $UsernamePlaintext = Read-Host -Prompt 'Type an example Username (in plaintext)';
 Write-Host "You typed `"${UsernamePlaintext}`"";
 
 
 # ------------------------------------------------------------
+#
+# # Read a Securestring in from the user & convert it to plaintext
+#
 
 $SecureString_ToPlainText = ([System.Runtime.InteropServices.Marshal]::PtrToStringUni([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($(Read-Host -AsSecureString -Prompt 'Type an example securestring'))));
 Write-Host "You typed `"${SecureString_ToPlainText}`"";

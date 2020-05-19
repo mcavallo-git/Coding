75d979d7bf8d7f43ae5d0909b28c1d84ef37c443
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 01:07:39 -0500

    Update PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1

--- powershell/PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1
+++ powershell/PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1
@@ -1,9 +1,9 @@
 
 $NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut($EachResolvedArr.Shortcut_Location);
 $NewShortcut.TargetPath=($EachResolvedArr.Shortcut_Target);
-$NewShortcut.Arguments=($EachResolvedArr.Shortcut_Arguments );
+$NewShortcut.Arguments=("-Command ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());");
 $NewShortcut.WorkingDirectory=($EachResolvedArr.Shortcut_WorkingDir);
 $NewShortcut.Save();
 $NewShortcut.FullName; # Show the filepath of the newly-created shortcut
 
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Command "((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());"
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe "-Command ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());"

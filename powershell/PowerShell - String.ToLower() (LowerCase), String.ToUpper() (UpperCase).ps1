904362cca72c68dc0b3154e95225dbb35ae45e50
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 14:17:41 -0500

    Create PowerShell - String.ToLower() (LowerCase), String.ToUpper() (UpperCase).ps1

---
+++ powershell/PowerShell - String.ToLower() (LowerCase), String.ToUpper() (UpperCase).ps1
@@ -0,0 +1,15 @@
+
+
+# String.ToLower() (LowerCase)
+
+
+# String.ToUpper() (UpperCase)
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   devblogs.microsoft.com  |  "PowerTip: Convert All Uppercase String to Lowercase | Scripting Blog"  |  https://devblogs.microsoft.com/scripting/powertip-convert-all-uppercase-string-to-lowercase/
+#
+# ------------------------------------------------------------

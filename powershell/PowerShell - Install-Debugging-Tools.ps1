33ac072e0a94add945e145700c372de9bf82234b
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 08:36:57 -0500

    Update PowerShell - Install-Debugging-Tools.ps1

--- powershell/PowerShell - Install-Debugging-Tools.ps1
+++ powershell/PowerShell - Install-Debugging-Tools.ps1
@@ -11,9 +11,13 @@ $ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointM
 
 }
 # ------------------------------------------------------------
-
 If ($True) {
-	<# Temporarily force TLS 1.1/1.2 as the default networking security protocol for external downloads #>
+	<# Windows Updates (Create a shortcut on the desktop) - Open, Check-for, and Download "Windows Updates" (let user select the "Install Now" button) #>
+	$NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Home}\Desktop\Check for Updates.lnk"); $NewShortcut.TargetPath=("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"); $NewShortcut.Arguments=("-Command `"Start-Process -Filepath ('C:\Windows\explorer.exe') -ArgumentList (@('ms-settings:windowsupdate')); ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());`""); $NewShortcut.WorkingDirectory=(""); $NewShortcut.Save();
+}
+# ------------------------------------------------------------
+If ($True) {
+	<# Temporarily force TLS 1.2 as the default networking security protocol for external downloads #>
 	$ProtoBak=([System.Net.ServicePointManager]::SecurityProtocol);
 	[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12
 	Clear-DnsClientCache;

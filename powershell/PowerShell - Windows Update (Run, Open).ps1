4dfe0bf00d595b1dceb6f341a7e17e45c6640c44
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 08:36:55 -0500

    Update PowerShell - Windows Update (Run, Open).ps1

--- powershell/PowerShell - Windows Update (Run, Open).ps1
+++ powershell/PowerShell - Windows Update (Run, Open).ps1
@@ -1,3 +1,4 @@
+# ------------------------------------------------------------
 
 
 <# Windows Updates - Open, Check-for, and Download "Windows Updates" (let user select the "Install Now" button) #>
@@ -6,6 +7,13 @@ Start-Process -Filepath ("C:\Windows\explorer.exe") -ArgumentList (@("ms-setting
 
 <# Windows Updates - Open the "Windows Update" page within Windows' "Settings" App #>
 Start-Process -Filepath ("C:\Windows\explorer.exe") -ArgumentList (@("ms-settings:windowsupdate"));
+
+
+# ------------------------------------------------------------
+
+
+<# Windows Updates (Create a shortcut on the desktop) - Open, Check-for, and Download "Windows Updates" (let user select the "Install Now" button) #>
+$NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Home}\Desktop\Check for Updates.lnk"); $NewShortcut.TargetPath=("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"); $NewShortcut.Arguments=("-Command `"Start-Process -Filepath ('C:\Windows\explorer.exe') -ArgumentList (@('ms-settings:windowsupdate')); ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());`""); $NewShortcut.WorkingDirectory=(""); $NewShortcut.Save();
 
 
 # ------------------------------------------------------------

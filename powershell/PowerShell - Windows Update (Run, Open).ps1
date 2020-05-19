8e9243a3cb8e366aaac662334a0ba8d8321cc4b8
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-19 01:14:11 -0500

    Create PowerShell - Windows Update (Run, Open).ps1

---
+++ powershell/PowerShell - Windows Update (Run, Open).ps1
@@ -0,0 +1,14 @@
+
+
+explorer ms-settings:windowsupdate
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Launch the Windows Settings app - UWP applications | Microsoft Docs"  |  https://docs.microsoft.com/en-us/windows/uwp/launch-resume/launch-settings-app
+#
+#   www.windows-commandline.com  |  "Run command for Windows update"  |  https://www.windows-commandline.com/run-command-for-windows-update/
+#
+# ------------------------------------------------------------

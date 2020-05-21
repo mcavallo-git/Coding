e278598240a18a7e4085fb8172413c2558683015
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-21 18:07:11 -0500

    Create Linux - Curl (download file).ps1

---
+++ linux/Linux - Curl (download file).ps1
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+curl -o "docker-compose.yml" "https://raw.githubusercontent.com/rundeck/docker-zoo/master/basic/docker-compose.yml";
+
+
+ 
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   stackoverflow.com  |  "Azure RedHat vm yum update fails with 'SSL peer rejected your certificate as expired.'"  |  https://stackoverflow.com/a/53445712
+#
+# ------------------------------------------------------------

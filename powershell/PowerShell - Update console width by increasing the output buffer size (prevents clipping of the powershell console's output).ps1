491920e1bd4f35463dbff388898f8548e5f3fbd5
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-21 18:07:20 -0500

    Update PowerShell - Update console width by increasing the output buffer size (prevents clipping of the powershell console's output).ps1

--- powershell/PowerShell - Update console width by increasing the output buffer size (prevents clipping of the powershell console's output).ps1
+++ powershell/PowerShell - Update console width by increasing the output buffer size (prevents clipping of the powershell console's output).ps1
@@ -7,7 +7,7 @@
 # ------------------------------------------------------------ 
 
 
-<# Update PowerShell console width to 16384 characters#>
+<# Update PowerShell console width to 16384 characters #>
 If (($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) { $Host.UI.RawUI.BufferSize = (New-Object ((($Host.UI.RawUI).BufferSize).GetType().FullName) (16384, $Host.UI.RawUI.BufferSize.Height)); };
 
 

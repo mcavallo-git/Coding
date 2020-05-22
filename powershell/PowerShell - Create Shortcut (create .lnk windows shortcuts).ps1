12b195288a4d5bb1e84340010ea022f37a894bb5
Matt Cavallo <mcavallo.git@gmail.com>
2020-05-22 11:03:29 -0500

    Update PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1

--- powershell/PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1
+++ powershell/PowerShell - Create Shortcut (create .lnk windows shortcuts).ps1
@@ -3,11 +3,23 @@
 <# Example Shortcut #>
 
 <# Windows Updates (Creates a shortcut on the desktop) - Open, Check-for, and Download "Windows Updates" (let user select the "Install Now" button) #>
-$NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Home}\Desktop\Check for Updates.lnk");
+$Filepath_NewShortcut = "${Home}\Desktop\Check for Updates.lnk";
+$NewShortcut = (New-Object -ComObject WScript.Shell).CreateShortcut("${Filepath_NewShortcut}");
 $NewShortcut.TargetPath=("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe");
 $NewShortcut.Arguments=("-Command `"Start-Process -Filepath ('C:\Windows\explorer.exe') -ArgumentList (@('ms-settings:windowsupdate')); ((New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow());`"");
 $NewShortcut.WorkingDirectory=("");
 $NewShortcut.Save();
 
+<# Run the shortcut as Admin #>
+$Bytes_NewShortcut = [System.IO.File]::ReadAllBytes("${Filepath_NewShortcut}");
+$Bytes_NewShortcut[0x15] = ($bytes[0x15] -bor 0x20); <# Perform a Bitwise-OR which checks the "Run as administrator" option on the shortcut #>
+[System.IO.File]::WriteAllBytes("${Filepath_NewShortcut}", ${Bytes_NewShortcut})
 
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   stackoverflow.com  |  "windows - How to create a Run As Administrator shortcut using Powershell - Stack Overflow"  |  https://stackoverflow.com/a/29002207
+#
 # ------------------------------------------------------------

72acf5d24a8624a999d6dbcd4f049f2da24eacd8
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-01 20:26:09 -0500

    prtg

---
+++ prtg/Custom Sensors/EXEXML/get_nvidia_gpu_power_draw.bat
@@ -0,0 +1,31 @@
+@echo off
+echo ^<?xml version="1.0" encoding="Windows-1252" ?^>
+echo ^<prtg^>
+
+echo ^<result^>
+echo        ^<channel^>GPU0 Power Draw^</channel^>
+echo ^<float^>1^</float^> 
+echo ^<value^>
+"C:\Program files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" --query-gpu=power.draw --format="csv,nounits,noheader" --id=0
+echo ^</value^>
+echo ^</result^>
+
+echo ^<result^>
+echo        ^<channel^>GPU1 Power Draw^</channel^>
+echo ^<float^>1^</float^> 
+echo ^<value^>
+"C:\Program files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" --query-gpu=power.draw --format="csv,nounits,noheader" --id=1
+echo ^</value^>
+echo ^</result^>
+
+echo ^</prtg^>
+
+
+
+REM ------------------------------------------------------------
+REM
+REM Citation(s)
+REM
+REM   www.reddit.com  |  "Graphing GPU Temps with PRTG : gpumining"  |  https://www.reddit.com/r/gpumining/comments/7v14xz/graphing_gpu_temps_with_prtg/dtoofn3/
+REM
+REM ------------------------------------------------------------
---
+++ prtg/Custom Sensors/EXEXML/get_nvidia_gpu_temperature.bat
@@ -0,0 +1,29 @@
+@echo off
+echo ^<?xml version="1.0" encoding="Windows-1252" ?^>
+echo ^<prtg^>
+
+echo ^<result^>
+echo        ^<channel^>GPU0 Temperature^</channel^>
+echo ^<value^>
+"C:\Program files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" --query-gpu=temperature.gpu --format="csv,nounits,noheader" --id=0
+echo ^</value^>
+echo ^</result^>
+
+echo ^<result^>
+echo        ^<channel^>GPU1 Temperature^</channel^>
+echo ^<value^>
+"C:\Program files\NVIDIA Corporation\NVSMI\nvidia-smi.exe" --query-gpu=temperature.gpu --format="csv,nounits,noheader" --id=1
+echo ^</value^>
+echo ^</result^>
+
+echo ^</prtg^>
+
+
+
+REM ------------------------------------------------------------
+REM
+REM Citation(s)
+REM
+REM   www.reddit.com  |  "Graphing GPU Temps with PRTG : gpumining"  |  https://www.reddit.com/r/gpumining/comments/7v14xz/graphing_gpu_temps_with_prtg/dtoofn3/
+REM
+REM ------------------------------------------------------------

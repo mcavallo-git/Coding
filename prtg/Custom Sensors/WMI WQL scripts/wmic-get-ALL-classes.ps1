8fb4ca10a2f579a7f49856e9902754d2c71ec42f
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-05 21:49:25 -0500

    Create wmic-get-ALL-classes.ps1

---
+++ prtg/Custom Sensors/WMI WQL scripts/wmic-get-ALL-classes.ps1
@@ -0,0 +1,12 @@
+
+
+Get-WmiObject -Query "SELECT * FROM meta_class";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Retrieving a WMI Class - Win32 apps | Microsoft Docs"  |  https://docs.microsoft.com/en-us/windows/win32/wmisdk/retrieving-a-class
+#
+# ------------------------------------------------------------

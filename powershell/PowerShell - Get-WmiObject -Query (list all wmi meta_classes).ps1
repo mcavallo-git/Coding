7bc3781d0591b582b587ff2058340686fd3d331e
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-05 22:35:58 -0500

    Update PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1

--- powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
+++ powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
@@ -10,15 +10,21 @@
 #
 # Output all to a logfile & display all available methods for each WMI class
 #
-$Logfile = "${Home}\Desktop\WMI_Classes_$($(${Env:USERNAME}).Trim())@$($(${Env:COMPUTERNAME}).Trim())" + $(If(${Env:USERDNSDOMAIN}){Write-Output ((".") + ($(${Env:USERDNSDOMAIN}).Trim()))}) +"_$(Get-Date -UFormat '%Y%m%d-%H%M%S').log.txt"; `
+$Logfile_txt = "${Home}\Desktop\WMI_Classes_$($(${Env:USERNAME}).Trim())@$($(${Env:COMPUTERNAME}).Trim())" + $(If(${Env:USERDNSDOMAIN}){Write-Output ((".") + ($(${Env:USERDNSDOMAIN}).Trim()))}) +"_$(Get-Date -UFormat '%Y%m%d-%H%M%S').log.txt"; `
+$Logfile_csv = "${Logfile_txt}.csv"; `
 If (($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) { $Host.UI.RawUI.BufferSize = (New-Object ((($Host.UI.RawUI).BufferSize).GetType().FullName) (16384, $Host.UI.RawUI.BufferSize.Height)); }; <# Update PowerShell console width to 16384 characters #> `
 Get-WmiObject -Query "SELECT * FROM meta_class" `
 | Where-Object { $_.Properties -NE @{} } `
 | Select-Object -Property Name,Properties `
 | Sort-Object -Property Name -Unique `
-| ForEach-Object { $EachLine=@(); $_.Properties | ForEach-Object { $EachLine += "$($_.Name)"; }; Write-Output "$($($_.Name.PadRight(120)))  { $($EachLine -join ', ') }"; } `
-| Out-File -Width 16384 -Append "${Logfile}"; `
-Notepad "${Logfile}";
+| ForEach-Object { `
+	$EachLine=@(); `
+	$_.Properties | ForEach-Object { $EachLine += "$($_.Name)"; }; `
+	Write-Output "$($($_.Name.PadRight(120)))  { $($EachLine -join ', ') }" | Out-File -Width 16384 -Append "${Logfile_txt}"; `
+	Write-Output "$($_.Name)	$($EachLine -join '	')}" | Out-File -Width 16384 -Append "${Logfile_csv}"; `
+} `
+Notepad "${Logfile_txt}";
+Notepad "${Logfile_csv}";
 
 
 # ------------------------------------------------------------

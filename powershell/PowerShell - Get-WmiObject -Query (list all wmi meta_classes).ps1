732f589b76e2d4e26e8fdc140dcbe12df00c6e9d
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-05 21:55:35 -0500

    Update PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1

--- powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
+++ powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
@@ -2,7 +2,7 @@
 If (($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) { $Host.UI.RawUI.BufferSize = (New-Object ((($Host.UI.RawUI).BufferSize).GetType().FullName) (16384, $Host.UI.RawUI.BufferSize.Height)); }; <# Update PowerShell console width to 16384 characters #>
 
 
-(Get-WmiObject -Query "SELECT * FROM meta_class").Name;
+(Get-WmiObject -Query "SELECT * FROM meta_class" | Sort-Object -Property Name -Unique).Name;
 
 
 # ------------------------------------------------------------

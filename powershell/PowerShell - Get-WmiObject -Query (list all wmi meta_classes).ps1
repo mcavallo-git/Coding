a2e3ab0b0c34222f5a38568ef8ffb9b86a722baa
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-05 22:01:47 -0500

    Update PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1

--- powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
+++ powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
@@ -12,7 +12,7 @@
 #
 $Logfile = "${Home}\Desktop\WMI_Classes_$($(${Env:USERNAME}).Trim())@$($(${Env:COMPUTERNAME}).Trim())" + $(If(${Env:USERDNSDOMAIN}){Write-Output ((".") + ($(${Env:USERDNSDOMAIN}).Trim()))}) +"_$(Get-Date -UFormat '%Y%m%d-%H%M%S').log.txt"; `
 If (($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) { $Host.UI.RawUI.BufferSize = (New-Object ((($Host.UI.RawUI).BufferSize).GetType().FullName) (16384, $Host.UI.RawUI.BufferSize.Height)); }; <# Update PowerShell console width to 16384 characters #> `
-(Get-WmiObject -Query "SELECT * FROM meta_class" `
+Get-WmiObject -Query "SELECT * FROM meta_class" `
 | Sort-Object -Property Name -Unique `
 | Format-Table -AutoSize `
 | Out-File -Width 16384 -Append "${Logfile}"; `

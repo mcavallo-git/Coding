c29ad98638d6d2d89b72f728821c341d42d60256
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-06 00:18:08 -0500

    Create PowerShell - Strings without quotes (via Write-Output stored into variable).ps1

---
+++ powershell/PowerShell - Strings without quotes (via Write-Output stored into variable).ps1
@@ -0,0 +1,12 @@
+
+
+$StringVar = Write-Output *StringWithoutQuotes*;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.idera.com  |  "Creating String Arrays without Quotes - Power Tips - Power Tips - IDERA Community"  |  https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/creating-string-arrays-without-quotes
+#
+# ------------------------------------------------------------

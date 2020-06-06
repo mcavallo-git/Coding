8240a3452f7b05cb62d16a29fdc7bd380aa12363
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-05 23:41:52 -0500

    rn

---
+++ prtg/Custom Sensors/EXEXML/restart-prtg-service.bat
@@ -0,0 +1,1 @@
+PowerShell -Command "Get-Service | Where-Object { ($_.DisplayName -Like '*PRTG*') } | ForEach-Object { Write-Host 'Restarting service $($_.DisplayName)...'; $_ | Restart-Service; };"
--- prtg/Custom Sensors/EXEXML/restart-prtg-services.ps1
+++
@@ -1,1 +0,0 @@
-Get-Service | Where-Object { ($_.DisplayName -Like '*PRTG*') } | ForEach-Object { Write-Host "Restarting service `"$($_.DisplayName)`"..."; $_ | Restart-Service; }; `

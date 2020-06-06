828232733fabad7292da0d8d787875345cc25ddb
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-05 22:19:14 -0500

    Create PowerShell - String.PadLeft(x), String.PadRight(x).ps1

---
+++ powershell/PowerShell - String.PadLeft(x), String.PadRight(x).ps1
@@ -0,0 +1,15 @@
+
+# Pad-Right
+Write-Output "$('pad'.PadRight(50))right";
+
+# Pad-Left
+Write-Output "pad$('left'.PadLeft(50))";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "String.PadRight Method (System) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/dotnet/api/system.string.padright?view=netframework-4.7
+#
+# ------------------------------------------------------------

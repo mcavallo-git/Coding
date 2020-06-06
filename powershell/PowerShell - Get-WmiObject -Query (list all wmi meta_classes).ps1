424ccc063a4314790653902d95ffc891346f8716
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-05 22:06:12 -0500

    Update PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1

--- powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
+++ powershell/PowerShell - Get-WmiObject -Query (list all wmi meta_classes).ps1
@@ -10,12 +10,13 @@
 #
 # Output all to a logfile & display all available methods for each WMI class
 #
+$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/Show/Show.psm1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak; `
 $Logfile = "${Home}\Desktop\WMI_Classes_$($(${Env:USERNAME}).Trim())@$($(${Env:COMPUTERNAME}).Trim())" + $(If(${Env:USERDNSDOMAIN}){Write-Output ((".") + ($(${Env:USERDNSDOMAIN}).Trim()))}) +"_$(Get-Date -UFormat '%Y%m%d-%H%M%S').log.txt"; `
 If (($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) { $Host.UI.RawUI.BufferSize = (New-Object ((($Host.UI.RawUI).BufferSize).GetType().FullName) (16384, $Host.UI.RawUI.BufferSize.Height)); }; <# Update PowerShell console width to 16384 characters #> `
 Get-WmiObject -Query "SELECT * FROM meta_class" `
 | Where-Object { $_.Properties -NE @{} } `
 | Sort-Object -Property Name -Unique `
-| ForEach-Object { Write-Output "`n"; Write-Output $_.Name; Write-Output $_.Properties; } `
+| ForEach-Object { Show $_; } `
 | Out-File -Width 16384 -Append "${Logfile}"; `
 Notepad "${Logfile}";
 

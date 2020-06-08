ea7f58292f501c2b4953a4dbbc3c0a52525e6ce5
Matt Cavallo <mcavallo.git@gmail.com>
2020-06-08 01:12:21 -0500

    Create Linux - ldapsearch (test LDAP queries from a linux box using openldap package).sh

---
+++ linux/Linux - ldapsearch (test LDAP queries from a linux box using openldap package).sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# ------------------------------------------------------------
+#
+# Test AD connection in Linux
+#
+
+yum -y install openldap*;
+
+ldapsearch \
+-x \
+-h "DC_FQDN_or_IPv4" \
+-D "DC=example,DC=com" \
+-W \
+-b "dc=example,dc=com" \
+-s sub "(cn=*)" cn mail sn;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   plugins.jenkins.io  |  "Active Directory | Jenkins plugin"  |  https://plugins.jenkins.io/active-directory/
+#
+#   stackoverflow.com  |  "hudson - How can I limit Jenkins LDAP access to users in a specific groupOfNames? - Stack Overflow"  |  https://stackoverflow.com/a/37191361
+#
+# ------------------------------------------------------------

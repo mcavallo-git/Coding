3bde581e31637626b98cfbfbe14a7a47e394a5cd
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-09 22:51:55 -0500

    NTP Updates

---
+++ networking/ntp-servers.sh
@@ -0,0 +1,38 @@
+# ------------------------------------------------------------
+#
+# Hostname NTP (Network Time Protocol) Servers
+#
+
+### If using 4 NTP Hostnames (Best-Practice)
+1.us.pool.ntp.org, pool.ntp.org, time-a-g.nist.gov, time.google.com
+
+### If using 3 NTP Hostnames
+1.us.pool.ntp.org, time-a-g.nist.gov, time.google.com
+
+### If using 2 NTP Hostnames
+pool.ntp.org, time.nist.gov
+
+### If using 1 NTP Hostname
+pool.ntp.org
+
+
+# ------------------------------------------------------------
+#
+# Static IPv4 NTP (Network Time Protocol) Servers
+#
+
+129.6.15.28      time-a-g.nist.gov       #  NIST, Gaithersburg, Maryland
+132.163.96.1     time-a-b.nist.gov       #  NIST, Boulder, Colorado
+132.163.97.1     time-a-wwv.nist.gov	   #  WWV, Fort Collins, Colorado
+128.138.140.44   utcnist.colorado.edu    #  University of Colorado, Boulder
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   gist.github.com  |  "List of Top Public Time Servers · GitHub"  |  https://gist.github.com/mutin-sa/eea1c396b1e610a2da1e5550d94b0453
+#
+#   tf.nist.gov  |  "NIST Internet Time Service"  |  https://tf.nist.gov/tf-cgi/servers.cgi
+#
+# ------------------------------------------------------------

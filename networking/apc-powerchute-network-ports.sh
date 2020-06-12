2049339ab500a375e7bc69d816d30cf47ae0b230
Matt Cavallo <mcavallo.git@gmail.com>
2020-06-12 11:54:38 -0500

    Update apc-powerchute-network-ports.sh

--- networking/apc-powerchute-network-ports.sh
+++ networking/apc-powerchute-network-ports.sh
@@ -5,51 +5,51 @@
 # ------------------------------------------------------------
 
 Published date:
-21 January 2020
+	21 January 2020
 
 # ------------------------------------------------------------
 
 Issue:
-What ports are used by each component of PowerChute Business Edition
+	What ports are used by each component of PowerChute Business Edition
 
 # ------------------------------------------------------------
 
 Product Line:
-PowerChute Business Edition
+	PowerChute Business Edition
 
 # ------------------------------------------------------------
 
 Environment:
-All supported OS
+	All supported OS
 
 # ------------------------------------------------------------
 
 Cause:
-Informational
+	Informational
 
 # ------------------------------------------------------------
 
 Solution:
 
-TCP
+	TCP
 
-2161 – Communication between Server and Agent (PowerChute 9.2.1 and below)
+		2161 – Communication between Server and Agent (PowerChute 9.2.1 and below)
 
-2160 – Communication between Console and Server (PowerChute 6.x)
+		2160 – Communication between Console and Server (PowerChute 6.x)
 
-2260 – Communication between Console and Server (PowerChute 7.x, 8.x, 9.x ending at 9.2.1)
+		2260 – Communication between Console and Server (PowerChute 7.x, 8.x, 9.x ending at 9.2.1)
 
-3052 – Agent Web UI and Logging features (PowerChute 9.2.1 and below)
+		3052 – Agent Web UI and Logging features (PowerChute 9.2.1 and below)
 
-6547 – Agent Web UI (PowerChute 9.x and 10.x)
+		6547 – Agent Web UI (PowerChute 9.x and 10.x)
+		
+	UDP
 
-UDP
+		2161 – Discovery of Agents by Server (PowerChute 9.2.1 and below)
 
-2161 – Discovery of Agents by Server (PowerChute 9.2.1 and below)
+		2160 – Discovery of Servers by Console (PowerChute 9.2.1 and below)
 
-2160 – Discovery of Servers by Console (PowerChute 9.2.1 and below)
-
-7846 – Business Edition SNMP Agent
+		7846 – Business Edition SNMP Agent
 
 
 # ------------------------------------------------------------

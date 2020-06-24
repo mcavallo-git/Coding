0fdeb73183a239e78942dfabfcda2fb1865c7fb1
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-23 20:23:01 -0500

    Create Linus - printf (repeat character or string multiple times).sh

---
+++ linux/Linus - printf (repeat character or string multiple times).sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# ------------------------------------------------------------
+#
+# Repeating a character in Linux using printf
+#
+
+printf '=%.0s' {1..60};
+
+
+# ------------------------------------------------------------
+#
+# NOTE: Dashes must be handled differently with printf
+#
+
+printf -- '-%.0s' {1..60};
+
+DASHES="$(printf -- '-%.0s' {1..60};)"; echo ${DASHES};
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   stackoverflow.com  |  "shell - How can I repeat a character in Bash? - Stack Overflow"  |  https://stackoverflow.com/a/5349842
+#
+#   unix.stackexchange.com  |  "bash - Dashes in printf - Unix & Linux Stack Exchange"  |  https://unix.stackexchange.com/a/22765
+#
+# ------------------------------------------------------------

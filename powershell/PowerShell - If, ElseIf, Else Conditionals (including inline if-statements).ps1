d93380b26b21d577d87411d33df2bd415cd693d9
Matt Cavallo <mcavallo.git@gmail.com>
2020-06-24 12:58:19 -0500

    Update PowerShell - If, ElseIf, Else Conditionals (including inline if-statements).ps1

--- powershell/PowerShell - If, ElseIf, Else Conditionals (including inline if-statements).ps1
+++ powershell/PowerShell - If, ElseIf, Else Conditionals (including inline if-statements).ps1
@@ -1,3 +1,3 @@
 
 <# Example of an Inline If-Else Conditional #>
-$GetLoopbackAddress = If ($PSBoundParameters.ContainsKey('GetLoopbackAddress') -Eq $True) { $True } Else { $GetLoopbackAddress };
+$GetLoopbackAddress = If ($PSBoundParameters.ContainsKey('GetLoopbackAddress') -Eq $True) { $True } ElseIf (0 -Eq 1) { 0 } Else { $GetLoopbackAddress };

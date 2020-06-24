2937ef76a89bd60a246d05ff517b582ddb4166e9
Matt Cavallo <mcavallo.work@gmail.com>
2020-06-23 20:54:20 -0500

    Update Linux - parted (disk partitioning).sh

--- linux/Linux - parted (disk partitioning).sh
+++ linux/Linux - parted (disk partitioning).sh
@@ -115,10 +115,13 @@ else
 	echo " |--> To mount device on-bootup (permanently add mount), copy-paste the following line to modify \"/etc/fstab\":";
 	echo "------------------------------------------------------------";
 	# Pull default bootup-mount-config-values from existing device's values
-	echo "echo \"${DEVICE} ${MOUNT_PATH} $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $3}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $4}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $5}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $6}';)\" >> '/etc/fstab';";
-	echo "------------------------------------------------------------";
+	echo "echo \"${DEVICE} ${MOUNT_PATH} $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $3}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $4}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $5}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $6}';)\" >> \"/etc/fstab\";";
+	echo " |";
+	read -p " |--> Apply this change to \"/etc/fstab\", now? (y/n)  " -n 1 -t 60 -r;
 	echo "";
-	if [ 0 -eq 1 ]; then  # ENABLE ONCE FSTAB FILE CHANGES ARE DONE AUTOMATICALLY
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "${DEVICE} ${MOUNT_PATH} $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $3}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $4}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $5}';) $(cat '/etc/fstab' | grep '^/dev' | head -n 1 | awk '{print $6}';)" >> "/etc/fstab";
+		REPLY="";
 		echo "$(date +'%Y-%m-%d_%H-%M-%S')  |  System reboot is required to apply change(s)";
 		read -p "  |--> Reboot, now? (y/n)  " -n 1 -t 60 -r;
 		echo "";
@@ -126,6 +129,8 @@ else
 			reboot now;
 		fi;
 	fi;
+	echo "------------------------------------------------------------";
+	echo "";
 fi;
 
 echo "";

484992c8ef1d056dcab596f98ddad4169b81dd0d
Matt Cavallo <mcavallo.git@gmail.com>
2020-06-29 01:52:31 -0500

    Update Linux - shopt (set bash environment variables).sh

--- linux/Linux - shopt (set bash environment variables).sh
+++ linux/Linux - shopt (set bash environment variables).sh
@@ -1,18 +1,50 @@
 #!/bin/bash
+# ------------------------------------------------------------
 #
 # Linux - shopt
 #
+# ------------------------------------------------------------
+# > shopt --help
+# shopt: shopt [-pqsu] [-o] [optname ...]
+#     Set and unset shell options.
+#
+#     Change the setting of each shell option OPTNAME.  Without any option
+#     arguments, list each supplied OPTNAME, or all shell options if no
+#     OPTNAMEs are given, with an indication of whether or not each is set.
+#
+#     Options:
+#       -o        restrict OPTNAMEs to those defined for use with `set -o'
+#       -p        print each shell option with an indication of its status
+#       -q        suppress output
+#       -s        enable (set) each OPTNAME
+#       -u        disable (unset) each OPTNAME
+#
+#     Exit Status:
+#     Returns success if OPTNAME is enabled; fails if an invalid option is
+#     given or OPTNAME is disabled.
+#
+# ------------------------------------------------------------
+#
+# Enabling a given shopt option
+#
+
+shopt -s OPTNAME;  # Replace "OPTNAME" with your desired shopt option
 
 
-shopt -s nocasematch; # If set, Bash matches patterns in a case-insensitive fashion when performing matching while executing case or [[ conditional commands, when performing pattern substitution word expansions, or when filtering possible completions as part of programmable completion.
+# ------------------------------------------------------------
+#
+# Examples (common use-cases for shopt):
+#
+
+shopt -s nocasematch;  # If set, bash matches patterns in a case-insensitive fashion when performing matching while executing case or [[ conditional commands, when performing pattern substitution word expansions, or when filtering possible completions as part of programmable completion.  (shopt OPTNAME description taken GNU Bash v4.4 manual), from man bash)
 
 
-shopt -s globstar; # If set, the pattern ‘**’ used in a filename expansion context will match all files and zero or more directories and subdirectories. If the pattern is followed by a ‘/’, only directories and subdirectories match.
+shopt -s globstar;  # If set, the pattern ** used in a pathname expansion context will match all files and zero or more directories and subdirectories.  If the pattern is followed by a /, only directories and subdirectories match.  (shopt OPTNAME description taken GNU Bash v4.4 manual), from man bash)
 
 
-shopt -s lastpipe; # If set, and job control is not active, the shell runs the last command of a pipeline not executed in the background in the current shell environment.
-          # |
-          # |--> e.g. extends the current shell into sub-shells (within piped-commands), sharing variables down-into them, as well
+shopt -s lastpipe;  # If set, and job control is not active, the shell runs the last command of a pipeline not executed in the background in the current shell environment.  (shopt OPTNAME description taken GNU Bash v4.4 manual), from man bash)
+                    #  |
+                    #  |--> e.g. extends the current shell into sub-shells (within piped-commands), sharing variables down-into them, as well
 
 
 # ------------------------------------------------------------

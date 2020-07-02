8415e563b7d4ad29e78d54cfbf82706075b15469
Matt Cavallo <mcavallo.git@gmail.com>
2020-07-01 22:35:31 -0500

    Update PowerCLI - Get-VM (List all VMs running on target ESXi Host).ps1

--- vmware/powercli (ESXi PowerShell CLI)/PowerCLI - Get-VM (List all VMs running on target ESXi Host).ps1
+++ vmware/powercli (ESXi PowerShell CLI)/PowerCLI - Get-VM (List all VMs running on target ESXi Host).ps1
@@ -5,35 +5,35 @@
 
 If ($True) {
 
-# Pre-Reqs: Check-for (and install if not found) the VMware PowerCLI PowerShell Module
-If ((Get-Module -ListAvailable -Name ("VMware.PowerCLI") -ErrorAction "SilentlyContinue") -Eq $Null) {
-	# Pre-Reqs: Check-for (and install if not found) the NuGet PowerShell Module-Repository
-	$PackageProvider = "NuGet";
-	If ((Get-PackageProvider -Name "${PackageProvider}" -ErrorAction "SilentlyContinue") -Eq $Null) {
-		$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol;
-		[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12;
-			Install-PackageProvider -Name ("${PackageProvider}") -Force -Confirm:$False; $InstallPackageProvider_ReturnCode = If($?){0}Else{1};  # Install-PackageProvider fails on default windows installs without at least TLS 1.1 as of 20200501T041624
-		[System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+	# Pre-Reqs: Check-for (and install if not found) the VMware PowerCLI PowerShell Module
+	If ((Get-Module -ListAvailable -Name ("VMware.PowerCLI") -ErrorAction "SilentlyContinue") -Eq $Null) {
+		# Pre-Reqs: Check-for (and install if not found) the NuGet PowerShell Module-Repository
+		$PackageProvider = "NuGet";
+		If ((Get-PackageProvider -Name "${PackageProvider}" -ErrorAction "SilentlyContinue") -Eq $Null) {
+			$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol;
+			[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12;
+				Install-PackageProvider -Name ("${PackageProvider}") -Force -Confirm:$False; $InstallPackageProvider_ReturnCode = If($?){0}Else{1};  # Install-PackageProvider fails on default windows installs without at least TLS 1.1 as of 20200501T041624
+			[System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+		}
+		Install-Module -Name ("VMware.PowerCLI") -Scope CurrentUser -Force;
 	}
-	Install-Module -Name ("VMware.PowerCLI") -Scope CurrentUser -Force;
-}
 
-# Ignore Invalid HTTPS Certs (for LAN servers, etc.)
-Set-PowerCLIConfiguration -InvalidCertificateAction "Ignore" -Confirm:$False;
+	# Ignore Invalid HTTPS Certs (for LAN servers, etc.)
+	Set-PowerCLIConfiguration -InvalidCertificateAction "Ignore" -Confirm:$False;
 
-$vSphere_ConnectionStream = Connect-VIServer -Server ($(Read-Host 'Enter FQDN/IP of vSphere Server')) -Port "443" -Protocol "https";
+	$vSphere_ConnectionStream = Connect-VIServer -Server ($(Read-Host 'Enter FQDN/IP of vSphere Server')) -Port "443" -Protocol "https";
 
-If ($vSphere_ConnectionStream -NE $Null) {
+	If ($vSphere_ConnectionStream -NE $Null) {
 
-# Do some action with the now-connected vSphere Hypervisor (ESXi Server) 
+	# Do some action with the now-connected vSphere Hypervisor (ESXi Server) 
 
-# Get-Datastore | Format-List;
+	# Get-Datastore | Format-List;
 
-Get-VM | Sort-Object -Property Name | Format-Table -Autosize
+	Get-VM | Sort-Object -Property Name | Format-Table -Autosize
 
-}
+	}
 
-Disconnect-VIServer "*" -Confirm:$False;
+	Disconnect-VIServer "*" -Confirm:$False;
 
 }
 

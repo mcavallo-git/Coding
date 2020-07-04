2725545899a57a36d5557ce86b93b39c33ceeed1
Matt Cavallo <mcavallo.git@gmail.com>
2020-07-03 23:47:05 -0500

    Update VMware ESXi - scp push (sending a file from local instance to remote host).sh

--- vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
+++ vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 
-# Connect to external SSH host (using SSH key) & send file(s)
-scp -o StrictHostKeyChecking=no -l 102400 -P 22 -i "/PATH/TO/SSH_KEY" "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
+### NO Rate-Limiting
+scp -o StrictHostKeyChecking=no -P 22 "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
+scp -o StrictHostKeyChecking=no -P 22 -r "/vmfs/volumes/datastore/VM DIR/" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\";
 
-scp -o StrictHostKeyChecking=no -l 102400 -P 22 -i "/PATH/TO/SSH_KEY" -r "/vmfs/volumes/datastore/VM DIR/" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\";
-
+### WITH Rate-Limiting
+scp -o StrictHostKeyChecking=no -l 102400 -P 22 "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
+scp -o StrictHostKeyChecking=no -l 102400 -P 22 -r "/vmfs/volumes/datastore/VM DIR/" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\";

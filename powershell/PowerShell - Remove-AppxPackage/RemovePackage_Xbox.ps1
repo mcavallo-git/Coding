ced36496f2b0f41d73a86d90adf5481e7901f80f
Matt Cavallo <mcavallo.git@gmail.com>
2020-07-23 02:58:53 -0500

    Update RemovePackage_Xbox.ps1

--- powershell/PowerShell - Remove-AppxPackage/RemovePackage_Xbox.ps1
+++ powershell/PowerShell - Remove-AppxPackage/RemovePackage_Xbox.ps1
@@ -2,16 +2,22 @@
 
 
 # Get locally-available Xbox apps (across all users on the local device) - must run in an elevated (run as admin) terminal --> shows apps that you may have uninstalled but want to re-install, and already exist, locally
-Get-AppXPackage -AllUsers -Name "Microsoft.Xbox*" | Sort-Object -Property Name | Format-Table -AutoSize;
-
+If ($True) {
+	Write-Host "`nXbox apps installed for at least one local user:" -ForegroundColor "Yellow"; Get-AppXPackage -AllUsers -Name "Microsoft.Xbox*" | Sort-Object -Property Name | Format-Table -AutoSize;
+}
 
 
 # Uninstall the built-in Xbox app(s) which come pre-packaged with stock installs of Win10 / WinServer2016 / WinServer2019
-Get-AppxPackage -Name "Microsoft.Xbox*" | Remove-AppxPackage;
+If ($True) {
+	Get-AppxPackage -Name "Microsoft.Xbox*" | Remove-AppxPackage;
+}
 
 
 # Re-install the built-in Xbox app(s) which come pre-packaged with stock installs of Win10 / WinServer2016 / WinServer2019
-Get-AppXPackage -AllUsers -Name "Microsoft.Xbox*" | Foreach { Write-Host "Attempting to install package `"$($_.Name)`"..." -ForegroundColor "Cyan"; Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"; };
+If ($True) {
+	Get-AppXPackage -AllUsers -Name "Microsoft.Xbox*" | Foreach { Write-Host "Attempting to install package `"$($_.Name)`"..." -ForegroundColor "Cyan"; Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"; };`
+	Write-Host "`nXbox apps installed for current user:" -ForegroundColor "Yellow"; Get-AppXPackage -Name "Microsoft.Xbox*" | Sort-Object -Property Name | Format-Table -AutoSize;
+}
 
 
 # ------------------------------------------------------------

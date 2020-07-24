8f5b1d4a7d47f975fe50391277ddd79f1d2b7601
Matt Cavallo <mcavallo.git@gmail.com>
2020-07-24 12:13:43 -0500

    Create Linux - exiftool (read-write file metadata).sh

---
+++ linux/Linux - exiftool (read-write file metadata).sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# ------------------------------------------------------------
+
+# exiftool - get all metadata tags' names and their associated values for target file
+exiftool -s example.mp4
+
+
+# exiftool - get the date-created timestamp metadata for target file
+exiftool -s -s -s -createdate example.mp4
+
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   exiftool.org  |  "https://exiftool.org/install.html#Windows"  |  Installing ExifTool
+#
+#   photo.stackexchange.com  |  "https://photo.stackexchange.com/a/56678"  |  software - How can I extract just the value of a tag without the name using exiftool? - Photography Stack Exchange
+#
+#   stackexchange.com  |  "https://stackoverflow.com/a/53926462"  |  windows - How to change Media Created Date in Exiftool? - Stack Overflow
+#
+# ------------------------------------------------------------

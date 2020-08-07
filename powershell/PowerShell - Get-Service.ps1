1cd6d6e481359eae4e29db9c647f24a1cf5b23b5
Matt Cavallo <mcavallo.git@gmail.com>
2020-08-07 09:12:45 -0500

    Update PowerShell - Get-Service.ps1

--- powershell/PowerShell - Get-Service.ps1
+++ powershell/PowerShell - Get-Service.ps1
@@ -1,18 +1,39 @@
+# ------------------------------------------------------------
+#
+# List all services matching a given Name (Unique-ID) or DisplayName (shown under the "Name" column in services.msc, ridiculously enough)
+#
+
+Get-Service -ErrorAction "SilentlyContinue" `
+| Where-Object { `
+	(($_.Name -Like "*ASUS*") -Or ($_.DisplayName -Like "*ASUS*")) `
+} `
+| Select-Object -Property DisplayName `
+| Sort-Object -Property DisplayName `
+;
+
 
 #
-# Show Running Services
+# List services which are have the "Running" status
 #
-Get-Service -ErrorAction "SilentlyContinue" | Where-Object { $_.Status -eq "Running"}
+Get-Service -ErrorAction "SilentlyContinue" `
+| Where-Object { `
+	($_.Status -eq "Running") `
+} `
+;
 
 
+# ------------------------------------------------------------
+#
+# STOP service(s) by Name
+#
 
-#
-# Stop Service(s) by Name
-#
-Get-Service -Name ("wuauserv") -ErrorAction "SilentlyContinue" | Where-Object { $_.Status -eq "Running"} | ForEach-Object {
-	Write-Host "`n$($MyInvocation.MyCommand.Name) - Task: Stopping Service `"$($_.Name)`" ...  " -ForegroundColor "Gray";
-	Stop-Service -Name ($_.Name) -Force -NoWait -ErrorAction "SilentlyContinue";
-}
+Get-Service -Name ("wuauserv") -ErrorAction "SilentlyContinue" `
+| Where-Object { $_.Status -eq "Running"} `
+| ForEach-Object { `
+	Write-Host "`n$($MyInvocation.MyCommand.Name) - Task: Stopping Service `"$($_.Name)`" ...  " -ForegroundColor "Gray"; `
+	Stop-Service -Name ($_.Name) -Force -NoWait -ErrorAction "SilentlyContinue"; `
+} `
+;
 
 
 # ------------------------------------------------------------

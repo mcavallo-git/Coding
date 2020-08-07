9ee81d2b65b816a16389987997fd4acbccf6a8f4
Matt Cavallo <mcavallo.git@gmail.com>
2020-08-07 14:35:41 -0500

    Create prtg.network-ports.sh

---
+++ networking/prtg.network-ports.sh
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------
+
+PRTG Network Monitor - Network Port Used
+ |
+ |--> Refer to https://kb.paessler.com/en/topic/61462-which-ports-does-prtg-use-on-my-system
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   kb.paessler.com  |  "Which ports does PRTG use on my system?"  |  https://kb.paessler.com/en/topic/61462-which-ports-does-prtg-use-on-my-system
+#
+# ------------------------------------------------------------

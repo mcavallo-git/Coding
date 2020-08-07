1e987bea67efdaf7804a7372f566b4c6d93fbd8b
Matt Cavallo <mcavallo.git@gmail.com>
2020-08-07 18:25:04 -0500

    Update Windows - powercfg -attributes (show hidden power options, settings, config).bat

--- windows/Windows - powercfg -attributes (show hidden power options, settings, config).bat
+++ windows/Windows - powercfg -attributes (show hidden power options, settings, config).bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM SET attrib=+ATTRIB_HIDE
-    SET attrib=-ATTRIB_HIDE
+SET attrib=-ATTRIB_HIDE
 
 REM Harddisk settings
 powercfg -attributes 0012ee47-9041-4b5d-9b77-535fba8b1442 0b2d69d7-a2a1-449c-9680-f91c70521c60 %attrib%

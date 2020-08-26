55fe294ff503e8251f1e1fdd9e8f0e389d25851c
Matt Cavallo <mcavallo.git@gmail.com>
2020-08-26 13:13:22 -0500

    Update HandBrake-Fast1080p.ps1

--- windows/HandBrake/HandBrakeCLI-InputOutput/HandBrake-Fast1080p.ps1
+++ windows/HandBrake/HandBrakeCLI-InputOutput/HandBrake-Fast1080p.ps1
@@ -10,10 +10,9 @@
 #
 If ($False) {
 
-. "${Home}\Documents\GitHub\Coding\windows\HandBrake\HandBrakeCLI-InputOutput\HandBrake-Fast1080p.ps1"
+# . "${Home}\Documents\GitHub\Coding\windows\HandBrake\HandBrakeCLI-InputOutput\HandBrake-Fast1080p.ps1"
 
-
-PowerShell -Command "Start-Process -Filepath ('C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe') -ArgumentList ('-File ${Home}\Documents\GitHub\Coding\windows\HandBrake\HandBrakeCLI-InputOutput\HandBrake-Fast1080p.ps1') -Verb 'RunAs' -Wait -PassThru | Out-Null;"
+# PowerShell -Command "Start-Process -Filepath ('C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe') -ArgumentList ('-File ${Home}\Documents\GitHub\Coding\windows\HandBrake\HandBrakeCLI-InputOutput\HandBrake-Fast1080p.ps1') -Verb 'RunAs' -Wait -PassThru | Out-Null;"
 
 $ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/windows/HandBrake/HandBrakeCLI-InputOutput/HandBrake-Fast1080p.ps1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
 

90af417103506306d217afb52d12a65964ab6350
Matt Cavallo <mcavallo.git@gmail.com>
2020-08-26 08:23:24 -0500

    ahkv2

--- powershell/PowerShell - PowerShell ... -Verb RunAs (Run command as Administrator).ps1
+++ powershell/PowerShell - PowerShell ... -Verb RunAs (Run command as Administrator).ps1
@@ -1,1 +1,1 @@
-Start-Process "${ENV:ProgramFiles}\AutoHotkey\Autohotkey.exe" "${ENV:USERPROFILE}\Documents\GitHub\Coding\ahk\_WindowsHotkeys.ahk" -Verb RunAs;
+Start-Process "${ENV:ProgramFiles}\AutoHotkey\Autohotkey.exe" "${ENV:USERPROFILE}\Documents\GitHub\Coding\ahk\_WindowsHotkeys.ahkv2" -Verb RunAs;

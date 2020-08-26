a9dce7c80c8f07acb280006c359fc731502d0bab
Matt Cavallo <mcavallo.git@gmail.com>
2020-08-26 11:30:29 -0500

    Update PowerShell - Set-Content (overwrite or append target file's contents).ps1

--- powershell/PowerShell - Set-Content (overwrite or append target file's contents).ps1
+++ powershell/PowerShell - Set-Content (overwrite or append target file's contents).ps1
@@ -6,7 +6,7 @@
 
 If ($True) {
 	$Path_TestScript = "${Env:UserProfile}\Desktop\test_set-content_$(Get-Date -UFormat '%Y%m%d-%H%M%S').ps1";
-	Set-Content -Path ("${Path_TestScript}") -Value ("Split-Path `"`${PSCommandPath}`" | Format-List;");
+	Set-Content -Path ("${Path_TestScript}") -Value ("Get-Item `"`${PSCommandPath}`" | Format-List *;");
 	. "${Path_TestScript}";
 }
 
@@ -16,5 +16,7 @@ If ($True) {
 # Citation(s)
 #
 #   docs.microsoft.com  |  "Set-Content - Writes new content or replaces existing content in a file"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-content?view=powershell-5.1
+#
+#   stackoverflow.com  |  "How to list all properties of a PowerShell object - Stack Overflow"  |  https://stackoverflow.com/a/7259178
 #
 # ------------------------------------------------------------

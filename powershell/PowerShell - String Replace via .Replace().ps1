82d241c7831475ed49c52a5d4a3ade90218fabe2
Matt Cavallo <mcavallo.git@gmail.com>
2020-09-23 01:49:22 -0500

    Update PowerShell - String Replace via .Replace().ps1

--- powershell/PowerShell - String Replace via .Replace().ps1
+++ powershell/PowerShell - String Replace via .Replace().ps1
@@ -1,7 +1,15 @@
+# ------------------------------------------------------------
 #
 # PowerShell
-# 	|--> String Replace via .Replace()
+# 	|
+# 	|--> String replace via .Replace()   !!!  CASE SENSITIVE  !!!
+# 	|
+# 	|--> String replace via -Replace     !!!  CASE IN-SENSITIVE  !!!
 #
+# ------------------------------------------------------------
 
 "Hello City!".Replace("City","World"); # Outputs: 'Hello World!'
+
+
+"Hello City!" -Replace "city","World"); # Outputs: 'Hello World!'
 

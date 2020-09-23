67850868f3ce743ff573df50e63457a2de606083
Matt Cavallo <mcavallo.git@gmail.com>
2020-09-22 23:45:06 -0500

    Update Get-MetaData.psm1

--- powershell/_WindowsPowerShell/Modules/Get-MetaData/Get-MetaData.psm1
+++ powershell/_WindowsPowerShell/Modules/Get-MetaData/Get-MetaData.psm1
@@ -1,3 +1,10 @@
+# ------------------------------------------------------------
+<# RUN THIS SCRIPT ON-THE-FLY:
+
+	$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol;	[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/Get-MetaData/Get-MetaData.psm1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak; Get-Command Get-MetaData;
+
+#>
+# ------------------------------------------------------------
 function Get-MetaData
 {
 	Param

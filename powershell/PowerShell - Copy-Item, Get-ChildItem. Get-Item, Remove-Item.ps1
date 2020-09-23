54d91016a4e28b182e688f962760c5b65d669451
Matt Cavallo <mcavallo.git@gmail.com>
2020-09-22 19:45:36 -0500

    Update PowerShell - Copy-Item, Get-ChildItem. Get-Item, Remove-Item.ps1

--- powershell/PowerShell - Copy-Item, Get-ChildItem. Get-Item, Remove-Item.ps1
+++ powershell/PowerShell - Copy-Item, Get-ChildItem. Get-Item, Remove-Item.ps1
@@ -6,6 +6,44 @@ Copy-Item -Path ($From) -Destination ($To) -Force;
 
 # ------------------------------------------------------------
 #
+# FIND FILES MATCHING FILENAME *.JSON TWO FOLDERS DEEP WITHIN CURRENT DIRECTORY
+#
+
+(Get-Item ".\*\*.json") | Format-List *;
+
+
+# ------------------------------------------------------------
+#
+# GOOGLE PHOTOS EXPORTS --> UPDATE MEDIA'S DATE-CREATED TIMESTAMP/DATETIME ON MEDIA FILES BASED OFF OF THEIR ASSOCIATED METADATA (JSON) FILES' CONTENTS
+#
+
+# Download and use "Json-Decoder" instead of using less-powerful (but native) "ConvertFrom-Json"
+Set-ExecutionPolicy -ExecutionPolicy "Bypass" -Scope "CurrentUser" -Force; New-Item -Force -ItemType "File" -Path ("${Env:TEMP}\JsonDecoder.psm1") -Value (($(New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/JsonDecoder/JsonDecoder.psm1"))) | Out-Null; Import-Module -Force ("${Env:TEMP}\JsonDecoder.psm1");
+
+<# Locate all json metadata files #>
+(Get-Item ".\*\*.json") | ForEach-Object {
+	$EachMetadata_Fullpath = ($_.FullName);
+	$EachMetadata_BaseName= ($_.BaseName);
+	$EachMetadata_DirectoryName = ($_.DirectoryName);
+	$EachMediaFile_Fullpath = "${EachMetadata_DirectoryName}\${EachMetadata_BaseName}";
+	<# Ensure associated media-file exists #>
+	If (Test-Path "${EachMediaFile_Fullpath}") {
+		<# Get the date-created timestamp/datetime from the json-file #>
+		$EachMetadata_Contents = [IO.File]::ReadAllText("${EachMetadata_Fullpath}");
+		$EachMetadata_Object = JsonDecoder -InputObject (${EachMetadata_Contents});
+		$EachCreation_EpochSeconds = $EachMetadata_Object.creationTime.timestamp;
+		$EachCreation_DateTime = (New-Object -Type DateTime -ArgumentList 1970, 1, 1, 0, 0, 0, 0).AddSeconds([Math]::Floor($EachCreation_EpochSeconds));
+		<# Update the date-created timestamp/datetime on the target media file  #>
+		(Get-Item "${EachMediaFile_Fullpath}").CreationTime = ($EachCreation_DateTime);
+		<# .CreationTime=("02 June 2020 03:22:03 UTC") #>
+
+	}
+}
+
+
+
+# ------------------------------------------------------------
+#
 # REMOVE FILES WHILE FIRST VERIFYING THAT THEY EXIST
 #
 
@@ -24,6 +62,7 @@ Get-ChildItem -Path ("${Parent_Directory}") -File -Recurse -Depth (1) -Force -Er
 ;
 
 
+# ------------------------------------------------------------
 #
 # GET ALL DIFFERENT LANGUAGE-TYPES HELD WITHIN TARGET FILES
 #
@@ -34,6 +73,7 @@ Get-ChildItem -Path ("C:\Windows\System32") -File -Recurse -Depth (0) -Force -Er
 ;
 
 
+# ------------------------------------------------------------
 #
 # SHOW FILEPATHS FOR FILES BASED IN CHINESE
 #
@@ -45,7 +85,7 @@ Get-ChildItem -Path ("C:\Windows\System32") -File -Recurse -Depth (0) -Force -Er
 
 # ------------------------------------------------------------
 #
-# Ex) REMOVE ASUS' EXISTENT LEFTOVER EXECUTABLES (FROM "C:\Windows\System32")
+# EX) REMOVE ASUS' EXISTENT LEFTOVER EXECUTABLES (FROM "C:\Windows\System32")
 #
 
 Get-ChildItem -Path ("C:\Windows\System32") -File -Recurse -Depth (1) -Force -ErrorAction "SilentlyContinue" `

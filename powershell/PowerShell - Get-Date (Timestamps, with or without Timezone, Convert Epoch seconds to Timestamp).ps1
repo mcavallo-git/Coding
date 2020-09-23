0b932c947f4b058785e2185c5b126d472c71e953
Matt Cavallo <mcavallo.git@gmail.com>
2020-09-23 01:58:32 -0500

    Update PowerShell - Get-Date (Timestamps, with or without Timezone, Convert Epoch seconds to Timestamp).ps1

--- powershell/PowerShell - Get-Date (Timestamps, with or without Timezone, Convert Epoch seconds to Timestamp).ps1
+++ powershell/PowerShell - Get-Date (Timestamps, with or without Timezone, Convert Epoch seconds to Timestamp).ps1
@@ -74,6 +74,22 @@ $DecimalSec_Mod_Padded = "$(([Decimal](Get-Date -UFormat ('%s')))%1)".Substring(
 
 
 # ------------------------------------------------------------
+#
+# Timezone Conversion
+#
+
+$TZ_SOURCE = [System.TimeZoneInfo]::GetSystemTimeZones() |  Where-Object { $_.Id -match "UTC" }
+$TZ_DESTINATION = [System.TimeZoneInfo]::GetSystemTimeZones() | Where-Object { $_.Id -match "UTC" }
+
+
+$time_to_convert = '2018-12-25T00:00:00'
+[System.TimeZoneInfo]::ConvertTime($time_to_convert, $intz, $autz)
+
+
+
+
+
+# ------------------------------------------------------------
 # Relative DateTime Queries (Common English Statements)
 #   |
 #   |--> Phrases such as "this Monday" become muddled & hard to identify when "today" is a Saturday/Sunday (weekend day).             For example's sake, let's say today is Saturday - From the Saturday perspective, saying "this Monday" could equally refer to the next Monday OR the previous Monday, which, respectively, is 2 days in the future vs. 5 days in the past.             The weekend prevents simple arithmetic such as "2 days is less than 5 so 'this Monday' must refer to the next Monday" from being correct, as Saturday appears much closer to the previous week's row-of-days on a paper calendar (lending to "this" referring to "previous" - keeping both Mondays plausibly in the picture).             The outcome: The date of "this Monday" ends up referring to a date in the future while also simultaneously referring to a date in the past - Monday has now become a paradox. Monday must be resolved.             This is where the English phrases "this last Monday" and "this upcoming Monday" neatly iron-out the confusion and restore polite, colloquial conversation to a level far from paradoxical Mondays.             (Should the week begin on Saturday on paper calendars?)

29bdce15d7f0380b6a04932088facb33c1794231
Matt Cavallo <mcavallo.git@gmail.com>
2020-09-22 23:43:07 -0500

    Update Get-FileMetadata.psm1

--- powershell/_WindowsPowerShell/Modules/Get-FileMetadata/Get-FileMetadata.psm1
+++ powershell/_WindowsPowerShell/Modules/Get-FileMetadata/Get-FileMetadata.psm1
@@ -1,5 +1,11 @@
+# ------------------------------------------------------------
+<# INCLUDE THIS MODULE ON-THE-FLY:
 
 
+	$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol;	[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/Get-FileMetadata/Get-FileMetadata.psm1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+
+#>
+# ------------------------------------------------------------
 Function Get-FileMetadata {
 	<#
 		.SYNOPSIS

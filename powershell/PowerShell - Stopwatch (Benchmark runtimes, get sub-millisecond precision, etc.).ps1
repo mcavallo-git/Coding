2cc11c2b1c395d4f3e0ac5f77972f5c381024132
Matt Cavallo <mcavallo.git@gmail.com>
2020-09-28 17:12:26 -0500

    Update PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1

--- powershell/PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1
+++ powershell/PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1
@@ -1,28 +1,28 @@
 
+
+If ($True) { <# Start the benchmark, run some code, stop the benchmark, show the results #> 
+$Benchmark = New-Object System.Diagnostics.Stopwatch;
+$Benchmark.Reset(); <# Reuse same benchmark/stopwatch object by resetting it #>
+$Benchmark.Start();
+Start-Sleep -Seconds (1); <# REPLACE THIS LINE WITH COMMAND OR PROCESS TO-BENCHMARK #>
+$Benchmark.Stop();
+$Benchmark.Elapsed;
+}
+
+
+
 If ($True) {
-
-	# Start the benchmark, run some code, stop the benchmark, show the results
-	$Benchmark = New-Object System.Diagnostics.Stopwatch;
-	$Benchmark.Reset(); <# Reuse same benchmark/stopwatch object by resetting it #>
-	$Benchmark.Start();
-	Start-Sleep -Seconds (1);
-	$Benchmark.Stop();
-	$Benchmark.Elapsed;
-
-	# Start the benchmark, run some code, stop the benchmark, show the results
-	$Benchmark.Reset(); <# Reuse same benchmark/stopwatch object by resetting it #>
-	$Benchmark.Start();
-	Start-Sleep -Seconds (65);
-	$Benchmark.Stop();
-	$Benchmark | Format-List;
-
-
-	$Benchmark.IsRunning;            # Ex:  False
-	$Benchmark.Elapsed;              # Ex:  00:00:02.1047087
-	$Benchmark.ElapsedMilliseconds;  # Ex:  2104
-	$Benchmark.ElapsedTicks;         # Ex:  5343986
-	$Benchmark.Elapsed.TotalSeconds; # Ex:  90.5865709
-
+# Start the benchmark, run some code, stop the benchmark, show the results
+$Benchmark.Reset(); <# Reuse same benchmark/stopwatch object by resetting it #>
+$Benchmark.Start();
+Start-Sleep -Seconds (65);
+$Benchmark.Stop();
+$Benchmark | Format-List; <# Show detailed benchmark results #>
+# $Benchmark.IsRunning;            # Ex:  False
+# $Benchmark.Elapsed;              # Ex:  00:00:02.1047087
+# $Benchmark.ElapsedMilliseconds;  # Ex:  2104
+# $Benchmark.ElapsedTicks;         # Ex:  5343986
+# $Benchmark.Elapsed.TotalSeconds; # Ex:  90.5865709
 }
 
 

47e8e1538e4178aff7f4e0758738f79e2c3674e9
Matt Cavallo <mcavallo.git@gmail.com>
2020-10-06 02:30:34 -0500

    Update PowerShell - Set-Variable (set a variable without using dollar signs).ps1

--- powershell/PowerShell - Set-Variable (set a variable without using dollar signs).ps1
+++ powershell/PowerShell - Set-Variable (set a variable without using dollar signs).ps1
@@ -4,10 +4,10 @@
 #  |
 #  |--> Ex) Set the variable [ $IsLinux ]
 
-If ((Test-Path "/bin") -And (-Not (Test-Path "/Library"))) {
-	Set-Variable -Name "IsLinux" -Scope "Global" -Visibility "Public" -Option "ReadOnly, AllScope" -Value (1);
+If ((Test-Path '/bin') -And (-Not (Test-Path '/Library'))) {
+	Set-Variable -Name 'IsLinux' -Scope 'Global' -Visibility 'Public' -Option 'ReadOnly, AllScope' -Value (1);
 } Else {
-	Set-Variable -Name "IsLinux" -Scope "Global" -Visibility "Public" -Option "ReadOnly, AllScope" -Value (0);
+	Set-Variable -Name 'IsLinux' -Scope 'Global' -Visibility 'Public' -Option 'ReadOnly, AllScope' -Value (0);
 };
 
 
@@ -16,15 +16,43 @@ If ((Test-Path "/bin") -And (-Not (Test-Path "/Library"))) {
 #  |
 #  |--> Ex) Get the value held by variable [ $IsLinux ]
 
-(Get-Variable -Name "IsLinux").Value;
+(Get-Variable -Name 'IsLinux').Value;
+
+
+# ------------------------------------------------------------
+# Clear-Variable
+#  |
+#  |--> Deletes the value of a variable.
+#  |
+#  |--> Ex) Delete the value-of variable [ $IsLinux ]
+
+Clear-Variable -Name 'IsLinux';
+
+#  If (Get-Variable -Name 'IsLinux' -ErrorAction 'SilentlyContinue') { 1 } Else { 0 }; <-- Returns 1 because the variable still exists
+
+
+# ------------------------------------------------------------
+# Remove-Variable
+#  |
+#  |--> Deletes a variable and its value.
+#  |
+#  |--> Ex) Delete the value-of AND unset variable [ $IsLinux ]
+
+Remove-Variable -Name 'IsLinux';
+
+#  If (Get-Variable -Name 'IsLinux' -ErrorAction 'SilentlyContinue') { 1 } Else { 0 }; <-- Returns 0 because the variable no-longer exists
 
 
 # ------------------------------------------------------------
 #
 # Citation(s)
 #
-#   docs.microsoft.com  |  "Get-Variable (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-variable?view=powershell-5.1
+#   docs.microsoft.com  |  'Clear-Variable (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs'  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/clear-variable?view=powershell-5.1
 #
-#   docs.microsoft.com  |  "Set-Variable (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/set-variable?view=powershell-5.1
+#   docs.microsoft.com  |  'Get-Variable (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs'  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/remove-variable?view=powershell-5.1
+#
+#   docs.microsoft.com  |  'Remove-Variable (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs'  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/get-variable?view=powershell-5.1
+#
+#   docs.microsoft.com  |  'Set-Variable (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs'  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/set-variable?view=powershell-5.1
 #
 # ------------------------------------------------------------

d285218d76ceb0042795cd90d4c43028023aa71f
Matt Cavallo <mcavallo.git@gmail.com>
2020-10-15 12:26:38 -0400

    Update PowerShell - Strings without quotes (via Write-Output stored into variable).ps1

--- powershell/PowerShell - Strings without quotes (via Write-Output stored into variable).ps1
+++ powershell/PowerShell - Strings without quotes (via Write-Output stored into variable).ps1
@@ -1,6 +1,19 @@
 
 
-$StringVar = Write-Output *StringWithoutQuotes*;
+# Set $StringVar to contain a string-typed value of "StringWithoutQuotes"
+SV StringVar StringWithoutQuotes; <# Method 1 #>
+SV StringVar -Value (Write-Output *StringWithoutQuotes*); <# Method 2 #>
+
+
+# Set $BooleanFalse / $BooleanTrue to their respective values
+SV BooleanFalse ([Boolean](0));
+SV BooleanTrue ([Boolean](1));
+
+
+# Get the values for above variable(s)
+((GV StringVar).Value);
+((GV BooleanFalse).Value);
+((GV BooleanTrue).Value);
 
 
 # ------------------------------------------------------------

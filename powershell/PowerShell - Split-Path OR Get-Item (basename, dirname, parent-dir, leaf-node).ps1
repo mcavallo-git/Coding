c16d311ce28a85f6281fd4fd46b4242a695c1804
Matt Cavallo <mcavallo.git@gmail.com>
2020-10-15 09:39:35 -0400

    Update PowerShell - Split-Path OR Get-Item (basename, dirname, parent-dir, leaf-node).ps1

--- powershell/PowerShell - Split-Path OR Get-Item (basename, dirname, parent-dir, leaf-node).ps1
+++ powershell/PowerShell - Split-Path OR Get-Item (basename, dirname, parent-dir, leaf-node).ps1
@@ -45,12 +45,13 @@ If ($True) {
 	Write-Host "";
 	$PathItem | Format-List -Property ([String][Char]42);
 	# Write-Host "`n `${PathItem}.FullName  = `"$( ${PathItem}.FullName )`"";              <# File's Fullpath #>
-	# Write-Host "`n `${PathItem}.DirectoryName  = `"$( ${PathItem}.DirectoryName )`"";    <# Directory Fullpath #>
-	# Write-Host "`n `${PathItem}.DirectoryName  = `"$( ${PathItem}.Directory )`"";        <# Directory File-Object #>
-	# Write-Host "`n `${PathItem}.Directory.Name  = `"$( ${PathItem}.Directory.Name )`"";  <# Directory's Basename #>
 	# Write-Host "`n `${PathItem}.Name  = `"$( ${PathItem}.Name )`"";                      <# File's Basename #>
 	# Write-Host "`n `${PathItem}.Basename  = `"$( ${PathItem}.Basename )`"";              <# File's Basename w/o Extension #>
 	# Write-Host "`n `${PathItem}.Extension  = `"$( ${PathItem}.Extension )`"";            <# File's Extension #>
+	# Write-Host "`n `${PathItem}.DirectoryName  = `"$( ${PathItem}.DirectoryName )`"";    <# Directory Fullpath #>
+	# Write-Host "`n `${PathItem}.DirectoryName  = `"$( ${PathItem}.Directory )`"";        <# Directory File-Object #>
+	# Write-Host "`n `${PathItem}.Directory.Name  = `"$( ${PathItem}.Directory.Name )`"";  <# Directory's Basename #>
+	# Write-Host "`n `${PathItem}.Directory.Parent.FullName  = `"$( ${PathItem}.Directory.Parent.FullName )`"";  <# Directory's Parent-Directory's Fullpath #>
 }
 
 

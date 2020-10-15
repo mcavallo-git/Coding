4a18d8d6a3bca41c2946d304ddb7f0fc737b6570
Matt Cavallo <mcavallo.git@gmail.com>
2020-10-15 13:03:04 -0400

    Create PowerShell - String concatenation without quotes (no double quotes, no single quotes).ps1

---
+++ powershell/PowerShell - String concatenation without quotes (no double quotes, no single quotes).ps1
@@ -0,0 +1,22 @@
+
+
+Get-Date -UFormat (([String][Char]37)+([String][Char]115));
+
+
+#
+#  ^
+#  Methods are equivalent
+#  v
+#
+
+
+Get-Date -UFormat "%s";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   4sysops.com  |  "Strings in PowerShell: Quotes, formatting, and concatenation – 4sysops"  |  https://4sysops.com/archives/strings-in-powershell-quotes-formatting-and-concatenation/#string-concatenation
+#
+# ------------------------------------------------------------

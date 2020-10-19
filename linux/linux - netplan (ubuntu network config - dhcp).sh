24b7358f573b992b269d2f0cceef272375c9483e
Matt Cavallo <mcavallo.git@gmail.com>
2020-10-19 11:28:50 -0400

    Create linux - netplan (ubuntu network config - dhcp).sh

---
+++ linux/linux - netplan (ubuntu network config - dhcp).sh
@@ -0,0 +1,23 @@
+#
+# CFG="/etc/netplan/99-netplan-config.yaml"; echo "" > "${CFG}"; vi "${CFG}"; netplan apply;
+#
+
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth0:
+      addresses: [192.168.1.25/24]  # Static IPv4 192.168.1.25
+      dhcp4: no  # Do not attempt to pull a DHCP lease
+      gateway4: 192.168.1.1  # Network Gateway IPv4 192.168.1.1
+      nameservers:
+        addresses: [192.168.1.1,8.8.8.8]  # Primary DNSv4 192.168.1.1, Secondary DNSv4 8.8.8.8
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   netplan.io  |  "Netplan | Backend-agnostic network configuration in YAML"  |  https://netplan.io/reference/
+#
+# ------------------------------------------------------------

02d9cb41779f0b51f30fb999e67e3ee6414b82e7
Matt Cavallo <mcavallo.git@gmail.com>
2020-10-19 11:12:52 -0400

    Update toggle_wifi_on_off.sh

--- linux/raspi/toggle_wifi_on_off.sh
+++ linux/raspi/toggle_wifi_on_off.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+
+# ------------------------------------------------------------
+#
+#   Change the value of wifi.powersave ( located in "/etc/NetworkManager/conf.d/default-wifi-powersave-on.conf" ) from 3 (enabled) to 2 (disabled)
+# 
+
+if [ -e "/etc/NetworkManager/conf.d/default-wifi-powersave-on.conf" ]; then
+sed -i".$(date +'%Y%m%d_%H%M%S').bak" -e "/^wifi.powersave/c\wifi.powersave = 2" "/etc/NetworkManager/conf.d/default-wifi-powersave-on.conf";
+fi;
+
+
+# ------------------------------------------------------------
 
 ### Wi-Fi Disabler 24/7
 ### Add the following line of code to [ root ]'s cron-jobs (scheduled events) via [ crontab -e ]
@@ -7,3 +19,12 @@
 ### Show Network Interfaces
 # ifconfig -s | grep wlan
 # iwconfig
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   itectec.com  |  "Ubuntu – Disable wifi power management – iTecTec"  |  https://itectec.com/ubuntu/ubuntu-disable-wifi-power-management/
+#
+# ------------------------------------------------------------

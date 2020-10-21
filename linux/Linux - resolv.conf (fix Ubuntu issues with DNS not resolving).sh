549f1c86a4fa7c2a270b32889b5df7fe3b172205
Matt Cavallo <mcavallo.git@gmail.com>
2020-10-21 13:57:02 -0400

    Create Linux - resolv.conf (fix Ubuntu issues with DNS not resolving).sh

---
+++ linux/Linux - resolv.conf (fix Ubuntu issues with DNS not resolving).sh
@@ -0,0 +1,12 @@
+
+
+### REFER TO GUIDE @ https://www.linuxuprising.com/2020/07/ubuntu-how-to-free-up-port-53-used-by.html
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.linuxuprising.com  |  "Ubuntu: How To Free Up Port 53, Used By systemd-resolved - Linux Uprising Blog"  |  https://www.linuxuprising.com/2020/07/ubuntu-how-to-free-up-port-53-used-by.html
+#
+# ------------------------------------------------------------

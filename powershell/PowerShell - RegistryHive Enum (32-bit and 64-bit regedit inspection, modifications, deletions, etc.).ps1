c8e06516fdece97bc9802a3eead98320bab2050b
Matt Cavallo <mcavallo.git@gmail.com>
2020-11-18 09:25:28 -0500

    Update PowerShell - RegistryHive Enum (32-bit and 64-bit regedit inspection, modifications, deletions, etc.).ps1

--- powershell/PowerShell - RegistryHive Enum (32-bit and 64-bit regedit inspection, modifications, deletions, etc.).ps1
+++ powershell/PowerShell - RegistryHive Enum (32-bit and 64-bit regedit inspection, modifications, deletions, etc.).ps1
@@ -4,23 +4,27 @@
 
 # ------------------------------------------------------------
 #
-# Ex) Determine the currently-installed version of .NET 4.0
+# .NET Framework v4 - Simplify protocol-management (by handing off control to OS) & Enforce strong cryptography
+#   |
+#   |--> Creating these keys forces any version of .NET 4.x below 4.6.2 to use strong crypto instead of allowing SSL 3.0 by default
 #
 
-<# Locate the .NET Framework v4 key to modify #> 
 $GlobPath_DotNet4 = (Get-Item -Path 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework\v4.0*');
-$RelPath_HKLM_DotNet4 = ("SOFTWARE\Microsoft\.NETFramework\$($GlobPath_DotNet4.PSChildName)");
+$RelPath_HKLM_DotNet4 = ("SOFTWARE\Microsoft\.NETFramework\$($GlobPath_DotNet4.PSChildName)"); <# Locate the .NET Framework v4 key to modify #> 
 
 <# Update the 64-bit registry #>
 $Registry_HKLM_64bit = ([Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry64));  <# Methods which update registry keys such as  [ New-ItemProperty ... ]  often only update the 64bit registry (by default) #>
 $SubKey_DotNet4_64bit = $Registry_HKLM_64bit.OpenSubKey("${RelPath_HKLM_DotNet4}", $True);  <# Retrieve the specified subkey for read/write access (argument #2 == $True) #>
-$SubKey_DotNet4_64bit.SetValue("SystemDefaultTlsVersions", 1, 4);  <# Update the key #> <# DWords' native RegistryValueKind is 4 #>
+<# Note: The third argument passed to '.SetValue()' is the 'RegistryValueKind' --> A value of '4' creates/sets a 'DWORD' typed property #>
+$SubKey_DotNet4_64bit.SetValue("SystemDefaultTlsVersions", 1, 4);  <# Allow the operating system to control the networking protocol used by apps (which run on this version of the .NET Framework) #>
+$SubKey_DotNet4_64bit.SetValue("SchUseStrongCrypto", 1, 4);  <# Enforce strong cryptography, using more secure network protocols (TLS 1.2, TLS 1.1, and TLS 1.0) and blocking protocols that are not secure #>
 $SubKey_DotNet4_64bit.Close();  <# Close the key & flush it to disk (if its contents have been modified) #>
 
 <# Update the 32-bit registry #>
 $Registry_HKLM_32bit = ([Microsoft.Win32.RegistryKey]::OpenBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, [Microsoft.Win32.RegistryView]::Registry32));  <# Methods which update registry keys such as  [ New-ItemProperty ... ]  often only update the 64bit registry (by default) #>
 $SubKey_DotNet4_32bit = $Registry_HKLM_32bit.OpenSubKey("${RelPath_HKLM_DotNet4}", $True);  <# Retrieve the specified subkey for read/write access (argument #2 == $True) #>
-$SubKey_DotNet4_32bit.SetValue("SystemDefaultTlsVersions", 1, 4);  <# Update the key #> <# DWords' native RegistryValueKind is 4 #>
+$SubKey_DotNet4_32bit.SetValue("SystemDefaultTlsVersions", 1, 4);  <# Allow the operating system to control the networking protocol used by apps (which run on this version of the .NET Framework) #>
+$SubKey_DotNet4_32bit.SetValue("SchUseStrongCrypto", 1, 4);  <# Enforce strong cryptography, using more secure network protocols (TLS 1.2, TLS 1.1, and TLS 1.0) and blocking protocols that are not secure #>
 $SubKey_DotNet4_32bit.Close();  <# Close the key & flush it to disk (if its contents have been modified) #>
 
 

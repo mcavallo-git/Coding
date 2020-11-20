5b167fdf7e6afd073a8ab6f28abb01fb8b6813b1
Matt Cavallo <mcavallo.git@gmail.com>
2020-11-19 23:22:19 -0500

    Update PowerShell - Determine Windows ISO's version number.ps1

--- powershell/PowerShell - Determine Windows ISO's version number.ps1
+++ powershell/PowerShell - Determine Windows ISO's version number.ps1
@@ -10,46 +10,48 @@ If ($False) {
 }
 # ------------------------------------------------------------
 
-$ISO_Fullpath = "${Home}\Desktop\Windows.iso";
-$MountDir = "${Home}\Desktop\Mount";
+If ($True) {
 
-Set-Variable -Name "DriveLetter" -Scope "Script" -Value "";
-$Possible_DriveLetters = @("E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-
-ForEach ($EachDriveLetter In (${Possible_DriveLetters})) {
-	If ((Test-Path -Path ("${EachDriveLetter}:\")) -Eq $False) {
-		$DriveLetter = "${EachDriveLetter}";
-		Break;
+	$ISO_Fullpath = "${Home}\Desktop\Windows.iso";
+	$MountDir = "${Home}\Desktop\Mount";
+	$Possible_DriveLetters = @("E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+	ForEach ($EachDriveLetter In (${Possible_DriveLetters})) {
+		If ((Test-Path -Path ("${EachDriveLetter}:\")) -Eq $False) {
+			Set-Variable -Name "DriveLetter" -Scope "Script" -Value "${EachDriveLetter}";
+			Break;
+		};
 	};
-};
 
-$Mounted_ISO = Mount-DiskImage -ImagePath ("${ISO_Fullpath}");
+	$Mounted_ISO = Mount-DiskImage -ImagePath ("${ISO_Fullpath}");
 
-If ((Test-Path ("${MountDir}")) -Eq $False) {
-New-Item -ItemType ("Directory") -Path ("${MountDir}") | Out-Null;
-Start-Sleep -Seconds (1);
-};
+	If ((Test-Path ("${MountDir}")) -Eq $False) {
+	New-Item -ItemType ("Directory") -Path ("${MountDir}") | Out-Null;
+	Start-Sleep -Seconds (1);
+	};
 
-Write-Host "`${DriveLetter} = [${DriveLetter}]";
 
-<# Get the version # of Windows (stored within the .iso file) #>
-$Install_Esd_MountPath = ( "${DriveLetter}:\sources\install.esd" );
-$Install_Esd_Desktop = ( "${Home}\Desktop\install.esd" );
+	<# Get the version # of Windows (stored within the .iso file) #>
+	$Install_Esd_MountPath = ( "${DriveLetter}:\sources\install.esd" );
+	$Install_Esd_Desktop = ( "${Home}\Desktop\install.esd" );
 
-$DISM_Info = (DISM /Get-WimInfo /WimFile:${Install_Esd_MountPath} /index:0);
+	$DISM_Info = (DISM /Get-WimInfo /WimFile:${Install_Esd_MountPath} /index:0);
 
-$Regex_Win10_VersionNum = "Version\s*:\s*[\d]+\.[\d]+\.[\d]+\.[\d]+\s*";
-$ISO_VersionNumber = ((((${DISM_Info} -match ${Regex_Win10_VersionNum}) -Replace "Version","") -Replace ":","") -Replace " ","");
-Write-Host "Version Number (Windows ISO):   !!!   ${ISO_VersionNumber}   !!!";
+	$Regex_Win10_VersionNum = "Version\s*:\s*[\d]+\.[\d]+\.[\d]+\.[\d]+\s*";
+	$ISO_VersionNumber = ((((${DISM_Info} -match ${Regex_Win10_VersionNum}) -Replace "Version","") -Replace ":","") -Replace " ","");
+	Write-Host "";
+	Write-Host "Version Number (Windows ISO):";
+	Write-Host "";
+	Write-Host "${ISO_VersionNumber}";
+	Write-Host "";
 
-Start-Sleep -Seconds (1);
+	${Mounted_ISO} | Dismount-DiskImage | Out-Null;
 
-${Mounted_ISO} | Dismount-DiskImage | Out-Null;
+	<# Copy-Item ("${Install_Esd_MountPath}") ("${Install_Esd_Desktop}") -Force; #>
+	<# Copy-Item ("${DriveLetter}:\*") ("${MountDir}\") -Recurse -Force; #>
+	<# $Install_Wim = "${MountDir}\sources\install.wim"; #>
+	<# $Install_Esd = "${MountDir}\sources\install.esd"; #>
 
-<# Copy-Item ("${Install_Esd_MountPath}") ("${Install_Esd_Desktop}") -Force; #>
-<# Copy-Item ("${DriveLetter}:\*") ("${MountDir}\") -Recurse -Force; #>
-<# $Install_Wim = "${MountDir}\sources\install.wim"; #>
-<# $Install_Esd = "${MountDir}\sources\install.esd"; #>
+}
 
 
 # ------------------------------------------------------------

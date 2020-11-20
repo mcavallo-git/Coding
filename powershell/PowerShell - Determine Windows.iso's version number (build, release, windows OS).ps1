0a98a33d044aa65e1faffcd1060d6b46a3a96886
Matt Cavallo <mcavallo.git@gmail.com>
2020-11-19 23:37:44 -0500

    Update PowerShell - Determine Windows.iso's version number (build, release, windows OS).ps1

--- powershell/PowerShell - Determine Windows.iso's version number (build, release, windows OS).ps1
+++ powershell/PowerShell - Determine Windows.iso's version number (build, release, windows OS).ps1
@@ -13,7 +13,7 @@ If ($False) {
 If ($True) {
 
 	$ISO_Fullpath = "${Home}\Desktop\Windows.iso";
-	$MountDir = "${Home}\Desktop\Mount";
+
 	$Possible_DriveLetters = @("E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 	ForEach ($EachDriveLetter In (${Possible_DriveLetters})) {
 		If ((Test-Path -Path ("${EachDriveLetter}:\")) -Eq $False) {
@@ -24,31 +24,17 @@ If ($True) {
 
 	$Mounted_ISO = Mount-DiskImage -ImagePath ("${ISO_Fullpath}");
 
-	If ((Test-Path ("${MountDir}")) -Eq $False) {
-	New-Item -ItemType ("Directory") -Path ("${MountDir}") | Out-Null;
-	Start-Sleep -Seconds (1);
-	};
-
-
 	<# Get the version # of Windows (stored within the .iso file) #>
 	$Install_Esd_MountPath = ( "${DriveLetter}:\sources\install.esd" );
-	$Install_Esd_Desktop = ( "${Home}\Desktop\install.esd" );
 
 	$DISM_Info = (DISM /Get-WimInfo /WimFile:${Install_Esd_MountPath} /index:0);
 
 	$Regex_Win10_VersionNum = "Version\s*:\s*[\d]+\.[\d]+\.[\d]+\.[\d]+\s*";
 	$ISO_VersionNumber = ((((${DISM_Info} -match ${Regex_Win10_VersionNum}) -Replace "Version","") -Replace ":","") -Replace " ","");
-	Write-Host "";
-	Write-Host "Version Number (Windows ISO):";
-	Write-Host "${ISO_VersionNumber}";
-	Write-Host "";
+
+	Write-Output "${ISO_VersionNumber}";
 
 	${Mounted_ISO} | Dismount-DiskImage | Out-Null;
-
-	<# Copy-Item ("${Install_Esd_MountPath}") ("${Install_Esd_Desktop}") -Force; #>
-	<# Copy-Item ("${DriveLetter}:\*") ("${MountDir}\") -Recurse -Force; #>
-	<# $Install_Wim = "${MountDir}\sources\install.wim"; #>
-	<# $Install_Esd = "${MountDir}\sources\install.esd"; #>
 
 }
 

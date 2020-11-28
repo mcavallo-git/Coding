56e3926af48b9040a326621a51959650212d4d2b
Matt Cavallo <mcavallo.git@gmail.com>
2020-11-28 13:35:58 -0500

    Update EdgeOS (UniFi) - config.boot (copy to home directory for quick-exports).sh

--- ubiquiti/EdgeOS (UniFi) - config.boot (copy to home directory for quick-exports).sh
+++ ubiquiti/EdgeOS (UniFi) - config.boot (copy to home directory for quick-exports).sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# SSH into target Ubiquiti UniFi router
+
+
 # Copy the target UniFi device's "config.boot" config file to the current user's home-dir
 # cat "/config/config.boot" > "${HOME}/config.$(date +'%Y%m%d_%H%M%S').$(hostname).boot";
 show configuration > "${HOME}/config.$(date +'%Y%m%d_%H%M%S').$(hostname).boot";
 
+
 # SFTP into the server and download the file, then delete the copy from the associated UniFi device's user-homedir
+

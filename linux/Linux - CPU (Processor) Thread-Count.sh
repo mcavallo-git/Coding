f7214e102ffcb53dfb99ff712c669d4583b11f78
Matt Cavallo <mcavallo.git@gmail.com>
2021-02-10 00:38:43 -0500

    Update Linux - CPU (Processor) Thread-Count.sh

--- linux/Linux - CPU (Processor) Thread-Count.sh
+++ linux/Linux - CPU (Processor) Thread-Count.sh
@@ -8,10 +8,10 @@ cat /proc/cpuinfo;
 cat /proc/meminfo; # (if you want memory stats)
 
 
-
+# ------------------------------------------------------------
 #
 # Citation(s)
 #
-#		Thanks to stackexchange user "htaccess" on the forum "https://unix.stackexchange.com/questions/218074"
+#   unix.stackexchange.com  |  "cpu - How to know number of cores of a system in Linux? - Unix & Linux Stack Exchange"  |  https://unix.stackexchange.com/a/279354
 #
-#
+# ------------------------------------------------------------

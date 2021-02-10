2d62c593bb55e3a4147703d7f134b04fca7b479e
Matt Cavallo <mcavallo.git@gmail.com>
2021-02-10 00:49:40 -0500

    Update Linux - du -sh (inspect disk space usage).sh

--- linux/Linux - du -sh (inspect disk space usage).sh
+++ linux/Linux - du -sh (inspect disk space usage).sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+# ------------------------------------------------------------
+#
+#  List files/directores sorted by filesize
+#
 
-cd /
+cd "/var/log";
+du --max-depth=0 "$(pwd)/"* | sort --numeric-sort 2> '/dev/null';
 
-sudo du -u --max-depth=1;
+
+# ------------------------------------------------------------
+#
+# Determining what is taking up so much disk space on a Linux box, manually
+#
+
+cd "/";
+du --max-depth=0 "$(pwd)/"* | sort --numeric-sort 2> '/dev/null';
+
+# |--> NEXT STEP:   look at bottom of list to determine which directories need to be delved into deeper
+
+# |--> NEXT STEP:   recursively go into large directories and perform same "du ..." command until you locate files you feel are in the clear to removed
+
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   man7.org  |  "sort(1) - Linux manual page"  |  https://man7.org/linux/man-pages/man1/sort.1.html
+#
+# ------------------------------------------------------------

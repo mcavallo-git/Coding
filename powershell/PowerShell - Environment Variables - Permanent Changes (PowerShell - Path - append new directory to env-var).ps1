31cdaea8a5b0d22682308b8e8e9ff1c0aeb6dd5e
Matt Cavallo <mcavallo.git@gmail.com>
2021-02-25 08:24:44 -0500

    Update PowerShell - Environment Variables - Permanent Changes (PowerShell - Path - append new directory to env-var).ps1

--- powershell/PowerShell - Environment Variables - Permanent Changes (PowerShell - Path - append new directory to env-var).ps1
+++ powershell/PowerShell - Environment Variables - Permanent Changes (PowerShell - Path - append new directory to env-var).ps1
@@ -3,7 +3,7 @@
 # Show Environment Variables (one-liner)
 #
 
-If(($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) { $rawUI=$Host.UI.RawUI; $oldSize=$rawUI.BufferSize; $typeName=$oldSize.GetType( ).FullName; $newSize=New-Object $typeName (16384, $oldSize.Height); $rawUI.BufferSize=$newSize; }; Get-ChildItem Env: | Format-List;  (${Env:Path}).Split(';'); 
+If(($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) { $rawUI=$Host.UI.RawUI; $oldSize=$rawUI.BufferSize; $typeName=$oldSize.GetType( ).FullName; $newSize=New-Object $typeName (16384, $oldSize.Height); $rawUI.BufferSize=$newSize; }; Get-ChildItem Env: | Format-List;  (${Env:Path}).Split([String][Char]59); 
 
 
 # ------------------------------------------------------------
@@ -23,8 +23,8 @@ If(($Host) -And ($Host.UI) -And ($Host.UI.RawUI)) {
 <# Get Environment Variables #> 
 Get-ChildItem Env: | Format-List; 
 
-<# Get the PATH environment variable, using semi-colon delimitation #> 
-(${Env:Path}).Split(';'); 
+<# Get the PATH environment variable, using semi-colon delimitation  -  Note:  [String][Char]59 === ";" (one semicolon) #> 
+(${Env:Path}).Split([String][Char]59);
 
 #
 # >> SHOW ENVIRONMENT VARIABLES  <<
@@ -33,23 +33,23 @@ Get-ChildItem Env: | Format-List;
 #
 # Show all PATH items (to the console)
 #
-(${Env:Path}).Split(';');
+(${Env:Path}).Split([String][Char]59);
 
 
 # Get the current Environment "Path" Directories (Combined System + User Dirs) & store them in the variable $EnvPath
-$EnvPath = (${Env:Path}).Split(';');
+$EnvPath = (${Env:Path}).Split([String][Char]59);
 
 
 # Search the PATH for results matching a given string
-(${Env:Path}).Split(';') | Select-String 'git'; <# Non-exact matching #>
-(${Env:Path}).Split(';') | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" } | ForEach-Object { $_ }; <# Exact matching #>
-((${Env:Path}).Split(';') | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" }).Count; <# Count the number of pre-existing exact matches #>
+(${Env:Path}).Split([String][Char]59) | Select-String 'git'; <# Non-exact matching #>
+(${Env:Path}).Split([String][Char]59) | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" } | ForEach-Object { $_ }; <# Exact matching #>
+((${Env:Path}).Split([String][Char]59) | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" }).Count; <# Count the number of pre-existing exact matches #>
 
 
 # Aappend to  > >  USER environment variable  < <  Permanently add a directory to the user's path (doesn't apply to other users on the same system)
 $AppendPath = "C:\Program Files (x86)\VMware\VMware Workstation"; `
 $UserPath = ((Get-ItemProperty -Path 'Registry::HKEY_CURRENT_USER\Environment').Path);
-If (((${Env:Path}).Split(';') | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" }).Count -Eq 0) {
+If (((${Env:Path}).Split([String][Char]59) | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" }).Count -Eq 0) {
 	[System.Environment]::SetEnvironmentVariable("Path","${UserPath};${AppendPath}",[System.EnvironmentVariableTarget]::User);
 }
 
@@ -57,7 +57,7 @@ If (((${Env:Path}).Split(';') | Where-Object { $_ -Eq "C:\Program Files\Git\cmd"
 # Append to  > >  SYSTEM environment variable  < <  Permanently add a directory to the system path (applies to all users on the same system)
 $AppendPath = "C:\Program Files (x86)\VMware\VMware Workstation"; `
 $SystemPath = ((Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment').Path);
-If (((${Env:Path}).Split(';') | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" }).Count -Eq 0) {
+If (((${Env:Path}).Split([String][Char]59) | Where-Object { $_ -Eq "C:\Program Files\Git\cmd" }).Count -Eq 0) {
 	[System.Environment]::SetEnvironmentVariable("Path","${SystemPath};${AppendPath}",[System.EnvironmentVariableTarget]::Machine);
 }
 

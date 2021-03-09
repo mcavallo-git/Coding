471f4431442b9c07ce0526475a07c7e562f65891
Matt Cavallo <mcavallo.git@gmail.com>
2021-03-09 02:57:09 -0500

    rm

--- powershell/PowerShell - New-Item, New-ItemProperty, Set-ItemProperty (set values for registry keys & their properties).ps1
+++ powershell/PowerShell - New-Item, New-ItemProperty, Set-ItemProperty (set values for registry keys & their properties).ps1
@@ -11,18 +11,31 @@ New-Item -ItemType "Directory" -Path ("${Home}\Desktop\ExampleDirectory_$(Get-Da
 # PowerShell - Create Registry-Keys (and their Properties) using [ New-Item & Set-ItemProperty ]
 #
 
+
 #
-### Creating Registry keys using  [ New-Item -Force ... ]
+# Creating Registry keys using  [ New-Item -Force ... ]
 #   |--> Upside - Creates ALL parent registry keys
 #   |--> Downside - DELETES all properties & child-keys if key already exists
 #   |--> Takeaway - Always use  [ Test-Path ... ]  to verify registry keys don't exist before using  [ New-Item -Force ... ]  to create the key
 #
-
 If ((Test-Path -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced')) -Eq $False) {
 	### Explorer Settings - Setting to [ 0 ] selects "Show hidden files, folders, and drives", setting to [ 1 ] selects "Don't show hidden files, folders, or drives"
 	New-Item -Force -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced') | Out-Null;
 }
 Set-ItemProperty -Force -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced') -Name ('Hidden') -Value (0) | Out-Null;
+
+
+#
+# Creating Registry key properties using  [ New-ItemProperty -Force ... ]
+#
+$EachProp = @{
+	Path="Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
+	Description="Show or hide seconds on the system tray clock. 0=[Hide], 1=[Show]";
+	Name="ShowSecondsInSystemClock";
+	Type="DWord";
+	Value=1;
+};
+New-ItemProperty -Force -LiteralPath ($EachProp.Path) -Name ($EachProp.Name) -PropertyType ($EachProp.Type) -Value ($EachProp.Value) | Out-Null;
 
 
 # ------------------------------------------------------------

f544ba82fd68f6ebc2d322a0677e6178962f526b
Matt Cavallo <mcavallo.git@gmail.com>
2021-03-09 13:05:08 -0500

    Update PowerShell - Try {...} Catch {...} Finally {...}.ps1

--- powershell/PowerShell - Try {...} Catch {...} Finally {...}.ps1
+++ powershell/PowerShell - Try {...} Catch {...} Finally {...}.ps1
@@ -5,11 +5,50 @@
 #
 # ------------------------------------------------------------
 
+If ($True) {
+	# Enchance PowerShell's default error output
+	Try {
+		# Write-Host "[--- TRY BLOCK - BEGIN ---]" -ForegroundColor ("DarkGray");
+		Example_InvalidCommand;
+		# Write-Host "[--- TRY BLOCK - END ---]" -ForegroundColor ("DarkGray");
+	} Catch {
+		Write-Host "[--- CATCH BLOCK - BEGIN SHOWING EXCEPTION ---]" -ForegroundColor ("Red");
+		Write-Host "";
+		Write-Host "`$_.TargetObject:";
+			Write-Host "$($_.TargetObject)" -ForegroundColor ("Yellow");
+		Write-Host "";
+		Write-Host "`$_.Exception.Message:";
+			Write-Host "$($_.Exception.Message)" -ForegroundColor ("Yellow");
+		Write-Host "";
+		Write-Host "`$_.InvocationInfo.PositionMessage:";
+			Write-Host "$($_.InvocationInfo.PositionMessage)" -ForegroundColor ("Yellow");
+		Write-Host "";
+		Write-Host "`$_.CategoryInfo:";
+			Write-Host "$($_.CategoryInfo)" -ForegroundColor ("Yellow");
+		Write-Host "";
+		Write-Host "`$_.FullyQualifiedErrorId:";
+			Write-Host "$($_.FullyQualifiedErrorId)" -ForegroundColor ("Yellow");
+		Write-Host "";
+		Write-Host "`$_.Exception.StackTrace:";
+			Write-Host "$($_.Exception.StackTrace)" -ForegroundColor ("Yellow");
+		Write-Host "";
+		Write-Host "[--- CATCH BLOCK - END SHOWING EXCEPTION ---]" -ForegroundColor ("Red");
+	} Finally {
+		# Write-Host "[--- FINALLY BLOCK - BEGIN ---]" -ForegroundColor ("DarkGray");
+		# Write-Host "[--- FINALLY BLOCK - END ---]" -ForegroundColor ("DarkGray");
+	}
+}
+
+
+# ------------------------------------------------------------
+
+# Example - Simple check of a given property on a registry key
+
 Try {
 	$RegistryProp = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\" | Select-Object -ExpandProperty "Version" -ErrorAction Stop;
 } Catch {
 	$RegistryProp = $Null;
-  Write-Host -NoNewLine "An error occurred: ";
+  Write-Host "An error occurred: ";
   Write-Host $_ -BackgroundColor "Black" -ForegroundColor "Yellow";
 } Finally {
 	If ($RegistryProp -NE $Null) {
@@ -27,5 +66,7 @@ Try {
 #   docs.microsoft.com  |  "About Try Catch Finally"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_try_catch_finally
 #
 #   jonathanmedd.net  |  "Testing for the Presence of a Registry Key and Value"  |  https://www.jonathanmedd.net/2014/02/testing-for-the-presence-of-a-registry-key-and-value.html
+#
+#   stackoverflow.com  |  "exception - Printing the error in try catch block in powershell - Stack Overflow"  |  https://stackoverflow.com/q/20619661
 #
 # ------------------------------------------------------------

9b3161fab3cacfef2a322164ecd8c43d47edb24a
Matt Cavallo <mcavallo.git@gmail.com>
2021-03-09 13:08:20 -0500

    Update PowerShell - New-Item, New-ItemProperty, Set-ItemProperty (set values for registry keys & their properties).ps1

--- powershell/PowerShell - New-Item, New-ItemProperty, Set-ItemProperty (set values for registry keys & their properties).ps1
+++ powershell/PowerShell - New-Item, New-ItemProperty, Set-ItemProperty (set values for registry keys & their properties).ps1
@@ -1,32 +1,21 @@
 # ------------------------------------------------------------
 #
-# PowerShell - Create a directory
+# PowerShell - New-Item
 #
 
+#
+# Ex)  Create a directory
+#
 New-Item -ItemType "Directory" -Path ("${Home}\Desktop\ExampleDirectory_$(Get-Date -UFormat '%Y%m%d-%H%M%S')") | Out-Null;
 
 
 # ------------------------------------------------------------
 #
-# PowerShell - Create Registry-Keys (and their Properties) using [ New-Item & Set-ItemProperty ]
+# PowerShell - New-ItemProperty
 #
 
-
 #
-# Creating Registry keys using  [ New-Item -Force ... ]
-#   |--> Upside - Creates ALL parent registry keys
-#   |--> Downside - DELETES all properties & child-keys if key already exists
-#   |--> Takeaway - Always use  [ Test-Path ... ]  to verify registry keys don't exist before using  [ New-Item -Force ... ]  to create the key
-#
-If ((Test-Path -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced')) -Eq $False) {
-	### Explorer Settings - Setting to [ 0 ] selects "Show hidden files, folders, and drives", setting to [ 1 ] selects "Don't show hidden files, folders, or drives"
-	New-Item -Force -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced') | Out-Null;
-}
-Set-ItemProperty -Force -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced') -Name ('Hidden') -Value (0) | Out-Null;
-
-
-#
-# Creating Registry key properties using  [ New-ItemProperty -Force ... ]
+# Ex)  Create registry key properties
 #
 $EachProp = @{
 	Path="Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced";
@@ -36,6 +25,25 @@ $EachProp = @{
 	Value=1;
 };
 New-ItemProperty -Force -LiteralPath ($EachProp.Path) -Name ($EachProp.Name) -PropertyType ($EachProp.Type) -Value ($EachProp.Value) | Out-Null;
+
+
+# ------------------------------------------------------------
+#
+# PowerShell - Set-ItemProperty
+#
+
+# Ex)  Create registry keys & set registry key properties
+#
+# Creating Registry keys using  [ New-Item -Force ... ]
+#   |--> Pros  : Creates ALL parent registry keys
+#   |--> Cons  : DELETES all properties & child-keys if key already exists
+#   |--> TL;DR : Always use  [ Test-Path ... ]  to verify registry keys don't exist before using  [ New-Item -Force ... ]  to create the key
+#
+If ((Test-Path -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced')) -Eq $False) {
+	### Explorer Settings - Setting to [ 0 ] selects "Show hidden files, folders, and drives", setting to [ 1 ] selects "Don't show hidden files, folders, or drives"
+	New-Item -Force -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced') | Out-Null;
+}
+Set-ItemProperty -Force -Path ('Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced') -Name ('Hidden') -Value (0) | Out-Null;
 
 
 # ------------------------------------------------------------

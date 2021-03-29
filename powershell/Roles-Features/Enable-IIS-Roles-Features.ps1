c936a1ac3e42716ab2c2ab051a753fa96250bd2b
Matt Cavallo <mcavallo.git@gmail.com>
2021-03-29 17:10:32 -0400

    Update Enable-IIS-Roles-Features.ps1

--- powershell/Roles-Features/Enable-IIS-Roles-Features.ps1
+++ powershell/Roles-Features/Enable-IIS-Roles-Features.ps1
@@ -3,7 +3,13 @@
 <#    Install required roles & features for IIS (Web) Server    #>
 <#                                                              #>
 <# ------------------------------------------------------------ #>
+If ($False) { <# RUN THIS SCRIPT REMOTELY #>
 
+
+$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; Clear-DnsClientCache; Set-ExecutionPolicy "ByPass" -Scope "CurrentUser" -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/Roles-Features/Enable-IIS-Roles-Features.ps1')); [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+
+
+}
 <# Install roles #>
 $Features_ToInstall = ((Get-WindowsFeature | Where-Object { ($_.Name -NE $Null) -And (@("FileAndStorage-Services", "File-Services", "FS-FileServer", "FS-SMB1", "NET-Framework-45-ASPNET", "NET-Framework-45-Core", "NET-Framework-45-Features", "NET-Framework-Core", "NET-Framework-Features", "NET-WCF-HTTP-Activation45", "NET-WCF-Pipe-Activation45", "NET-WCF-Services45", "NET-WCF-TCP-Activation45", "NET-WCF-TCP-PortSharing45", "PowerShell", "PowerShell-ISE", "PowerShellRoot", "PowerShell-V2", "RSAT", "RSAT-Feature-Tools", "RSAT-SMTP", "Server-Gui-Mgmt-Infra", "Server-Gui-Shell", "SMTP-Server", "SNMP-Service", "SNMP-WMI-Provider", "Storage-Services", "Telnet-Client", "User-Interfaces-Infra", "WAS", "WAS-Config-APIs", "WAS-Process-Model", "Web-App-Dev", "Web-ASP", "Web-Asp-Net", "Web-Asp-Net45", "Web-Common-Http", "Web-Custom-Logging", "Web-DAV-Publishing", "Web-Default-Doc", "Web-Dir-Browsing", "Web-Filtering", "Web-Health", "Web-Http-Errors", "Web-Http-Logging", "Web-Http-Redirect", "Web-Http-Tracing", "Web-IP-Security", "Web-ISAPI-Ext", "Web-ISAPI-Filter", "Web-Lgcy-Mgmt-Console", "Web-Log-Libraries", "Web-Metabase", "Web-Mgmt-Compat", "Web-Mgmt-Console", "Web-Mgmt-Service", "Web-Mgmt-Tools", "Web-Net-Ext", "Web-Net-Ext45", "Web-ODBC-Logging", "Web-Performance", "Web-Security", "Web-Server", "Web-Stat-Compression", "Web-Static-Content", "Web-WebServer", "WoW64-Support").Contains($_.Name) -Eq $True) } | Where-Object { ( $_.Installed -Match "False" ) } | Select-Object -Property ("Name")).Name);
 If (${Features_ToInstall} -NE $Null) {

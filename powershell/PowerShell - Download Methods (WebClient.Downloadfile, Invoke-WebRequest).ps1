9c27480c558fc301124d55d219b2fde067ab2880
Matt Cavallo <mcavallo.git@gmail.com>
2021-04-18 22:03:31 -0400

    Update PowerShell - Download Methods (WebClient.Downloadfile, Invoke-WebRequest).ps1

--- powershell/PowerShell - Download Methods (WebClient.Downloadfile, Invoke-WebRequest).ps1
+++ powershell/PowerShell - Download Methods (WebClient.Downloadfile, Invoke-WebRequest).ps1
@@ -27,14 +27,45 @@ $ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.Servic
 # Invoke-WebRequest
 #   |
 #   |--> Has pre-built-in timeout parameter [ -TimeoutSec ]
+#   |
+#   |--> Make sure to set preference variable [ $ProgressPreference='SilentlyContinue'; ] <-- Hides Invoke-WebRequest's progress bar, increasing speed by 10x or more #>
 #
 
+# Example 1 - Download Azure Pipelines Agent (for Azure DevOps, previously VSTS, previously previously VSO)
+If ($True) {
+ 
+# Setup Runtime vars for remote URI(s) && local filepath(s)
+$URL_AgentZip='https://vstsagentpackage.azureedge.net/agent/2.184.2/vsts-agent-win-x64-2.184.2.zip';
+$FullPath_WorkingDir = ((${Home})+('\Downloads\agent'));
+$FullPath_AgentZip=((${FullPath_WorkingDir})+('\')+(Split-Path -Path (${URL_AgentZip}) -Leaf));
+ 
+# Ensure the working directory exists
+New-Item -ItemType 'Directory' -Path (${FullPath_WorkingDir}) | Out-Null;
+Set-Location -Path (${FullPath_WorkingDir});
+ 
+# Download the pipeline agent's zip archive
+$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol;
+[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12;
+$ProgressPreference='SilentlyContinue'; <# Hide Invoke-WebRequest's progress bar #>
+Measure-Command { Invoke-WebRequest -Uri (${URL_AgentZip}) -OutFile ("${FullPath_AgentZip}") -TimeoutSec (15) };
+[System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+ 
+# Extract the pipeline agent's zip archive
+Add-Type -AssemblyName ('System.IO.Compression.FileSystem');
+[System.IO.Compression.ZipFile]::ExtractToDirectory((${FullPath_AgentZip}),(${FullPath_WorkingDir}));
+ 
+}
+
+
+# Example 2 - Download Notepad Replacer
 Invoke-WebRequest -Uri ("https://www.binaryfortress.com/Data/Download/?package=notepadreplacer") -OutFile ("${Home}\Downloads\NotepadReplacerSetup.exe") -TimeoutSec (7.5);
 
 
 # ------------------------------------------------------------
 #
 # Citation(s)
+#
+#   docs.microsoft.com  |  "about_Preference_Variables - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-5.1#progresspreference
 #
 #   docs.microsoft.com  |  "Invoke-WebRequest (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest?view=powershell-5.1
 #

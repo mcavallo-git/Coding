627165a01d77ae6baccb084035742d9bc5e00877
Matt Cavallo <mcavallo.git@gmail.com>
2021-04-27 17:35:01 -0400

    Create GetTimestamp.psm1

---
+++ powershell/_WindowsPowerShell/Modules/GetTimestamp/GetTimestamp.psm1
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------
+#
+# @ GetTimestamp
+#   |--> Get the current datetime as a string
+#
+# ------------------------------------------------------------
+Function GetTimestamp {
+	Param()
+	# ------------------------------------------------------------
+	If ($False) { # RUN THIS SCRIPT:
+
+		$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; $ProgressPreference='SilentlyContinue'; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Try { Invoke-Expression ((Invoke-WebRequest -TimeoutSec (7.5) -Uri ('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/GetTimestamp/GetTimestamp.psm1') ).Content) } Catch {}; If (-Not (Get-Command -Name 'GetTimestamp' -ErrorAction 'SilentlyContinue')) { Import-Module ([String]::Format('{0}\Documents\GitHub\Coding\powershell\_WindowsPowerShell\Modules\GetTimestamp\GetTimestamp.psm1', ((Get-Variable -Name 'HOME').Value))); }; [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
+
+	}
+	# ------------------------------------------------------------
+
+	Return (([String](Get-Date -Date ((New-Object -Type DateTime -ArgumentList 1970,1,1,0,0,0,0).AddSeconds([Math]::Floor($([Decimal](Get-Date -UFormat ("%s")))))) -UFormat ("%Y-%m-%dT%H:%M:%S")))+(([String](($([Decimal](Get-Date -UFormat ("%s")))%1))).Substring(1).PadRight(6,"0"))+(Get-Date -UFormat ("%Z")));
+
+	# ------------------------------------------------------------
+}
+
+<# Only export the module if the caller is attempting to import it #>
+If (($MyInvocation.GetType()) -Eq ("System.Management.Automation.InvocationInfo")) {
+	Export-ModuleMember -Function "GetTimestamp";
+}
+
+
+# ------------------------------------------------------------

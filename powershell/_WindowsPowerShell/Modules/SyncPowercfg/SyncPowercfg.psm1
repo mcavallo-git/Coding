98d70cedce0526f3ac5e227b8de1d2cb22f0978d
Matt Cavallo <mcavallo.git@gmail.com>
2021-05-05 15:48:46 -0400

    Update SyncPowercfg.psm1

--- powershell/_WindowsPowerShell/Modules/SyncPowercfg/SyncPowercfg.psm1
+++ powershell/_WindowsPowerShell/Modules/SyncPowercfg/SyncPowercfg.psm1
@@ -294,6 +294,7 @@ Function SyncPowercfg() {
 	Return;
 
 }
+
 <# Only export the module if the caller is attempting to import it #>
 If (($MyInvocation.GetType()) -Eq ("System.Management.Automation.InvocationInfo")) {
 	Export-ModuleMember -Function "SyncPowercfg";

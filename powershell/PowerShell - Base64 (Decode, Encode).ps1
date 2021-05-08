49678e3a1f04c76789d854dfd79aec17917a85d1
Matt Cavallo <mcavallo.git@gmail.com>
2021-05-07 21:57:59 -0400

    Update PowerShell - Base64 (Decode, Encode).ps1

--- powershell/PowerShell - Base64 (Decode, Encode).ps1
+++ powershell/PowerShell - Base64 (Decode, Encode).ps1
@@ -1,14 +1,21 @@
 
+
 #  ENCODE  :::  Base64 - Encode/Encoding a String (via PowerShell):
 $secret_plaintext = "Some secret, i.e. a password, token, etc.";
-$secret_base64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes(${secret_plaintext}));
+$secret_base64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(${secret_plaintext}));
 Write-Host ${secret_base64};
 
 
 #  DECODE  :::  Base64 - Decode/Decoding a String (via PowerShell):
 $secret_base64 = "UwBvAG0AZQAgAHMAZQBjAHIAZQB0ACwAIABpAC4AZQAuACAAYQAgAHAAYQBzAHMAdwBvAHIAZAAsACAAdABvAGsAZQBuACwAIABlAHQAYwAuAA==";
-$secret_plaintext = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String(${secret_base64}));
+$secret_plaintext = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(${secret_base64}));
 Write-Host ${secret_plaintext};
 
 
-#  CITATION  :::  "Simple Obfuscation with PowerShell using Base64 Encoding" - https://adsecurity.org/?p=478
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   adsecurity.org  |  "How to Enable or Disable Reading List in Google Chrome"  |  https://adsecurity.org/?p=478
+#
+# ------------------------------------------------------------

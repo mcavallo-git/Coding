e874d7913f63761077907a59edbf6603ca04df32
Matt Cavallo <mcavallo.git@gmail.com>
2021-05-19 17:07:22 -0400

    Update PowerShell - Base64 (Decode, Encode).ps1

--- powershell/PowerShell - Base64 (Decode, Encode).ps1
+++ powershell/PowerShell - Base64 (Decode, Encode).ps1
@@ -1,15 +1,41 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Base64 encode/encoding & decode/decoding strings
+#
+# ------------------------------------------------------------
+#
+# ENCODING
+#
 
-
-#  ENCODE  :::  Base64 - Encode/Encoding a String (via PowerShell):
-$secret_plaintext = "Some secret, i.e. a password, token, etc.";
-$secret_base64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(${secret_plaintext}));
+# Base64 encode (Using the UTF8 character set)
+$secret_plaintext="0123456789
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+abcdefghijklmnopqrstuvwxyz
+`~!@#$%^&()-_=+\|;:";
+$secret_base64=([Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(${secret_plaintext})));
 Write-Host ${secret_base64};
 
 
-#  DECODE  :::  Base64 - Decode/Decoding a String (via PowerShell):
-$secret_base64 = "UwBvAG0AZQAgAHMAZQBjAHIAZQB0ACwAIABpAC4AZQAuACAAYQAgAHAAYQBzAHMAdwBvAHIAZAAsACAAdABvAGsAZQBuACwAIABlAHQAYwAuAA==";
-$secret_plaintext = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(${secret_base64}));
+# Base64 encode (Using the UTF8 character set) - OneLiner (for quick one-offs/debugging)
+[Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("0123456789
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+abcdefghijklmnopqrstuvwxyz
+`~!@#$%^&()-_=+\|;:"));
+
+# ------------------------------------------------------------
+#
+# DECODING
+#
+
+
+# Base64 decode (using the UTF8 character set)
+$secret_base64="MDEyMzQ1Njc4OQpBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWgphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5egp+IUAjJCVeJigpLV89K1x8Ozo=";
+$secret_plaintext=([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String(${secret_base64})));
 Write-Host ${secret_plaintext};
+
+
+# Base64 decode (using the UTF8 character set) - OneLiner (for quick one-offs/debugging)
+[System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("MDEyMzQ1Njc4OQpBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWgphYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5egp+IUAjJCVeJigpLV89K1x8Ozo="));
 
 
 # ------------------------------------------------------------

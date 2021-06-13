01cd844a075388b54ccc122c3db15e0c7acf6a58
Matt Cavallo <mcavallo.git@gmail.com>
2021-06-12 20:51:37 -0400

    Update PowerCLI - _Install PowerCLI.ps1

--- vmware/powercli (ESXi PowerShell CLI)/PowerCLI - _Install PowerCLI.ps1
+++ vmware/powercli (ESXi PowerShell CLI)/PowerCLI - _Install PowerCLI.ps1
@@ -1,7 +1,8 @@
 # ------------------------------------------------------------
 #
-# VMware PowerCLI - Install NuGet Repo & VMware PowerCLI PowerShell Module
+# PowerShell - Install VMware's PowerCLI Module
 #
+# ------------------------------------------------------------
 
 If ($True) {
 	# Pre-Reqs: Check-for (and install if not found) the VMware PowerCLI PowerShell Module
@@ -23,6 +24,9 @@ If ($True) {
 #
 # Citation(s)
 #
+#   docs.microsoft.com  |  "Install-Module (PowerShellGet) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/powershellget/install-module?view=powershell-5.1
+#
+#   docs.microsoft.com  |  "Install-PackageProvider (PackageManagement) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/packagemanagement/install-packageprovider?view=powershell-5.1
 #
 #   powercli-core.readthedocs.io  |  "Install and Set Up vSphere PowerCLI"  |  https://pubs.vmware.com/vsphere-50/index.jsp?topic=%2Fcom.vmware.vsphere.install.doc_50%2FGUID-F02D0C2D-B226-4908-9E5C-2E783D41FE2D.html
 #

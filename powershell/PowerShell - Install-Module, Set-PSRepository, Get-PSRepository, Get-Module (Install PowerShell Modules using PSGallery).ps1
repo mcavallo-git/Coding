179f9e9ec5f30cc5a16e87bbde8ee41c5bb62d53
Matt Cavallo <mcavallo.git@gmail.com>
2021-06-12 21:12:47 -0400

    Update PowerShell - Install-Module, Set-PSRepository, Get-PSRepository, Get-Module (Install PowerShell Modules using PSGallery).ps1

--- powershell/PowerShell - Install-Module, Set-PSRepository, Get-PSRepository, Get-Module (Install PowerShell Modules using PSGallery).ps1
+++ powershell/PowerShell - Install-Module, Set-PSRepository, Get-PSRepository, Get-Module (Install PowerShell Modules using PSGallery).ps1
@@ -1,20 +1,42 @@
 # ------------------------------------------------------------
 #
-# PowerShell - Install a PowerShell Module from a given PowerShell Repository
+# PowerShell - Install a PowerShell Module from the "PSGallery" PowerShell Module Repository using the "NuGet" Package Management Provider
 #
 # ------------------------------------------------------------
 
-$PSModule_Name=("SqlServer");
+If ($True) {
+	$PSModule_Name=("SqlServer");
+	# $PSModule_Name=("VMware.PowerCLI");
+	If ((Get-Module -ListAvailable -Name "${PSModule_Name}" -ErrorAction SilentlyContinue) -Eq $Null) {
+		[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12;
+		$ProgressPreference="SilentlyContinue";
+		$PackageProvider_Name=("NuGet");
+		Install-PackageProvider `
+			-Name ("${PackageProvider_Name}") `
+			-MinimumVersion ("2.8.5.201") `
+			-Force `
+			-Confirm:$False;
+		$PSRepository_Name=("PSGallery");
+		If ((Get-PSRepository -Name ("${PSRepository_Name}") -ErrorAction SilentlyContinue) -Eq $Null) {
+			Register-PSRepository `
+				-Name ("${PSRepository_Name}") `
+				-PackageManagementProvider ("${PackageProvider_Name}") `
+				-SourceLocation ("https://www.powershellgallery.com/api/v2/") `
+				-PublishLocation ("https://www.powershellgallery.com/api/v2/package/") `
+				-ScriptSourceLocation ("https://www.powershellgallery.com/api/v2/items/psscript/") `
+				-ScriptPublishLocation ("https://www.powershellgallery.com/api/v2/package/") `
+				-InstallationPolicy ("Trusted");
 
-$PSRepository_Name=("PSGallery");
-
-If ((Get-Module -ListAvailable -Name "${PSModule_Name}" -ErrorAction SilentlyContinue) -Eq $Null) {
-	[System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12;
-	If ((Get-PSRepository -Name "${PSRepository_Name}" -ErrorAction SilentlyContinue) -Eq $Null) {
-		Set-PSRepository -Name "${PSRepository_Name}" -InstallationPolicy Trusted;
-	};
-	Install-Module -Name "${PSModule_Name}" -Scope CurrentUser -AllowClobber -Force;
-};
+		}
+		Install-Module `
+			-Name ("${PSModule_Name}") `
+			-Repository ("${PSRepository_Name}") `
+			-Scope ("CurrentUser") `
+			-AllowClobber `
+			-Force `
+			-Confirm:$False;
+	}
+}
 
 
 # ------------------------------------------------------------
@@ -23,8 +45,10 @@ If ((Get-Module -ListAvailable -Name "${PSModule_Name}" -ErrorAction SilentlyCon
 #
 #   addictivetips.com  |  "How To Add A Trusted Repository In PowerShell In Windows 10"  |  https://www.addictivetips.com/windows-tips/add-a-trusted-repository-in-powershell-windows-10/
 #
-#   addictivetips.com  |  "Install-Module (PowerShellGet) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/powershellget/install-module?view=powershell-5.1
+#   docs.microsoft.com  |  "Install-Module (PowerShellGet) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/powershellget/install-module?view=powershell-5.1
 #
-#   addictivetips.com  |  "Set-PSRepository (PowerShellGet) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/powershellget/set-psrepository?view=powershell-5.1
+#   docs.microsoft.com  |  "Install-PackageProvider (PackageManagement) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/packagemanagement/install-packageprovider?view=powershell-5.1
+#
+#   docs.microsoft.com  |  "Set-PSRepository (PowerShellGet) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/powershellget/set-psrepository?view=powershell-5.1
 #
 # ------------------------------------------------------------

9e06ac744e37b5af604cb71c07bf98b7311a3a3b
Matt Cavallo <mcavallo.git@gmail.com>
2021-06-21 19:17:46 -0400

    Update Linux - systemctl (show enabled, generated services).sh

--- linux/Linux - systemctl (show enabled, generated services).sh
+++ linux/Linux - systemctl (show enabled, generated services).sh
@@ -1,30 +1,57 @@
 #!/bin/bash
+
+# ------------------------------------------------------------
 #
-# ------------------------------------------------------------
-# List services enabled thru systemctl
+# ENABLE SERVICE @ STARTUP ("Autostart" on bootup - also starts the service when ran)
+#
 
-systemctl list-unit-files --type service --state enabled,generated;
+SVC="nginx";
+if \
+  [ "$(systemctl is-enabled ${SVC}.service 2>/dev/null;)" != "enabled" ] || \
+  [ "$(systemctl is-active ${SVC}.service 2>/dev/null;)" != "active" ] || \
+  [ $(systemctl is-enabled ${SVC}.service 1>/dev/null 2>&1; echo $?;) -ne 0 ] || \
+  [ $(systemctl is-active ${SVC}.service 1>/dev/null 2>&1; echo $?;) -ne 0 ]; \
+then
+  echo "Info:  Service \"${SVC}\" is not both \"enabled\" and \"active\", currently";
+  echo "Info:  Calling  [ systemctl enable \"${SVC}.service\" --now; ]  ...";
+  systemctl enable "${SVC}.service" --now; # "enable --now" autostarts service at bootup && starts service immediately
+fi;
+
 
 # ------------------------------------------------------------
-# Check one service's status
+#
+# DISABLE SERVICE @ STARTUP (do NOT "autostart" on bootup - also stops the service when ran)
+#
+
+SVC="nginx";
+if \
+  [ "$(systemctl is-enabled ${SVC}.service 2>/dev/null;)" == "enabled" ] || \
+  [ "$(systemctl is-active ${SVC}.service 2>/dev/null;)" == "active" ]; \
+then
+  echo "Info:  Service \"${SVC}\" is either \"enabled\" or \"active\", currently";
+  echo "Info:  Calling  [ systemctl disable \"${SVC}.service\" --now; ]  ...";
+  systemctl disable "${SVC}.service" --now; # "enable --now" autostarts service at bootup && starts service immediately
+fi;
+
+
+# ------------------------------------------------------------
+# GET SERVICE STATUS
 
 SVC="nginx"; systemctl is-enabled "${SVC}.service"; systemctl is-active "${SVC}.service"; systemctl status "${SVC}.service";
 
 
 # ------------------------------------------------------------
-# Autostart on bootup (as well as right now)
+# 
+# GET SERVICE STARTUP CONFIG/OPTIONS
+# 
 
-SVC="nginx";
-if \
-	[ "$(systemctl is-enabled ${SVC}.service 2>/dev/null;)" != "enabled" ] || \
-	[ "$(systemctl is-active ${SVC}.service 2>/dev/null;)" != "active" ] || \
-	[ $(systemctl is-enabled ${SVC}.service 1>/dev/null 2>&1; echo $?;) -ne 0 ] || \
-	[ $(systemctl is-active ${SVC}.service 1>/dev/null 2>&1; echo $?;) -ne 0 ]; \
-then
-	echo "Info:  Service \"${SVC}\" is not both \"enabled\" and \"active\", currently";
-	echo "Info:  Calling  [ systemctl enable \"${SVC}.service\" --now; ]  ...";
-	systemctl enable "${SVC}.service" --now; # "enable --now" autostarts service at bootup && starts service immediately
-fi;
+SERVICE_NAME="nginx"; cat "/lib/systemd/system/${SERVICE_NAME}.service";
+
+
+# ------------------------------------------------------------
+# List services enabled thru systemctl
+
+systemctl list-unit-files --type service --state enabled,generated;
 
 
 # ------------------------------------------------------------
@@ -34,18 +61,14 @@ fi;
 
 
 # ------------------------------------------------------------
-# 
-# Show startup config/options for a given service
-# 
-
-SERVICE_NAME="mongod"; cat "/lib/systemd/system/${SERVICE_NAME}.service";
-
-
-# ------------------------------------------------------------
 # Citation(s)
 #
 #   askubuntu.com  |  "systemd - How to list all enabled services from systemctl? - Ask Ubuntu"  |  https://askubuntu.com/a/1060852
 #
 #   askubuntu.com  |  "systemd - What is the difference between "systemctl start" and "systemctl enable"? - Ask Ubuntu"  |  https://askubuntu.com/a/733510
+#
+#   askubuntu.com  |  "Difference between systemctl and service commands - Ask Ubuntu"  |  https://askubuntu.com/a/903405
+#
+#   www.reddit.com  |  "What is the relation between update-rc.d and systemctl : linuxquestions"  |  https://www.reddit.com/r/linuxquestions/comments/4sw5dr/what_is_the_relation_between_updatercd_and/
 #
 # ------------------------------------------------------------

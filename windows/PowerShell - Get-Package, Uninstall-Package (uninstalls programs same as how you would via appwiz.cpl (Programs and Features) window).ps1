cc4e6f72d487d4e904c51a37a0a78fbdcbff6a98
Matt Cavallo <mcavallo.git@gmail.com>
2021-06-23 11:13:22 -0400

    Create PowerShell - Get-Package, Uninstall-Package (uninstalls programs same as how you would via appwiz.cpl (Programs and Features) window).ps1

---
+++ windows/PowerShell - Get-Package, Uninstall-Package (uninstalls programs same as how you would via appwiz.cpl (Programs and Features) window).ps1
@@ -0,0 +1,31 @@
+
+# Uninstall Multiple Packages via PowerShell
+
+@(
+"AI Suite 3", 
+"AMD Software", 
+"AMD_Chipset_Drivers", 
+"ARMOURY CRATE Lite Service", 
+"ASUS Framework Service", 
+"AURA", 
+"AURA lighting effect add-on", 
+"AURA lighting effect add-on x64", 
+"AURA Service", 
+"GALAX GAMER RGB", 
+"ROG Live Service"
+) | ForEach-Object {
+	Get-Package -Name "${_}" -ErrorAction SilentlyContinue | Uninstall-Package;
+}
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.spiceworks.com  |  "[SOLVED] uninstall a program via powershell - Spiceworks"  |  https://community.spiceworks.com/topic/1966595-uninstall-a-program-via-powershell?page=1#entry-6649944
+#
+#   docs.microsoft.com  |  "Get-Package (PackageManagement) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/packagemanagement/get-package?view=powershell-5.1
+#
+#   docs.microsoft.com  |  "Uninstall-Package (PackageManagement) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/packagemanagement/uninstall-package?view=powershell-5.1
+#
+# ------------------------------------------------------------

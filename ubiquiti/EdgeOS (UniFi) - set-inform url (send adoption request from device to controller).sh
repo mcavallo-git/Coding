4118c44b5a13e2ce81321c08fad5edea1f3502cc
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-08 14:00:04 -0400

    Update EdgeOS (UniFi) - set-inform url (send adoption request from device to controller).sh

--- ubiquiti/EdgeOS (UniFi) - set-inform url (send adoption request from device to controller).sh
+++ ubiquiti/EdgeOS (UniFi) - set-inform url (send adoption request from device to controller).sh
@@ -2,6 +2,16 @@
 #
 # Linux - UniFi - Send Adoption request to target-URL (from devices being adopted)
 #
+# ------------------------------------------------------------
+
+# GET INFORM URL
+
+info
+
+
+# ------------------------------------------------------------
+
+# SET INFORM URL
 
 UNIFI_CONTROLLER_IPv4="192.168.1.15"; sudo set-inform "http://${UNIFI_CONTROLLER_IPv4}:8080/inform";
 
@@ -12,3 +22,12 @@ UNIFI_CONTROLLER_IPv4="192.168.1.15"; sudo set-inform "http://${UNIFI_CONTROLLER
 ### 2. issue the set-inform command again
 ### 3. <inform_url> will be saved after device is successfully managed
 ### 
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.ui.com  |  "Where do I find current inform URL? | Ubiquiti Community"  |  https://community.ui.com/questions/Where-do-I-find-current-inform-URL/627dfeb7-7ffd-4975-b709-c947296438f4
+#
+# ------------------------------------------------------------

f7b456ea23cf1311268099f530fd6203053055a2
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-08 19:38:11 -0400

    Update EdgeOS (UniFi) - Update UCK-G2 firmware (UniFi Cloud Key Gen 2).sh

--- ubiquiti/EdgeOS (UniFi) - Update UCK-G2 firmware (UniFi Cloud Key Gen 2).sh
+++ ubiquiti/EdgeOS (UniFi) - Update UCK-G2 firmware (UniFi Cloud Key Gen 2).sh
@@ -14,9 +14,12 @@ if [ 1 -eq 1 ]; then
 
 # Firmware - UniFi OS - Cloud Key Gen2 x.xx.xx
 
-# FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/e9cc-UCKG2-2.1.11-1cf4bd8262b64919ac100cc5b69dc0b1.bin";  # v2.1.11
-FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/4724-UCKG2-2.1.7-11ca020dba684770b2ebe6a832cb9911.bin";  # v2.1.7
-# FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/fbdc-UCKG2-2.0.27-2ceea428cab74b96bd456d67412b9a6c.bin";  # v2.0.27
+FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/e9cc-UCKG2-2.1.11-1cf4bd8262b64919ac100cc5b69dc0b1.bin";  # v2.1.11  (2021-05-19)
+# FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/4724-UCKG2-2.1.7-11ca020dba684770b2ebe6a832cb9911.bin";     # v2.1.7   (2021-04-12)
+# FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/fbdc-UCKG2-2.0.27-2ceea428cab74b96bd456d67412b9a6c.bin";  # v2.0.27  (2021-01-29)
+# FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/ca54-UCKG2-2.0.26-52af8c26f959414b918ee1b7cae94a64.bin";  # v2.0.26  (2021-01-21)
+# FIRMWARE_URL="https://fw-download.ubnt.com/data/unifi-cloudkey/02ae-UCKG2-2.0.24-6a82e33e88174d1895a46ecb8953f562.bin";  # v2.0.24  (2020-12-17)
+# FIRMWARE_URL="https://dl.ui.com/unifi/cloudkey/firmware/UCKG2/UCKG2.apq8053.v1.1.13.818cc5f.200430.0948.bin";            # v1.1.13  (2020-06-22)
 
 ubnt-systool fwupdate "${FIRMWARE_URL}";
 
@@ -34,9 +37,16 @@ fi;
 
 if [ 1 -eq 1 ]; then
 
-# 1. Get latest software URL @ https://www.ui.com/download/unifi/unifi-cloud-key-gen2
+# Software -  UniFi Network Application x.x.xx for Debian/Ubuntu Linux and UniFi Cloud Key
 
-SOFTWARE_URL="https://dl.ui.com/unifi/6.2.26/unifi_sysvinit_all.deb";  # Software -  UniFi Network Application x.x.xx for Debian/Ubuntu Linux and UniFi Cloud Key
+# 1. Get latest software URL from Ubiquiti's download portal
+
+SOFTWARE_URL="https://dl.ui.com/unifi/6.2.26/unifi_sysvinit_all.deb";  # v6.2.26  (2021-06-21)
+# SOFTWARE_URL="https://dl.ui.com/unifi/6.2.25/unifi_sysvinit_all.deb";    # v6.2.25  (2021-05-19)
+# SOFTWARE_URL="https://dl.ui.com/unifi/6.1.71/unifi_sysvinit_all.deb";  # v6.1.71  (2021-03-25)
+# SOFTWARE_URL="https://dl.ui.com/unifi/6.0.45/unifi_sysvinit_all.deb";  # v6.0.45  (2021-01-27)
+# SOFTWARE_URL="https://dl.ui.com/unifi/6.0.43/unifi_sysvinit_all.deb";  # v6.0.43  (2020-12-28)
+# SOFTWARE_URL="https://dl.ui.com/unifi/6.0.41/unifi_sysvinit_all.deb";  # v6.0.41  (2020-12-03)
 
 # 2. Open a SSH session using your favorite SSH/Telnet client program (for example PuTTY or the macOS/Linux Terminal). Access the Command Line Interface on the UDM using SSH. See how to log into a UniFi OS Console in the "Other Credentials" section of this article.
 
@@ -58,7 +68,6 @@ curl -o "${LOCAL_INSTALL}" "${SOFTWARE_URL}";
 # 8. Wait for the firmware download process to complete and install the new version using the dpkg command:
 if [ 1 -eq 1 ]; then
 dpkg -i "${LOCAL_INSTALL}";
-echo "done with dpkg";
 fi;
 
 # 9. Wait for the installation process to complete and remove the unifi_sysvinit_all.deb file afterwards.

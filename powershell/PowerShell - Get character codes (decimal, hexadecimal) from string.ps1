fe39de42d8cee960a5fda10fffaec4ccde2b156b
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-10 23:20:33 -0400

    Create PowerShell - Get character codes (decimal, hexadecimal) from string.ps1

---
+++ powershell/PowerShell - Get character codes (decimal, hexadecimal) from string.ps1
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Get Decimal/Hexadecimal character code for a given character
+#
+# ------------------------------------------------------------
+
+
+[int][char]"❖";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.idera.com  |  "Converting ASCII and Characters - Power Tips - Power Tips - IDERA Community"  |  https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/converting-ascii-and-characters
+#
+# ------------------------------------------------------------

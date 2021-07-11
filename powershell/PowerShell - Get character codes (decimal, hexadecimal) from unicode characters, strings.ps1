d56fff70c8f1272daf4152b1847bccc0014db1df
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-11 00:39:20 -0400

    Update PowerShell - Get character codes (decimal, hexadecimal) from unicode characters, strings.ps1

--- powershell/PowerShell - Get character codes (decimal, hexadecimal) from unicode characters, strings.ps1
+++ powershell/PowerShell - Get character codes (decimal, hexadecimal) from unicode characters, strings.ps1
@@ -6,17 +6,27 @@
 
 If ($True) {
 
-$Input="❖";
+# $Input="❖";
+$Input="👍";
+# $Input="👍🏽";
+# $Input = Read-Host -Prompt 'Enter string/character to get character codes for';
+
+Add-Type -AssemblyName ("System.Web");
 
 $Character=@{};
+# $Character.("Characters")=("${Input}".ToCharArray());
+$Character.("HTML Entity (Decimal)")=@();
+$Character.("HTML Entity (Hex)")=@();
+$Character.("UTF-16 Code (Decimal)")=@();
+$Character.("UTF-16 Code (Hex)")=@();
 
-$Character.("Character")=${Input};
-
-$Character.("UTF-16 Code (Decimal)")=([Int][Char]${Input});
-$Character.("UTF-16 Code (Hex)")=("0x$('{0:x}' -f ([Int][Char]${Input}))");
-
-$Character.("HTML Entity (Decimal)")=("&#$([Int][Char]${Input})");
-$Character.("HTML Entity (Hex)")=("&#x$('{0:x}' -f ([Int][Char]${Input}))");
+("${Input}".ToCharArray()) | ForEach-Object {
+	$EachChar="${_}";
+	$Character.("HTML Entity (Decimal)") += ("&#$([Int][Char]${EachChar})");
+	$Character.("HTML Entity (Hex)") += ("&#x$('{0:x}' -f ([Int][Char]${EachChar}))");
+	$Character.("UTF-16 Code (Decimal)") += ([Int][Char]${EachChar});
+	$Character.("UTF-16 Code (Hex)") += ("0x$('{0:x}' -f ([Int][Char]${EachChar}))");
+}
 
 Write-Output ($Character);
 
@@ -28,6 +38,10 @@ Write-Output ($Character);
 # Citation(s)
 #
 #   community.idera.com  |  "Converting ASCII and Characters - Power Tips - Power Tips - IDERA Community"  |  https://community.idera.com/database-tools/powershell/powertips/b/tips/posts/converting-ascii-and-characters
+#
+#   docs.microsoft.com  |  "WebUtility Class (System.Net) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/dotnet/api/system.net.webutility?view=net-5.0
+#
+#   stackoverflow.com  |  "What is the best way to escape HTML-specific characters in a string (PowerShell)? - Stack Overflow"  |  https://stackoverflow.com/a/48762484
 #
 #   www.fileformat.info  |  "Unicode Character 'BLACK DIAMOND MINUS WHITE X' (U+2756)"  |  https://www.fileformat.info/info/unicode/char/2756/index.htm
 #

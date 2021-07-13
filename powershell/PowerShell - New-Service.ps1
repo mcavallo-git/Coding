65356402f6c009f588571edeaebea9aa61ca9fe8
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-13 05:09:50 -0400

    Update PowerShell - New-Service.ps1

--- powershell/PowerShell - New-Service.ps1
+++ powershell/PowerShell - New-Service.ps1
@@ -1,19 +1,22 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Add a service via "New-Service"
+#
 
-$params = @{
-  Name = "TestService"
-  BinaryPathName = "C:\WINDOWS\System32\svchost.exe -k netsvcs"
-  DependsOn = "NetLogon"
-  DisplayName = "Test Service"
-  StartupType = "Manual"
-  Description = "This is a test service."
+$NewService_SplatParams = @{
+  Name = "TestService";
+  DisplayName = "Test Service";
+  BinaryPathName = "C:\WINDOWS\System32\svchost.exe -k netsvcs";
+  DependsOn = "NetLogon";
+  StartupType = "Automatic";  <# Valid values are [ Automatic, Disabled, Manual, Boot, System ] #>
+  Description = "This is a test service.";
 }
-New-Service @params
-
+New-Service @NewService_SplatParams;
 
 # ------------------------------------------------------------
 #
 # Citation(s)
 #
-#   docs.microsoft.com  |  "New-Service - Creates a new Windows service"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-service?view=powershell-7
+#   docs.microsoft.com  |  "New-Service - Creates a new Windows service"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-service?view=powershell-5.1
 #
 # ------------------------------------------------------------

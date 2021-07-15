ae0956821b3574e478e231521d657b7a510413f7
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-15 17:48:40 -0400

    Create PowerShell - wsl.exe, udate to use WSL 2.ps1

---
+++ powershell/PowerShell - wsl.exe, udate to use WSL 2.ps1
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------
+# wsl.exe - Update to use WSL 2
+# ------------------------------------------------------------
+
+
+wsl.exe --list --verbose;  # The line starting with an asterisk '*' is the current terminal's Linux distro
+
+
+wsl.exe --set-version Ubuntu 2;  # DON'T USE QUOTES ON DISTRO NAME
+
+
+wsl.exe --set-default-version 2;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.docker.com  |  "Docker Desktop WSL 2 backend | Docker Documentation"  |  https://docs.microsoft.com/en-us/windows/wsl/about#what-is-wsl-2
+#
+#   docs.microsoft.com  |  "What is Windows Subsystem for Linux | Microsoft Docs"  |  https://docs.microsoft.com/en-us/windows/wsl/about
+#
+# ------------------------------------------------------------

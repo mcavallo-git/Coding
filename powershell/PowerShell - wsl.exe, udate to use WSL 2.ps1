b6a4a6e908846a3edc9224ec92db470f45a0654a
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-15 17:51:28 -0400

    Update PowerShell - wsl.exe, udate to use WSL 2.ps1

--- powershell/PowerShell - wsl.exe, udate to use WSL 2.ps1
+++ powershell/PowerShell - wsl.exe, udate to use WSL 2.ps1
@@ -12,11 +12,15 @@ wsl.exe --set-version Ubuntu 2;  # DON'T USE QUOTES ON DISTRO NAME
 wsl.exe --set-default-version 2;
 
 
+
+
 # ------------------------------------------------------------
 #
 # Citation(s)
 #
 #   docs.docker.com  |  "Docker Desktop WSL 2 backend | Docker Documentation"  |  https://docs.microsoft.com/en-us/windows/wsl/about#what-is-wsl-2
+#
+#   docs.microsoft.com  |  "Install WSL on Windows 10 | Microsoft Docs"  |  https://docs.microsoft.com/en-us/windows/wsl/install-win10#step-4---download-the-linux-kernel-update-package
 #
 #   docs.microsoft.com  |  "What is Windows Subsystem for Linux | Microsoft Docs"  |  https://docs.microsoft.com/en-us/windows/wsl/about
 #

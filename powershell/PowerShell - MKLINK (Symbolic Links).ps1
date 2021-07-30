fcdc74f23e0870ae4f46237b1b0a9ca988f5e64f
Matt Cavallo <mcavallo.git@gmail.com>
2021-07-29 20:08:25 -0400

    Update PowerShell - MKLINK (Symbolic Links).ps1

--- powershell/PowerShell - MKLINK (Symbolic Links).ps1
+++ powershell/PowerShell - MKLINK (Symbolic Links).ps1
@@ -30,17 +30,22 @@ If ((Get-Item "${Home}\Documents").Attributes.ToString() -Match "ReparsePoint") 
 
 <# Ex)  *** WINDOWS FAX AND SCAN *** - Dredirect scans going into [ My Documents -> Scanned Documents ] to be placed on the desktop, instead #>
 If ($True) {
-	$REDIRECT_FROM="${HOME}\Documents\Scanned Documents";
-	$REDIRECT_TO="${HOME}\Desktop";
-	<# Make sure destination directory exists #>
-	If (-Not (Test-Path -PathType "Container" -Path ("${REDIRECT_TO}"))) { <# Ensure that the destination directory exists #>
-		New-Item -ItemType "Directory" -Path ("${REDIRECT_TO}") | Out-Null;
+<# *** WINDOWS FAX AND SCAN *** - Dredirect scans going into [ My Documents -> Scanned Documents ] to be placed on the desktop, instead #>
+	$REDIRECT_FROM_LINK="${HOME}\Documents\Scanned Documents";
+	$REDIRECT_TO_EXISTING="${HOME}\Desktop";
+	If ((Get-Item "${REDIRECT_FROM_LINK}").Attributes.ToString() -Match "ReparsePoint") {
+		Write-Host 	"Path `"${REDIRECT_FROM_LINK}`" is already Symbolic Link / MKLink";
+	} Else {
+		<# Make sure destination directory exists #>
+		If (-Not (Test-Path -PathType "Container" -Path ("${REDIRECT_TO_EXISTING}"))) { <# Ensure that the destination directory exists #>
+			New-Item -ItemType "Directory" -Path ("${REDIRECT_TO_EXISTING}") | Out-Null;
+		}
+		If (Test-Path -PathType "Container" -Path ("${REDIRECT_FROM_LINK}")) { <# Ensure that the source directory (which will become a symbolic link) does NOT exist, and that if it DOES exist, that its files are copied to the destination directory before the source directory is removed #>
+			Copy-Item -Path ("${REDIRECT_FROM_LINK}\*") -Destination ("${REDIRECT_TO_EXISTING}") -Force -Recurse;
+			Remove-Item -Path ("${REDIRECT_FROM_LINK}") -Recurse -Force -Confirm:$False;
+		}
+		Start-Process -Filepath ("${env:COMSPEC}") -ArgumentList (@("/C","MKLINK /D `"${REDIRECT_FROM_LINK}`" `"${REDIRECT_TO_EXISTING}`"")) -NoNewWindow  -Wait -PassThru -ErrorAction ("SilentlyContinue");
 	}
-	If (Test-Path -PathType "Container" -Path ("${REDIRECT_FROM}")) { <# Ensure that the source directory (which will become a symbolic link) does NOT exist, and that if it DOES exist, that its files are copied to the destination directory before the source directory is removed #>
-		Copy-Item -Path ("${REDIRECT_FROM}\*") -Destination ("${REDIRECT_TO}") -Force -Recurse;
-		Remove-Item -Path ("$REDIRECT_FROM") -Recurse -Force -Confirm:$False;
-	}
-	Start-Process -Filepath ("${env:COMSPEC}") -ArgumentList (@("/C","MKLINK /D `"${REDIRECT_FROM}`" `"${REDIRECT_TO}`"")) -NoNewWindow  -Wait -PassThru -ErrorAction ("SilentlyContinue");
 }
 
 

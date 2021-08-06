482d0fff3f24bc665dbdef319c395939033a55af
Matt Cavallo <mcavallo.git@gmail.com>
2021-08-06 07:29:11 -0400

    Revert "Update Linux - Get LAN IPv4 (ip addr show).sh"
    This reverts commit 6e7f6bd00f574a3f2932f90ca139579a90434fbd.

--- linux/Linux - Get LAN IPv4 (ip addr show).sh
+++ linux/Linux - Get LAN IPv4 (ip addr show).sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-THIS_IPv4=$(ip addr show | grep 'inet' | grep 'scope global' | awk '{ print $2; }' | sed 's/\/.*$//' | grep '\.' | head -n 1);
+THIS_IPv4=$(ip addr show | grep 'inet' | grep 'scope global' | awk '{ print $2; }' | sed 's/\/.*$//' | grep '\.');
 echo "${THIS_IPv4}";

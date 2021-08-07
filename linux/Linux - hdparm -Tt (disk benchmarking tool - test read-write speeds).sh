861a1a88fcddac36132ebec26bcd5a7fedf8051e
Matt Cavallo <mcavallo.git@gmail.com>
2021-08-07 15:03:34 -0400

    Create Linux - hdparm -Tt (disk benchmarking tool - test read-write speeds).sh

---
+++ linux/Linux - hdparm -Tt (disk benchmarking tool - test read-write speeds).sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DISK_PATH=$(lsblk | grep disk | awk '{print $1}';);
+
+hdparm -Tt "${DISK_PATH}";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   askubuntu.com  |  "How to check hard disk performance - Ask Ubuntu"  |  https://askubuntu.com/a/87036
+#
+# ------------------------------------------------------------

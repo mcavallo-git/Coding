0a74601a982b70184762de0f01561aa991660323
Matt Cavallo <mcavallo.git@gmail.com>
2021-08-20 20:09:45 -0400

    rn

---
+++ vmware/VMware ESXi - esxcfg-advcfg --list (List all VMkernel advanced config options).sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# ------------------------------------------------------------
+# VMware ESXi - esxcfg-advcfg --list (List all VMkernel advanced config options)
+# ------------------------------------------------------------
+
+esxcfg-advcfg --list;
+
+
+# ------------------------------------------------------------
+# Citation(s)
+#
+#   kb.vmware.com  |  "Configuring advanced options for ESXi/ESX (1038578)"  |  https://kb.vmware.com/s/article/1038578
+# 
+# ------------------------------------------------------------

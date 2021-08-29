9c94311bd1a37a100fa2c2c8c9acb07b8e8f753a
Matt Cavallo <mcavallo.git@gmail.com>
2021-08-29 16:56:29 -0400

    Update Linux - Bashisms (string replacement, lowercase, uppercase, parameter substitution).sh

--- linux/Linux - Bashisms (string replacement, lowercase, uppercase, parameter substitution).sh
+++ linux/Linux - Bashisms (string replacement, lowercase, uppercase, parameter substitution).sh
@@ -5,11 +5,54 @@
 #
 # ------------------------------------------------------------
 #
+### Left-Trim
+#
+#   SYNTAX:   ${VARIABLE_TO_TRIM:N}
+#                                 ^
+#                                 |-- Trims N characters off the left side of the string
+#
+
+# Bash Left-Trim Example: Remove the first N characters from a string
+VARIABLE_TO_TRIM="ABCDEF";
+
+N=1; echo "${VARIABLE_TO_TRIM:${N}}";
+###  Outputs  "BCDEF"
+
+echo "${VARIABLE_TO_TRIM:2}";
+###  Outputs  "CDEF"
+
+N=3; echo "${VARIABLE_TO_TRIM:${N}}";
+###  Outputs  "DEF"
+
+
+# ------------------------------------------------------------
+#
+### Left-Slice
+#
+#   SYNTAX:   ${VARIABLE_TO_SLICE::N}
+#                                  ^
+#                                  |-- Keeps N characters from the left side of the string
+#
+
+# Bash Left-Slice Example: Remove the first N characters from a string
+VARIABLE_TO_SLICE="ABCDEF";
+
+N=1; echo "${VARIABLE_TO_SLICE::${N}}";
+###  Outputs  "A"
+
+echo "${VARIABLE_TO_SLICE::2}";
+###  Outputs  "AB"
+
+N=4; echo "${VARIABLE_TO_SLICE::${N}}";
+###  Outputs  "ABCD"
+
+
+# ------------------------------------------------------------
+#
 ### Substring Replacement
 #
-# Syntax:
-#         ${VARIABLE_TO_SEARCH//STRING_TO_FIND/STRING_TO_REPLACE_WITH}
-#         ${HAYSTACK//${NEEDLE}/${REPLACEMENT}}   <-- using nested variables
+#   SYNTAX:   ${VARIABLE_TO_SEARCH//STRING_TO_FIND/STRING_TO_REPLACE_WITH}
+#             ${HAYSTACK//${NEEDLE}/${REPLACEMENT}}   <-- using nested variables
 #
 
 # Bash Substring Replacement Exaxmple: Replace dashes with underscores
@@ -35,7 +78,7 @@ MODIFIED_STRING="${HAYSTACK//${NEEDLE}/${REPLACEMENT}}" && echo "${MODIFIED_STRI
 #
 ### String variable to-Lowercase
 #
-# SYNTAX:   ${VARNAME,,}
+#   SYNTAX:   ${VARNAME,,}
 #
 
 EXAMPLE_TO_LOWERCASE="Dat-Example-Doe" && echo "${EXAMPLE_TO_LOWERCASE,,}";
@@ -45,7 +88,7 @@ EXAMPLE_TO_LOWERCASE="Dat-Example-Doe" && echo "${EXAMPLE_TO_LOWERCASE,,}";
 #
 ### String variable to-Uppercase
 #
-# SYNTAX:   ${VARNAME^^}
+#   SYNTAX:   ${VARNAME^^}
 #
 
 EXAMPLE_TO_UPPERCASE="Dat-Example-Doe" && echo "${EXAMPLE_TO_UPPERCASE^^}";
@@ -67,7 +110,11 @@ echo "${VAR_NOT_SET:-${VAR_ALSO_NOT_SET:-72}}";
 # ------------------------------------------------------------
 # Citation(s)
 #
+#   betterprogramming.pub  |  "24 Bashism To Avoid for POSIX-Compliant Shell Scripts | by Shinichi Okada | Aug, 2021 | Better Programming"  |  https://betterprogramming.pub/24-bashism-to-avoid-for-posix-compliant-shell-scripts-8e7c09e0f49a
+#
 #   stackoverflow.com  |  "Case insensitive comparison of strings in shell script"  |  https://stackoverflow.com/a/19411918
+#
+#   stackoverflow.com  |  "Remove first character of a string in Bash - Stack Overflow"  |  https://stackoverflow.com/a/46699430
 #
 #   tldp.org  |  "Manipulating Strings"  |  https://tldp.org/LDP/abs/html/string-manipulation.html
 #

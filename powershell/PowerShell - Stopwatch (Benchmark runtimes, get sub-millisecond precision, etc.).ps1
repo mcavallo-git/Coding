fa1fb4fab199082e3376f44e3a0586bcb986efb0
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-13 18:20:47 -0400

    Update PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1

--- powershell/PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1
+++ powershell/PowerShell - Stopwatch (Benchmark runtimes, get sub-millisecond precision, etc.).ps1
@@ -1,6 +1,7 @@
+# ------------------------------------------------------------
 
-
-If ($True) { <# Start the benchmark, run some code, stop the benchmark, show the results #> 
+If ($True) {
+<# Start a benchmarking stopwatch > run command(s) to-be-benchmarked > stop the stopwatch > show the results #> 
 $Benchmark = New-Object System.Diagnostics.Stopwatch;
 $Benchmark.Restart(); <# [Re-]Start the stopwatch #>
 Start-Sleep -Seconds (1); <# REPLACE THIS LINE WITH COMMAND OR PROCESS TO-BENCHMARK #>
@@ -8,15 +9,44 @@ $Benchmark.Stop();
 Write-Output "`$Benchmark.Elapsed = $(${Benchmark}.Elapsed)";
 }
 
+# ------------------------------------------------------------
 
+<# Example benchmark between two approaches of achieving a similar outcome to determine which method is faster #>
 
 If ($True) {
-# Start the benchmark, run some code, stop the benchmark, show the results
-$Benchmark.Reset(); <# Reuse same benchmark/stopwatch object by resetting it #>
-$Benchmark.Start();
+$LoopIterations = 10000;
+$Benchmark = New-Object System.Diagnostics.Stopwatch;
+<# Test method 1 of getting timestamps w/ decimal-based seconds + timezone #>
+$DecimalTimestampShort = $Null;
+$Benchmark.Restart(); <# [Re-]Start the stopwatch #>
+For ($i = 0; $i -LT ${LoopIterations}; $i++) {
+$DecimalTimestampShort = $(Get-Date -Format 'yyyyMMddTHHmmss.ffffffzz')
+}
+$Benchmark.Stop();
+Write-Output "`$Benchmark.Elapsed = $(${Benchmark}.Elapsed)";
+<# Test method A of getting timestamps w/ decimal-based seconds + timezone #>
+$DecimalTimestampShort = $Null;
+$Benchmark.Restart(); <# [Re-]Start the stopwatch #>
+For ($i = 0; $i -LT ${LoopIterations}; $i++) {
+$EpochDate = ([Decimal](Get-Date -UFormat ("%s")));
+$EpochToDateTime = (New-Object -Type DateTime -ArgumentList 1970,1,1,0,0,0,0).AddSeconds([Math]::Floor($EpochDate));
+$DecimalTimestampShort = ( ([String](Get-Date -Date ("${EpochToDateTime}") -UFormat ("%Y%m%d-%H%M%S"))) + (([String]((${EpochDate}%1))).Substring(1).PadRight(6,"0")) );
+}
+$Benchmark.Stop();
+Write-Output "`$Benchmark.Elapsed = $(${Benchmark}.Elapsed)";
+}
+
+
+# ------------------------------------------------------------
+
+If ($True) {
+$Benchmark.Restart(); <# [Re-]Start the stopwatch #>
 Start-Sleep -Seconds (65);
 $Benchmark.Stop();
+# $Benchmark.Reset;
+# $Benchmark.Start;
 $Benchmark | Format-List; <# Show detailed benchmark results #>
+# $Benchmark.IsRunning;            # Ex:  False
 # $Benchmark.IsRunning;            # Ex:  False
 # $Benchmark.Elapsed;              # Ex:  00:00:02.1047087
 # $Benchmark.ElapsedMilliseconds;  # Ex:  2104

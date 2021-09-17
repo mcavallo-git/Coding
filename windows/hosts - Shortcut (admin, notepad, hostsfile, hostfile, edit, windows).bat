6cec569156ea4dccd5a033652fe7e3560807960a
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-17 05:42:32 -0400

    Update hosts - Shortcut (admin, notepad, hostsfile, hostfile, edit, windows).bat

--- windows/hosts - Shortcut (admin, notepad, hostsfile, hostfile, edit, windows).bat
+++ windows/hosts - Shortcut (admin, notepad, hostsfile, hostfile, edit, windows).bat
@@ -1,1 +1,6 @@
-PowerShell -NoProfile "Start-Process 'C:\Windows\System32\notepad.exe' -ArgumentList 'C:\Windows\System32\drivers\etc\hosts' -Verb 'RunAs'"
+
+REM Open Windows' Hostsfile in Notepad++.exe (fall-back to Notepad.exe)
+PowerShell -NoProfile "If (Test-Path C:\Program` Files\Notepad++\notepad++.exe) { Start-Process C:\Program` Files\Notepad++\notepad++.exe -ArgumentList C:\Windows\System32\drivers\etc\hosts -Verb RunAs; } ElseIf (Test-Path C:\Program` Files` (x86)\Notepad++\notepad++.exe) { Start-Process C:\Program` Files` (x86)\Notepad++\notepad++.exe -ArgumentList C:\Windows\System32\drivers\etc\hosts -Verb RunAs; } Else { Start-Process C:\Windows\System32\notepad.exe -ArgumentList C:\Windows\System32\drivers\etc\hosts -Verb RunAs; };"
+
+REM Open Windows' Hostsfile in Notepad.exe
+PowerShell -NoProfile "Start-Process 'C:\Windows\System32\notepad.exe' -ArgumentList 'C:\Windows\System32\drivers\etc\hosts' -Verb 'RunAs';"

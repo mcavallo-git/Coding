a7205b158e5da5415f139f20ce46a6e3cfe50897
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-17 05:07:27 -0400

    rn

---
+++ linux/Linux - printf (right-pad (rpad) strings, prepending strings before command-output, etc.).sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# ------------------------------------------------------------
+
+
+# printf - right-pad (rpad) a string with spaces
+TEST_PADDING="tester"; echo "[$(printf '%-10s' "$TEST_PADDING";)]"
+
+
+# ------------------------------------------------------------
+
+
+# printf - prepending strings before command-output
+find "/var/log/" \
+-type f \
+-iname syslog* \
+-exec printf "$(date +'%Y-%m-%d %H:%M:%S') $(whoami)@$(hostname) | " \; \
+-exec echo "------------------------------------------------------------" \; \
+-exec cat '{}' \; \
+-exec echo "------------------------------------------------------------" \; \
+;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   unix.stackexchange.com  |  "Bash add trailing spaces to justify string - Unix & Linux Stack Exchange"  |  https://unix.stackexchange.com/a/354094
+#
+# ------------------------------------------------------------

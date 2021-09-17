fb9c7a7966a8b3bd2c5588e485a78c0b988490a2
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-17 05:13:02 -0400

    Update PowerShell - docker_kill_all (remove docker containers, images & networks).ps1

--- powershell/PowerShell - docker_kill_all (remove docker containers, images & networks).ps1
+++ powershell/PowerShell - docker_kill_all (remove docker containers, images & networks).ps1
@@ -1,1 +1,27 @@
-docker rm --force $(docker stop $(docker ps --all --quiet)); docker rmi --force $(docker images --all --quiet); docker network rm $((docker network ls --format='{{.ID}}  {{.Name}}') | Where-Object { (@("bridge","host","none") -contains (("${_}" -Split "  ")[1])) -Eq $False} | ForEach-Object {("${_}" -Split "  ")[0]}); # remove docker containers, images & networks
+If ($True) {
+$DASH_NL="------------------------------------------------------------`n";
+
+<# Docker - Ensure Docker Desktop is running #>
+If ((Get-Process -Name "Docker Desktop" -ErrorAction "SilentlyContinue") -Eq $Null) {
+Write-Host "${DASH_NL}[ $(Get-Date -Format 'yyyy-MM-ddTHH:mm:sszzz') ] Starting Docker Desktop...";
+Start-Process -Filepath ("C:\Program Files\Docker\Docker\Docker Desktop.exe") -ArgumentList (@("restart","${ServiceName}")) -PassThru;
+Start-Sleep -Seconds 60;
+}
+
+<# Docker - Kill containers #>
+Write-Host "${DASH_NL}[ $(Get-Date -Format 'yyyy-MM-ddTHH:mm:sszzz') ] Killing Docker containers...";
+docker rm --force $(docker stop $(docker ps --all --quiet));
+
+<# Docker - Kill images #>
+Write-Host "${DASH_NL}[ $(Get-Date -Format 'yyyy-MM-ddTHH:mm:sszzz') ] Killing Docker images...";
+docker rmi --force $(docker images --all --quiet);
+
+<# Docker - Kill networks #>
+Write-Host "${DASH_NL}[ $(Get-Date -Format 'yyyy-MM-ddTHH:mm:sszzz') ] Killing Docker networks...";
+docker network rm $((docker network ls --format='{{.ID}}  {{.Name}}') | Where-Object { (@("bridge","host","none") -contains (("${_}" -Split "  ")[1])) -Eq $False} | ForEach-Object {("${_}" -Split "  ")[0]}) 2> $Null;
+
+<# Docker - Kill volumes #>
+Write-Host "${DASH_NL}[ $(Get-Date -Format 'yyyy-MM-ddTHH:mm:sszzz') ] Killing Docker volumes...";
+docker volume rm $(docker volume ls --quiet);
+
+}

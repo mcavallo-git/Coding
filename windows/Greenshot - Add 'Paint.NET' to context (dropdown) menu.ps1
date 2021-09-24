084196ede16079efcb0229a60047d6285b7c9b4e
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-24 18:07:22 -0400

    Update Greenshot - Add 'Paint.NET' to context (dropdown) menu.ps1

--- windows/Greenshot - Add 'Paint.NET' to context (dropdown) menu.ps1
+++ windows/Greenshot - Add 'Paint.NET' to context (dropdown) menu.ps1
@@ -6,8 +6,10 @@
 #   |--> Right click the Greenshot Icon in the Tray on the Taskbar (bottom right of screen, next to clock)
 #         |
 #         |--> Click "Configure external commands"
+#         |     |
+#         |     |--> Use the "Add" & "Delete" bottons on the popup window to configure context menu options as-intended
 #         |
-#         |--> Use the "Add" & "Delete" bottons on the popup window to configure context menu options as-intended
+#         |--> Troubleshooting:  If the "Configure external commands" context menu option doesn't exist, uninstall Greenshot & reinstall making sure to enable (check the checkbox) the option the option "Open with external command plug-in" at the screen titled "Select Components"
 #
 #
 # ------------------------------------------------------------

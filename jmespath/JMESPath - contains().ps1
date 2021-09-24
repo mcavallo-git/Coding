9c066073fab16287c62cd2724d2209988626f893
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-24 16:52:40 -0400

    Update JMESPath - contains().ps1

--- jmespath/JMESPath - contains().ps1
+++ jmespath/JMESPath - contains().ps1
@@ -1,13 +1,12 @@
 # ------------------------------------------------------------
 
 
-# Get Azure Managed Identities who are in any Resource Group which contains a given string (used for finding identities within nested AKS Cluster Resource Groups, which contain the name of the AKS Cluster's Resource Group concatenated within their sub/node Resource Group name)
-$ALL_RESOURCE_GROUPS=(az group list --query "sort_by[], &name) | [].name" --output "tsv");
-$ALL_RESOURCE_GROUPS | ForEach-Object {
-	Write-Host "------------------------------------------------------------";
-	Write-Host "Listing Managed Identities in Resource Group `"${_}`"...";
+# Get Azure Managed Identities who are in any Resource Group which contains a given string
+#  |--> Intended to find Managed Identities within nested AKS Clusters' Resource Groups (which contain their parent resource group in their name)
+(az group list --query "sort_by[], &name) | [].name" --output "tsv") `
+| ForEach-Object {
+	Write-Host "------------------------------------------------------------`nListing Managed Identities in Resource Group `"${_}`"...";
 	az identity list --query "[? contains(resourceGroup,'${_}')] | sort_by([], &name) | [].name" --output "tsv";
-	Write-Host "";
 }
 
 

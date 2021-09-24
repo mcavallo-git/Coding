73d8fc3fc3961ba3c07eef01555af149e2c03dd5
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-24 16:52:39 -0400

    Update JMESPath - sort_by().ps1

--- jmespath/JMESPath - sort_by().ps1
+++ jmespath/JMESPath - sort_by().ps1
@@ -6,6 +6,18 @@ az role definition list --custom-role-only "true" --query "[] | sort_by(@, &role
 
 
 # ------------------------------------------------------------
+
+
+# Get Azure Managed Identities who are in any Resource Group which contains a given string
+#  |--> Intended to find Managed Identities within nested AKS Clusters' Resource Groups (which contain their parent resource group in their name)
+(az group list --query "sort_by[], &name) | [].name" --output "tsv") `
+| ForEach-Object {
+	Write-Host "------------------------------------------------------------`nListing Managed Identities in Resource Group `"${_}`"...";
+	az identity list --query "[? contains(resourceGroup,'${_}')] | sort_by([], &name) | [].name" --output "tsv";
+}
+
+
+# ------------------------------------------------------------
 #
 # Citation(s)
 #

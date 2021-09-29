136b25748d2d5cf4b7c0745e6065e70bf78894eb
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-29 18:33:44 -0400

    Update EdgeOS (UniFi) - Flush DHCP Leases (Force DHCP Lease-Renewal).sh

--- ubiquiti/EdgeOS (UniFi) - Flush DHCP Leases (Force DHCP Lease-Renewal).sh
+++ ubiquiti/EdgeOS (UniFi) - Flush DHCP Leases (Force DHCP Lease-Renewal).sh
@@ -2,12 +2,17 @@
 
 # ------------------------------------------------------------
 #
-# USG-3P (Unifi)  :::  Clear DHCP Leases - Step 1, clear-out "/var/run/dnsmasq-dhcp.leases" && "/config/dnsmasq-dhcp.leases"
+# EdgeOS (UniFi) - Flush DHCP Leases (Force DHCP Lease-Renewal)
 #
 
 sudo -i;
 
-if [ 1 ]; then
+# One-liner - Flush all DHCP Leases (EdgeOS/UniFi)
+LV="/var/run/dnsmasq-dhcp.leases" && R1='(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))' && R2='(3[0-2]|[1-2]?[0-9])' && R3='(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])' && SS="s/^\\S+_eth1_(${R1}\\-${R2})\\ .+\$/\\1/p" && CR=$(show dhcp statistics | sed -rne "${SS}";) && FS="${IFS}" && IFS=$'\n'; for EC in ${CR}; do OT=$(echo "${EC}" | cut -d '-' -f 1 | cut -d '.' -f 1-3); SD="/^.+ ${OT}.${R3} .+\$/d"; sed -re "${SD}" -i "${LV}"; done; IFS="${FS}" && cp -f "${LV}" "/config/$(basename ${LV})" && service dhcpd restart && sleep 3 && clear dhcp leases && echo "DHCP leases flushed";
+
+
+# Decompressed one-liner (above)'s source code
+if [ 1 ]; then \
 LIVE_DNSMASQ_LEASES="/var/run/dnsmasq-dhcp.leases" && \
 CACHE_DNSMASQ_LEASES="/config/$(basename ${LIVE_DNSMASQ_LEASES})" && \
 REGEX_MATCH_IPv4_ADDRESS='(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))' && \
@@ -17,22 +22,23 @@ SED_SUBNET_CIDRS="s/^\\S+_eth1_(${REGEX_MATCH_IPv4_ADDRESS}\\-${REGEX_MATCH_NETM
 SUBNET_CIDRS=$(show dhcp statistics | sed -rne "${SED_SUBNET_CIDRS}";) && \
 ROLLBACK_IFS="${IFS}" && IFS=$'\n'; \
 for EACH_SUBNET_CIDR in ${SUBNET_CIDRS}; do \
-echo "Info:  [ Step 1 ]  Flushing DHCP leases for CIDR \"${EACH_SUBNET_CIDR}\" from cache-file \"${LIVE_DNSMASQ_LEASES}\"" &&
-ETH1_NETWORK_IPv4=$(echo "${SUBNET_CIDRS}" | cut -d '-' -f 1); \
-ETH1_NETMASK_BITS=$(echo "${SUBNET_CIDRS}" | cut -d '-' -f 2); \
+echo "Info:  [ Step 1 ]  Flushing DHCP leases for CIDR \"${EACH_SUBNET_CIDR}\" from cache-file \"${LIVE_DNSMASQ_LEASES}\"" && \
+ETH1_NETWORK_IPv4=$(echo "${EACH_SUBNET_CIDR}" | cut -d '-' -f 1); \
+ETH1_NETMASK_BITS=$(echo "${EACH_SUBNET_CIDR}" | cut -d '-' -f 2); \
 ETH1_NETWORK_FIRST_THREE_OCTETS=$(echo "${ETH1_NETWORK_IPv4}" | cut -d '.' -f 1-3); \
 SED_DNSMASQ_LEASES="/^.+ ${ETH1_NETWORK_FIRST_THREE_OCTETS}.${REGEX_MATCH_LAST_OCTET} .+\$/d"; \
 sed --in-place=".$(date +'%Y-%m-%d_%H-%M-%S').bak" -re "${SED_DNSMASQ_LEASES}" "${LIVE_DNSMASQ_LEASES}"; \
 done; \
 IFS="${ROLLBACK_IFS}" && \
-echo "Info:  [ Step 2 ]  Copying file \"${LIVE_DNSMASQ_LEASES}\" over file \"${CACHE_DNSMASQ_LEASES}\"" &&
+echo "Info:  [ Step 2 ]  Copying file \"${LIVE_DNSMASQ_LEASES}\" over file \"${CACHE_DNSMASQ_LEASES}\"" && \
 cp -f "${LIVE_DNSMASQ_LEASES}" "${CACHE_DNSMASQ_LEASES}" && \
 service dhcpd restart && \
 sleep 3 && \
-echo "Info:  [ Step 3 ]  Flushing contents of cache-files '/var/run/dhcpd.leases' && '/config/dhcpd.leases'" &&
+echo "Info:  [ Step 3 ]  Flushing contents of cache-files '/var/run/dhcpd.leases' && '/config/dhcpd.leases'" && \
 clear dhcp leases && \
-echo "Info:  Done - All DHCP leases have been flushed";
+echo "Info:  Done - All DHCP leases have been flushed"; \
 fi;
+
 
 
 # SUBNET_CIDR=$(show dhcp statistics | sed -rne "s/^\S+_eth1_((((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))\-(3[0-2]|[1-2]?[0-9]))\ .+$/\1/p";)
@@ -40,7 +46,7 @@ fi;
 
 # ------------------------------------------------------------
 #
-# USG-3P (Unifi)  :::  If you want to snipe one, specific DHCP lease (& make it expire) without touching the majority of the live DHCP leases
+#   Unifi USG-3P  :::  If you want to snipe one, specific DHCP lease (& make it expire) without touching the majority of the live DHCP leases
 #
 
 IP_RELEASE_DHCP="192.168.1.100" && clear dhcp lease ip ${IP_RELEASE_DHCP}; # Clear one, specific device/ip's lease from DHCP

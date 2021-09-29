83f3bec8f293ce58ec82ef5070afb971a20f61eb
Matt Cavallo <mcavallo.git@gmail.com>
2021-09-29 01:16:08 -0400

    Update Linux - Bashisms (string replacement, lowercase, uppercase, parameter substitution).sh

--- linux/Linux - Bashisms (string replacement, lowercase, uppercase, parameter substitution).sh
+++ linux/Linux - Bashisms (string replacement, lowercase, uppercase, parameter substitution).sh
@@ -74,6 +74,15 @@ REPLACEMENT="d";
 MODIFIED_STRING="${HAYSTACK//${NEEDLE}/${REPLACEMENT}}" && echo "${MODIFIED_STRING}";
 
 
+# Bash Substring Replacement Example:  Slice one static substring out of another string
+HAYSTACK="/var/log/long/filepath";
+SLICE_OUT="/var/log/";
+REMAINDER="${HAYSTACK//${SUBSTRING_TO_REMOVE//\//\\/}/}";
+echo "HAYSTACK:   [ ${HAYSTACK} ]"; \
+echo "SLICE_OUT:  [ ${SLICE_OUT} ]"; \
+echo "REMAINDER:  [ ${REMAINDER} ]";
+
+
 # ------------------------------------------------------------
 #
 ### String variable to-Lowercase

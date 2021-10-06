476dce1f32b401635ab1ea6363285fa4c2b33fb8
Matt Cavallo <mcavallo.git@gmail.com>
2021-10-06 11:36:13 -0400

    Create Windows - Git-Bash for Windows Terminal.ps1

---
+++ windows/Windows - Git-Bash for Windows Terminal.ps1
@@ -0,0 +1,19 @@
+# ------------------------------ #
+#  Git Bash in Windows Terminal  #
+# ------------------------------ #
+
+Open "Windows Terminal" > "⚙️ Settings" > "+ Add a new profile" > Enter the following:
+
+Name:  Git Bash
+Command Line:  "C:\Program Files\Git\bin\bash.exe" --login -i
+Starting Directory:   %USERPROFILE%\Desktop
+Icon:   "C:\Program Files\Git\mingw64\share\git\git-for-windows.ico"
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.youtube.com  |  "How to Setup Git Bash in Windows Terminal in under a minute. - YouTube"  |  https://www.youtube.com/watch?v=zM9Mb-otqww
+#
+# ------------------------------------------------------------

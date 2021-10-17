34817eb09136571f1141c21161a8a31da416a9e7
Matt Cavallo <mcavallo.git@gmail.com>
2021-10-17 06:48:13 -0400

    Update Linux - ping (run a while loop to continuously check device online-offline status).sh

--- linux/Linux - ping (run a while loop to continuously check device online-offline status).sh
+++ linux/Linux - ping (run a while loop to continuously check device online-offline status).sh
@@ -2,7 +2,7 @@
 # ------------------------------------------------------------
 
 # ping (run a while loop to continuously check device online-offline status)
-PING_HOST="8.8.8.8"; while [ 1 -eq 1 ]; do echo -n "Pinging \"${PING_HOST}\":   "; ping -w 1 -c 1 "${PING_HOST}" | grep ms | head -n 1; done;
+PING_HOST="8.8.8.8"; while [ 1 -eq 1 ]; do echo -n "Pinging \"${PING_HOST}\":   "; ping -w 1 -c 1 "${PING_HOST}" | grep ms | head -n 1; sleep 1; done;
 
 
 # ------------------------------------------------------------

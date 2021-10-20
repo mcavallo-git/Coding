5208c0ab100d53d1b3e1df5f14f94185ed8f2aee
Matt Cavallo <mcavallo.git@gmail.com>
2021-10-20 02:10:46 -0400

    Update PowerShell - Version Number.ps1

--- powershell/PowerShell - Version Number.ps1
+++ powershell/PowerShell - Version Number.ps1
@@ -5,14 +5,21 @@
 #
 # ------------------------------------------------------------
 
-Write-Output "PowerShell v$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Minor)/10))";
+
+# Get the version of Powershell (as a string value (Major.Minor.Patch)
+$PSV=""; If ((${PSVersionTable}.PSVersion.Major)) { $PSV+=(${PSVersionTable}.PSVersion.Major); If ((${PSVersionTable}.PSVersion.Minor)) { $PSV+="."; $PSV+=(${PSVersionTable}.PSVersion.Minor); If ((${PSVersionTable}.PSVersion.Patch)) { $PSV+="."; $PSV+=(${PSVersionTable}.PSVersion.Patch); }; }; }; Write-Output "PowerShell v${PSV}";
+
+
+# Get the version of Powershell (as a decimal/numeric value (Major.Minor, only))
+$PSV=$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Minor)/10)); Write-Output "PowerShell v${PSV}";
 
 
 # ------------------------------------------------------------
+#
+# PowerShell Core has a build-in method for checking versioning
+#
 
-# As a Variable
-$PowerShellVersion=$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Minor)/10));
-Write-Output "PowerShell v$PowerShellVersion";
+pwsh --version;
 
 
 # ------------------------------------------------------------

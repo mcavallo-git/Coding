25eb5ecf9e30c9c774390defb8ebf752bc6877ff
Matt Cavallo <mcavallo.git@gmail.com>
2021-10-19 23:31:30 -0400

    install choco

--- powershell/PowerShell - PowerShell ... -Verb RunAs (Run command as Administrator).ps1
+++ powershell/PowerShell - PowerShell ... -Verb RunAs (Run command as Administrator).ps1
@@ -18,7 +18,7 @@ PowerShell -Command "If (GCM pwsh -ErrorAction SilentlyContinue) { SV PS ((GCM p
 
 
 <# PowerShell/pwsh - Run as admin (child terminal) - Install choco package manager #>
-PowerShell -Command "If (GCM pwsh -ErrorAction SilentlyContinue) { SV PS ((GCM pwsh).Source); } Else { SV PS ((GCM powershell).Source); }; Start-Process -Filepath ((GV PS).Value) -ArgumentList ('-Command SV ProgressPreference SilentlyContinue; If ((GCM choco -ErrorAction SilentlyContinue) -Eq ((GV null).Value)) { Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString((Write-Output https://chocolatey.org/install.ps1))); }') -Verb RunAs -Wait -PassThru | Out-Null;";
+PowerShell -Command "If (GCM pwsh -ErrorAction SilentlyContinue) { SV PS ((GCM pwsh).Source); } Else { SV PS ((GCM powershell).Source); }; Start-Process -Filepath ((GV PS).Value) -ArgumentList ('-Command SV ProgressPreference SilentlyContinue; If ((GCM choco -ErrorAction SilentlyContinue) -Eq ((GV null).Value)) { Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString((Write-Output https://chocolatey.org/install.ps1))); }; Start-Sleep -Seconds 60;') -Verb RunAs -Wait -PassThru | Out-Null;";
 
 
 # ------------------------------------------------------------

c95c1e29f172037a35e71f9cfeaf7f6b6e024b23
Matt Cavallo <mcavallo.git@gmail.com>
2021-10-29 05:41:59 -0400

    Create Linux - math calculations (basic addition, substraction, multiplication-multiply, division-divide, modulus-modulo, etc.).sh

---
+++ linux/Linux - math calculations (basic addition, substraction, multiplication-multiply, division-divide, modulus-modulo, etc.).sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# ------------------------------------------------------------
+# Linux - math calculations (basic addition, substraction, multiplication-multiply, division-divide, etc.)
+# ------------------------------------------------------------
+
+# Addition
+echo $(( 5 + 3 ));  # Outputs "8"
+expr 5 + 3;         # Outputs "8"
+
+
+# ------------------------------------------------------------
+
+# Substraction
+echo $(( 5 - 3 ));  # Outputs "2"
+expr 5 - 3;         # Outputs "2"
+
+
+# ------------------------------------------------------------
+
+# Multiplication
+echo $(( 5 * 3 ));  # Outputs "15"
+expr 5 \* 3;        # Outputs "15"
+
+
+# ------------------------------------------------------------
+
+# Division
+echo $(( 5 / 3 ));  # Outputs "1"
+expr 5 / 3;         # Outputs "1"
+
+
+# ------------------------------------------------------------
+
+# Modulus/Modulo
+echo $(( 10 % 6 ));  # Outputs "4"
+expr 10 % 6;         # Outputs "4"
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   man7.org  |  "expr(1) - Linux manual page"  |  https://man7.org/linux/man-pages/man1/expr.1.html
+#
+# ------------------------------------------------------------

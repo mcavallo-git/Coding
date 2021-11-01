e8a6c377624a7b54269aad25590e50090bf2e551
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-01 11:55:07 -0400

    Update Linux - ufw.sh

--- linux/Linux - ufw.sh
+++ linux/Linux - ufw.sh
@@ -27,9 +27,9 @@ ufw enable; # enable ufw module
 
 # ------------------------------------------------------------
 
-ufw status numbered; # List UFW rules by-ID
+ufw status numbered; # List UFW rules by ID
 
-ufw delete RULE_ID;  # Delete UFW rules by-ID
+ufw delete RULE_ID;  # Delete UFW rules by ID
 
 
 # ------------------------------------------------------------

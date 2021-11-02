e5018f67699a060cc4e646337afe86472f25f7b3
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-01 21:45:47 -0400

    Update PowerShell - Get-ItemProperty (registry keys, regedit).ps1

--- powershell/PowerShell - Get-ItemProperty (registry keys, regedit).ps1
+++ powershell/PowerShell - Get-ItemProperty (registry keys, regedit).ps1
@@ -1,35 +1,50 @@
+# ------------------------------------------------------------
+#
+# Get-ItemProperty
+#   |
+#   |--> Example: Check registry "HKEY_CURRENT_USER:\" for user-specific "Path" environment variables 
+#         |--> Note: Powershell's built-in environment variable $Env:Path is a combination of system & user-specific environment-variables)
+#
+# ------------------------------------------------------------
 
-#
-#	Get-ItemProperty
-#		|
-#		|--> Example: Check registry "HKEY_CURRENT_USER:\" for user-specific "Path" environment variables 
-#		|--> Note: Powershell's built-in environment variable $Env:Path is a combination of system & user-specific environment-variables)
-#
+If ($True) {
 
 $RegEdit = @{
 	Path="HKCU:\Software\Microsoft\Windows\CurrentVersion\Search";
 	Name="BingSearchEnabled";
 	Type="DWord";
 	Description="Enables (1) or Disables (0) Cortana's ability to send search-resutls to Bing.com. Set to '1' to resolve high-cpu bug from Windows-Update KB4512941 (2019-Sept).";
-	Value=1;
+	Value=0;
 };
 
-If ((Test-Path -Path ($RegEdit.Path)) -Eq $True) {
-	Set-ItemProperty -Path ($RegEdit.Path) -Name ($RegEdit.Name) -Value ($RegEdit.Value);
+If ((Test-Path -Path (${RegEdit}.Path)) -Eq $True) {
+	$GetEachItemProp = (Get-ItemPropertyValue -LiteralPath (${RegEdit}.Path) -Name (${RegEdit}.Name) -ErrorAction ("Stop"));
+	If ((${GetEachItemProp}) -Eq (${RegEdit}.Value)) {
+		Write-Host "`nInfo:  (Skipped) Registry key `"$(${RegEdit}.Path)`"'s property `"$(${RegEdit}.Name)`" is already set to value `"$(${RegEdit}.Value)`"";
+	} Else {
+		Write-Host "`nInfo:  Setting Registry key `"$(${RegEdit}.Path)`"'s property `"$(${RegEdit}.Name)`" to value `"$(${RegEdit}.Value)`"...";
+		Set-ItemProperty -LiteralPath (${RegEdit}.Path) -Name (${RegEdit}.Name) -Value (${RegEdit}.Value);
+	}
 } Else {
-	New-ItemProperty -Path ($RegEdit.Path) -Name ($RegEdit.Name) -PropertyType ($RegEdit.Type) -Value ($RegEdit.Value);
+	Write-Host "`nInfo:  Creating Registry key `"$(${RegEdit}.Path)`"'s property `"$(${RegEdit}.Name)`" with type `"$(${RegEdit}.Type)`" and value `"$(${RegEdit}.Value)`"...";
+	New-ItemProperty -LiteralPath (${RegEdit}.Path) -Name (${RegEdit}.Name) -PropertyType (${RegEdit}.Type) -Value (${RegEdit}.Value);
+}
+Write-Host "`nInfo:  Getting info for Registry key `"$(${RegEdit}.Path)`"'s property `"$(${RegEdit}.Name)`"...";
+Get-ItemProperty -LiteralPath (${RegEdit}.Path) -Name (${RegEdit}.Name);
+
 }
 
-Get-ItemProperty -Path ($RegEdit.Path) -Name ($RegEdit.Name)
 
-
-<# Example of Registry Path w/ inline Property name #>
+<# Example - Separate vs Combined [ Registry Key Path ] & [ Registry Key Property Name ] #>
+<#  |-->  DEPRECATED ??? Not sure where this works (or what cmdlets/modules it applies to) ???
 # Path="Registry::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows NT\Terminal Services!MaxCompressionLevel";
 
 
 # ------------------------------------------------------------
 #
 #	Citation(s)
+#
+#		docs.microsoft.com  |  "Get-ItemProperty (Microsoft.PowerShell.Management) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-itemproperty
 #
 #		ss64.com  |  "New-ItemProperty - Set a new property, for an item at a given location"  |  https://ss64.com/ps/new-itemproperty.html
 #

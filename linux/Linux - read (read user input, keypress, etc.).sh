e72d0b66afa382887e8358106b4a41b0d23879ab
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-04 03:47:59 -0400

    Update Linux - read (read user input, keypress, etc.).sh

--- linux/Linux - read (read user input, keypress, etc.).sh
+++ linux/Linux - read (read user input, keypress, etc.).sh
@@ -2,14 +2,16 @@
 
 # ------------------------------------------------------------
 
-read -p "Press any key to continue...  " -n 1 -t 60 -r; # Await single keypress
+read -p "Press any key to continue...  " -n 1 -t 60 REPLY <'/dev/tty'; # Await single keypress (works with curl'ed scripts)
+
+read -p "Press any key to continue...  " -n 1 -t 60 -r; # Await single keypress (using [ -r ] breaks on curl commands (fails read immediately))
 
 # ------------------------------------------------------------
 # Await multiple keypresses (general keyboard string-entry)
 
 if [ 1 -eq 1 ]; then
 	READ_TIMEOUT=60;
-	read -p "Enter a string:  " -t ${READ_TIMEOUT} -r; RETURN_CODE_READ=$?;
+	read -p "Enter a string:  " -t ${READ_TIMEOUT} REPLY <'/dev/tty'; RETURN_CODE_READ=$?;
 	echo "";
 	if [ ${RETURN_CODE_READ} -le 128 ] && [ -n "${REPLY}" ]; then
 		echo "Info:  Response received: \"${REPLY}\"";
@@ -28,7 +30,7 @@ fi;
 if [ 1 -eq 1 ]; then
   ACTION_DESCRIPTION="ACTION_DESCRIPTION_HERE";
   READ_TIMEOUT=3;
-  read -p "Perform action [ ${ACTION_DESCRIPTION} ], now? (y/n)  " -n 1 -t ${READ_TIMEOUT} -r; RETURN_CODE_READ=$?;
+  read -p "Perform action [ ${ACTION_DESCRIPTION} ], now? (y/n)  " -n 1 -t ${READ_TIMEOUT} REPLY <'/dev/tty'; RETURN_CODE_READ=$?;
   echo "";
   if [ ${RETURN_CODE_READ} -gt 128 ]; then
     echo "Error:  Response timed out after ${READ_TIMEOUT}s";
@@ -52,11 +54,11 @@ if [ 1 -eq 1 ]; then
   echo -e "! ! NOTICE ! !  THIS IS A DOUBLE CONFIRMATION MESSAGE  ! ! NOTICE ! !";
   echo -e "! !   |    ! !  PLEASE READ IT BEFORE HITTING Y (YES)  ! !        ! !";
   echo -e "      |";
-  read -p "      |--> Are you sure you want to continue? (y/n)  " -n 1 -t 60 -r; # Await single keypress
+  read -p "      |--> Are you sure you want to continue? (y/n)  " -n 1 -t 60 REPLY <'/dev/tty'; # Await single keypress
   echo -e "";
   if [ -n "${REPLY}" ] && [ "$(echo ${REPLY} | tr '[:lower:]' '[:upper:]';)" == "Y" ]; then
     echo -e "      |";
-    read -p "      |--> Are you completely positive you wish to continue? (y/n)  " -n 1 -t 60 -r; # Await single keypress
+    read -p "      |--> Are you completely positive you wish to continue? (y/n)  " -n 1 -t 60 REPLY <'/dev/tty'; # Await single keypress
     echo -e "";
     if [ -n "${REPLY}" ] && [ "$(echo ${REPLY} | tr '[:lower:]' '[:upper:]';)" == "Y" ]; then
       echo "Confirmed twice!";

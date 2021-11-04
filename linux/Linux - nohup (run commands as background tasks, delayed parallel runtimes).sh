fedcde4fd2c1c1dcf24dc89b6a1c0c481922aa6d
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-04 03:17:34 -0400

    Update Linux - nohup (run commands as background tasks, delayed parallel runtimes).sh

--- linux/Linux - nohup (run commands as background tasks, delayed parallel runtimes).sh
+++ linux/Linux - nohup (run commands as background tasks, delayed parallel runtimes).sh
@@ -1,32 +1,49 @@
 #!/bin/bash
+# ------------------------------------------------------------
+#
+# Linux - Background jobs
+#
 
-# Basic syntax for running tasks in background
+# Background: When you enter an ampersand (&) symbol at the end of a command line, the command runs without occupying the terminal window. The shell prompt is displayed immediately after you press Return. This is an example of a background job.
 
-nohup seq 1234568 > ~/tester.txt 2>&1 &
+# General Syntax
+command &
+
+# General Syntax (using nohup)
+nohup command &
+
 
 # ------------------------------------------------------------
 
-echo "$(date +'%D %r')";
+# Example - Background job in Linux
+if [ 1 -eq 1 ]; then
+  echo "$(date +'%D %r')";
+  LOGFILE="${HOME}/tester_$(date +'%s';)";
+  ( sleep 3; echo "$(date +'%D %r')">"${LOGFILE}"; echo "whoami: $(whoami);">>"${LOGFILE}"; ) & echo "";
+  for i in {1..5}; do
+    if [ -f "${LOGFILE}" ] && [ -n "$(cat "${LOGFILE}" 2>'/dev/null';)" ]; then
+      echo "------------------------------";
+      cat "${LOGFILE}";
+      echo "------------------------------";
+      break;
+    fi;
+    sleep 1;
+  done;
+fi;
 
-TEST_OUTPUT="${HOME}/tester_$(date +'%s')";
+# Example - Background job in Linux (using nohup)
+nohup seq 1234568 > ~/tester.txt 2>&1 &
 
-(sleep 5; echo "$(date +'%D %r')">"${TEST_OUTPUT}"; echo "whoami: $(whoami);">>"${TEST_OUTPUT}"; ) & echo "" 1>'/dev/null';
-
-# (sleep 5; echo "$(date +'%D %r')">"${TEST_OUTPUT}"; echo "whoami: $(whoami);">>"${TEST_OUTPUT}"; ) & 
-
-sleep 2 && cat "${TEST_OUTPUT}"; \
-sleep 2 && cat "${TEST_OUTPUT}"; \
-sleep 2 && cat "${TEST_OUTPUT}";
 
 # ------------------------------------------------------------
 #
 # DELAYED REBOOT
 #
 
-## Reboot @ 11:59 PM
+# Reboot @ 11:59 PM
 sudo shutdown -r 23:59 &
 
-## Reboot in 1 minute
+# Reboot in 1 minute
 sudo shutdown --reboot "+1" &
 
 
@@ -34,6 +51,10 @@ sudo shutdown --reboot "+1" &
 #
 # Citation(s)
 #
+#   linuxize.com  |  "How to Run Linux Commands in Background | Linuxize"  |  https://linuxize.com/post/how-to-run-linux-commands-in-background/
+#
 #   www.golinuxhub.com  |  "How to keep a process running after putty or terminal is closed - GoLinuxHub"  |  https://www.golinuxhub.com/2014/11/how-to-keep-process-running-after-putty.html
+#
+#   www.thegeekdiary.com  |  "Understanding the job control commands in Linux – bg, fg and CTRL+Z – The Geek Diary"  |  https://www.thegeekdiary.com/understanding-the-job-control-commands-in-linux-bg-fg-and-ctrlz/
 #
 # ------------------------------------------------------------

40daa1cf2b8b7d6b59e824d0f0f23c595ccf981f
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-12 20:38:54 -0500

    Update Linux - Clear terminal history (Ubuntu-Debian, history -c + rm .bash_history).sh

--- linux/Linux - Clear terminal history (Ubuntu-Debian, history -c + rm .bash_history).sh
+++ linux/Linux - Clear terminal history (Ubuntu-Debian, history -c + rm .bash_history).sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# ------------------------------------------------------------
 
 # Linux - Clear the cached/saved terminal history
-# Distros: Debian, Ubuntu, etc.
+#    Distros: Debian, Ubuntu, etc.
 
-history -c; test -e ~/.bash_history && rm ~/.bash_history;
+history -c; if [ -f "${HOME}/.bash_history" ]; then rm --verbose "${HOME}/.bash_history"; fi;
 
 
 # ------------------------------------------------------------
 #
 # Citation(s)
+#
+#   man7.org  |  history(3) - Linux manual page”  |  https://man7.org/linux/man-pages/man3/history.3.html
 #
 #   www.cyberciti.biz |  “How To Clear Shell History In Ubuntu Linux - nixCraft”  |  https://www.cyberciti.biz/faq/clear-the-shell-history-in-ubuntu-linux/
 #

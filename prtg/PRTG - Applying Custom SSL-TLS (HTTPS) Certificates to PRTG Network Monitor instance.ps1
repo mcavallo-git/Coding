064853156a6fae940e8e553fb3eeda208169ef50
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-17 02:16:43 -0500

    Update PRTG - Applying Custom SSL-TLS (HTTPS) Certificates to PRTG Network Monitor instance.ps1

--- prtg/PRTG - Applying Custom SSL-TLS (HTTPS) Certificates to PRTG Network Monitor instance.ps1
+++ prtg/PRTG - Applying Custom SSL-TLS (HTTPS) Certificates to PRTG Network Monitor instance.ps1
@@ -37,59 +37,62 @@ If ($True) {
 
 		} Else {
 
-		$TIMESTAMP=(Get-Date -Format 'yyyyMMddTHHmmss.ffffffzz');
-		$STEP_NUM=0;
+			$TIMESTAMP=(Get-Date -Format 'yyyyMMddTHHmmss.ffffffzz');
+			$STEP_NUM=0;
 
-		# Certificate (1 of 3 SSL/TLS files)
-		$TYPE_OF_CERT="Certificate";
-		$PRTG_CERT="prtg.crt";
-		$LETS_ENCRYPT_CERT="cert.pem";
-		# Backup existing PRTG certificate
-		Copy-Item -Path ("${PRTG_CERTS_DIR}\${PRTG_CERT}") -Destination ("${PRTG_CERTS_DIR}\${PRTG_CERT}.${TIMESTAMP}.bak") -Force;
-		# Direct user for where to place their cert file(s)
-		Write-Host "";
-		Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
-		Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host " file (" -NoNewLine;
-		Write-Host "`"${LETS_ENCRYPT_CERT}`" for Let's Encrypt certs" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host ") to filepath `"" -NoNewLine;
-		Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host "`" " -NoNewLine;
+			# Certificate (1 of 3 SSL/TLS files)
+			$TYPE_OF_CERT="Certificate";
+			$PRTG_CERT="prtg.crt";
+			$LETS_ENCRYPT_CERT="cert.pem";
+			# Backup existing PRTG certificate
+			Copy-Item -Path ("${PRTG_CERTS_DIR}\${PRTG_CERT}") -Destination ("${PRTG_CERTS_DIR}\${PRTG_CERT}.${TIMESTAMP}.bak") -Force;
+			# Direct user for where to place their cert file(s)
+			Write-Host "";
+			Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
+			Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host " file (" -NoNewLine;
+			Write-Host "`"${LETS_ENCRYPT_CERT}`" for Let's Encrypt certs" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host ") to filepath `"" -NoNewLine;
+			Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host "`" " -NoNewLine;
 
-		# Private Key (1 of 3 SSL/TLS files)
-		$TYPE_OF_CERT="Private Key";
-		$PRTG_CERT="prtg.key";
-		$LETS_ENCRYPT_CERT="privkey.pem";
-		# Backup existing PRTG certificate
-		Copy-Item -Path ("${PRTG_CERTS_DIR}\${PRTG_CERT}") -Destination ("${PRTG_CERTS_DIR}\${PRTG_CERT}.${TIMESTAMP}.bak") -Force;
-		# Direct user for where to place their cert file(s)
-		Write-Host "";
-		Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
-		Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host " file (" -NoNewLine;
-		Write-Host "`"${LETS_ENCRYPT_CERT}`" for Let's Encrypt certs" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host ") to filepath `"" -NoNewLine;
-		Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host "`" " -NoNewLine;
+			# Private Key (1 of 3 SSL/TLS files)
+			$TYPE_OF_CERT="Private Key";
+			$PRTG_CERT="prtg.key";
+			$LETS_ENCRYPT_CERT="privkey.pem";
+			# Backup existing PRTG certificate
+			Copy-Item -Path ("${PRTG_CERTS_DIR}\${PRTG_CERT}") -Destination ("${PRTG_CERTS_DIR}\${PRTG_CERT}.${TIMESTAMP}.bak") -Force;
+			# Direct user for where to place their cert file(s)
+			Write-Host "";
+			Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
+			Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host " file (" -NoNewLine;
+			Write-Host "`"${LETS_ENCRYPT_CERT}`" for Let's Encrypt certs" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host ") to filepath `"" -NoNewLine;
+			Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host "`" " -NoNewLine;
 
-		# Public Root Certificate(s) for Certificate Issuer (1 of 3 SSL/TLS files)
-		$TYPE_OF_CERT="CABundle";
-		$PRTG_CERT="root.pem";
-		$LETS_ENCRYPT_CERT="chain.pem";
-		# Backup existing PRTG certificate
-		Copy-Item -Path ("${PRTG_CERTS_DIR}\${PRTG_CERT}") -Destination ("${PRTG_CERTS_DIR}\${PRTG_CERT}.${TIMESTAMP}.bak") -Force;
-		# Direct user for where to place their cert file(s)
-		Write-Host "";
-		Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
-		Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host " file (" -NoNewLine;
-		Write-Host "`"${LETS_ENCRYPT_CERT}`" for Let's Encrypt certs" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host ") to filepath `"" -NoNewLine;
-		Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
-		Write-Host "`" " -NoNewLine;
-		Write-Host "";
+			# Public Root Certificate(s) for Certificate Issuer (1 of 3 SSL/TLS files)
+			$TYPE_OF_CERT="CABundle";
+			$PRTG_CERT="root.pem";
+			$LETS_ENCRYPT_CERT="chain.pem";
+			# Backup existing PRTG certificate
+			Copy-Item -Path ("${PRTG_CERTS_DIR}\${PRTG_CERT}") -Destination ("${PRTG_CERTS_DIR}\${PRTG_CERT}.${TIMESTAMP}.bak") -Force;
+			# Direct user for where to place their cert file(s)
+			Write-Host "";
+			Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
+			Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host " file (" -NoNewLine;
+			Write-Host "`"${LETS_ENCRYPT_CERT}`" for Let's Encrypt certs" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host ") to filepath `"" -NoNewLine;
+			Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
+			Write-Host "`" " -NoNewLine;
 
-		# Open the PRTG certificates directory to assist user in placing their SSL/TLS files
+			# Open the PRTG certificates directory to assist user in placing their SSL/TLS files
+			explorer.exe "${PRTG_CERTS_DIR}";
+			Write-Host "";
+
+		}
 
 	}
 

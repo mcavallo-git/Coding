1508e2092645c6e759cc91c24a3fdb5e5bbe09a5
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-17 02:47:17 -0500

    Update PRTG - Applying Custom SSL-TLS (HTTPS) Certificates to PRTG Network Monitor instance.ps1

--- prtg/PRTG - Applying Custom SSL-TLS (HTTPS) Certificates to PRTG Network Monitor instance.ps1
+++ prtg/PRTG - Applying Custom SSL-TLS (HTTPS) Certificates to PRTG Network Monitor instance.ps1
@@ -32,8 +32,10 @@ If ($True) {
 		# Require Escalated Privileges
 		If ((RunningAsAdministrator) -NE ($True)) {
 
+			Write-Host "------------------------------";
 			Write-Host "";
 			Write-Host "ERROR  -  Requires elevated permissions - Please re-run as Administrator";
+			Write-Host "";
 
 		} Else {
 
@@ -41,58 +43,82 @@ If ($True) {
 			$STEP_NUM=0;
 
 			# ------------------------------
+			#
 			# Backup existing PRTG certificates directory
+			#
+			Write-Host "------------------------------";
 			Write-Host "";
-			Write-Host "Backing up existing PRTG certificates directory to `"${PRTG_CERTS_DIR}.bak.${TIMESTAMP}`"..." -NoNewLine;
+			Write-Host "  Backing up existing PRTG certificates directory to filepath:`n    `"${PRTG_CERTS_DIR}.bak.${TIMESTAMP}`"..." -NoNewLine;
 			Copy-Item -Path ("${PRTG_CERTS_DIR}") -Destination ("${PRTG_CERTS_DIR}.bak.${TIMESTAMP}") -Force -Recurse;
+			Write-Host "";
+
 
 			# ------------------------------
+			#
 			# Certificate (1 of 3 SSL/TLS files)
+			#
 			$TYPE_OF_CERT="Certificate";
 			$PRTG_CERT="prtg.crt";
 			$LETS_ENCRYPT_CERT="cert.pem";
 			# Direct user for where to place their cert file(s)
+			Write-Host "------------------------------";
 			Write-Host "";
-			Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
+			Write-Host "  Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
 			Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host " file (`"" -NoNewLine;
 			Write-Host "${LETS_ENCRYPT_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host "`" for Let's Encrypt certs) to filepath `"" -NoNewLine;
 			Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host "`" ";
+			Write-Host "";
 
 			# ------------------------------
+			#
 			# Private Key (1 of 3 SSL/TLS files)
+			#
 			$TYPE_OF_CERT="Private Key";
 			$PRTG_CERT="prtg.key";
 			$LETS_ENCRYPT_CERT="privkey.pem";
 			# Direct user for where to place their cert file(s)
+			Write-Host "------------------------------";
 			Write-Host "";
-			Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
+			Write-Host "  Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
 			Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host " file (`"" -NoNewLine;
 			Write-Host "${LETS_ENCRYPT_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host "`" for Let's Encrypt certs) to filepath `"" -NoNewLine;
 			Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host "`" ";
+			Write-Host "";
 
 			# ------------------------------
-			# Public Root Certificate(s) for Certificate Issuer (1 of 3 SSL/TLS files)
+			#
+			# CABundle (Public Root Certificate(s) from Certificate Issuer) (1 of 3 SSL/TLS files)
+			#
 			$TYPE_OF_CERT="CABundle";
 			$PRTG_CERT="root.pem";
 			$LETS_ENCRYPT_CERT="chain.pem";
 			# Direct user for where to place their cert file(s)
+			Write-Host "------------------------------";
 			Write-Host "";
-			Write-Host "Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
+			Write-Host "  Step $(${STEP_NUM}++;${STEP_NUM};):  Copy your PEM-formatted SSL/TLS " -NoNewLine;
 			Write-Host "${TYPE_OF_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host " file (`"" -NoNewLine;
 			Write-Host "${LETS_ENCRYPT_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host "`" for Let's Encrypt certs) to filepath `"" -NoNewLine;
 			Write-Host "${PRTG_CERTS_DIR}\${PRTG_CERT}" -NoNewLine -ForegroundColor "Yellow";
 			Write-Host "`" ";
+			Write-Host "";
 
 			# ------------------------------
+
+			# ------------------------------
+			#
 			# Open the PRTG certificates directory to assist user in placing their SSL/TLS files
+			#
+
+			Write-Host "------------------------------";
+			Write-Host "";
 			explorer.exe "${PRTG_CERTS_DIR}";
 
 			# ------------------------------
@@ -100,8 +126,6 @@ If ($True) {
 		}
 
 	}
-
-	Write-Host "";
 
 }
 

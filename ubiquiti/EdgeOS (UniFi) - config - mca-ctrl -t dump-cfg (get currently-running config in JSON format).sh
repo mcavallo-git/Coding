fa6fb29ffc7f7c6d35552737b1671fdeccdaf562
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-23 08:00:08 -0500

    Update EdgeOS (UniFi) - config - mca-ctrl -t dump-cfg (get currently-running config in JSON format).sh

--- ubiquiti/EdgeOS (UniFi) - config - mca-ctrl -t dump-cfg (get currently-running config in JSON format).sh
+++ ubiquiti/EdgeOS (UniFi) - config - mca-ctrl -t dump-cfg (get currently-running config in JSON format).sh
@@ -14,7 +14,6 @@
 
 # Step 2 - Export the config to JSON format (via the following command, which outputs to logged-in user's directory regardless if they're masquerading as root (via sudo) or not)
 OUTFILE="$(getent passwd ${SUDO_USER:-${USER}} | cut -d : -f 6)/mca-ctrl -t dump-cfg.$(date +'%Y%m%d_%H%M%S').$(hostname).json"; mca-ctrl -t dump-cfg > "${OUTFILE}"; echo "OUTFILE=[ ${OUTFILE} ]";
-# mca-ctrl -t dump-cfg > "${HOME}/mca-ctrl -t dump-cfg.$(date +'%Y%m%d_%H%M%S').$(hostname).json";
 
 
 # Step 3 - Download the exported JSON file via your SFTP tool of choice (the JSON file will have been placed in user's home directory)

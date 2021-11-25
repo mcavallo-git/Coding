91d22424213d0cc4ad8298439063d608bc8e5bc0
Matt Cavallo <mcavallo.git@gmail.com>
2021-11-25 05:36:42 -0500

    Create PowerShell - 7-zip (extracting file(s) from an archive).ps1

---
+++ powershell/PowerShell - 7-zip (extracting file(s) from an archive).ps1
@@ -0,0 +1,30 @@
+# @ECHO OFF
+# ------------------------------------------------------------
+# 
+#  Windows/Linux - 7-zip setup & action(s) via CLI
+#
+# ------------------------------------------------------------
+#
+# General Syntax
+#
+
+7z x "${INPUT_ARCHIVES}/*.7z" -o${OUTPUT_DIRECTORY};
+
+
+# ------------------------------------------------------------
+#
+# In-Depth Analysis (Example)
+#  |
+#  |-->  Refer to:  https://raw.githubusercontent.com/mcavallo-git/Coding/master/windows/HandBrake/HandBrakeCLI-Encoder/HandBrakeCLI-Encoder.ps1
+#
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   7-zip.org  |  "Download"  |  7-zip.org/download.html
+#
+#   documentation.help  |  "- 7-Zip Documentation"  |  https://documentation.help/7-Zip/
+#
+# ------------------------------------------------------------

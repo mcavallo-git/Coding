ce7ea4f53a064250c30a97d4ad9449767075ecdd
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-07 00:28:25 -0500

    Update Linux - comm (get duplicate, unique, common, matching lines between two files (strings)).sh

--- linux/Linux - comm (get duplicate, unique, common, matching lines between two files (strings)).sh
+++ linux/Linux - comm (get duplicate, unique, common, matching lines between two files (strings)).sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 # ------------------------------------------------------------
 
-# Print lines present in both file1 & file2
+# Print lines present in both files
 comm -12 file1 file2
 
-# Print lines from file1 that do not match any line in file2  -  e.g. print lines in file1 not in file2, and vice versa
+# Print lines unique to either file
 comm -3 file1 file2
+
+# Print lines unique to the first file
+comm -23 file1 file2
+
+# Print lines unique to the second file
+comm -13 file1 file2
 
 
 # ------------------------------------------------------------
 
 # Example - comm  (setup two files with contents, then compare)
-VV="a"; echo -e "${VV}-1\n${VV}-2\n${VV}-3\n1\n22\n333" > ${VV}.txt
-VV="b"; echo -e "${VV}-1\n${VV}-2\n${VV}-3\n1\n22\n333" > ${VV}.txt
-comm -3 a.txt b.txt;  # Print lines from a.txt that do not match any line in b.txt
+VV="a"; echo -e "${VV}-1\n${VV}-2\n${VV}-3\n1\n22\n333" | sort > ${VV}.txt;
+VV="b"; echo -e "${VV}-1\n${VV}-2\n${VV}-3\n1\n22\n333" | sort > ${VV}.txt;
 
+comm -12 a.txt b.txt;  # Print lines present in both files
+
+comm -3 a.txt b.txt;  # Print lines unique to either file
+
+comm -23 a.txt b.txt;  # Print lines unique to the first file
+
+comm -13 a.txt b.txt;  # Print lines unique to the second file
 
 
 

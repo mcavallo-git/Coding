a5a64c976f4f9e3c100f6382da3713aea476ab78
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-07 00:23:59 -0500

    Update Linux - sort strings by character length (awk + sort + cut).sh

--- linux/Linux - sort strings by character length (awk + sort + cut).sh
+++ linux/Linux - sort strings by character length (awk + sort + cut).sh
@@ -12,6 +12,24 @@ echo -e "12characters\n18stringcharacters\n6chars" | awk '{ print length, $0 }' 
 
 # ------------------------------------------------------------
 #
+# Advanced sorting using 'LC_COLLATE=...' declaration
+#
+
+if [ 1 -eq 1 ]; then
+echo -e "1\na\nB\n2\nA\nb" > test-sort.txt;
+SORT="(none)"; echo -e "-----\nSORT=[ ${SORT} ]"; cat test-sort.txt;
+SORT="general-numeric"; echo -e "-----\nSORT=[ ${SORT} ]"; sort --sort="${SORT}" test-sort.txt;
+SORT="human-numeric"; echo -e "-----\nSORT=[ ${SORT} ]"; sort --sort="${SORT}" test-sort.txt;
+SORT="month"; echo -e "-----\nSORT=[ ${SORT} ]"; sort --sort="${SORT}" test-sort.txt;
+SORT="numeric"; echo -e "-----\nSORT=[ ${SORT} ]"; sort --sort="${SORT}" test-sort.txt;
+SORT="random"; echo -e "-----\nSORT=[ ${SORT} ]"; sort --sort="${SORT}" test-sort.txt;
+SORT="version"; echo -e "-----\nSORT=[ ${SORT} ]"; sort --sort="${SORT}" test-sort.txt;
+echo "-----";
+fi;
+
+
+# ------------------------------------------------------------
+#
 # Example)  Sort all filepaths in a git repository by full filepath string length
 #
 
@@ -23,5 +41,7 @@ REPO="Coding"; find "${HOME}/Documents/GitHub/${REPO}" -not -path "${HOME}/Docum
 # Citation(s)
 #
 #   stackoverflow.com  |  "bash - Sort a text file by line length including spaces - Stack Overflow"  |  https://stackoverflow.com/a/5917762
+#
+#   unix.stackexchange.com  |  "linux - Specify the sort order with LC_COLLATE so lowercase is before uppercase - Unix & Linux Stack Exchange"  |  https://unix.stackexchange.com/a/361021
 #
 # ------------------------------------------------------------

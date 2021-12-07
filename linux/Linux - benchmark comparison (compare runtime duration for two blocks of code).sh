4cb92de693054e51a2e529ffc7cca7503913e263
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-06 23:43:00 -0500

    Update Linux - benchmark comparison (compare runtime duration for two blocks of code).sh

--- linux/Linux - benchmark comparison (compare runtime duration for two blocks of code).sh
+++ linux/Linux - benchmark comparison (compare runtime duration for two blocks of code).sh
@@ -17,12 +17,16 @@ if [ 1 -eq 1 ]; then
   # Setup/run benchmark #1
   #
   BENCHMARK_1_START=$(date +'%s.%N';);
-  for i in $(seq ${LOOP_ITERATIONS}); do # ---- START CODE BLOCK #1
+  for i in $(seq ${LOOP_ITERATIONS}); do
+    # ---- START CODE BLOCK #1
+
 
     # sed - Trim leading whitespace && trailing whitespace (method 1)
     echo "  a  b  c  d  " | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' 1>/dev/null 2>&1;
 
-  done; # ------------------------------------- END CODE BLOCK #1
+
+    # ---- END CODE BLOCK #1
+  done;
   BENCHMARK_1_END=$(date +'%s.%N';)
   BENCHMARK_1_DELTA=$(echo "scale=4; (${BENCHMARK_1_END} - ${BENCHMARK_1_START})/1" | bc -l | sed 's/\([^0-9]\|^\)\(\.[0-9]*\)/\10\2/g';);
 
@@ -31,12 +35,16 @@ if [ 1 -eq 1 ]; then
   # Setup/run benchmark #2
   #
   BENCHMARK_2_START=$(date +'%s.%N';);
-  for i in $(seq ${LOOP_ITERATIONS}); do # ---- START CODE BLOCK #2
+  for i in $(seq ${LOOP_ITERATIONS}); do
+    # ---- START CODE BLOCK #2
+
 
     # sed - Trim leading whitespace && trailing whitespace (method 2)
     echo "  a  b  c  d  " | sed -e 's/^[ \t]*//;s/[ \t]*$//' 1>/dev/null 2>&1;
 
-  done; # ------------------------------------- END CODE BLOCK #2
+
+    # ---- END CODE BLOCK #2
+  done;
   BENCHMARK_2_END=$(date +'%s.%N';)
   BENCHMARK_2_DELTA=$(echo "scale=4; (${BENCHMARK_2_END} - ${BENCHMARK_1_START})/1" | bc -l | sed 's/\([^0-9]\|^\)\(\.[0-9]*\)/\10\2/g';);
 

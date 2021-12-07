dba7cca9a4aa60e3aadd4518ab258bf07d40676b
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-06 23:35:03 -0500

    Update Linux - benchmark comparison (compare runtime duration for two blocks of code).sh

--- linux/Linux - benchmark comparison (compare runtime duration for two blocks of code).sh
+++ linux/Linux - benchmark comparison (compare runtime duration for two blocks of code).sh
@@ -48,10 +48,10 @@ if [ 1 -eq 1 ]; then
   BENCHMARK_1_MINUS_2_DELTA=$(echo "scale=4; (${BENCHMARK_1_DELTA} - ${BENCHMARK_2_DELTA})/1" | bc -l;);
   BENCHMARK_2_MINUS_1_DELTA=$(echo "scale=4; (${BENCHMARK_2_DELTA} - ${BENCHMARK_1_DELTA})/1" | bc -l;);
 
-  BENCHMARK_1_DIV_2_DECIMAL=$(echo "scale=4; (${BENCHMARK_1_DELTA}/${BENCHMARK_2_DELTA})" | bc -l;);
+  BENCHMARK_1_DIV_2_DECIMAL=$(echo "scale=4; (${BENCHMARK_1_DELTA}/${BENCHMARK_2_DELTA})" | bc -l | sed 's/\([^0-9]\|^\)\(\.[0-9]*\)/\10\2/g';);
   BENCHMARK_1_DIV_2_PERCENTAGE=$(echo "scale=2; (${BENCHMARK_1_DIV_2_DECIMAL}*100)/1" | bc -l;);
 
-  BENCHMARK_2_DIV_1_DECIMAL=$(echo "scale=4; (${BENCHMARK_2_DELTA}/${BENCHMARK_1_DELTA})" | bc -l;);
+  BENCHMARK_2_DIV_1_DECIMAL=$(echo "scale=4; (${BENCHMARK_2_DELTA}/${BENCHMARK_1_DELTA})" | bc -l | sed 's/\([^0-9]\|^\)\(\.[0-9]*\)/\10\2/g';);
   BENCHMARK_2_DIV_1_PERCENTAGE=$(echo "scale=2; (${BENCHMARK_2_DIV_1_DECIMAL}*100)/1" | bc -l;);
 
   # ------------------------------------------------------------
@@ -62,23 +62,23 @@ if [ 1 -eq 1 ]; then
   echo "------------------------------";
   echo "Results:";
   echo "";
-  echo "  Runtime Duration - Code Block #1:  ${BENCHMARK_1_DELTA}s";
-  echo "  Runtime Duration - Code Block #2:  ${BENCHMARK_2_DELTA}s";
+  echo "  -  Runtime Duration - Code Block #1:  ${BENCHMARK_1_DELTA}s";
+  echo "  -  Runtime Duration - Code Block #2:  ${BENCHMARK_2_DELTA}s";
   echo "";
   echo "------------------------------";
   echo "Winner:";
   if [ $(echo "${BENCHMARK_1_DELTA} < ${BENCHMARK_2_DELTA}" | bc;) -eq 1 ]; then
     # Code Block #1 ran faster
     echo "";
-    echo "  Code Block #1";
+    echo "  CODE BLOCK #1  ! ! !";
     echo "    -  Ran faster than Code Block #2 w/ a ratio of [ 1.0000 : ${BENCHMARK_2_DIV_1_DECIMAL} ]";
     echo "    -  Completed in ${BENCHMARK_1_DIV_2_PERCENTAGE}% of the time that it took to complete Code Block #2";
   else
     # Code Block #2 ran faster
     echo "";
     echo "  Code Block #1";
-    echo "    -  Ran faster than Code Block #2 w/ a ratio of [ 1.0000 : ${BENCHMARK_1_DIV_2_DECIMAL} ]";
-    echo "    -  Completed in ${BENCHMARK_2_DIV_1_PERCENTAGE}% of the time that it took to complete Code Block #2";
+    echo "    ^--  Ran faster than Code Block #2 w/ a ratio of [ 1.0000 : ${BENCHMARK_1_DIV_2_DECIMAL} ]";
+    echo "    ^--  Completed in ${BENCHMARK_2_DIV_1_PERCENTAGE}% of the time that it took to complete Code Block #2";
   fi;
   echo "";
   echo "------------------------------";

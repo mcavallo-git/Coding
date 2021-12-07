9803c6de1c7db504585311dafffb3fbe21a33d47
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-06 23:00:30 -0500

    Update Linux - benchmark comparison (compare runtime duration for two blocks of code).sh

--- linux/Linux - benchmark comparison (compare runtime duration for two blocks of code).sh
+++ linux/Linux - benchmark comparison (compare runtime duration for two blocks of code).sh
@@ -5,6 +5,10 @@
 #
 
 if [ 1 -eq 1 ]; then
+  # ------------------------------
+
+  # Set how many times to run each code block
+  LOOP_ITERATIONS=1000;
 
   # ------------------------------
   # Ensure command "bc" exists
@@ -12,18 +16,18 @@ if [ 1 -eq 1 ]; then
     apt-get update -y; apt-get install -y bc;
   fi;
 
-  echo "";
 
   # ------------------------------------------------------------
   #
   # Setup/run benchmark #1
   #
   BENCHMARK_1_START=$(date +'%s.%N';);
-  if [ 1 -eq 1 ]; then # --- START CODE BLOCK #1
+  for i in $(seq ${LOOP_ITERATIONS}); do # ---- START CODE BLOCK #1
 
-    sleep 1;
+    # sed - Trim leading whitespace && trailing whitespace (method 1)
+    echo "  a  b  c  d  " | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' 1>/dev/null 2>&1;
 
-  fi; # -------------------- END CODE BLOCK #1
+  done; # ------------------------------------- END CODE BLOCK #1
   BENCHMARK_1_END=$(date +'%s.%N';)
   BENCHMARK_1_DELTA=$(echo "scale=4; (${BENCHMARK_1_END} - ${BENCHMARK_1_START})/1" | bc -l;);
   echo -e "\nRuntime Duration - Code Block #1:  ${BENCHMARK_1_DELTA}s";
@@ -33,11 +37,12 @@ if [ 1 -eq 1 ]; then
   # Setup/run benchmark #2
   #
   BENCHMARK_2_START=$(date +'%s.%N';);
-  if [ 1 -eq 1 ]; then # --- START CODE BLOCK #1
+  for i in $(seq ${LOOP_ITERATIONS}); do # ---- START CODE BLOCK #2
 
-    sleep 2;
+    # sed - Trim leading whitespace && trailing whitespace (method 2)
+    echo "  a  b  c  d  " | sed -e 's/^[ \t]*//;s/[ \t]*$//' 1>/dev/null 2>&1;
 
-  fi; # -------------------- END CODE BLOCK #1
+  done; # ------------------------------------- END CODE BLOCK #2
   BENCHMARK_2_END=$(date +'%s.%N';)
   BENCHMARK_2_DELTA=$(echo "scale=4; (${BENCHMARK_2_END} - ${BENCHMARK_1_START})/1" | bc -l;);
   echo -e "\nRuntime Duration - Code Block #2:  ${BENCHMARK_2_DELTA}s";

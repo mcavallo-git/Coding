63011f57ac744ac8b225cb62afdf2f546b6e20e9
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-15 23:22:34 -0500

    sensors

--- prtg/Custom Sensors/EXE/get-openhardwaremonitor-power-gpu-min.bat
+++ prtg/Custom Sensors/EXE/get-openhardwaremonitor-power-gpu-min.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 SET LOGFILE=C:\ISO\OpenHardwareMonitor\OHW-Current-Power-GPU-Min.txt
 IF NOT EXIST "%LOGFILE%" (
-EXIT 1
+EXIT 2
 ) ELSE (
 TYPE %LOGFILE%
-FOR /F "tokens=* delims=: USEBACKQ" %%a IN ( `TYPE %LOGFILE% ^| FINDSTR "DOWN"` ) DO ( EXIT 1 )
+FOR /F "tokens=* delims=: USEBACKQ" %%a IN ( `TYPE %LOGFILE% ^| FINDSTR "DOWN"` ) DO ( EXIT 2 )
 EXIT 0
 )

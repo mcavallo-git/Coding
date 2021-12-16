916c693905b5d974e74a58bf54220980b2e45b09
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-15 23:11:36 -0500

    update-sensors

--- prtg/Custom Sensors/EXE/get-openhardwaremonitor-fan-pump-max.bat
+++ prtg/Custom Sensors/EXE/get-openhardwaremonitor-fan-pump-max.bat
@@ -1,5 +1,10 @@
 @ECHO OFF
 SET LOGFILE=C:\ISO\OpenHardwareMonitor\OHW-Current-Fan-Pump-Max.txt
+IF NOT EXIST "%LOGFILE%" (
+ECHO :DOWN
+EXIT 1
+) ELSE (
 TYPE %LOGFILE%
 FOR /F "tokens=* delims=: USEBACKQ" %%a IN ( `TYPE %LOGFILE% ^| FINDSTR "DOWN"` ) DO ( EXIT 1 )
 EXIT 0
+)

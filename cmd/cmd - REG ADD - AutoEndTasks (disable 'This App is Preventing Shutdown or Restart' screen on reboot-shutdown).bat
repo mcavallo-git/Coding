2f12352491e6d0cbdcc9e8cf349c5c0983cf4ee5
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-15 21:22:49 -0500

    Update cmd - REG ADD - AutoEndTasks (disable 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown).bat

--- cmd/cmd - REG ADD - AutoEndTasks (disable 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown).bat
+++ cmd/cmd - REG ADD - AutoEndTasks (disable 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown).bat
@@ -1,31 +1,37 @@
-@ECHO OFF
-
-SET KeyName=HKCU\Control Panel\Desktop
-SET ValueName=AutoEndTasks
-SET DataType=REG_SZ
-SET DataValue=1
-
-FOR /F "tokens=* USEBACKQ" %%a IN (
-`REG QUERY "%KeyName%" /v "AutoEndTasks" /t "%DataType%" /f "%DataValue%"`
-) DO ( 
-	SET QUERY_KEY_VAL=%%a
+IF 1==1 (
+  @ECHO OFF
+  ECHO.
+  ECHO.
+  SET KeyName=HKCU\Control Panel\Desktop
+  SET ValueName=AutoEndTasks
+  SET DataType=REG_SZ
+  SET DataValue=1
+  REM Note: Use  [ %%a ] if running from within a batch script
+  REM Note: Use  [  %a ] if running directly in a CMD terminal
+  FOR /F "tokens=* USEBACKQ" %a IN (
+  `REG QUERY "%KeyName%" /v "AutoEndTasks" /t "%DataType%" /f "%DataValue%"`
+  ) DO (
+    SET QUERY_KEY_VAL=%a
+  )
+  SET ARE_REGEDITS_NEEDED=%QUERY_KEY_VAL:End of search: 1 match(es) found.=NO_REGEDITS_ARE_NEEDED%
+  IF "%ARE_REGEDITS_NEEDED%"=="NO_REGEDITS_ARE_NEEDED" (
+    ECHO %KeyName%
+    ECHO   %ValueName% ^(%DataType%^) - No update^(s^) required - property is already set to "%DataValue%"^
+  ) ELSE (
+    ECHO %KeyName%
+    ECHO   %ValueName% ^(%DataType%^) - Setting to "%DataValue%" ...
+    REG ADD "%KeyName%" /v "%ValueName%" /t "%DataType%" /d "%DataValue%" /f
+  )
+  ECHO.
+  ECHO ON
 )
 
-SET ARE_REGEDITS_NEEDED=%QUERY_KEY_VAL:End of search: 1 match(es) found.=NO_REGEDITS_ARE_NEEDED%
+@REM ECHO.
+@REM ECHO Final state of registry key "%KeyName%", value "%ValueName%":
+@REM REG QUERY "%KeyName%" /v "%ValueName%"
 
-ECHO.
-IF "%ARE_REGEDITS_NEEDED%"=="NO_REGEDITS_ARE_NEEDED" (
-	ECHO Registry-key "%KeyName%" already has a value with name="%ValueName%", type="%DataType%", and data="%DataValue%"
-) ELSE (
-	ECHO Adding a value with name="%ValueName%", type="%DataType%", and data="%DataValue%" to registry-key "%KeyName%"
-	REG ADD "%KeyName%" /v "%ValueName%" /t "%DataType%" /d "%DataValue%" /f
-)
+@REM TIMEOUT /T 30
 
-ECHO.
-ECHO Final state of registry key "%KeyName%", value "%ValueName%":
-REG QUERY "%KeyName%" /v "%ValueName%"
-
-TIMEOUT /T 30
 
 REM	------------------------------------------------------------
 REM

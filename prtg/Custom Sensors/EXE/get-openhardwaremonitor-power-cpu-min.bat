6ad1349ad07a1a12dc875044a497fecf8f455b75
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-15 23:40:19 -0500

    docs

--- prtg/Custom Sensors/EXE/get-openhardwaremonitor-power-cpu-min.bat
+++ prtg/Custom Sensors/EXE/get-openhardwaremonitor-power-cpu-min.bat
@@ -8,3 +8,4 @@ TYPE %LOGFILE%
 FOR /F "tokens=* delims=: USEBACKQ" %%a IN ( `TYPE %LOGFILE% ^| FINDSTR "ERROR"` ) DO ( EXIT 2 )
 EXIT 0
 )
+REM https://www.paessler.com/manuals/prtg/custom_sensors#standard_exescript

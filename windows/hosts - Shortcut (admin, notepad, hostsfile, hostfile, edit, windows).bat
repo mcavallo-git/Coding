8926788509de1334b05ddcccfc103e3df1aaaf3f
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-15 19:13:24 -0500

    Update hosts - Shortcut (admin, notepad, hostsfile, hostfile, edit, windows).bat

--- windows/hosts - Shortcut (admin, notepad, hostsfile, hostfile, edit, windows).bat
+++ windows/hosts - Shortcut (admin, notepad, hostsfile, hostfile, edit, windows).bat
@@ -1,9 +1,18 @@
+REM ------------------------------------------------------------
 
 
-REM Open Windows' hostsfile in Notepad.exe (Run as admin)
+REM notepad.exe - Edit hostsfile  (AS ADMIN)
 PowerShell -NoProfile "Start-Process 'C:\Windows\System32\notepad.exe' -ArgumentList 'C:\Windows\System32\drivers\etc\hosts' -Verb 'RunAs';"
 
 
-REM Open Windows' hostsfile in Notepad++.exe (Run as admin) (fall-back to Notepad.exe)
+REM notepad++.exe - Edit hostsfile  (AS ADMIN)
 PowerShell -NoProfile "If (Test-Path C:\Program` Files\Notepad++\notepad++.exe) { Start-Process C:\Program` Files\Notepad++\notepad++.exe -ArgumentList C:\Windows\System32\drivers\etc\hosts -Verb RunAs; } ElseIf (Test-Path C:\Program` Files` `(x86`)\Notepad++\notepad++.exe) { Start-Process C:\Program` Files` `(x86`)\Notepad++\notepad++.exe -ArgumentList C:\Windows\System32\drivers\etc\hosts -Verb RunAs; } Else { Start-Process C:\Windows\System32\notepad.exe -ArgumentList C:\Windows\System32\drivers\etc\hosts -Verb RunAs; };"
 
+
+REM ------------------------------------------------------------
+REM
+REM Citation(s)
+REM
+REM   docs.microsoft.com  |  "Start-Process (Microsoft.PowerShell.Management) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/start-process
+REM
+REM ------------------------------------------------------------

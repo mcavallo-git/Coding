8b3f498d8016dbc02331ca418444638f088ba0bf
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-15 22:20:29 -0500

    Update cmd - REG QUERY + REG ADD (AutoEndTasks, disable 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown).bat

--- cmd/cmd - REG QUERY + REG ADD (AutoEndTasks, disable 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown).bat
+++ cmd/cmd - REG QUERY + REG ADD (AutoEndTasks, disable 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown).bat
@@ -3,42 +3,29 @@ REM
 REM REG QUERY + REG ADD  -  Set a registry key property (if not already set as-intended)
 REM
 
-
-REM @SETLOCAL DisableDelayedExpansion
-@SETLOCAL EnableDelayedExpansion
-IF 1==1 (
-  @ECHO OFF
-
-  ECHO.
-  ECHO.
-
-  REM HKCU\Control Panel\Desktop > AutoEndTasks > Setting to "1" disables the 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown)
-  SET KeyName=HKCU\Control Panel\Desktop
-  SET ValueName=AutoEndTasks
-  SET DataType=REG_SZ
-  SET Set_Value=1
-  SET Current_Value=VALUE_IS_UNDEFINED
-
-  REM ------------------
-  REM Note: Use  [ %%a ] if running from within a batch script
-  REM Note: Use  [  %a ] if running directly in a CMD terminal
-  FOR /F "tokens=3 USEBACKQ" %a IN (
-    `REG QUERY "%KeyName%" /v "%ValueName%" /t "%DataType%" ^| findstr "%ValueName%" ^| findstr "%DataType%"`
-  ) DO (
-    SET Current_Value=%a
-  )
-
-  ECHO Set_Value = [ %Set_Value% ]
-
-  ECHO Current_Value = [ %Current_Value% ]
-
-  IF NOT %Current_Value%==%Set_Value% ( REG ADD "%KeyName%" /v "%ValueName%" /t "%DataType%" /d "%Set_Value%" /f )
-
-  REG QUERY "%KeyName%" /v "%ValueName%"
-  ECHO.
-
-  @ECHO ON
+@ECHO OFF
+ECHO.
+ECHO.
+REM HKCU\Control Panel\Desktop > AutoEndTasks > Setting to "1" disables the 'This App is Preventing Shutdown or Restart' screen on reboot-shutdown)
+SET KeyName=HKCU\Control Panel\Desktop
+SET ValueName=AutoEndTasks
+SET DataType=REG_SZ
+SET Set_Value=1
+SET Current_Value=VALUE_IS_UNDEFINED
+REM ------------------
+REM Note: Use  [ %%a ] if running from within a batch script
+REM Note: Use  [  %a ] if running directly in a CMD terminal
+FOR /F "tokens=3 USEBACKQ" %a IN (
+  `REG QUERY "%KeyName%" /v "%ValueName%" /t "%DataType%" ^| findstr "%ValueName%" ^| findstr "%DataType%"`
+) DO (
+  SET Current_Value=%a
 )
+ECHO Set_Value = [ %Set_Value% ]
+ECHO Current_Value = [ %Current_Value% ]
+IF NOT %Current_Value%==%Set_Value% ( REG ADD "%KeyName%" /v "%ValueName%" /t "%DataType%" /d "%Set_Value%" /f )
+REG QUERY "%KeyName%" /v "%ValueName%"
+ECHO.
+@ECHO ON
 
 
 REM ------------------------------------------------------------

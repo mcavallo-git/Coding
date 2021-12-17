d121853b5cb1dc7cf36b0c409fa07f8bb5bd8db8
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-17 17:32:34 -0500

    Update PowerShell - Get version number.ps1

--- powershell/PowerShell - Get version number.ps1
+++ powershell/PowerShell - Get version number.ps1
@@ -1,25 +1,34 @@
 # ------------------------------------------------------------
 #
 # PowerShell
-#   |--> Get PowerShell's Version Number
+#   |--> Get the installed version of PowerShell
 #
 # ------------------------------------------------------------
 
 
-# Get the version of Powershell (as a string value (Major.Minor.Patch)
-$PSV=""; If ((${PSVersionTable}.PSVersion.Major)) { $PSV+=(${PSVersionTable}.PSVersion.Major); If ((${PSVersionTable}.PSVersion.Minor)) { $PSV+="."; $PSV+=(${PSVersionTable}.PSVersion.Minor); If ((${PSVersionTable}.PSVersion.Patch)) { $PSV+="."; $PSV+=(${PSVersionTable}.PSVersion.Patch); }; }; }; Write-Output "PowerShell v${PSV}";
-
-
-# Get the version of Powershell (as a decimal/numeric value (Major.Minor, only))
-$PSV=$(($($PSVersionTable.PSVersion.Major))+($($PSVersionTable.PSVersion.Minor)/10)); Write-Output "PowerShell v${PSV}";
-
-
-# ------------------------------------------------------------
-#
-# PowerShell Core has a build-in method for checking versioning
-#
-
+# pwsh (PowerShell Core) has a build-in method for checking versioning (thankfully)
 pwsh --version;
+
+
+# PowerShell has no simple way to get its version (especially not from external sources, sadly)
+PowerShell -Command "Write-Output ((Write-Output PowerShell)+([String][Char]32)+(((GV PSVersionTable).Value).PSVersion.Major)+(Write-Output .)+(((GV PSVersionTable).Value).PSVersion.Minor))";
+
+
+# ------------------------------------------------------------
+#
+# Get the version of PowerShell (for the current/running PowerShell session)
+
+
+#
+#  Format:   Major.Minor.Patch
+#
+$PSV="";If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Major)){$PSV+=(${PSVersionTable}.PSVersion.Major);If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Minor)){$PSV+="."; $PSV+=(${PSVersionTable}.PSVersion.Minor);If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Patch)){$PSV+="."; $PSV+=(${PSVersionTable}.PSVersion.Patch);};};};Write-Output "PowerShell ${PSV}"; # Get the version of PowerShell with the format [ Major.Minor.Patch ]
+
+
+#
+#  Format:   Major.Minor
+#
+$PSV="";If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Major)){$PSV+=(${PSVersionTable}.PSVersion.Major);If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Minor)){$PSV+="."; $PSV+=(${PSVersionTable}.PSVersion.Minor);};};Write-Output "PowerShell ${PSV}"; # Get the version of PowerShell with the format [ Major.Minor ]
 
 
 # ------------------------------------------------------------

80d16609b19f92ff3c199dd36cf445f064c944b5
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-17 18:33:04 -0500

    Create PowerShell - Get-PSDrive (list connected disk volumes-drives, C-Drive, D-Drive, etc.).ps1

---
+++ powershell/PowerShell - Get-PSDrive (list connected disk volumes-drives, C-Drive, D-Drive, etc.).ps1
@@ -0,0 +1,17 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Get-PSDrive (list connected disk volumes-drives, C-Drive, D-Drive, etc.)
+#
+# ------------------------------------------------------------
+
+
+Get-PSDrive -PSProvider "FileSystem";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Get-PSDrive (Microsoft.PowerShell.Management) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-psdrive
+#
+# ------------------------------------------------------------

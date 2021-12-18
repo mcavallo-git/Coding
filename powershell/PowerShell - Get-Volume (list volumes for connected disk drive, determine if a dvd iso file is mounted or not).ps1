64e9992c7749eda94aafde5361e53fe7a879bb6f
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-17 19:40:43 -0500

    Update PowerShell - Get-Volume (list volumes for connected disk drive, determine if a dvd iso file is mounted or not).ps1

--- powershell/PowerShell - Get-Volume (list volumes for connected disk drive, determine if a dvd iso file is mounted or not).ps1
+++ powershell/PowerShell - Get-Volume (list volumes for connected disk drive, determine if a dvd iso file is mounted or not).ps1
@@ -34,7 +34,7 @@ If ($True) {
 $ISO_Fullpath = "${HOME}\Desktop\Windows.iso";
 
 # Determine if the iso file is already mounted
-$Mount_DriveLetter = ((Get-DiskImage -ImagePath "${ISO_FullPath}" | Get-Volume).DriveLetter);
+$Mounted_DriveLetter = ((Get-DiskImage -ImagePath "${ISO_FullPath}" | Get-Volume).DriveLetter);
 
 # If iso file is not already mounted, then mount it now
 If ((Get-DiskImage -ImagePath "${ISO_FullPath}" | Get-Volume) -Eq $Null) {
@@ -42,9 +42,9 @@ If ((Get-DiskImage -ImagePath "${ISO_FullPath}" | Get-Volume) -Eq $Null) {
 }
 
 # Get the mounted iso file's drive letter
-$Mount_DriveLetter = ((Get-DiskImage -ImagePath "${ISO_FullPath}" | Get-Volume).DriveLetter); $Mount_DriveLetter;
+$Mounted_DriveLetter = ((Get-DiskImage -ImagePath "${ISO_FullPath}" | Get-Volume).DriveLetter); $Mounted_DriveLetter;
 
-If (([String]::IsNullOrEmpty("${Mount_DriveLetter}")) -Eq $True) {
+If (([String]::IsNullOrEmpty("${Mounted_DriveLetter}")) -Eq $True) {
 
 	# Error(s) mounting ISO file
 	Write-Host "Error:  Unable to mount ISO file";
@@ -52,7 +52,7 @@ If (([String]::IsNullOrEmpty("${Mount_DriveLetter}")) -Eq $True) {
 } Else {
 
 	# ISO file mounted successfully
-	Write-Host "Info:  Mount_DriveLetter = [ ${Mount_DriveLetter} ]";
+	Write-Host "Info:  Mounted_DriveLetter = [ ${Mounted_DriveLetter} ]";
 
 	# Unmount the iso file
 	Get-DiskImage -ImagePath "${ISO_FullPath}" | Dismount-DiskImage | Out-Null;
@@ -67,5 +67,11 @@ If (([String]::IsNullOrEmpty("${Mount_DriveLetter}")) -Eq $True) {
 # Citation(s)
 #
 #   docs.microsoft.com  |  "Get-Volume (Storage) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/storage/get-volume
+#
+#   docs.microsoft.com  |  "Get-DiskImage (Storage) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/storage/get-diskimage
+#
+#   docs.microsoft.com  |  "Mount-DiskImage (Storage) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/storage/mount-diskimage
+#
+#   docs.microsoft.com  |  "Dismount-DiskImage (Storage) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/storage/dismount-diskimage
 #
 # ------------------------------------------------------------

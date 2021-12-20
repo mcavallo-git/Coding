ccd512e33dc401ec104906dfeb60bf7cd6710c39
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-20 08:55:14 -0500

    Update Linux - printf (right-pad (rpad) & left-pad (lpad) strings, prepending strings before command-output, etc.).sh

--- linux/Linux - printf (right-pad (rpad) & left-pad (lpad) strings, prepending strings before command-output, etc.).sh
+++ linux/Linux - printf (right-pad (rpad) & left-pad (lpad) strings, prepending strings before command-output, etc.).sh
@@ -2,12 +2,21 @@
 # ------------------------------------------------------------
 
 
-# printf - right-pad (rpad) a string with spaces
-TEST_PADDING="rpad"; echo "[$(printf '%-10s' "${TEST_PADDING}";)]";
+# RPad (Right-Pad)  -->  w/ SPACES
+STRING="rpad"; LENGTH="10"; echo "[$(printf "%-${LENGTH}s" "${STRING}";)]";
 
 
-# printf - left-pad (lpad) a string with spaces
-TEST_PADDING="lpad"; echo "[$(printf '%10s' "${TEST_PADDING}";)]";
+# RPad (Right-Pad)  -->  w/ ZEROES
+FLOAT="123.45"; LENGTH="10"; echo "${FLOAT}" | awk '{printf "%*.*f\n", FL, FL-1-length(int($1)), $1}' FL="${LENGTH}";
+
+
+# LPad (Left-Pad)  -->  w/ SPACES
+STRING="lpad"; LENGTH="10"; echo "[ $(printf "%${LENGTH}s" "${STRING}";)]";
+
+
+# LPad (Left-Pad)  -->  w/ ZEROES
+INTEGER="12345"; LENGTH="10"; echo "[$(printf "%0${LENGTH}d" "${INTEGER}";)]";
+FLOAT="123.45"; LENGTH="10"; LENGTH_DECIMALS="3"; echo "[$(printf "%0${LENGTH}.${LENGTH_DECIMALS}f" "${FLOAT}";)]";
 
 
 # ------------------------------------------------------------

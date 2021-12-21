551257a61fbd1cc5c7c2307923bb29cc6c65390b
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-21 06:12:16 -0500

    rn

---
+++ linux/Linux - hdparm -Tt (disk benchmarking tool - test read speeds (no write test)).sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# ------------------------------------------------------------
+#  Linux - hdparm -Tt (disk benchmarking tool - test read speeds (no write test))
+# ------------------------------------------------------------
+#
+# hdparm
+#        -t     Perform timings of device reads for benchmark and comparison purposes
+#        -T     Perform  timings of cache reads for benchmark and comparison purposes
+#
+# ------------------------------------------------------------
+
+# Ex) Benchmark read speeds of the topmost disk returned by lsblk
+
+DISK_PATH=$(lsblk | grep disk | awk '{print $1}' | head -n 1;); hdparm -Tt "/dev/${DISK_PATH}";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   askubuntu.com  |  "How to check hard disk performance - Ask Ubuntu"  |  https://askubuntu.com/a/87036
+#
+# ------------------------------------------------------------

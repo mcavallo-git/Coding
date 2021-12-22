86f6fb7ce0579175fde98e68b3dc03c7105deeca
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-22 15:47:21 -0500

    Update Linux - sqlite3 (get schema for database table).sh

--- linux/Linux - sqlite3 (get schema for database table).sh
+++ linux/Linux - sqlite3 (get schema for database table).sh
@@ -1,3 +1,18 @@
-TABLE_NAME="domainlist";
-DB_FILEPATH="/etc/pihole/gravity.db";
-DOCKER_NAME="pihole"; docker exec "$(docker ps --all --quiet --filter "name=${DOCKER_NAME}";)" sqlite3 "${DB_FILEPATH}" ".schema ${TABLE_NAME}";
+#!/bin/bash
+# ------------------------------------------------------------
+# Linux - sqlite3 (get schema for database table).sh
+# ------------------------------------------------------------
+
+
+TABLE_NAME="domainlist"; FULLPATH_DATABASE="/etc/pihole/gravity.db"; sqlite3 "${FULLPATH_DATABASE}" ".schema ${TABLE_NAME}";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   discourse.pi-hole.net  |  "V5.0 Docker, Whitelist domain, writing to readonly database - Help - Pi-hole Userspace"  |  https://www.sqlitetutorial.net/sqlite-tutorial/sqlite-describe-table/
+#
+#   www.sqlitetutorial.net  |  "SQLite Describe Table"  |  https://www.sqlitetutorial.net/sqlite-tutorial/sqlite-describe-table/
+#
+# ------------------------------------------------------------

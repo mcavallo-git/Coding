5f2852f464a4114ac25067e92c1978231658b955
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-22 13:09:27 -0500

    Update Linux - read (get user input, keypress, single-multiple character entry, etc.).sh

--- linux/Linux - read (get user input, keypress, single-multiple character entry, etc.).sh
+++ linux/Linux - read (get user input, keypress, single-multiple character entry, etc.).sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # ------------------------------------------------------------
-# 
-
-# Note: [ <'/dev/tty' ]  is required for read to function with curl'ed scripts
-# Note: [ -r ] argument breaks curl'ed scripts (avoid using it)
+# Linux - read (get user input, keypress, single-multiple character entry, etc.)
+# ------------------------------------------------------------
+# Note: Use [ <'/dev/tty' ] to pipe terminal input to the 'read' command so that it functions with curl'ed scripts
+# Note: Avoid using the [ -r ] argument when calling 'read', as it breaks curl'ed scripts containing escaped characters
 # ------------------------------------------------------------
 #
 # Single character entry
@@ -11,36 +11,27 @@
 
 if [ 1 -eq 1 ]; then
   ACTION_DESCRIPTION="Sleep for 2 seconds";
-  READ_TIMEOUT=3;
+  READ_TIMEOUT=60;
   USER_RESPONSE="";
   read -p "Perform action [ ${ACTION_DESCRIPTION} ], now?  (press 'y' to confirm)  " -a USER_RESPONSE -n 1 -t ${READ_TIMEOUT} <'/dev/tty'; EXIT_CODE=${?};
   echo "";
   if [[ "${EXIT_CODE}" -gt 128 ]]; then
-    # DENIED - USER INPUT TIMED OUT
-    echo "  |--> Error:  Response timed out after ${READ_TIMEOUT}s";
+    # FAILED - USER INPUT TIMED OUT
+    echo "Error:  Response timed out after ${READ_TIMEOUT}s";
   elif [[ -z "${USER_RESPONSE}" ]]; then
     # DENIED - USER INPUT IS EMPTY
-    echo "  |--> Warning:  DENIED  (empty response received)";
+    echo "Warning:  DENIED  (empty response received)";
   elif [[ ! "${USER_RESPONSE}" =~ ^[Yy]$ ]]; then
     # DENIED - USER INPUT FAILED REGEX TEST
-    echo "  |--> Warning:  DENIED  (response '${USER_RESPONSE}' received)";
+    echo "Warning:  DENIED  (response '${USER_RESPONSE}' received)";
   else
-    # CONFIRMED - DO THE ACTION
-    echo "  |--> Info:  CONFIRMED  (response '${USER_RESPONSE}' received)";
-    echo "  |--> Info:  Performing action [ ${ACTION_DESCRIPTION} ]...";
+    # CONFIRMED - USER INPUT IS NON-EMPTY AND PASSED REGEX TEST
+    echo "Info:  CONFIRMED  (response '${USER_RESPONSE}' received)";
+    echo "Info:  Performing action [ ${ACTION_DESCRIPTION} ]...";
     sleep 2;
   fi;
 fi;
 
-
-# Note: [ <'/dev/tty' ]  is required for read to function with curl'ed scripts
-# Note: [ -r ] argument breaks curl'ed scripts (avoid using it)
-
-read -p "Press any key to continue...  " -a KEY_PRESSED -n 1 -t 60 <'/dev/tty';
-echo -e "\nKEY_PRESSED=[ ${KEY_PRESSED} ]";
-
-# AVOID USING [ -r ] --> USE ABOVE METHODOLOGY OF [ <'/dev/tty' ], INSTEAD
-# read -p "Press any key to continue...  " -n 1 -t 60 -r;  # !!! Note: [ -r ] breaks on curl commands (fails read immediately, returning exit code 1)
 
 # ------------------------------------------------------------
 #
@@ -49,15 +40,19 @@ echo -e "\nKEY_PRESSED=[ ${KEY_PRESSED} ]";
 
 
 if [ 1 -eq 1 ]; then
+  ACTION_DESCRIPTION="Sleep for 2 seconds";
   READ_TIMEOUT=60;
+  USER_RESPONSE="";
   read -p "Enter a string:  " -a USER_RESPONSE -t ${READ_TIMEOUT} <'/dev/tty'; EXIT_CODE=${?};
-  echo "";
-  if [ ${EXIT_CODE} -gt 128 ]; then
-    echo "  |--> Error:  Response timed out after ${READ_TIMEOUT}s";
-  elif [ ${#USER_RESPONSE} -eq 0 ]; then
-    echo "  |--> Error:  Response's string length is zero (empty)";
+  if [[ "${EXIT_CODE}" -gt 128 ]]; then
+    # ERROR - USER INPUT TIMED OUT
+    echo "Error:  Response timed out after ${READ_TIMEOUT}s";
+  elif [[ -z "${USER_RESPONSE}" ]]; then
+    # WARNING - USER INPUT IS EMPTY
+    echo "Warning:  Empty response received";
   else
-    echo "  |--> Info:  Response received:  \"${USER_RESPONSE}\"";
+    # INFO - USER INPUT IS NON-EMPTY
+    echo "Info:  Response received:  \"${USER_RESPONSE}\"";
   fi;
 fi;
 
@@ -76,11 +71,11 @@ if [ 1 -eq 1 ]; then
   echo -e "! ! NOTICE ! !  THIS IS A DOUBLE CONFIRMATION MESSAGE  ! ! NOTICE ! !";
   echo -e "! !   |    ! !  PLEASE READ IT BEFORE HITTING Y (YES)  ! !        ! !";
   echo -e "      |";
-  read -p "      |--> Are you sure you want to continue?  (press 'y' to confirm)" -n 1 -t 60 <'/dev/tty'; # Await single keypress
+  read -p "    Are you sure you want to continue?  (press 'y' to confirm)" -n 1 -t 60 <'/dev/tty'; # Await single keypress
   echo -e "";
   if [ -n "${REPLY}" ] && [ "$(echo ${REPLY} | tr '[:lower:]' '[:upper:]';)" == "Y" ]; then
     echo -e "      |";
-    read -p "      |--> Are you completely positive you wish to continue?  (press 'y' to confirm)" -n 1 -t 60 <'/dev/tty'; # Await single keypress
+    read -p "    Are you completely positive you wish to continue?  (press 'y' to confirm)" -n 1 -t 60 <'/dev/tty'; # Await single keypress
     echo -e "";
     if [ -n "${REPLY}" ] && [ "$(echo ${REPLY} | tr '[:lower:]' '[:upper:]';)" == "Y" ]; then
       echo "Confirmed twice!";

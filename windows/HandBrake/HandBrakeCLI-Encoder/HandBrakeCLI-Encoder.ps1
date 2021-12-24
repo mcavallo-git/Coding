2b594c113bf4b1d534301c116ae1f7bbc98e92e5
Matt Cavallo <mcavallo.git@gmail.com>
2021-12-24 11:42:31 -0500

    Update HandBrakeCLI-Encoder.ps1

--- windows/HandBrake/HandBrakeCLI-Encoder/HandBrakeCLI-Encoder.ps1
+++ windows/HandBrake/HandBrakeCLI-Encoder/HandBrakeCLI-Encoder.ps1
@@ -40,13 +40,13 @@ If ("${Env:HandBrake_Preset}" -NE "") {
 	$HandBrake_Preset = "Very Fast 1080p30";
 	# $HandBrake_Preset = "Fast 1080p30";
 	# $HandBrake_Preset = "H.265 MKV 2160p60";
-	# $HandBrake_Preset = "H.265 NVENC 2160P 4K";
+	# $HandBrake_Preset = "H.265 NVENC 2160p 4K";
 	# $HandBrake_Preset = "H.265 NVENC 1080p";
 	# ------------------------------
 	# ${Env:HandBrake_Preset}="Very Fast 1080p30";
 	# ${Env:HandBrake_Preset}="Fast 1080p30";
 	# ${Env:HandBrake_Preset}="H.265 MKV 2160p60";
-	# ${Env:HandBrake_Preset}="H.265 NVENC 2160P 4K";
+	# ${Env:HandBrake_Preset}="H.265 NVENC 2160p 4K";
 	# ${Env:HandBrake_Preset}="H.265 NVENC 1080p";
 	# ------------------------------
 }

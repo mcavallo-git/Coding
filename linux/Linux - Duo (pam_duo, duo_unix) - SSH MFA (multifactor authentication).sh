a61b1970be7f5b4ac60bb9cead2d6c9d05a978c0
Matt Cavallo <mcavallo.git@gmail.com>
2022-01-04 22:28:19 -0500

    Move Duo to cloud-infrastructure

---
+++ linux/Linux - Duo (pam_duo, duo_unix) - SSH MFA (multifactor authentication).sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ------------------------------
+#
+# REFER TO SCRIPT @ [ https://raw.githubusercontent.com/mcavallo-git/cloud-infrastructure/master/usr/local/sbin/install_duo_unix ]
+#
+# ------------------------------

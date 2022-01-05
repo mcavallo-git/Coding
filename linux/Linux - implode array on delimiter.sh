226027ecf7ed2c28009f835db764c2eec3bcc4c9
Matt Cavallo <mcavallo.git@gmail.com>
2022-01-05 00:31:40 -0500

    Update Linux - implode array on delimiter.sh

--- linux/Linux - implode array on delimiter.sh
+++ linux/Linux - implode array on delimiter.sh
@@ -4,14 +4,16 @@
 #
 # Sub-Function: Takes first argument ($1) as a SINGLE-CHARACTER delimiter, combines all remaining arguments ($2,$3,...,$n) into a string delimited by aforementioned delimiter ($1)
 #
-function implode { local IFS="$1"; shift; echo "$*"; }
+function implode { local IFS="$1"; shift; echo "$*"; };
 
+# ------------------------------
 
 # Example 1.1:
 implode ',' a b c;
 # Output:
 #a,b,c
 
+# ------------------------------
 
 # Example 1.2:
 implode $'\n' a b c;
@@ -20,6 +22,7 @@ implode $'\n' a b c;
 #b
 #c
 
+# ------------------------------
 
 # Example 1.3:
 unset DAT_ARRAY; declare -a DAT_ARRAY; # [Re-]Instantiate bash array
@@ -30,9 +33,18 @@ DAT_ARRAY+=("Item-Two");
 DAT_ARRAY+=("Item-A");
 DAT_ARRAY+=("Item-A");
 DAT_ARRAY+=("Item-B");
+
 implode , "${DAT_ARRAY[@]}";
-# Output:
-#Item-One,Item-One,Item-Two,Item-A,Item-A,Item-B
+# ^ Outputs:  Item-One,Item-One,Item-Two,Item-A,Item-A,Item-B
+
+implode $'\n' "${DAT_ARRAY[@]}";
+# ^ Outputs:
+# Item-One
+# Item-One
+# Item-Two
+# Item-A
+# Item-A
+# Item-B
 
 
 

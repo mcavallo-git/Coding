6f5c3c670931e041385221d3df6ea44f3e0ae625
Matt Cavallo <mcavallo.git@gmail.com>
2022-01-07 00:13:34 -0500

    Update home-assistant-icons-all.updater.sh

--- smarthome/homeassistant/icons/home-assistant-icons-all.updater.sh
+++ smarthome/homeassistant/icons/home-assistant-icons-all.updater.sh
@@ -12,7 +12,7 @@
 #
 if [[ 1 -eq 1 ]]; then
 CHEATSHEET_URL="https://pictogrammers.github.io/@mdi/font/6.5.95/";
-cd "$(if [[ -n "$(command -v "wslvar" 2>'/dev/null';)" ]]; then wslpath -u "$(wslvar --sys "USERPROFILE";)"; else echo "${HOME}"; fi;)/Documents/GitHub/Coding/smarthome/homeassistant/icons";
+cd "$(if [[ -n "$(command -v "wslvar" 2>'/dev/null';)" ]]; then wslpath -u "$(wslvar -s "USERPROFILE";)"; else echo "${HOME}"; fi;)/Documents/GitHub/Coding/smarthome/homeassistant/icons";
 curl -sL "${CHEATSHEET_URL}" | grep 'var icons = ' | head -n 1 | tr "{" "\n" | sed -rne "s/name:\"([^\"]+)\".+$/mdi:\1/pi" | sort -u > "mdi-icons-all.txt";
 echo -e -n "\n\n# ------------------------------------------------------------\n#\n# Citation(s)\n#\n#   materialdesignicons.com  |  \"Material Design Icons\"  |  https://materialdesignicons.com/\n#\n# ------------------------------------------------------------" >> "mdi-icons-all.txt";
 fi;

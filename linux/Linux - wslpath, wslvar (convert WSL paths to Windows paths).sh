dbd411d1c93294fb2465aac3d7ca415c6acd2b67
Matt Cavallo <mcavallo.git@gmail.com>
2022-01-07 00:13:25 -0500

    Update Linux - wslpath, wslvar (convert WSL paths to Windows paths).sh

--- linux/Linux - wslpath, wslvar (convert WSL paths to Windows paths).sh
+++ linux/Linux - wslpath, wslvar (convert WSL paths to Windows paths).sh
@@ -17,6 +17,18 @@ wslpath -w "/";  # Outputs:  \\wsl$\Ubuntu\
 
 # ------------------------------
 #
+# NOTE:  It is MUCH faster to use Linux's "find" command instead of wslpath + wslvar, e.g.:
+#
+
+# Get cmd.exe's filepath for WSL via Linux's find module
+find /mnt/*/Windows/System32/cmd.exe -mindepth 0 -maxdepth 0 -type f | head -n 1;
+
+# Get cmd.exe's filepath for WSL via wslvar + wslpath modules
+find $(wslpath -u "$(wslvar -s "ComSpec";)";);
+
+
+# ------------------------------
+#
 #
 # wslvar
 #    -s, --sys - use data from system local & global variables.
@@ -52,6 +64,19 @@ echo "$(wslpath -u "$(wslvar -s "SystemDrive")/";)";  # Outputs:  /mnt/c/
 echo "$(wslpath -w "/";)";     # Outputs:  \\wsl$\Ubuntu\
 echo "$(wslpath -w "/root";)"; # Outputs:  \\wsl$\Ubuntu\root
 echo "$(wslpath -w "/tmp";)"; # Outputs:   \\wsl$\Ubuntu\tmp
+
+
+# ------------------------------
+#
+# Prebuilt queries to locate essential exes
+#
+
+CMD_EXE="$(if [[ -n "$(command -v cmd.exe 2>'/dev/null';)" ]]; then echo "$(realpath "$(command -v "cmd.exe";)";)"; elif [[ -f "$(find /mnt/*/Windows/System32/cmd.exe -mindepth 0 -maxdepth 0 -type f | head -n 1;)" ]]; then find /mnt/*/Windows/System32/cmd.exe -mindepth 0 -maxdepth 0 -type f | head -n 1; else echo "$(wslpath -u "$(wslvar -s "ComSpec";)";)"; fi;)";
+
+
+POWERSHELL_EXE="$(if [[ -n "$(command -v powershell.exe 2>'/dev/null';)" ]]; then echo "$(realpath "$(command -v "powershell.exe";)";)"; elif [[ -f "$(find /mnt/*/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -mindepth 0 -maxdepth 0 -type f | head -n 1;)" ]]; then find /mnt/*/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -mindepth 0 -maxdepth 0 -type f | head -n 1; else echo "$(wslpath -u "$(wslvar -s "windir";)";)/System32/WindowsPowerShell/v1.0/powershell.exe"; fi;)";
+
+WSL_EXE="$(if [[ -n "$(command -v wsl.exe 2>'/dev/null';)" ]]; then echo "$(realpath "$(command -v "wsl.exe";)" 2>'/dev/null';)"; elif [[ -f "$(find /mnt/*/Windows/System32/wsl.exe -mindepth 0 -maxdepth 0 -type f | head -n 1;)" ]]; then find /mnt/*/Windows/System32/wsl.exe -mindepth 0 -maxdepth 0 -type f | head -n 1; else echo "$(wslpath -u "$(wslvar -s "ComSpec";)";)"; fi;)";
 
 
 # ------------------------------

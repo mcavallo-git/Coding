1dd1fe0892a0701f19188b561e32aab381e2b738
Matt Cavallo <mcavallo.git@gmail.com>
2022-01-17 07:32:04 -0500

    Update PowerShell - Get-DnsClientServerAddress.ps1

--- powershell/PowerShell - Get-DnsClientServerAddress.ps1
+++ powershell/PowerShell - Get-DnsClientServerAddress.ps1
@@ -1,8 +1,19 @@
 # ------------------------------------------------------------
 # PowerShell - Get-DnsClientServerAddress
 # ------------------------------------------------------------
+#
+# Get first set of non-null DNS servers from local network connections
+#
 
 Get-DnsClientServerAddress | Where-Object { ${_}.Address -NE $Null; } | Where-Object { ${_}.ElementName -NotLike '*Loopback*'; } | Select-Object -First 1 -ExpandProperty 'Address' | ForEach-Object { Write-Host (('DNS Server Address:  [ ')+(${_})+(' ]')); };
+
+
+# ------------------------------------------------------------
+#
+# WSL - Get the Windows host's primary DNS server
+#
+
+WINDOWS_DNS_SERVER="$(PowerShell -Command 'Get-DnsClientServerAddress | Where-Object { ${_}.Address -NE $Null; } | Where-Object { ${_}.ElementName -NotLike "*Loopback*"; } | Select-Object -First 1 -ExpandProperty "Address" | Select-Object -First 1;' | sed -e "/^\s*$/d" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//';)"; echo "[${WINDOWS_DNS_SERVER}]";
 
 
 # ------------------------------------------------------------

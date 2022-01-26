d64c60bc4fd3db2ee207c9e4a5b6e28cc139c903
Matt Cavallo <mcavallo.git@gmail.com>
2022-01-26 16:31:01 -0500

    Update PowerShell - Scheduled Task - Run script block as Administrator (without referencing a script file).ps1

--- powershell/PowerShell - Scheduled Task - Run script block as Administrator (without referencing a script file).ps1
+++ powershell/PowerShell - Scheduled Task - Run script block as Administrator (without referencing a script file).ps1
@@ -26,10 +26,27 @@ $Args_SchedTask=@{ TaskName="SetHomepage_Edge"; Description="SetHomepage_Edge"; 
 
 # ------------------------------------------------------------
 #
-# StopProcesses_OnStartup  (Runs as Admin)
+# Startup_StopProcesses  (Runs as Admin)
 #
 
-$Args_SchedTask=@{ TaskName="StopProcesses_OnStartup"; Description="StopProcesses_OnStartup"; Trigger=(New-ScheduledTaskTrigger -Once -At (Get-Date)); Action=(New-ScheduledTaskAction -Execute ((GCM powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((GCM powershell).Source) -ArgumentList ('-Command While (1) { Get-Process -Name redcloak* | Stop-Process -Force <# redcloak #>; Get-Process -Name flow | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name smartaudio* | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name tanium* | Stop-Process -Force <# tanium #>; Get-Process -Name fc* | Stop-Process -Force <# mcafee #>; Start-Sleep -Seconds (1); };') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
+$Args_SchedTask=@{ TaskName="Startup_StopProcesses"; Description="Startup_StopProcesses"; Trigger=(New-ScheduledTaskTrigger -AtStartup); Action=(New-ScheduledTaskAction -Execute ((GCM powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((GCM powershell).Source) -ArgumentList ('-Command While (1) { Get-Process -Name redcloak* 2>`$Null | Stop-Process -Force <# redcloak #>; Get-Process -Name flow 2>`$Null | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name smartaudio* 2>`$Null | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name tanium* 2>`$Null | Stop-Process -Force <# tanium #>; Get-Process -Name fc* 2>`$Null | Stop-Process -Force <# mcafee #>; Start-Sleep -Seconds (1); };') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
 
 
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "New-ScheduledTaskAction (ScheduledTasks) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/new-scheduledtaskaction
+#
+#   docs.microsoft.com  |  "New-ScheduledTaskTrigger (ScheduledTasks) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/new-scheduledtasktrigger
+#
+#   docs.microsoft.com  |  "Register-ScheduledTask (ScheduledTasks) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/register-scheduledtask
+#
+#   docs.microsoft.com  |  "Start-ScheduledTask (ScheduledTasks) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/start-scheduledtask
+#
+#   docs.microsoft.com  |  "Unregister-ScheduledTask (ScheduledTasks) | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/unregister-scheduledtask
+#
+#   stackoverflow.com  |  "windows - How to run a process as non-admin from an elevated PowerShell console? - Stack Overflow"  |  https://stackoverflow.com/a/37335079
+#
 # ------------------------------------------------------------

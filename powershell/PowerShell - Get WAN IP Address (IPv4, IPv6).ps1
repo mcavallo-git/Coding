5b448d131d6d70d0192026066b3384a31f2d8912
Matt Cavallo <mcavallo.git@gmail.com>
2022-02-05 01:21:54 -0500

    Update PowerShell - Get WAN IP Address (IPv4, IPv6).ps1

--- powershell/PowerShell - Get WAN IP Address (IPv4, IPv6).ps1
+++ powershell/PowerShell - Get WAN IP Address (IPv4, IPv6).ps1
@@ -1,47 +1,7 @@
-# ------------------------------------------------------------
 #
-# PowerShell - Get current device's WAN IPv4 address
+# REFER TO "ResolveIP" PowerShell Module located at:
 #
 
-If ($True) {
 
-$ProgressPreference='SilentlyContinue'; <# Hide Invoke-WebRequest's progress bar #>
+		$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; $ProgressPreference='SilentlyContinue'; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Try { Invoke-Expression ((Invoke-WebRequest -UseBasicParsing -TimeoutSec (7.5) -Uri ('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/ResolveIP/ResolveIP.psm1') ).Content) } Catch {}; If (-Not (Get-Command -Name 'ResolveIP' -ErrorAction 'SilentlyContinue')) { Import-Module ([String]::Format('{0}\Documents\GitHub\Coding\powershell\_WindowsPowerShell\Modules\ResolveIP\ResolveIP.psm1', ((Get-Variable -Name 'HOME').Value))); }; [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;   ResolveIP;
 
-# ------------------------------
-
-# IPv4 Resolvers
-$URL_IPv4_RESOLVER="https://ipv4.icanhazip.com";
-# $URL_IPv4_RESOLVER="https://v4.ident.me";
-# $URL_IPv4_RESOLVER="https://ipinfo.io/ip";
-# $URL_IPv4_RESOLVER="https://ipecho.net/plain";
-
-$PUBLIC_IPV4=((Invoke-WebRequest -UseBasicParsing -Uri "${URL_IPv4_RESOLVER}").Content -split ([String][Char]10) | Select-Object -First 1);
-
-Write-Host "PUBLIC_IPV4 = [${PUBLIC_IPV4}]";
-
-
-# ------------------------------
-
-# IPv6 Resolvers
-$URL_IPv6_RESOLVER="https://ipv6.icanhazip.com";
-# $URL_IPv6_RESOLVER="https://v6.ident.me";
-# $URL_IPv6_RESOLVER="https://bot.whatismyipaddress.com";
-# $URL_IPv6_RESOLVER="https://checkip.amazonaws.com";
-
-$PUBLIC_IPV6=((Invoke-WebRequest -UseBasicParsing -Uri "${URL_IPv6_RESOLVER}").Content -split ([String][Char]10) | Select-Object -First 1);
-
-Write-Host "PUBLIC_IPV6 = [${PUBLIC_IPV6}]";
-
-
-# ------------------------------
-
-}
-
-
-# ------------------------------------------------------------
-#
-# Citation(s)
-#
-#   docs.microsoft.com  |  "Invoke-WebRequest (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest
-#
-# ------------------------------------------------------------

6195dc8a350edcd0ef60c3c41945c1886b337641
Matt Cavallo <mcavallo.git@gmail.com>
2022-02-05 01:20:56 -0500

    Update ResolveIP.psm1

--- powershell/_WindowsPowerShell/Modules/ResolveIP/ResolveIP.psm1
+++ powershell/_WindowsPowerShell/Modules/ResolveIP/ResolveIP.psm1
@@ -25,8 +25,7 @@ function ResolveIP {
 	# ------------------------------------------------------------
 	If ($False) { # RUN THIS SCRIPT REMOTELY:
 
-		$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; $ProgressPreference='SilentlyContinue'; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Try { Invoke-Expression ((Invoke-WebRequest -UseBasicParsing -TimeoutSec (7.5) -Uri ('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/ResolveIP/ResolveIP.psm1') ).Content) } Catch {}; If (-Not (Get-Command -Name 'ResolveIP' -ErrorAction 'SilentlyContinue')) { Import-Module ([String]::Format('{0}\Documents\GitHub\Coding\powershell\_WindowsPowerShell\Modules\ResolveIP\ResolveIP.psm1', ((Get-Variable -Name 'HOME').Value))); }; [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;
-		ResolveIP
+		$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; $ProgressPreference='SilentlyContinue'; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Try { Invoke-Expression ((Invoke-WebRequest -UseBasicParsing -TimeoutSec (7.5) -Uri ('https://raw.githubusercontent.com/mcavallo-git/Coding/master/powershell/_WindowsPowerShell/Modules/ResolveIP/ResolveIP.psm1') ).Content) } Catch {}; If (-Not (Get-Command -Name 'ResolveIP' -ErrorAction 'SilentlyContinue')) { Import-Module ([String]::Format('{0}\Documents\GitHub\Coding\powershell\_WindowsPowerShell\Modules\ResolveIP\ResolveIP.psm1', ((Get-Variable -Name 'HOME').Value))); }; [System.Net.ServicePointManager]::SecurityProtocol=$ProtoBak;   ResolveIP;
 
 	}
 	# ------------------------------------------------------------

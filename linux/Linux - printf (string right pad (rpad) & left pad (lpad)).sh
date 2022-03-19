9ab84c2803a2d3b16a0645fa502ac5060182a61d
Matt Cavallo <mcavallo.git@gmail.com>
2022-03-19 18:16:26 -0400

    rn

--- linux/Linux - printf (string right pad (rpad) & left pad (lpad)).sh
+++ linux/Linux - printf (string right pad (rpad) & left pad (lpad)).sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 # ------------------------------------------------------------
+# Linux - printf (string right pad (rpad) & left pad (lpad))
+# ------------------------------------------------------------
 
 
 # RPad (Right-Pad)  -->  w/ SPACES
@@ -10,6 +12,9 @@ STRING="rpad"; LENGTH="10"; echo "[$(printf "%-${LENGTH}s" "${STRING}";)]";
 FLOAT="123.45"; LENGTH="10"; echo "${FLOAT}" | awk '{printf "%*.*f\n", FL, FL-1-length(int($1)), $1}' FL="${LENGTH}";
 
 
+# ------------------------------------------------------------
+
+
 # LPad (Left-Pad)  -->  w/ SPACES
 STRING="lpad"; LENGTH="10"; echo "[ $(printf "%${LENGTH}s" "${STRING}";)]";
 
@@ -17,20 +22,6 @@ STRING="lpad"; LENGTH="10"; echo "[ $(printf "%${LENGTH}s" "${STRING}";)]";
 # LPad (Left-Pad)  -->  w/ ZEROES
 INTEGER="12345"; LENGTH="10"; echo "[$(printf "%0${LENGTH}d" "${INTEGER}";)]";
 FLOAT="123.45"; LENGTH="10"; LENGTH_DECIMALS="3"; echo "[$(printf "%0${LENGTH}.${LENGTH_DECIMALS}f" "${FLOAT}";)]";
-
-
-# ------------------------------------------------------------
-
-
-# printf - prepending strings before command-output
-find "/var/log/" \
--type f \
--iname syslog* \
--exec printf "$(date --utc +'%Y-%m-%dT%H:%M:%S.%NZ';) $(whoami)@$(hostname) | " \; \
--exec echo "------------------------------------------------------------" \; \
--exec cat '{}' \; \
--exec echo "------------------------------------------------------------" \; \
-;
 
 
 # ------------------------------------------------------------

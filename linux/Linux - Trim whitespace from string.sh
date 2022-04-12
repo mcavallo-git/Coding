adf54a293c28ca9ccf6c4365eded6b91d0d8698f
Matt Cavallo <mcavallo.git@gmail.com>
2022-04-12 19:51:42 -0400

    Update Linux - Trim whitespace from string.sh

--- linux/Linux - Trim whitespace from string.sh
+++ linux/Linux - Trim whitespace from string.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+# ------------------------------------------------------------
 
+# trim string to specific length
+STRING="$(seq 0 9 | tr -d "\n";)";  # Repeat "-" 60 times)";
+STRING_TRIMMED="$(echo -e "${STRING}" | awk "{print substr(\$0,5,${#STRING})}";)";
+echo -e "\nSTRING=[${STRING}]\nSTRING_TRIMMED=[${STRING_TRIMMED}]\n";
+# returns:
+# STRING=[0123456789]
+# STRING_TRIMMED=[456789]
+
+
+# ------------------------------------------------------------
+
+# trim whitespace from a string
 STRING="  a  b  c  d  ";
 STRING_TRIMMED="$(echo -e "${STRING}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//';)";
-echo "STRING=[${STRING}]";
-echo "STRING_TRIMMED=[${STRING_TRIMMED}]";
+echo -e "\nSTRING=[${STRING}]\nSTRING_TRIMMED=[${STRING_TRIMMED}]\n";
+# returns:
+# STRING=[  a  b  c  d  ]
+# STRING_TRIMMED=[a  b  c  d]
 
 
 # ------------------------------------------------------------
@@ -11,5 +26,7 @@ echo "STRING_TRIMMED=[${STRING_TRIMMED}]";
 # Citation(s)
 #
 #   stackoverflow.com  |  "string - How to trim whitespace from a Bash variable? - Stack Overflow"  |  https://stackoverflow.com/a/3232433
+#
+#   unix.stackexchange.com  |  "linux - Trim lines to a specific length - Unix & Linux Stack Exchange"  |  https://unix.stackexchange.com/a/446965
 #
 # ------------------------------------------------------------

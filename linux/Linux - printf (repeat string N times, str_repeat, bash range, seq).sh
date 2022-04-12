63b67cca457d52ec3b17393268db4e296332723a
Matt Cavallo <mcavallo.git@gmail.com>
2022-04-12 19:39:18 -0400

    rn

--- linux/Linux - printf (repeat string N times, str_repeat, bash range, seq).sh
+++ linux/Linux - printf (repeat string N times, str_repeat, bash range, seq).sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 # ------------------------------------------------------------
-# Linux - printf (repeat string N times, str_repeat)
+# Linux - printf (repeat string N times, str_repeat, bash range, seq)
 # ------------------------------------------------------------
 
 
-# Repeat string  -  General syntax (hardcoded count)
+# Repeat string  -  General syntax (hardcoded count using a bash range)
 printf -- "-%.0s" {1..60};  # Repeat "-" 60 times
 
 
-# Repeat string  -  General syntax (variable count)
+# Repeat string  -  General syntax (hardcoded count using seq)
+printf -- "-%.0s" $(seq 60);  # Repeat "-" 60 times
+
+
+# Repeat string  -  General syntax (variable count using seq)
 CHAR="-"; COUNT=60; printf -- "${CHAR}%.0s" $(seq ${COUNT});  # Repeat "-" 60 times
 
 
 # ------------------------------------------------------------
 #
 # Citation(s)
+#
+#   linuxhint.com  |  "Bash Range"  |  https://linuxhint.com/bash_range/
 #
 #   www.cyberciti.biz  |  "How to repeat a character 'n' times in Bash - nixCraft"  |  https://www.cyberciti.biz/faq/repeat-a-character-in-bash-script-under-linux-unix/
 #

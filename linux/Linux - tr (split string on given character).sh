7bb8e44ee59b4cc13888020c7d1c1e3949b9c445
Matt Cavallo <mcavallo.git@gmail.com>
2022-04-12 19:48:15 -0400

    Update Linux - tr (split string on given character).sh

--- linux/Linux - tr (split string on given character).sh
+++ linux/Linux - tr (split string on given character).sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-#
+# ------------------------------------------------------------
 # tr - "translate or delete characters" (e.g. splits strings on a given character)
+# ------------------------------------------------------------
+
+# tr - remove newlines from string
+seq 0 9 | tr -d "\n";
+# returns: "0123456789"
+
+
 # ------------------------------------------------------------
 
 
@@ -9,8 +16,8 @@ for EACH_PATH in $(manpath | tr ":" "\n"); do echo -e "\n${EACH_PATH}"; ls -al "
 
 # ------------------------------------------------------------
 #
-#	Citation(s)
+# Citation(s)
 #
-#		stackoverflow.com  |  "How do I split a string on a delimiter in Bash?"  |  https://stackoverflow.com/a/918931
+#   stackoverflow.com  |  "How do I split a string on a delimiter in Bash?"  |  https://stackoverflow.com/a/918931
 #
 # ------------------------------------------------------------

245666967b239fb3708afae0841df707895bcfe8
Matt Cavallo <mcavallo.git@gmail.com>
2022-04-12 16:41:28 -0400

    Update Linux - jp (command-line interpreter-processor for JMESPath (a JSON query language)).sh

--- linux/Linux - jp (command-line interpreter-processor for JMESPath (a JSON query language)).sh
+++ linux/Linux - jp (command-line interpreter-processor for JMESPath (a JSON query language)).sh
@@ -3,7 +3,7 @@
 # Linux - jp (command-line interpreter-processor for JMESPath (a JSON query language))
 # ------------------------------------------------------------
 #
-# jp - Install
+# jp - install
 #
 
 curl -H 'Cache-Control: no-cache' -s "https://raw.githubusercontent.com/mcavallo-git/cloud-infrastructure/master/usr/local/sbin/install_jp?t=$(date +'%s.%N')" | env GITHUB_OWNER="jmespath" GITHUB_REPO="jp" VERSION_CLI="--version 2>'/dev/null' | rev | cut -d' ' -f1 | rev" bash;  # install jp

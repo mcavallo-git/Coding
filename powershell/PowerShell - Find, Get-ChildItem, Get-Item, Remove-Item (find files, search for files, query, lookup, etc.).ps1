0e11db0a26bd0649ed40504247916a4cffae0dfd
Matt Cavallo <mcavallo.git@gmail.com>
2022-06-22 15:19:08 -0400

    rn

--- powershell/PowerShell - Find, Get-ChildItem, Get-Item, Remove-Item (find files, search for files, query, lookup, etc.).ps1
+++ powershell/PowerShell - Find, Get-ChildItem, Get-Item, Remove-Item (find files, search for files, query, lookup, etc.).ps1
@@ -7,7 +7,6 @@
 # Get-ChildItem - Find local files whose...
 #
 
-
 # Filename contains "___"
 Get-ChildItem -Path ("C:\") -File -Recurse -Force -EA:0 | Where-Object { ($_.Name -Like "*___*") } | ForEach-Object { $_.FullName; };
 
@@ -128,6 +127,106 @@ Get-ChildItem -Path ("C:\") -File -Recurse -Force -EA:0 | Where-Object { @("deve
 
 # ------------------------------------------------------------
 #
+# File files matching "*.json" nested exactly two folders deep within current directory
+#
+
+(Get-Item ".\*\*.json") | Format-List *;
+
+
+# ------------------------------------------------------------
+#
+# Remove files (after verifying that they exist)
+#
+
+$Parent_Directory = ".";
+$Filenames_To_Remove = @();
+$Filenames_To_Remove += ("metadata.json");
+Get-ChildItem -Path ("${Parent_Directory}") -File -Recurse -Depth (1) -Force -ErrorAction "SilentlyContinue" `
+| Where-Object { (${Filenames_To_Remove}) -Contains ("$($_.Name)"); } `
+| ForEach-Object { `
+	$Each_Fullpath = ("$($_.FullName)");
+	Write-Host "Removing file with path  `"${Each_Fullpath}`"  ..."; `
+	Remove-Item -Path ("${Each_Fullpath}") -Force; `
+}
+
+
+# ------------------------------------------------------------
+#
+# Get all different language types from amongst target files
+#
+Get-ChildItem -Path ("C:\Windows\System32") -File -Recurse -Depth (0) -Force -ErrorAction "SilentlyContinue" `
+| ForEach-Object { If (($_.VersionInfo) -NE ($Null)) { $_.VersionInfo.Language; }; } `
+| Select-Object -Unique `
+| Sort-Object `
+;
+
+
+# ------------------------------------------------------------
+#
+# Show filepaths for files based in the [ Chinese ] language
+#
+Get-ChildItem -Path ("C:\Windows\System32") -File -Recurse -Depth (0) -Force -ErrorAction "SilentlyContinue" `
+| Where-Object { If (($_.VersionInfo) -NE ($Null)) { $_.VersionInfo.Language -Like "*Chinese*"; } Else { $False; }; } `
+| ForEach-Object { $_.FullName; } `
+
+
+# ------------------------------------------------------------
+#
+# Get-Item --> Get target filepath's dirname, basename, file-extension, etc.
+#
+If ($True) {
+	$FullPath = "${PSCommandPath}";
+	$PathItem = (Get-Item -Path "${FullPath}");
+	Write-Host "";
+	Write-Host "`n `${PathItem}.FullName  = `"$( ${PathItem}.FullName )`"";
+	Write-Host "`n `${PathItem}.DirectoryName  = `"$( ${PathItem}.DirectoryName )`"";
+	Write-Host "`n `${PathItem}.Name  = `"$( ${PathItem}.Name )`"";
+	Write-Host "`n `${PathItem}.Basename  = `"$( ${PathItem}.Basename )`"";
+	Write-Host "`n `${PathItem}.Extension  = `"$( ${PathItem}.Extension )`"";
+}
+
+
+# ------------------------------------------------------------
+#
+# EX) Google Photos Export --> Update media's "Date Created" timestamp/datetime on media files based off of their included (exported) JSON metadata files' contents
+#
+
+### REFER TO:  "PowerShell - Google Photo Export Cleanup.ps1"  (in the same source-repository as this file)
+
+
+# ------------------------------------------------------------
+#
+# EX) Remove ASUS' existent leftover executables (from "C:\Windows\System32")
+#
+
+Get-ChildItem -Path ("C:\Windows\System32") -File -Recurse -Depth (1) -Force -ErrorAction "SilentlyContinue" `
+| Where-Object { @("AsusDownloadAgent.exe", "AsusDownLoadLicense.exe", "AsusUpdateCheck.exe") -Contains ("$($_.Name)"); } `
+| ForEach-Object { `
+	$Each_Fullpath = ("$($_.FullName)");
+	Write-Host "Removing file with path  `"${Each_Fullpath}`"  ..."; `
+	Remove-Item -Path ("$Each_Fullpath") -Recurse -Force -Confirm:$False; `
+} `
+;
+
+
+# ------------------------------------------------------------
+#
+# Delete files (to the recycle bin)
+#  |--> Date-based (based on LastWriteTime) --> File [ matches filename syntax ... ]  &&  [ is older than ... ]
+#
+
+Add-Type -AssemblyName "Microsoft.VisualBasic"; <# Required to use Recycle Bin action 'SendToRecycleBin' #>
+$FullPath_Pattern = "C:\ISO\*";
+$Retention_Days = "90";
+$Retention_OldestAllowedDate = (Get-Date).AddDays([int]${Retention_Days} * -1);
+Get-ChildItem -Path "$(Split-Path -Path ("${FullPath_Pattern}") -Parent;)" -File -Recurse -Force -EA:0 `
+| Where-Object { ($_.Name -Like "$(Split-Path -Path ("${FullPath_Pattern}") -Leaf;)") } `
+| Where-Object { $_.LastWriteTime -LT ${Retention_OldestAllowedDate} } `
+| ForEach-Object { [Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile("$(${_}.FullName)",'OnlyErrorDialogs','SendToRecycleBin'); <# Delete file to the Recycle Bin #> } `
+;
+
+# ------------------------------------------------------------
+#
 # TO-DO:  Debug & determine the most effective syntax for file searches between [ [System.IO.Directory]::EnumerateFiles(...) ] and [ Get-Childitem ... ]  (also how to use the former in place of the latter (GCI) in all use cases where GCI can be used)
 #
 #
@@ -137,6 +236,8 @@ Get-ChildItem -Path ("C:\") -File -Recurse -Force -EA:0 | Where-Object { @("deve
 # ------------------------------------------------------------
 #
 #	Citation(s)
+#
+#   docs.microsoft.com  |  "about_Properties - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_properties?view=powershell-5.1
 #
 #   docs.microsoft.com  |  "about_Providers - PowerShell"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_providers
 #
@@ -150,13 +251,19 @@ Get-ChildItem -Path ("C:\") -File -Recurse -Force -EA:0 | Where-Object { @("deve
 #
 #   docs.microsoft.com  |  "Get-ChildItem - Gets the items and child items in one or more specified locations"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem?view=powershell-5.1
 #
+#   docs.microsoft.com  |  "Get-Item - Gets the item at the specified location"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-item?view=powershell-5.1
+#
+#   docs.microsoft.com  |  "Remove-Item - Deletes the specified items"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/remove-item?view=powershell-5.1
+#
+#   powershell-guru.com  |  "# The fastest Powershell 4 : Count all files in a large network share"  |  https://powershell-guru.com/fastest-powershell-2-count-all-files-in-large-network-share/
+#
 #   stackoverflow.com  |  "Delete files older than 15 days using PowerShell - Stack Overflow"  |  https://stackoverflow.com/a/19326146
+#
+#   stackoverflow.com  |  "How can I delete files with PowerShell without confirmation? - Stack Overflow"  |  https://stackoverflow.com/a/43611773
 #
 #   stackoverflow.com  |  "Variables in nested Foreach-Object and Where-Object"  |  https://stackoverflow.com/a/26715697
 #
 #   superuser.com  |  "Powershell to delete all files with a certain file extension"  |  https://superuser.com/a/1233722
-#
-#   powershell-guru.com  |  "# The fastest Powershell 4 : Count all files in a large network share"  |  https://powershell-guru.com/fastest-powershell-2-count-all-files-in-large-network-share/
 #
 #   www.thomasmaurer.ch  |  "PowerShell: Delete Files older than - Thomas Maurer"  |  https://www.thomasmaurer.ch/2010/12/powershell-delete-files-older-than/
 #

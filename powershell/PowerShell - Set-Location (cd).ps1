fe165943e7decfbcbb2a77b5d75a6bf65627216e
Matt Cavallo <mcavallo.git@gmail.com>
2022-06-22 17:08:21 -0400

    Create PowerShell - Set-Location (cd).ps1

---
+++ powershell/PowerShell - Set-Location (cd).ps1
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Set-Location (cd)
+#  |
+#  |--> Sets the current working location to a specified location  (e.g. changes directory to the given directory)
+#
+# ------------------------------------------------------------
+
+
+Set-Location -Path ("${Env:USERPROFILE}\Desktop");
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Set-Location (Microsoft.PowerShell.Management) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-location?view=powershell-7.2
+#
+# ------------------------------------------------------------

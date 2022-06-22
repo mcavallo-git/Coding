13e51786fd48537529f73e5d412fee977e699b8b
Matt Cavallo <mcavallo.git@gmail.com>
2022-06-22 16:28:45 -0400

    Create PowerShell - Select-String (regex multiline).ps1

---
+++ powershell/PowerShell - Select-String (regex multiline).ps1
@@ -0,0 +1,34 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Select-String
+#
+# ------------------------------------------------------------
+
+
+<# Select-String (case IN-sensitive) #>
+Write-Output "Line 1" "Line 2" "Line 3" "Line 4" "Line 5" | Select-String -Pattern '^[a-z]';  # Returns all lines
+
+
+<# Select-String (case SENSITIVE) #>
+Write-Output "Line 1" "Line 2" "Line 3" "Line 4" "Line 5" | Select-String -CaseSensitive -Pattern '^[a-z]';  # Returns ""
+
+
+<# Using "-Context PRE,POST" to get the lines before/after a matched line #>
+Write-Output "Line 1" "Line 2" "Line 3" "Line 4" "Line 5" `
+| Select-String -Pattern '^Line 3$' -Context 1,1 `
+| ForEach-Object {
+Write-Host "`$_.Context.PreContext:   $($_.Context.PreContext)";
+Write-Host "`$_.Line:                 $($_.Line)";
+Write-Host "`$_.Context.PostContext:  $($_.Context.PostContext)";
+}
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.microsoft.com  |  "Select-String (Microsoft.PowerShell.Utility) - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/select-string
+#
+#   stackoverflow.com  |  "powershell - How to Select-String multiline? - Stack Overflow"  |  https://stackoverflow.com/a/55374303
+#
+# ------------------------------------------------------------

a00cd3c65f2d2b68e06320dbcf85e471c248ce46
Matt Cavallo <mcavallo.git@gmail.com>
2022-06-29 08:05:26 -0400

    Update Linux - if, elif, else (conditionals, elif=else if).sh

--- linux/Linux - if, elif, else (conditionals, elif=else if).sh
+++ linux/Linux - if, elif, else (conditionals, elif=else if).sh
@@ -1,53 +1,54 @@
 # ------------------------------------------------------------
 #
-#	IF VARIABLE IS SET/EMPTY
+# IF VARIABLE IS SET/EMPTY
 #
 
 if [ ! -v var ]; then
-	# UNSET
-	echo "\$var is unset";
+  # UNSET
+  echo "\$var is unset";
 if [ -z ${var+x} ]; then # Note: Do NOT wrap ${var+x} with quotes
-	# UNSET
-	echo "\$var is unset";
+  # UNSET
+  echo "\$var is unset";
 elif [ -z "${var}" ]; then
-	# SET, EMPTY
-	echo "\$var is set but empty";
+  # SET, EMPTY
+  echo "\$var is set but empty";
 else
-	# SET, NOT-EMPTY
-	echo "\$var is set to '$var'";
+  # SET, NOT-EMPTY
+  echo "\$var is set to '$var'";
 fi;
 
 
 # ------------------------------------------------------------
 #
-#	IF STRING EQUALS ... EXACTLY
+# IF STRING EQUALS ... EXACTLY
 #
 
 if [ -n "${SUBDOMAIN}" ] && [ -n "${DOMAIN}" ]; then # vars are both set & not empty
-	FQDN="${SUBDOMAIN}.${DOMAIN}";
-	echo "${FQDN}";
+  FQDN="${SUBDOMAIN}.${DOMAIN}";
+  echo "${FQDN}";
 else
-	echo "STRING VAR(S) UNSET / EMPTY";
+  echo "STRING VAR(S) UNSET / EMPTY";
 fi;
 
 
 # ------------------------------------------------------------
 #
-# IF USER CONFIRMS-YES
+# IF USER CONFIRMS "YES"
 #  |--> Prompt for user confirmation  (press 'y' to confirm)
 #
+
 read -p "Are you sure you want to continue?  (press 'y' to confirm)" -n 1 -t 20 <'/dev/tty'; # Await single keypress
 if [ -n "${REPLY}" ] && [ "$(echo ${REPLY} | tr '[:lower:]' '[:upper:]';)" == "Y" ]; then
-	echo "Confirmed";
+  echo "Confirmed";
 else
-	echo "Skipped";
+  echo "Skipped";
 fi;
 
 
 # ------------------------------------------------------------
 #
-#	IF STRING EQUALS ...
-#	    |--> Test if a given string (LEFT) is an exact match to a known value (RIGHT)
+# IF STRING EQUALS ...
+#     |--> Test if a given string (LEFT) is an exact match to a known value (RIGHT)
 #
 
 if [ "string" == "value" ]; then echo 1; else echo 0; fi;
@@ -55,8 +56,8 @@ if [ "string" == "value" ]; then echo 1; else echo 0; fi;
 
 # ------------------------------------------------------------
 #
-#	IF STRING CONTAINS ...
-#	    |--> Test if a string (LEFT) contains a given substring/value (RIGHT) using [ Needle-in-Haystack ] methodology
+# IF STRING CONTAINS ...
+#     |--> Test if a string (LEFT) contains a given substring/value (RIGHT) using [ Needle-in-Haystack ] methodology
 #
 
 if [[ "string" == *"needle"* ]]; then echo 1; else echo 0; fi;
@@ -64,52 +65,53 @@ if [[ "string" == *"needle"* ]]; then echo 1; else echo 0; fi;
 
 # ------------------------------------------------------------
 #
-#	IF STRING MATCHES REGEX-PATTERN
-#	    |--> Note: Do NOT wrap regex pattern in quotes
+# IF STRING MATCHES REGEX-PATTERN
+#     |--> Note: Do NOT wrap regex pattern in quotes
 #
 
 if [[ "string" =~ pattern ]]; then echo 1; else echo 0; fi;
 
 
 #
-#	Example
-#		Check to see if string is a date in 'yyyymmdd' format
-#		( Citation: Thanks to user 'fedorqui' on stackoverflow - https://stackoverflow.com/questions/21112707 )
+# Example
+#   Check to see if string is a date in 'yyyymmdd' format
+#   ( Citation: Thanks to user 'fedorqui' on stackoverflow - https://stackoverflow.com/questions/21112707 )
 #
 
 StringToTest="20190331";
 if [[ ${StringToTest} =~ ^[0-9]{4}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$ ]]; then
-	echo "Valid Date: \"${StringToTest}\"";
+  echo "Valid Date: \"${StringToTest}\"";
 else
-	echo "Invalid Date: \"${StringToTest}\"";
+  echo "Invalid Date: \"${StringToTest}\"";
 fi;
 
 
 # ------------------------------------------------------------
 #
-#	If [...] Then {...}
-#	Else-If [...] Then {...}
-#	Else-If [...] Then {...}
-#	Else {...}
+# If [...] Then {...}
+# Else-If [...] Then {...}
+# Else-If [...] Then {...}
+# Else {...}
 #
-#		Example File: /bash_x
-#		Example Desc:  Check if a valid-first-argument was passed to current script, fail-out otherwise
+#   Example File: /bash_x
+#   Example Desc:  Check if a valid-first-argument was passed to current script, fail-out otherwise
 #
+
 if [ -n "$0" ]; then
-	DK_CMD="${0}";
-	if [[ "${DK_CMD}" == "/bash_production" ]] || [[ "${DK_CMD}" == "/bashprod" ]]; then
-		echo "Production";
-	elif [[ "${DK_CMD}" == "/bash_development" ]] || [[ "${DK_CMD}" == "/bashdev" ]]; then
-		echo "Development";
-	elif [[ "${DK_CMD}" == "/bash_quality_assurance" ]] || [[ "${DK_CMD}" == "/bashqa" ]]; then
-		echo "Quality Assurance";
-	else
-		echo -e "\n\n$ 0: Un-handled Bash Command: \"${0}\"\n\n";
-		exit 1;
-	fi;
+  DK_CMD="${0}";
+  if [[ "${DK_CMD}" == "/bash_production" ]] || [[ "${DK_CMD}" == "/bashprod" ]]; then
+    echo "Production";
+  elif [[ "${DK_CMD}" == "/bash_development" ]] || [[ "${DK_CMD}" == "/bashdev" ]]; then
+    echo "Development";
+  elif [[ "${DK_CMD}" == "/bash_quality_assurance" ]] || [[ "${DK_CMD}" == "/bashqa" ]]; then
+    echo "Quality Assurance";
+  else
+    echo -e "\n\n$ 0: Un-handled Bash Command: \"${0}\"\n\n";
+    exit 1;
+  fi;
 else 
-	echo -e "\n\n$ 0: Variable is either unset or contains a null value\n\n";
-	exit 1;
+  echo -e "\n\n$ 0: Variable is either unset or contains a null value\n\n";
+  exit 1;
 fi;
 exit 0;
 
@@ -119,23 +121,24 @@ exit 0;
 # IF COMMAND EXISTS
 #  |--> Determine if a given Linux-Command exists
 #
+
 COMMAND_LOOKUP="docker";
 COMMAND_FOUND=0;
 echo "";
 echo "Checking for Linux Command \"${COMMAND_LOOKUP}\" on \"$(hostname)\"...";
 if ! COMMAND_RESOLVED_PATH="$(type -p "$COMMAND_LOOKUP")" || [ -z "$COMMAND_RESOLVED_PATH" ]; then
-	echo "  Command NOT found: \"${COMMAND_LOOKUP}\"";
-	COMMAND_FOUND=0;
+  echo "  Command NOT found: \"${COMMAND_LOOKUP}\"";
+  COMMAND_FOUND=0;
 else
-	echo "  Command FOUND: \"${COMMAND_LOOKUP}\" -> \"${COMMAND_RESOLVED_PATH}\"";
-	COMMAND_FOUND=1;
+  echo "  Command FOUND: \"${COMMAND_LOOKUP}\" -> \"${COMMAND_RESOLVED_PATH}\"";
+  COMMAND_FOUND=1;
 fi;
 IS_DOCKER_INSTALLED=${COMMAND_FOUND};
 
 
 # ------------------------------------------------------------
 #
-#	USING [ test ] COMMAND INSTEAD OF [ if-elif-else-fi ] CONDITIONAL BLOCK(S)
+# USING [ test ] COMMAND INSTEAD OF [ if-elif-else-fi ] CONDITIONAL BLOCK(S)
 #
 
 test "1" == "0" && echo 1 || echo 0;
@@ -147,6 +150,7 @@ TEST_PATH="${HOME}/.bashrc"; test -e "${TEST_PATH}" && echo "Path \"${TEST_PATH}
 #
 # Uppercase a string (Bashism - requires /bin/bash to use)
 #
+
 EXAMPLE="Dat-Example-Doe" && echo "${EXAMPLE^^}";
 
 
@@ -154,13 +158,26 @@ EXAMPLE="Dat-Example-Doe" && echo "${EXAMPLE^^}";
 #
 # Lowercase a string (Bashism - requires /bin/bash to use)
 #
+
 EXAMPLE="Dat-Example-Doe" && echo "${EXAMPLE,,}";
+
+
+# ------------------------------------------------------------
+#
+# Case insensitive string comparisons
+#
+
+VAR1="STRING";
+VAR2="string";
+if [ "${VAR1}" = "${VAR2}" ]; then echo 1; else echo 0; fi;  # Returns 0 (no match)
+if [ "${VAR1,,}" = "${VAR2,,}" ]; then echo 1; else echo 0; fi;  # Returns 1 (successful match)
 
 
 # ------------------------------------------------------------
 #
 # Determine if a given Linux-User exists
 #
+
 DOES_USER_EXIST="ubuntu";
 if [ -n "$(id -u ${DOES_USER_EXIST})" ]; then # user-id exists in this environment
 # if [[ $(id -u ${DOES_USER_EXIST}) =~ ^-?[0-9]+$ ]]; then
@@ -175,17 +192,18 @@ fi;
 # Get a NON-EMPTY response string from the user running a given script
 #   NOTE: allows spaces and zero
 #
+
 echo "";
 echo "Did you enter a non-empty string?";
 read -p "Your response:  " -n 1 -t 60 <'/dev/tty'; # Await single keypress
 # Check for valid data-entry
 if [ ! -n "${REPLY}" ]; then
-	echo "";
-	echo "Empty or Null response detected - exiting...";
-	exit 1;
+  echo "";
+  echo "Empty or Null response detected - exiting...";
+  exit 1;
 else
-	echo "";
-	echo "Great! You entered a non-empty string!";
+  echo "";
+  echo "Great! You entered a non-empty string!";
 fi;
 
 
@@ -208,90 +226,91 @@ OS_IS_UBUNTU=$(if [[ $(cat /etc/*release | grep -i ubuntu | wc -l) -gt 0 ]]; the
 #
 # Get all SSH-Enabled users on the server
 #
+
 ALL_LINUX_USERS=$(cut -d: -f1 /etc/passwd);
 for EACH_SSH_USER in ${ALL_LINUX_USERS}; do
-	DIR_USER_HOME=$(eval echo ~${EACH_SSH_USER});
-	DIR_SSH_HOME="${DIR_USER_HOME}/.ssh";
-	DIR_ETC_KEYS="/etc/ssh/authorized_keys";
-	FILE_SSH_ETC="${DIR_ETC_KEYS}/${EACH_SSH_USER}";
-	if [ -d "${DIR_SSH_HOME}" ] || [ -f "${FILE_SSH_ETC}" ]; then
-		echo "User [${EACH_SSH_USER}] IS an SSH-User";
-	else
-		echo "User [${EACH_SSH_USER}] ISNT an SSH-User";
-	fi;
+  DIR_USER_HOME=$(eval echo ~${EACH_SSH_USER});
+  DIR_SSH_HOME="${DIR_USER_HOME}/.ssh";
+  DIR_ETC_KEYS="/etc/ssh/authorized_keys";
+  FILE_SSH_ETC="${DIR_ETC_KEYS}/${EACH_SSH_USER}";
+  if [ -d "${DIR_SSH_HOME}" ] || [ -f "${FILE_SSH_ETC}" ]; then
+    echo "User [${EACH_SSH_USER}] IS an SSH-User";
+  else
+    echo "User [${EACH_SSH_USER}] ISNT an SSH-User";
+  fi;
 done;
 
 
 # ------------------------------------------------------------
 #
-#	Variable Comparators
-#		|--> Integer Comparators
-#		|--> Float/Double Comparators
-#		|--> ASCII String Comparators
+# Variable Comparators
+#   |--> Integer Comparators
+#   |--> Float/Double Comparators
+#   |--> ASCII String Comparators
 #
 #
-#  ==		String (IS_INTEGER TEST):
-#					if [[ $A =~ ^-?[0-9]+$ ]]   :::   True if $A is an integer (string input, only)
-#					if [[ "$A" =~ ^-?[0-9]+$ ]]   :::   True if $A is an integer (strings, integer, or float input)
+#  ==   String (IS_INTEGER TEST):
+#         if [[ $A =~ ^-?[0-9]+$ ]]   :::   True if $A is an integer (string input, only)
+#         if [[ "$A" =~ ^-?[0-9]+$ ]]   :::   True if $A is an integer (strings, integer, or float input)
 #
 #
-#  ==		Strings:
-#					if [ "$A" == "$B" ]  :::  True if $A is equal to $B
-#  ==		Integers:
-#					if [ $A -eq $B ]     :::  True if $A is equal to $B
-#					if [ "$a" == "z*" ]  :::  True if $A is equal to z* (literal matching)
-#					if [ $a == z* ]      :::  File globbing and word splitting take place
-#					if [[ $A == z* ]]    :::  True if $A starts with a "z" (string pattern matching)
-#					if [[ $A == *z ]]    :::  True if $A ends with a "z" (string pattern matching)
-#					if [ "$a" == "z*" ]  :::  True if $A is equal to z* (literal matching)
-#					if [[ $a == "z*" ]]  :::  True if $a is equal to z* (literal matching)
-#  ==		Floats/Doubles:
-#					if [ $(echo "$A == $B" | bc) -eq 1 ]; then echo "$A IS equal to $B"; else echo "$A ISNT equal to $B"; fi;
+#  ==   Strings:
+#         if [ "$A" == "$B" ]  :::  True if $A is equal to $B
+#  ==   Integers:
+#         if [ $A -eq $B ]     :::  True if $A is equal to $B
+#         if [ "$a" == "z*" ]  :::  True if $A is equal to z* (literal matching)
+#         if [ $a == z* ]      :::  File globbing and word splitting take place
+#         if [[ $A == z* ]]    :::  True if $A starts with a "z" (string pattern matching)
+#         if [[ $A == *z ]]    :::  True if $A ends with a "z" (string pattern matching)
+#         if [ "$a" == "z*" ]  :::  True if $A is equal to z* (literal matching)
+#         if [[ $a == "z*" ]]  :::  True if $a is equal to z* (literal matching)
+#  ==   Floats/Doubles:
+#         if [ $(echo "$A == $B" | bc) -eq 1 ]; then echo "$A IS equal to $B"; else echo "$A ISNT equal to $B"; fi;
 #
 #
-#  !=		Strings:
-#					if [ "$A" != "$B" ] :::  True if $A not equal to $B
-#  !=		Integers:
-#					if [ $A -ne $B ]    :::  True if $A not equal to $B
-#					if [ "$A" != "$B" ] :::  True if $A not equal to $B (can also pattern match, see '==' section, above)
-#  !=		Floats/Doubles:
-#					if [ $(echo "$A != $B" | bc) -eq 1 ]; then echo "$A IS a different value than $B"; else echo "$A ISNT a different value than $B"; fi;
+#  !=   Strings:
+#         if [ "$A" != "$B" ] :::  True if $A not equal to $B
+#  !=   Integers:
+#         if [ $A -ne $B ]    :::  True if $A not equal to $B
+#         if [ "$A" != "$B" ] :::  True if $A not equal to $B (can also pattern match, see '==' section, above)
+#  !=   Floats/Doubles:
+#         if [ $(echo "$A != $B" | bc) -eq 1 ]; then echo "$A IS a different value than $B"; else echo "$A ISNT a different value than $B"; fi;
 #
 #
-#  <		Integers:
-#					if [ $A -lt $B ]     :::  True if $A is less than $B
-#					if (("$A" < "$B"))   :::  True if $A is less than $B
-#					if [[ "$A" < "$B" ]] :::  True if $A, as a string, has a lesser ASCII value than $B does, also as a string
-#					if [ "$A" \< "$B" ]  :::  Same as previous? --> Note that the "<" needs to be escaped within a [ ] construct
-#  <		Floats/Doubles:
-#					if [ $(echo "$A < $B" | bc) -eq 1 ]; then echo "$A IS less than $B"; else echo "$A ISNT less than $B"; fi;
-#
-#
-#
-#  <=		Integers:
-#					if [ $A -le $B ]     :::  True if $A is less than or equal to $B
-#					if (("$A" <= "$B"))  :::  True if $A is less than or equal to $B
-#  <=		Floats/Doubles:
-#					if [ $(echo "$A <= $B" | bc) -eq 1 ]; then echo "$A IS less than or equal to $B"; else echo "$A ISNT less than or equal to $B"; fi;
+#  <    Integers:
+#         if [ $A -lt $B ]     :::  True if $A is less than $B
+#         if (("$A" < "$B"))   :::  True if $A is less than $B
+#         if [[ "$A" < "$B" ]] :::  True if $A, as a string, has a lesser ASCII value than $B does, also as a string
+#         if [ "$A" \< "$B" ]  :::  Same as previous? --> Note that the "<" needs to be escaped within a [ ] construct
+#  <    Floats/Doubles:
+#         if [ $(echo "$A < $B" | bc) -eq 1 ]; then echo "$A IS less than $B"; else echo "$A ISNT less than $B"; fi;
 #
 #
 #
-#  >		Integers:
-#					if [ $A -gt $B ]     :::  True if $A (as an int) is greater than $B (as an int)
-#					if [ "$A" -gt "$B" ] :::  True if $A (as an int) is greater than $B (as an int)
-#					if (("$A" > "$B"))   :::  True if $A is greater than $B
-#					if [[ "$A" > "$B" ]] :::  True if $A (as a string) is greater than $B (as a string) - the greater ASCII value trumps
-#					if [ "$A" \> "$B" ]  :::  Same as previous? --> Note that the ">" needs to be escaped within a [ ] construct
-#  >		Floats/Doubles:
-#					if [ $(echo "$A > $B" | bc) -eq 1 ]; then echo "$A IS greater than $B"; else echo "$A ISNT greater than $B"; fi;
-#	
+#  <=   Integers:
+#         if [ $A -le $B ]     :::  True if $A is less than or equal to $B
+#         if (("$A" <= "$B"))  :::  True if $A is less than or equal to $B
+#  <=   Floats/Doubles:
+#         if [ $(echo "$A <= $B" | bc) -eq 1 ]; then echo "$A IS less than or equal to $B"; else echo "$A ISNT less than or equal to $B"; fi;
 #
 #
-#  >=		Integers:
-#					if [ $A -ge $B ]    :::  True if $A is greater than or equal to $B
-#					if (("$A" >= "$B")) :::  True if $A is greater than or equal to $B
-#  >=		Floats/Doubles:
-#					if [ $(echo "$A >= $B" | bc) -eq 1 ]; then echo "$A IS greater than or equal to $B"; else echo "$A ISNT greater than or equal to $B"; fi;
+#
+#  >   Integers:
+#         if [ $A -gt $B ]     :::  True if $A (as an int) is greater than $B (as an int)
+#         if [ "$A" -gt "$B" ] :::  True if $A (as an int) is greater than $B (as an int)
+#         if (("$A" > "$B"))   :::  True if $A is greater than $B
+#         if [[ "$A" > "$B" ]] :::  True if $A (as a string) is greater than $B (as a string) - the greater ASCII value trumps
+#         if [ "$A" \> "$B" ]  :::  Same as previous? --> Note that the ">" needs to be escaped within a [ ] construct
+#  >   Floats/Doubles:
+#         if [ $(echo "$A > $B" | bc) -eq 1 ]; then echo "$A IS greater than $B"; else echo "$A ISNT greater than $B"; fi;
+# 
+#
+#
+#  >=   Integers:
+#         if [ $A -ge $B ]    :::  True if $A is greater than or equal to $B
+#         if (("$A" >= "$B")) :::  True if $A is greater than or equal to $B
+#  >=   Floats/Doubles:
+#         if [ $(echo "$A >= $B" | bc) -eq 1 ]; then echo "$A IS greater than or equal to $B"; else echo "$A ISNT greater than or equal to $B"; fi;
 #
 #
 #
@@ -303,7 +322,7 @@ done;
 #
 
 if [ -n "${foo}" ] && [[ -z ${foo//[0-9]/} ]]; then
-	echo "foo is an integer!";
+  echo "foo is an integer!";
 fi;
 
 
@@ -315,17 +334,17 @@ fi;
 
 # BASMISM - REQUIRES THE  [ /etc/bash ]  SHELL
 if [ -n "${foo}" ] && [[ $((foo)) == $foo ]]; then
-	echo "foo is an integer!";
+  echo "foo is an integer!";
 fi;
 
 
 # ------------------------------------------------------------
 #
 #  null
-#					if [ -z $A ]      :::  True if A is a string and is null (has zero length)
+#         if [ -z $A ]      :::  True if A is a string and is null (has zero length)
 #
 #  not-null
-#					if [ -n $A ]      :::  True if A is a string and ISNT null (non-zero string-length)
+#         if [ -n $A ]      :::  True if A is a string and ISNT null (non-zero string-length)
 #
 #
 # ------------------------------------------------------------
@@ -389,21 +408,21 @@ if [ ${DAT_ARRAY[${DAT_KEY}]+X} ]; then echo "1 key is set"; else echo "0 key no
 #
 # Citation(s)
 #
-#		www.gnu.org  |  "6.4 Bash Conditional Expressions"  |  https://www.gnu.org/software/bash/manual/bashref.html#Bash-Conditional-Expressions
+#   www.gnu.org  |  "6.4 Bash Conditional Expressions"  |  https://www.gnu.org/software/bash/manual/bashref.html#Bash-Conditional-Expressions
 #
-#		www.gnu.org  |  "6.5 Shell Arithmetic"  |  https://www.gnu.org/software/bash/manual/html_node/Shell-Arithmetic.html#Shell-Arithmetic
+#   www.gnu.org  |  "6.5 Shell Arithmetic"  |  https://www.gnu.org/software/bash/manual/html_node/Shell-Arithmetic.html#Shell-Arithmetic
 #
-#		www.tldp.org  |  "7.3. Other Comparison Operators" (Integer/String Conditionals)  |  https://www.tldp.org/LDP/abs/html/comparison-ops.html
+#   www.tldp.org  |  "7.3. Other Comparison Operators" (Integer/String Conditionals)  |  https://www.tldp.org/LDP/abs/html/comparison-ops.html
 #
-#		pubs.opengroup.org  |  "2.6.2 Parameter Expansion"  |  https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_06_02
+#   pubs.opengroup.org  |  "2.6.2 Parameter Expansion"  |  https://pubs.opengroup.org/onlinepubs/9699919799/utilities/V3_chap02.html#tag_18_06_02
 #
-#		stackexchange.com  |  "Bash regex =~ operator"  |  https://unix.stackexchange.com/a/340485
+#   stackexchange.com  |  "Bash regex =~ operator"  |  https://unix.stackexchange.com/a/340485
 #
 #   stackoverflow.com  |  "Case insensitive comparison of strings in shell script"  |  https://stackoverflow.com/a/19411918
 #
-#		stackoverflow.com  |  "How to check if a variable is set in Bash?"  |  https://stackoverflow.com/a/13864829
+#   stackoverflow.com  |  "How to check if a variable is set in Bash?"  |  https://stackoverflow.com/a/13864829
 #
-#		stackoverflow.com  |  "Is there an easy way to determine if user input is an integer in bash?"  |  https://stackoverflow.com/a/4141321
+#   stackoverflow.com  |  "Is there an easy way to determine if user input is an integer in bash?"  |  https://stackoverflow.com/a/4141321
 #
 #   stackoverflow.com  |  "bash - Easiest way to check for an index or a key in an array? - Stack Overflow"  |  https://stackoverflow.com/a/13221491
 #

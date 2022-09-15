95f81ebf0d1bf082bd2295b13ec4177e59faa0c3
Matt Cavallo <mcavallo.git@gmail.com>
2022-09-15 07:46:41 -0400

    Create unifi-os - common ssh commands (UDM).sh

---
+++ ubiquiti/unifi-os - common ssh commands (UDM).sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   3os.org  |  "CLI Commands - 3os"  |  https://3os.org/infrastructure/ubiquiti/udm-dream-machine/cli-commands/
+#
+# ------------------------------------------------------------

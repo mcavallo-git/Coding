0eabfe283f1af6fec1d7b6b1c626eb78ef1314b4
Matt Cavallo <mcavallo.git@gmail.com>
2022-09-23 03:58:41 -0400

    Update JMESPath - contains().ps1

--- jmespath/JMESPath - contains().ps1
+++ jmespath/JMESPath - contains().ps1
@@ -1,13 +1,28 @@
 # ------------------------------------------------------------
+# JMESPath - contains()
+# ------------------------------------------------------------
 
-
-# Get Azure Managed Identities who are in any Resource Group which contains a given string
-#  |--> Intended to find Managed Identities within nested AKS Clusters' Resource Groups (which contain their parent resource group in their name)
+#
+# Ex)  Get Azure Managed Identities who are in any Resource Group which contains a given string
+#       |--> Managed Identities within nested AKS Cluster Resource Groups (which contain their parent Resource Group in their name)
+#
 (az group list --query "sort_by[], &name) | [].name" --output "tsv") `
 | ForEach-Object {
 	Write-Host "------------------------------------------------------------`nListing Managed Identities in Resource Group `"${_}`"...";
 	az identity list --query "[? contains(resourceGroup,'${_}')] | sort_by([], &name) | [].name" --output "tsv";
 }
+
+
+#
+# Ex)  Get Azure IPv4 Ranges
+#
+IPV4_ADDRESSES="$(az network list-service-tags --location "eastus2" --output "tsv" --query "values[? name=='AzureCloud.eastus2'].properties.addressPrefixes[] | [? ! contains(@,'::')]";)"; echo "${IPV4_ADDRESSES}";
+
+
+#
+# Ex)  Get Azure IPv6 Ranges
+#
+IPV6_ADDRESSES="$(az network list-service-tags --location "eastus2" --output "tsv" --query "values[? name=='AzureCloud.eastus2'].properties.addressPrefixes[] | [? contains(@,'::')]";)"; echo "${IPV6_ADDRESSES}";
 
 
 # ------------------------------------------------------------

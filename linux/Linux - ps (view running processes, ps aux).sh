826a5437943f3c9efecbcf08baad99fac6e876ad
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-07 16:07:03 -0400

    Update Linux - ps (view running processes, ps aux).sh

--- linux/Linux - ps (view running processes, ps aux).sh
+++ linux/Linux - ps (view running processes, ps aux).sh
@@ -3,21 +3,18 @@
 #------------------------------------------------------------
 #
 # view all running processes
+#
 
-# ps aux;
-# ^-- this is NOT the same as [ ps -aux ] - do not add any dashes to the arguments list of 'aux'
+# ps aux; ##  this is NOT the same as [ ps -aux ] - do not add any dashes to the arguments list of 'aux'
 
-ps -A \
---format 'fname,user,pid,%cpu,%mem,drs,rss,maj_flt,cmd' \
---sort='user,fname';
-
+ps -A --format 'fname,user,pid,%cpu,%mem,drs,rss,maj_flt,cmd' --sort='user,fname';
 
 
 # ------------------------------------------------------------
 #
 #	Match a specific process-name, process runtikme-user, etc.
 #  |--> return any associated process-id(s) found to match
-
+#
 
 PROC_CMD="rsyslogd"; \
 PROC_USER="syslog"; \
@@ -31,37 +28,34 @@ echo -e "\n\nPROC_RESULTS:"; \
 echo -e "${PROC_RESULTS}\n\n";
 
 
-
 # ------------------------------------------------------------
 #
 #	get the absolute path (of a running process)
+#
 
 sudo ls -l /proc/PID/exe;
-
 
 
 # ------------------------------------------------------------
 #
 #	get the working directory (of a running process)
+#
 
 pwdx $pid;
-
 
 
 # ------------------------------------------------------------
 #
 # Determine linux-user running a given process
 #
-# Ex) Determine user running the Jenkins service
 
+# Ex) Determine user running the Jenkins service
 PS_AUX_COMMAND_CONTAINS="jenkins.war";
 PS_AUX_MATCHED_LINES=$(ps aux | grep '/usr/bin/daemon' | grep "${PS_AUX_COMMAND_CONTAINS}");
-
 PROCESS_UNAME=$(echo "${PS_AUX_MATCHED_LINES}" | awk {'print $1'});
 PROCESS_GNAME=$(id -gn ${PROCESS_UNAME});
 PROCESS_GID=$(id -g ${PROCESS_UNAME});
 PROCESS_UID=$(id -u ${PROCESS_UNAME});
-
 echo ""; \
 echo -e "PS_AUX_MATCHED_LINES\n${PS_AUX_MATCHED_LINES}"; \
 echo ""; \
@@ -70,7 +64,6 @@ echo "PROCESS_GNAME = ${PROCESS_GNAME}"; \
 echo "PROCESS_GID = ${PROCESS_GID}"; \
 echo "PROCESS_UID = ${PROCESS_UID}"; \
 echo "";
-
 
 
 # ------------------------------------------------------------

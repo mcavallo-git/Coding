16f0ba23d13d60e6f78ec67df09d65aab633b4f3
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-11 07:34:20 -0400

    Update VMware ESXi - Set Hostname.sh

--- vmware/VMware ESXi - Set Hostname.sh
+++ vmware/VMware ESXi - Set Hostname.sh
@@ -8,16 +8,24 @@ esxcfg-advcfg -g "/Misc/hostname";
 esxcfg-advcfg -s "HOSTNAME.DOMAIN.TLD" "/Misc/hostname"; /etc/init.d/hostd restart;
 
 
-### Manual Network Config 
+### Manual Network Config
+if [ 1 -eq 1 ]; then
 LAN_IPv4="";
-echo "# Do not remove the following line, or various programs" > "/etc/hosts"; \
-echo "# that require network functionality will fail." >> "/etc/hosts"; \
-echo "127.0.0.1       localhost.localdomain localhost" >> "/etc/hosts"; \
-echo "::1             localhost.localdomain localhost" >> "/etc/hosts"; \
-if [ -n "${LAN_IPv4}" ]; then \
-echo "${LAN_IPv4}       ${FQDN} ${HOSTNAME}" >> "/etc/hosts"; \
+if [ -z "${LAN_IPv4}" ]; then
+echo "No IPv4 address defined - Please set \$LAN_IPV4 with desired static IPv4 address";
+else
+echo "# Do not remove the following line, or various programs" > "/etc/hosts";
+echo "# that require network functionality will fail." >> "/etc/hosts";
+echo "127.0.0.1       localhost.localdomain localhost" >> "/etc/hosts";
+echo "::1             localhost.localdomain localhost" >> "/etc/hosts";
+if [ -n "$(hostname -d)" ] && [ "$(hostname -f)" != "$(hostname -s)" ]; then
+echo "${LAN_IPv4}     $(hostname -f) $(hostname -s)" >> "/etc/hosts";
+else
+echo "${LAN_IPv4}     $(hostname)" >> "/etc/hosts";
 fi;
 /etc/init.d/hostd restart; # Restart the [ ESXi host daemon ] service (should be accessible again within ~15-30 seconds) Note: Doesn't affect any VMs
+fi;
+fi;
 
 
 

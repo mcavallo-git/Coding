86499bfb1fab49e86209455aefde3dd892a2e041
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-11 18:59:23 -0400

    rn

--- powershell/PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger).ps1
+++ powershell/PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger).ps1
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------
-# PowerShell - Scheduled Task - Run script block as Administrator (without referencing a script file)
+# PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger)
 # ------------------------------------------------------------
 #
 # QuickNoteSniper  (Runs as NON-Admin)
@@ -30,6 +30,32 @@ $Args_SchedTask=@{ TaskName="SetHomepage_Edge"; Description="SetHomepage_Edge"; 
 #
 
 $Args_SchedTask=@{ TaskName="Startup_StopProcesses"; Description="Startup_StopProcesses"; Trigger=(New-ScheduledTaskTrigger -AtStartup); Action=(New-ScheduledTaskAction -Execute ((GCM powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((GCM powershell).Source) -ArgumentList ('-Command While (1) { Get-Process -Name redcloak* 2>`$Null | Stop-Process -Force <# redcloak #>; Get-Process -Name flow 2>`$Null | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name smartaudio* 2>`$Null | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name tanium* 2>`$Null | Stop-Process -Force <# tanium #>; Get-Process -Name fc* 2>`$Null | Stop-Process -Force <# mcafee #>; Start-Sleep -Seconds (1); };') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
+
+# ------------------------------------------------------------
+#
+# Run program as NON-Admin from an elevated session-terminal
+#  |
+#  |--> Permission Step-Down/De-Escalation/De-Elevation via a temporary NON-elevated scheduled task which runs desired runtime
+#
+
+
+If (1 -Eq 1) {
+  SV TEMP_Execute C:\Windows\System32\notepad.exe;
+  SV TEMP_Name (Get-Date -UFormat (([String][Char]37)+([String][Char]115)));
+  SV TEMP_Action (New-ScheduledTaskAction -Execute ((GV TEMP_Execute).Value));
+  SV TEMP_Trigger (New-ScheduledTaskTrigger -Once -At (Get-Date));
+  Register-ScheduledTask -Action ((GV TEMP_Action).Value) -Trigger ((GV TEMP_Trigger).Value) -TaskName ((GV TEMP_Name).Value) | Out-Null;
+  Start-ScheduledTask -TaskName ((GV TEMP_Name).Value);
+  Start-Sleep -Seconds 1;
+  Unregister-ScheduledTask -TaskName ((GV TEMP_Name).Value) -Confirm:([Boolean](0));
+}
+
+# As a one-liner:
+If (1 -Eq 1) { SV TEMP_Execute C:\Windows\System32\notepad.exe; SV TEMP_Name (Get-Date -UFormat (([String][Char]37)+([String][Char]115))); SV TEMP_Action (New-ScheduledTaskAction -Execute ((GV TEMP_Execute).Value)); SV TEMP_Trigger (New-ScheduledTaskTrigger -Once -At (Get-Date)); Register-ScheduledTask -Action ((GV TEMP_Action).Value) -Trigger ((GV TEMP_Trigger).Value) -TaskName ((GV TEMP_Name).Value) | Out-Null; Start-ScheduledTask -TaskName ((GV TEMP_Name).Value); Start-Sleep -Seconds 1; Unregister-ScheduledTask -TaskName ((GV TEMP_Name).Value) -Confirm:([Boolean](0)); };
+
+
+# As a nested command intended to be used from an external app kicking off this command through cmd
+cmd.exe /C "powershell.exe -WindowStyle Hidden -Command ""If (1 -Eq 1) { SV TEMP_Execute C:\Windows\System32\notepad.exe; SV TEMP_Name (Get-Date -UFormat (([String][Char]37)+([String][Char]115))); SV TEMP_Action (New-ScheduledTaskAction -Execute ((GV TEMP_Execute).Value)); SV TEMP_Trigger (New-ScheduledTaskTrigger -Once -At (Get-Date)); Register-ScheduledTask -Action ((GV TEMP_Action).Value) -Trigger ((GV TEMP_Trigger).Value) -TaskName ((GV TEMP_Name).Value); Start-ScheduledTask -TaskName ((GV TEMP_Name).Value); Start-Sleep -Seconds 1; Unregister-ScheduledTask -TaskName ((GV TEMP_Name).Value) -Confirm:([Boolean](0)); };"""
 
 
 # ------------------------------------------------------------

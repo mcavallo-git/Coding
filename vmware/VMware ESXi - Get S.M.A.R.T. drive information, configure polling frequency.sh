7c22f6a1091c401fb39f071e9ba4f832d4267ee7
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-11 20:23:46 -0400

    Update VMware ESXi - Get S.M.A.R.T. drive information, configure polling frequency.sh

--- vmware/VMware ESXi - Get S.M.A.R.T. drive information, configure polling frequency.sh
+++ vmware/VMware ESXi - Get S.M.A.R.T. drive information, configure polling frequency.sh
@@ -14,8 +14,7 @@ cat /var/log/syslog.log | tail -n 20;
 #         (default Polling interval is 30 minutes)
 
 
-/sbin/smartd -i 1;
-
+# /sbin/smartd -i 5; # THIS IS WHAT IS DESIRED TO RUN AS THE SERVICE
 
 cat /etc/init.d/smartd;
 
@@ -25,22 +24,9 @@ find / -iname *smart*;
 
 cat /var/run/vmware/smartd.PID
 # /etc/init.d/smartd
-# /etc/vmware/smart_plugin.conf
-# /etc/vmware/default.map.d/smartpqi.map
 # /var/run/vmware/smartd.PID
-# /var/lib/initenvs/installer/chkconfig/disabled/smartd
-# /tardisks/smartpqi.v00
 # /bin/smartd
-# /lib64/libsmartmgt.so
-# /lib/libsmartmgt.so
-# /usr/share/hwdata/default.pciids.d/smartpqi.ids
-# /usr/lib/vmware/smart_plugins
-# /usr/lib/vmware/smart_plugins/libsmartnvme.so
-# /usr/lib/vmware/smart_plugins/libsmartmicron.so
-# /usr/lib/vmware/smart_plugins/libsmartscsi.so
-# /usr/lib/vmware/smart_plugins/libsmartsata.so
-# /usr/lib/vmware/vm-support/bin/smartinfo
-# /usr/lib/vmware/vmkmod/smartpqi
+# ...
 
 
 # Find the process which is holding up smartd from working as intended
@@ -52,6 +38,8 @@ ps | grep $(cat /var/run/vmware/smartd.PID;);
 
 rm -fv /var/run/vmware/smartd.PID;
 
+/etc/init.d/smartd status;
+
 /etc/init.d/smartd start;
 
 # ------------------------------
@@ -60,24 +48,39 @@ vi "/etc/rc.local.d/local.sh";
 
 # Add the following into "local.sh":
 
-# Poll the SMART daemon every 5 minutes (instead of the default of every 30)
-SMARTD_POLL_INTERVAL=5
+# Poll the SMART daemon every 15 minutes (instead of the default of every 30)
+SMARTD_POLL_INTERVAL=15
 /etc/init.d/smartd stop;
-sed -r "s/^(.+ -t \"\\\$\{MAX_RETRIES\}\" )(.+$)/\1-i ${SMARTD_POLL_INTERVAL:-5} \2/g" -i "/etc/init.d/smartd";
+sed -r "s/^(.+ -t \"\\\$\{MAX_RETRIES\}\" )(\"\\\$\{SMARTD\}\".+$)/\1-i ${SMARTD_POLL_INTERVAL:-15} \2/g" -i "/etc/init.d/smartd";
 /etc/init.d/smartd start;
-
 
 
 # ------------------------------
 
 # Pre-Check - Get the config & service status before running the update
-cat /etc/init.d/smartd;  ps -c | grep smartd | grep -v grep; 
+cat /etc/init.d/smartd;
+ps -c | grep smartd | grep -v grep;
 
 # Run the update
 /etc/rc.local.d/local.sh
 
 # Post-Check - Verify the configuration was applied as-intended
-cat /etc/init.d/smartd;  ps -c | grep smartd | grep -v grep; 
+cat /etc/init.d/smartd;
+ps -c | grep smartd | grep -v grep;
+
+
+
+# ------------------------------
+# Finally, get smart disk info at a reasonable pace
+
+esxcli storage core device list \
+| grep -i 'Other UIDs:' \
+| awk '{print $3}' \
+| while read EACH_DEVICE_ID; do
+echo "------------------------------";
+echo "EACH_DEVICE_ID:  ${EACH_DEVICE_ID}";
+esxcli storage core device smart get -d "${EACH_DEVICE_ID}";
+done;
 
 
 # ------------------------------------------------------------

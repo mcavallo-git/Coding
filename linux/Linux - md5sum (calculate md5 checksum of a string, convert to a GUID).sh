068c0a410dba3ede14e8cb7f58e77438521cbcd6
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-17 08:06:30 -0400

    Create Linux - md5sum (calculate md5 checksum of a string, convert to a GUID).sh

---
+++ linux/Linux - md5sum (calculate md5 checksum of a string, convert to a GUID).sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# ------------------------------------------------------------
+# Linux - md5sum (calculate md5 checksum of a string, convert to a GUID)
+# ------------------------------------------------------------
+
+# Get the md5 checksum for a given string
+echo -n "tester" | md5sum | awk '{print toupper($1)}';
+
+
+# ------------------------------------------------------------
+
+# Get the md5 checksum for a given string - convert to GUID format
+echo -n "tester" | md5sum | awk '{print toupper($1)}' | sed -rne "s/^([0-9A-F]{8})([0-9A-F]{4})([0-9A-F]{4})([0-9A-F]{4})([0-9A-F]{12})$/\1-\2-\3-\4-\5/p";
+
+
+# ------------------------------------------------------------

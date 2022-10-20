7e014209f4ba73e3630208db51cbb1d39446acdd
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-20 06:48:15 -0400

    Update PowerShell - Update-SSH-Managers (MobaXterm, FileZilla).ps1

--- powershell/PowerShell - Update-SSH-Managers (MobaXterm, FileZilla).ps1
+++ powershell/PowerShell - Update-SSH-Managers (MobaXterm, FileZilla).ps1
@@ -10,7 +10,7 @@ $FilezillaSiteSettings = ("${Env:USERPROFILE}\AppData\Roaming\FileZilla\sitemana
 $Haystack = (Get-Content ("${FilezillaSiteSettings}"));
 
 # Define the "Pitchfork", e.g. the regex pattern which helps us parse through the "Haystack" to find the "Needle"
-$Pitchfork_RegexDeprecatedKeys = ('<Keyfile>([a-zA-Z]\:[\w\s\\\-_\.\,\(\)]+(?:.ssh|_SSH)[\w\s\\\-_\.\,\(\)]*(?:boneal|bnl)[\w\s\\\-_\.\,\(\)]*\.pem)</Keyfile>');
+$Pitchfork_RegexDeprecatedKeys = ('<Keyfile>([a-zA-Z]\:[\w\s\\\-_\.\,\(\)]+(?:.ssh|_SSH)[\w\s\\\-_\.\,\(\)]*(?:FIRSTNAME|LASTNAME)[\w\s\\\-_\.\,\(\)]*\.pem)</Keyfile>');
 
 # Define the "Needle", e.g. the matches returned from the regex (the output from the "Pitchfork" pitching through the "Haystack")
 $Needle_Key = [Regex]::Match(${Haystack}, ${Pitchfork_RegexDeprecatedKeys}); # Parse through the "Haystack", looking for the "Needle"
@@ -19,7 +19,7 @@ While (($Needle_Key.Success) -eq $true) {
 	$Needle_Key = [Regex]::Match(${Haystack}, ${Pitchfork_RegexDeprecatedKeys});
 }
 
-$Pitchfork_RegexDeprecatedUsers = ('<User>boneal</User>');
+$Pitchfork_RegexDeprecatedUsers = ('<User>FIRSTNAME LASTNAME</User>');
 $Needle_User = [Regex]::Match(${Haystack}, ${Pitchfork_RegexDeprecatedUsers});
 While (($Needle_User.Success) -eq $true) {
 	$Haystack = $Haystack.Replace(($Needle_User.Value),(('<User>')+(${NewUser})+('</User>')));

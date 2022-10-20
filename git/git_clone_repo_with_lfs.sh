6905777a1439c4048f4fc542e3fcf5c2f6f16b16
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-20 06:44:50 -0400

    Update git_clone_repo_with_lfs.sh

--- git/git_clone_repo_with_lfs.sh
+++ git/git_clone_repo_with_lfs.sh
@@ -1,20 +1,15 @@
-
+#!/bin/sh
+# ------------------------------------------------------------
 # Cloning a Git Repository & keeping Git-Logs & Large-File-Storage
-#     --> MCavallo, 2018-11-13_17-51-25
-
-## Start by Git Bash'ing at your base GitHub Repository directory
-##   --> AKA: Open windows explorer to "%USERPROFILE%\Documents\GitHub" then right click the folder & select "Git Bash Here"
-
-mkdir clonefolder
-
-cd clonefolder/
-
-# Following Guide "Mirroring a repository that contains Git Large File Storage objects"
-#   from: https://help.github.com/articles/duplicating-a-repository/#mirroring-a-repository-that-contains-git-large-file-storage-objects
+# ------------------------------------------------------------
 
 SOURCE_REPO="https://github.com/OWNER_SOURCE/REPO_SOURCE";
 
 DESTINATION_REPO="https://github.com/OWNER_DEST/REPO_DEST.git";
+
+mkdir "${HOME}/temp_mirror_dir";
+
+cd "${HOME}/temp_mirror_dir";
 
 git clone --bare ${SOURCE_REPO};
 
@@ -34,4 +29,13 @@ rm -rf "$(basename "${REPO_SOURCE}";).git";
 
 cd ..;
 
-rm -rf clonefolder;
+rm -rf "${HOME}/temp_mirror_dir";
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   docs.github.com  |  "Duplicating a repository - GitHub Docs"  |  https://docs.github.com/en/repositories/creating-and-managing-repositories/duplicating-a-repository#mirroring-a-repository-that-contains-git-large-file-storage-objects
+#
+# ------------------------------------------------------------

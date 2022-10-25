a9280373ccac87b1a1267b28896e3614c65e042e
Matt Cavallo <mcavallo.git@gmail.com>
2022-10-25 03:23:45 -0400

    Update Linux - touch (create file, set last-modified datetime, timestamp, etc.).sh

--- linux/Linux - touch (create file, set last-modified datetime, timestamp, etc.).sh
+++ linux/Linux - touch (create file, set last-modified datetime, timestamp, etc.).sh
@@ -11,22 +11,22 @@ touch "example.txt";
 
 # ------------------------------------------------------------
 #
-# Set the last-modified datetime/timestamp for a file
+# Set a file's last-modified datetime
 #
 
-# Set last-modified datetime using input format:  YYYY-MM-DD HH:MM:SS
+# Set last-modified datetime - Input format:  YYYY-MM-DD HH:MM:SS
 touch -d "$(date -R --date='2018-10-01 14:54:22';)" "example.txt";
 
-
-# Set last-modified datetime using input format:  @Unix-Seconds (Epoch Seconds)
+# Set last-modified datetime - Input format:  @Unix-Seconds (Epoch Seconds)
 touch -d "$(date -R --date='@1539785880';)" "example.txt";
 
-
-# Set last-modified datetime using input format:  iso-8601
+# Set last-modified datetime - Input format:  iso-8601
 touch -d "$(date -R --date='2018-10-01T14:54:22,384929728-04:00';)" "example.txt";
 
-
-# Set last-modified datetime - Parameterized example
+# ------------------------------------------------------------
+#
+# Set last-modified datetime (and create datetime if file doesn't exist) - Parameterized example
+#
 if [[ 1 -eq 1 ]]; then
 FILEPATH_TO_UPDATE="example.txt";  # Target filepath to update
 DATE_LAST_MODIFIED="3000-12-31 23:59:59";  # Timestamp to set modified date to

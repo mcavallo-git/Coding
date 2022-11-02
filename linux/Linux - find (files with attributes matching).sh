18329c4826009b56c9e9019db503d70067739548
Matt Cavallo <mcavallo.git@gmail.com>
2022-11-02 09:30:21 -0400

    Update Linux - find (files with attributes matching).sh

--- linux/Linux - find (files with attributes matching).sh
+++ linux/Linux - find (files with attributes matching).sh
@@ -78,9 +78,9 @@ find "/var/log" -type 'f' -name "*" | wc -l;
 
 
 # ------------------------------
-
-# Extensions - List unique file extensions (sorted) for all files contained within a directory & subdirectories
-
+#
+# Extensions - List unique file extensions (sorted) for all files contained within a directory & subdirectories (note: ignores extensionless files)
+#
 for EACH_FILE in $(find "/var/log" -type "f" -exec basename '{}' \;;); do if [[ "${EACH_FILE}" != "${EACH_FILE##*.}" ]]; then echo "${EACH_FILE##*.}"; fi; done | sort -u;
 
 # ------------------------------

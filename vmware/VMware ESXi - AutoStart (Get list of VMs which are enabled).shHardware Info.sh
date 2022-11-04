6d49f7c660b12733e1120dc1ae4d010d5b9e8c0b
Matt Cavallo <mcavallo.git@gmail.com>
2022-11-04 03:33:12 -0400

    Update VMware ESXi - AutoStart (Get list of VMs which are enabled).shHardware Info.sh

--- vmware/VMware ESXi - AutoStart (Get list of VMs which are enabled).shHardware Info.sh
+++ vmware/VMware ESXi - AutoStart (Get list of VMs which are enabled).shHardware Info.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
+# ------------------------------
 
-if [[ 1 -eq 1 ]]; then
 #
-# ESXi - Check the VMs startup settings
+# ESXi - Get startup settings for all VMs
 #
 vim-cmd hostsvc/autostartmanager/get_autostartseq
 
-fi;
+
+#
+# ESXi - Disable autostart for a VM which keeps starting on bootup even though the "AutoStart" option for it is set to "Disabled" (in the ESXi GUI)
+#  |
+#  |--> Note: Get Vmid (Virtual Machine ID#) from command [ vim-cmd vmsvc/getallvms ]
+#
+vim-cmd hostsvc/autostartmanager/update_autostartentry [Vmid] "none" "5" "1" "none" "5" "yes"
+
+
+# Example:  Disable Autostart for VM with Vmid==3
+vim-cmd hostsvc/autostartmanager/update_autostartentry 3 "none" "5" "1" "none" "5" "yes"
 
 
 # ------------------------------------------------------------
 #
 # Citation(s)
+#
+#   superuser.com  |  "How can I disable autostart for a VM in ESXi or vSphere? - Super User"  |  https://superuser.com/a/1305659
 #
 #   theitbros.com  |  "Configure Autostart of VM on VMware ESXi – TheITBros"  |  https://theitbros.com/config-virtual-machine-auto-startup-vmware-esxi/
 #

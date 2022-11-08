feee28384af28b1fa943c05ff06916f28f9f6cad
Matt Cavallo <mcavallo.git@gmail.com>
2022-11-08 00:59:56 -0500

    Remote Sensor Monitor Sensors

---
+++ prtg/Custom Sensors/EXE/get-rsm-gpuz------------------------------------------------------------.bat
@@ -0,0 +1,1 @@
+REM Note: This file is intended to exist only as a simple filename landmark between sections/categories of similarly named sensor "EXE/Script" filenames (used when creating a "Custom EXE/Script Sensor" sensor via the "EXE/Script" dropdown)
---
+++ prtg/Custom Sensors/EXE/get-rsm-gpuz-voltage-gpu-8-pin.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+SET LOGFILE=C:\ISO\RemoteSensorMonitor\results\GPU-Z.GPU.8-Pin #1 Voltage.txt
+IF NOT EXIST "%LOGFILE%" (
+ECHO :ERROR - File not found: %LOGFILE%
+EXIT 2
+) ELSE (
+TYPE %LOGFILE%
+FOR /F "tokens=* delims=: USEBACKQ" %%a IN ( `TYPE %LOGFILE% ^| FINDSTR "ERROR"` ) DO ( EXIT 2 )
+EXIT 0
+)
+REM https://www.paessler.com/manuals/prtg/custom_sensors#standard_exescript

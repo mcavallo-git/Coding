d6cb20ad699256933ab35b05f22a87736ecaea1c
Matt Cavallo <mcavallo.git@gmail.com>
2022-11-11 23:25:31 -0500

    Update PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1

--- powershell/PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1
+++ powershell/PowerShell - Resolve redirected (301,302) Urls in WebRequests.ps1
@@ -2,20 +2,23 @@
 # PowerShell - Resolve redirected Urls (301, 302, System.Net.HttpWebRequest)
 # ------------------------------------------------------------
 
+# General Syntax - Resolve a URL
+[System.Net.HttpWebRequest]::Create("__URL_TO_RESOLVE__").GetResponse().ResponseUri.AbsoluteUri;
 
-# Resolve the URL pointing to version "10.1.22621.755" of the "Windows Development SDK" (Software Development Kit)
+
+# ------------------------------
+
+# Ex) Resolve the URL pointing to version "10.1.22621.755" of the "Windows Development SDK" (Software Development Kit)
 [System.Net.HttpWebRequest]::Create("https://go.microsoft.com/fwlink/p/?linkid=2196241").GetResponse().ResponseUri.AbsoluteUri;
 
-# ------------------------------------------------------------
-
-# Resolve the URL pointing to the latest version for "kubectl" (Kubernetes CLI utility)
+# Ex) Resolve the URL pointing to the latest version for "kubectl" (Kubernetes CLI utility)
 [System.Net.HttpWebRequest]::Create("https://dl.k8s.io/release/stable.txt").GetResponse().ResponseUri.AbsoluteUri;
 
 
 # ------------------------------------------------------------
-# 
+#
 # Citation(s)
-# 
-# https://stackoverflow.com/a/45593554
-# 
+#
+#   stackoverflow.com  |  "url redirection - Powershell determine new URL of a permanently moved (redirected) resource - Stack Overflow"  |  https://stackoverflow.com/a/45593554
+#
 # ------------------------------------------------------------

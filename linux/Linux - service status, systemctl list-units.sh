3ef2f2193796c657c69aba985232bd02d3bb8343
Matt Cavallo <mcavallo.git@gmail.com>
2022-11-25 00:11:02 -0500

    Update Linux - service status, systemctl list-units.sh

--- linux/Linux - service status, systemctl list-units.sh
+++ linux/Linux - service status, systemctl list-units.sh
@@ -2,9 +2,9 @@
 # ------------------------------------------------------------
 # Linux - service status, systemctl list-units
 # ------------------------------------------------------------
-
-# List services
-
+#
+# List all services
+#
 service --status-all;
 
 systemctl --type=service;  # For OSes without the "service" command built-in (doesn't work on WSL, as systemctl doesn't work on WSL)
@@ -12,15 +12,16 @@ systemctl --type=service;  # For OSes without the "service" command built-in (do
 
 # ------------------------------
 #
-# Grab all services into a variable (requires 'sed' & 'service' packages)
+# List all services & store the result in a variable (requires 'sed' & 'service' packages)
 #
 ALL_SERVICES=$(service --status-all 2>'/dev/null' | sed --regexp-extended --quiet --expression='s/^\ \[ (\+|\-) \]\ \ (\S+)$/\2/p');
 echo "${ALL_SERVICES}";
 
 
 # ------------------------------
-
+#
 # Check if one, specific service exists, locally (requires 'sed' package)
+#
 SERVICE_NAME="sshd";
 SERVICE_RET_CODE=$(/bin/systemctl status "${SERVICE_NAME}" --no-pager --full 1>'/dev/null' 2>&1; echo $?;);
 if [ ${SERVICE_RET_CODE} -eq 0 ]; then

7b7e8d84f78dd66ce6dd4d66229e27ef84cf9c02
Matt Cavallo <mcavallo.git@gmail.com>
2022-11-26 06:28:38 -0500

    Update HomeAssistant - Pulse audio hotfix-workaround.sh

--- smarthome/homeassistant/HomeAssistant - Pulse audio hotfix-workaround.sh
+++ smarthome/homeassistant/HomeAssistant - Pulse audio hotfix-workaround.sh
@@ -11,6 +11,16 @@
 # Open terminal to HomeAssistant OS (not using "Terminal & SSH" app), or, alternatively, directly access the computer/vm directly which is running target homassistant OS.
 
 
+# Run the following command:
+find "/" -type 'f' \( -iname "system.pa" -o -iname "default.pa" \) -exec sed -r -e '/^(.*module-rescue-streams.*)$/ s/^#*/# /' -i '{}' \;;
+
+
+# Restart the hassio_audio container
+docker restart "hassio_audio";
+
+
+# ------------------------------------------------------------
+
 # Get sound card driver
 udevadm info -a -p `udevadm info -q path -n /dev/snd/controlC0` | grep -i driver | grep -i snd
 
@@ -26,16 +36,38 @@ docker exec -it "hassio_audio" '/bin/bash' 2>'/dev/null';
 
 
 # Locate references to `module-rescue-streams` within the pulse system files
-cat "/etc/pulse/default.pa" | grep -i rescue; cat "/etc/pulse/system.pa" | grep -i rescue;
+/bin/cat /etc/pulse/default.pa | /bin/grep -i rescue;
+/bin/cat /etc/pulse/system.pa | /bin/grep -i rescue;
+
+docker exec "hassio_audio" /bin/cat /etc/pulse/default.pa | /bin/grep -i rescue; \
+docker exec "hassio_audio" /bin/cat /etc/pulse/system.pa | /bin/grep -i rescue;
 
 
 # Truncate references to `module-rescue-streams` within the pulse system files
-sed -r -e "/module-rescue-streams/d" -i "/etc/pulse/default.pa"; sed -r -e "/module-rescue-streams/d" -i "/etc/pulse/system.pa";
+# sed -r -e "/module-rescue-streams/d" -i "/etc/pulse/default.pa";
+# sed -r -e "/module-rescue-streams/d" -i "/etc/pulse/system.pa";
 
 
-# Restart the docker container
+# Comment out lines containing `module-rescue-streams` within the pulse system files
+docker exec "hassio_audio" /bin/sed -r -e '/^(.*module-rescue-streams.*)$/ s/^#*/# /' -i /etc/pulse/default.pa; \
+docker exec "hassio_audio" /bin/sed -r -e '/^(.*module-rescue-streams.*)$/ s/^#*/# /' -i /etc/pulse/system.pa; \
+
+
+# Restart the hassio_audio container
 docker restart "hassio_audio";
 
+# ------------------------------------------------------------
+
+# All-in-one (fixed error) - Restart the hassio_audio container then immediately begin commenting out pulse's config files
+docker restart "hassio_audio"; while [[ 1 -eq 1 ]]; do docker exec "hassio_audio" /bin/sed -r -e '/^(.*module-rescue-streams.*)$/ s/^#*/# /' -i /etc/pulse/default.pa; docker exec "hassio_audio" /bin/sed -r -e '/^(.*module-rescue-streams.*)$/ s/^#*/# /' -i /etc/pulse/system.pa; done;
+
+
+# Locate OTHER containers which are storing the system.pa and default.pa rescue settings, and comment THOSE out (otherwise they get recreated upon hassio restart)
+find "/" -type 'f' \( -iname "system.pa" -o -iname "default.pa" \) -exec echo '{}' \; -exec grep 'rescue' '{}' \;;
+
+find "/" -type 'f' \( -iname "system.pa" -o -iname "default.pa" \) -exec grep -h 'rescue' '{}' \;;
+
+find "/" -type 'f' \( -iname "system.pa" -o -iname "default.pa" \) -exec sed -r -e '/^(.*module-rescue-streams.*)$/ s/^#*/# /' -i '{}' \;;
 
 
 # ------------------------------------------------------------
@@ -45,5 +77,7 @@ docker restart "hassio_audio";
 #   bbs.archlinux.org  |  "[solved] Pulseaudio warning about module-rescue-streams / Multimedia and Games / Arch Linux Forums"  |  https://bbs.archlinux.org/viewtopic.php?id=259601
 #
 #   github.com  |  "Audio stopped working on the host system · Issue #12 · home-assistant/plugin-audio · GitHub"  |  https://github.com/home-assistant/plugin-audio/issues/12#issuecomment-1179508339
+#
+#   stackoverflow.com  |  "linux - find -name "*.xyz" -o -name "*.abc" -exec to Execute on all found files, not just the last suffix specified - Stack Overflow"  |  https://stackoverflow.com/a/8888037
 #
 # ------------------------------------------------------------

46a641a8107318777247faa6724fb9d2d29cf930
Matt Cavallo <mcavallo.git@gmail.com>
2022-12-05 02:36:02 -0500

    Update PowerShell - String contains using -contains, .contains(), regex match (search for substrings or characters within a string).ps1

--- powershell/PowerShell - String contains using -contains, .contains(), regex match (search for substrings or characters within a string).ps1
+++ powershell/PowerShell - String contains using -contains, .contains(), regex match (search for substrings or characters within a string).ps1
@@ -19,8 +19,24 @@
 
 # ------------------------------------------------------------
 #
+# Matching operators
+#
+
+<string[]> -like    <wildcard-expression>
+
+<string[]> -notlike <wildcard-expression>
+
+<string[]> -match    <regular-expression>
+
+<string[]> -notmatch <regular-expression>
+
+
+# ------------------------------------------------------------
+#
 # Citation(s)
 #
 #   devblogs.microsoft.com  |  "PowerTip: Finding Letters in Strings with PowerShell | Scripting Blog"  |  https://devblogs.microsoft.com/scripting/powertip-finding-letters-in-strings-with-powershell/
+#
+#   learn.microsoft.com  |  "about Comparison Operators - PowerShell | Microsoft Learn"  |  https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-7.3#matching-operators
 #
 # ------------------------------------------------------------

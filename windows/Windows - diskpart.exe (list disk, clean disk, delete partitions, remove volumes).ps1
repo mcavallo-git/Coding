c4190f1cc24ded751305740964191f02e427a9ff
Matt Cavallo <mcavallo.git@gmail.com>
2022-12-12 12:09:17 -0500

    Create Windows - diskpart.exe (list disk, clean disk, delete partitions, remove volumes).ps1

---
+++ windows/Windows - diskpart.exe (list disk, clean disk, delete partitions, remove volumes).ps1
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------
+#  Windows - diskpart.exe (list disk, clean disk, delete partitions, remove volumes)
+# ------------------------------------------------------------
+#
+# Wipe partitions from a disk using diskpart
+#  |--> Open "diskpart.exe" --> Run the following commands (replacing "[X]" with your desired disk number):
+#
+
+> LIST DISK
+
+> SELECT DISK [X]
+
+> DETAIL DISK
+
+> LIST PARTITION
+
+> SELECT PARTITION [X]
+
+> DELETE PARTITION
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   learn.microsoft.com  |  "diskpart | Microsoft Learn"  |  https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/diskpart
+#
+#   www.easeus.com  |  "Tutorial: Use DiskPart Delete All Partitions | Everything You Need to Know - EaseUS"  |  https://www.easeus.com/partition-master/diskpart-delete-all-partitions.html
+#
+# ------------------------------------------------------------

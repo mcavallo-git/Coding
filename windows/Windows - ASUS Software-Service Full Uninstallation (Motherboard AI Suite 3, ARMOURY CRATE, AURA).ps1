16a96346e19d0c7d5ed07f3acc9075dba34bfc25
Matt Cavallo <mcavallo.git@gmail.com>
2022-12-12 22:18:54 -0500

    Update Windows - ASUS Software-Service Full Uninstallation (Motherboard AI Suite 3, ARMOURY CRATE, AURA).ps1

--- windows/Windows - ASUS Software-Service Full Uninstallation (Motherboard AI Suite 3, ARMOURY CRATE, AURA).ps1
+++ windows/Windows - ASUS Software-Service Full Uninstallation (Motherboard AI Suite 3, ARMOURY CRATE, AURA).ps1
@@ -11,7 +11,7 @@
 # ------------------------------------------------------------
 If ($False) { # RUN THIS SCRIPT REMOTELY:
 
-$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; $ProgressPreference='SilentlyContinue'; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Try { Invoke-Expression ((Invoke-WebRequest -UseBasicParsing -TimeoutSec (7.5) -Uri ('https://raw.githubusercontent.com/mcavallo-git/Coding/main/windows/Windows+-+ASUS+Software-Service+Full+Uninstallation+(Motherboard+AI+Suite+3%2c+ARMOURY+CRATE%2c+AURA).ps1') ).Content) } Catch {};
+$ProtoBak=[System.Net.ServicePointManager]::SecurityProtocol; [System.Net.ServicePointManager]::SecurityProtocol=[System.Net.SecurityProtocolType]::Tls12; $ProgressPreference='SilentlyContinue'; Clear-DnsClientCache; Set-ExecutionPolicy "RemoteSigned" -Scope "CurrentUser" -Force; Try { Invoke-Expression ((Invoke-WebRequest -UseBasicParsing -TimeoutSec (7.5) -Uri ('https://raw.githubusercontent.com/mcavallo-git/Coding/main/windows/Windows%20-%20ASUS%20Software-Service%20Full%20Uninstallation%20(Motherboard%20AI%20Suite%203,%20ARMOURY%20CRATE,%20AURA).ps1') ).Content) } Catch {};
 
 }
 # ------------------------------------------------------------

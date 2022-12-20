eb137912337863fc7a065152a2ea2383d4233a7d
Matt Cavallo <mcavallo.git@gmail.com>
2022-12-19 20:03:21 -0500

    mv

---
+++ powershell/Windows - Run, RunOnce (run program-script at user logon, edit task manager startup programs).ps1
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------
+# Windows - Run, RunOnce (run program-script at user logon, edit task manager startup programs)
+# ------------------------------------------------------------
+#
+# Microsoft: "Use Run or RunOnce registry keys to make a program run when a user logs on."
+#
+
+#  Run  (System)    -  Run a given command the next ANY user logs on
+HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run
+
+#  Run  (System)    -  Run a given command the next CURRENT user logs on
+HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
+
+
+#  RunOnce  (User)  -  Run a given command the next ANY user logs on (but first remove the command from the "RunOnce" registry key)
+HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce
+
+#  RunOnce  (User)  -  Run a given command the next CURRENT user logs on (but first remove the command from the "RunOnce" registry key)
+HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   learn.microsoft.com  |  "Run and RunOnce Registry Keys - Windows applications | Microsoft Docs"  |  https://learn.microsoft.com/en-us/windows/win32/setupapi/run-and-runonce-registry-keys
+#
+# ------------------------------------------------------------

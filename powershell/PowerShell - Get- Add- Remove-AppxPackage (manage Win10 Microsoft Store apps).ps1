ffd2d0de5f9cd0bb848d153a088493e36adf70c3
Matt Cavallo <mcavallo.git@gmail.com>
2022-12-19 21:34:57 -0500

    Update PowerShell - Get- Add- Remove-AppxPackage (manage Win10 Microsoft Store apps).ps1

--- powershell/PowerShell - Get- Add- Remove-AppxPackage (manage Win10 Microsoft Store apps).ps1
+++ powershell/PowerShell - Get- Add- Remove-AppxPackage (manage Win10 Microsoft Store apps).ps1
@@ -64,6 +64,32 @@ If ($True) {
 }
 
 
+#
+# Some packages are installed under the "System" user, and you must create a scheduled task to masquerade as the "System" user & uninstall them
+#
+# Open "Task Scheduler"
+#   > Create Task
+#     > Name/Description:  "Uninstall-Microsoft.YourPhone.vbs"
+#     > Trigger:  Run at system startup  (to auto-select "System" user)
+#     > Action:  Start a Program
+#       > Program/script:  C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+#       > Add arguments:  -c "Remove-AppxPackage -User 'S-1-5-21-664227053-1759174218-1597799016-1008' -Package 'Microsoft.YourPhone_1.22102.229.0_x64__8wekyb3d8bbwe';"
+#
+# Before running the task, check for the package via powershell:  Get-AppxPackage Microsoft.YourPhone -AllUsers
+#
+# Run the scheduled task
+#
+# After running the task, check for the package via powershell:  Get-AppxPackage Microsoft.YourPhone -AllUsers
+#
+
+
+#
+# Note: The App "Microsoft.YourPhone" can no longer be uninstalled - Refer to Microsoft post @ https://support.microsoft.com/en-us/windows/why-can-t-i-uninstall-the-your-phone-app-1c0ac9f4-26a2-7e5c-ec76-feb9e715db59
+#  |
+#  |--> "The Your Phone app is deeply integrated into Windows to light up multiple cross-device experiences now and in the future. In order to build more of these experiences between phones, PCs, and other devices, the app can't be uninstalled."
+#
+
+
 # ------------------------------------------------------------
 #
 # Install package(s)/application(s)   (via an installation file "Appxmanifest.xml" (standardized filename))

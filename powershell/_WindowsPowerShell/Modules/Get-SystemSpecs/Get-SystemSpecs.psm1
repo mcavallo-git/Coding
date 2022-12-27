61f18df778c9ad0bcf235d0d94fd881154d0856d
Matt Cavallo <mcavallo.git@gmail.com>
2022-12-27 01:04:10 -0500

    Update Get-SystemSpecs.psm1

--- powershell/_WindowsPowerShell/Modules/Get-SystemSpecs/Get-SystemSpecs.psm1
+++ powershell/_WindowsPowerShell/Modules/Get-SystemSpecs/Get-SystemSpecs.psm1
@@ -44,7 +44,7 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  CPU  (Processor) ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_Processor" | Select-Object -Property CurrentClockSpeed,MaxClockSpeed,Name,NumberOfCores,NumberOfLogicalProcessors | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_Processor" | Select-Object -Property CurrentClockSpeed,MaxClockSpeed,Name,NumberOfCores,NumberOfLogicalProcessors | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -54,7 +54,7 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  GPU  (Graphics/Video Card) ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_VideoController" | Select-Object -Property AdapterRAM,Description,DriverVersion,Name | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_VideoController" | Select-Object -Property AdapterRAM,Description,DriverVersion,Name | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -64,12 +64,12 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  Memory/RAM (Capacity in Bytes) ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_PhysicalMemory" | Select-Object -Property BankLabel,Capacity,DeviceLocator,FormFactor,Manufacturer,PartNumber,Speed | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_PhysicalMemory" | Select-Object -Property BankLabel,Capacity,DeviceLocator,FormFactor,Manufacturer,PartNumber,Speed | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
   Add-Content -Path ("${Logfile}") -Value ("");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  Motherboard RAM Limits (MaxCapacity in kilobytes, MemoryDevices is total RAM Slots) ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_PhysicalMemoryArray" | Select-Object -Property MaxCapacity,MemoryDevices | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_PhysicalMemoryArray" | Select-Object -Property MaxCapacity,MemoryDevices | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -79,7 +79,7 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  Disk(s)   (Size in bytes) ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_DiskDrive" | Select-Object -Property Caption,DeviceID,Index,Partitions,Size | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_DiskDrive" | Select-Object -Property Caption,DeviceID,Index,Partitions,Size | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -89,7 +89,7 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  Model / Manufacturer ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_ComputerSystem" | Select-Object -Property Manufacturer,Model,Name,SystemType | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_ComputerSystem" | Select-Object -Property Manufacturer,Model,Name,SystemType | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -103,12 +103,12 @@ Function Get-SystemSpecs() {
   Write-Output "--- " | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
   Write-Output "-----  Motherboard " | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
   Write-Output "--- " | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
-  (Get-CimInstance -ClassName "Win32_BaseBoard" | Select-Object -Property Manufacturer,Product,SerialNumber | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_BaseBoard" | Select-Object -Property Manufacturer,Product,SerialNumber | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
   Add-Content -Path ("${Logfile}") -Value ("");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  BIOS ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_BIOS" | Select-Object -Property ReleaseDate,SMBIOSBIOSVersion,SMBIOSMajorVersion,SMBIOSMinorVersion | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_BIOS" | Select-Object -Property ReleaseDate,SMBIOSBIOSVersion,SMBIOSMajorVersion,SMBIOSMinorVersion | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -118,7 +118,7 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  NICs (Network Interface Cards) ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_NetworkAdapterConfiguration" | Select-Object -Property Index,Description,IPAddress,MACAddress,DefaultIPGateway,DHCPServer | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_NetworkAdapterConfiguration" | Select-Object -Property Index,Description,IPAddress,MACAddress,DefaultIPGateway,DHCPServer | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -128,7 +128,7 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  OS (Operating System) ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_OperatingSystem" | Select-Object -Property Caption,OSArchitecture,Version | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_OperatingSystem" | Select-Object -Property Caption,OSArchitecture,Version | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 
@@ -138,7 +138,7 @@ Function Get-SystemSpecs() {
   Add-Content -Path ("${Logfile}") -Value ("--- ");
   Add-Content -Path ("${Logfile}") -Value ("-----  User/Domain ");
   Add-Content -Path ("${Logfile}") -Value ("--- ");
-  (Get-CimInstance -ClassName "Win32_ComputerSystem" | Select-Object -Property Domain,PartOfDomain,PrimaryOwnerName,UserName | Format-Table) -creplace '\P{IsBasicLatin}' | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
+  Get-CimInstance -ClassName "Win32_ComputerSystem" | Select-Object -Property Domain,PartOfDomain,PrimaryOwnerName,UserName | Format-Table | Out-File -Width 16384 -Append -FilePath ("${Logfile}");
 
   # ------------------------------
 

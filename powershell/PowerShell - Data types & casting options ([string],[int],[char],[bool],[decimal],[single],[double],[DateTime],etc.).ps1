8cf47cb8ee318d02780ed004de5de13e147198b1
Matt Cavallo <mcavallo.git@gmail.com>
2023-01-05 01:49:07 -0500

    rn

--- powershell/PowerShell - Data types & casting options ([string],[int],[char],[bool],[decimal],[single],[double],[DateTime],etc.).ps1
+++ powershell/PowerShell - Data types & casting options ([string],[int],[char],[bool],[decimal],[single],[double],[DateTime],etc.).ps1
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------
 #
-# PowerShell Data-Types (Variable casting options)
+# PowerShell - Data types & casting options ([string],[int],[char],[bool],[decimal],[single],[double],[DateTime],etc.)
+#  |
 #  |--> For insight into numeric precision for applications without being wasteful regarding system resources)
 #
 # ------------------------------------------------------------
@@ -18,7 +19,50 @@
 #   [double]    Double-precision 64-bit floating point number
 #   [DateTime]  Date and Time
 #
+# ------------------------------
 #
+#  Integer data types (determine bounds)
+#
+
+Write-Host "[short]:`nMin=[$([short]::MinValue)],`nMax=[$([short]::MaxValue)]`n";
+Write-Host "[int16]:`nMin=[$([int16]::MinValue)],`nMax=[$([int16]::MaxValue)]`n";
+
+
+Write-Host "[ushort]:`nMin=[$([ushort]::MinValue)],`nMax=[$([ushort]::MaxValue)]`n";
+Write-Host "[uint16]:`nMin=[$([uint16]::MinValue)],`nMax=[$([uint16]::MaxValue)]`n";
+
+
+Write-Host "[int]:`nMin=[$([int]::MinValue)],`nMax=[$([int]::MaxValue)]`n";
+Write-Host "[int32]:`nMin=[$([int32]::MinValue)],`nMax=[$([int32]::MaxValue)]`n";
+
+
+Write-Host "[uint]:`nMin=[$([uint]::MinValue)],`nMax=[$([uint]::MaxValue)]`n";
+Write-Host "[uint32]:`nMin=[$([uint32]::MinValue)],`nMax=[$([uint32]::MaxValue)]`n";
+
+
+Write-Host "[long]:`nMin=[$([long]::MinValue)],`nMax=[$([long]::MaxValue)]`n";
+Write-Host "[int64]:`nMin=[$([int64]::MinValue)],`nMax=[$([int64]::MaxValue)]`n";
+
+
+Write-Host "[ulong]:`nMin=[$([ulong]::MinValue)],`nMax=[$([ulong]::MaxValue)]`n";
+Write-Host "[uint64]:`nMin=[$([uint64]::MinValue)],`nMax=[$([uint64]::MaxValue)]`n";
+
+
+# ------------------------------
+#
+#  Decimal data types (determine bounds)
+#
+
+Write-Host "[decimal]:`nMin=[$([decimal]::MinValue)],`nMax=[$([decimal]::MaxValue)]`n";
+
+
+Write-Host "[single]:`nMin=[$([single]::MinValue)],`nMax=[$([single]::MaxValue)]`n";
+Write-Host "[float]:`nMin=[$([float]::MinValue)],`nMax=[$([float]::MaxValue)]`n";
+
+
+Write-Host "[double]:`nMin=[$([double]::MinValue)],`nMax=[$([double]::MaxValue)]`n";
+
+
 # ------------------------------------------------------------
 #
 # Citation(s)

94e47becd96e397da431b52525b89077967a9ffc
Matt Cavallo <mcavallo.git@gmail.com>
2023-01-06 08:17:13 -0500

    Update HWiNFO64_AtStartup_AsAdmin.ps1

--- visual basic/HWiNFO64_AtStartup_AsAdmin.ps1
+++ visual basic/HWiNFO64_AtStartup_AsAdmin.ps1
@@ -3,7 +3,6 @@ If ((GV True).Value) {
   If (((GV True ).Value) -Eq ((GV IS_LOGGED_ON).Value)) {
     Set-Location 'C:\ISO\HWiNFO64\Reports\';
     SV Logfile ((write HWiNFO64-)+(Get-Date -Format (write yyyy-MM-dd))+(write .csv));
-    SV DefaultPort (Get-Content 'C:\ISO\RemoteSensorMonitor\DefaultPort.txt' -EA:0);
     If (((Get-Process -Name 'HWiNFO64' -EA:0).Count -Eq 0) -Or ((Test-Path ((GV Logfile).Value)) -Eq ((GV False).Value))) {
       Get-Process -Name 'HWiNFO64' -EA:0 | Stop-Process -Force;
       Start-Sleep -Seconds 2;
@@ -16,23 +15,27 @@ If ((GV True).Value) {
       };
       Start-Sleep -Seconds 10;
     };
-    SV ErrorActionPreference 0;
-    SV ProgressPreference 0;
-    Try {
-      (Invoke-WebRequest -UseBasicParsing -Uri ([String]::Format('http://localhost:{0}/',((GV DefaultPort).Value))) -TimeoutSec (2)) | Out-Null;
-      SV RequestSuccess 1;
-    } Catch {
-      SV RequestSuccess 0;
-    };
-    If ((([int]((Get-Process -Name 'Remote Sensor Monitor' -EA:0).Count)) -Eq 0) -Or (((GV RequestSuccess).Value) -Eq 0)) {
-      Set-Location 'C:\ISO\RemoteSensorMonitor';
-      Get-Process -Name 'Remote Sensor Monitor' -EA:0 | Stop-Process -Force;
-      Start-Sleep -Seconds 1;
-      Start-Process -Filepath ('C:\ISO\RemoteSensorMonitor\Remote Sensor Monitor.exe') -ArgumentList ([String]::Format('-p {0} --hwinfo 1 --gpuz 0 --aida64 0 --ohm 0', ((GV DefaultPort).Value))) -NoNewWindow -EA:0;
-      If (Test-Path 'C:\ISO\RemoteSensorMonitor\DefaultConfig.txt' -EA:0) {
-        SV DefaultConf (Get-Content 'C:\ISO\RemoteSensorMonitor\DefaultConfig.txt' -EA:0);
-        Start-Sleep -Seconds 3;
-        Invoke-WebRequest -UseBasicParsing -Uri ([String]::Format('http://localhost:{0}/apply_config',((GV DefaultPort).Value))) -ContentType 'application/ -www-form-urlencoded;charset=UTF-8' -Method 'POST' -Body ((GV DefaultConf).Value);
+    SV Run_RemoteSensorMonitor ((GV False).Value);
+    If (((GV True).Value) -Eq ((GV Run_RemoteSensorMonitor).Value)) {
+      SV ErrorActionPreference 0;
+      SV ProgressPreference 0;
+      SV DefaultPort (Get-Content 'C:\ISO\RemoteSensorMonitor\DefaultPort.txt' -EA:0);
+      Try {
+        (Invoke-WebRequest -UseBasicParsing -Uri ([String]::Format('http://localhost:{0}/',((GV DefaultPort).Value))) -TimeoutSec (2)) | Out-Null;
+        SV RequestSuccess 1;
+      } Catch {
+        SV RequestSuccess 0;
+      };
+      If ((([int]((Get-Process -Name 'Remote Sensor Monitor' -EA:0).Count)) -Eq 0) -Or (((GV RequestSuccess).Value) -Eq 0)) {
+        Set-Location 'C:\ISO\RemoteSensorMonitor';
+        Get-Process -Name 'Remote Sensor Monitor' -EA:0 | Stop-Process -Force;
+        Start-Sleep -Seconds 1;
+        Start-Process -Filepath ('C:\ISO\RemoteSensorMonitor\Remote Sensor Monitor.exe') -ArgumentList ([String]::Format('-p {0} --hwinfo 1 --gpuz 0 --aida64 0 --ohm 0', ((GV DefaultPort).Value))) -NoNewWindow -EA:0;
+        If (Test-Path 'C:\ISO\RemoteSensorMonitor\DefaultConfig.txt' -EA:0) {
+          SV DefaultConf (Get-Content 'C:\ISO\RemoteSensorMonitor\DefaultConfig.txt' -EA:0);
+          Start-Sleep -Seconds 3;
+          Invoke-WebRequest -UseBasicParsing -Uri ([String]::Format('http://localhost:{0}/apply_config',((GV DefaultPort).Value))) -ContentType 'application/ -www-form-urlencoded;charset=UTF-8' -Method 'POST' -Body ((GV DefaultConf).Value);
+        };
       };
     };
   };

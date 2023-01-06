2eaed3aada18f500e2e54e8ceb6f38183db80014
Matt Cavallo <mcavallo.git@gmail.com>
2023-01-05 22:34:28 -0500

    Update get-hardware-sensor-readings.ps1

--- prtg/Custom Sensors/EXEXML/get-hardware-sensor-readings.ps1
+++ prtg/Custom Sensors/EXEXML/get-hardware-sensor-readings.ps1
@@ -254,7 +254,7 @@ If ($True) {
           # }
 
         }
-        
+
         If ($False) {
           # Output sensor data directly to file
           # ------------------------------
@@ -288,8 +288,12 @@ If ($True) {
 
   $Logfile_Dirname_HWiNFO = "C:\ISO\HWiNFO64";
   $Logfile_Input_StarsWith_HWiNFO = "HWiNFO64-";
-  $Logfile_Input_FullPath_HWiNFO = "${Logfile_Dirname_HWiNFO}\${Logfile_Input_StarsWith_HWiNFO}$(Get-Date -UFormat '%Y-%m-%d').csv";
+  $Logfile_Input_FullPath_HWiNFO = "${Logfile_Dirname_HWiNFO}\Reports\${Logfile_Input_StarsWith_HWiNFO}$(Get-Date -UFormat '%Y-%m-%d').csv";
   If ((Test-Path -PathType "Leaf" -Path ("${Logfile_Input_FullPath_HWiNFO}") -ErrorAction ("SilentlyContinue")) -Eq $False) {
+
+    $Sensor_ErrorMessage_OHW="ERROR - HWiNFO64 logfile not found: ${Logfile_Input_FullPath_HWiNFO}";
+
+  } Else {
 
     # $CsvHeadersArr_OHW = @('Time', 'Fan Control #1', 'Fan Control #2', 'Fan Control #3', 'Fan Control #4', 'Fan Control #5', 'Fan Control #6', 'Fan Control #7', 'CPU VCore', 'Voltage #2', 'AVCC', '3VCC', 'Voltage #5', 'Voltage #6', 'Voltage #7', '3VSB', 'VBAT', 'VTT', 'Voltage #11', 'Voltage #12', 'Voltage #13', 'Voltage #14', 'Voltage #15', 'Temperature #1', 'Temperature #2', 'Temperature #3', 'Temperature #4', 'Temperature #5', 'Temperature #6', 'Fan #1', 'Fan #2', 'Fan #4', 'Fan #6', 'CPU Core #1', 'CPU Core #2', 'CPU Core #3', 'CPU Core #4', 'CPU Core #5', 'CPU Core #6', 'CPU Total', 'CPU Package', 'Bus Speed', 'CPU Core #1', 'CPU Core #2', 'CPU Core #3', 'CPU Core #4', 'CPU Core #5', 'CPU Core #6', 'CPU Package', 'CPU CCD #1', 'CPU Core #1', 'CPU Core #2', 'CPU Core #3', 'CPU Core #4', 'CPU Core #5', 'CPU Core #6', 'CPU Cores', 'Memory', 'Used Memory', 'Available Memory', 'GPU Core', 'GPU Core', 'GPU Memory', 'GPU Shader', 'GPU Core', 'GPU Frame Buffer', 'GPU Video Engine', 'GPU Bus Interface', 'GPU Fan', 'GPU', 'GPU Memory Total', 'GPU Memory Used', 'GPU Memory Free', 'GPU Memory', 'GPU Power', 'GPU PCIE Rx', 'GPU PCIE Tx', 'Used Space');
 

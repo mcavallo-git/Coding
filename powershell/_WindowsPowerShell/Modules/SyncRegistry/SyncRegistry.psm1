ce22cabfa1e2574ddd1bf94eb1460bb55f2bf4b2
Matt Cavallo <mcavallo.git@gmail.com>
2023-01-26 00:45:07 -0500

    Update SyncRegistry.psm1

--- powershell/_WindowsPowerShell/Modules/SyncRegistry/SyncRegistry.psm1
+++ powershell/_WindowsPowerShell/Modules/SyncRegistry/SyncRegistry.psm1
@@ -1916,36 +1916,33 @@ function SyncRegistry {
       # Path="Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services!MaxCompressionLevel"; <# Example of Registry Path w/ inline Property name #>
 
       # ------------------------------------------------------------
-      #
-      # Power Options
-      #
 
       If (-Not ($PSBoundParameters.ContainsKey('SkipPowercfgUpdates'))) {
-
-        Write-Output "`n";
+        #
+        # Power Options
+        #
+        Write-Output "Power Options";
 
         # Set idle timeouts to 20 minutes
-        Write-Output "`nPower Options - Setting idle/monitor timeout to 20 minutes";
+        Write-Output "  |-->  Setting idle/monitor timeout to 20 minutes";
         powercfg.exe -setacvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOIDLE 1200
         powercfg.exe -setacvalueindex SCHEME_CURRENT SUB_VIDEO VIDEOCONLOCK 1200
         powercfg.exe -x -monitor-timeout-ac 1200
         powercfg.exe -x -monitor-timeout-dc 1200
 
         # Disable Hibernation
-        Write-Output "`nPower Options - Disabling hibernation";
+        Write-Output "  |-->  Disabling hibernation";
         powercfg.exe -hibernate off
         powercfg.exe -x -hibernate-timeout-ac 0
         powercfg.exe -x -hibernate-timeout-dc 0
 
         # Disable Sleep Mode
-        Write-Output "`nPower Options - Disabling sleep mode";
+        Write-Output "  |-->  Disabling sleep mode";
         powercfg.exe -x -standby-timeout-ac 0
         powercfg.exe -x -standby-timeout-dc 0
 
-        Write-Output "`nPower Options - Saving current power scheme";
+        Write-Output "  |-->  Saving current power scheme";
         powercfg.exe -setactive SCHEME_CURRENT
-
-        Write-Output "`n";
 
       }
 

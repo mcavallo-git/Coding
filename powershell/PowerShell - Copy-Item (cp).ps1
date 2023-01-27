1b3cc85aadf0d580cd1fa62dc5003494f3be5298
Matt Cavallo <mcavallo.git@gmail.com>
2023-01-27 18:58:04 -0500

    Update PowerShell - Copy-Item (cp).ps1

--- powershell/PowerShell - Copy-Item (cp).ps1
+++ powershell/PowerShell - Copy-Item (cp).ps1
@@ -1,7 +1,26 @@
 # ------------------------------------------------------------
+#
+# PowerShell - Copy file(s) from Source filepath to Destination filepath
+#
+
+Copy-Item -Path ("${Source}") -Destination ("${Destination}") -Force;
 
 
-Copy-Item -Path ($From) -Destination ($To) -Force;
+# ------------------------------------------------------------
+#
+# PowerShell - Copy files and directories from Source directory to Destination directory
+#
+
+If ($True) {
+  [string]$Source="${env:USERPROFILE}\Desktop\Source\*";
+  [string]$Destination="${env:USERPROFILE}\Desktop\Destination\";
+  # Ensure destination directory exists
+  If (($False) -Eq (Test-Path -PathType "Container" -Path ("${Destination}"))) {
+    New-Item -ItemType "Directory" -Path ("${Destination}") | Out-Null;
+  }
+  # Copy files & directories from source to destination
+  Copy-Item -Force -Recurse -Path ("${Source}") -Destination ("${Destination}");
+}
 
 
 # ------------------------------------------------------------
@@ -9,5 +28,9 @@ Copy-Item -Path ($From) -Destination ($To) -Force;
 # Citation(s)
 #
 #   learn.microsoft.com  |  "Copy-Item (Microsoft.PowerShell.Management) - PowerShell | Microsoft Learn"  |  https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/copy-item?view=powershell-5.1
+#
+#   learn.microsoft.com  |  "New-Item (Microsoft.PowerShell.Management) - PowerShell | Microsoft Learn"  |  https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/new-item?view=powershell-5.1
+#
+#   stackoverflow.com  |  "How to copy content of a folder to another specific folder using powershell? - Stack Overflow"  |  https://stackoverflow.com/a/36310667
 #
 # ------------------------------------------------------------

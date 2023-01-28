0a89a5e04be2bf8a62a0508a56771219df25f80b
Matt Cavallo <mcavallo.git@gmail.com>
2023-01-28 16:14:54 -0500

    Update PowerShell - Remove-Service (delete services by name).ps1

--- powershell/PowerShell - Remove-Service (delete services by name).ps1
+++ powershell/PowerShell - Remove-Service (delete services by name).ps1
@@ -5,15 +5,29 @@
 
 # Remove service(s) starting with "Corsair"  (case insensitive) - Used for iCUE full uninstallation
 Get-Service -Name ("Corsair*") | ForEach-Object {
-  If ("$($_.Status)" -Eq "Running") { Stop-Service -Name ($_.Name) -Force; }; <# Stop the service (if it is running) #> 
+  $Service_Name=($_.Name);
+  Write-Host "Service Name: `"${Service_Name}`"";
+  # Stop the service (if it is running)
+  If ("$($_.Status)" -Eq "Running") {
+    Write-Host "  |--> Stopping Service...";
+    Stop-Service -Name ($_.Name) -Force;
+  };
+  Write-Host "  |--> Removing Service...";
   $_ | Remove-Service; <# Remove the service (don't require confirmation) #>
 };
 
 
 # Remove service(s) named "NGINX" (case insensitive)
 Get-Service -Name ("NGINX") | ForEach-Object {
-  If ("$($_.Status)" -Eq "Running") { Stop-Service -Name ($_.Name) -Force; }; <# Stop the service (if it is running) #> 
-  $_ | Remove-Service -Confirm; <# Remove the service (require confirmation) #>
+  $Service_Name=($_.Name);
+  Write-Host "Service Name: `"${Service_Name}`"";
+  # Stop the service (if it is running)
+  If ("$($_.Status)" -Eq "Running") {
+    Write-Host "  |--> Stopping Service...";
+    Stop-Service -Name ($_.Name) -Force;
+  };
+  Write-Host "  |--> Removing Service...";
+  $_ | Remove-Service; <# Remove the service (don't require confirmation) #>
 };
 
 

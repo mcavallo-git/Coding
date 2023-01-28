c59ab941bb26fa9293fe43be0476f9a860a3dbd1
Matt Cavallo <mcavallo.git@gmail.com>
2023-01-28 16:19:04 -0500

    Update Windows - Run, RunOnce (startup programs in task manager, run program-script at user logon).ps1

--- windows/Windows - Run, RunOnce (startup programs in task manager, run program-script at user logon).ps1
+++ windows/Windows - Run, RunOnce (startup programs in task manager, run program-script at user logon).ps1
@@ -5,10 +5,10 @@
 # Microsoft: "Use Run or RunOnce registry keys to make a program run when a user logs on."
 #
 
-#  Run  (System)    -  Run a given command the next time ANY user logs on
+#  Run  (System)    -  Run a given command the next time ANY user logs on       ⚠️ Startup Program(s) ⚠️
 HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run
 
-#  Run  (System)    -  Run a given command the next time CURRENT user logs on
+#  Run  (System)    -  Run a given command the next time CURRENT user logs on   ⚠️ Startup Program(s) ⚠️
 HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run
 
 

e4ffefae5b8987921fa371b17961abc4fc613fc5
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-06 13:00:06 -0500

    Create Linux - smartctl (get disk S.M.A.R.T. values, remaining disk life, wear leveling count).sh

---
+++ linux/Linux - smartctl (get disk S.M.A.R.T. values, remaining disk life, wear leveling count).sh
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------
+# Linux - smartctl (get disk S.M.A.R.T. values, remaining disk life, wear leveling count)
+# ------------------------------------------------------------
+
+# Install smartctl
+sudo apt-get -y update;
+sudo apt-get -y install smartmontools;
+
+# Enable S.M.A.R.T. on target disk
+smartctl --smart=on /dev/sda
+
+
+# Get target disk's S.M.A.R.T. information (-i) and data (-A)
+smartctl -A -i /dev/sda;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   superuser.com  |  "smart - Samsung SSD "Wear_Leveling_Count" meaning - Super User"  |  https://superuser.com/a/1615974
+#
+# ------------------------------------------------------------

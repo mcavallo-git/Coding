573044f7a4e8aec2f3fe3412089b7c83b8c8d08f
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-08 06:35:23 -0500

    Update VMware ESXi - Advanced Options (get, set).sh

--- vmware/VMware ESXi - Advanced Options (get, set).sh
+++ vmware/VMware ESXi - Advanced Options (get, set).sh
@@ -6,12 +6,12 @@
 
 # Get the value of an Advanced Option
 vim-cmd hostsvc/advopt/view "Net.FollowHardwareMac";  # Get Advanced Option using "vim-cmd"
-esxcli system settings advanced list --option "/Net/FollowHardwareMac";  # Get Advanced Option using "esxcli"
+esxcli system settings advanced list --option="/Net/FollowHardwareMac";  # Get Advanced Option using "esxcli"
 
 
 # Set the value of an Advanced Option
 vim-cmd hostsvc/advopt/update "Net.FollowHardwareMac" long "1";
-esxcli system settings advanced set --option "/Net/FollowHardwareMac" --int-value=1;  # Set Advanced Option using "esxcli"
+esxcli system settings advanced set --option="/Net/FollowHardwareMac" --int-value=1;  # Set Advanced Option using "esxcli"
 
 
 # ------------------------------------------------------------

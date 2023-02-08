f140680db9bd7b987748694e6da1503bdab35c0f
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-08 00:21:18 -0500

    Update SQLite - Get all tables.sql

--- mysql/SQLite - Get all tables.sql
+++ mysql/SQLite - Get all tables.sql
@@ -7,7 +7,6 @@ SELECT
   NAME
 FROM
   sqlite_master
-  -- sqlite_schema
 WHERE
   TYPE='table'
   AND NAME NOT LIKE 'sqlite_%'
@@ -19,13 +18,15 @@ ORDER BY
 -- ------------------------------------------------------------
 --
 -- Note: Different versions of SQLite use different schema tables:
---        |--> "Older" versions of SQLite use the "sqlite_master" table
---        |--> "Newer" versions of SQLite use the "sqlite_schema" table
---               |--> (Need to determine when the schema switched)
+--        |--> Table "sqlite_schema" is used by versions since v3.33.0
+--        |--> Table "sqlite_master" is used by versions before v3.33.0
+--              |--> Note that "sqlite_master" is commonly an alias to "sqlite_schema"
 --
 -- ------------------------------------------------------------
 --
 -- Citation(s)
+--
+--   www.sqlite.org  |  "SQLite Forum: Documentation issue (wrong table name) in FAQ, point 7"  |  https://www.sqlite.org/forum/forumpost/d90adfbb0a
 --
 --   www.sqlite.org  |  "The Schema Table"  |  https://www.sqlite.org/schematab.html
 --

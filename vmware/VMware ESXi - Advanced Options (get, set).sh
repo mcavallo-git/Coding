bae922d510e09537402c5f2a3f1580e0f9b8b7cd
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-08 06:31:17 -0500

    Create VMware ESXi - Advanced Options (get, set).sh

---
+++ vmware/VMware ESXi - Advanced Options (get, set).sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# ------------------------------------------------------------
+#
+# VMware ESXi - Advanced Options (get, set)
+#
+
+# Get the value of an Advanced Option
+vim-cmd hostsvc/advopt/view "Net.FollowHardwareMac";  # Get Advanced Option using "vim-cmd"
+esxcli system settings advanced list --option "/Net/FollowHardwareMac";  # Get Advanced Option using "esxcli"
+
+
+# Set the value of an Advanced Option
+vim-cmd hostsvc/advopt/update "Net.FollowHardwareMac" int 1;  # Set Advanced Option using "vim-cmd"
+esxcli system settings advanced set --option "/Net/FollowHardwareMac" --int-value=1;  # Set Advanced Option using "esxcli"
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   sites.google.com  |  "Fix My ScratchConfig Location - MyTechNotesProject"  |  https://sites.google.com/site/mytechnotesproject/vmware/fix-my-scratchconfig-location
+#
+# ------------------------------------------------------------

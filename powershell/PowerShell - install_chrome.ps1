275e35a580aad6d5d49508e533b96658b7d84642
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-22 18:00:54 -0500

    Create PowerShell - install_chrome.ps1

---
+++ powershell/PowerShell - install_chrome.ps1
@@ -0,0 +1,109 @@
+################################################################################
+##  File:  Install-Chrome.ps1
+##  Desc:  Install Google Chrome
+################################################################################
+
+# Download and install latest Chrome browser
+$ChromeInstallerFile = "googlechromestandaloneenterprise64.msi"
+$ChromeInstallerUrl = "https://dl.google.com/tag/s/dl/chrome/install/${ChromeInstallerFile}"
+Install-Binary -Url $ChromeInstallerUrl -Name $ChromeInstallerFile -ArgumentList @()
+
+# Prepare firewall rules
+Write-Host "Adding the firewall rule for Google update blocking..."
+New-NetFirewallRule -DisplayName "BlockGoogleUpdate" -Direction Outbound -Action Block -Program "C:\Program Files (x86)\Google\Update\GoogleUpdate.exe"
+
+$GoogleSvcs = ('gupdate','gupdatem')
+$GoogleSvcs | Stop-SvcWithErrHandling -StopOnError
+$GoogleSvcs | Set-SvcWithErrHandling -Arguments @{StartupType = "Disabled"}
+
+$regGoogleUpdatePath = "HKLM:\SOFTWARE\Policies\Google\Update"
+$regGoogleUpdateChrome = "HKLM:\SOFTWARE\Policies\Google\Chrome"
+($regGoogleUpdatePath, $regGoogleUpdateChrome) | ForEach-Object {
+  New-Item -Path $_ -Force
+}
+
+$regGoogleParameters = @(
+  @{ Name = "AutoUpdateCheckPeriodMinutes"; Value = 00000000},
+  @{ Name = "UpdateDefault"; Value = 00000000 },
+  @{ Name = "DisableAutoUpdateChecksCheckboxValue"; Value = 00000001 },
+  @{ Name = "Update{8A69D345-D564-463C-AFF1-A69D9E530F96}"; Value = 00000000 },
+  @{ Path = $regGoogleUpdateChrome; Name = "DefaultBrowserSettingEnabled"; Value = 00000000 }
+)
+
+$regGoogleParameters | ForEach-Object {
+  $Arguments = $_
+  if (-not ($Arguments.Path))
+  {
+    $Arguments.Add("Path", $regGoogleUpdatePath)
+  }
+  $Arguments.Add("Force", $true)
+  New-ItemProperty @Arguments
+}
+
+# Install Chrome WebDriver
+Write-Host "Install Chrome WebDriver..."
+$ChromeDriverPath = "$($env:SystemDrive)\SeleniumWebDrivers\ChromeDriver"
+if (-not (Test-Path -Path $ChromeDriverPath))
+{
+  New-Item -Path $ChromeDriverPath -ItemType Directory -Force
+}
+
+Write-Host "Get the Chrome WebDriver version..."
+$RegistryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths"
+$ChromePath = (Get-ItemProperty "$RegistryPath\chrome.exe").'(default)'
+[version]$ChromeVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($ChromePath).ProductVersion
+$ChromeDriverVersionUrl = "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$($ChromeVersion.Major).$($ChromeVersion.Minor).$($ChromeVersion.Build)"
+
+$ChromeDriverVersionFile = Start-DownloadWithRetry -Url $ChromeDriverVersionUrl -Name "versioninfo.txt" -DownloadPath $ChromeDriverPath
+
+Write-Host "Download Chrome WebDriver..."
+$ChromeDriverVersion = Get-Content -Path $ChromeDriverVersionFile
+$ChromeDriverArchName = "chromedriver_win32.zip"
+$ChromeDriverZipDownloadUrl = "https://chromedriver.storage.googleapis.com/${ChromeDriverVersion}/${ChromeDriverArchName}"
+
+$ChromeDriverArchPath = Start-DownloadWithRetry -Url $ChromeDriverZipDownloadUrl -Name $ChromeDriverArchName
+
+Write-Host "Expand Chrome WebDriver archive..."
+Extract-7Zip -Path $ChromeDriverArchPath -DestinationPath $ChromeDriverPath
+
+Write-Host "Setting the environment variables..."
+setx ChromeWebDriver "$ChromeDriverPath" /M
+
+$regEnvKey = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\'
+$PathValue = Get-ItemPropertyValue -Path $regEnvKey -Name 'Path'
+$PathValue += ";$ChromeDriverPath\"
+Set-ItemProperty -Path $regEnvKey -Name 'Path' -Value $PathValue
+
+Invoke-PesterTests -TestFile "Browsers" -TestName "Chrome"
+
+$ChromeExePath=((Get-ItemProperty "${RegistryPath}\chrome.exe")."(default)");
+$ChromeDirname=((Get-ItemProperty "${RegistryPath}\chrome.exe")."Path");
+
+
+If ((($False -Eq [String]::IsNullOrEmpty("${SYSTEM_TASKINSTANCENAME}")) -And ($False -Eq [String]::IsNullOrEmpty("${SYSTEM_COLLECTIONURI}")) -And ("${SYSTEM_COLLECTIONURI}" -like "https://dev.azure.com*")) -Or (($False -Eq [String]::IsNullOrEmpty("${SYSTEM_TASKINSTANCENAME}")) -And ($False -Eq [String]::IsNullOrEmpty("${SYSTEM_TASKDEFINITIONSURI}")) -And ("${SYSTEM_TASKDEFINITIONSURI}" -like "https://dev.azure.com*"))) {
+  #
+  # Detect Azure Pipeline Environments
+  #
+
+  Write-Host "##[debug]RegistryPath =[ ${RegistryPath} ]";
+  Write-Host "##[debug]ChromeExePath=[ ${ChromeExePath} ]";
+
+  Write-Host "##[debug]Info:  Setting variable ChromeWebDriver=[ ${ChromeDriverPath} ]";
+  Write-Host "##vso[task.setvariable variable=ChromeWebDriver;]${ChromeDriverPath}";
+
+  Write-Host "##[debug]Info:  Prepending to PATH (env var):  [ ${ChromeDirname} ]";
+  Write-Host "##vso[task.prependpath]${ChromeDirname}";
+
+  Write-Host "Info:  Prepending to PATH (env var):  [ ${ChromeDriverPath} ]";
+  Write-Host "##vso[task.prependpath]${ChromeDriverPath}";
+
+}
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   github.com  |  "runner-images/Install-Chrome.ps1 at main · actions/runner-images · GitHub"  |  https://github.com/actions/runner-images/blob/main/images/win/scripts/Installers/Install-Chrome.ps1
+#
+# ------------------------------------------------------------

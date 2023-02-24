f1e83ed05bd229ae6a0fc12469a08590fee63779
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-24 16:41:21 -0500

    Update VMware ESXi - scp push (sending a file from local instance to remote host).sh

--- vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
+++ vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
@@ -5,6 +5,6 @@
 scp -o StrictHostKeyChecking=no -P 22 "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
 scp -o StrictHostKeyChecking=no -P 22 -r "/vmfs/volumes/datastore/VM DIR/" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\";
 
-### WITH Rate-Limiting
+### WITH Rate-Limiting  - Note that 102,400 Kbps (Kilobit/s) ~= 100 Mbps  (Megabit/s) ~= 12.5 MBps (Megabyte/s)
 scp -o StrictHostKeyChecking=no -l 102400 -P 22 "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
 scp -o StrictHostKeyChecking=no -l 102400 -P 22 -r "/vmfs/volumes/datastore/VM DIR/" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\";

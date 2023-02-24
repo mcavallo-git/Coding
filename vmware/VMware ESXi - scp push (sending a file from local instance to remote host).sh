0570172c0cda877e5221f7e00e372de8f80e8ddc
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-24 16:51:40 -0500

    Update VMware ESXi - scp push (sending a file from local instance to remote host).sh

--- vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
+++ vmware/VMware ESXi - scp push (sending a file from local instance to remote host).sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+# ------------------------------------------------------------
+
+# Enable ESXi's SSH client (e.g. enable outgoing SSH connections)
+esxcli network firewall ruleset set --ruleset-id sshClient --enabled=true;
 
 
-### NO Rate-Limiting
+# NO Rate-Limiting
 scp -o StrictHostKeyChecking=no -P 22 "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
 scp -o StrictHostKeyChecking=no -P 22 -r "/vmfs/volumes/datastore/VM DIR/" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\";
 
-### WITH Rate-Limiting  - Note that 102,400 Kbps (Kilobit/s) ~= 100 Mbps  (Megabit/s) ~= 12.5 MBps (Megabyte/s)
+
+# WITH Rate-Limiting  - Note that 102,400 Kbps (Kilobit/s) ~= 100 Mbps  (Megabit/s) ~= 12.5 MBps (Megabyte/s)
 scp -o StrictHostKeyChecking=no -l 102400 -P 22 "/vmfs/volumes/datastore/VM DIR/VM FILE" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\VM\\ FILE";
 scp -o StrictHostKeyChecking=no -l 102400 -P 22 -r "/vmfs/volumes/datastore/VM DIR/" USERNAME@HOSTNAME:"/vmfs/volumes/datastore/VM\\ DIR\\";
+
+# ------------------------------------------------------------

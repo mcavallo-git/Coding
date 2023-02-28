1a3fdffa25421b54b8831166bc40c29031e0a859
Matt Cavallo <mcavallo.git@gmail.com>
2023-02-28 03:57:26 -0500

    Update VMware ESXi - Datastore Space-Reclamation (Recalculate disk usage, vmfs6 recalculation, reclaim, consolidation).sh

--- vmware/VMware ESXi - Datastore Space-Reclamation (Recalculate disk usage, vmfs6 recalculation, reclaim, consolidation).sh
+++ vmware/VMware ESXi - Datastore Space-Reclamation (Recalculate disk usage, vmfs6 recalculation, reclaim, consolidation).sh
@@ -1,27 +1,35 @@
 #!/bin/sh
-#
 # ------------------------------------------------------------
 
-### Check datastore reclamation settings
+# List Datastores
+esxcli storage filesystem list | grep -v '^\(-----\|Mount\)' | grep -v '\s\s\(BOOTBANK\|LOCKER\)';
+
+# Count Datastores
+DATASTORE_COUNT="$(esxcli storage filesystem list | grep -v '^\(-----\|Mount\)' | grep -v '\s\s\(BOOTBANK\|LOCKER\)' | wc -l;)"; echo -e "\n""DATASTORE_COUNT = [ ${DATASTORE_COUNT} ]""\n";
+
+
+# ------------------------------------------------------------
+
+# Check datastore reclamation settings
 esxcli storage vmfs reclaim config get -l=VMFS_LABEL;
 esxcli storage vmfs reclaim config get -u=VMFS_UUID;
 
-### Manually reclaim all space on datastore
+# Manually reclaim all space on datastore
 esxcli storage vmfs unmap -l=VMFS_LABEL -n=RECLAIM_RATE
 
-### Run the same job as above as a background job
+# Run the same job as above as a background job
 nohup esxcli storage vmfs unmap -l=VMFS_LABEL > "/tmp/vmfs_unmap_$(date +'%Y%m%d%H%M%S').log" 2>&1 &
 
 
 # ------------------------------------------------------------
 
-### Manually reclaim all space on datastore named "datastore2"
+# Manually reclaim all space on datastore named "datastore2"
 esxcli storage vmfs unmap -l=datastore2
 
-### Run the same job as above w/o the units declaration and as a background job
+# Run the same job as above w/o the units declaration and as a background job
 nohup esxcli storage vmfs unmap -l=datastore2 > "/tmp/vmfs_unmap_$(date +'%Y%m%d%H%M%S').log" 2>&1 &
 
-### Watch the background job's log
+# Watch the background job's log
 while [ 1 ]; do clear; date; echo -e "\n\n"; df -h; echo -e "\n\n"; cat /tmp/vmfs_unmap_$(date +'%Y%m%d')*.log; sleep 2; done;
 
 

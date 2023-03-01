0cfaa29f4232ed3191211a9c0fa5317c95c69630
Matt Cavallo <mcavallo.git@gmail.com>
2023-03-01 09:48:30 -0500

    Update PowerShell - Get version number.ps1

--- powershell/PowerShell - Get version number.ps1
+++ powershell/PowerShell - Get version number.ps1
@@ -10,14 +10,16 @@
 pwsh --version;
 
 
-# PowerShell has no simple way to get its version (especially not from external sources, sadly)
+# Native PowerShell (v5.1 and lower) has no simple way to get its version (especially not from external sources, sadly)
 PowerShell -Command 'Write-Output ((Write-Output PowerShell)+([String][Char]32)+(((GV PSVersionTable).Value).PSVersion.Major)+(Write-Output .)+(((GV PSVersionTable).Value).PSVersion.Minor))';
+
+PowerShell -Command '(gv PSVersionTable).Value';
 
 
 # ------------------------------------------------------------
 #
 # Get the version of PowerShell (for the current/running PowerShell session)
-
+#
 
 #
 #  Format:   Major.Minor.Build (PowerShell)  /  Major.Minor.Patch  (pwsh)
@@ -31,4 +33,19 @@ $Version="";If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Major)){
 $Version="";If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Major)){$Version+=(${PSVersionTable}.PSVersion.Major);If(-Not [String]::IsNullOrEmpty(${PSVersionTable}.PSVersion.Minor)){$Version+="."; $Version+=(${PSVersionTable}.PSVersion.Minor);};};Write-Output "PowerShell ${Version}"; <# Get the version of PowerShell with the format [ Major.Minor ] #>
 
 
+# ------------------------------------------------------------
+
+# Open a PowerShell v2 terminal --> Requires Windows Optional Feature ".NET Framework 3.5 (includes .NET 2.0 and 3.0)"
+
+PowerShell.exe -Version 2
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   learn.microsoft.com  |  "Using the Windows PowerShell 2.0 Engine - PowerShell | Microsoft Learn"  |  https://learn.microsoft.com/en-us/powershell/scripting/windows-powershell/starting-the-windows-powershell-2.0-engine?view=powershell-7.3
+#
+#   4sysops.com  |  "Differences between PowerShell versions – 4sysops"  |  https://4sysops.com/wiki/differences-between-powershell-versions/
+#
 # ------------------------------------------------------------

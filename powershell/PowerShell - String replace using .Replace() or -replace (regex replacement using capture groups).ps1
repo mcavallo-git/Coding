5753a6044a9b8f737f72710649e276f9b1cb8d40
Matt Cavallo <mcavallo.git@gmail.com>
2023-03-16 18:19:47 -0400

    PowerShell - String replace using .Replace() or -replace (regex replacement using capture groups)

--- powershell/PowerShell - String replace using .Replace() or -replace (regex replacement using capture groups).ps1
+++ powershell/PowerShell - String replace using .Replace() or -replace (regex replacement using capture groups).ps1
@@ -1,20 +1,20 @@
 # ------------------------------------------------------------
 #
-# PowerShell
-# 	|
-# 	|--> String replace via .Replace()   !!!  CASE SENSITIVE  !!!
-# 	|
-# 	|--> String replace via -Replace     !!!  CASE IN-SENSITIVE  !!!  (uses regex-matching)
+# PowerShell - String replace using .Replace() or -replace (regex replacement using capture groups)
+#   |
+#   |--> .Replace()   !!!  CASE SENSITIVE     !!!
+#   |
+#   |--> -replace     !!!  CASE IN-SENSITIVE  !!!  (regex-matching)
 #
 # ------------------------------------------------------------
 
-"Hello City!".Replace("City","World");  <# Returns "Hello World!" #>
+"Hello World!".Replace("World","City");  <# Returns "Hello World!" #>
 
-"Hello City!".Replace("city","World");  <# Returns "Hello City!" - CASE SENsitive #>
+"Hello World!".Replace("world","City");  <# Returns "Hello World!" - CASE SENsitive #>
 
-"Hello City!" -Replace "city","World";  <# Returns "Hello World!" - CASE INsensitive #>
+"Hello World!" -replace "world","City";  <# Returns "Hello City!" - CASE INsensitive #>
 
-"..*[$#(In!#<$>#va!#lid + @!# Char# !>>< act ers)<<].." -Replace "[^a-zA-Z0-9-_%\[\]\(\)\+\.]","";  <# Returns "..[(Invalid+Characters)].."  #>
+"..*[$#(In!#<$>#va!#lid + @!# Char# !>>< act ers)<<].." -replace "[^a-zA-Z0-9-_%\[\]\(\)\+\.]","";  <# Returns "..[(Invalid+Characters)].."  #>
 
 
 # Regex replacements using capture groups
@@ -26,6 +26,17 @@
 '+13:45:00' -replace "^([-+]?)(\d+):(\d+):(\d+)$","[`$1] [`$2] [`$3] [`$4]";  <# Returns "[+] [13] [45] [00]" #>
 
 $TZ_MinutesOffset=$(([String](Get-TimeZone).BaseUtcOffset) -replace "^([-+]?)(\d+):(\d+):(\d+)$",":`$3"); $TZ_MinutesOffset;  <# Returns ":00" (minutes offset for current system's timezone) #>
+
+
+# ------------------------------
+#
+# Example - Perform regex replacements to remove a substring from a string
+#
+
+$String="Hello World!";
+$SliceSubstring="World";
+${String} -replace "${SliceSubstring}";
+
 
 
 # ------------------------------
@@ -65,5 +76,7 @@ $TZ_MinutesOffset=$(([String](Get-TimeZone).BaseUtcOffset) -replace "^([-+]?)(\d
 #   docs.microsoft.com  |  "about_Comparison_Operators - PowerShell | Microsoft Docs"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_comparison_operators?view=powershell-5.1#replacement-operator
 #
 #   powershell.org  |  "Regular Expressions are a -replace's best friend – PowerShell.org"  |  https://powershell.org/2013/08/regular-expressions-are-a-replaces-best-friend/
+#
+#   stackoverflow.com  |  "PowerShell to remove text from a string - Stack Overflow"  |  https://stackoverflow.com/a/19169526
 #
 # ------------------------------------------------------------

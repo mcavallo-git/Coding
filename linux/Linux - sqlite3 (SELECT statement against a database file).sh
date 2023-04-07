8c8334adee5692ad4d87756039f31000cf9335b9
Matt Cavallo <mcavallo.git@gmail.com>
2023-04-07 10:26:32 -0400

    Update Linux - sqlite3 (SELECT statement against a database file).sh

--- linux/Linux - sqlite3 (SELECT statement against a database file).sh
+++ linux/Linux - sqlite3 (SELECT statement against a database file).sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# NOTE: The following commands are for managing a FreeNAS MySQL database which exists at path "/data/freenas-v1.db" (Active Directory management)
+# NOTE: The following commands are for managing a FreeNAS MySQL database which exists at path "/data/freenas-v1.db"
 #       To target a different db, change the "/data/freenas-v1.db" string to match the path to your respective database
 
-# FreeNAS - Show all table names
+# SQLite3 - Show all table names
 sqlite3 "/data/freenas-v1.db" "SELECT name from sqlite_master where type='table';";
 
-# FreeNAS - Show all columns in a given table
+# SQLite3 - Show all columns in a given table (FreeNAS Active Directory Management)
 sqlite3 "/data/freenas-v1.db" "PRAGMA table_info(directoryservice_activedirectory);";
 
-# FreeNAS - Show values defined in current Active Directory settings
+# SQLite3 - Show all rows in a given table (FreeNAS Active Directory Management)
 sqlite3 "/data/freenas-v1.db" "SELECT * from directoryservice_activedirectory;";
 
 

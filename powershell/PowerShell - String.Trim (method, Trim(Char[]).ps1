b4577a3dc4d873f1f13ecb1a9d47980a5423b741
Matt Cavallo <mcavallo.git@gmail.com>
2023-05-11 11:26:52 -0400

    Update PowerShell - String.Trim (method, Trim(Char[]).ps1

--- powershell/PowerShell - String.Trim (method, Trim(Char[]).ps1
+++ powershell/PowerShell - String.Trim (method, Trim(Char[]).ps1
@@ -2,17 +2,34 @@
 #	PowerShell - String.Trim()
 # ------------------------------
 
+#
+# String.Trim()  -  Spaces
+#
+
 Write-Output "   123   ";  # Returns "   123   "
 
 Write-Output "   123   ".Trim();  # Returns "123"
 
 Write-Output "   123   ".Trim(" ");  # Returns "123"
 
+
+#
+# String.Trim()  -  Specific Character(s)
+#
+
 Write-Output "   123   ".Trim(" ","1");  # Returns "23"
 
 Write-Output "   123   ".Trim(" ","1","3");  # Returns "2"
 
 Write-Output "   123   ".Trim(" ","1","2");  # Returns "3"
+
+
+#
+# Note:  String.Trim() will ONLY strip characters that are found on the left and/or right sides of the string
+#         |--> e.g. Trim will NOT trim chars in the middle of a string (unless all chars to the left or right are also trimmed)
+#
+
+Write-Output "   123   ".Trim("1","2","3");  # Returns "   123   "
 
 
 # ------------------------------------------------------------

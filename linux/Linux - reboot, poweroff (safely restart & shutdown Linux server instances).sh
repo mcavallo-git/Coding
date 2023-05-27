f29e84b541d3000dca95a814d0e88e2ac600dcdc
Matt Cavallo <mcavallo.git@gmail.com>
2023-05-27 10:49:35 -0400

    Create Linux - reboot, poweroff (safely restart & shutdown Linux server instances).sh

---
+++ linux/Linux - reboot, poweroff (safely restart & shutdown Linux server instances).sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+exit 1;
+# ------------------------------------------------------------
+# Linux - reboot, poweroff (safely restart & shutdown Linux server instances)
+# ------------------------------------------------------------
+
+# Reboot  (safely)
+reboot;
+
+# Shutdown  (safely)
+poweroff;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   linuxhandbook.com  |  "Restart Ubuntu Server With These 3 Commands"  |  https://linuxhandbook.com/restart-ubuntu-server/
+#
+#   man7.org  |  "reboot(2) - Linux manual page"  |  https://man7.org/linux/man-pages/man2/reboot.2.html
+#
+#   askubuntu.com  |  "command line - What is the proper terminal-way to shutdown? - Ask Ubuntu"  |  https://askubuntu.com/a/73698
+#
+# ------------------------------------------------------------

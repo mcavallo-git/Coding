528943f58029ada44a3d4abec0a90ab8f3e55c7e
Matt Cavallo <mcavallo.git@gmail.com>
2023-06-04 21:18:08 -0400

    Update PowerShell - Regex (-Match - regex-matching capture groups).ps1

--- powershell/PowerShell - Regex (-Match - regex-matching capture groups).ps1
+++ powershell/PowerShell - Regex (-Match - regex-matching capture groups).ps1
@@ -9,7 +9,7 @@
 #
 [Regex]::Match('Haystack','Pattern');
 
- 
+
 #
 # General Syntax - NO capture groups
 #
@@ -46,13 +46,13 @@ If ($True) {
 
   $Haystack = 'hello world'; # "Haystack", aka the string to parse (may have newlines aplenty)
   $Pattern  = '^(hello)\s(world)$'; # Regex pattern which defines the "Needle" to match while parsing the through the "Haystack"
-  $Needle   = [Regex]::Match($Haystack, $Pattern); # Parse through the "Haystack", looking for the "Needle"
-  If ($Needle.Success -ne $False) {
-    Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[0].Value)"; # 'hello world'   (regex capture group 0, e.g. whole string)
-    Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[1].Value)"; # 'hello'  (regex capture group 1)
-    Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[2].Value)"; # 'world'  (regex capture group 2)
+  $Needle   = [Regex]::Match(${Haystack}, ${Pattern}); # Parse through the "Haystack", looking for the "Needle"
+  If (${Needle}.Success -ne $False) {
+    Write-Output "`n`${Needle}.Groups[0].Value: $(${Needle}.Groups[0].Value)"; # 'hello world'   (regex capture group 0, e.g. whole string)
+    Write-Output "`n`${Needle}.Groups[0].Value: $(${Needle}.Groups[1].Value)"; # 'hello'  (regex capture group 1)
+    Write-Output "`n`${Needle}.Groups[0].Value: $(${Needle}.Groups[2].Value)"; # 'world'  (regex capture group 2)
   }
-  Write-Output ("`$Needle.Success = [ $($Needle.Success) ]");
+  Write-Output ("`${Needle}.Success = [ $(${Needle}.Success) ]");
 
 }
 
@@ -62,31 +62,41 @@ If ($True) {
 # Ex 2)  Obtain the version of local Azure-CLI Module
 #
 
-$Haystack = (az --version);
+If ($True) {
 
-$RegexPattern = '^azure\-cli\s*\(?(\d+)\.(\d+)\.(\d+)\)?\s*\*?$';
+  $Haystack = (az --version);
 
-ForEach ($EachLine in $Haystack){
-  $Needle = [Regex]::Match($EachLine, $RegexPattern);
-  If ($Needle.Success -eq $True) {
-    Break;
+  $RegexPattern = '^azure\-cli\s*\(?(\d+)\.(\d+)\.(\d+)\)?\s*\*?$';
+
+  ForEach ($EachLine in ${Haystack}){
+    $Needle = [Regex]::Match(${EachLine}, ${RegexPattern});
+    If (${Needle}.Success -eq $True) {
+      Break;
+    }
   }
-}
 
-If ($Needle.Success -eq $True) {
-  Write-Host (("Found local install: `"")+($Needle.Groups[1].Value) + ("`" v") + ($Needle.Groups[2].Value));
-} Else {
-  Write-Host ("No matches found");
+  If (${Needle}.Success -eq $True) {
+    Write-Host (("Found local install: `"")+(${Needle}.Groups[1].Value) + ("`" v") + (${Needle}.Groups[2].Value));
+  } Else {
+    Write-Host ("No matches found");
+  }
+
 }
 
 
 # ------------------------------------------------------------
+#
+# Ex 3) String & Integer regex-matching
+#
 
-# Ex ) String & Integer regex-matching
-$Distro_Name = "CanonicalGroupLimited.Ubuntu18.04onWindows_79rhkp1fndgsc";
-$RegexTest = "CanonicalGroupLimited\.(Ubuntu)[\d\.]{0,6}onWindows_[a-z0-9]{13}";
-$MatchResults = ($Distro_Basename -match $RegexTest);
-$MatchResults;
+If ($True) {
+  
+  $Distro_Name = "CanonicalGroupLimited.Ubuntu18.04onWindows_79rhkp1fndgsc";
+  $RegexTest = "CanonicalGroupLimited\.(Ubuntu)[\d\.]{0,6}onWindows_[a-z0-9]{13}";
+  $MatchResults = (${Distro_Basename} -match ${RegexTest});
+  $MatchResults;
+
+}
 
 
 # ------------------------------------------------------------

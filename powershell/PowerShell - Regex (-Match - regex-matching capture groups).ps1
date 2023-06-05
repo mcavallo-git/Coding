44e57562a36a96a2e98709e48aebd2b1b29b09c8
Matt Cavallo <mcavallo.git@gmail.com>
2023-06-04 21:12:19 -0400

    Update PowerShell - Regex (-Match - regex-matching capture groups).ps1

--- powershell/PowerShell - Regex (-Match - regex-matching capture groups).ps1
+++ powershell/PowerShell - Regex (-Match - regex-matching capture groups).ps1
@@ -9,7 +9,7 @@
 #
 [Regex]::Match('Haystack','Pattern');
 
-
+ 
 #
 # General Syntax - NO capture groups
 #
@@ -32,9 +32,9 @@
 #  Regex Patterns
 #
 
-$Pattern_UUID='^\b[0-9A-Fa-f]{8}\b-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-\b[0-9A-Fa-f]{12}\b$';  # Regex Patterns:  Microsoft UUIDs
+$Pattern_UUID='^\b[0-9A-Fa-f]{8}\b-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-\b[0-9A-Fa-f]{12}\b$';  # Regex Patterns:  Microsoft UUIDs/GUIDs
 
-$Pattern_UUID='^{[0-9A-Fa-f]{8}\b-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-\b[0-9A-Fa-f]{12}}$';  # Regex Patterns:  Microsoft UUIDs
+$Pattern_UUID='^{[0-9A-Fa-f]{8}\b-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-\b[0-9A-Fa-f]{12}}$';  # Regex Patterns:  Microsoft UUIDs/GUIDs
 
 
 # ------------------------------------------------------------
@@ -42,15 +42,19 @@ $Pattern_UUID='^{[0-9A-Fa-f]{8}\b-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-\
 # Ex 1)  Hello World
 #
 
-$Haystack = 'hello world'; # "Haystack", aka the string to parse (may have newlines aplenty)
-$Pattern  = '^(hello)\s(world)$'; # Regex pattern which defines the "Needle" to match while parsing the through the "Haystack"
-$Needle   = [Regex]::Match($Haystack, $Pattern); # Parse through the "Haystack", looking for the "Needle"
-If ($Needle.Success -ne $False) {
-	Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[0].Value)"; # 'hello world'   (regex capture group 0, e.g. whole string)
-	Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[1].Value)"; # 'hello'  (regex capture group 1)
-	Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[2].Value)"; # 'world'  (regex capture group 2)
+If ($True) {
+
+  $Haystack = 'hello world'; # "Haystack", aka the string to parse (may have newlines aplenty)
+  $Pattern  = '^(hello)\s(world)$'; # Regex pattern which defines the "Needle" to match while parsing the through the "Haystack"
+  $Needle   = [Regex]::Match($Haystack, $Pattern); # Parse through the "Haystack", looking for the "Needle"
+  If ($Needle.Success -ne $False) {
+    Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[0].Value)"; # 'hello world'   (regex capture group 0, e.g. whole string)
+    Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[1].Value)"; # 'hello'  (regex capture group 1)
+    Write-Output "`n`$Needle.Groups[0].Value: $($Needle.Groups[2].Value)"; # 'world'  (regex capture group 2)
+  }
+  Write-Output ("`$Needle.Success = [ $($Needle.Success) ]");
+
 }
-Write-Output ("`$Needle.Success = [ $($Needle.Success) ]");
 
 
 # ------------------------------------------------------------
@@ -63,16 +67,16 @@ $Haystack = (az --version);
 $RegexPattern = '^azure\-cli\s*\(?(\d+)\.(\d+)\.(\d+)\)?\s*\*?$';
 
 ForEach ($EachLine in $Haystack){
-	$Needle = [Regex]::Match($EachLine, $RegexPattern);
-	If ($Needle.Success -eq $True) {
-		Break;
-	}
+  $Needle = [Regex]::Match($EachLine, $RegexPattern);
+  If ($Needle.Success -eq $True) {
+    Break;
+  }
 }
 
 If ($Needle.Success -eq $True) {
-	Write-Host (("Found local install: `"")+($Needle.Groups[1].Value) + ("`" v") + ($Needle.Groups[2].Value));
+  Write-Host (("Found local install: `"")+($Needle.Groups[1].Value) + ("`" v") + ($Needle.Groups[2].Value));
 } Else {
-	Write-Host ("No matches found");
+  Write-Host ("No matches found");
 }
 
 

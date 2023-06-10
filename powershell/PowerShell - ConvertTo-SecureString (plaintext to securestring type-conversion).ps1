26d0a9184126c7d88b19edde02118d087d7d6cb5
Matt Cavallo <mcavallo.git@gmail.com>
2023-06-10 01:12:50 -0400

    Update PowerShell - ConvertTo-SecureString (plaintext to securestring type-conversion).ps1

--- powershell/PowerShell - ConvertTo-SecureString (plaintext to securestring type-conversion).ps1
+++ powershell/PowerShell - ConvertTo-SecureString (plaintext to securestring type-conversion).ps1
@@ -15,6 +15,15 @@ Write-Output "`n`nInitial PlainText string: `"${SecureString_ToPlainText}`"`n`n"
 
 # ------------------------------------------------------------
 #
+# ConvertFrom-SecureString
+#
+
+# Example (read user input as secure input (each star typed shows as an asterisk "*" on screen), then convert to back plaintext once entered
+Read-Host -AsSecureString -Prompt 'Enter value to show as plaintext' | ConvertFrom-SecureString -AsPlainText;
+
+
+# ------------------------------------------------------------
+#
 # Citation(s)
 #
 #   docs.microsoft.com  |  "ConvertTo-SecureString - Converts encrypted standard strings to secure strings. It can also convert plain text to secure strings. It is used with ConvertFrom-SecureString and Read-Host"  |  https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.security/convertto-securestring?view=powershell-5.1

b34b2265c268b61a91ac6f71479736f4a10de94c
Matt Cavallo <mcavallo.git@gmail.com>
2023-06-30 17:08:49 -0400

    Update Linux - timezone (get, set local device time zone).sh

--- linux/Linux - timezone (get, set local device time zone).sh
+++ linux/Linux - timezone (get, set local device time zone).sh
@@ -1,41 +1,42 @@
 #!/bin/sh
 # ------------------------------------------------------------
-# Linux - timezone (set localtime, set timezone)
+# Linux - timezone (get, set time zone, localtime, tz, timedatectl)
 # ------------------------------------------------------------
 #
-# Get Time Zone via "/etc/localtime" (symbolic link)
+# Get time zone via "/etc/localtime" (symbolic link)
 #
 
-# Get Time Zone for current device
+# Get time zone for current device
 realpath '/etc/localtime' | sed -e 's|/usr/share/zoneinfo/||g';
 
-# Get Time Zone for current device (as a variable)
+# Get time zone for current device (as a variable)
 GET_TZ="$(realpath '/etc/localtime' | sed -e 's|/usr/share/zoneinfo/||g';)";
 echo "GET_TZ = \"${GET_TZ}\"";
 
 
 # ------------------------------------------------------------
 #
-# Get Time Zone via  "/etc/timezone"
+# Get time zone via  "/etc/timezone"
 #
 
-# Get Time Zone for current device
+# Get time zone for current device
 cat '/etc/timezone';
 
-# Get Time Zone for current device (as a variable) - Strip whitespace & blank lines
+# Get time zone for current device (as a variable) - Strip whitespace & blank lines
 GET_TZ="$(cat '/etc/timezone' | sed -e "s/^[[:space:]]*//" -e "s/[[:space:]]*$//" -e "/^\s*$/d";)";
 echo "GET_TZ = \"${GET_TZ}\"";
 
 
 # ------------------------------------------------------------
 #
-# Set Time Zone for current device
+# Set time zone for target device
+#
 
 if [[ 1 -eq 1 ]]; then
   # Debian/RHEL Linux distros
   SET_TZ="America/New_York";
   ln -snf "/usr/share/zoneinfo/${SET_TZ}" "/etc/localtime";
-  echo -n "${SET_TZ}"; > "/etc/timezone";
+  echo -n "${SET_TZ}" > "/etc/timezone";
   timedatectl set-timezone "${SET_TZ}";
 fi;
 

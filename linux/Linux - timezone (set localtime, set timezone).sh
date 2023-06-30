3c6de11bdea9c484ffe20d8e54fc8d38cb959957
Matt Cavallo <mcavallo.git@gmail.com>
2023-06-30 16:48:54 -0400

    Update Linux - timezone (set localtime, set timezone).sh

--- linux/Linux - timezone (set localtime, set timezone).sh
+++ linux/Linux - timezone (set localtime, set timezone).sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+# ------------------------------------------------------------
+# Linux - timezone (set localtime, set timezone)
 # ------------------------------------------------------------
 #
 # Get Time Zone via  "/etc/timezone"
@@ -24,21 +27,29 @@ TZ="$(realpath '/etc/localtime' | sed -e 's|/usr/share/zoneinfo/||g';)";
 
 # ------------------------------------------------------------
 #
-# Set Time Zone ("/etc/localtime" && "/etc/timezone") for current device:
+# Set Time Zone for current device
 
 if [[ 1 -eq 1 ]]; then
-  ## Debian/RHEL Linux distros
+  # Debian/RHEL Linux distros
   SET_TZ="America/New_York";
   ln -snf "/usr/share/zoneinfo/${SET_TZ}" "/etc/localtime";
   echo -n "${SET_TZ}"; > "/etc/timezone";
   timedatectl set-timezone "${SET_TZ}";
 fi;
 
-## Alpine Linux distros
+
 if [[ 0 -eq 1 ]]; then
-SET_TZ="UTC+4";
-ln -snf "/usr/share/zoneinfo/${SET_TZ}" "/etc/localtime";
-echo "${SET_TZ}" > "/etc/TZ";
+  # Alpine Linux distros
+  SET_TZ="UTC+4";
+  ln -snf "/usr/share/zoneinfo/${SET_TZ}" "/etc/localtime";
+  echo "${SET_TZ}" > "/etc/TZ";
 fi;
 
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.tecmint.com  |  "How to Check Timezone in Linux"  |  https://www.tecmint.com/check-linux-timezone/
+#
 # ------------------------------------------------------------

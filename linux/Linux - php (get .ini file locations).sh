8919782251f5d7a63d7a2dd810515e4e0964f68e
Matt Cavallo <mcavallo.git@gmail.com>
2023-06-30 15:45:30 -0400

    Create Linux - php (get .ini file locations).sh

---
+++ linux/Linux - php (get .ini file locations).sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# ------------------------------------------------------------
+# Linux - php (get .ini file locations)
+# ------------------------------------------------------------
+
+if [[ 1 -eq 1 ]]; then
+
+  # php --ini
+
+  FULLPATH_PHP_INI="$(php --ini | sed -rne "s/^Loaded Configuration File:\s*(.+)\s*$/\1/p";)";
+  FULLPATH_PHP_CONF_D="$(php --ini | sed -rne "s/^Scan for additional .ini files in:\s*(.+)\s*$/\1/p";)";
+
+  echo "";
+  echo "FULLPATH_PHP_INI:     ${FULLPATH_PHP_INI}";
+  echo "";
+  echo "FULLPATH_PHP_CONF_D:  ${FULLPATH_PHP_CONF_D}";
+  echo "";
+
+fi;
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   stackoverflow.com  |  "linux - Where can I find php.ini? - Stack Overflow"  |  https://stackoverflow.com/a/8684638
+#
+#   www.php.net  |  "PHP: ini_set - Manual"  |  https://www.php.net/manual/en/function.ini-set
+#
+# ------------------------------------------------------------

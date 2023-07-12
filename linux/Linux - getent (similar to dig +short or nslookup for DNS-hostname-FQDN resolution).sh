3cfe2e835eab377f1c4fd38cff6c0a8346a6be8f
Matt Cavallo <mcavallo.git@gmail.com>
2023-07-12 11:50:13 -0400

    Update Linux - getent (similar to dig +short or nslookup for DNS-hostname-FQDN resolution).sh

--- linux/Linux - getent (similar to dig +short or nslookup for DNS-hostname-FQDN resolution).sh
+++ linux/Linux - getent (similar to dig +short or nslookup for DNS-hostname-FQDN resolution).sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 # ------------------------------------------------------------
+#
+# Get info on users
+#
 
-getent ahosts "www.google.com" | head -n 1 | cut -d' ' -f1;  # Gets DNS A Record
+getent passwd;  # Get info on all users
 
-getent group "www-data";  # Gets the users in a group
+getent passwd "${USER}" "www-data";  # Get info on multiple users
+
+getent passwd "${USER}";  # Get info on single user
+
+
+# ------------------------------------------------------------
+#
+# Get info on [user] groups
+#
+
+getent group;  # Get info on all users
+
+getent group "${USER}" "www-data";  # Get info on multiple users
+
+getent group "${USER}";  # Get info on single user
+
+
+# ------------------------------------------------------------
+#
+# Get info on network hosts
+#
+
+getent ahosts;  # Get info on all network hosts
+
+getent ahosts "example.com" "google.com";  # Get info on multiple network hosts
+
+getent ahosts "example.com";  # Get info on single network host
 
 
 # ------------------------------------------------------------

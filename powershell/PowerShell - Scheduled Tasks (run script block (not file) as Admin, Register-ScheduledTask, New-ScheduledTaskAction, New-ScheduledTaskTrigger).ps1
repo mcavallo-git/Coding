a86e59608e3e256daaa12f1d2bfd132a413817bc
Matt Cavallo <mcavallo.git@gmail.com>
2023-08-01 10:40:55 -0400

    Update PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger).ps1

--- powershell/PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger).ps1
+++ powershell/PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger).ps1
@@ -21,6 +21,18 @@ $Args_SchedTask=@{ TaskName="QuickNoteSniper"; Description="QuickNoteSniper"; Tr
 
 # ------------------------------------------------------------
 #
+# SetDeviceProperties_DisableUsbPciePowerSaver  (Runs as ADMIN)
+#  |
+#  |--> Manually set trigger: At system startup
+#  |--> Manually set trigger: At log on of any user (delay task for 1 minute)
+#  |--> Manually set trigger: On workstation unlock of any user (delay task for 1 minute)
+#
+
+$Args_SchedTask=@{ TaskName="SetDeviceProperties_DisableUsbPciePowerSaver"; Description="Uncheck 'Allow the computer to turn off this device to save power' on all USB Controllers & PCIe Devices"; Trigger=(New-ScheduledTaskTrigger -Once -At (Get-Date)); Action=(New-ScheduledTaskAction -Execute ((gcm powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((gcm powershell).Source) -ArgumentList ('-Command Set-CimInstance -Namespace root/WMI -Query ((write SELECT`` *`` FROM`` MSPower_DeviceEnable`` WHERE`` InstanceName`` LIKE`` )+([string][char]34)+(write USB\\%)+([string][char]34)+([string][char]32)+(write OR`` InstanceName`` LIKE`` )+([string][char]34)+(write PCI\\%)+([string][char]34)) -Property @{Enable=((gv false).Value)};') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
+
+
+# ------------------------------------------------------------
+#
 # SetHomepage_Chrome  (Runs as ADMIN)
 #
 
@@ -41,18 +53,6 @@ $Args_SchedTask=@{ TaskName="SetHomepage_Edge"; Description="SetHomepage_Edge"; 
 #
 
 $Args_SchedTask=@{ TaskName="Startup_StopProcesses"; Description="Startup_StopProcesses"; Trigger=(New-ScheduledTaskTrigger -AtStartup); Action=(New-ScheduledTaskAction -Execute ((gcm powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((gcm powershell).Source) -ArgumentList ('-Command While (1) { Get-Process -Name redcloak* 2>`$Null | Stop-Process -Force <# redcloak #>; Get-Process -Name flow 2>`$Null | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name smartaudio* 2>`$Null | Stop-Process -Force <# bang & olufsen #>; Get-Process -Name tanium* 2>`$Null | Stop-Process -Force <# tanium #>; Get-Process -Name fc* 2>`$Null | Stop-Process -Force <# mcafee #>; Start-Sleep -Seconds (1); };') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
-
-
-# ------------------------------------------------------------
-#
-# SetDeviceProperties_DisableUsbPciePowerSaver  (Runs as ADMIN)
-#  |
-#  |--> Manually set trigger: At system startup
-#  |--> Manually set trigger: At log on of any user (delay task for 1 minute)
-#  |--> Manually set trigger: On workstation unlock of any user (delay task for 1 minute)
-#
-
-$Args_SchedTask=@{ TaskName="SetDeviceProperties_DisableUsbPciePowerSaver"; Description="Uncheck 'Allow the computer to turn off this device to save power' on all USB Controllers & PCIe Devices"; Trigger=(New-ScheduledTaskTrigger -Once -At (Get-Date)); Action=(New-ScheduledTaskAction -Execute ((gcm powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((gcm powershell).Source) -ArgumentList ('-Command Set-CimInstance -Namespace root/WMI -Query ((write SELECT`` *`` FROM`` MSPower_DeviceEnable`` WHERE`` InstanceName`` LIKE`` )+([string][char]34)+(write USB\\%)+([string][char]34)+([string][char]32)+(write OR`` InstanceName`` LIKE`` )+([string][char]34)+(write PCI\\%)+([string][char]34)) -Property @{Enable=((gv false).Value)};') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
 
 
 # ------------------------------------------------------------

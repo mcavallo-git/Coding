3aa976bff04c49fbe6c45846a01a70350a2c87c7
Matt Cavallo <mcavallo.git@gmail.com>
2023-08-01 08:01:30 -0400

    Update _PRTG_IPinfoSensorReadings.ps1

--- prtg/Custom Sensors/EXEXML/_PRTG_IPinfoSensorReadings.ps1
+++ prtg/Custom Sensors/EXEXML/_PRTG_IPinfoSensorReadings.ps1
@@ -15,10 +15,6 @@ If ($True) {
 
   $AccessToken = "";
 
-  $IP_Address = "";
-
-  $ISP_Name = "";
-
   $ErrorMessage_IPinfo_EmptyValue = "ERROR - IPinfo API call returned a null or empty value";
 
   $ErrorMessage_IPinfo_InvalidToken = "ERROR - Access token not found at filepath [ ${Filepath_AccessToken_IPinfo} ] - Create a token via [ https://ipinfo.io/account/token ]";
@@ -40,51 +36,61 @@ If ($True) {
     [System.Net.ServicePointManager]::SecurityProtocol = ([System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12);  # Ensure TLS 1.2 exists amongst available HTTPS Protocols
     $ProgressPreference = 'SilentlyContinue'; # Hide Invoke-WebRequest's progress bar
     $ResponseObj = (Invoke-WebRequest -UseBasicParsing -Uri ("${URL_IPinfo}"));
+    # Parse the response (should be formatted as a JSON response)
     $ResponseContent = (([String](${ResponseObj} | Select-Object -ExpandProperty "Content")).Trim());
     $ResponseJson = (${ResponseContent} | ConvertFrom-Json);
-
-    $IP_Address = (([String](${ResponseJson} | Select-Object -ExpandProperty "ip")).Trim());
-
-    $ISP_Name = (([String](${ResponseJson} | Select-Object -ExpandProperty "org")).Trim());
-
   }
 
-  # Build the output JSON as a hash table / arrays, then convert it to JSON afterward
-  $Output_HashTable = @{"prtg"=@{"result"=@();};};
+  # Set which response properties should be logged
+  $Log_JsonProps = @();
+  $Log_JsonProps += "ip";
+  $Log_JsonProps += "hostname";
+  $Log_JsonProps += "city";
+  $Log_JsonProps += "region";
+  $Log_JsonProps += "country";
+  $Log_JsonProps += "loc";
+  $Log_JsonProps += "org";
+  $Log_JsonProps += "postal";
+  $Log_JsonProps += "timezone";
 
-  $EmptyValues = 0;
+  # Walk through each of the returned data points
+  @(${Log_JsonProps}) | ForEach-Object {
 
-  # IP Address  -  Append to JSON output
-  If ([String]::IsNullOrEmpty("${IP_Address}")) {
-    $EmptyValues++;
-  } Else {
-    $Output_HashTable.prtg.result += @{ "channel"="[WAN IP] ${IP_Address}"; "decimalmode"=0; "float"=0; "limitmode"=0; "unit"="Percent"; "value"=100; };
+    $Each_JsonProp = "${_}";
+
+    # Check for each property
+    If ($Null -Eq ${ResponseJson}) {
+      $Each_JsonValue = "";
+    } Else {
+      $Each_JsonValue = (([String](${ResponseJson} | Select-Object -ExpandProperty "${Each_JsonProp}")).Trim());
+    }
+
+    # Build the output JSON as a hash table / arrays, then convert it to JSON afterward
+    $Output_HashTable = @{"prtg"=@{"result"=@();};};
+
+    # Check for invalid data
+    If ([String]::IsNullOrEmpty("${AccessToken}")) {
+      # Error - No API token has been setup (yet)
+      $Output_HashTable = @{"prtg"=@{"error"=1;"text"="${ErrorMessage_IPinfo_InvalidToken}";};};
+    } ElseIf ([String]::IsNullOrEmpty("${Each_JsonValue}")) {
+      # Error - All values found to be empty, send error in the JSON body (instead of sending empty data)
+      $Output_HashTable = @{"prtg"=@{"error"=1;"text"="${ErrorMessage_IPinfo_EmptyValue}";};};
+    } Else {
+      # Success - Use the sensor data in the results
+      $Output_HashTable.prtg.result += @{ "channel"="${Each_JsonProp}"; "decimalmode"=0; "float"=0; "limitmode"=0; "unit"="Percent"; "value"=100; };
+    }
+
+    # Convert the data to JSON
+    $Output_Json = (${Output_HashTable} | ConvertTo-Json -Depth 50 -Compress);
+
+    # Handle invalid characters in sensor names - Note that PRTG does not function if certain unicode characters are in the filename (such as a degree symbol)
+    $Output_Basename=(((("IPinfo.${Each_JsonProp}.json").Split([System.IO.Path]::GetInvalidFileNameChars()) -join '_') -Replace "[^a-zA-Z0-9-_\[\]\(\)\+\.]","_") -Replace "\.\.",".");
+    $Output_Fullpath=("${Logfile_Dirname_IPinfo}\Sensors\${Output_Basename}");
+
+    # Output the results to sensor-specific files
+    Set-Content -LiteralPath ("${Output_Fullpath}") -Value ("${Output_Json}") -NoNewline;
+
   }
-
-  # ISP Name  -  Append to JSON output
-  If ([String]::IsNullOrEmpty("${ISP_Name}")) {
-    $EmptyValues++;
-  } Else {
-    $Output_HashTable.prtg.result += @{ "channel"="[ISP Name] ${ISP_Name}"; "decimalmode"=0; "float"=0; "limitmode"=0; "unit"="Percent"; "value"=100; };
-  }
-
-  # Check for errors
-  If ([String]::IsNullOrEmpty("${AccessToken}")) {
-    # Error - No API token has been setup (yet)
-    $Output_HashTable = @{"prtg"=@{"error"=1;"text"="${ErrorMessage_IPinfo_InvalidToken}";};};
-  } ElseIf (${EmptyValues} -GE 2) {
-    # Error - All values found to be empty, send error in the JSON body (instead of sending empty data)
-    $Output_HashTable = @{"prtg"=@{"error"=1;"text"="${ErrorMessage_IPinfo_EmptyValue}";};};
-  }
-
-  $Output_Json = (${Output_HashTable} | ConvertTo-Json -Depth 50 -Compress);
-
-  # Handle invalid characters in sensor names - Note that PRTG does not function if certain unicode characters are in the filename (such as a degree symbol)
-  $Output_Basename=(((("IPinfo._.json").Split([System.IO.Path]::GetInvalidFileNameChars()) -join '_') -Replace "[^a-zA-Z0-9-_\[\]\(\)\+\.]","_") -Replace "\.\.",".");
-  $Output_Fullpath=("${Logfile_Dirname_IPinfo}\Sensors\${Output_Basename}");
-
-  # Output the results to sensor-specific files
-  Set-Content -LiteralPath ("${Output_Fullpath}") -Value ("${Output_Json}") -NoNewline;
 
 }
 

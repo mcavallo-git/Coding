e159c841810e625f87aaa1c21ae162e34a571d21
Matt Cavallo <mcavallo.git@gmail.com>
2023-08-01 10:40:36 -0400

    SetDeviceProperties_DisableUsbPciePowerSaver

--- powershell/PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger).ps1
+++ powershell/PowerShell - Scheduled Tasks (run script block (not file) as Admin, Register-ScheduledTask, New-ScheduledTaskAction, New-ScheduledTaskTrigger).ps1
@@ -45,14 +45,14 @@ $Args_SchedTask=@{ TaskName="Startup_StopProcesses"; Description="Startup_StopPr
 
 # ------------------------------------------------------------
 #
-# USB_PCIe_Devices_DisablePowerSaver  (Runs as ADMIN)
+# SetDeviceProperties_DisableUsbPciePowerSaver  (Runs as ADMIN)
 #  |
 #  |--> Manually set trigger: At system startup
 #  |--> Manually set trigger: At log on of any user (delay task for 1 minute)
 #  |--> Manually set trigger: On workstation unlock of any user (delay task for 1 minute)
 #
 
-$Args_SchedTask=@{ TaskName="USB_PCIe_Devices_DisablePowerSaver"; Description="Uncheck 'Allow the computer to turn off this device to save power' on all USB Controllers & PCIe Devices"; Trigger=(New-ScheduledTaskTrigger -Once -At (Get-Date)); Action=(New-ScheduledTaskAction -Execute ((gcm powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((gcm powershell).Source) -ArgumentList ('-Command Set-CimInstance -Namespace root/WMI -Query ((write SELECT`` *`` FROM`` MSPower_DeviceEnable`` WHERE`` InstanceName`` LIKE`` )+([string][char]34)+(write USB\\%)+([string][char]34)+([string][char]32)+(write OR`` InstanceName`` LIKE`` )+([string][char]34)+(write PCI\\%)+([string][char]34)) -Property @{Enable=((gv false).Value)};') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
+$Args_SchedTask=@{ TaskName="SetDeviceProperties_DisableUsbPciePowerSaver"; Description="Uncheck 'Allow the computer to turn off this device to save power' on all USB Controllers & PCIe Devices"; Trigger=(New-ScheduledTaskTrigger -Once -At (Get-Date)); Action=(New-ScheduledTaskAction -Execute ((gcm powershell).Source) -Argument (("-Command `"Start-Process -Filepath ((gcm powershell).Source) -ArgumentList ('-Command Set-CimInstance -Namespace root/WMI -Query ((write SELECT`` *`` FROM`` MSPower_DeviceEnable`` WHERE`` InstanceName`` LIKE`` )+([string][char]34)+(write USB\\%)+([string][char]34)+([string][char]32)+(write OR`` InstanceName`` LIKE`` )+([string][char]34)+(write PCI\\%)+([string][char]34)) -Property @{Enable=((gv false).Value)};') -Verb 'RunAs' -Wait -PassThru | Out-Null;`""))); User="System";}; Register-ScheduledTask @Args_SchedTask;
 
 
 # ------------------------------------------------------------

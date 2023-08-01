fd7999b34638a25953d5f0dfa0d744ccdf1e642e
Matt Cavallo <mcavallo.git@gmail.com>
2023-08-01 13:10:00 -0400

    Update SetDeviceProperties_NonRemovableFingerprintScanners.ps1

--- powershell/DeviceManagement/SetDeviceProperties_NonRemovableFingerprintScanners.ps1
+++ powershell/DeviceManagement/SetDeviceProperties_NonRemovableFingerprintScanners.ps1
@@ -8,13 +8,18 @@ If ($True) {
       Path="Registry::HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Enum\${InstanceId}";
       Name="Capabilities";
       Type="DWord";
+      Value=0x00000060;
     };
     If ((Test-Path -Path (${RegEdit}.Path)) -Eq $True) {
       $GetEachItemProp = (Get-ItemPropertyValue -LiteralPath (${RegEdit}.Path) -Name (${RegEdit}.Name) -ErrorAction ("Stop"));
-      ${RegEdit}.Value = ($GetEachItemProp);
-      $(0x00000002, 0x00000004) | ForEach-Object {
-        ${RegEdit}.Value = ((${Regedit}.Value) - ((${Regedit}.Value) -band ${_}));
+
+      If ($False) {
+        ${RegEdit}.Value = ($GetEachItemProp);
+        $(0x00000002, 0x00000004) | ForEach-Object {
+          ${RegEdit}.Value = ((${Regedit}.Value) - ((${Regedit}.Value) -band ${_}));
+        }
       }
+
       If ((${GetEachItemProp}) -NE (${RegEdit}.Value)) {
         Set-ItemProperty -LiteralPath (${RegEdit}.Path) -Name (${RegEdit}.Name) -Value (${RegEdit}.Value);
       }

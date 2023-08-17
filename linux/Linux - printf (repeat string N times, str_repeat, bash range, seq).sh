d84d481db5b063cd0f026ea1219991e0e1b256ea
Matt Cavallo <mcavallo.git@gmail.com>
2023-08-17 04:07:37 -0400

    Update Linux - printf (repeat string N times, str_repeat, bash range, seq).sh

--- linux/Linux - printf (repeat string N times, str_repeat, bash range, seq).sh
+++ linux/Linux - printf (repeat string N times, str_repeat, bash range, seq).sh
@@ -18,9 +18,21 @@ CHAR="-"; COUNT=60; printf -- "${CHAR}%.0s" $(seq ${COUNT});  # Repeat "-" 60 ti
 
 # ------------------------------------------------------------
 #
+# Example - printf
+#
+
+printf -- '-%.0s' {1..10}; printf " Script started at [ $(date --utc +'%Y-%m-%dT%H:%M:%S.%NZ';) ]\n";
+
+
+# ------------------------------------------------------------
+#
 # Citation(s)
 #
 #   linuxhint.com  |  "Bash Range"  |  https://linuxhint.com/bash_range/
+#
+#   stackoverflow.com  |  "shell - How can I repeat a character in Bash? - Stack Overflow"  |  https://stackoverflow.com/a/5349842
+#
+#   unix.stackexchange.com  |  "bash - Dashes in printf - Unix & Linux Stack Exchange"  |  https://unix.stackexchange.com/a/22765
 #
 #   www.cyberciti.biz  |  "How to repeat a character 'n' times in Bash - nixCraft"  |  https://www.cyberciti.biz/faq/repeat-a-character-in-bash-script-under-linux-unix/
 #

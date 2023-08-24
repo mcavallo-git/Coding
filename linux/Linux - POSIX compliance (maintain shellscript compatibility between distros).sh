f49b94a0be843fc14008ea4978ae74dd9394bd01
Matt Cavallo <mcavallo.git@gmail.com>
2023-08-24 16:28:46 -0400

    Create Linux - POSIX compliance (maintain shellscript compatibility between distros).sh

---
+++ linux/Linux - POSIX compliance (maintain shellscript compatibility between distros).sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# ------------------------------------------------------ #
+#
+# Linux - POSIX compliance (maintain shellscript compatibility between distros)
+#
+# ------------------------------------------------------ #
+
+# See Citation(s), below
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   en.wikipedia.org  |  "POSIX - Wikipedia"  |  https://en.wikipedia.org/wiki/POSIX
+#
+#   stackoverflow.com  |  "Which shell should be used for Linux/MacOS/UNIX best compatibility? - Stack Overflow"  |  https://stackoverflow.com/a/23437358
+#
+# ------------------------------------------------------------

8b1af081b0e0b63006a61e5ecd0252096b308c98
Matt Cavallo <mcavallo.git@gmail.com>
2024-10-01 01:01:45 -0400

    Create EdgeOS (UniFi) - Force DDNS update.sh

---
+++ ubiquiti/EdgeOS (UniFi) - Force DDNS update.sh
@@ -0,0 +1,16 @@
+#!/bin/vbash
+# ------------------------------------------------------------
+#
+# EdgeOS (UniFi) - Force DDNS update
+#
+
+/usr/sbin/inadyn -n -1 --force -f /run/ddns-eth4-inadyn.conf
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   www.reddit.com  |  "UDM force DDNS Update : r/Ubiquiti"  |  https://www.reddit.com/r/Ubiquiti/comments/gbppr9/udm_force_ddns_update/
+#
+# ------------------------------------------------------------

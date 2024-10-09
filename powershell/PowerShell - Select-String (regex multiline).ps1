e2b17faaf7dc2d0c87e569f9f3c9ef23c9582667
Matt Cavallo <mcavallo.git@gmail.com>
2024-10-08 21:19:26 -0400

    Update PowerShell - Select-String (regex multiline).ps1

--- powershell/PowerShell - Select-String (regex multiline).ps1
+++ powershell/PowerShell - Select-String (regex multiline).ps1
@@ -13,6 +13,10 @@ Write-Output "Line 1" "Line 2" "Line 3" "Line 4" "Line 5" | Select-String -Patte
 Write-Output "Line 1" "Line 2" "Line 3" "Line 4" "Line 5" | Select-String -CaseSensitive -Pattern '^[a-z]';  # Returns ""
 
 
+<# Select-String using Regex matching w/ Capture Groups (requires a Dockerfile) #>
+( Get-Content "_PATH_TO\Dockerfile" | Select-String -Pattern '^FROM (.+) as base' | Select-Object -First 1 | ForEach-Object { ${_}.Matches.Captures.Groups[1].Value } );
+
+
 <# Using "-Context PRE,POST" to get the lines before/after a matched line #>
 Write-Output "Line 1" "Line 2" "Line 3" "Line 4" "Line 5" `
 | Select-String -Pattern '^Line 3$' -Context 1,1 `

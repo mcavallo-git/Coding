42fa62cd0533d852f54fe1b0364de9bbc6ad4b65
Matt Cavallo <mcavallo.git@gmail.com>
2025-01-08 22:50:44 -0500

    Update PowerShell - Get-Set Metadata on file(s) using exiftool (last-modified date, media created date).ps1

--- powershell/PowerShell - Get-Set Metadata on file(s) using exiftool (last-modified date, media created date).ps1
+++ powershell/PowerShell - Get-Set Metadata on file(s) using exiftool (last-modified date, media created date).ps1
@@ -4,18 +4,22 @@
 #
 
 # Find files (in the current directory) matching "*.mov" and mirror their creation & modified dates onto a neighboring "*.mp4" file with the same basename
-Get-ChildItem -File | Where-Object { ($_.Name -Like "*.mov") } | ForEach-Object {
-  $Basename=($_.BaseName);
-  $LastAccessTime=($_.LastAccessTime);
-  $LastWriteTime=($_.LastWriteTime);
-  $MediaCreatedDate=(exiftool -s -s -s -createdate "${Basename}.mov");
-  Write-Host "Syncing creationg & modified dates from `"${Basename}.mov`" to `"${Basename}.mp4`"";
-  exiftool -overwrite_original -createdate="${MediaCreatedDate}" "${Basename}.mp4";
-  Get-ChildItem "${Basename}.mp4" | % {
-    $_.LastWriteTime = ${LastWriteTime};
-    $_.LastAccessTime = ${LastAccessTime};
+If ($True) {
+  $FromExtension = "mov";
+  $ToExtension = "mp4";
+  Get-ChildItem -File | Where-Object { ($_.Name -Like "*.${FromExtension}") } | ForEach-Object {
+    $Basename=($_.BaseName);
+    $LastAccessTime=($_.LastAccessTime);
+    $LastWriteTime=($_.LastWriteTime);
+    $MediaCreatedDate=(exiftool -s -s -s -createdate "${Basename}.${FromExtension}");
+    Write-Host "Syncing creationg & modified dates from `"${Basename}.${FromExtension}`" to `"${Basename}.${ToExtension}`"";
+    exiftool -overwrite_original -createdate="${MediaCreatedDate}" "${Basename}.${ToExtension}";
+    Get-ChildItem "${Basename}.${ToExtension}" | % {
+      $_.LastWriteTime = ${LastWriteTime};
+      $_.LastAccessTime = ${LastAccessTime};
+    };
   };
-};
+}
 
 
 # ------------------------------------------------------------

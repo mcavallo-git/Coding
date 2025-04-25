483922a4ab61520edb5603c32b5459335f502e17
Matt Cavallo <mcavallo.git@gmail.com>
2025-04-25 02:29:05 -0400

    HWiNFO updates

--- visual basic/HWiNFO64_SensorLogging_AtStartup_AsAdmin.ps1
+++ visual basic/HWiNFO64_SensorLogging_AtStartup_AsAdmin.ps1
@@ -16,7 +16,7 @@ If ((GV True).Value) {
       If (((GV True).Value) -Eq ((GV IS_LOGON_SCRIPT).Value)) {
         Start-Process -Filepath ((gc env:\\ProgramFiles)+(write \AutoHotkey\v2\AutoHotkey.exe)) -ArgumentList ((gc env:\\REPOS_DIR)+(write \Coding\ahk\Archive\Windows_RefreshTrayIcons.ahkv2)) -NoNewWindow;
       };
-      Start-Process -Filepath ((write C:\Program)+([string][char]32)+(write Files\HWiNFO64\HWiNFO64.EXE)) -ArgumentList ((write -l)+((GV Logfile).Value)) -NoNewWindow;
+      Start-Process -Filepath ((write C:\Program)+([string][char]32)+(write Files\HWiNFO64\HWiNFO64.EXE)) -ArgumentList ((write -l)+((GV Logfile).Value));
     };
   };
 };

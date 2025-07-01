4b03eb885e715b23171599335105e25bd7f63f44
Matt Cavallo <mcavallo.it@gmail.com>
2025-07-01 05:20:46 -0400

    UFW - Delete rules using rule syntax

--- linux/Linux - ufw.sh
+++ linux/Linux - ufw.sh
@@ -5,7 +5,7 @@
 #
 # ------------------------------------------------------------
 #
-# General Ruleset
+# UFW - General Ruleset
 #
 
 ufw allow 22/tcp;  # allow ssh/sftp-requests (22)
@@ -26,6 +26,9 @@ ufw enable; # enable ufw module
 
 
 # ------------------------------------------------------------
+#
+# UFW - Delete a rule by rule ID
+#
 
 ufw status numbered; # List UFW rules by ID
 
@@ -34,15 +37,30 @@ ufw delete RULE_ID;  # Delete UFW rules by ID
 
 # ------------------------------------------------------------
 #
+# UFW - Delete rules using rule syntax
+#
+
+ufw allow proto tcp from 127.0.0.1 to any port 80; # Create a rule using rule syntax
+
+ufw status; # List UFW rules
+
+ufw delete allow proto tcp from 127.0.0.1 to any port 80; # Delete the rule using rule syntax
+
+ufw status; # List UFW rules
+
+
+# ------------------------------------------------------------
+#
 # Citation(s)
 #
+#   askubuntu.com  |  "How to enable ufw firewall to allow icmp response?"  |  https://askubuntu.com/a/10314
 #
-#		digitalocean.com  |  "UFW Essentials: Common Firewall Rules and Commands"  |  https://www.digitalocean.com/community/tutorials/ufw-essentials-common-firewall-rules-and-commands
+#   digitalocean.com  |  "How To Set Up a Firewall with UFW on Ubuntu 18.04"  |  https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-18-04
 #
-#		digitalocean.com  |  "How To Set Up a Firewall with UFW on Ubuntu 18.04"  |  https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-with-ufw-on-ubuntu-18-04
+#   digitalocean.com  |  "UFW Essentials: Common Firewall Rules and Commands"  |  https://www.digitalocean.com/community/tutorials/ufw-essentials-common-firewall-rules-and-commands
 #
-#		askubuntu.com  |  "How to enable ufw firewall to allow icmp response?"  |  https://askubuntu.com/a/10314
+#   manpages.ubuntu.com  |  "Ubuntu manuals - NAME"  |  https://manpages.ubuntu.com/manpages/trusty/man8/ufw.8.html
 #
-#		manpages.ubuntu.com  |  "Ubuntu manuals - NAME"  |  https://manpages.ubuntu.com/manpages/trusty/man8/ufw.8.html
+#   superuser.com  |  "Ubuntu: Delete several ufw rules - Super User"  |  https://superuser.com/a/1564148
 #
 # ------------------------------------------------------------

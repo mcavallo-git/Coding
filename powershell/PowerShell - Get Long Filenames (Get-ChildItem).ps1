ed158c9b27f0baecdef32cf644a856024e228a59
Matt Cavallo <mcavallo.it@gmail.com>
2025-11-19 04:02:53 -0500

    Update PowerShell - Get Long Filenames (Get-ChildItem).ps1

--- powershell/PowerShell - Get Long Filenames (Get-ChildItem).ps1
+++ powershell/PowerShell - Get Long Filenames (Get-ChildItem).ps1
@@ -7,7 +7,7 @@
 
 # Get filenames in the current directory which are longer than [MINIMUM_LENGTH] characters
 
-$MINIMUM_LENGTH=230; Get-ChildItem -Recurse | Where-Object {$_.FullName.Length -ge ${MINIMUM_LENGTH}} | Select-Object -Property "FullName";
+$MINIMUM_LENGTH=220; Get-ChildItem -Recurse | Where-Object {$_.FullName.Length -ge ${MINIMUM_LENGTH}} | Select-Object -Property "FullName";
 
 
 # ------------------------------------------------------------

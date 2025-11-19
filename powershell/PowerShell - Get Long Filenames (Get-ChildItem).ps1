f9a14ee264d2a827ca24fd813449c0273a126fa3
Matt Cavallo <mcavallo.it@gmail.com>
2025-11-19 03:14:55 -0500

    Create PowerShell - Get Long Filenames (Get-ChildItem).ps1

---
+++ powershell/PowerShell - Get Long Filenames (Get-ChildItem).ps1
@@ -0,0 +1,19 @@
+# ------------------------------------------------------------
+#
+# PowerShell - Get Long Filenames
+#
+# ------------------------------------------------------------
+
+
+# Get filenames in the current directory which are longer than [MINIMUM_LENGTH] characters
+
+$MINIMUM_LENGTH=230; Get-ChildItem -Recurse | Where-Object {$_.FullName.Length -ge ${MINIMUM_LENGTH}} | Select-Object -Property "FullName";
+
+
+# ------------------------------------------------------------
+#
+#	Citation(s)
+#
+#   stackoverflow.com  |  "How do I find files with a path length greater than 260 characters in Windows? - Stack Overflow"  |  https://stackoverflow.com/a/28914214
+#
+# ------------------------------------------------------------

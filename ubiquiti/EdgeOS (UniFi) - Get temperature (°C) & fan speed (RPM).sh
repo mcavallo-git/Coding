cdda11a18bcaa1d427959339dbcf4eaf63b82af1
Matt Cavallo <mcavallo.it@gmail.com>
2025-12-01 06:30:08 -0500

    Create EdgeOS (UniFi) - Get temperature (°C) & fan speed (RPM).sh

---
+++ ubiquiti/EdgeOS (UniFi) - Get temperature (°C) & fan speed (RPM).sh
@@ -0,0 +1,16 @@
+#!/bin/vbash
+# ------------------------------------------------------------
+#
+# EdgeOS (UniFi) - Get temperature (°C) & fan speed (RPM)
+#
+
+sensors
+
+
+# ------------------------------------------------------------
+#
+# Citation(s)
+#
+#   community.ui.com  |  "ALARM Fan speed below minimum | Ubiquiti Community"  |  https://community.ui.com/questions/ALARM-Fan-speed-below-minimum/533c7b64-1e87-4ba7-84e1-3acd8bb768cf
+#
+# ------------------------------------------------------------
